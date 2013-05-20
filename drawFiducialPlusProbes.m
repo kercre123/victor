913516d9f3af40db4451b3a723a8af53aeb64e78
Andrew Stein <astein@tandentvision.com>
2013-05-20 11:14:23 -0400

    New method for drawing/simulating fiducials

--- drawFiducialPlusProbes.m
+++ drawFiducialPlusProbes.m
@@ -1,11 +1,11 @@
-function drawFiducialPlusProbes(varargin)
+function [edgeCenterPairs, symmetryPairs] = drawFiducialPlusProbes(varargin)
 
 blockHalfWidth = 5;
 dotRadius = blockHalfWidth - 1;
 viewAngle = 60;
 rotationAngle = 20;
 maxAngle = 70;
-numSymmetryProbes = 8;
+numSymmetryProbes = 4;
 numEdgeCenterProbes = 8;
 
 parseVarargin(varargin{:});
@@ -40,27 +40,35 @@ set(gca, 'XTick', -(blockHalfWidth+.5):(blockHalfWidth+.5), ...
 
 % Center/Edge Probes
 edgeCenterAngles = linspace(0, 2*pi, numEdgeCenterProbes+1);
+edgeCenterPairs = zeros(numEdgeCenterProbes,2);
 for i = 1:numEdgeCenterProbes
-    rectangle('Pos', ...
-        [round(blockHalfWidth*cos(edgeCenterAngles(i)))-.5 ...
-        round(blockHalfWidth*sin(edgeCenterAngles(i)))-.5 ...
-        1 1], 'EdgeColor', 'b');
+    edgeX = round(blockHalfWidth*cos(edgeCenterAngles(i)));
+    edgeY = round(blockHalfWidth*sin(edgeCenterAngles(i)));
+    rectangle('Pos', [edgeX-.5 edgeY-.5 1 1], 'EdgeColor', 'b');
     
-    rectangle('Pos', ...
-        [round(centerRadius/2*cos(edgeCenterAngles(i)))-.5 ...
-        round(centerRadius/2*sin(edgeCenterAngles(i)))-.5 ...
-        1 1], 'EdgeColor', 'b');
+    cenX = round(centerRadius/2*cos(edgeCenterAngles(i)));
+    cenY = round(centerRadius/2*sin(edgeCenterAngles(i)));
+    rectangle('Pos', [cenX-.5 cenY-.5 1 1], 'EdgeColor', 'b');
+    
+    edgeCenterPairs(i,:) = sub2ind([blockSize blockSize], ...
+        [edgeY cenY]+blockHalfWidth+1, [edgeX cenX]+blockHalfWidth+1);
+        
 end
 
 
 % Symmetry Probes
 symmetryRadius = (dotRadius + centerRadius)/2;
-symmetryAngles = linspace(0, 2*pi, numSymmetryProbes+1);
+symmetryAngles = linspace(0, pi, numSymmetryProbes+1);
+symmetryPairs = zeros(numSymmetryProbes, 2);
 for i = 1:numSymmetryProbes
-    symmetryProbes(i) = rectangle('Pos', ...
-        [round(symmetryRadius*cos(symmetryAngles(i)))-.5 ...
-        round(symmetryRadius*sin(symmetryAngles(i)))-.5 ...
-        1 1], 'EdgeColor', 'g');
+    symX = round(symmetryRadius*cos(symmetryAngles(i)+[0 pi]));
+    symY = round(symmetryRadius*sin(symmetryAngles(i)+[0 pi]));
+    
+    rectangle('Pos', [symX(1)-.5 symY(1)-.5 1 1], 'EdgeColor', 'g');
+    rectangle('Pos', [symX(2)-.5 symY(2)-.5 1 1], 'EdgeColor', 'g');
+    
+    symmetryPairs(i,:) = sub2ind([blockSize blockSize], ...
+        symY+blockHalfWidth+1, symX+blockHalfWidth+1);
 end
 
 set(gca, 'XLim', (blockHalfWidth+1)*[-1 1], 'YLim', (blockHalfWidth+1)*[-1 1]);
