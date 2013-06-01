cb2a4ac19c97d126c82bd645ab29ecdc91ce555d
Andrew Stein <astein@tandentvision.com>
2013-05-31 21:09:39 -0400

    Adding helper function to be used with CameraCapture for doing live detection and display.

---
+++ detectAndDisplay.m
@@ -0,0 +1,38 @@
+function detectAndDisplay(img)
+
+if isempty(img) || any(size(img)==0)
+    warning('Empty image passed in!');
+    return
+end
+
+detections = simpleDetector(img, [], 'thresholdFraction', 1, ...
+    'computeTransformFromBoundary', true, 'usePerimeterCheck', false);
+
+numDetections = length(detections);
+if numDetections > 0
+
+    namedFigure('LiveDetections');
+    hold off
+    h_img = imagesc(img);
+    axis image 
+    hold on
+    
+    h_axes = get(h_img, 'Parent');
+
+    for i = 1:numDetections
+        draw(detections{i}, h_axes);
+    end
+    
+    set(h_axes, 'XColor', 'g', 'YColor', 'g', 'LineWidth', 5, ...
+        'Box', 'on', 'XTick', [], 'YTick', []);
+else
+    h_axes = findobj(namedFigure('LiveDetections'), 'Type', 'axes');
+    if ~isempty(h_axes)
+        set(h_axes, 'XColor', 'r', 'YColor', 'r');
+    end
+end
+
+
+
+
+end
