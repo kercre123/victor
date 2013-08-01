9338aec54cfa5ddac5dd1eda3ef7d4292ae2ab7b
Andrew Stein <andrew@anki.com>
2013-08-01 15:51:48 -0700

    Removing non cozmo-specific code.

--- matlab/@UnitQuaternion/SphericalLinearInterp.m
+++
@@ -1,9 +0,0 @@
-function Qi = SphericalLinearInterp(P, Q, t)
-% SLERP, Spherical Linear Interpolation of Quaternions P and Q at t = [0,1].
-
-assert(isa(P, 'UnitQuaternion') && isa(Q, 'UnitQuaternion') && isscalar(t) ...
-    && isa(t, 'double'), 'Expecting two UnitQuaternions and a scalar double.');
-
-Qi = P * (conj(P) * Q)^t;
-
-end
