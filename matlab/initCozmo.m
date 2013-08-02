d330b11b69ea754706baa42b217d845b83d5000a
Peter Barnum <peter@anki.com>
2013-08-01 17:43:58 -0700

    Changed from "externals" to "external"

--- matlab/initCozmo.m
+++ matlab/initCozmo.m
@@ -3,7 +3,7 @@ addpath mex
 
 rootDir = fileparts(fileparts(fileparts(which('initCozmo.m'))));
 
-dirs = {'coretech-common', 'coretech-vision', 'coretech-math', 'coretech-externals'};
+dirs = {'coretech-common', 'coretech-vision', 'coretech-math', 'coretech-external'};
 for i = 1:length(dirs)
     addpath(genpath(fullfile(rootDir, dirs{i}, 'matlab')));
 end
