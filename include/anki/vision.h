dabc760e5e69020899e6b8828b35b16872a34cd0
Peter Barnum <peter@anki.com>
2013-08-02 12:14:36 -0700

    Added c++ source and projects

---
+++ include/anki/vision.h
@@ -0,0 +1,8 @@
+#ifndef _ANKICORETECH_VISION_VISION_H_
+#define _ANKICORETECH_VISION_VISION_H_
+
+#include "anki/vision/config.h"
+
+
+
+#endif //_ANKICORETECH_VISION_VISION_H_
---
+++ include/anki/vision/config.h
@@ -0,0 +1,11 @@
+#ifndef _ANKICORETECH_VISION_CONFIG_H_
+#define _ANKICORETECH_VISION_CONFIG_H_
+
+#include "anki/common/config.h"
+
+#if defined(ANKICORETECH_USE_OPENCV)
+#include "opencv2/opencv.hpp"
+#endif
+
+
+#endif // _ANKICORETECH_VISION_CONFIG_H_
