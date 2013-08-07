f9b1e4269f51379434a160cabd94e8f7d158a00d
Peter Barnum <peter@anki.com>
2013-08-07 12:24:37 -0700

    Added msvc and Xcode projects for library and test. The Xcode project currently doesn't work, so I'll probably try it again.

---
+++ include/anki/math.h
@@ -0,0 +1,7 @@
+#ifndef _ANKICORETECH_VISION_VISION_H_
+#define _ANKICORETECH_VISION_VISION_H_
+
+#include "anki/math/config.h"
+#include "anki/common.h"
+
+#endif //_ANKICORETECH_VISION_VISION_H_
---
+++ src/math.cpp
@@ -0,0 +1,7 @@
+
+#include "anki/math.h"
+
+void doNothing()
+{
+  printf("I'm doing nothing\n");
+}
