16be6c15e1e813dd3d03ce3d79c1d1a13907d1f1
Andrew Stein <andrew@anki.com>
2013-08-08 16:22:26 -0700

    Adding additional path to initCozmo

--- matlab/initCozmo.m
+++ matlab/initCozmo.m
@@ -4,7 +4,8 @@ addpath(fullfile(cozmoDir, 'mex'));
 
 rootDir = fileparts(fileparts(cozmoDir));
 
-dirs = {'coretech-common', 'coretech-vision', 'coretech-math', 'coretech-external'};
+dirs = {'coretech-common', 'coretech-vision', 'coretech-math', ...
+    'coretech-external', 'products-cozmo'};
 for i = 1:length(dirs)
     addpath(genpath(fullfile(rootDir, dirs{i}, 'matlab')));
 end
