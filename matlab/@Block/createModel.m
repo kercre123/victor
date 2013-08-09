4f1df9933946d2b4777625c83e771977f2bf9d79
Andrew Stein <andrew@anki.com>
2013-08-09 16:19:50 -0700

    Making block coordinates w.r.t. origin at the center of the block.

--- matlab/@Block/createModel.m
+++ matlab/@Block/createModel.m
@@ -17,80 +17,80 @@ switch(this.blockType)
         scale = [63 31 75];
         this.markers = cell(1,2);
         
-        defineFaceHelper(this, 'front', 1, [19 -e 44.5], firstMarkerID);
-        defineFaceHelper(this, 'back', 5, [45.5 scale(2)+e 44.5], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [19 -e 44.5], scale, firstMarkerID);
+        defineFaceHelper(this, 'back', 5, [45.5 scale(2)+e 44.5], scale, firstMarkerID);
         
     case 15 % Blue 2x1x4 Block, two marker faces
         this.color = 'b';
         scale = [63 31 75];
         this.markers = cell(1,2);
         
-        defineFaceHelper(this, 'front', 1, [19 -e 44], firstMarkerID);
-        defineFaceHelper(this, 'back', 5, [44 scale(2)+e 43], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [19 -e 44], scale, firstMarkerID);
+        defineFaceHelper(this, 'back', 5, [44 scale(2)+e 43], scale, firstMarkerID);
         
     case 20 % Red 2x1x4 Block, two marker faces
         this.color = 'r';
         scale = [63 31 75];
         this.markers = cell(1,2);
         
-        defineFaceHelper(this, 'front', 1, [19 -e 44], firstMarkerID);
-        defineFaceHelper(this, 'back', 5, [44 scale(2)+e 43], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [19 -e 44], scale, firstMarkerID);
+        defineFaceHelper(this, 'back', 5, [44 scale(2)+e 43], scale, firstMarkerID);
         
     case 25 % Blue 2x1x5 Block, two marker faces
         this.color = 'b';
         scale = [63 31 94.5];
         this.markers = cell(1,2);
         
-        defineFaceHelper(this, 'front', 1, [18 -e 44], firstMarkerID);
-        defineFaceHelper(this, 'back', 5, [45 scale(2)+e 44], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [18 -e 44], scale, firstMarkerID);
+        defineFaceHelper(this, 'back', 5, [45 scale(2)+e 44], scale, firstMarkerID);
             
     case 30 % Red 2x1x5 Block, two marker faces
         this.color = 'r';
         scale = [63 31 94.5];
         this.markers = cell(1,2);
         
-        defineFaceHelper(this, 'front', 1, [17 -e 43.5], firstMarkerID);
-        defineFaceHelper(this, 'back', 5, [45.5 scale(2)+e 44], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [17 -e 43.5], scale, firstMarkerID);
+        defineFaceHelper(this, 'back', 5, [45.5 scale(2)+e 44], scale, firstMarkerID);
                 
     case 35 % Red 2x2x3 Block
         this.color = 'r';
         scale = [63 63 56];
         this.markers = cell(1, 4);
         
-        defineFaceHelper(this, 'front', 1, [19 -e 38], firstMarkerID);
-        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38], firstMarkerID);
-        defineFaceHelper(this, 'back',  8, [44.5 scale(2)+e 38.5], firstMarkerID);
-        defineFaceHelper(this, 'left', 10, [-e 45 38.5], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [19 -e 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'back',  8, [44.5 scale(2)+e 38.5], scale, firstMarkerID);
+        defineFaceHelper(this, 'left', 10, [-e 45 38.5], scale, firstMarkerID);
                 
     case 40 % Green 2x2x3 Block
         this.color = 'g';
         scale = [63 63 56];
         this.markers = cell(1, 4);
         
-        defineFaceHelper(this, 'front', 1, [18.5 -e 38.5], firstMarkerID);
-        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38.5], firstMarkerID);
-        defineFaceHelper(this, 'back',  8, [44.5 scale(2)+e 38], firstMarkerID);
-        defineFaceHelper(this, 'left', 10, [-e 45 38.5], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [18.5 -e 38.5], scale, firstMarkerID);
+        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38.5], scale, firstMarkerID);
+        defineFaceHelper(this, 'back',  8, [44.5 scale(2)+e 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'left', 10, [-e 45 38.5], scale, firstMarkerID);
                 
     case 45 % Blue 3x2x3 Block
         this.color = 'b';
         scale = [94 63 56];
         this.markers = cell(1, 4);
         
-        defineFaceHelper(this, 'front', 1, [33.5 -e 38], firstMarkerID);
-        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38], firstMarkerID);
-        defineFaceHelper(this, 'back',  8, [61 scale(2)+e 38], firstMarkerID);
-        defineFaceHelper(this, 'left', 10, [-e 45 38.5], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [33.5 -e 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'back',  8, [61 scale(2)+e 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'left', 10, [-e 45 38.5], scale, firstMarkerID);
         
     case 50 % Yellow 4x2x3 Block
         this.color = 'y';
         scale = [126 63 56];
         this.markers = cell(1, 4);
         
-        defineFaceHelper(this, 'front', 1, [48 -e 38.5], firstMarkerID);
-        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38], firstMarkerID);
-        defineFaceHelper(this, 'back',  8, [74 scale(2)+e 38], firstMarkerID);
-        defineFaceHelper(this, 'left', 10, [-e 44 39], firstMarkerID);
+        defineFaceHelper(this, 'front', 1, [48 -e 38.5], scale, firstMarkerID);
+        defineFaceHelper(this, 'right', 5, [scale(1)+e 18 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'back',  8, [74 scale(2)+e 38], scale, firstMarkerID);
+        defineFaceHelper(this, 'left', 10, [-e 44 39], scale, firstMarkerID);
 
     case 60 % Webot Simulated Red 1x1 Block
         scale = makeWebotBlockHelper(this, 'r', firstMarkerID);
@@ -112,11 +112,11 @@ X = scale(1)*[0 1 1 0; 0 1 1 0; 0 0 0 0; 1 1 1 1; 0 1 1 0; 0 1 1 0]';
 Y = scale(2)*[0 0 1 1; 0 0 1 1; 0 0 1 1; 0 0 1 1; 1 1 1 1; 0 0 0 0]';
 Z = scale(3)*[0 0 0 0; 1 1 1 1; 0 1 1 0; 0 1 1 0; 0 0 1 1; 0 0 1 1]';
 
-this.model = [X(:) Y(:) Z(:)];
+this.model = [X(:)-scale(1)/2 Y(:)-scale(2)/2 Z(:)-scale(3)/2];
 
 end
 
-function defineFaceHelper(this, whichFace, faceType, origin, firstMarkerID)
+function defineFaceHelper(this, whichFace, faceType, origin, scale, firstMarkerID)
 
 switch(whichFace)
     case 'front'
@@ -137,7 +137,7 @@ end
 
 index = this.faceTypeToIndex.Count + 1;
 this.faceTypeToIndex(faceType) = index;
-pose = Pose(R, origin);
+pose = Pose(R, origin-scale/2);
 
 assert(index <= length(this.markers), 'Index for markers too large.');
 this.markers{index} = BlockMarker3D(this, faceType, pose, ...
@@ -161,10 +161,10 @@ offset2 = scale(1)/2 + halfwidth;
 % pointing away from you.  z is elevation off the ground.  However, in
 % Webot's world, x points away from, z points left/right, and y is
 % elevation off the ground.
-defineFaceHelper(this, 'front',  1, [offset1 -e offset2], firstMarkerID);
-defineFaceHelper(this, 'back',   2, [offset2 scale(2)+e offset2], firstMarkerID);
-defineFaceHelper(this, 'top',    3, [offset1 offset2 scale(3)+e], firstMarkerID);
-defineFaceHelper(this, 'bottom', 4, [offset1 offset1 -e], firstMarkerID);
-defineFaceHelper(this, 'left',   5, [-e offset2 offset2], firstMarkerID);
-defineFaceHelper(this, 'right',  6, [scale(1)+e offset1 offset2], firstMarkerID);
+defineFaceHelper(this, 'front',  1, [offset1 -e offset2], scale, firstMarkerID);
+defineFaceHelper(this, 'back',   2, [offset2 scale(2)+e offset2], scale, firstMarkerID);
+defineFaceHelper(this, 'top',    3, [offset1 offset2 scale(3)+e], scale, firstMarkerID);
+defineFaceHelper(this, 'bottom', 4, [offset1 offset1 -e], scale, firstMarkerID);
+defineFaceHelper(this, 'left',   5, [-e offset2 offset2], scale, firstMarkerID);
+defineFaceHelper(this, 'right',  6, [scale(1)+e offset1 offset2], scale, firstMarkerID);
 end
