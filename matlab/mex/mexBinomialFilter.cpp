72db42f52558cb1bf757373127b3a03a120e0e44
Peter Barnum <peter@anki.com>
2013-08-14 18:29:16 -0700

    Added tests to mexBinomialFilter to check the new mexWrapper functionality

--- matlab/mex/mexBinomialFilter.cpp
+++ matlab/mex/mexBinomialFilter.cpp
@@ -8,10 +8,22 @@
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     Anki::Matrix<u8> img = mxArray2AnkiMatrix<u8>(prhs[0]);
+    mxArray* imgM = ankiMatrix2mxArray<u8>(img);
+    Anki::Matrix<u8> imgMA = mxArray2AnkiMatrix<u8>(imgM);
+
+    assignInMatlab(imgM, "test5");
 
     Anki::Matrix<u8> filteredImg = Anki::AllocateMatrixFromHeap<u8>(img.get_size(0), img.get_size(1));
     
-    printf("%d %d\n", *img.Pointer(10,10), *img.Pointer(100,10));
+    printf("%d %d\n"
+           "%d %d\n",
+           *img.Pointer(10,10), *img.Pointer(100,10),
+           *imgMA.Pointer(10,10), *imgMA.Pointer(100,10));
+    
+    delete(img.get_data());
+    delete(imgMA.get_data());
+    
+    plhs[0] = imgM;
     
     /*
     double sigma = mxGetScalar(prhs[1]);
