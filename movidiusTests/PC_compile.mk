eeacde744c1ddb965f614440e550f44073ab4baf
Peter Barnum <peter@anki.com>
2013-08-23 15:37:53 -0700

    ImageTwoKernels compiles in the cozmo directory (and it runs, though the output may be different?)

--- movidiusTests/PC_compile.mk
+++ movidiusTests/PC_compile.mk
@@ -2,12 +2,14 @@
 
 PLATFORM = $(shell uname)
 
-PC_FOLDERS= leon_code/main_code pc_code kernels ../../common/swCommon/pcModel ../../common/swCommon/shave_code/myriad1/include ../../common/shared/include shave_code .
+PC_FOLDERS= leon_code/main_code pc_code kernels $(MDK_DIRECTORY)/common/swCommon/pcModel $(MDK_DIRECTORY)/common/swCommon/shave_code/myriad1/include $(MDK_DIRECTORY)/common/shared/include shave_code .
 
 PC_SOURCES = shave_code/ImageKernelProcessing.cpp leon_code/main_code/main_code.c \
              pc_code/pcKernelProcessing.cpp\
-             kernels/gaussVx2Shave.cpp kernels/gaussHx2Shave.cpp ../../common/swCommon/pcModel/myriadModel.cpp 
+             kernels/gaussVx2Shave.cpp kernels/gaussHx2Shave.cpp $(MDK_DIRECTORY)/common/swCommon/pcModel/myriadModel.cpp 
 
+
+			 
 ifeq ($(PLATFORM),Linux)
 Kern_Executable = output/$(APPNAME)
 else
