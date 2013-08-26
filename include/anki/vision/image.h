30d9a5f1eef15f87344241fff21bd1f4c2a24638
Andrew Stein <andrew@anki.com>
2013-08-26 09:55:22 -0700

    Adding initial camera, image, and marker2d classes (largely placeholder classes - very little fleshing out)

---
+++ include/anki/vision/image.h
@@ -0,0 +1,27 @@
+//
+//  image.h
+//  CoreTech_Vision
+//
+//  Created by Andrew Stein on 8/23/13.
+//  Copyright (c) 2013 Anki, Inc. All rights reserved.
+//
+
+#ifndef __CoreTech_Vision__image__
+#define __CoreTech_Vision__image__
+
+#include "anki/common/array2d.h"
+
+namespace Anki {
+  
+  template<class PixelType>
+  class Image : public Array2d<PixelType>
+  {
+  public:
+    
+  protected:
+    
+  }; // class Image
+  
+} // namespace Anki
+
+#endif // __CoreTech_Vision__image__
---
+++ include/anki/vision/marker2d.h
@@ -0,0 +1,27 @@
+//
+//  marker2d.h
+//  CoreTech_Vision
+//
+//  Created by Andrew Stein on 8/23/13.
+//  Copyright (c) 2013 Anki, Inc. All rights reserved.
+//
+
+#ifndef __CoreTech_Vision__marker2d__
+#define __CoreTech_Vision__marker2d__
+
+namespace Anki {
+
+  // Abstract Marker2d class for encoding and decoding our 2D barcodes.
+  // TODO: Fill this in like the Matlab class
+  class Marker2d
+  {
+  public:
+
+  protected:
+    
+    
+  }; // class Marker2d
+  
+} // namespace Anki
+
+#endif // __CoreTech_Vision__marker2d__
