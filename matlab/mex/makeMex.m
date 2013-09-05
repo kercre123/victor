ee761df693a430fd731c16a2d97e01c34cafe718
Peter Barnum <peter@anki.com>
2013-09-05 15:48:23 -0700

    Simplified makeMex.m

--- matlab/mex/makeMex.m
+++ matlab/mex/makeMex.m
@@ -12,57 +12,7 @@ if useAndrewsPaths
         '-L/usr/local/bin/', '-l opencv_core', '-l opencv_contrib', ...
         '-l opencv_calib3d');
 else % if useAndrewsPaths
-    compileVerbose = '-v';
     
-    rootDir = fileparts(fileparts(fileparts(fileparts(which('makeMex.m')))));
-    rootDir(rootDir == '\') = '/';
-    mexWrapperDir = fullfile(rootDir, 'coretech-common', 'matlab', 'mex');
-    mexWrapperDir(mexWrapperDir == '\') = '/';
-    ankiLibraryDirs = {[rootDir, '/coretech-common/include/'], [rootDir, '/coretech-vision/include/']};
-
-    if ismac()
-        useDebugLibraries = true;
-        openCvLibDir = [rootDir, '/coretech-external/build/xcode4/opencv-2.4.6.1/lib/Debug/'];
-        openCvVersionString = '';
-        
-        ankiLibDirs = {[rootDir, '/coretech-common/build/xcode4/lib/'],...
-                       [rootDir, '/coretech-vision/build/xcode4/lib/']};
-                   
-        if useDebugLibraries
-            openCvLibDir = [rootDir, '/coretech-external/build/xcode4/opencv-2.4.6.1/lib/Debug/'];
-            ankiLibs = {'-lCoreTech_Common_32Debug', '-lCoreTech_Vision_32Debug'}; 
-        else
-            openCvLibDir = [rootDir, '/coretech-external/build/xcode4/opencv-2.4.6.1/lib/Release/'];
-            ankiLibs = {'-lCoreTech_Common_32Release', '-lCoreTech_Vision_32Release'};
-        end
-    elseif ispc()
-        useDebugLibraries = false;
-        openCvVersionString = '246';
-        
-        ankiLibDirs = {[rootDir, '/coretech-common/build/msvc2012/lib/'],...
-                       [rootDir, '/coretech-vision/build/msvc2012/lib/']};
-                   
-        if useDebugLibraries
-            openCvLibDir = [rootDir, '/coretech-external/build/msvc2012/opencv-2.4.6.1/lib/Debug/'];
-            ankiLibs = {'-lCoreTech_Common_32Debug', '-lCoreTech_Vision_32Debug'}; 
-        else
-            openCvLibDir = [rootDir, '/coretech-external/build/msvc2012/opencv-2.4.6.1/lib/Release/'];
-            ankiLibs = {'-lCoreTech_Common_32Release', '-lCoreTech_Vision_32Release'};
-        end
-    end % if ismac() ... elseif ispc()
-
-    openCvModules = {'calib3d', 'contrib', 'core', 'features2d', 'flann', 'highgui', 'imgproc', 'legacy', 'ml', 'nonfree', 'objdetect', 'ocl', 'photo', 'stitching', 'superres', 'video', 'videostab'};
-    openCvIncludeDirs = cell(length(openCvModules)+1,1);
-    openCvIncludeDirs{1} = [rootDir, '/coretech-external/opencv-2.4.6.1/include/'];
-    for i = 1:length(openCvModules)
-        openCvIncludeDirs{i+1} = [rootDir, '/coretech-external/opencv-2.4.6.1/modules/', openCvModules{i}, '/include'];
-    end
+    compileAnkiMex('mexBundleAdjust.cpp');
     
-    %%
-    mex(compileVerbose, '-compatibleArrayDims', 'mexBundleAdjust.cpp', ...
-        fullfile(mexWrapperDir, 'mexWrappers.cpp'), ...
-        ['-I', mexWrapperDir], ...
-        ['-I', ankiLibraryDirs{1}], ['-I', ankiLibraryDirs{2}], ...
-        ['-I', openCvIncludeDirs{1}], ['-I', openCvIncludeDirs{2}], ['-I', openCvIncludeDirs{3}], ['-I', openCvIncludeDirs{4}], ['-I', openCvIncludeDirs{5}], ['-I', openCvIncludeDirs{6}], ['-I', openCvIncludeDirs{7}], ['-I', openCvIncludeDirs{8}], ['-I', openCvIncludeDirs{9}], ['-I', openCvIncludeDirs{10}], ['-I', openCvIncludeDirs{11}], ['-I', openCvIncludeDirs{12}], ['-I', openCvIncludeDirs{13}], ['-I', openCvIncludeDirs{14}], ['-I', openCvIncludeDirs{15}], ['-I', openCvIncludeDirs{16}], ['-I', openCvIncludeDirs{17}], ['-I', openCvIncludeDirs{18}], ...
-        ['-L', openCvLibDir], ['-lopencv_core', openCvVersionString], ['-lopencv_contrib', openCvVersionString], ['-lopencv_calib3d', openCvVersionString]); 
 end % if useAndrewsPaths ... else
