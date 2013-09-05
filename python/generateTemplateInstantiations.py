40422c2d5dc322e26e2aaca58ad933e9f428d26c
Peter Barnum <peter@anki.com>
2013-09-05 13:34:16 -0700

    * Added Python script to auto-generated Point c++ template instantiations. * Added Print() method to Point classes

--- python/generateTemplateInstantiations.py
+++ python/generateTemplateInstantiations.py
@@ -1,9 +1,14 @@
 import generateArray_h
 import generateArray_cpp
+import generatePoint_h
+import generatePoint_cpp
 
 def generateAll():
     generateArray_h.GenerateAndWriteFile()
     generateArray_cpp.GenerateAndWriteFile()
+    
+    generatePoint_cpp.GenerateAndWriteFile()
+    generatePoint_cpp.GenerateAndWriteFile()
 
 if __name__ == "__main__":
     generateAll()
