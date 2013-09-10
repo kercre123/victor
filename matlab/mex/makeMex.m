2b4194f3ff5437c5d7a7a6544c615bbb8032fa38
Andrew Stein <andrew@anki.com>
2013-09-10 13:36:44 -0700

    Update to coretech-vision's makeMex to compile all available mex files a bit smarter...

--- matlab/mex/makeMex.m
+++ matlab/mex/makeMex.m
@@ -1,39 +1,14 @@
+function makeMex(varargin)
 
-useAndrewsPaths = false;
+mexFiles = getfnames('.', '*.cpp');
+numFiles = length(mexFiles);
+for i = 1:numFiles
+    try
+        fprintf('Building %d of %d "%s"...\n', i, numFiles, mexFiles{i});
+        compileAnkiMex(mexFiles{i}, varargin{:});
+    catch E
+       warning('Failed to build "%s": %s', mexFiles{i}, E.message); 
+    end
+end
 
-if useAndrewsPaths
-    rootDir = fileparts(fileparts(fileparts(fileparts(which('makeMex.m')))));
-    mexWrapperDir = fullfile(rootDir, 'coretech-common', 'matlab', 'mex');
-
-    %%
-    mex mexRegionProps.cpp
-    %%
-    mex mexClosestIndex.cpp
-    %% 
-    mex mexHist.cpp
-    %%
-    mex('mexGaussianBlur.cpp', ...
-        fullfile(mexWrapperDir, 'mexWrappers.cpp'), ...
-        '-I/usr/local/include/', ...
-        ['-I', mexWrapperDir], ...
-        '-L/usr/local/bin/', '-l opencv_core', '-l opencv_imgproc');
-    %%
-    mex('-g', 'mexBundleAdjust.cpp', ...
-        fullfile(mexWrapperDir, 'mexWrappers.cpp'), ...
-        '-I/usr/local/include/', ['-I', mexWrapperDir], ...
-        '-L/usr/local/bin/', '-l opencv_core', '-l opencv_contrib', ...
-        '-l opencv_calib3d');
-else % if useAndrewsPaths
-
-    compileAnkiMex('mexRegionProps.cpp');
-    compileAnkiMex('mexClosestIndex.cpp');
-    compileAnkiMex('mexHist.cpp');
-    compileAnkiMex('mexGaussianBlur.cpp');
-    compileAnkiMex('mex_cp2tform_projective.cpp');
-    compileAnkiMex('mexBinomialFilter.cpp');
-    compileAnkiMex('mexDownsampleByFactor.cpp');
-    compileAnkiMex('mexComputeCharacteristicScale.cpp');
-    compileAnkiMex('mexTraceBoundary.cpp');
-
-end % if useAndrewsPaths ... else
-   
+end
