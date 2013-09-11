aa89f4fbd2b9e9dc4f73f01b2f692b4255c0ab21
Andrew Stein <andrew@anki.com>
2013-09-10 19:21:42 -0700

    Updates to mex build process

--- matlab/initVisionPath.m
+++ matlab/initVisionPath.m
@@ -2,5 +2,6 @@ function initVisionPath
 
 visionDir = fileparts(which(mfilename)); % includes 'matlab'
 addpath(genpath(visionDir));
+addpath(fullfile(visionDir, '..', 'build', 'mex'));
 
 end % initVisionPath()
