c1926361049598dd09515dd365d3bcb7ff129bd7
Peter Barnum <peter@anki.com>
2013-09-19 14:15:57 -0700

    Created DrawExampleSquaresImage(), that works the same as the matlab version.

--- include/anki/embeddedCommon/draw.h
+++ include/anki/embeddedCommon/draw.h
@@ -2,6 +2,7 @@
 #define _ANKICORETECHEMBEDDED_COMMON_DRAW_H_
 
 #include "anki/embeddedCommon/config.h"
+#include "anki/embeddedCommon/array2d.h"
 
 namespace Anki
 {
@@ -9,6 +10,17 @@ namespace Anki
   {
     // Draw a box with the two opposing corners point1 and point2
     // If lineWidth < 1, then the box will be solid. Otherwise, the inside will be filled with backgroundColor
+    template<typename Type> Result DrawRectangle(Array<Type> &image, Point<s16> point1, Point<s16> point2, const s32 lineWidth, const Type lineColor, const Type backgroundColor);
+
+    // Create a test pattern image, full of different types of squares
+    // Example:
+    // Array<u8> testSquaresImage = AllocateArrayFromHeap<u8>(480,640);
+    // DrawExampleSquaresImage(testSquaresImage);
+    // toast.Show("testSquaresImage", true);
+    template<typename Type> Result DrawExampleSquaresImage(Array<Type> &image);
+
+#pragma mark --- Implementations ---
+
     template<typename Type> Result DrawRectangle(Array<Type> &image, Point<s16> point1, Point<s16> point2, const s32 lineWidth, const Type lineColor, const Type backgroundColor)
     {
       AnkiConditionalErrorAndReturnValue(image.IsValid(),
@@ -28,16 +40,16 @@ namespace Anki
         point2inner.y = point2.y - static_cast<s16>(lineWidth);
 
         point1inner.x = static_cast<s16>(CLIP(point1inner.x, 0, image.get_size(1)-1));
-        point1inner.y = static_cast<s16>(CLIP(point1inner.y, 0, image.get_size(1)-1));
+        point1inner.y = static_cast<s16>(CLIP(point1inner.y, 0, image.get_size(0)-1));
         point2inner.x = static_cast<s16>(CLIP(point2inner.x, 0, image.get_size(1)-1));
-        point2inner.y = static_cast<s16>(CLIP(point2inner.y, 0, image.get_size(1)-1));
+        point2inner.y = static_cast<s16>(CLIP(point2inner.y, 0, image.get_size(0)-1));
       }
 
       // Make the corners fit inside the image
       point1.x = static_cast<s16>(CLIP(point1.x, 0, image.get_size(1)-1));
-      point1.y = static_cast<s16>(CLIP(point1.y, 0, image.get_size(1)-1));
+      point1.y = static_cast<s16>(CLIP(point1.y, 0, image.get_size(0)-1));
       point2.x = static_cast<s16>(CLIP(point2.x, 0, image.get_size(1)-1));
-      point2.y = static_cast<s16>(CLIP(point2.y, 0, image.get_size(1)-1));
+      point2.y = static_cast<s16>(CLIP(point2.y, 0, image.get_size(0)-1));
 
       for(s16 y=point1.y; y<=point2.y; y++) {
         Type * restrict image_rowPointer = image.Pointer(y, 0);
@@ -47,12 +59,6 @@ namespace Anki
       }
 
       if(lineWidth > 0) {
-        /*for(s16 y=point1.y+static_cast<s16>(lineWidth); y<=(point2.y-static_cast<s16>(lineWidth)); y++) {
-        Type * restrict image_rowPointer = image.Pointer(y, 0);
-        for(s16 x=point1.x+static_cast<s16>(lineWidth); x<=(point2.x-static_cast<s16>(lineWidth)); x++) {
-        image_rowPointer[x] = backgroundColor;
-        }
-        }*/
         for(s16 y=point1inner.y; y<=point2inner.y; y++) {
           Type * restrict image_rowPointer = image.Pointer(y, 0);
           for(s16 x=point1inner.x; x<=point2inner.x; x++) {
@@ -63,6 +69,101 @@ namespace Anki
 
       return RESULT_OK;
     } // template<typename Type> Result DrawRectangle(Array<Type> &image, const Point<s16> point1, const Point<s16> point2, const s32 lineWidth, const Type lineColor, const Type backgroundColor)
+
+    template<typename Type> Result DrawExampleSquaresImage(Array<Type> &image)
+    {
+      const s16 boxWidth = 60;
+      const Type lineColor = 0;
+      const Type backgroundColor = 128;
+
+      image.Set(128);
+
+      s16 upperLeft[] = {19,19};
+      for(s16 i=1; i<=5; i++) {
+        //    image = drawBox(image, upperLeft + [0, boxWidth*2*(i-1)], upperLeft + boxWidth + [0, boxWidth*2*(i-1)], i, lineColor, backgroundColor);
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+boxWidth*2*(i-1), upperLeft[0]), Point<s16>(upperLeft[1]+boxWidth+boxWidth*2*(i-1), upperLeft[0]+boxWidth), i, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [2*boxWidth,0];
+      upperLeft[0] += 2*boxWidth;
+      for(s16 i=1; i<=5; i++) {
+        //    image = drawBox(image, upperLeft + [0, boxWidth*2*(i-1)], upperLeft + boxWidth + [0, boxWidth*2*(i-1)], i+5, lineColor, backgroundColor);
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+boxWidth*2*(i-1), upperLeft[0]), Point<s16>(upperLeft[1]+boxWidth+boxWidth*2*(i-1), upperLeft[0]+boxWidth), i+5, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [2*boxWidth,0];
+      upperLeft[0] += 2*boxWidth;
+      for(s16 i=1; i<=5; i++) {
+        //    image = drawBox(image, upperLeft + [0, boxWidth*2*(i-1)], upperLeft + boxWidth + [0, boxWidth*2*(i-1)], i+10, lineColor, backgroundColor);
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+boxWidth*2*(i-1), upperLeft[0]), Point<s16>(upperLeft[1]+boxWidth+boxWidth*2*(i-1), upperLeft[0]+boxWidth), i+10, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [2*boxWidth,0];
+      upperLeft[0] += 2*boxWidth;
+      //speckWidths = 1:22;
+      const s16 maxSpeckWidth = 22;
+      for(s16 i=1; i<=maxSpeckWidth; i++) {
+        //    image = drawBox(image, upperLeft + [0, 2*sum(speckWidths(1:i))], upperLeft + [0, i + 2*sum(speckWidths(1:i))], -1, lineColor, backgroundColor);
+
+        s16 sumVal = 0;
+        for(s16 iSum=1; iSum<=i; iSum++) {
+          sumVal += iSum;
+        }
+
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+2*sumVal, upperLeft[0]), Point<s16>(upperLeft[1]+i+2*sumVal, upperLeft[0]), -1, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [10,0];
+      upperLeft[0] += 10;
+      for(s16 i=1; i<=maxSpeckWidth; i++) {
+        //    image = drawBox(image, upperLeft + [0, 2*sum(speckWidths(1:i))], upperLeft + [1, i + 2*sum(speckWidths(1:i))], -1, lineColor, backgroundColor);
+
+        s16 sumVal = 0;
+        for(s16 iSum=1; iSum<=i; iSum++) {
+          sumVal += iSum;
+        }
+
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+2*sumVal, upperLeft[0]), Point<s16>(upperLeft[1]+i+2*sumVal, upperLeft[0]+1), -1, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [10,0];
+      upperLeft[0] += 10;
+      for(s16 i=1; i<=maxSpeckWidth; i++) {
+        //    image = drawBox(image, upperLeft + [0, 2*sum(speckWidths(1:i))], upperLeft + [2, i + 2*sum(speckWidths(1:i))], -1, lineColor, backgroundColor);
+        s16 sumVal = 0;
+        for(s16 iSum=1; iSum<=i; iSum++) {
+          sumVal += iSum;
+        }
+
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+2*sumVal, upperLeft[0]), Point<s16>(upperLeft[1]+i+2*sumVal, upperLeft[0]+2), -1, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [10,0];
+      upperLeft[0] += 10;
+      for(s16 i=1; i<=maxSpeckWidth; i++) {
+        //    image = drawBox(image, upperLeft + [0, 2*sum(speckWidths(1:i))], upperLeft + [4, i + 2*sum(speckWidths(1:i))], -1, lineColor, backgroundColor);
+        s16 sumVal = 0;
+        for(s16 iSum=1; iSum<=i; iSum++) {
+          sumVal += iSum;
+        }
+
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+2*sumVal, upperLeft[0]), Point<s16>(upperLeft[1]+i+2*sumVal, upperLeft[0]+4), -1, lineColor, backgroundColor);
+      }
+
+      //upperLeft = upperLeft + [13,0];
+      upperLeft[0] += 13;
+      for(s16 i=1; i<=maxSpeckWidth; i++) {
+        //    image = drawBox(image, upperLeft + [0, 2*sum(speckWidths(1:i))], upperLeft + [8, i + 2*sum(speckWidths(1:i))], -1, lineColor, backgroundColor);
+        s16 sumVal = 0;
+        for(s16 iSum=1; iSum<=i; iSum++) {
+          sumVal += iSum;
+        }
+
+        DrawRectangle<Type>(image, Point<s16>(upperLeft[1]+2*sumVal, upperLeft[0]), Point<s16>(upperLeft[1]+i+2*sumVal, upperLeft[0]+8), -1, lineColor, backgroundColor);
+      }
+
+      return RESULT_OK;
+    } // template<typename Type> Result DrawExampleSquaresImage(Array<Type> &image)
   } // namespace Embedded
 } // namespace Anki
 
