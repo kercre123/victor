716f0802b94f8e250b6b112146aa31bb78776038
Peter Barnum <peter@anki.com>
2013-09-19 13:34:53 -0700

    Removed junk test

--- test/embedded/run_commonTests.cpp
+++ test/embedded/run_commonTests.cpp
@@ -18,10 +18,6 @@ int main()
   ::testing::InitGoogleTest(&argc, argv);
 #endif
 
-  Array<u8> toast = AllocateArrayFromHeap<u8>(480,640);
-  DrawRectangle<u8>(toast, Point<s16>(-20, 100), Point<s16>(100, 40), 5, 128, 255);
-  toast.Show("toast", true);
-
   RUN_ALL_TESTS();
 
   return 0;
