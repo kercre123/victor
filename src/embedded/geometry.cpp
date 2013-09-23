fcd10abbccbc8cddbea2d3cac6a710fbc4af8c99
Peter Barnum <peter@anki.com>
2013-09-23 16:49:05 -0700

    Added Quadrilateral class

--- src/embedded/geometry.cpp
+++ src/embedded/geometry.cpp
@@ -24,5 +24,23 @@ namespace Anki
     {
       printf("(%f,%f)->(%f,%f) ", this->left, this->top, this->right, this->bottom);
     }
+
+    template<> IN_DDR void Quadrilateral<f32>::Print() const
+    {
+      printf("{(%f,%f), (%f,%f), (%f,%f), (%f,%f)} ",
+        this->points[0].x, this->points[0].y,
+        this->points[1].x, this->points[1].y,
+        this->points[2].x, this->points[2].y,
+        this->points[3].x, this->points[3].y);
+    }
+
+    template<> IN_DDR void Quadrilateral<f64>::Print() const
+    {
+      printf("{(%f,%f), (%f,%f), (%f,%f), (%f,%f)} ",
+        this->points[0].x, this->points[0].y,
+        this->points[1].x, this->points[1].y,
+        this->points[2].x, this->points[2].y,
+        this->points[3].x, this->points[3].y);
+    }
   } // namespace Embedded
 } // namespace Anki
