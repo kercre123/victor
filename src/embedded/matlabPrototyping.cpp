7d4f8e3eb041c1492235fc0247fb08b161dc8011
Peter Barnum <peter@anki.com>
2013-09-27 15:00:53 -0700

    Moved matlabPrototyping functions into a separate file

---
+++ src/embedded/matlabPrototyping.cpp
@@ -0,0 +1,230 @@
+#include "anki/embeddedVision/fixedLengthList_vision.h"
+
+namespace Anki
+{
+  namespace Embedded
+  {
+    // Replaces the matlab code for the first three steps of SimpleDetector
+    IN_DDR Result SimpleDetector_Steps123(
+      const Array<u8> &image,
+      const s32 scaleImage_numPyramidLevels,
+      const s16 component1d_minComponentWidth, const s16 component1d_maxSkipDistance,
+      const s32 component_minimumNumPixels, const s32 component_maximumNumPixels,
+      const s32 component_sparseMultiplyThreshold, const s32 component_solidMultiplyThreshold,
+      ConnectedComponents &extractedComponents,
+      MemoryStack scratch1,
+      MemoryStack scratch2)
+    {
+      // TODO: figure out a simpler way to write code that reuses big blocks of memory
+
+      Array<u8> binaryImage(image.get_size(0), image.get_size(1), scratch2);
+
+      // 1. Compute the Scale image (use local scratch1)
+      // 2. Binarize the Scale image (store in outer scratch2)
+      {
+        PUSH_MEMORY_STACK(scratch1); // Push the current state of the scratch buffer onto the system stack
+
+        FixedPointArray<u32> scaleImage(image.get_size(0), image.get_size(1), 16, scratch1);
+
+        if(ComputeCharacteristicScaleImage(image, scaleImage_numPyramidLevels, scaleImage, scratch2) != RESULT_OK) {
+          return RESULT_FAIL;
+        }
+
+        if(ThresholdScaleImage(image, scaleImage, binaryImage) != RESULT_OK) {
+          return RESULT_FAIL;
+        }
+
+        // image.Show("image", false, false);
+        // binaryImage.Show("binaryImage", true, true);
+      } // PUSH_MEMORY_STACK(scratch1);
+
+      // 3. Compute connected components from the binary image (use local scratch2, store in outer scratch1)
+      //FixedLengthList<ConnectedComponentSegment> extractedComponents(maxConnectedComponentSegments, scratch1);
+      {
+        PUSH_MEMORY_STACK(scratch2); // Push the current state of the scratch buffer onto the system stack
+
+        if(extractedComponents.Extract2dComponents(binaryImage, component1d_minComponentWidth, component1d_maxSkipDistance, scratch2) != RESULT_OK) {
+          return RESULT_FAIL;
+        }
+
+        //{
+        //  PUSH_MEMORY_STACK(scratch1);
+        //  Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        //  DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+        //  drawnComponents.Show("drawnComponents0", false);
+        //}
+
+        extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+        //{
+        //  PUSH_MEMORY_STACK(scratch1);
+        //  Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        //  DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+        //  drawnComponents.Show("drawnComponents1", false);
+        //}
+
+        if(extractedComponents.InvalidateSmallOrLargeComponents(component_minimumNumPixels, component_maximumNumPixels, scratch2) != RESULT_OK) {
+          return RESULT_FAIL;
+        }
+
+        //{
+        //  PUSH_MEMORY_STACK(scratch1);
+        //  Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        //  DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+        //  drawnComponents.Show("drawnComponents2", false);
+        //}
+
+        extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+        //{
+        //  PUSH_MEMORY_STACK(scratch1);
+        //  Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        //  DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+        //  drawnComponents.Show("drawnComponents3", false);
+        //}
+
+        if(extractedComponents.InvalidateSolidOrSparseComponents(component_sparseMultiplyThreshold, component_solidMultiplyThreshold, scratch2) != RESULT_OK) {
+          return RESULT_FAIL;
+        }
+
+        //{
+        //  PUSH_MEMORY_STACK(scratch1);
+        //  Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        //  DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+        //  drawnComponents.Show("drawnComponents4", false);
+        //}
+
+        extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+        //{
+        //  PUSH_MEMORY_STACK(scratch1);
+        //  Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        //  DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+        //  drawnComponents.Show("drawnComponents4", true);
+        //}
+      } // PUSH_MEMORY_STACK(scratch2);
+
+      return RESULT_OK;
+    } // SimpleDetector_Steps123()
+
+    IN_DDR Result SimpleDetector_Steps1234(
+      const Array<u8> &image,
+      FixedLengthList<FiducialMarker> &markers,
+      const s32 scaleImage_numPyramidLevels,
+      const s16 component1d_minComponentWidth, const s16 component1d_maxSkipDistance,
+      const s32 component_minimumNumPixels, const s32 component_maximumNumPixels,
+      const s32 component_sparseMultiplyThreshold, const s32 component_solidMultiplyThreshold,
+      const s32 component_percentHorizontal, const s32 component_percentVertical,
+      const s32 quads_minQuadArea, const s32 quads_quadSymmetryThreshold, const s32 quads_minDistanceFromImageEdge,
+      MemoryStack scratch1,
+      MemoryStack scratch2)
+    {
+      // TODO: figure out a simpler way to write code that reuses big blocks of memory
+
+      const s32 maxConnectedComponentSegments = u16_MAX;
+      const s32 maxCandidateMarkers = 1000;
+      const s32 maxExtractedQuads = 100;
+
+      // Stored in the outermost scratch2
+      FixedLengthList<FiducialMarker> candidateMarkers(maxCandidateMarkers, scratch2);
+
+      ConnectedComponents extractedComponents; // This isn't allocated until after the scaleImage
+      {
+        PUSH_MEMORY_STACK(scratch2); // Push the current state of the scratch buffer onto the system stack
+        Array<u8> binaryImage(image.get_size(0), image.get_size(1), scratch2);
+
+        // 1. Compute the Scale image (use local scratch1)
+        // 2. Binarize the Scale image (store in outer scratch2)
+        {
+          PUSH_MEMORY_STACK(scratch1); // Push the current state of the scratch buffer onto the system stack
+
+          FixedPointArray<u32> scaleImage(image.get_size(0), image.get_size(1), 16, scratch1);
+
+          if(ComputeCharacteristicScaleImage(image, scaleImage_numPyramidLevels, scaleImage, scratch2) != RESULT_OK)
+            return RESULT_FAIL;
+
+          if(ThresholdScaleImage(image, scaleImage, binaryImage) != RESULT_OK)
+            return RESULT_FAIL;
+        } // PUSH_MEMORY_STACK(scratch1);
+
+        // 3. Compute connected components from the binary image (use local scratch2, store in outer scratch1)
+        extractedComponents = ConnectedComponents(maxConnectedComponentSegments, scratch1);
+        {
+          PUSH_MEMORY_STACK(scratch2); // Push the current state of the scratch buffer onto the system stack
+
+          if(extractedComponents.Extract2dComponents(binaryImage, component1d_minComponentWidth, component1d_maxSkipDistance, scratch2) != RESULT_OK)
+            return RESULT_FAIL;
+
+          extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+          if(extractedComponents.InvalidateSmallOrLargeComponents(component_minimumNumPixels, component_maximumNumPixels, scratch2) != RESULT_OK)
+            return RESULT_FAIL;
+
+          extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+          if(extractedComponents.InvalidateSolidOrSparseComponents(component_sparseMultiplyThreshold, component_solidMultiplyThreshold, scratch2) != RESULT_OK)
+            return RESULT_FAIL;
+
+          extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+          {
+            Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+            DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+
+            Matlab matlab(false);
+            matlab.PutArray(drawnComponents, "drawnComponents0");
+            //drawnComponents.Show("drawnComponents0", true, false);
+          }
+
+          // TODO: invalidate filled center components
+          if(extractedComponents.InvalidateFilledCenterComponents(component_percentHorizontal, component_percentVertical, scratch2) != RESULT_OK)
+            return RESULT_FAIL;
+
+          extractedComponents.CompressConnectedComponentSegmentIds(scratch2);
+
+          extractedComponents.SortConnectedComponentSegments();
+        } // PUSH_MEMORY_STACK(scratch2);
+      } // PUSH_MEMORY_STACK(scratch2);
+
+      {
+        Array<u8> drawnComponents(image.get_size(0), image.get_size(1), scratch1);
+        DrawComponents<u8>(drawnComponents, extractedComponents, 64, 255);
+
+        Matlab matlab(false);
+        matlab.PutArray(drawnComponents, "drawnComponents1");
+        //drawnComponents.Show("drawnComponents0", true, false);
+      }
+
+      // 4. Compute candidate quadrilaterals from the connected components
+      FixedLengthList<Quadrilateral<s16> > extractedQuads(maxExtractedQuads, scratch2);
+      {
+        PUSH_MEMORY_STACK(scratch2); // Push the current state of the scratch buffer onto the system stack
+
+        if(ComputeQuadrilateralsFromConnectedComponents(extractedComponents, quads_minQuadArea, quads_quadSymmetryThreshold, quads_minDistanceFromImageEdge, image.get_size(0), image.get_size(1), extractedQuads, scratch2) != RESULT_OK)
+          return RESULT_FAIL;
+      } // PUSH_MEMORY_STACK(scratch2);
+
+      // 4b. Compute a homography for each extracted quadrilateral
+      markers.set_size(extractedQuads.get_size());
+      for(s32 iQuad=0; iQuad<extractedQuads.get_size(); iQuad++) {
+        PUSH_MEMORY_STACK(scratch2); // Push the current state of the scratch buffer onto the system stack
+
+        FiducialMarker &currentMarker = markers[iQuad];
+
+        if(ComputeHomographyFromQuad(extractedQuads[iQuad], currentMarker.homography, scratch2) != RESULT_OK)
+          return RESULT_FAIL;
+
+        markers[iQuad].blockType = -1;
+        markers[iQuad].faceType = -1;
+        for(s32 i=0; i<4; i++) {
+          markers[iQuad].corners[i] = extractedQuads[iQuad].corners[i];
+        }
+      } // for(iQuad=0; iQuad<; iQuad++)
+
+      // 5. Decode fiducial markers from the candidate quadrilaterals
+      // TODO: do decoding
+
+      return RESULT_OK;
+    } //  SimpleDetector_Steps1234()
+  } // namespace Embedded
+} // namespace Anki
