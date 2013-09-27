e86cc07cf25dc5d50baa7e7af445751cc2189088
Andrew Stein <andrew@anki.com>
2013-09-27 13:27:15 -0700

    Fleshing out, cleaning up, and documenting Pose classes.

--- include/anki/math/pose.h
+++ include/anki/math/pose.h
@@ -1,3 +1,33 @@
+/**
+ * File: pose.h
+ *
+ * Author: Andrew Stein (andrew)
+ * Created: 9/10/2013
+ *
+ * Information on last revision to this file:
+ *    $LastChangedDate$
+ *    $LastChangedBy$
+ *    $LastChangedRevision$
+ *
+ * Description: Implements two classes for storing Pose, meaning a rotation 
+ *              translation.  
+ *
+ *                Pose2d stores 2D pose representing rotation
+ *                in a plane with a 2D translation vector. Thus it has three
+ *                degrees of freedom: x, y, and angle.
+ *
+ *                Pose3d stores 3D pose representing a 3D rotation and
+ *                a 3D translation vector.  Thus it has six degrees of
+ *                freedom: three rotation angles and x, y, z translation.
+ *               
+ *                Each has the ability to represent a kinematic tree using
+ *                a "parent" pointer. Thus, a pose "with respect to" another
+ *                pose can be requested using the getWithRespectTo() method.
+ *
+ * Copyright: Anki, Inc. 2013
+ *
+ **/
+
 #ifndef _ANKICORETECH_MATH_POSE_H_
 #define _ANKICORETECH_MATH_POSE_H_
 
@@ -11,22 +41,36 @@ namespace Anki {
 
   // Forward declarations of types used below:
   typedef Point3f Vec3f;
-  template<typename T> class Matrix;
+  //template<typename T> class Matrix;
   
+
+  // TODO: Have Pose2d and Pose3d inherit from an (abstract?) base class?
+  //  Then the base class could define the common elements like the parent
+  //  pointer, the getTreeDepth() method, and the getWithRespectTo method.
   
+  //
+  // Pose2d Class
+  //
+  // Stores 2d translation and orienation in a plane.
+  //
   class Pose2d
   {
   public:
+    static Pose2d* World;
+    
     // Constructors:
-    Pose2d(const float x, const float y, const Radians angle);
+    Pose2d(const Radians &angle, const Point2f &translation);
+    Pose2d(const Radians &angle, const float x, const float y);
+
+    /* TODO: Add constructor to take in covariance
     Pose2d(const float x, const float y, const Radians angle,
            const Matrix<float> &cov);
+     */
     
     // Accessors:
-    inline float   get_x()     const;
-    inline float   get_y()     const;
-    inline Point2f get_xy()    const;
-    inline Radians get_angle() const;
+    inline float   get_x()        const;
+    inline float   get_y()        const;
+    inline Radians get_angle()    const;
     
     Vec3f get_normal(void) const;
     void  set_normal(const Vec3f &normal_new);
@@ -36,6 +80,7 @@ namespace Anki {
     // Composition with another Pose2d:
     void   operator*=(const Pose2d &other); // in place
     Pose2d operator* (const Pose2d &other) const;
+    void preComposeWith(const Pose2d &other);
     
     // "Apply" Pose to a 2D point (i.e. transform that point by this Pose)
     Point2f operator*(const Point2f &point) const;
@@ -43,22 +88,44 @@ namespace Anki {
     Pose2d getInverse(void); const // return new Pose
     void Invert(void); // in place
     
+    // Get this pose with respect to another pose.  Other pose
+    // must be an ancestor of this pose, otherwise an error
+    // will be generated.  Use Pose2d::World to get the pose with
+    // respect to the root / world pose.
+    Pose2d getWithRespectTo(const Pose2d *otherPose) const;
+
   protected:
-    float x, y;
+    Point2f translation;
     Radians angle;
     
     // Stores the orientation of this 2D plane in 3D space
     Vec3f normal;
     
+    /* TODO: Add and use 3DOF covariance
     Matrix<float> covariance;
+     */
     
+    // "Parent" for defining linkages or sequences of poses, so we can get
+    // a pose "with respect to" a parent or to root (world) pose.
+    const Pose2d *parent;
+    //unsigned int treeDepth; // helps find common ancestor with other poses
+    unsigned int getTreeDepth(void) const;
+    
+    friend Pose2d getWithRespectToHelper(const Pose2d *from,
+                                         const Pose2d *to);
+
   }; // class Pose2d
   
-
+  
   // TODO: Add thin wrapper or typedef for "Transform2d/3d" objects?
   
   
-  // A class for encapsulating 6DOF pose (3D translation plus 3-axis rotation).
+  //
+  // Pose3d Class
+  //
+  // Stores a 6DOF (Degree Of Freedom) pose: 3D translation plus
+  //   3-axis rotation.
+  //
   class Pose3d {
     
   public:
@@ -71,21 +138,27 @@ namespace Anki {
     Pose3d(const RotationVector3d &Rvec, const Vec3f &T,
            const Pose3d *parentPose = Pose3d::World);
     
+    /* TODO: Add constructor that takes in covariance
     Pose3d(const RotationVector3d &Rvec, const Vec3f &T, const Matrix<float> &cov,
            const Pose3d *parentPose = Pose3d::World);
+     */
     
     // Construct from rotation matrix and translation vector
     // TODO: do we want a version that takes in covariance too?
     Pose3d(const RotationMatrix3d &Rmat, const Vec3f &T,
            const Pose3d *parentPose = Pose3d::World);
     
-    
+    void update(const RotationMatrix3d &Rmat, const Vec3f &T);
+    void update(const RotationVector3d &Rvec, const Vec3f &T);
+
     // TODO: Copy constructor?
-    
+    Pose3d(const Pose3d &otherPose);
+    Pose3d(const Pose3d *otherPose);
     
     // Composition with another Pose
     void   operator*=(const Pose3d &other); // in place
     Pose3d operator*(const Pose3d &other) const;
+    void preComposeWith(const Pose3d &other);
     
     // "Apply" Pose to a 3D point (i.e. transform that point by this Pose)
     Point3f operator*(const Point3f &point) const;
@@ -105,14 +178,20 @@ namespace Anki {
     RotationMatrix3d  rotationMatrix;
     Vec3f             translation;
     
+    /* TODO: Add and use 6DOF covariance.
     // 6x6 covariance matrix (upper-left 3x3 block corresponds to rotation
     // vector elements, lower right 3x3 block corresponds to translation)
     Matrix<float> covariance;
+     */
     
     // "Parent" for defining linkages or sequences of poses, so we can get
     // a pose "with respect to" a parent or to root (world) pose.
     const Pose3d *parent;
-    unsigned int treeDepth; // helps find common ancestor with other poses
+    //unsigned int treeDepth; // helps find common ancestor with other poses
+    unsigned int getTreeDepth(void) const;
+    
+    friend Pose3d getWithRespectToHelper(const Pose3d *from,
+                                         const Pose3d *to);
     
   }; // class Pose3d
   
--- src/pose.cpp
+++ src/pose.cpp
@@ -6,22 +6,30 @@
 #include <stdexcept>
 
 namespace Anki {
-  
+    
 #pragma mark --- Pose2d Implementations ---
+  Pose2d* Pose2d::World = NULL;
+  
+  Pose2d::Pose2d(const Radians &theta, const Point2f &t)
+  : translation(t), angle(theta)
+  {
+    
+  }
+  
+  Pose2d::Pose2d(const Radians &theta, const float x, const float y)
+  : translation(x,y), angle(theta)
+  {
+    
+  }
   
   float Pose2d::get_x(void) const
   {
-    return this->x;
+    return this->translation.x();
   }
   
   float Pose2d::get_y(void) const
   {
-    return this->y;
-  }
-  
-  Point2f Pose2d::get_xy(void) const
-  {
-    return Point2f(this->x, this->y);
+    return this->translation.y();
   }
   
   Radians Pose2d::get_angle(void) const
@@ -38,53 +46,105 @@ namespace Anki {
     
   } // Pose2d::get_rotationMatrix()
   
+  void Pose2d::operator*=(const Pose2d &other)
+  {
+    this->angle += other.angle;
+    
+    this->translation += this->get_rotationMatrix() * other.translation;
+    
+  }
+  
+  Pose2d Pose2d::operator* (const Pose2d &other) const
+  {
+    Radians newAngle(this->angle + other.angle);
+    RotationMatrix2d newRotation(newAngle);
+    Point2f newTranslation(newRotation * other.translation);
+    newTranslation += this->translation;
+    
+    return Pose2d(newAngle, newTranslation);
+  }
+  
+  void Pose2d::preComposeWith(const Pose2d &other)
+  {
+    this->angle += other.angle;
+
+    this->translation = other.get_rotationMatrix() * this->translation;
+    this->translation += other.translation;
+  }
+  
 #pragma mark --- Pose3d Implementations ---
   
   Pose3d* Pose3d::World = NULL;
   
   Pose3d::Pose3d()
-  : translation(0.f, 0.f, 0.f), parent(NULL), treeDepth(0)
+  : translation(0.f, 0.f, 0.f), parent(Pose3d::World)
   {
     
   } // Constructor: Pose3d()  
   
   Pose3d::Pose3d(const RotationVector3d &Rvec_in, const Vec3f &T_in, const Pose3d *parentPose)
-  : rotationVector(Rvec_in), translation(T_in),
-    rotationMatrix(rotationVector), parent(parentPose)
+  : rotationVector(Rvec_in),
+    rotationMatrix(rotationVector),
+    translation(T_in),
+    parent(parentPose)
   {
-    if(parentPose != NULL) {
-      treeDepth = parentPose->treeDepth + 1;
-    }
+
   } // Constructor: Pose3d(Rvec, T)
   
+  /* TODO: Add this constructor once we're using covariance
   Pose3d::Pose3d(const RotationVector3d &Rvec_in, const Vec3f &T_in,
              const Matrix<float> &cov_in, const Pose3d *parentPose)
   : Pose3d(Rvec_in, T_in, parentPose)
   {
     covariance = cov_in;
   } // Constructor: Pose3d(Rvec, T, cov)
+  */
   
   Pose3d::Pose3d(const RotationMatrix3d &Rmat_in, const Vec3f &T_in, const Pose3d *parentPose)
-  : rotationMatrix(Rmat_in), translation(T_in), rotationVector(rotationMatrix), parent(parentPose)
+  : rotationMatrix(Rmat_in),
+    rotationVector(rotationMatrix),
+    translation(T_in),
+    parent(parentPose)
   {
-    if(parentPose != NULL) {
-      treeDepth = parentPose->treeDepth + 1;
-    }
+
   } // Constructor: Pose3d(Rmat, T)
   
   
 #pragma mark --- Operator Overloads ---
+  // Composition: this = this*other
   void Pose3d::operator*=(const Pose3d &other)
   {
-    // TODO: Implement [this.Rmat this.t] * [other.Rmat other.t]
-    
+   
+    // this.T = this.R*other.T + this.T;
+    Vec3f thisTranslation(this->translation); // temp storage
+    this->translation = this->rotationMatrix * other.translation;
+    this->translation += thisTranslation;
+
+    // this.R = this.R * other.R
+    // Note: must do this _after_ the translation update, since that uses this.R
+    this->rotationMatrix *= other.rotationMatrix;
+
   } // operatore*=(Pose3d)
   
+  // Composition: new = this * other
   Pose3d Pose3d::operator*(const Pose3d &other) const
   {
-    // TODO: Implement [this.Rmat this.t] * [other.Rmat other.t]
-
+    Vec3f newTranslation = this->rotationMatrix * other.translation;
+    newTranslation += this->translation;
+    
+    RotationMatrix3d newRotation(this->rotationMatrix);
+    newRotation *= other.rotationMatrix;
+    
+    return Pose3d(newRotation, newTranslation);
   } // operator*(Pose3d)
+  
+  // Composition: this = other * this
+  void Pose3d::preComposeWith(const Pose3d &other)
+  {
+    this->rotationMatrix.preMultiplyBy(other.rotationMatrix);
+    this->translation = other.rotationMatrix * this->translation;
+    this->translation += other.translation;
+  }
   
   
 #pragma mark --- Member Methods ---
@@ -103,19 +163,122 @@ namespace Anki {
     this->translation = this->rotationMatrix * this->translation;
   }
   
-  Pose3d Pose3d::getWithRespectTo(const Anki::Pose3d *otherPose) const
+  // Count number of steps to root ("World") node, by walking up
+  // the chain of parents.
+  unsigned int Pose3d::getTreeDepth(void) const
   {
-    const Pose3d *current = this->parent;
-    while(current != otherPose) {
-      
-      // TODO: Chain pose inversions together here as we work our way to parent
-      
-      if(current->parent == NULL) {
-        // TODO: replace this with some "approved" error handling method
-        throw std::runtime_error("Parent pose not found.");
-      }
+    unsigned int treeDepth = 1;
+    
+    const Pose3d* current = this;
+    while(current->parent != Pose3d::World)
+    {
+      ++treeDepth;
+      current = current->parent;
     }
     
-  } // Pose3d::withRespectTo()
+    return treeDepth;
+    
+  } // getTreeDepth()
+  
+  template<class POSE>
+  POSE getWithRespectToHelper(const POSE *from, const POSE *to)
+  {
+    POSE P_from(*from);
+    
+    POSE *P_wrt_other = NULL;
+    
+    if(to == POSE::World) {
+      
+      // Special (but common!) case: get with respect to the
+      // world pose.  Just chain together poses up to the root.
+      
+      while(from->parent != POSE::World) {
+        P_from.preComposeWith( *(from->parent) );
+        from = from->parent;
+      }
+      
+      P_wrt_other = &P_from;
+      
+    } else {
+      
+      POSE P_to(*to);
+      
+      // First make sure we are pointing at two nodes of the same tree depth,
+      // which is the only way they could possibly share the same parent.
+      // Until that's true, walk the deeper node up until it is at the same
+      // depth as the shallower node, keeping track of the total transformation
+      // along the way. (NITE: Only one of the following two while loops should
+      // run, depending on which node is deeper in the tree)
+      
+      while(from->getTreeDepth() > to->getTreeDepth())
+      {
+        CORETECH_ASSERT(from->parent != NULL);
+        
+        P_from.preComposeWith( *(from->parent) );
+        from = from->parent;
+        
+      }
+      
+      while(to->getTreeDepth() > from->getTreeDepth())
+      {
+        CORETECH_ASSERT(to->parent != NULL);
+        
+        P_to.preComposeWith( *(to->parent) );
+        to = to->parent;
+      }
+      
+      // Treedepths should now match:
+      CORETECH_ASSERT(to->getTreeDepth() == from->getTreeDepth());
+      
+      // Now that we are pointing to the nodes of the same depth, keep moving up
+      // until those nodes have the same parent, totalling up the transformations
+      // along the way
+      while(to->parent != from->parent)
+      {
+        CORETECH_ASSERT(from->parent != NULL && to->parent != NULL);
+        
+        P_from.preComposeWith( *(from->parent) );
+        P_to.preComposeWith( *(to->parent) );
+        
+        to = to->parent;
+        from = from->parent;
+      }
+      
+      // Now compute the total transformation from this pose, up the "from" path
+      // in the tree, to the common ancestor, and back down the "to" side to the
+      // final other pose.
+      //     P_wrt_other = P_to.inv * P_from;
+      P_to.Invert();
+      P_to *= P_from;
+      
+      P_wrt_other = &P_to;
+      
+    } // IF/ELSE other is the World pose
+    
+    // The Pose we are about to return is w.r.t. the "other" pose provided (that
+    // was the whole point of the exercise!), so set its parent accordingly:
+    P_wrt_other->parent = to;
+    
+    CORETECH_ASSERT(P_wrt_other != NULL);
+    
+    return *P_wrt_other;
+
+  } // getWithRespectToHelper()
+  
+  Pose2d Pose2d::getWithRespectTo(const Anki::Pose2d *otherPose) const
+  {
+    return getWithRespectToHelper(this, otherPose);
+  }
+  
+  Pose3d Pose3d::getWithRespectTo(const Anki::Pose3d *otherPose) const
+  {
+    return getWithRespectToHelper(this, otherPose);
+  }
+  
+  void testPoseInstantiation(void)
+  {
+    Pose2d p2(30.f, 10.f, -5.f);
+    Pose3d p3;
+  }
   
 } // namespace Anki
