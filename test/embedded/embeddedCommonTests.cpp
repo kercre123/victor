108061efcbd87dff2f65af541ca38b0fe02a842c
Andrew Stein <andrew@anki.com>
2013-09-30 17:28:26 -0700

    Fixing a couple of compiler warnings

--- test/embedded/embeddedCommonTests.cpp
+++ test/embedded/embeddedCommonTests.cpp
@@ -5,7 +5,8 @@
 using namespace Anki::Embedded;
 
 #if defined(ANKICORETECHEMBEDDED_USE_OPENCV)
-#include "opencv2/opencv.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 #endif
 
 #if defined(ANKICORETECHEMBEDDED_USE_MATLAB)
