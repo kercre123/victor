42e4a22822f76a29b9ff16dd32bc6e283fee66ce
Andrew Stein <andrew@anki.com>
2013-10-01 18:55:44 -0700

    Adding initial very rough BlockWorld.h/cpp and a tad bit more fleshing out of Block/Robot classes.

--- src/block.cpp
+++ src/block.cpp
@@ -11,34 +11,69 @@
 
 namespace Anki {
   namespace Cozmo {
+
+#pragma mark --- BlockMarker2d Dimensions & Layout---
+    
+    const char BlockMarker2d::Layout[25] = {'A', 'B', 'C'};
+    
+#pragma mark --- BlockMarker3d Dimensions & Layout---
+    
+    
+    const float BlockMarker3d::TotalWidth          = 38.f;
+    const float BlockMarker3d::FiducialSquareWidth = 4.f;
+    const float BlockMarker3d::FiducialSpacing     = 3.f;
+    
+    const float BlockMarker3d::SquareWidthOutside =
+    (BlockMarker3d::TotalWidth - 2.f*BlockMarker3d::FiducialSpacing);
+    
+    const float BlockMarker3d::SquareWidthInside =
+    (BlockMarker3d::SquareWidthOutside - 2.f*BlockMarker3d::FiducialSquareWidth);
+    
+#if(BLOCKMARKER3D_USE_OUTSIDE_SQUARE)
+    const float BlockMarker3d::ReferenceWidth = BlockMarker3d::SquareWidthOutside;
+#else
+    const float BlockMarker3d::ReferenceWidth = BlockMarker3d::SquareWidthInside;
+#endif
+    
+    const float BlockMarker3d::CodeSquareWidth =
+    (BlockMarker3d::TotalWidth - 4.f*BlockMarker3d::FiducialSpacing -
+     2.f*BlockMarker3d::FiducialSquareWidth) / BlockMarker2d::NumCodeSquaresPerSide;
+    
+    const float BlockMarker3d::DockingDotSpacing =
+    BlockMarker3d::CodeSquareWidth/2.f;
+    
+    const float BlockMarker3d::DockingDotWidth =
+    BlockMarker3d::DockingDotSpacing/2.f;
+    
     
 #pragma mark --- BlockMarker3d Implementation ---
-    
-    const float BlockMarker3d::Width = 25;
+
     
     BlockMarker3d::BlockMarker3d(const Pose3d &pose_in)
-    : pose(pose_in), squareCorners(4)
+    : squareCorners(4), pose(pose_in)
     {
       // Define canonical coordinates relative to center:
-      const float halfWidth = 0.5f * Width;
+      const float halfWidth = 0.5f * BlockMarker3d::ReferenceWidth;
       
       // TODO: these may need to be re-ordered and rotated in 3d
       
-      squareCorners[0].x = -halfWidth;
-      squareCorners[0].y = -halfWidth;
-      squareCorners[0].z = 0.f;
+      
+      // Define canonical corner locations:
+      squareCorners[BlockMarker2d::TopLeftCorner].x()     = -halfWidth;
+      squareCorners[BlockMarker2d::TopLeftCorner].y()     = -halfWidth;
+      squareCorners[BlockMarker2d::TopLeftCorner].z()     =  0.f;
 
-      squareCorners[1].x = -halfWidth;
-      squareCorners[1].y =  halfWidth;
-      squareCorners[1].z = 0.f;
+      squareCorners[BlockMarker2d::BottomLeftCorner].x()  = -halfWidth;
+      squareCorners[BlockMarker2d::BottomLeftCorner].y()  =  halfWidth;
+      squareCorners[BlockMarker2d::BottomLeftCorner].z()  =  0.f;
 
-      squareCorners[2].x =  halfWidth;
-      squareCorners[2].y = -halfWidth;
-      squareCorners[2].z = 0.f;
+      squareCorners[BlockMarker2d::TopRightCorner].x()    =  halfWidth;
+      squareCorners[BlockMarker2d::TopRightCorner].y()    = -halfWidth;
+      squareCorners[BlockMarker2d::TopRightCorner].z()    =  0.f;
 
-      squareCorners[3].x =  halfWidth;
-      squareCorners[3].y =  halfWidth;
-      squareCorners[3].z = 0.f;
+      squareCorners[BlockMarker2d::BottomRightCorner].x() =  halfWidth;
+      squareCorners[BlockMarker2d::BottomRightCorner].y() =  halfWidth;
+      squareCorners[BlockMarker2d::BottomRightCorner].z() =  0.f;
       
     } // Constructor: BlockMarker3d(pose)
     
