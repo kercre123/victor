3f4227b838e4c22669702fea13ab5a66eaf213ff
Peter Barnum <peter@anki.com>
2013-10-02 15:13:37 -0700

    Updated makefiles to 13.10.1 format. Everything compiles (I think), but the linker fails due to lack of space.

---
+++ project/myriad1/unitTests/config/config_shaves.mk
@@ -0,0 +1,5 @@
+# This file holds the configuration of apps to shaves mapping.
+# It is to be included in the application's Makefile.
+
+#--------------------------[ Shave applications section ]--------------------------#
+#No local shave applications for this example
