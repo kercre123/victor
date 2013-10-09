a7f68249c5bc8ea980b10051b5aa7ee5be12f9b6
Peter Barnum <peter@anki.com>
2013-10-09 16:59:30 -0700

    * Alphabetized embeddedVision.h

--- include/anki/embeddedVision.h
+++ include/anki/embeddedVision.h
@@ -5,11 +5,11 @@
 
 #include "anki/embeddedCommon.h"
 
-#include "anki/embeddedVision/dataStructures_vision.h"
 #include "anki/embeddedVision/array2d_vision.h"
+#include "anki/embeddedVision/connectedComponents.h"
+#include "anki/embeddedVision/dataStructures_vision.h"
+#include "anki/embeddedVision/draw_vision.h"
 #include "anki/embeddedVision/fixedLengthList_vision.h"
 #include "anki/embeddedVision/miscVisionKernels.h"
-#include "anki/embeddedVision/connectedComponents.h"
-#include "anki/embeddedVision/draw_vision.h"
 
 #endif //_ANKICORETECH_VISION_VISION_H_
