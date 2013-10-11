e213f399320e313ce8ed5d913d2ddf5d601d1c49
Andrew Stein <andrew@anki.com>
2013-10-11 13:47:36 -0700

    Step 1 of massive rearranging after meeting with Nathan et al.

--- controllers/cozmo_basicdrive01/build/cozmo_basicdrive01.d
+++
@@ -1,7 +0,0 @@
-build/release/cozmo_basicdrive01.o: cozmo_basicdrive01.c \
-  /Applications/Webots/include/controller/c/webots/robot.h \
-  /Applications/Webots/include/controller/c/webots/types.h \
-  /Applications/Webots/include/controller/c/webots/nodes.h \
-  /Applications/Webots/include/controller/c/webots/motor.h \
-  /Applications/Webots/include/controller/c/webots/camera.h \
-  /Applications/Webots/include/controller/c/webots/connector.h
--- controllers/goalspin/build/goalspin.d
+++
@@ -1,7 +0,0 @@
-build/release/goalspin.o: goalspin.c \
-  /Applications/Webots/include/controller/c/webots/robot.h \
-  /Applications/Webots/include/controller/c/webots/types.h \
-  /Applications/Webots/include/controller/c/webots/nodes.h \
-  /Applications/Webots/include/controller/c/webots/motor.h \
-  /Applications/Webots/include/controller/c/webots/camera.h \
-  /Applications/Webots/include/controller/c/webots/connector.h
