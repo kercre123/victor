22d242802295035058f2dbf0169712868e679750
Andrew Stein <andrew@anki.com>
2013-10-16 14:10:28 -0700

    Fleshing out some missing stuff in coretech/vision.

--- coretech/vision/basestation/src/camera.cpp
+++ coretech/vision/basestation/src/camera.cpp
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Anki, Inc. All rights reserved.
 //
 
+#include <algorithm>
+
 #include "anki/vision/basestation/camera.h"
 
 namespace Anki {
@@ -23,6 +25,16 @@ namespace Anki {
   } // Constructor: Camera(calibration, pose)
   
   
+  CameraCalibration::CameraCalibration()
+  : focalLength_x(1.f), focalLength_y(1.f),
+    center_x(0.f), center_y(0.f),
+    skew(0.f)
+  {
+    std::fill(this->distortionCoeffs.begin(),
+              this->distortionCoeffs.end(),
+              0.f);
+  }
+  
   Matrix_3x3f CameraCalibration::get_calibrationMatrix(void) const
   {
     float K_data[9] = {
@@ -34,5 +46,37 @@ namespace Anki {
     
     return K;
   } // get_calibrationMatrix()
+  
+  
+  Pose3d Camera::computeObjectPose(const std::vector<Point2f> &imgPoints,
+                           const std::vector<Point3f> &objPoints) const
+  {
+    // TODO: Implement!
+    CORETECH_THROW("Unimplemented!")
+    return Pose3d();
+  }
+  
+  Pose3d Camera::computeObjectPose(const Quad2f &imgPoints,
+                           const Quad3f &objPoints) const
+  {
+    // TODO: Implement!
+    CORETECH_THROW("Unimplemented!")
+    return Pose3d();
+  }
+  
+  
+  // Compute the projected image locations of a set of 3D points:
+  void Camera::project3dPoints(const std::vector<Point3f> &objPoints,
+                       std::vector<Point2f>       &imgPoints) const
+  {
+    
+  }
+  
+  void Camera::project3dPoints(const Quad3f &objPoints,
+                       Quad2f       &imgPoints) const
+  {
+    
+  }
+
   
 } // namespace Anki
