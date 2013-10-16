e85db1df127415370f14390bc8503e973c94071a
Andrew Stein <andrew@anki.com>
2013-10-16 15:56:47 -0700

    vision portion of ALL_BUILD now working.

--- coretech/vision/robot/mex/mexComputeCharacteristicScale.cpp
+++ coretech/vision/robot/mex/mexComputeCharacteristicScale.cpp
@@ -1,7 +1,9 @@
 #include "mex.h"
 
-#include "anki/embeddedCommon.h"
-#include "anki/embeddedVision.h"
+#include "anki/common/robot/matlabInterface.h"
+#include "anki/common/robot/fixedLengthList.h"
+
+#include "anki/vision/robot/miscVisionKernels.h"
 
 #define VERBOSITY 0
 
