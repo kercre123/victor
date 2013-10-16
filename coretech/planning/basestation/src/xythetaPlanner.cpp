128ee8d97f98b34d0aac9f2677b74c2f790db713
Brad Neuman <brad@anki.com>
2013-10-16 14:54:48 -0700

    Added coretech planning library with hello world class

---
+++ coretech/planning/basestation/src/xythetaPlanner.cpp
@@ -0,0 +1,19 @@
+#include "anki/planning/basestation/xythetaPlanner.h"
+
+#include <iostream>
+
+namespace Anki {
+namespace Planning {
+
+xythetaPlanner::xythetaPlanner()
+{
+
+}
+
+void xythetaPlanner::SayHello() const
+{
+  std::cout<<"Hello form inside the planner!\n";
+}
+
+}
+}
