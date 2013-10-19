892d3542775eabd319b4c040ee590966699c2d2c
Andrew Stein <andrew@anki.com>
2013-10-18 18:16:14 -0700

    Adding ability to rotate a Marker2D and adjusting the upAngle so it points in the direction of the UpBit (which did not appear to be true before)

--- coretech/vision/matlab/@Marker2D/orientAndThreshold.m
+++ coretech/vision/matlab/@Marker2D/orientAndThreshold.m
@@ -20,19 +20,19 @@ end
 switch(whichDir)
     case 1 % 'up'
         this.reorderCorners = 1:4;
-        this.upAngle = 0;
+        this.upAngle =-pi/2;
     case 2 % 'down'
         means = rot90(rot90(means));
         this.reorderCorners = [4 3 2 1];
-        this.upAngle = pi;
+        this.upAngle = pi/2;
     case 3 % 'left'
         means = rot90(rot90(rot90(means)));
         this.reorderCorners = [2 4 1 3];
-        this.upAngle = pi/2;
+        this.upAngle = pi;
     case 4 % 'right'
         means = rot90(means);
         this.reorderCorners = [3 1 4 2];
-        this.upAngle = 3*pi/2;
+        this.upAngle = 0;
     otherwise
         error('Unrecognized whichDir "%d"', whichDir);
 end
