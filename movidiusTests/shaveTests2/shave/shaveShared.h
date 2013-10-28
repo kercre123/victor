75c05fb1872fa437c5db7e1074a3efdebb82a218
Peter Barnum <peter@anki.com>
2013-10-28 16:39:22 -0700

    Shave passes test

--- movidiusTests/shaveTests2/shave/shaveShared.h
+++ movidiusTests/shaveTests2/shave/shaveShared.h
@@ -9,6 +9,4 @@ __attribute__ ((aligned (16))) extern const int integralImage[IMAGE_HEIGHT*IMAGE
 
 void testIntegralImage();
 
-void ScrollingIntegralImage_u8_s32_FilterRow_shaveInnerLoop(const int * restrict integralImage_00, const int * restrict integralImage_01, const int * restrict integralImage_10, const int * restrict integralImage_11, const int numPixelsToProcess, int * restrict output);
-
 #endif // _SHAVE_SHARED_H_
