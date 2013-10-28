b2969b44ccd52ce4b2414607c6a7da82c61da918
Peter Barnum <peter@anki.com>
2013-10-28 13:35:53 -0700

    Commented out old version of integral image on shave

--- coretech/vision/robot/src/movidiusPrototyping.c
+++ coretech/vision/robot/src/movidiusPrototyping.c
@@ -31,88 +31,88 @@ void ScrollingIntegralImage_u8_s32_FilterRow_shaveInnerLoop(const s32 * restrict
 #else
 
 #endif // #ifndef USING_MOVIDIUS_SHAVE_COMPILER ... #else
-  s32 x;
+  //s32 x;
 
-  const int4 * restrict integralImageX4_00  = &(integralImage_00[0]);
+  //const int4 * restrict integralImageX4_00  = &(integralImage_00[0]);
 
-  const int4 * restrict integralImageX4_10  = &(integralImage_10[0]);
+  //const int4 * restrict integralImageX4_10  = &(integralImage_10[0]);
 
-  const int4 * restrict integralImageX4_01a = &(integralImage_01[-1]);
-  const int4 * restrict integralImageX4_01b = &(integralImage_01[3]);
+  //const int4 * restrict integralImageX4_01a = &(integralImage_01[-1]);
+  //const int4 * restrict integralImageX4_01b = &(integralImage_01[3]);
 
-  const int4 * restrict integralImageX4_11a = &(integralImage_11[-1]);
-  const int4 * restrict integralImageX4_11b = &(integralImage_11[3]);
+  //const int4 * restrict integralImageX4_11a = &(integralImage_11[-1]);
+  //const int4 * restrict integralImageX4_11b = &(integralImage_11[3]);
 
-  int4 * restrict output = &(output[0]);
+  //int4 * restrict output = &(output[0]);
 
-  __asm(
-  ".set integralImage_00_address i20 \n"
-    ".set integralImage_10_address i21 \n"
-    ".set integralImage_01a_address i22 \n"
-    ".set integralImage_01b_address i23 \n"
-    ".set integralImage_11a_address i24 \n"
-    ".set integralImage_11b_address i25 \n"
-    ".set output_address i26 \n"
-    "nop 5 \n"
-    "cmu.cpii integralImage_00_address %0 \n"
-    "cmu.cpii integralImage_10_address %1 \n"
-    "cmu.cpii integralImage_01a_address %2 \n"
-    "cmu.cpii integralImage_01b_address %3 \n"
-    "cmu.cpii integralImage_11a_address %4 \n"
-    "cmu.cpii integralImage_11b_address %5 \n"
-    "cmu.cpii output_address %6 \n"
-    : //Output registers
-  :"r"(&integralImageX4_00[0]), "r"(&integralImageX4_10[0]), "r"(&integralImageX4_01a[0]), "r"(&integralImageX4_01b[0]), "r"(&integralImageX4_11a[0]), "r"(&integralImageX4_11b[0]), "r"(&output[0]), //Input registers
-    :"i20", "i21", "i22", "i23", "i24", "i25", "i26" //Clobbered registers
-    );
+  //__asm(
+  //".set integralImage_00_address i20 \n"
+  //  ".set integralImage_10_address i21 \n"
+  //  ".set integralImage_01a_address i22 \n"
+  //  ".set integralImage_01b_address i23 \n"
+  //  ".set integralImage_11a_address i24 \n"
+  //  ".set integralImage_11b_address i25 \n"
+  //  ".set output_address i26 \n"
+  //  "nop 5 \n"
+  //  "cmu.cpii integralImage_00_address %0 \n"
+  //  "cmu.cpii integralImage_10_address %1 \n"
+  //  "cmu.cpii integralImage_01a_address %2 \n"
+  //  "cmu.cpii integralImage_01b_address %3 \n"
+  //  "cmu.cpii integralImage_11a_address %4 \n"
+  //  "cmu.cpii integralImage_11b_address %5 \n"
+  //  "cmu.cpii output_address %6 \n"
+  //  : //Output registers
+  //:"r"(&integralImageX4_00[0]), "r"(&integralImageX4_10[0]), "r"(&integralImageX4_01a[0]), "r"(&integralImageX4_01b[0]), "r"(&integralImageX4_11a[0]), "r"(&integralImageX4_11b[0]), "r"(&output[0]), //Input registers
+  //  :"i20", "i21", "i22", "i23", "i24", "i25", "i26" //Clobbered registers
+  //  );
 
-  //im = zeros([64,64], 'uint8');
-  //for y = 1:64
-  //  im(y, :) = 1 + (y * (1:64)) / 12;
-  //end
+  ////im = zeros([64,64], 'uint8');
+  ////for y = 1:64
+  ////  im(y, :) = 1 + (y * (1:64)) / 12;
+  ////end
 
-  //ii = integralimage(double(im));
+  ////ii = integralimage(double(im));
 
-  for(x=minXSimd; x<maxXSimd; x+=4) {
-    __asm(
-    ".set integralImage_00 v0 \n"
-      ".set integralImage_10 v1 \n"
-      ".set integralImage_01 v2 \n"
-      ".set integralImage_01a v3 \n"
-      ".set integralImage_01b v4 \n"
-      ".set integralImage_11 v5 \n"
-      ".set integralImage_11a v6 \n"
-      ".set integralImage_11b v7 \n"
-      ".set output v8 \n"
-      "lsu0.ldxv integralImage_00 integralImage_00_address || lsu1.ldxv integralImage_10 integralImage_10_address\n"
-      "nop 1 \n"
-      "lsu0.ldxv integralImage_01a integralImage_01a_address || lsu1.ldxv integralImage_01b integralImage_01b_address\n"
-      "nop 1 \n"
-      "lsu0.ldxv integralImage_11a integralImage_11a_address || lsu1.ldxv integralImage_11b integralImage_11b_address\n"
-      "nop 1 \n"
-      "iau.add integralImage_00_address integralImage_00_address 16 \n"
-      "iau.add integralImage_10_address integralImage_10_address 16 \n"
-      "iau.add integralImage_01a_address integralImage_01a_address 16 \n"
-      "iau.add integralImage_01b_address integralImage_01b_address 16 \n"
-      "iau.add integralImage_11a_address integralImage_11a_address 16 \n"
-      "iau.add integralImage_11b_address integralImage_11b_address 16 \n"
-      "nop 1 \n"
-      "vau.alignvec integralImage_01 integralImage_01a integralImage_01b 4 \n"
-      "vau.alignvec integralImage_11 integralImage_11a integralImage_11b 4 \n"
-      "nop 1 \n"
-      "vau.sub output integralImage_11 integralImage_10\n" //output[x] = integralImage_11[x] - integralImage_10[x] + integralImage_00[x] - integralImage_01[x];
-      "nop 1 \n"
-      "vau.add output output integralImage_00 \n"
-      "nop 1 \n"
-      "vau.sub output output integralImage_01 \n"
-      "nop 1 \n"
-      "lsu0.stvx output output_address \n"
-      "iau.add output_address output_address 16 \n \n"
-      : //Output registers
-    ://Input registers
-    :"i20", "i21", "i22", "i23", "i24", "i25", "i26", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8" //Clobbered registers
-      );
-  }
+  //for(x=minXSimd; x<maxXSimd; x+=4) {
+  //  __asm(
+  //  ".set integralImage_00 v0 \n"
+  //    ".set integralImage_10 v1 \n"
+  //    ".set integralImage_01 v2 \n"
+  //    ".set integralImage_01a v3 \n"
+  //    ".set integralImage_01b v4 \n"
+  //    ".set integralImage_11 v5 \n"
+  //    ".set integralImage_11a v6 \n"
+  //    ".set integralImage_11b v7 \n"
+  //    ".set output v8 \n"
+  //    "lsu0.ldxv integralImage_00 integralImage_00_address || lsu1.ldxv integralImage_10 integralImage_10_address\n"
+  //    "nop 1 \n"
+  //    "lsu0.ldxv integralImage_01a integralImage_01a_address || lsu1.ldxv integralImage_01b integralImage_01b_address\n"
+  //    "nop 1 \n"
+  //    "lsu0.ldxv integralImage_11a integralImage_11a_address || lsu1.ldxv integralImage_11b integralImage_11b_address\n"
+  //    "nop 1 \n"
+  //    "iau.add integralImage_00_address integralImage_00_address 16 \n"
+  //    "iau.add integralImage_10_address integralImage_10_address 16 \n"
+  //    "iau.add integralImage_01a_address integralImage_01a_address 16 \n"
+  //    "iau.add integralImage_01b_address integralImage_01b_address 16 \n"
+  //    "iau.add integralImage_11a_address integralImage_11a_address 16 \n"
+  //    "iau.add integralImage_11b_address integralImage_11b_address 16 \n"
+  //    "nop 1 \n"
+  //    "vau.alignvec integralImage_01 integralImage_01a integralImage_01b 4 \n"
+  //    "vau.alignvec integralImage_11 integralImage_11a integralImage_11b 4 \n"
+  //    "nop 1 \n"
+  //    "vau.sub output integralImage_11 integralImage_10\n" //output[x] = integralImage_11[x] - integralImage_10[x] + integralImage_00[x] - integralImage_01[x];
+  //    "nop 1 \n"
+  //    "vau.add output output integralImage_00 \n"
+  //    "nop 1 \n"
+  //    "vau.sub output output integralImage_01 \n"
+  //    "nop 1 \n"
+  //    "lsu0.stvx output output_address \n"
+  //    "iau.add output_address output_address 16 \n \n"
+  //    : //Output registers
+  //  ://Input registers
+  //  :"i20", "i21", "i22", "i23", "i24", "i25", "i26", "v0", "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8" //Clobbered registers
+  //    );
+  //}
 }
 
 void ScrollingIntegralImage_u8_s32_FilterRow_innerLoop_emulateShave(const s32 * restrict integralImage_00, const s32 * restrict integralImage_01, const s32 * restrict integralImage_10, const s32 * restrict integralImage_11, const s32 minX, const s32 maxX, const s32 imageWidth, s32 * restrict output)
