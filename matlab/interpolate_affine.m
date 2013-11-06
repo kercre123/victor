33ae9e9255de8fcec4356551b09bc26c7642af84
Peter Barnum <peter@anki.com>
2013-11-05 17:48:26 -0800

    * Added vertical 0.5 offset. Doing the horizontal 0.5 should be trivial. * Simplified minima and maxima functions into one extrema function

--- matlab/interpolate_affine.m
+++ matlab/interpolate_affine.m
@@ -39,23 +39,18 @@ end
 
 [~, leftmostIndex] = min(warpedPoints(1,:));
 [~, rightmostIndex] = max(warpedPoints(1,:));
-% [~, topmostIndex] = min(warpedPoints(2,:));
-% [~, bottommostIndex] = max(warpedPoints(2,:));
 
 minIndexes = Inf * ones(size(template,1),1);
 maxIndexes = -Inf * ones(size(template,1),1);
 
 %check if the quad is flipped
+% TODO: make simpler
 centroid = mean(warpedPoints, 2);
-% oppositeLeftCorner = mod(mod(leftmostIndex, 4) + 1, 4) + 1;
-% oppositeTopCorner = mod(mod(topmostIndex, 4) + 1, 4) + 1;
-anglesFromCenter = atan2(warpedPoints(2,:)'-repmat(centroid(2,:), [1,4])', warpedPoints(1,:)'-repmat(centroid(1,:), [1,4])')
+anglesFromCenter = atan2(warpedPoints(2,:)'-repmat(centroid(2,:), [1,4])', warpedPoints(1,:)'-repmat(centroid(1,:), [1,4])');
 
 % Find the minimum angle to the left of the current one
-% TODO: make simpler
-
 inds = (anglesFromCenter<0);
-anglesFromCenter(inds) = anglesFromCenter(inds) + 2*pi
+anglesFromCenter(inds) = anglesFromCenter(inds) + 2*pi;
 
 if anglesFromCenter(1) == max(anglesFromCenter)
     [~,ind] = min(anglesFromCenter); 
@@ -66,10 +61,6 @@ else
 
     ind = ind + 1;
 end
-% 
-% if anglesFromCenter(1)<0
-%     anglesFromCenter = anglesFromCenter + pi; 
-% end 
 
 assert(ind ~= 3); % I don't think this should ever happen
 
@@ -79,13 +70,6 @@ else
     direction = -1;
 end
 
-% disp(sprintf('%f->%d',aa(1),aa(ind+1)));
-
-% return;
-
-% if (warpedPoints(1, oppositeLeftCorner) > warpedPoints(1, leftmostIndex) || warpedPoints(2, oppositeLeftCorner) > warpedPoints(2, leftmostIndex)) ...
-%         && ~(warpedPoints(1, oppositeLeftCorner) > warpedPoints(1, leftmostIndex) && warpedPoints(2, oppositeLeftCorner) > warpedPoints(2, leftmostIndex))
-% if (warpedPoints(1, oppositeLeftCorner) > warpedPoints(1, leftmostIndex))
 if direction == 1
     disp('normal');
     leftmost_clockwise1 = mod(leftmostIndex, 4) + 1;
@@ -123,44 +107,44 @@ end
 
 % Compute the mins
 
-minIndexes = updateMinimums(warpedPoints(:,leftmostIndex),...
+minIndexes = updateExtrema(warpedPoints(:,leftmostIndex),...
     warpedPoints(:,leftmost_counter1),...
     minIndexes, size(template));
 
 if warpedPoints(2,leftmost_counter2) > warpedPoints(2,leftmostIndex)
-    minIndexes = updateMinimums(warpedPoints(:,leftmost_counter1),...
+    minIndexes = updateExtrema(warpedPoints(:,leftmost_counter1),...
         warpedPoints(:,leftmost_counter2),...
         minIndexes, size(template));
 end
 
-minIndexes = updateMinimums(warpedPoints(:,leftmost_clockwise1),...
+minIndexes = updateExtrema(warpedPoints(:,leftmost_clockwise1),...
     warpedPoints(:,leftmostIndex),...
     minIndexes, size(template));
 
 if warpedPoints(2,leftmostIndex) > warpedPoints(2,leftmost_clockwise2)
-    minIndexes = updateMinimums(warpedPoints(:,leftmost_clockwise2),...
+    minIndexes = updateExtrema(warpedPoints(:,leftmost_clockwise2),...
         warpedPoints(:,leftmost_clockwise1),...
         minIndexes, size(template));
 end
 
 % Compute the maxes
 
-maxIndexes = updateMaximums(warpedPoints(:,rightmostIndex),...
+maxIndexes = updateExtrema(warpedPoints(:,rightmostIndex),...
     warpedPoints(:,rightmost_clockwise1),...
     maxIndexes, size(template));
 
 if warpedPoints(2,rightmost_clockwise2) > warpedPoints(2,rightmostIndex)
-    maxIndexes = updateMinimums(warpedPoints(:,rightmost_clockwise1),...
+    maxIndexes = updateExtrema(warpedPoints(:,rightmost_clockwise1),...
         warpedPoints(:,rightmost_clockwise2),...
         maxIndexes, size(template));
 end
 
-maxIndexes = updateMaximums(warpedPoints(:,rightmost_counter1),...
+maxIndexes = updateExtrema(warpedPoints(:,rightmost_counter1),...
     warpedPoints(:,rightmostIndex),...
     maxIndexes, size(template));
 
 if warpedPoints(2,rightmostIndex) > warpedPoints(2,rightmost_counter2)
-    maxIndexes = updateMinimums(warpedPoints(:,rightmost_counter2),...
+    maxIndexes = updateExtrema(warpedPoints(:,rightmost_counter2),...
         warpedPoints(:,rightmost_counter1),...
         maxIndexes, size(template));    
 end
@@ -188,7 +172,7 @@ end
 if DISPLAY_PLOTS
     for i = 1:length(minIndexes)
         hold on;
-        plot([minIndexes(i),maxIndexes(i)], [-i+1,-i+1], 'g');        
+        plot([minIndexes(i),maxIndexes(i)], [-i+0.5,-i+0.5], 'g');        
         axis equal
     end
 end % if DISPLAY_PLOTS
@@ -199,7 +183,7 @@ maxIndexes
 
 end % function interpolate_affine()
 
-function minIndexes = updateMinimums(topPoint, bottomPoint, minIndexes, templateSize)
+function extremaIndexes = updateExtrema(topPoint, bottomPoint, extremaIndexes, templateSize)
     if bottomPoint(2) > topPoint(2)
         x = topPoint(1);
 
@@ -209,18 +193,18 @@ function minIndexes = updateMinimums(topPoint, bottomPoint, minIndexes, template
             dx = (topPoint(1) - bottomPoint(1)) /...
                  (topPoint(2) - bottomPoint(2));
         end
-
+        
         minY = topPoint(2);
-        minYRounded = min(templateSize(1)-1, max(0, ceil(minY)));
+        minYRounded = min(templateSize(1)-0.5, max(0.5, ceil(minY-0.5)+0.5));
         
         if ceil(minY) >= templateSize(1)
             return;
         end
 
         maxY = bottomPoint(2);
-        maxYRounded = min(templateSize(1)-1, max(0, floor(maxY)));
+        maxYRounded = min(templateSize(1)-0.5, max(0.5, floor(maxY-0.5)+0.5));
         
-        if maxY < 0
+        if maxY < 0.5
             return;
         end
 
@@ -229,46 +213,11 @@ function minIndexes = updateMinimums(topPoint, bottomPoint, minIndexes, template
         x = x + dx * (minYRounded-minY);
 
         for y = minYRounded:maxYRounded
-            minIndexes(y+1) = x;
+            extremaIndexes(y+0.5) = x;
             x = x + dx;
         end
     end
-end % function updateMinimums()
+end % function updateExtrema()
 
-function maxIndexes = updateMaximums(topPoint, bottomPoint, maxIndexes, templateSize)
-    if bottomPoint(2) > topPoint(2)
-        x = topPoint(1);
-    
-        if abs(topPoint(1) - bottomPoint(1)) < .0001
-            dx = 0;
-        else
-            dx = (topPoint(1) - bottomPoint(1)) /...
-                 (topPoint(2) - bottomPoint(2));
-        end
-
-        minY = topPoint(2);
-        minYRounded = min(templateSize(1)-1, max(0, ceil(minY)));
-        
-        if ceil(minY) >= templateSize(1)
-            return;
-        end
-
-        maxY = bottomPoint(2);
-        maxYRounded = min(templateSize(1)-1, max(0, floor(maxY)));
-
-        if maxY < 0
-            return;
-        end
-        
-        % x was first computed at the floating-point coordinate of the
-        % corner. Update it to the nearest integer y coordinate
-        x = x + dx * (minYRounded-minY);
-
-        for y = minYRounded:maxYRounded
-            maxIndexes(y+1) = x;
-            x = x + dx;
-        end
-    end
-end % function updateMaximums()
 
 
