79c3297ec6afdd64f22537defcf1d4a155097343
Peter Barnum <peter@anki.com>
2013-11-06 15:54:44 -0800

    Sometimes the rowLimits could be valid for one additional row (I think this won't cause issues).

--- coretech/vision/matlab/interpolateAffine_rowLimits.m
+++ coretech/vision/matlab/interpolateAffine_rowLimits.m
@@ -1,17 +1,18 @@
-% function output = interpolateAffine_rowLimits(inputImageQuad, homography, outputImageSize)
+% function output = interpolateAffine_rowLimits(inputImageQuad, homography, templateSize, debugDisplay)
 
 % homography can be either 2x3 or 3x3, though only the first two rows are
 % used. The x-coordinate comes first.
-% 
+%
 % homography warps the input image coordinates to the template coordinates
 
-% interpolateAffine_rowLimits([0,0;3,1;2,4;-1,3], [2,1.5,0;.1,2,1.5], [13,11]);
-% for i=1:100 interpolateAffine_rowLimits(4*[0,0;3,1;2,4;-1,3], 5*(2*rand([2,3])-1), [13,11]); pause(); end
+% interpolateAffine_rowLimits([0,0;3,1;2,4;-1,3], [2,1.5,0;.1,2,1.5], [13,11], true);
+% for i=1:100 interpolateAffine_rowLimits(4*[0,0;3,1;2,4;-1,3], 5*(2*rand([2,3])-1), [13,11], true); pause(); end
 
-function [minIndexes, maxIndexes] = interpolateAffine_rowLimits(inputImageQuad, homography, templateSize)
+function [minIndexes, maxIndexes] = interpolateAffine_rowLimits(inputImageQuad, homography, templateSize, debugDisplay)
 
-% DISPLAY_PLOTS = true;
-DISPLAY_PLOTS = false;
+if ~exist('debugDisplay', 'var')
+    debugDisplay = false;
+end
 
 homography = homography(1:2, :)
 
@@ -27,21 +28,21 @@ inputImageQuadSqueezed = squeezeQuadrilateral(inputImageQuad, 1.0);
 inputImageQuadSqueezed(:,3) = 1;
 warpedPoints = homography*inputImageQuadSqueezed';
 
-if DISPLAY_PLOTS
-    figure(1);
+if debugDisplay
+    figure(2);
     templateCornerPoints = [0,                0,              ;
                             templateSize(2), 0,              ;
                             templateSize(2), templateSize(1);
                             0,                templateSize(1)]';
-                        
+
     inputImageQuad(:,3) = 1;
-    warpedPoints_nonSqueezed = homography*inputImageQuad';                        
-                        
+    warpedPoints_nonSqueezed = homography*inputImageQuad';
+
     hold off;
-    plot(templateCornerPoints(1,[1:4,1]), -templateCornerPoints(2,[1:4,1]), 'b');
+    plot(templateCornerPoints(1,[1:4,1]), -templateCornerPoints(2,[1:4,1]), 'k--');
     hold on;
-    plot(warpedPoints_nonSqueezed(1,[1:4,1]), -warpedPoints_nonSqueezed(2,[1:4,1]), 'y');
-    plot(warpedPoints(1,[1:4,1]), -warpedPoints(2,[1:4,1]), 'g');
+    plot(warpedPoints_nonSqueezed(1,[1:4,1]), -warpedPoints_nonSqueezed(2,[1:4,1]), 'y--');
+    plot(warpedPoints(1,[1:4,1]), -warpedPoints(2,[1:4,1]), 'b--');
     axis equal
     hold off
 end
@@ -62,18 +63,18 @@ inds = (anglesFromCenter<0);
 anglesFromCenter(inds) = anglesFromCenter(inds) + 2*pi;
 
 if anglesFromCenter(1) == max(anglesFromCenter)
-    [~,ind] = min(anglesFromCenter); 
+    [~,ind] = min(anglesFromCenter);
 else
     angleDifferences = anglesFromCenter(2:4) - anglesFromCenter(1);
-    angleDifferences(angleDifferences<0) = Inf; 
-    [~,ind] = min(angleDifferences); 
+    angleDifferences(angleDifferences<0) = Inf;
+    [~,ind] = min(angleDifferences);
 
     ind = ind + 1;
 end
 
 assert(ind ~= 3); % I don't think this should ever happen
 
-if ind == 2 
+if ind == 2
     direction = 1;
 else
     direction = -1;
@@ -92,7 +93,7 @@ if direction == 1
 
     rightmost_counter1 = mod(rightmostIndex-2, 4) + 1;
     rightmost_counter2 = mod(rightmost_counter1-2, 4) + 1;
-else    
+else
     disp('flipped');
     leftmost_clockwise1 = mod(leftmostIndex-2, 4) + 1;
     leftmost_clockwise2 = mod(leftmost_clockwise1-2, 4) + 1;
@@ -110,11 +111,11 @@ end
 % warpedPoints(:, leftmostIndex)
 % warpedPoints(:, leftmost_counter1)
 % warpedPoints(:, leftmost_counter2)
-% 
+%
 % warpedPoints(:, leftmost_clockwise1)
 % warpedPoints(:, leftmost_clockwise2)
 
-% Compute the mins
+% Compute the mins 
 
 minIndexes = updateExtrema(warpedPoints(:,leftmostIndex),...
     warpedPoints(:,leftmost_counter1),...
@@ -155,7 +156,7 @@ maxIndexes = updateExtrema(warpedPoints(:,rightmost_counter1),...
 if warpedPoints(2,rightmostIndex) > warpedPoints(2,rightmost_counter2)
     maxIndexes = updateExtrema(warpedPoints(:,rightmost_counter2),...
         warpedPoints(:,rightmost_counter1),...
-        maxIndexes, templateSize);    
+        maxIndexes, templateSize);
 end
 
 assert(length(minIndexes) == length(maxIndexes));
@@ -164,7 +165,7 @@ assert(length(minIndexes) == length(maxIndexes));
 for i = 1:length(minIndexes)
     if minIndexes(i) >= maxIndexes(i)
         minIndexes(i) = Inf;
-        maxIndexes(i) = -Inf;        
+        maxIndexes(i) = -Inf;
     end
 end
 
@@ -174,17 +175,17 @@ maxIndexes = min(templateSize(2), maxIndexes);
 for i = 1:length(minIndexes)
     if minIndexes(i) >= maxIndexes(i)
         minIndexes(i) = Inf;
-        maxIndexes(i) = -Inf;        
+        maxIndexes(i) = -Inf;
     end
 end
 
-if DISPLAY_PLOTS
+if debugDisplay
     for i = 1:length(minIndexes)
         hold on;
-        plot([minIndexes(i),maxIndexes(i)], [-i+0.5,-i+0.5], 'g');        
+        plot([minIndexes(i),maxIndexes(i)], [-i+0.5,-i+0.5], 'g');
         axis equal
     end
-end % if DISPLAY_PLOTS
+end % if debugDisplay
 
 % minIndexes
 % maxIndexes
@@ -203,17 +204,18 @@ function extremaIndexes = updateExtrema(topPoint, bottomPoint, extremaIndexes, t
             dx = (topPoint(1) - bottomPoint(1)) /...
                  (topPoint(2) - bottomPoint(2));
         end
-        
+
         minY = topPoint(2);
         minYRounded = min(templateSize(1)-0.5, max(0.5, ceil(minY-0.5)+0.5));
-        
-        if ceil(minY) >= templateSize(1)
+
+%         if ceil(minY) >= templateSize(1)
+        if (minY+0.5) >= templateSize(1)
             return;
         end
 
         maxY = bottomPoint(2);
         maxYRounded = min(templateSize(1)-0.5, max(0.5, floor(maxY-0.5)+0.5));
-        
+
         if maxY < 0.5
             return;
         end
@@ -232,9 +234,9 @@ end % function updateExtrema()
 function quad = squeezeQuadrilateral(quad, numPixelsToReduce)
 
     assert(size(quad,1) == 4);
-    
+
     centroid = mean(quad, 1);
-    
+
     for i = 1:4
         offset = quad(i,:) - centroid;
         offsetMagnitude = sqrt(offset(1)^2+offset(2)^2);
@@ -243,7 +245,7 @@ function quad = squeezeQuadrilateral(quad, numPixelsToReduce)
         quad(i,1) = quad(i,1) - numPixelsToReduce*normalizedOffset(1);
         quad(i,2) = quad(i,2) - numPixelsToReduce*normalizedOffset(2);
     end
-    
+
 end % function quad = squeezeQuadrilateral()
 
 
