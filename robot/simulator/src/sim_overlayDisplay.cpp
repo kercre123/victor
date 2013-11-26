0bb061e2e73a8b35af1dc4d859187175b9013c15
Andrew Stein <andrew@anki.com>
2013-11-25 18:34:04 -0800

    Adding explicit Init() for overlayDisplay to handle case when no CozmoPose “robot” is present in a Webots world file.

--- robot/simulator/src/sim_overlayDisplay.cpp
+++ robot/simulator/src/sim_overlayDisplay.cpp
@@ -21,9 +21,18 @@ namespace Anki {
           
           char displayText_[MAX_TEXT_DISPLAY_LENGTH];
           
-          webots::Node* estPose_      = CozmoBot->getFromDef("CozmoBotPose");
-          webots::Field* translation_ = estPose_->getField("translation");
-          webots::Field* rotation_    = estPose_->getField("rotation");
+          webots::Node* estPose_      = NULL;
+          webots::Field* translation_ = NULL;
+          webots::Field* rotation_    = NULL;
+        }
+        
+        void Init(void)
+        {
+          estPose_ = CozmoBot->getFromDef("CozmoBotPose");
+          if(estPose_ != NULL) {
+            translation_ = estPose_->getField("translation");
+            rotation_    = estPose_->getField("rotation");
+          }
         }
         
         void SetText(TextID ot_id, const char* formatStr, ...)
@@ -40,11 +49,15 @@ namespace Anki {
         
         void UpdateEstimatedPose(const f32 x, const f32 y, const f32 angle)
         {
-          const double estTrans[3] = {x, 0, y};
-          const double estRot[4] = {0, 1, 0, angle};
+          if(translation_ != NULL) {
+            const double estTrans[3] = {x, 0, y};
+            translation_->setSFVec3f(estTrans);
+          }
           
-          translation_->setSFVec3f(estTrans);
-          rotation_->setSFRotation(estRot);
+          if(rotation_ != NULL) {
+            const double estRot[4] = {0, 1, 0, angle};
+            rotation_->setSFRotation(estRot);
+          }
         }
         
       } // namespace OverlayDisplay
