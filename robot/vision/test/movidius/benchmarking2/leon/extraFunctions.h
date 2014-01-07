414d033c7e37af56a829025157ac8286954b47b5
peter-anki <peter@anki.com>
2014-01-06 19:18:16 -0800

    Removed robot/vision/test

--- robot/vision/test/movidius/benchmarking2/leon/extraFunctions.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EXTRA_FUNCTIONS_H_
-#define _EXTRA_FUNCTIONS_H_
-
-long strtol(const char *nptr, char **endptr, int base);
-
-#endif // _EXTRA_FUNCTIONS_H_
--- robot/vision/test/movidius/benchmarking2/shave/shaveMain.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-  printf("Doing nothing...\n");
-
-  return 0;
-}
