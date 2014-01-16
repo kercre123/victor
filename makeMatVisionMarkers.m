453208903285c780d354d8dd54578c3c5f1f2a90
Andrew Stein <andrew@anki.com>
2014-01-16 15:00:44 -0800

    Mat updates to try color mat and markers and to fix localization bogosity by handling how rotated markers/fiducials are implemented and handled.

--- makeMatVisionMarkers.m
+++ makeMatVisionMarkers.m
@@ -1,3 +1,7 @@
+ForegroundColor = [ 14 108 184]/255; % Anki light blue
+%BackgroundColor = [107 107 107]/255; % Anki light gray
+BackgroundColor = [1 1 .7]; % Light yellow
+
 % We will create all mat markers with the lower left corner marker in the
 % actual lower left corner of the image -- with the code/icon rotated
 % instead.  We can then rotate these codes when we "print" them on the mat
@@ -6,15 +10,18 @@
 ankiMat = cell(1,9);
 angles = [-90   -180   -180    -90     0   90     0     0   90];
 
-ankiMat{1} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(1)), 'NumCorners', 3, 'AddPadding', false);
-ankiMat{2} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(2)), 'NumCorners', 2, 'AddPadding', false);
-ankiMat{3} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(3)), 'NumCorners', 3, 'AddPadding', false);
-ankiMat{4} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(4)), 'NumCorners', 2, 'AddPadding', false);
-ankiMat{5} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(5)), 'NumCorners', 1, 'AddPadding', false);
-ankiMat{6} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(6)), 'NumCorners', 2, 'AddPadding', false);
-ankiMat{7} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(7)), 'NumCorners', 3, 'AddPadding', false);
-ankiMat{8} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(8)), 'NumCorners', 2, 'AddPadding', false);
-ankiMat{9} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(9)), 'NumCorners', 3, 'AddPadding', false);
+commonArgs = {'AddPadding', false, 'ForegroundColor', ForegroundColor, ...
+    'BackgroundColor', BackgroundColor};
+
+ankiMat{1} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(1)), 'NumCorners', 3, commonArgs{:});
+ankiMat{2} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(2)), 'NumCorners', 2, commonArgs{:});
+ankiMat{3} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(3)), 'NumCorners', 3, commonArgs{:});
+ankiMat{4} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(4)), 'NumCorners', 2, commonArgs{:});
+ankiMat{5} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(5)), 'NumCorners', 1, commonArgs{:});
+ankiMat{6} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(6)), 'NumCorners', 2, commonArgs{:});
+ankiMat{7} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(7)), 'NumCorners', 3, commonArgs{:});
+ankiMat{8} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(8)), 'NumCorners', 2, commonArgs{:});
+ankiMat{9} = VisionMarker.DrawFiducial('Image', imrotate(anki, -angles(9)), 'NumCorners', 3, commonArgs{:});
 
 %%
 [xgrid, ygrid] = meshgrid([-250 0 250]);
