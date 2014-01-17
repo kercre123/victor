d2a8d3d5d97f50501fe6e2c7042b0bf397ed5111
andrew-anki <andrew@anki.com>
2014-01-17 15:50:10 -0800

    Opencv release libraries can't save an image with the debug opencv libraries

--- coretech/anki.cmake
+++ coretech/anki.cmake
@@ -259,6 +259,8 @@ link_directories(
 # above, so we'll add link directories for where it put its products)
 if(WIN32)
 	link_directories(${EXTERNAL_DIR}/build/${OPENCV_DIR}/lib/Debug)
+	link_directories(${EXTERNAL_DIR}/build/${OPENCV_DIR}/lib/Release)
+	link_directories(${EXTERNAL_DIR}/build/${OPENCV_DIR}/lib/RelWithDebInfo)
 else()
 	link_directories(${EXTERNAL_DIR}/build/${OPENCV_DIR}/lib)
 endif(WIN32)
@@ -273,7 +275,8 @@ function(fix_opencv_lib_names NAMES)
 
 if(WIN32)
 	foreach(NAME IN LISTS ${NAMES})
-		list(APPEND ${NAMES}_TMP ${NAME}248d)
+		#list(APPEND ${NAMES}_TMP ${NAME}248d)
+		list(APPEND ${NAMES}_TMP ${NAME}248)
 	endforeach()
 
 	set(${NAMES} "${${NAMES}_TMP}" PARENT_SCOPE)
