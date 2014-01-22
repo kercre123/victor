49a46d349c77b8d8d91129835efd118854178412
peter-anki <peter@anki.com>
2014-01-22 13:58:02 -0800

    Revert "Added QVGA options to the remaining files. It gives out a 320x240x16bpp image with stripes on the top, and constant static on the bottom."
    This reverts commit 4d24e466500f96deeb79e3d4dae515b71ca799ed.

--- robot/make.py
+++ robot/make.py
@@ -407,7 +407,6 @@ if __name__ == '__main__':
       target = 'capture-images'
       LEON_ASM_C_CXX_OPT += '-DUSE_OFFBOARD_VISION=0 -DUSE_CAPTURE_IMAGES '
       LEON_ASM_C_CXX_OPT += '-DDEFAULT_BAUDRATE=2000000 '
-      LEON_ASM_C_CXX_OPT += '-DUSE_QVGA_CAMERA '
     else:
       print 'Invalid argument: ' + arg
       sys.exit(1)
