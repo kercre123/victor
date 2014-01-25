704c2a825c2ff5266293caedf75b70f04ef5248e
peter-anki <peter@anki.com>
2014-01-24 18:26:12 -0800

    continuing on the system testing

---
+++ systemTests/docking/run_testTrackers.m
@@ -0,0 +1,7 @@
+function run_testTrackers()
+
+groundTruthFilename = 'Z:/Documents/Box Documents/Cozmo SE/cozmoShort/labeledCorners.mat';
+
+[trackingImages, centeredImage, tiltedImage, shiftedImage] = loadTestImages();
+
+% test various template sizes
