b3aff36caf3209fb125201d4c36f9de4861896b1
peter-anki <peter@anki.com>
2014-01-27 17:48:12 -0800

    selectPoints loads from a config file, and can detect where you click

--- systemTests/docking/selectPoints.m
+++ systemTests/docking/selectPoints.m
@@ -18,14 +18,13 @@ function varargout = selectPoints(varargin)
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 % 
-%      jsonData = loadjson('C:/Anki/systemTestImages/test_1.json');
-%      selectPoints(jsonData)
+%      selectPoints('C:/Anki/systemTestImages/test_1.json')
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
 % Edit the above text to modify the response to help selectPoints
 
-% Last Modified by GUIDE v2.5 27-Jan-2014 16:12:52
+% Last Modified by GUIDE v2.5 27-Jan-2014 17:32:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -64,20 +63,26 @@ guidata(hObject, handles);
 % UIWAIT makes selectPoints wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
+global jsonConfigFilename;
 global jsonData;
 global curSequence;
 global maxSequence;
 global curImage;
 global maxImage;
 global image;
+global dataPath;
 
-jsonData = varargin{1};
-
+jsonConfigFilename = get(handles.configFilename, 'String');
+jsonConfigFilename = strrep(jsonConfigFilename, '\', '/');
+jsonData = loadjson(jsonConfigFilename);
 curSequence = 1;
 maxSequence = length(jsonData.sequences);
-curImage = 10;
-maxImage = 10;
+curImage = 1;
+maxImage = 0;
 image = rand([480,640]);
+
+slashIndexes = strfind(jsonConfigFilename, '/');
+dataPath = jsonConfigFilename(1:(slashIndexes(end)));
 
 sequenceChanged(handles)
 
@@ -134,6 +139,19 @@ function curSequence_Callback(hObject, eventdata, handles)
 % Hints: get(hObject,'String') returns contents of curSequence as text
 %        str2double(get(hObject,'String')) returns contents of curSequence as a double
 
+global curSequence;
+global maxSequence;
+global curImage;
+global maxImage;
+
+curSequenceNew = str2double(get(hObject,'String'));
+
+if curSequenceNew <= maxSequence && curSequenceNew >= 1
+    curSequence = curSequenceNew;
+end
+
+sequenceChanged(handles);
+
 % --- Executes on button press in previousImage.
 function previousImage_Callback(hObject, eventdata, handles)
 % hObject    handle to previousImage (see GCBO)
@@ -174,7 +192,18 @@ function curImage_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'String') returns contents of curImage as text
 %        str2double(get(hObject,'String')) returns contents of curImage as a double
+global curSequence;
+global maxSequence;
+global curImage;
+global maxImage;
 
+curImageNew = str2double(get(hObject,'String'));
+
+if curImageNew <= maxSequence && curImageNew >= 1
+    curImage = curImageNew;
+end
+
+sequenceChanged(handles);
 
 % --- Executes during object creation, after setting all properties.
 function curSequence_CreateFcn(hObject, eventdata, handles)
@@ -249,17 +278,119 @@ function axes1_CreateFcn(hObject, eventdata, handles)
 % handles    empty - handles not created until after all CreateFcns called
 
 global image;
-imshow(image)
-impixelinfo;
+% imshow(image)
+% impixelinfo;
 
 
 function sequenceChanged(handles)
+global jsonData;
+global dataPath;
 global curSequence;
 global maxSequence;
 global curImage;
 global maxImage;
+global image;
+global allHandles;
+
+allHandles = handles;
+
+if curSequence < 1
+    curSequence = 1;
+end
+
+if curSequence > maxSequence
+    curSequence = maxSequence;
+end
+
+maxImage = length(jsonData.sequences{1}.frameNumbers);
+
+if curImage < 1
+    curImage = 1;
+end
+
+if curImage > maxImage
+    curImage = maxImage;
+end
+
+curFilename = [dataPath, sprintf(jsonData.sequences{curSequence}.filenamePattern, curImage)];
+image = imread(curFilename);
+
+% figure(2); imshow(axes1);
 
 set(handles.curSequence, 'String', num2str(curSequence))
 set(handles.maxSequence, 'String', num2str(maxSequence))
 set(handles.curImage, 'String', num2str(curImage))
 set(handles.maxImage, 'String', num2str(maxImage))
+
+%figure(get(handles.axes1, 'IntegerHandle'));
+% try    
+%     figure(handles.axes1);
+    figure(get(handles.axes1,'Parent')) 
+    imshow(image);
+    set(get(handles.axes1,'Parent'),'WindowButtonDownFcn',@axes1_ButtonDownFcn)
+% catch
+% end
+% 
+
+
+function configFilename_Callback(hObject, eventdata, handles)
+% hObject    handle to configFilename (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of configFilename as text
+%        str2double(get(hObject,'String')) returns contents of configFilename as a double
+
+
+
+% --- Executes during object creation, after setting all properties.
+function configFilename_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to configFilename (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on mouse press over axes background.
+function axes1_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to axes1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% disp('toast');
+% mousePosition = get(hObject, 'CurrentPoint');
+% disp(mousePosition)
+
+global allHandles;
+global image;
+
+% keyboard
+
+axesPosition = get(allHandles.axes1, 'Position');
+mousePosition = get(hObject, 'CurrentPoint');
+
+imPosition = mousePosition - axesPosition(1:2);
+
+if imPosition(1) < 0 || imPosition(2) < 0 || imPosition(1) >= axesPosition(3) || imPosition(2) >= axesPosition(4)
+    return;
+end
+
+imPosition(1) = imPosition(1);
+imPosition(2) = axesPosition(4) - imPosition(2);
+
+imPosition(1) = size(image,2) * imPosition(1) / axesPosition(3);
+imPosition(2) = size(image,1) * imPosition(2) / axesPosition(4);
+
+% percentLocation = 
+disp(axesPosition)
+disp(imPosition)
+
+% --- Executes on mouse press over figure background.
+function figure1_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to figure1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
