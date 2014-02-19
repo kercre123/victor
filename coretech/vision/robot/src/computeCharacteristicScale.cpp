57108e08c23265acbc69edd1e36c68ad4e6f6209
peter-anki <peter@anki.com>
2014-02-19 14:54:14 -0800

    Unrolled binarization loop. totalTime: 62ms Event ecvcs_computeBinaryImage: Mean:50us Min:49us Max:51us Total:11936us NumEvents:240

--- coretech/vision/robot/src/computeCharacteristicScale.cpp
+++ coretech/vision/robot/src/computeCharacteristicScale.cpp
@@ -24,6 +24,9 @@ namespace Anki
 #define MAX_PYRAMID_LEVELS 8
 #define MAX_ALPHAS 128
 
+    staticInline void ecvcs_filterRows(const ScrollingIntegralImage_u8_s32 &integralImage, const s32 scaleImage_numPyramidLevels, const s32 imageY, const s32 imageWidth, Array<s32> * restrict filteredRows);
+    staticInline void ecvcs_computeBinaryImage(const Array<u8> &image, const Array<s32> * restrict filteredRows, const s32 scaleImage_numPyramidLevels, const s32 scaleImage_thresholdMultiplier, const s32 imageY, const s32 imageWidth, u8 * restrict pBinaryImageRow);
+
     staticInline void ecvcs_filterRows(const ScrollingIntegralImage_u8_s32 &integralImage, const s32 scaleImage_numPyramidLevels, const s32 imageY, const s32 imageWidth, Array<s32> * restrict filteredRows)
     {
       // To normalize a sum of 1 / ((2*n+1)^2), we approximate a division as a mulitiply and shift.
@@ -54,32 +57,69 @@ namespace Anki
     {
       const s32 thresholdMultiplier_numFractionalBits = 16;
 
-      const s32 * restrict pFilteredRows[5];
-      for(s32 pyramidLevel=0; pyramidLevel<=scaleImage_numPyramidLevels; pyramidLevel++) {
-        pFilteredRows[pyramidLevel] = filteredRows[pyramidLevel][0];
-      }
-
       const u8 * restrict pImage = image[imageY];
 
-      for(s32 x=0; x<imageWidth; x++) {
-        s32 dogMax = s32_MIN;
-        s32 scaleValue = -1;
-        for(s32 pyramidLevel=0; pyramidLevel<scaleImage_numPyramidLevels; pyramidLevel++) {
-          const s32 dog = ABS(pFilteredRows[pyramidLevel+1][x] - pFilteredRows[pyramidLevel][x]);
+      if(scaleImage_numPyramidLevels == 3) {
+        const s32 * restrict pFilteredRows0 = filteredRows[0][0];
+        const s32 * restrict pFilteredRows1 = filteredRows[1][0];
+        const s32 * restrict pFilteredRows2 = filteredRows[2][0];
+        const s32 * restrict pFilteredRows3 = filteredRows[3][0];
 
-          if(dog > dogMax) {
-            dogMax = dog;
-            scaleValue = pFilteredRows[pyramidLevel+1][x];
-          } // if(dog > dogMax)
-        } // for(s32 pyramidLevel=0; pyramidLevel<scaleImage_numPyramidLevels; scaleImage_numPyramidLevels++)
+        for(s32 x=0; x<imageWidth; x++) {
+          s32 scaleValue;
 
-        const s32 thresholdValue = (scaleValue*scaleImage_thresholdMultiplier) >> thresholdMultiplier_numFractionalBits;
-        if(pImage[x] < thresholdValue) {
-          pBinaryImageRow[x] = 1;
-        } else {
-          pBinaryImageRow[x] = 0;
+          //for(s32 pyramidLevel=0; pyramidLevel<3; pyramidLevel++) {
+          const s32 dog0 = ABS(pFilteredRows1[x] - pFilteredRows0[x]);
+          const s32 dog1 = ABS(pFilteredRows2[x] - pFilteredRows1[x]);
+          const s32 dog2 = ABS(pFilteredRows3[x] - pFilteredRows2[x]);
+
+          if(dog0 > dog1) {
+            if(dog0 > dog2) {
+              scaleValue = pFilteredRows1[x];
+            } else {
+              scaleValue = pFilteredRows3[x];
+            }
+          } else if(dog1 > dog2) {
+            scaleValue = pFilteredRows2[x];
+          } else {
+            scaleValue = pFilteredRows3[x];
+          }
+
+          //} // for(s32 pyramidLevel=0; pyramidLevel<3; scaleImage_numPyramidLevels++)
+
+          const s32 thresholdValue = (scaleValue*scaleImage_thresholdMultiplier) >> thresholdMultiplier_numFractionalBits;
+          if(pImage[x] < thresholdValue) {
+            pBinaryImageRow[x] = 1;
+          } else {
+            pBinaryImageRow[x] = 0;
+          }
+        } // for(s32 x=0; x<imageWidth; x++)
+      } else { // if scaleImage_numPyramidLevels != 3
+        const s32 * restrict pFilteredRows[5];
+        for(s32 pyramidLevel=0; pyramidLevel<=scaleImage_numPyramidLevels; pyramidLevel++) {
+          pFilteredRows[pyramidLevel] = filteredRows[pyramidLevel][0];
         }
-      } // for(s32 x=0; x<imageWidth; x++)
+
+        for(s32 x=0; x<imageWidth; x++) {
+          s32 scaleValue = -1;
+          s32 dogMax = s32_MIN;
+          for(s32 pyramidLevel=0; pyramidLevel<scaleImage_numPyramidLevels; pyramidLevel++) {
+            const s32 dog = ABS(pFilteredRows[pyramidLevel+1][x] - pFilteredRows[pyramidLevel][x]);
+
+            if(dog > dogMax) {
+              dogMax = dog;
+              scaleValue = pFilteredRows[pyramidLevel+1][x];
+            } // if(dog > dogMax)
+          } // for(s32 pyramidLevel=0; pyramidLevel<scaleImage_numPyramidLevels; scaleImage_numPyramidLevels++)
+
+          const s32 thresholdValue = (scaleValue*scaleImage_thresholdMultiplier) >> thresholdMultiplier_numFractionalBits;
+          if(pImage[x] < thresholdValue) {
+            pBinaryImageRow[x] = 1;
+          } else {
+            pBinaryImageRow[x] = 0;
+          }
+        } // for(s32 x=0; x<imageWidth; x++)
+      } // if(scaleImage_numPyramidLevels == 3) ... else
     } // staticInline void ecvcs_computeBinaryImage()
 
     Result ExtractComponentsViaCharacteristicScale(
