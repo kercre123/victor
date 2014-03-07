9a7bdc21b4043be1cca40df0548f4381eae3d855
peter-anki <peter@anki.com>
2014-03-07 13:12:07 -0800

    Updated DrawTree.m to optionally use short labels

--- coretech/vision/matlab/DrawTree.m
+++ coretech/vision/matlab/DrawTree.m
@@ -1,4 +1,21 @@
-    function DrawTree(root, x)
+    
+% Example:
+% DrawTree(probeTree, 0, true, Inf);
+    function DrawTree(root, x, useShortLabels, maxDepth)
+        if ~exist('maxDepth', 'var')
+            maxDepth = Inf;
+        end
+        
+        if ~exist('useShortLabels', 'var')
+            useShortLabels = false;
+        end        
+    
+        if maxDepth == 0
+            return;
+        end
+        
+        maxDepth = maxDepth - 1;
+        
         y = root.depth;
         if y == 0
             hold off
@@ -12,10 +29,18 @@
         end
         
         if isfield(root, 'labelName')
-            text(x,-(y+.5), root.labelName, 'Hor', 'c');
+            if useShortLabels
+                text(x+.01,-y, createSimpleLabelString(root));
+            else
+                text(x,-(y+.5), root.labelName, 'Hor', 'c');
+            end
         elseif isfield(root, 'infoGain')
-            %text(x+.01,-y, sprintf('(X=%.2f,Y=%.2f, IG=%.3f)', root.x, root.y, root.infoGain), 'Hor', 'left');
-            text(x+.01,-y, sprintf('(X=%.2f,Y=%.2f, [%s])', root.x, root.y, root.remainingLabels), 'Hor', 'left');
+            if useShortLabels
+                text(x+.01,-y, createSimpleLabelString(root));
+            else
+                %text(x+.01,-y, sprintf('(X=%.2f,Y=%.2f, IG=%.3f)', root.x, root.y, root.infoGain), 'Hor', 'left');
+                text(x+.01,-y, sprintf('(X=%.2f,Y=%.2f, [%s])', root.x, root.y, root.remainingLabels), 'Hor', 'left');
+            end
         else 
             text(x+.01,-y, 'Abandoned');
         end
@@ -26,13 +51,75 @@
             %plot(x+0.5, -(y+1), 'o');
             plot([x x+xspacing], -[y y+1]);
             
-            DrawTree(root.rightChild, x + xspacing);
+            DrawTree(root.rightChild, x + xspacing, useShortLabels, maxDepth);
         end
         
         if isfield(root, 'leftChild')
             %plot(x-xspacing, -(y+1), 'o');
             plot([x x-xspacing], -[y y+1]);
-            DrawTree(root.leftChild, x-xspacing);
+            DrawTree(root.leftChild, x-xspacing, useShortLabels, maxDepth);
         end
         
     end
+    
+    function textLabel = createSimpleLabelString(root)    
+        
+        if isfield(root, 'x')
+            textLabel = sprintf('(.%d,.%d) ', round(root.x*100), round(root.y*100));
+        else
+            textLabel = '';
+        end
+        
+        if isfield(root, 'remainingLabels')
+            remainingLabels = root.remainingLabels;
+        else
+            remainingLabels = root.labelName;
+        end
+                
+        typeStrings = {'ALL_BLACK','ALL_WHITE','angryFace','ankiLogo','batteries','bullseye','fire','squarePlusCorners'};
+
+        originalLabelString = [remainingLabels, '    '];
+        shortLabelString = '';
+        for iType = 1:length(typeStrings)
+            inds = strfind(originalLabelString, typeStrings{iType});
+            
+            if isempty(inds)
+                continue;
+            end
+            
+            shortDegreesString = '_{';
+            withRotation = true;
+            for i = inds
+                degreesStart = i + length(typeStrings{iType});
+                if originalLabelString(degreesStart) == ' '
+                    withRotation = false;
+                    assert(length(inds) == 1);
+                else % == '_'
+                    degreeString = originalLabelString((degreesStart+1):(degreesStart+3));
+                    if strcmpi(degreeString, '000')
+                        shortDegreesString = [shortDegreesString, '0'];
+                    elseif strcmpi(degreeString, '090')
+                        shortDegreesString = [shortDegreesString, '1'];
+                    elseif strcmpi(degreeString, '180')
+                        shortDegreesString = [shortDegreesString, '2'];
+                    elseif strcmpi(degreeString, '270')
+                        shortDegreesString = [shortDegreesString, '3'];
+                    end
+                end
+%                 originalLabelString(degreesStart:(degreesStart+2))
+            end
+            shortDegreesString = [shortDegreesString, '}'];
+            
+            if withRotation
+                shortLabelString = [shortLabelString, ' ', sprintf('%d',iType-1), shortDegreesString];
+            else
+                shortLabelString = [shortLabelString, ' ', sprintf('%d',iType-1)];
+            end
+        end
+        
+        textLabel = [textLabel, sprintf('\n'), shortLabelString];
+    end
+
+    
+    
+    
