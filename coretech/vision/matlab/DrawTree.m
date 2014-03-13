4a2258f1a1b765c563f317ee87667f06bdf8b1c8
Andrew Stein <andrew@anki.com>
2014-03-13 13:17:24 -0700

    A little more cleanup to DrawTree that wasn't saved for last commit

--- coretech/vision/matlab/DrawTree.m
+++ coretech/vision/matlab/DrawTree.m
@@ -1,7 +1,3 @@
-    
-% Example:
-% DrawTree(probeTree, 0, true, Inf);
-
 function DrawTree(root, varargin)
 % Draw a probe tree.
 %
@@ -47,7 +43,8 @@ cla
 DrawTreeHelper(root, 0, labelFcn, maxDepth);
 set(gca, 'XTick', [], 'YTick', []);
 
-end
+end % DrawTree()
+
 
 function LongLabelTextFcn(node, xpos, ypos)
 
@@ -131,66 +128,4 @@ end
 
 end % DrawTreeHelper()
 
-    
-    
-    function textLabel = createSimpleLabelString(root)    
-        % Take the long string of labels from the root node, and create a
-        % numbers-only version
-        if isfield(root, 'x')
-            textLabel = sprintf('(.%d,.%d) ', round(root.x*100), round(root.y*100));
-        else
-            textLabel = '';
-        end
-        
-        if isfield(root, 'remainingLabels')
-            remainingLabels = root.remainingLabels;
-        else
-            remainingLabels = root.labelName;
-        end
-                
-        typeStrings = {'ALL_BLACK','ALL_WHITE','angryFace','ankiLogo','batteries','bullseye','fire','squarePlusCorners'};
-
-        originalLabelString = [remainingLabels, '    '];
-        shortLabelString = '';
-        for iType = 1:length(typeStrings)
-            inds = strfind(originalLabelString, typeStrings{iType});
-            
-            if isempty(inds)
-                continue;
-            end
-            
-            shortDegreesString = '_{';
-            withRotation = true;
-            for i = inds
-                degreesStart = i + length(typeStrings{iType});
-                if originalLabelString(degreesStart) == ' '
-                    withRotation = false;
-                    assert(length(inds) == 1);
-                else % == '_'
-                    degreeString = originalLabelString((degreesStart+1):(degreesStart+3));
-                    if strcmpi(degreeString, '000')
-                        shortDegreesString = [shortDegreesString, '0'];
-                    elseif strcmpi(degreeString, '090')
-                        shortDegreesString = [shortDegreesString, '1'];
-                    elseif strcmpi(degreeString, '180')
-                        shortDegreesString = [shortDegreesString, '2'];
-                    elseif strcmpi(degreeString, '270')
-                        shortDegreesString = [shortDegreesString, '3'];
-                    end
-                end
-            end
-            shortDegreesString = [shortDegreesString, '}'];
-            
-            if withRotation
-                shortLabelString = [shortLabelString, ' ', sprintf('%d',iType-1), shortDegreesString];
-            else
-                shortLabelString = [shortLabelString, ' ', sprintf('%d',iType-1)];
-            end
-        end
-        
-        textLabel = [textLabel, sprintf('\n'), shortLabelString];
-    end
-
-    
-    
     
