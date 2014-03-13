51d50f6d5540fde33c9ccc7908ffa806ef417671
Andrew Stein <andrew@anki.com>
2014-03-13 10:37:10 -0700

    Switching mexDetectFiducialMarkers to use mx version of calloc, malloc, and free, which seems to fix problem of mex file being cached and newly compiled version not being run unless Matlab is restarted.

--- coretech/vision/robot/mex/mexDetectFiducialMarkers.cpp
+++ coretech/vision/robot/mex/mexDetectFiducialMarkers.cpp
@@ -52,37 +52,37 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
   AnkiConditionalErrorAndReturn(nrhs == 15 && (nlhs == 3 || nlhs == 2), "mexDetectFiducialMarkers", "Call this function as following: [quads, markerTypes, <markerNames>] = mexDetectFiducialMarkers(uint8(image), scaleImage_numPyramidLevels, scaleImage_thresholdMultiplier, component1d_minComponentWidth, component1d_maxSkipDistance, component_minimumNumPixels, component_maximumNumPixels, component_sparseMultiplyThreshold, component_solidMultiplyThreshold, component_minHollowRatio, quads_minQuadArea, quads_quadSymmetryThreshold, quads_minDistanceFromImageEdge, decode_minContrastRatio, returnInvalidMarkers);");
 
-  MemoryStack memory(malloc(bufferSize), bufferSize);
+  MemoryStack memory(mxMalloc(bufferSize), bufferSize);
   AnkiConditionalErrorAndReturn(memory.IsValid(), "mexDetectFiducialMarkers", "Memory could not be allocated");
 
-  Array<u8> image = mxArrayToArray<u8>(prhs[0], memory);
-  const s32 scaleImage_numPyramidLevels = static_cast<s32>(mxGetScalar(prhs[1]));
-  const s32 scaleImage_thresholdMultiplier = static_cast<s32>(Round(pow(2,16)*mxGetScalar(prhs[2]))); // Convert from double to SQ15.16
-  const s16 component1d_minComponentWidth = static_cast<s16>(mxGetScalar(prhs[3]));
-  const s16 component1d_maxSkipDistance = static_cast<s16>(mxGetScalar(prhs[4]));
-  const s32 component_minimumNumPixels = static_cast<s32>(mxGetScalar(prhs[5]));
-  const s32 component_maximumNumPixels = static_cast<s32>(mxGetScalar(prhs[6]));
-  const s32 component_sparseMultiplyThreshold = static_cast<s32>(Round(pow(2,5)*mxGetScalar(prhs[7]))); // Convert from double to SQ26.5
-  const s32 component_solidMultiplyThreshold = static_cast<s32>(Round(pow(2,5)*mxGetScalar(prhs[8]))); // Convert from double to SQ26.5
-  const f32 component_minHollowRatio = static_cast<f32>(mxGetScalar(prhs[9]));
-  const s32 quads_minQuadArea = static_cast<s32>(mxGetScalar(prhs[10]));
-  const s32 quads_quadSymmetryThreshold = static_cast<s32>(Round(pow(2,8)*mxGetScalar(prhs[11]))); // Convert from double to SQ23.8
-  const s32 quads_minDistanceFromImageEdge = static_cast<s32>(mxGetScalar(prhs[12]));
-  const f32 decode_minContrastRatio = static_cast<f32>(mxGetScalar(prhs[13]));
-  const bool returnInvalidMarkers = static_cast<bool>(RoundS32(mxGetScalar(prhs[14])));
+  Array<u8>  image                             = mxArrayToArray<u8>(prhs[0], memory);
+  const s32  scaleImage_numPyramidLevels       = static_cast<s32>(mxGetScalar(prhs[1]));
+  const s32  scaleImage_thresholdMultiplier    = static_cast<s32>(Round(pow(2,16)*mxGetScalar(prhs[2]))); // Convert from double to SQ15.16
+  const s16  component1d_minComponentWidth     = static_cast<s16>(mxGetScalar(prhs[3]));
+  const s16  component1d_maxSkipDistance       = static_cast<s16>(mxGetScalar(prhs[4]));
+  const s32  component_minimumNumPixels        = static_cast<s32>(mxGetScalar(prhs[5]));
+  const s32  component_maximumNumPixels        = static_cast<s32>(mxGetScalar(prhs[6]));
+  const s32  component_sparseMultiplyThreshold = static_cast<s32>(Round(pow(2,5)*mxGetScalar(prhs[7]))); // Convert from double to SQ26.5
+  const s32  component_solidMultiplyThreshold  = static_cast<s32>(Round(pow(2,5)*mxGetScalar(prhs[8]))); // Convert from double to SQ26.5
+  const f32  component_minHollowRatio          = static_cast<f32>(mxGetScalar(prhs[9]));
+  const s32  quads_minQuadArea                 = static_cast<s32>(mxGetScalar(prhs[10]));
+  const s32  quads_quadSymmetryThreshold       = static_cast<s32>(Round(pow(2,8)*mxGetScalar(prhs[11]))); // Convert from double to SQ23.8
+  const s32  quads_minDistanceFromImageEdge    = static_cast<s32>(mxGetScalar(prhs[12]));
+  const f32  decode_minContrastRatio           = static_cast<f32>(mxGetScalar(prhs[13]));
+  const bool returnInvalidMarkers              = static_cast<bool>(RoundS32(mxGetScalar(prhs[14])));
 
   AnkiConditionalErrorAndReturn(image.IsValid(), "mexDetectFiducialMarkers", "Could not allocate image");
 
-  MemoryStack scratch0(calloc(bufferSize,1), bufferSize);
+  MemoryStack scratch0(mxCalloc(bufferSize,1), bufferSize);
   AnkiConditionalErrorAndReturn(scratch0.IsValid(), "mexDetectFiducialMarkers", "Scratch0 could not be allocated");
 
-  MemoryStack scratch1(calloc(bufferSize,1), bufferSize);
+  MemoryStack scratch1(mxCalloc(bufferSize,1), bufferSize);
   AnkiConditionalErrorAndReturn(scratch1.IsValid(), "mexDetectFiducialMarkers", "Scratch1 could not be allocated");
 
-  MemoryStack scratch2(calloc(bufferSize,1), bufferSize);
+  MemoryStack scratch2(mxCalloc(bufferSize,1), bufferSize);
   AnkiConditionalErrorAndReturn(scratch2.IsValid(), "mexDetectFiducialMarkers", "Scratch2 could not be allocated");
 
-  MemoryStack scratch3(calloc(bufferSize,1), bufferSize);
+  MemoryStack scratch3(mxCalloc(bufferSize,1), bufferSize);
   AnkiConditionalErrorAndReturn(scratch3.IsValid(), "mexDetectFiducialMarkers", "Scratch3 could not be allocated");
 
   FixedLengthList<VisionMarker> markers(maxMarkers, scratch0);
@@ -197,9 +197,9 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     //plhs[3] = orientationsMatlab;
   } // if(numMarkers != 0) ... else
 
-  free(memory.get_buffer());
-  free(scratch0.get_buffer());
-  free(scratch1.get_buffer());
-  free(scratch2.get_buffer());
-  free(scratch3.get_buffer());
+  mxFree(memory.get_buffer());
+  mxFree(scratch0.get_buffer());
+  mxFree(scratch1.get_buffer());
+  mxFree(scratch2.get_buffer());
+  mxFree(scratch3.get_buffer());
 }
