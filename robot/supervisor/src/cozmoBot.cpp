931de607d79692bd03a9e08979c9f96a2cc0f4c1
Kevin Yoon <kevin@anki.com>
2014-03-19 18:25:06 -0700

    * Reactivated head calibration
    * Updated headController to be more like liftController. Still needs tuning once head hw is working.

--- robot/supervisor/src/cozmoBot.cpp
+++ robot/supervisor/src/cozmoBot.cpp
@@ -63,7 +63,7 @@ namespace Anki {
       void StartMotorCalibrationRoutine()
       {
         LiftController::StartCalibrationRoutine();
-        //HeadController::StartCalibrationRoutine();
+        HeadController::StartCalibrationRoutine();
 #if defined(HAVE_ACTIVE_GRIPPER) && HAVE_ACTIVE_GRIPPER
         GripController::DisengageGripper();
 #endif
@@ -79,7 +79,7 @@ namespace Anki {
         
         if(
            LiftController::IsCalibrated()
-           //&& HeadController::IsCalibrated()
+           && HeadController::IsCalibrated()
            ) {
           PRINT("Motors calibrated\n");
           isDone = true;
