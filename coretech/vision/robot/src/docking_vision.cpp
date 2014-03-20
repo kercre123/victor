953bfcbe22e661b8157ef17a8696cbcd66bfe4f5
Andrew Stein <andrew@anki.com>
2014-03-19 18:18:53 -0700

    Using clockwise sorting method now in Quadrilateral class inside compute docking error signal (rather than duplicating the code there)

--- coretech/vision/robot/src/docking_vision.cpp
+++ coretech/vision/robot/src/docking_vision.cpp
@@ -92,27 +92,9 @@ namespace Anki
         //[th,~] = cart2pol(corners(:,1)-mean(corners(:,1)), corners(:,2)-mean(corners(:,2)));
         //[~,sortIndex] = sort(th);
 
-        Array<f32> thetas(1,4,scratch);
-        Array<s32> indexes(1,4,scratch);
-        Point<f32> center = templateRegion.ComputeCenter();
-
-        for(s32 i=0; i<4; i++) {
-          f32 rho = 0.0f;
-
-          Cart2Pol<f32>(
-            templateRegion[i].x - center.x,
-            templateRegion[i].y - center.y,
-            rho, thetas[0][i]);
-        }
-
-        Matrix::Sort(thetas, indexes, 1);
-
-        //upperLeft = corners(sortIndex(1),:);
-        //upperRight = corners(sortIndex(2),:);
-        //        const Point<f32> upperLeft = templateRegion[indexes[0][0]];
-        //        const Point<f32> upperRight = templateRegion[indexes[0][1]];
-        const Point<f32> lineLeft = templateRegion[indexes[0][3]];  // bottomLeft
-        const Point<f32> lineRight = templateRegion[indexes[0][2]]; // bottomRight
+        Quadrilateral<f32> sortedQuad = templateRegion.ComputeClockwiseCorners();
+        const Point<f32>& lineLeft  = sortedQuad[3]; // bottomLeft
+        const Point<f32>& lineRight = sortedQuad[2]; // bottomRight
 
         //AnkiAssert(upperRight(1) > upperLeft(1), ...%if upperRight(1) < upperLeft(1)
         //  ['UpperRight corner should be to the right ' ...
