e30359c4b5e4f2a4eba6bde55cd351a5d4406965
peter-anki <peter@anki.com>
2014-03-19 17:12:40 -0700

    Added histogram.cpp (currently empty)

---
+++ coretech/vision/robot/src/histogram.cpp
@@ -0,0 +1,18 @@
+/**
+File: histogram.cpp
+Author: Peter Barnum
+Created: 2014-03-19
+
+Copyright Anki, Inc. 2014
+For internal use only. No part of this code may be used without a signed non-disclosure agreement with Anki, inc.
+**/
+
+#include "anki/vision/robot/histogram.h"
+
+namespace Anki
+{
+  namespace Embedded
+  {
+
+  } // namespace Embedded
+} // namespace Anki
