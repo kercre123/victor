780b541c06fd59fdfa6b962b4eb27ad8b122654d
Andrew Stein <andrew@anki.com>
2014-05-02 16:37:25 -0700

    - Adding clockwise corner sorting methods to basestation Quadrilateral class. - Adding linearAlgebra header file, currently just with a projection operator for now

---
+++ coretech/common/include/anki/common/basestation/math/linearAlgebra_impl.h
@@ -0,0 +1,50 @@
+/**
+ * File: linearAlgebra_impl.h
+ *
+ * Author: Andrew Stein (andrew)
+ * Created: 5/2/2014
+ *
+ * Information on last revision to this file:
+ *    $LastChangedDate$
+ *    $LastChangedBy$
+ *    $LastChangedRevision$
+ *
+ * Description:
+ *    Implements various linear algebra methods defined in linearAlgebra.h
+ *
+ * Copyright: Anki, Inc. 2014
+ *
+ **/
+
+#ifndef ANKI_COMMON_BASESTATION_MATH_LINEAR_ALGEBRA_IMPL_H
+#define ANKI_COMMON_BASESTATION_MATH_LINEAR_ALGEBRA_IMPL_H
+
+#include "anki/common/basestation/math/linearAlgebra.h"
+
+namespace Anki {
+  
+  template<size_t N, typename T>
+  SmallSquareMatrix<N,T> GetProjectionOperator(const Point<N,T>& normal)
+  {
+    SmallSquareMatrix<N,T> P;
+  
+    // Normal needs to be a unit vector!
+    CORETECH_ASSERT(normal.length() == 1.f);
+    
+    for(MatDimType i=0; i<N; ++i) {
+      // Fill in diagonal elements
+      P(i,i) = static_cast<T>(1) - normal[i]*normal[i];
+      
+      // Fill in off-diagonal elements
+      for(MatDimType j=i+1; j<N; ++j) {
+        P(i,j) = -normal[i]*normal[j];
+        P(j,i) = P(i,j);
+      }
+    }
+    
+    return P;
+  }
+  
+} // namespace Anki
+
+#endif // ANKI_COMMON_BASESTATION_MATH_LINEAR_ALGEBRA_IMPL_H
--- coretech/common/include/anki/common/basestation/math/quad.h
+++ coretech/common/include/anki/common/basestation/math/quad.h
@@ -5,6 +5,7 @@
 #include <array>
 
 #include "anki/common/basestation/exceptions.h"
+#include "anki/common/basestation/math/linearAlgebra_impl.h"
 #include "anki/common/basestation/math/point.h"
 
 #if ANKICORETECH_USE_OPENCV
@@ -28,7 +29,9 @@ namespace Anki {
     CornerName& operator++(CornerName& cname);
 
     // postfix operator (cname++)
-    CornerName operator++(CornerName& cname, int);   
+    CornerName operator++(CornerName& cname, int);
+    
+    CornerName operator+(CornerName& cname, int value);
     
   } // namespace Quad
   
@@ -83,6 +86,19 @@ namespace Anki {
     
     using std::array<Point<N,T>, 4>::operator=;
     
+    // WARNING:
+    // The width and height of a floating point Rectangle is different than that of an integer rectangle.
+    //template<typename OutType> Rectangle<OutType> ComputeBoundingRectangle() const;
+    
+    // Sorts the quad's corners, so they are clockwise around the centroid, with
+    // respect to the given plane normal.
+    // Warning: This may give weird results for non-convex quadrilaterals
+    Quadrilateral<N,T> SortCornersClockwise(const Point<N,T>& aroundNormal) const; // in place
+
+    // Special version for 2D quads: no normal needed, since they exist in a 2D
+    // plane by definition.
+    Quadrilateral<2,T> SortCornersClockwise(void) const;
+
   protected:
     
     //Point<N,T> corners[4];
@@ -280,6 +296,99 @@ namespace Anki {
   {
     //return this->corners[whichCorner];
     return std::array<Point<N,T>,4>::operator[](static_cast<int>(whichCorner));
+  }
+  
+  bool sortHelper(const std::pair<f32,QuadDimType>& pair1,
+                  const std::pair<f32,QuadDimType>& pair2)
+  {
+    return pair1.first < pair2.first;
+  }
+  
+  
+  template<QuadDimType N, typename T>
+  Quadrilateral<2,T> Quadrilateral<N,T>::SortCornersClockwise() const
+  {
+    // Catch invalid usage of this method with a non-2D quad at compile time.
+    static_assert(N==2, "No normal should be supplied when sorting a 2D Quadrilateral's corners.");
+
+    using namespace Quad;
+    
+    std::array<std::pair<f32,Quad::CornerName>,4> angleIndexPairs;
+    
+    Point<N,T> center = this->computeCentroid();
+    
+    for(CornerName i=FirstCorner; i<NumCorners; i++) {
+      Point<N,T> corner((*this)[i]);
+      corner -= center;
+      
+      angleIndexPairs[i].first  = atan2f(static_cast<f32>(corner.y()), static_cast<f32>(corner.x()));
+      angleIndexPairs[i].second = i;
+    }
+    
+    // Sort by angle, bringing corner index along for the ride
+    std::sort(angleIndexPairs.begin(), angleIndexPairs.end(), sortHelper);
+    
+    // Re-order the corners using the sorted indices
+    const Quadrilateral<2,T> sortedQuad((*this)[angleIndexPairs[0].second],
+                                        (*this)[angleIndexPairs[1].second],
+                                        (*this)[angleIndexPairs[2].second],
+                                        (*this)[angleIndexPairs[3].second]);
+    
+    return sortedQuad;
+    
+  } // SortCornersClockWise(), Specialized for N==2
+  
+  
+  template<QuadDimType N, typename T>
+  Quadrilateral<N,T> Quadrilateral<N,T>::SortCornersClockwise(const Point<N,T>& aroundNormal) const
+  {
+    // Catch invalid usage of this method with a 2D quad at compile time.
+    static_assert(N != 2, "A normal must be supplied when sorting an ND Quadrilateral's corners.");
+    
+    using namespace Quad;
+    
+    Point<N,T> unitNormal(aroundNormal);
+    unitNormal.makeUnitLength();
+
+    Point<N,T> center = this->computeCentroid();
+    SmallSquareMatrix<N,T> P = GetProjectionOperator(unitNormal);
+    center = P * center;
+
+    // Need to establish an arbitrary coordinate system in the plane.
+    // Use the first corner as the first coordinate axis.
+    Point<N,T> unitU(P * (*this)[Quad::FirstCorner]);
+    unitU -= center;
+    unitU.makeUnitLength();
+    
+    // The cross product of the first coordinate axis vector with the normal
+    // to define the (orthogonal) other coordinate axis vector
+    Point<N,T> unitV( cross(unitU, unitNormal) );
+    
+    // By definition, the first corner's angle is 0.  Sort the other three
+    // relative to it, using the plane's coordinate system we just established
+    std::array<std::pair<f32,CornerName>,3> angleIndexPairs;
+    for(s32 i=0; i<3; i++)
+    {
+      CornerName index = static_cast<CornerName>(i+1);
+      Point<N,T> corner( P * (*this)[index] );
+      corner -= center;
+      
+      const T u = dot(corner, unitU);
+      const T v = dot(corner, unitV);
+      
+      angleIndexPairs[i].first = atan2f(static_cast<f32>(v), static_cast<f32>(u));
+      angleIndexPairs[i].second = index;
+    }
+    
+    // Sort by angle, taking the corner index along for the ride
+    std::sort(angleIndexPairs.begin(), angleIndexPairs.end(), sortHelper);
+    
+    const Quadrilateral<N,T> sortedQuad((*this)[Quad::FirstCorner],
+                                        (*this)[angleIndexPairs[0].second],
+                                        (*this)[angleIndexPairs[1].second],
+                                        (*this)[angleIndexPairs[2].second]);
+              
+    return sortedQuad;
   }
   
   /*
