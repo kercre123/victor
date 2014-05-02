2169e1c26b6405f0b2d794f2050dbf406c679fec
peter-anki <peter@anki.com>
2014-05-02 15:54:09 -0700

    Added robust option for fitting 2d curver

--- coretech/common/matlab/fit2dCurve.m
+++ coretech/common/matlab/fit2dCurve.m
@@ -1,5 +1,5 @@
 
-% function [model, meanError] = fit2dCurve(data, polynomialDegree, fixedParameters, displayResult)
+% function [model, inBounds_meanAbsError, all_meanAbsError, minCenterPoint, maxCenterPoint] = fit2dCurve(data, polynomialDegree, fixedParameters, displayResult)
 
 % Output model is in the form [constant, x, y, x^2, y^2, x^3, y^3, ...]
 
@@ -9,15 +9,15 @@
 % example:
 % [x,y] = meshgrid(0.5:(10.5), 0.5:(10.5)); data = 5 + 6*x + 7*y + 40*rand(size(x));
 % fixedParameters = [nan, nan, nan]; % [constant, x, y]
-% [model, meanAbsError, centerPoint] = fit2dCurve(data, 1, fixedParameters, [], [], true);
+% [model, inBounds_meanAbsError, all_meanAbsError, minCenterPoint, maxCenterPoint] = fit2dCurve(data, 1, fixedParameters, [], [], true);
 
 % estimate vignetting (non-robust):
-% imr = double(im); [model, meanError, centerPoint] = fit2dCurve(1./ (imr / max(imr(:))), 2, [nan, nan, nan, nan, nan], [], [], false)
+% imr = double(im); [model, inBounds_meanAbsError, all_meanAbsError, minCenterPoint, maxCenterPoint] = fit2dCurve(1./ (imr / max(imr(:))), 2, [nan, nan, nan, nan, nan], [], [], false)
 
 % estimate vignetting (robust):
-% imr = double(im); [model, meanError, centerPoint] = fit2dCurve(1./ (imr / max(imr(:))), 2, [nan, nan, nan, nan, nan], .05, 100, false)
+% imr = double(im); [model, inBounds_meanAbsError, all_meanAbsError, minCenterPoint, maxCenterPoint] = fit2dCurve(1./ (imr / max(imr(:))), 2, [nan, nan, nan, nan, nan], .05, 100, false)
 
-function [model, meanAbsError, centerPoint] = fit2dCurve(data, polynomialDegree, fixedParameters, robustThreshold, robustNumIterations, displayResult)
+function [model, inBounds_meanAbsError, all_meanAbsError, minCenterPoint, maxCenterPoint] = fit2dCurve(data, polynomialDegree, fixedParameters, robustThreshold, robustNumIterations, displayResult)
 
 if ~exist('fixedParameters', 'var') || isempty(fixedParameters)
     fixedParameters = nan * ones(1, 1 + 2*polynomialDegree);
@@ -27,9 +27,12 @@ if ~exist('displayResult', 'var')
     displayResult = false;
 end
 
-if ~exist('robustThreshold', 'var') || ~exist('robustNumIterations', 'var')
-    robustThreshold = [];
-    robustNumIterations = [];
+if ~exist('robustThreshold', 'var') || isempty(robustThreshold)
+    robustThreshold = Inf;
+end
+
+if ~exist('robustNumIterations', 'var') || isempty(robustNumIterations)
+    robustNumIterations = 1;
 end
 
 numParametersTotal = 1 + 2*polynomialDegree;
@@ -45,49 +48,87 @@ data = double(data);
 x = x(:);
 y = y(:);
 
+mask = ones(numel(data), 1);
+
 scaledData = computeScaledData(x, y, data, fixedParameters, polynomialDegree);
 
-model = computeModel(x, y, scaledData, fixedParameters, polynomialDegree, numParametersToEstimate, numParametersTotal);
-
-values = model(1) * ones(length(scaledData), 1);
-
-for degree = 1:polynomialDegree
-    values = values + model(2*(degree-1) + 2) * x(:).^degree;
-    values = values + model(2*(degree-1) + 3) * y(:).^degree;
+if displayResult
+    figure(1); close(1);
+    figure(2); close(2);
 end
 
-diff = values - data(:);
+for iteration = 1:robustNumIterations
+    model = computeModel(x, y, scaledData, mask, fixedParameters, polynomialDegree, numParametersToEstimate, numParametersTotal);
 
-meanAbsError = mean(abs(diff));
+    [~, diff, inBounds_meanAbsError, ~, ~] = computeError(x, y, data, mask, model, polynomialDegree);
 
-valuesMax = max(values(:));
+    mask = abs(diff) < robustThreshold;
 
-[indsY, indsX] = find(reshape(values,size(data)) == valuesMax);
+    [values, ~, all_meanAbsError, minCenterPoint, maxCenterPoint] = computeError(x, y, data, ones(numel(data), 1), model, polynomialDegree);
 
-centerPoint = [mean(indsY), mean(indsX)];
+    outputString = [sprintf('After %d iterations, inBounds_meanAbsError:%f all_meanAbsError:%f numInBounds:%d/%d model:[', iteration, inBounds_meanAbsError, all_meanAbsError, length(abs(diff) < robustThreshold), numel(data)), sprintf('%f, ', model), ']'];
+
+    disp(outputString);
+
+    if displayResult
+        figure(1);
+        hold off;
+        surf(reshape(values, size(data)));
+        shading interp
+        hold on;
+        title('original and fit model');
+        % surf(reshape(scaledData, size(data)), 'EdgeColor', 'black');
+        surf(reshape(scaledData, size(data)), zeros([size(data), 3]), 'EdgeColor', 'white');
+        hold off;
+
+%         figure(2);
+%         hold off;
+%         surf(reshape(scaledData - values, size(data)));
+%         shading interp
+%         title('difference from model');
+
+%         pause(.5);
+    end
+end % for iterator = 1:robustNumIterations
 
 if displayResult
-    close 1
-    close 2
-    
-    figure(1);
-    hold off;
-    surf(reshape(values, size(data))); 
-    shading interp
-    hold on; 
-    title('original and fit model');
-    % surf(reshape(scaledData, size(data)), 'EdgeColor', 'black');
-    surf(reshape(scaledData, size(data)), zeros([size(data), 3]), 'EdgeColor', 'white');
-    hold off;
-
     figure(2);
     hold off;
-    surf(reshape(scaledData - values, size(data))); 
+    surf(reshape(scaledData - values, size(data)));
     shading interp
     title('difference from model');
 end
 
+
 % keyboard
+
+function [values, diff, meanAbsError, minCenterPoint, maxCenterPoint] = computeError(x, y, data, mask, model, polynomialDegree)
+    numValidSamples = length(find(mask > 0));
+
+    values = model(1) * ones(numValidSamples, 1);
+
+    for degree = 1:polynomialDegree
+        values = values + model(2*(degree-1) + 2) * x(mask > 0).^degree;
+        values = values + model(2*(degree-1) + 3) * y(mask > 0).^degree;
+    end
+
+    diff = values - data(mask > 0);
+
+    meanAbsError = mean(abs(diff));
+
+    if isempty(find(mask == 0, 1))
+        valuesMin = min(values(:));        
+        valuesMax = max(values(:));        
+        
+        [indsY, indsX] = find(reshape(values,size(data)) == valuesMin);
+        minCenterPoint = [mean(indsY), mean(indsX)];
+        
+        [indsY, indsX] = find(reshape(values,size(data)) == valuesMax);
+        maxCenterPoint = [mean(indsY), mean(indsX)];
+    else
+        minCenterPoint = [Inf, Inf];
+        maxCenterPoint = [Inf, Inf];
+    end
 
 function scaledData = computeScaledData(x, y, data, fixedParameters, polynomialDegree)
     scaledData = data(:);
@@ -109,13 +150,15 @@ function scaledData = computeScaledData(x, y, data, fixedParameters, polynomialD
         end
     end
 
-function model = computeModel(x, y, scaledData, fixedParameters, polynomialDegree, numParametersToEstimate, numParametersTotal)   
-    A = zeros(length(scaledData), numParametersToEstimate); 
-    
+function model = computeModel(x, y, scaledData, mask, fixedParameters, polynomialDegree, numParametersToEstimate, numParametersTotal)
+    numValidSamples = length(find(mask > 0));
+
+    A = zeros(numValidSamples, numParametersToEstimate);
+
     curParameter = 1;
-    
+
     if isnan(fixedParameters(1))
-        A(:,curParameter) = ones(length(scaledData), 1);
+        A(:,curParameter) = ones(numValidSamples, 1);
         curParameter = curParameter + 1;
     end
 
@@ -124,17 +167,17 @@ function model = computeModel(x, y, scaledData, fixedParameters, polynomialDegre
         curYParameter = 3 + 2*(degree-1);
 
         if isnan(fixedParameters(curXParameter))
-            A(:,curParameter) = x.^degree;
+            A(:,curParameter) = x(mask > 0).^degree;
             curParameter = curParameter + 1;
         end
 
         if isnan(fixedParameters(curYParameter))
-            A(:,curParameter) = y.^degree;
+            A(:,curParameter) = y(mask > 0).^degree;
             curParameter = curParameter + 1;
         end
     end
-    
-    modelRaw = scaledData' / A';
+
+    modelRaw = scaledData(mask > 0)' / A';
 
     curRawParameter = 1;
 
@@ -144,7 +187,7 @@ function model = computeModel(x, y, scaledData, fixedParameters, polynomialDegre
         model(1) = modelRaw(curRawParameter);
         curRawParameter = curRawParameter + 1;
     else
-        model(1) = fixedParameters(1);    
+        model(1) = fixedParameters(1);
     end
 
     for degree = 1:polynomialDegree
