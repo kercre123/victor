eea07513c3ed2c10edc6f8cd841466ff493e0795
peter-anki <peter@anki.com>
2014-05-02 15:13:40 -0700

    * Added fixed parameter option to curve fitting, and removed automatic scaling * Added correctVignetting matlab function

--- coretech/common/matlab/fit2dCurve.m
+++ coretech/common/matlab/fit2dCurve.m
@@ -1,58 +1,142 @@
 
-% function [model, meanError] = fit2dCurve(data, polynomialDegree)
+% function [model, meanError] = fit2dCurve(data, polynomialDegree, fixedParameters, displayResult)
+
+% Output model is in the form [constant, x, y, x^2, y^2, x^3, y^3, ...]
+
+% If a fixedParameter is nan, it is estimated. If it is a number, that
+% number is used
 
 % example:
-% [x,y] = meshgrid(0:(10), 0:(10)); data = 5 + 6*x + 7*y + 40*rand(size(x));
-% [model, meanError] = fit2dCurve(data, 1, true);
+% [x,y] = meshgrid(0.5:(10.5), 0.5:(10.5)); data = 5 + 6*x + 7*y + 40*rand(size(x));
+% fixedParameters = [nan, nan, nan]; % [constant, x, y]
+% [model, meanAbsError, centerPoint] = fit2dCurve(data, 1, fixedParameters, true);
 
-function [model, meanError] = fit2dCurve(data, polynomialDegree, displayResult)
+% estimate vignetting:
+% imr = double(im); [model, meanError, centerPoint] = fit2dCurve(1./ (imr / max(imr(:))), 2, [nan, nan, nan, nan, nan], false)
 
-[x,y] = meshgrid(0:(size(data,2)-1), 0:(size(data,1)-1));
+function [model, meanAbsError, centerPoint] = fit2dCurve(data, polynomialDegree, fixedParameters, displayResult)
+
+if ~exist('fixedParameters', 'var') || isempty(fixedParameters)
+    fixedParameters = nan * ones(1, 1 + 2*polynomialDegree);
+end
+
+if ~exist('displayResult', 'var')
+    displayResult = false;
+end
+
+numParametersTotal = 1 + 2*polynomialDegree;
+
+assert(length(fixedParameters) == numParametersTotal);
+
+numParametersToEstimate = length(find(isnan(fixedParameters)));
+
+[x,y] = meshgrid(0.5:(size(data,2)-0.5), 0.5:(size(data,1)-0.5));
 
 x = x(:);
 y = y(:);
 
-dataPercent = double(data(:)) / double(max(data(:)));
+data = double(data);
 
-% dataV = double(data(:));
+scaledData = data(:);
 
-A = ones(length(dataPercent), 1);
+A = zeros(length(scaledData), numParametersToEstimate); 
 
-for degree = 1:polynomialDegree
-    A(:,end+1) = x.^degree;
-    A(:,end+1) = y.^degree;
+curParameter = 1;
+
+if isnan(fixedParameters(1))
+    A(:,curParameter) = ones(length(scaledData), 1);
+    curParameter = curParameter + 1;
+else
+    scaledData = scaledData - fixedParameters(1);
 end
 
-% model = A' / dataPercent';
-model = dataPercent' / A'
+for degree = 1:polynomialDegree
+    curXParameter = 2 + 2*(degree-1);
+    curYParameter = 3 + 2*(degree-1);
+    
+    if isnan(fixedParameters(curXParameter))
+        A(:,curParameter) = x.^degree;
+        curParameter = curParameter + 1;
+    else
+        scaledData = scaledData - fixedParameters(2 + 2*(degree-1)) * x.^degree;
+    end
+    
+    if isnan(fixedParameters(curYParameter))
+        A(:,curParameter) = y.^degree;
+        curParameter = curParameter + 1;
+    else
+        scaledData = scaledData - fixedParameters(3 + 2*(degree-1)) * y.^degree;
+    end
+end
 
-values = model(1) * ones(length(dataPercent), 1);
+modelRaw = scaledData' / A';
+
+curRawParameter = 1;
+
+model = zeros(1, numParametersTotal);
+
+if isnan(fixedParameters(1))
+    model(1) = modelRaw(curRawParameter);
+    curRawParameter = curRawParameter + 1;
+else
+    model(1) = fixedParameters(1);    
+end
 
 for degree = 1:polynomialDegree
-    model(2*(degree-1) + 2)
-    model(2*(degree-1) + 3)
+    curXParameter = 2 + 2*(degree-1);
+    curYParameter = 3 + 2*(degree-1);
+    
+    if isnan(fixedParameters(curXParameter))
+        model(curXParameter) = modelRaw(curRawParameter);
+        curRawParameter = curRawParameter + 1;
+    else
+        model(curXParameter) = fixedParameters(curXParameter);
+    end
+    
+    if isnan(fixedParameters(curYParameter))
+        model(curYParameter) = modelRaw(curRawParameter);
+        curRawParameter = curRawParameter + 1;
+    else
+        model(curYParameter) = fixedParameters(curYParameter);
+    end
+end
+
+values = model(1) * ones(length(scaledData), 1);
+
+for degree = 1:polynomialDegree
     values = values + model(2*(degree-1) + 2) * x(:).^degree;
     values = values + model(2*(degree-1) + 3) * y(:).^degree;
 end
 
-diff = values - dataPercent;
+diff = values - data(:);
 
-meanError = mean(diff);
+meanAbsError = mean(abs(diff));
 
-if exist('displayResult', 'var') && displayResult > 0
+valuesMax = max(values(:));
+
+[indsY, indsX] = find(reshape(values,size(data)) == valuesMax);
+
+centerPoint = [mean(indsY), mean(indsX)];
+
+if displayResult
+    close 1
+    close 2
+    
     figure(1);
     hold off;
     surf(reshape(values, size(data))); 
+    shading interp
     hold on; 
     title('original and fit model');
-    % surf(reshape(dataPercent, size(data)), 'EdgeColor', 'black');
-    surf(reshape(dataPercent, size(data)), zeros([size(data), 3]), 'EdgeColor', 'white');
+    % surf(reshape(scaledData, size(data)), 'EdgeColor', 'black');
+    surf(reshape(scaledData, size(data)), zeros([size(data), 3]), 'EdgeColor', 'white');
     hold off;
 
     figure(2);
     hold off;
+    surf(reshape(scaledData - values, size(data))); 
+    shading interp
     title('difference from model');
-    surf(reshape(dataPercent - values, size(data))); 
 end
 
 % keyboard
