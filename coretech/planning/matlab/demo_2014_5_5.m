14aceb883d7afe5faca2f8b5a785f04e5a8a403d
Brad Neuman <brad@anki.com>
2014-05-05 10:55:44 -0700

    Added matlab script for small demo today

---
+++ coretech/planning/matlab/demo_2014_5_5.m
@@ -0,0 +1,33 @@
+% run this on brads machine form the matlab directory
+
+system('rm -f path.txt');
+
+close all;
+
+rectWorld('test3.env');
+
+mprim1 = 'cozmo_mprim.json'
+mprim2 = 'cozmo_mprim_expensiveTurns.json'
+
+plot(0,0,'ro');
+
+[x,y] = ginput(2);
+
+plot(x(1),y(1),'ko');
+plot(x,y,'k-');
+
+theta = atan2(diff(y), diff(x));
+
+tic
+system(sprintf('"/Users/bneuman/Documents/code/products-cozmo/build/Unix Makefiles/bin/Debug/run_coreTechPlanningStandalone" %s test3.env %f %f %f', mprim1, x(1), y(1), theta));
+toc
+
+S = plotPath('path.txt', true, {}, 'g.-');
+
+n = ginput(1)
+
+tic
+system(sprintf('"/Users/bneuman/Documents/code/products-cozmo/build/Unix Makefiles/bin/Debug/run_coreTechPlanningStandalone" %s test3.env %f %f %f', mprim2, x(1), y(1), theta));
+toc
+
+S = plotPath('path.txt', true, {}, 'c.-');
