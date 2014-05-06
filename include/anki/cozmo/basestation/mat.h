5b7de60e013ab2f238ddf3835d39c09706590674
Andrew Stein <andrew@anki.com>
2014-05-06 15:14:59 -0700

    Adding new required virtual GetCorners() method to MatPiece, inherited from ObservableObject.

--- include/anki/cozmo/basestation/mat.h
+++ include/anki/cozmo/basestation/mat.h
@@ -22,6 +22,13 @@ namespace Anki {
       
       virtual std::vector<RotationMatrix3d> const& GetRotationAmbiguities() const;
       
+      // Required by Vision::ObservableObject
+      // NOTE: this just returns an empty vector of corners for now.
+      // TODO: Actually define mat piece corners?
+      virtual void GetCorners(const Pose3d& atPose, std::vector<Point3f>& corners) const {
+        corners.resize(0);
+      }
+      
     protected:
       static const std::vector<RotationMatrix3d> rotationAmbiguities_;
       
