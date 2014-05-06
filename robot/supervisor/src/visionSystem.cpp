ada94222f694952f22952a14d5a782e834f19939
peter-anki <peter@anki.com>
2014-05-06 11:40:07 -0700

    Changed default vignetting correction from "off" to "software"

--- robot/supervisor/src/visionSystem.cpp
+++ robot/supervisor/src/visionSystem.cpp
@@ -139,7 +139,7 @@ namespace Anki {
         // TODO: Should these be moved to (their own struct in) visionParameters.h/cpp?
         static f32 exposureTime;
 
-        static VignettingCorrection vignettingCorrection = VignettingCorrection_Off;
+        static VignettingCorrection vignettingCorrection = VignettingCorrection_Software;
         static const f32 vignettingCorrectionParameters[5] = {1.56852140958887f, -0.00619880766167132f, -0.00364222219719291f, 2.75640497906470e-05f, 1.75476361058157e-05f}; //< for vignettingCorrection == VignettingCorrection_Software, computed by fit2dCurve.m
 
         static s32 frameNumber;
