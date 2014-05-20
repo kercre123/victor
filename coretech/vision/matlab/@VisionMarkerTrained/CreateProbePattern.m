9195628d3a73d412dcef216d1fe68c0ca44d9d8f
Andrew Stein <andrew@anki.com>
2014-05-20 16:54:05 -0700

    Updating probe radius and re-training with 8-bit Anki Logo mat codes.

--- coretech/vision/matlab/@VisionMarkerTrained/CreateProbePattern.m
+++ coretech/vision/matlab/@VisionMarkerTrained/CreateProbePattern.m
@@ -5,7 +5,7 @@ theta = theta(1:end-1);
 
 probeRegion = VisionMarkerTrained.ProbeRegion;
 
-radius = (probeRegion(2)-probeRegion(1))/(2*(VisionMarkerTrained.ProbeParameters.GridSize+1));
+radius = (probeRegion(2)-probeRegion(1))/(3*VisionMarkerTrained.ProbeParameters.GridSize);
 
 pattern = struct( ...
     'x', [0 radius*cos(theta)], ...
