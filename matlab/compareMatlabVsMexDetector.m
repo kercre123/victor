4b9c12963be7fd7b999483a0dd1b3da59310b0b8
Andrew Stein <andrew@anki.com>
2014-06-30 16:27:49 -0700

    Adding option to see corner "spread" while comparing matlab vs. embedded detectors

--- matlab/compareMatlabVsMexDetector.m
+++ matlab/compareMatlabVsMexDetector.m
@@ -1,13 +1,19 @@
 function compareMatlabVsMexDetector(varargin)
 % Helper to run detectAndDisplay with CameraCapture.  Passes extra args to CameraCapture.
 
+ShowCornerSpread = false;
 
 CamCapArgs = parseVarargin(varargin{:});
 
+if ShowCornerSpread
+    h_spreadFig = namedFigure('CornerPosition');
+    clf(h_spreadFig);
+    h_spreadAxes = axes('Parent', h_spreadFig);
+    hold(h_spreadAxes, 'on');
+end
+
 CameraCapture('resolution', [320 240], 'processFcn', @detectionHelper, ...
     'doContinuousProcessing', true, CamCapArgs{:});
-
-end
 
 
 function detectionHelper(img, h_axes, h_img)
@@ -16,7 +22,10 @@ axis(h_axes, 'on');
 set(h_axes, 'LineWidth', 5, 'Box', 'on', 'XTick', [], 'YTick', [], 'XLim', [1 2*size(img,2)]);
 set(h_img, 'CData', [img img]);
 
-img = rgb2gray(img);
+if size(img,3)>1
+    img = rgb2gray(img);
+end
+
 imageSize = size(img);
 
 if isempty(img) || any(size(img)==0)
@@ -43,9 +52,10 @@ quads_minDistanceFromImageEdge = 2;
 decode_minContrastRatio = 1.25;
 returnInvalidMarkers = 0;
 quadRefinementIterations = 25;
+quadRefinementMaxCornerChange = 5;
 numRefinementSamples = 100;
 
-[quads, ~, markerNames] = mexDetectFiducialMarkers(img, scaleImage_numPyramidLevels, scaleImage_thresholdMultiplier, component1d_minComponentWidth, component1d_maxSkipDistance, component_minimumNumPixels, component_maximumNumPixels, component_sparseMultiplyThreshold, component_solidMultiplyThreshold, component_minHollowRatio, quads_minQuadArea, quads_quadSymmetryThreshold, quads_minDistanceFromImageEdge, decode_minContrastRatio, quadRefinementIterations, numRefinementSamples, returnInvalidMarkers);
+[quads, ~, markerNames, ~] = mexDetectFiducialMarkers(img, scaleImage_numPyramidLevels, scaleImage_thresholdMultiplier, component1d_minComponentWidth, component1d_maxSkipDistance, component_minimumNumPixels, component_maximumNumPixels, component_sparseMultiplyThreshold, component_solidMultiplyThreshold, component_minHollowRatio, quads_minQuadArea, quads_quadSymmetryThreshold, quads_minDistanceFromImageEdge, decode_minContrastRatio, quadRefinementIterations, numRefinementSamples, quadRefinementMaxCornerChange, returnInvalidMarkers);
 
 for i = 1:length(quads)
     quads{i} = quads{i} + 1;
@@ -67,26 +77,38 @@ if ~isempty(h_detections)
 end
 
 hold(h_axes, 'on')
+
 for i = 1:numMatlabDetections
     matlabDetections{i}.Draw('Tag', 'VisionMarkers', ...
         'FontSize', 14, 'Parent', h_axes);
+    
+    if ShowCornerSpread
+        corners = matlabDetections{i}.corners;
+        plot(corners(:,1), corners(:,2), 'rx', 'Parent', h_spreadAxes);
+    end
 end
 order = [1 2 4 3 1];
 ncols = size(img,2);
 for i = 1:numMexDetections
     name = strrep(markerNames{i}, 'MARKER_', '');
-    plot(quads{i}(order,1)+ncols,quads{i}(order,2), 'r', 'LineWidth', 3, 'Tag', 'VisionMarkers');
-    plot(quads{i}([1 3],1)+ncols,quads{i}([1 3],2), 'g', 'LineWidth', 3, 'Tag', 'VisionMarkers');
+    plot(quads{i}(order,1)+ncols,quads{i}(order,2), 'r', 'LineWidth', 3, 'Tag', 'VisionMarkers', 'Parent', h_axes);
+    plot(quads{i}([1 3],1)+ncols,quads{i}([1 3],2), 'g', 'LineWidth', 3, 'Tag', 'VisionMarkers', 'Parent', h_axes);
     text(...
         mean(quads{i}(:,1))+ncols, mean(quads{i}(:,2)),...
         name, ...
         'HorizontalAlignment', 'center', 'BackgroundColor', [.7 .9 .7], ...
-        'Interpreter', 'none', 'Tag', 'VisionMarkers');
+        'Interpreter', 'none', 'Tag', 'VisionMarkers', 'Parent', h_axes);
+    
+    if ShowCornerSpread
+        plot(quads{i}(:,1), quads{i}(:,2), 'bo', 'Parent', h_spreadAxes);
+    end
 end
 
 drawnow
 
 %fprintf('Rest of detectAndDisplay took %.2f seconds\n', toc(t));
 
+
+end
 
 end
