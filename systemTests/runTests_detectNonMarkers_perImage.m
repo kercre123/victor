9d8c6c8b3c9b191ca808cfc0628ef2d55d573637
Peter Barnum <peter@anki.com>
2014-07-18 17:41:51 -0700

    More output for spurious detection script

--- systemTests/runTests_detectNonMarkers_perImage.m
+++ systemTests/runTests_detectNonMarkers_perImage.m
@@ -1,5 +1,7 @@
 function runTests_detectNonMarkers_perImage(workQueue, algorithmParameters)
     
+    showImageDetectionWidth = 640;
+    
     perImageTic = tic();
     for iWork = 1:length(workQueue)
         image = imread(workQueue{iWork}.inputFilename);
@@ -17,9 +19,46 @@ function runTests_detectNonMarkers_perImage(workQueue, algorithmParameters)
         
         imageSize = size(image); %#ok<NASGU>
         
-        [detectedQuads, detectedQuadValidity, detectedMarkers] = extractMarkers(image, algorithmParameters); %#ok<NASGU,ASGLU>
+        [detectedQuads, detectedQuadValidity, detectedMarkers] = extractMarkers(image, algorithmParameters);
         
         save(workQueue{iWork}.outputFilename, 'detectedQuads', 'detectedQuadValidity', 'detectedMarkers', 'imageSize', 'scale');
+        
+        if showImageDetectionWidth(1) ~= size(image,2)
+            showImageDetectionsScale = showImageDetectionWidth(1) / size(image,2);
+        else
+            showImageDetectionsScale = 1;
+        end
+        
+        toShowResults = {
+            workQueue{iWork}.iPattern - 1,...
+            workQueue{iWork}.iFilename - 1,...            
+            0,...
+            0,...
+            0,...
+            0,...
+            0,...
+            'none',...
+            0,...
+            0,...
+            0,...
+            length(detectedQuads),...
+            length(detectedQuads),...
+            length(find(detectedQuadValidity == 0))};
+        
+        markersToDisplay = cell(0,3);        
+        
+        if ~isempty(detectedMarkers)
+            for iMarker = 1:length(detectedMarkers)
+                markersToDisplay(end+1,:) = {detectedMarkers{iMarker}.corners, 6, detectedMarkers{iMarker}.name(8:end)}; %#ok<AGROW>
+            end
+        end
+        
+        drawnImage = mexDrawSystemTestResults(uint8(image), detectedQuads, detectedQuadValidity, markersToDisplay(:,1), int32(cell2mat(markersToDisplay(:,2))), markersToDisplay(:,3), showImageDetectionsScale, [workQueue{iWork}.inputFilename, '_out.png'], toShowResults);
+        drawnImage = drawnImage(:,:,[3,2,1]);
+        
+        if ~isempty(find(detectedQuadValidity == 0, 1))
+            disp(sprintf('Spurrious detection at %d %d on %s', workQueue{iWork}.iPattern, workQueue{iWork}.iFilename, workQueue{iWork}.inputFilename));
+        end
         
         if mod(iWork, 100) == 0
             disp(sprintf('Finished %d/%d in %f seconds', iWork, length(workQueue), toc(perImageTic)));
