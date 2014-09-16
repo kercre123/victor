a6e809ae7467a0db76e793c66ac335839eb7b1a5
Peter Barnum <peter@anki.com>
2014-09-16 13:14:15 -0700

    * Started python-based decision tree testing * SVM testing in Matlab

--- coretech/vision/matlab/tmp_svm_testExamples.m
+++ coretech/vision/matlab/tmp_svm_testExamples.m
@@ -1,11 +1,12 @@
 
 function tmp_svm_testExamples()
     
-    rerunTraining = true;
+    rerunTraining = false;
+    
+    suffixes = {'100', '100B', '300', '300B', '600', '600B', '1000', '1000B'};
+    %         suffixes = {'100B', '300', '300B', '600', '600B', '1000', '1000B'};
     
     if rerunTraining
-        suffixes = {'100', '100B', '300', '300B', '600', '600B', '1000', '1000B'};
-        
         for iSuffix = 1:length(suffixes)
             trainedSvmClassifiers = runTest('~/tmp/labels', suffixes{iSuffix});
             
@@ -13,6 +14,24 @@ function tmp_svm_testExamples()
         end
     end
     
+    for iSuffix = 1:length(suffixes)
+        if suffixes{iSuffix}(end) == 'B'
+            finalSuffix = [suffixes{iSuffix}(1:(end-1)), ''];
+            load(['~/tmp/labels', suffixes{iSuffix}(1:(end-1))], 'probeLocationsXGrid', 'probeLocationsYGrid');
+        else
+            finalSuffix = [suffixes{iSuffix}(1:end), 'B'];
+            load(['~/tmp/labels', suffixes{iSuffix}], 'probeLocationsXGrid', 'probeLocationsYGrid');
+        end
+        
+        load(['~/tmp/trainedSvm', suffixes{iSuffix}]);
+        
+        load(['~/tmp/labels', finalSuffix],...
+            ['labelNames', finalSuffix], ['labels', finalSuffix], ['featureValues', finalSuffix]);
+        
+        [numCorrect, numTotal] = computeAccuracy(trainedSvmClassifiers, labels100B, labelNames100B, featureValues100B);
+        
+        keyboard
+    end
     
 end % tmp_testExamples()
 
@@ -24,3 +43,32 @@ function trainedSvmClassifiers = runTest(saveFilenamePrefix, suffix)
     eval(['trainedSvmClassifiers = svm_train(labels', suffix, ', featureValues', suffix, ');']);
 end % runTest()
 
+
+function [numCorrect, numTotal] = computeAccuracy(trainedSvmClassifiers, labels, labelNames, featureValues)
+    
+    pBar = ProgressBar('svm_testExamples');
+    pBar.showTimingInfo = true;
+    pBarCleanup = onCleanup(@()delete(pBar));
+    
+    pBar.set_increment(5 / size(featureValues, 2));
+    pBar.set(0);
+    
+    maxLabelId = max(labels);
+    
+    numCorrect = zeros(maxLabelId + 1, 1);
+    numTotal = zeros(maxLabelId + 1, 1);
+    for iImage = 1:size(featureValues, 2)
+        [~, labelId] = svm_predict(trainedSvmClassifiers, featureValues(:,iImage), [], [], [], labelNames, 0, 255);
+
+        numTotal(labelId) = numTotal(labelId) + 1;
+
+        if labelId == labels(iImage)
+            numCorrect(labelId) = numCorrect(labelId) + 1;
+        end
+        
+        if mod(iImage, 5) == 0
+            pBar.set_message(sprintf('Testing %d/%d. Current accuracy %d/%d=%f', iImage, size(featureValues, 2), sum(numCorrect), sum(numTotal), sum(numCorrect)/sum(numTotal)));
+            pBar.increment();
+        end
+    end
+end
