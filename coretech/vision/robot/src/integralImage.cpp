aee6068af56943c618dd107a55aeb719b991902f
Peter Barnum <pbarnum@ubuntu.(none)>
2014-10-13 14:22:18 -0700

    Added manual 4x2 unrolling. Slower than the automoatic 4x4 unrolling ExcTot:4,664us

--- coretech/vision/robot/src/integralImage.cpp
+++ coretech/vision/robot/src/integralImage.cpp
@@ -453,12 +453,55 @@ namespace Anki
           pOutput[x] = static_cast<u8>( pIntegralImage_11[x] - pIntegralImage_10[x] + pIntegralImage_00[x] - pIntegralImage_01[x] );
         }
       } else {
-        s32 x;
+/*        s32 x;
         for(x=minX; x<=(maxX-3); x+=4) {
           pOutput[x]   = static_cast<u8>( ((pIntegralImage_11[x]   - pIntegralImage_10[x]   + pIntegralImage_00[x]   - pIntegralImage_01[x])   * outputMultiply) >> outputRightShift ) ;
           pOutput[x+1] = static_cast<u8>( ((pIntegralImage_11[x+1] - pIntegralImage_10[x+1] + pIntegralImage_00[x+1] - pIntegralImage_01[x+1]) * outputMultiply) >> outputRightShift ) ;
           pOutput[x+2] = static_cast<u8>( ((pIntegralImage_11[x+2] - pIntegralImage_10[x+2] + pIntegralImage_00[x+2] - pIntegralImage_01[x+2]) * outputMultiply) >> outputRightShift ) ;
           pOutput[x+3] = static_cast<u8>( ((pIntegralImage_11[x+3] - pIntegralImage_10[x+3] + pIntegralImage_00[x+3] - pIntegralImage_01[x+3]) * outputMultiply) >> outputRightShift ) ;
+        }
+
+        for(; x<=maxX; x++) {
+          pOutput[x] = static_cast<u8>( ((pIntegralImage_11[x] - pIntegralImage_10[x] + pIntegralImage_00[x] - pIntegralImage_01[x]) * outputMultiply) >> outputRightShift ) ;
+        }
+*/
+        const u32 * restrict pIntegralImageU32_00 = reinterpret_cast<const u32*>(pIntegralImage_00);
+        const u32 * restrict pIntegralImageU32_01 = reinterpret_cast<const u32*>(pIntegralImage_01);
+        const u32 * restrict pIntegralImageU32_10 = reinterpret_cast<const u32*>(pIntegralImage_10);
+        const u32 * restrict pIntegralImageU32_11 = reinterpret_cast<const u32*>(pIntegralImage_11);
+
+        s32 x;
+        for(x=minX; x<=(maxX-7); x+=8) {
+          const uint32x4_t ii00_0 = vld1q_u32(&pIntegralImageU32_00[x]);
+          const uint32x4_t ii01_0 = vld1q_u32(&pIntegralImageU32_01[x]);
+          const uint32x4_t ii10_0 = vld1q_u32(&pIntegralImageU32_10[x]);
+          const uint32x4_t ii11_0 = vld1q_u32(&pIntegralImageU32_11[x]);
+          const uint32x4_t ii00_1 = vld1q_u32(&pIntegralImageU32_00[x+4]);
+          const uint32x4_t ii01_1 = vld1q_u32(&pIntegralImageU32_01[x+4]);
+          const uint32x4_t ii10_1 = vld1q_u32(&pIntegralImageU32_10[x+4]);
+          const uint32x4_t ii11_1 = vld1q_u32(&pIntegralImageU32_11[x+4]);
+
+          const uint32x4_t boxSum_0 = ii11_0 - ii10_0 + ii00_0 - ii01_0;
+          const uint32x4_t boxSum_1 = ii11_1 - ii10_1 + ii00_1 - ii01_1;
+
+          const uint32x4_t normalizedBoxSum_0 = (boxSum_0 * outputMultiply) >> outputRightShift;
+          const uint32x4_t normalizedBoxSum_1 = (boxSum_1 * outputMultiply) >> outputRightShift;
+
+          const uint16x4_t normalizedBoxSumU16_0 = vmovn_u32(normalizedBoxSum_0);
+          const uint16x4_t normalizedBoxSumU16_1 = vmovn_u32(normalizedBoxSum_1);
+  
+          const uint16x8_t normalizedBoxSumU16_10 = vcombine_u16(normalizedBoxSumU16_0, normalizedBoxSumU16_1);
+
+          const uint8x8_t normalizedBoxSumU8_10 = vmovn_u16(normalizedBoxSumU16_10);
+
+          vst1_u8(&pOutput[x], normalizedBoxSumU8_10);
+
+//          const uint8x8_t  normalizedBoxSumU8  = (normalizedBoxSumU16);
+/*
+          pOutput[x]   = static_cast<u8>( ((pIntegralImage_11[x]   - pIntegralImage_10[x]   + pIntegralImage_00[x]   - pIntegralImage_01[x])   * outputMultiply) >> outputRightShift ) ;
+          pOutput[x+1] = static_cast<u8>( ((pIntegralImage_11[x+1] - pIntegralImage_10[x+1] + pIntegralImage_00[x+1] - pIntegralImage_01[x+1]) * outputMultiply) >> outputRightShift ) ;
+          pOutput[x+2] = static_cast<u8>( ((pIntegralImage_11[x+2] - pIntegralImage_10[x+2] + pIntegralImage_00[x+2] - pIntegralImage_01[x+2]) * outputMultiply) >> outputRightShift ) ;
+          pOutput[x+3] = static_cast<u8>( ((pIntegralImage_11[x+3] - pIntegralImage_10[x+3] + pIntegralImage_00[x+3] - pIntegralImage_01[x+3]) * outputMultiply) >> outputRightShift ) ;*/
         }
 
         for(; x<=maxX; x++) {
