ac7f17ae13b1a8ab4df87f020349ab07dcc81682
Peter Barnum <peter@anki.com>
2014-10-28 16:57:34 -0700

    Converted mexFaceDetect to return as many eyes as were found.

--- matlab/mex/mexFaceDetect.cpp
+++ matlab/mex/mexFaceDetect.cpp
@@ -1,3 +1,6 @@
+
+// Usage example: [faces,eyes] = mexFaceDetect(im);
+
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
@@ -27,6 +30,15 @@ void closeHelper(void) {
 #ifndef OPENCV_ROOT_PATH
 #error OPENCV_ROOT_PATH should be defined. You may need to re-run cmake.
 #endif
+
+typedef struct
+{
+  double index;
+  double x;
+  double y;
+  double w;
+  double h;
+} Eye;
 
 //int main(int argc, const char** argv)
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
@@ -79,58 +91,64 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 
   size_t numFaces = faces.size();
 
-  plhs[0] = mxCreateDoubleMatrix(faces.size(), 12, mxREAL);
+  plhs[0] = mxCreateDoubleMatrix(faces.size(), 4, mxREAL);
+  
+  std::vector<Eye> storedEyes;
+  
   double *x = mxGetPr(plhs[0]);
   double *y = x + numFaces;
   double *w = y + numFaces;
   double *h = w + numFaces;
-
-  double *eye1_x = h + numFaces;
-  double *eye1_y = eye1_x + numFaces;
-  double *eye1_w = eye1_y + numFaces;
-  double *eye1_h = eye1_w + numFaces;
-
-  double *eye2_x = eye1_h + numFaces;
-  double *eye2_y = eye2_x + numFaces;
-  double *eye2_w = eye2_y + numFaces;
-  double *eye2_h = eye2_w + numFaces;
-
-  for(int i = 0; i < faces.size(); i++)
+  
+  for(int iFace = 0; iFace < faces.size(); iFace++)
   {
     // Record the face:
-    x[i] = faces[i].x;
-    y[i] = faces[i].y;
-    w[i] = faces[i].width;
-    h[i] = faces[i].height;
+    x[iFace] = faces[iFace].x;
+    y[iFace] = faces[iFace].y;
+    w[iFace] = faces[iFace].width;
+    h[iFace] = faces[iFace].height;
 
     // Try to find eyes:
-    cv::Mat faceROI = grayscaleFrame( faces[i] );
+    cv::Mat faceROI = grayscaleFrame( faces[iFace] );
     std::vector<cv::Rect> eyes;
 
     eyes_cascade->detectMultiScale( faceROI, eyes, 1.1, 2,
       0 |CV_HAAR_SCALE_IMAGE, cv::Size(30, 30) );
 
-    size_t numEyes = eyes.size();
-    if(numEyes < 2) {
-      eye2_x[i] = -1.; eye2_y[i] = -1.; eye2_w[i] = -1.; eye2_h[i] = -1.;
-    }
-    else {
-      eye2_x[i] = x[i] + eyes[1].x;
-      eye2_y[i] = y[i] + eyes[1].y;
-      eye2_w[i] = eyes[1].width;
-      eye2_h[i] = eyes[1].height;
-    }
-
-    if(numEyes < 1) {
-      eye1_x[i] = -1.; eye1_y[i] = -1.; eye1_w[i] = -1.; eye1_h[i] = -1.;
-    }
-    else {
-      eye1_x[i] = x[i] + eyes[0].x;
-      eye1_y[i] = y[i] + eyes[0].y;
-      eye1_w[i] = eyes[0].width;
-      eye1_h[i] = eyes[0].height;
+    const size_t numEyes = eyes.size();
+    for(int iEye=0; iEye<numEyes; iEye++)
+    {
+      Eye eye;
+      eye.index = double(iFace + 1);
+      eye.x = double(x[iFace] + eyes[iEye].x);
+      eye.y = double(y[iFace] + eyes[iEye].y);
+      eye.w = double(eyes[iEye].width);
+      eye.h = double(eyes[iEye].height);
+      
+      storedEyes.push_back(eye);
     }
   } // FOR each face
 
+  plhs[1] = mxCreateDoubleMatrix(storedEyes.size(), 5, mxREAL); // [faceIndex, left, top, width, height]
+
+  double *eye_index = mxGetPr(plhs[1]);
+  double *eye_x = eye_index + storedEyes.size();
+  double *eye_y = eye_x + storedEyes.size();
+  double *eye_w = eye_y + storedEyes.size();
+  double *eye_h = eye_w + storedEyes.size();
+  for(int iEye=0; iEye<storedEyes.size(); iEye++)
+  {
+    eye_index[iEye] =storedEyes[iEye].index;
+    eye_x[iEye] = storedEyes[iEye].x;
+    eye_y[iEye] = storedEyes[iEye].y;
+    eye_w[iEye] = storedEyes[iEye].w;
+    eye_h[iEye] = storedEyes[iEye].h;
+  }
+
   return;
 }
+
+
+
+
+
