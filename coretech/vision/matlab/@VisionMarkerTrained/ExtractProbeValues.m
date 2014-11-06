8f6623f2cbde3cfe1060035ac2574b7fff317cd7
Andrew Stein <andrew@anki.com>
2014-11-06 14:51:09 -0800

    Adding inversions as another "perturbation" of original images, not simply inverting already-perturbed images. This should result in more accurate synthetic examples, especially in the case of varying exposure.

--- coretech/vision/matlab/@VisionMarkerTrained/ExtractProbeValues.m
+++ coretech/vision/matlab/@VisionMarkerTrained/ExtractProbeValues.m
@@ -91,8 +91,8 @@ numSizes     = length(imageSizes);
 numExposures = length(exposures);
 
 fprintf('Will create %d samples for each of %d images = %d total samples.\n', ...
-  numBlurs*numSizes*numExposures*numPerturbations, numImages, ...
-  numBlurs*numSizes*numExposures*numPerturbations*numImages);
+  numBlurs*numSizes*numExposures*numPerturbations*(double(addInversions)+1), numImages, ...
+  numBlurs*numSizes*numExposures*numPerturbations*numImages*(double(addInversions)+1));
 
 labelNames = cell(1, numImages);
 %img = cell(1, numImages);
@@ -101,9 +101,9 @@ corners = [0 0; 0 1; 1 0; 1 1];
 
 labels        = cell(numImages, numBlurs, numSizes, numExposures);
 
-probeValues = cell(numImages, numBlurs, numSizes, numExposures, numPerturbations);
+probeValues = cell(numImages, numBlurs, numSizes, numExposures, numPerturbations, double(addInversions)+1);
 if computeGradMag
-  gradMagValues = cell(numImages, numBlurs, numSizes, numExposures, numPerturbations);
+  gradMagValues = cell(numImages, numBlurs, numSizes, numExposures, numPerturbations, double(addInversions)+1);
 end
 
 perturbSigma = perturbSigma/workingResolution;
@@ -146,68 +146,75 @@ for iImg = 1:numImages
     [nrowsOrig,ncolsOrig,~] = size(imgOrig);
     
     % numBlurs, numSizes, numExposures, numPerturbations
-    for iBlur = 1:numBlurs
-      imgBlur = single(imgOrig);
-      
-      blurSigma = blurSigmas(iBlur);
-      if blurSigma > 0
-        imgBlur = separable_filter(imgBlur, gaussian_kernel(blurSigma*sqrt(nrowsOrig^2 + ncolsOrig^2)));
+    for iInvert = 1:(double(addInversions)+1)
+      if iInvert == 2
+        imgOrig = 1 - imgOrig;
       end
       
-      for iSize = 1:numSizes
+      for iBlur = 1:numBlurs
+        imgBlur = single(imgOrig);
         
-        imgResized = imresize(imgBlur, imageSizes(iSize)*[1 1], 'bilinear'); 
-      
-        for iExp = 1:numExposures
-          img = min(1, (imgResized+blackPoint/255)*exposures(iExp));
+        blurSigma = blurSigmas(iBlur);
+        if blurSigma > 0
+          imgBlur = separable_filter(imgBlur, gaussian_kernel(blurSigma*sqrt(nrowsOrig^2 + ncolsOrig^2)));
+        end
+        
+        for iSize = 1:numSizes
           
-          % Same for all perturbations in following loop
-          img = single(img);
-          [nrows,ncols,~] = size(img);
-          imageCoordsX = linspace(0, 1, ncols);
-          imageCoordsY = linspace(0, 1, nrows);
+          imgResized = imresize(imgBlur, imageSizes(iSize)*[1 1], 'bilinear');
+          
+          for iExp = 1:numExposures
+            img = min(1, (imgResized+blackPoint/255)*exposures(iExp));
             
-          for iPerturb = 1:numPerturbations
+            % Same for all perturbations in following loop
+            img = single(img);
+            [nrows,ncols,~] = size(img);
+            imageCoordsX = linspace(0, 1, ncols);
+            imageCoordsY = linspace(0, 1, nrows);
             
-            if strcmp(perturbationType, 'shift')
-              xPerturb = X + xShift(iPerturb);
-              yPerturb = Y + yShift(iPerturb);
-            else
-              % Sample a perturbation of the corners
-              switch(perturbationType)
-                case 'normal'
-                  perturbation = max(-3*perturbSigma, min(3*perturbSigma, perturbSigma*randn(4,2)));
-                case 'uniform'
-                  perturbation = 2*perturbSigma*rand(4,2) - perturbSigma;
-                otherwise
-                  error('Unrecognized perturbationType "%s".', perturbationType);
+            for iPerturb = 1:numPerturbations
+              
+              if strcmp(perturbationType, 'shift')
+                xPerturb = X + xShift(iPerturb);
+                yPerturb = Y + yShift(iPerturb);
+              else
+                % Sample a perturbation of the corners
+                switch(perturbationType)
+                  case 'normal'
+                    perturbation = max(-3*perturbSigma, min(3*perturbSigma, perturbSigma*randn(4,2)));
+                  case 'uniform'
+                    perturbation = 2*perturbSigma*rand(4,2) - perturbSigma;
+                  otherwise
+                    error('Unrecognized perturbationType "%s".', perturbationType);
+                end
+                
+                corners_i = corners + perturbation;
+                T = cp2tform(corners_i, corners, 'projective');
+                [xPerturb, yPerturb] = tforminv(T, X, Y);
               end
               
-              corners_i = corners + perturbation;
-              T = cp2tform(corners_i, corners, 'projective');
-              [xPerturb, yPerturb] = tforminv(T, X, Y);
-            end
+              probeValues{iImg, iBlur, iSize, iExp, iPerturb, iInvert} = mean(interp2(imageCoordsX, imageCoordsY, img, ...
+                xPerturb, yPerturb, 'linear', 1), 2);
+              
+              if computeGradMag
+                imgGradMag = single(smoothgradient(img));
+                gradMagValues{iImg, iBlur, iSize, iExp, iPerturb, iInvert} = mean(interp2(imageCoordsX, imageCoordsY, img, ...
+                  xPerturb, yPerturb, 'linear', 0), 2);
+              end
+              
+              labels{iImg, iBlur, iSize, iExp, iPerturb, iInvert} = uint32(iImg + (iInvert-1)*numImages);
+              
+            end % FOR each perturbation
             
-            probeValues{iImg, iBlur, iSize, iExp, iPerturb} = mean(interp2(imageCoordsX, imageCoordsY, img, ...
-              xPerturb, yPerturb, 'linear', 1), 2);
+            %labels{iImg, iBlur, iSize, iExp} = iImg*ones(1,numPerturbations, 'uint32');
             
-            if computeGradMag
-              imgGradMag = single(smoothgradient(img));
-              gradMagValues{iImg, iBlur, iSize, iExp, iPerturb} = mean(interp2(imageCoordsX, imageCoordsY, img, ...
-                xPerturb, yPerturb, 'linear', 0), 2);
-            end
-           
-            labels{iImg, iBlur, iSize, iExp, iPerturb} = uint32(iImg);
-            
-          end % FOR each perturbation
+          end % FOR each exposure
           
-          %labels{iImg, iBlur, iSize, iExp} = iImg*ones(1,numPerturbations, 'uint32');
-          
-        end % FOR each exposure
+        end % FOR each size
         
-      end % FOR each size
+      end % FOR each blur
       
-    end % FOR each blur
+    end % FOR inversion
         
     pBar.increment();
     
@@ -282,9 +289,9 @@ pBar.set_message(sprintf(['Interpolating probe locations ' ...
 pBar.set_increment(1/numNeg);
 pBar.set(0);
 
-probeValuesNeg   = zeros(workingResolution^2, numNeg, 'single');
+probeValuesNeg   = zeros(workingResolution^2, numNeg*(double(addInversions)+1), 'single');
 if computeGradMag
-  gradMagValuesNeg = zeros(workingResolution^2, numNeg, 'single');
+  gradMagValuesNeg = zeros(workingResolution^2, numNeg*(double(addInversions)+1), 'single');
 end
 
 for iImg = 1:numNeg
@@ -296,21 +303,34 @@ for iImg = 1:numNeg
         imgNeg = imreadAlphaHelper(fnamesNeg{iImg});
     end
     
-    
     imgNeg = single(imgNeg);
     
     [nrows,ncols,~] = size(imgNeg);
     imageCoordsX = linspace(0, 1, ncols);
     imageCoordsY = linspace(0, 1, nrows);
     
-    probeValuesNeg(:,iImg) = mean(interp2(imageCoordsX, imageCoordsY, imgNeg, ...
-      X, Y, 'linear', 1), 2);
-    
-    if computeGradMag
-      imgGradMag = single(smoothgradient(imgNeg));
-      gradMagValuesNeg(:,iImg) = mean(interp2(imageCoordsX, imageCoordsY, imgGradMag, ...
-        X, Y, 'linear', 0), 2);
-    end
+    for iInvert = 1:(double(addInversions)+1)
+      if computeGradMag
+        imgGradMag = single(smoothgradient(imgNeg));
+        gradMagValuesNeg(:,iImg) = mean(interp2(imageCoordsX, imageCoordsY, imgGradMag, ...
+          X, Y, 'linear', 0), 2);
+      end
+      
+      if iInvert == 2
+        imgNeg = 1 - imgNeg;
+        
+        probeValuesNeg(:,iImg + numNeg) = mean(interp2(imageCoordsX, imageCoordsY, 1 - imgNeg, ...
+          X, Y, 'linear', 1), 2);
+        
+        if computeGradMag
+          % No need to recompute this for negated image
+          gradMagValuesNeg(:,iImg + numNeg) = gradMagValuesNeg(:,iImg);
+        end
+      else 
+        probeValuesNeg(:,iImg) = mean(interp2(imageCoordsX, imageCoordsY, imgNeg, ...
+          X, Y, 'linear', 1), 2);    
+      end
+    end % FOR inversion
     
     pBar.increment();
 end % for each negative example
@@ -327,7 +347,7 @@ if any(noInfo)
 end
 
 labelNames{end+1} = 'INVALID';
-labels = [labels length(labelNames)*ones(1,numNeg,'uint32')];
+labels = [labels length(labelNames)*ones(1,size(probeValuesNeg,2),'uint32')];
 %fnames = [fnames; fnamesNeg];
 probeValues = [probeValues probeValuesNeg];
 if computeGradMag
@@ -336,12 +356,12 @@ end
 
 if addInversions
     % Add white-on-black versions of everything
-    labels        = [labels labels+length(labelNames)];
+    %labels        = [labels labels+length(labelNames)];
     labelNames    = [labelNames cellfun(@(name)['INVERTED_' name], labelNames, 'UniformOutput', false)];
-    probeValues   = [probeValues 1-probeValues];
-    if computeGradMag
-      gradMagValues = [gradMagValues gradMagValues];
-    end
+    %probeValues   = [probeValues 1-probeValues];
+    %if computeGradMag
+    %  gradMagValues = [gradMagValues gradMagValues];
+    %end
     
     % Get rid of INVERTED_INVALID label -- it's just INVALID
     invertedInvalidLabel = find(strcmp(labelNames, 'INVERTED_INVALID'));
