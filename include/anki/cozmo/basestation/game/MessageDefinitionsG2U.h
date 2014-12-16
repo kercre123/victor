5e6aaa4fe92c61f4362ac41b07f9872f8c04a7de
Andrew Stein <andrew@anki.com>
2014-12-15 19:36:01 -0800

    Adding messages and handlers for Game (to UI) messages coming out of basestation/game

---
+++ include/anki/cozmo/basestation/game/MessageDefinitionsG2U.h
@@ -0,0 +1,19 @@
+#if !defined(COZMO_BASESTATION)
+#error COZMO_BASESTATION should be defined when using MessageDefinitionsG2U.h
+#endif
+
+#include "anki/cozmo/shared/MessageDefMacros_Basestation.h"
+
+// ObjectVisionMarker for telling the UI that a VisionMarker from an object
+//  with specified ID was seen at a particular location in the image
+START_MESSAGE_DEFINITION(G2U_ObjectVisionMarker, 1)
+ADD_MESSAGE_MEMBER(u32, objectID)
+ADD_MESSAGE_MEMBER(u16, topLeft_x)
+ADD_MESSAGE_MEMBER(u16, topLeft_y)
+ADD_MESSAGE_MEMBER(u16, topRight_x)
+ADD_MESSAGE_MEMBER(u16, topRight_y)
+ADD_MESSAGE_MEMBER(u16, bottomLeft_x)
+ADD_MESSAGE_MEMBER(u16, bottomLeft_y)
+ADD_MESSAGE_MEMBER(u16, bottomRight_x)
+ADD_MESSAGE_MEMBER(u16, bottomRight_y)
+END_MESSAGE_DEFINITION(G2U_ObjectVisionMarker)
