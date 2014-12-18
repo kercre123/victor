966d5689ea85f27f4fe94c526fc8d39bdfe39748
Jordan Matthew Rivas <jordan@anki.com>
2014-12-18 15:21:51 -0800

    Add Sound Coordinator

--- coretech/common/include/anki/common/basestation/platformPathManager_iOS_interface.h
+++ coretech/common/include/anki/common/basestation/platformPathManager_iOS_interface.h
@@ -1,14 +1,20 @@
 #ifndef ANKI_COMMON_PLATFORM_PATH_MANAGER_IOS_H_
 #define ANKI_COMMON_PLATFORM_PATH_MANAGER_IOS_H_
 
-enum PlatformPathManager_iOS_Scope {
-  PlatformPathManager_iOS_Scope_Test,
+#ifndef NS_ENUM
+#define NS_ENUM(_type, _name) enum _name : _type _name; enum _name : _type
+#endif
+
+typedef NS_ENUM(int, PlatformPathManager_iOS_Scope) {
+  PlatformPathManager_iOS_Scope_Test = 0,
   PlatformPathManager_iOS_Scope_Config,
   PlatformPathManager_iOS_Scope_Animation,
   PlatformPathManager_iOS_Scope_Sound,
-  PlatformPathManager_iOS_Scope_Resource
+  PlatformPathManager_iOS_Scope_Resource,
   //Temp
+  PlatformPathManager_iOS_Scope_Count
 };
+
 
 bool PlatformPathManager_iOS_GetPath(PlatformPathManager_iOS_Scope scope, char *buffer, const int MAX_PATH_LENGTH);
 
---
+++ ios/CozmoVision/CozmoVision/System/SoundCoordinator.h
@@ -0,0 +1,17 @@
+//
+//  SoundCoordinator.h
+//  CozmoVision
+//
+//  Created by Jordan Rivas on 12/17/14.
+//  Copyright (c) 2014 Anki, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SoundCoordinator : NSObject
+
++ (instancetype)defaultCoordinator;
+
+- (void)playSoundWithFilename:(NSString*)filename;
+
+@end
