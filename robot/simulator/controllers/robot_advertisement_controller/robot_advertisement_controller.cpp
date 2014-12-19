f5d16ec11a0dee7c6d663444b55468a93d75b490
Kevin Yoon <kevin@anki.com>
2014-12-19 14:22:56 -0800

    Fixed bug with forced robot advertisement (for physical robots).

--- robot/simulator/controllers/robot_advertisement_controller/robot_advertisement_controller.cpp
+++ robot/simulator/controllers/robot_advertisement_controller/robot_advertisement_controller.cpp
@@ -58,7 +58,8 @@ int main(int argc, char **argv)
   forcedRegistrationMsg.id = forcedRobotId;
   forcedRegistrationMsg.port = ROBOT_RADIO_BASE_PORT;
   forcedRegistrationMsg.protocol = USE_UDP_ROBOT_COMMS == 1 ? Anki::Comms::UDP : Anki::Comms::TCP;
-  snprintf(forcedRegistrationMsg, sizeof(forcedRegistrationMsg.ip), "%s", forcedRobotIP);
+  forcedRegistrationMsg.enableAdvertisement = 1;
+  snprintf((char*)forcedRegistrationMsg.ip, sizeof(forcedRegistrationMsg.ip), "%s", forcedRobotIP);
   
   robotAdService.ProcessRegistrationMsg(forcedRegistrationMsg);
 #endif
