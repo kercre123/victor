b445dbcbc5d922d03e6734ddd8ed97d65d8ba4d4
Daniel Casner <daniel@anki.com>
2015-01-07 16:16:44 -0800

    More yak shaving.

--- robot/som/ov2686/ov2686.c
+++ robot/som/ov2686/ov2686.c
@@ -1,5 +1,15 @@
+#include <linux/init.h>
 #include <linux/module.h>
-#include <linux/kernel.h>
+#include <linux/slab.h>
+#include <linux/i2c.h>
+#include <linux/delay.h>
+#include <linux/videodev2.h>
+#include <media/v4l2-ctrls.h>
+#include <media/v4l2-device.h>
+#include <media/v4l2-subdev.h>
+
+#include <media/v4l2-chip-ident.h>
+#include <media/v4l2-mediabus.h>
 
 int init_module(void) {
   printk(KERN_INFO "init_module() called\n");
@@ -9,4 +19,3 @@ int init_module(void) {
 void cleanup_module(void) {
   printk(KERN_INFO "cleanup_module() called\n");
 }
-
