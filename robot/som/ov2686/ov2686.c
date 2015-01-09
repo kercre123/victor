6f75ef84f9bf8850e58e71cb481f9ad75723a00d
Daniel Casner <daniel@anki.com>
2015-01-08 17:14:13 -0800

    Progress toward a meaningful ov2686 driver. Added modified board I2C definition file to version control. This file lives in arch/arm/mach-omap3logic in the linux source tree in LTIB.

--- robot/som/ov2686/ov2686.c
+++ robot/som/ov2686/ov2686.c
@@ -11,36 +11,111 @@
 #include <media/v4l2-chip-ident.h>
 #include <media/v4l2-mediabus.h>
 
+#include "ov2686.h"
+
 /*** Type definitions */
 
 struct ov2686_info {
   struct v4l2_subdev subdev;
-
+  struct ov2686_platform_data *pdata;
+  struct mutex power_lock;
+  int power_count;
   int ident;
-};
-
-/*** Static data */
-
-static struct v4l2_subdev_ops ov2686_subdev_ops = {
-  .core	 = NULL, // TODO Make this not NULL
-  .video = NULL, // TODO Make this not NULL
 };
 
 /*** Functions */
 
+static inline struct ov2686_info *to_state(struct v4l2_subdev *subdev)
+{
+  return container_of(subdev, struct ov2686_info, subdev);
+}
+
+/* Low-level interface to clock/power */
+
+static int __ov2686_set_power(struct ov2686_info *info, bool on)
+{
+  if (!on) {
+    if (info->pdata->s_xclk) info->pdata->s_xclk(&info->subdev, 0);
+  }
+  else {
+    if (info->pdata->s_xclk) info->pdata->s_xclk(&info->subdev, 1);
+    msleep(1);
+  }
+
+  return 0;
+}
+
+static int ov2686_s_power(struct v4l2_subdev *sd, int on)
+{
+  struct ov2686_info *info = to_state(sd);
+  int ret = 0;
+
+  mutex_lock(&info->power_lock);
+
+  /* If the power count is modified from 0 to != 0 or from != 0 to 0,
+  * update the power state.
+  */
+  if (info->power_count == !on) {
+    ret = __ov2686_set_power(info, !!on);
+    if (ret < 0) goto out;
+  }
+
+  /* Update the power count. */
+  info->power_count += on ? 1 : -1;
+  WARN_ON(info->power_count < 0);
+
+out:
+  mutex_unlock(&info->power_lock);
+  return ret;
+}
+
+/*** Static data */
+
+static const struct v4l2_subdev_core_ops ov2686_subdev_core_ops = {
+  .s_power      = ov2686_s_power,
+};
+
+/*static const struct v4l2_subdev_video_ops ov2686_subdev_video_ops = {
+  .s_stream       = ov2686_s_stream, TODO
+};*/
+
+static struct v4l2_subdev_ops ov2686_subdev_ops = {
+  .core	 = &ov2686_subdev_core_ops,
+  .video = NULL, // TODO &ov2686_subdev_video_ops,
+};
+
+/*** I2C module functions */
 
 static int ov2686_probe(struct i2c_client *client, const struct i2c_device_id *device) {
   struct ov2686_info *info;
+  struct ov2686_platform_data *pdata;
+  struct i2c_adapter *adapter;
 
   printk(KERN_INFO "ov2686 i2c probe\n");
 
-  // Do some initial i2c camera communication here if nessisary
+  pdata = client->dev.platform_data;
+  if (pdata == NULL) {
+    printk(KERN_WARNING "No platform data\n");
+    return -EINVAL;
+  }
+
+  adapter = to_i2c_adapter(client->dev.parent);
+
+  if ( !i2c_check_functionality(adapter, I2C_FUNC_SMBUS_BYTE_DATA)) {
+    printk(KERN_WARNING "I2C-Adapter doesn't support I2C_FUNC_SMBUS_BYTE_DATA\n");
+    return -EIO;
+  }
 
   info = kzalloc(sizeof(struct ov2686_info), GFP_KERNEL);
   if (info == NULL) {
     printk(KERN_WARNING "Failed to allocate driver memory\n");
     return -ENOMEM;
   }
+
+  info->pdata = pdata;
+
+  // Do some initial i2c camera communication here if nessisary
+
 
   v4l2_i2c_subdev_init(&info->subdev, client, &ov2686_subdev_ops);
 
