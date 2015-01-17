009fba9e083748eba4208faba572f2370d537b81
Peter Barnum <peter@anki.com>
2015-01-16 18:03:38 -0800

    Added rgbLeds_simplePattern Arduino. It has LED duty cycle, plus PWM brightness control.

---
+++ tools/arduino_led/rgbLeds_simplePattern/rgbLeds_simplePattern.ino
@@ -0,0 +1,173 @@
+/*
+Modulate LED based on input from serial communication
+*/
+
+#include <stdio.h>
+
+// Each RGB LED counts as three LEDs
+const int numLeds = 6;
+
+const int ledPins[numLeds] = {31, 33, 35, 37, 39, 41};
+
+int modulationPeriodMicroseconds;
+int modulationOnMicroseconds; // percentBrightness = modulationOnMicroseconds / modulationPeriodMicroseconds
+
+int totalPeriodMicroseconds;
+int ledOnMicroseconds[numLeds]; 
+
+void setDefaultTimings()
+{ 
+  modulationPeriodMicroseconds = 1000;
+  modulationOnMicroseconds = 100;
+  totalPeriodMicroseconds = 1000000;
+
+  for(int iLed=0; iLed<numLeds; iLed++) {
+    ledOnMicroseconds[iLed] = totalPeriodMicroseconds / 2;
+  }
+} // void setDefaultTimings()
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+  Serial.begin(19200);
+ 
+  setDefaultTimings();
+  
+  for(int iLed=0; iLed<numLeds; iLed++) {
+    pinMode(ledPins[iLed], OUTPUT);
+  }
+} // void setup() 
+
+void updateTimings()
+{
+  if(Serial.available()) {
+    const int bufferLength = 1024;
+    char buffer[bufferLength];
+    
+    int local_modulationPeriodMicroseconds = -1;
+    int local_modulationOnMicroseconds = -1;
+    int local_totalPeriodMicroseconds = -1;
+    int local_ledOnMicroseconds[numLeds];
+    
+    delay(250);
+    
+    int i;
+    for(i=0; i<bufferLength-1; i++) {
+      if(!Serial.available())
+        break;
+     
+      const int byteRead = Serial.read();
+      
+      buffer[i] = byteRead;
+    }
+    buffer[i] = '\0';
+    
+    //Serial.println(buffer);
+    
+    for(int iLed=0; iLed<numLeds; iLed++) {
+      local_ledOnMicroseconds[iLed] = -1;
+    }
+    
+    // Example 1000,100,1000000,500000,500000,500000,250000,250000,250000
+    sscanf(buffer, "%d,%d,%d,%d,%d,%d,%d,%d,%d", &local_modulationPeriodMicroseconds, &local_modulationOnMicroseconds, &local_totalPeriodMicroseconds, &local_ledOnMicroseconds[0], &local_ledOnMicroseconds[1], &local_ledOnMicroseconds[2], &local_ledOnMicroseconds[3], &local_ledOnMicroseconds[4], &local_ledOnMicroseconds[5]);
+    
+    bool isValid = true;
+    
+    if(local_modulationPeriodMicroseconds < 0 || local_modulationPeriodMicroseconds > 100000 || local_modulationPeriodMicroseconds > totalPeriodMicroseconds ||
+      local_modulationOnMicroseconds < 0 || local_modulationOnMicroseconds > local_modulationOnMicroseconds ||
+      local_totalPeriodMicroseconds < 0 || local_totalPeriodMicroseconds > 10000000) {
+        isValid = false;
+    }
+    
+    for(int iLed=0; iLed<numLeds; iLed++) {
+      if(ledOnMicroseconds[iLed] < 0 || ledOnMicroseconds[iLed] > totalPeriodMicroseconds) {
+        isValid = false;
+        break;
+      }
+    }
+    
+    Serial.print("(");
+    Serial.print(local_modulationPeriodMicroseconds, DEC);
+    Serial.print(", ");
+    Serial.print(local_modulationOnMicroseconds, DEC);
+    Serial.print(", ");
+    Serial.print(local_totalPeriodMicroseconds, DEC);
+    Serial.print(") ");
+    
+    for(int iLed=0; iLed<numLeds; iLed++) {
+      ledOnMicroseconds[iLed] = local_ledOnMicroseconds[iLed];
+      
+      Serial.print(iLed, DEC);
+      Serial.print(":");
+      Serial.print(local_ledOnMicroseconds[iLed], DEC);
+      Serial.print("  ");
+    }
+    
+    if(!isValid) {
+      Serial.println("Invalid input. Use format: modulationPeriodMicroseconds, modulationOnMicroseconds, totalPeriodMicroseconds, ledOnMicroseconds[0], ledOnMicroseconds[1], ledOnMicroseconds[2], ledOnMicroseconds[3], ledOnMicroseconds[4], ledOnMicroseconds[5]");
+      return;
+    }
+
+    // If we received too many bytes, just throw the rest out
+    while(Serial.available()) {
+      Serial.read();
+    }
+    
+    local_modulationPeriodMicroseconds = modulationPeriodMicroseconds;
+    local_modulationOnMicroseconds = modulationOnMicroseconds;
+    local_totalPeriodMicroseconds = totalPeriodMicroseconds;
+  } // if(Serial.available()) {
+} // void updateTimings()
+
+// the loop function runs over and over again forever
+void loop()
+{
+  const unsigned long mainStartTime = micros();
+  
+  bool ledIsOn[numLeds];
+  
+  unsigned long outerStartTime = micros();
+
+  // Outer loop, for "On" vs Off
+  while((outerStartTime - mainStartTime) < totalPeriodMicroseconds) {
+    const unsigned long mainLoopTimeDelta = outerStartTime - mainStartTime;
+    
+    // Turn all lights on that should be on
+    for(int iLed=0; iLed<numLeds; iLed++) {
+      if(mainLoopTimeDelta < ledOnMicroseconds[iLed]) {
+        digitalWrite(ledPins[iLed], HIGH);
+        ledIsOn[iLed] = true;
+      }
+    }
+    
+    // Inner loop, modulating on and off very quickly, to form a brightness
+    // For each LED, wait for the appropriate amount of time, then turn it off
+    const unsigned long innerStartTime = micros();
+    while(true) {
+      const unsigned long curTime = micros();
+      const unsigned long deltaTime = curTime - innerStartTime;
+      
+      if(deltaTime >= modulationPeriodMicroseconds) {
+        break;
+      }
+      
+      for(int iLed=0; iLed<numLeds; iLed++) {
+        if(ledIsOn[iLed] && (deltaTime > modulationOnMicroseconds)) {
+          digitalWrite(ledPins[iLed], LOW);
+          ledIsOn[iLed] = false;
+        }
+      }
+    } // while(true)
+    
+    for(int iLed=0; iLed<numLeds; iLed++) {
+      if(ledIsOn[iLed]) {
+        digitalWrite(ledPins[iLed], LOW);
+        ledIsOn[iLed] = false;
+      }
+    }
+    
+    outerStartTime = micros();
+  } // while((mainLoopTime - loopStartTime) < totalPeriodMicroseconds)
+  
+  updateTimings();
+} // void loop() 
