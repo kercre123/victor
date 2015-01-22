fc3f96311ec2e964e242a8adb5b89e98cebb26c1
Kevin Yoon <kevin@anki.com>
2015-01-22 11:57:53 -0800

    Created .def file for cozmoEngineSignals and using macros a la messages. Interface is safer (i.e. can't accidentally instantiate a signal when you're trying to get it!)

--- basestation/src/signals/cozmoEngineSignals.cpp
+++ basestation/src/signals/cozmoEngineSignals.cpp
@@ -8,33 +8,14 @@
 //  Copyright (c) 2015 Anki, Inc. All rights reserved.
 //
 
-#include "anki/cozmo/basestation/signals/CozmoEngineSignals.h"
-
-// Macro for creating signal defintion
-// e.g.
-// DEF_SIGNAL(CozmoEngineSignals, RobotConnect) creates
-//
-//  static CozmoEngineSignals::RobotConnectSignal _RobotConnectSignal;
-//  CozmoEngineSignals::RobotConnectSignal& CozmoEngineSignals::GetRobotConnectSignal() { return _RobotConnectSignal; }
-//
-#define DEF_SIGNAL(__CLASSNAME__,__SIGNAME__) \
-static __CLASSNAME__::__SIGNAME__##Signal _##__SIGNAME__##Signal; \
-__CLASSNAME__::__SIGNAME__##Signal& __CLASSNAME__::Get##__SIGNAME__##Signal() { return _##__SIGNAME__##Signal; } \
+#include "anki/cozmo/basestation/signals/cozmoEngineSignals.h"
 
 
 namespace Anki {
   namespace Cozmo {
-
-    DEF_SIGNAL(CozmoEngineSignals,RobotConnected)
-    DEF_SIGNAL(CozmoEngineSignals,RobotAvailable)
     
-    DEF_SIGNAL(CozmoEngineSignals,PlaySoundForRobot)
-    DEF_SIGNAL(CozmoEngineSignals,StopSoundForRobot)
-    
-    DEF_SIGNAL(CozmoEngineSignals,RobotImageAvailable)
-    DEF_SIGNAL(CozmoEngineSignals,DeviceDetectedVisionMarker)
-    DEF_SIGNAL(CozmoEngineSignals,RobotObservedObject)
-    DEF_SIGNAL(CozmoEngineSignals,RobotObservedFace)
+#define SIGNAL_DEFINITION_MODE SIGNAL_CLASS_DEFINITION_MODE
+#include "anki/cozmo/basestation/signals/cozmoEngineSignals.def"
     
   } // namespace Cozmo
 } // namespace Anki
--- include/anki/cozmo/basestation/signals/cozmoEngineSignals.h
+++ include/anki/cozmo/basestation/signals/cozmoEngineSignals.h
@@ -14,60 +14,22 @@
 #include "anki/common/basestation/utils/signals/simpleSignal.hpp"
 
 
-// Macro for declaring signal type and accessor function
-// e.g. DECL_SIGNAL(RobotConnect, RobotID_t robotID, bool successful) creates
-//
-//  using RobotConnectSignal = Signal::Signal<void (RobotID_t robotID, bool successful)>;
-//  static RobotConnectSignal& GetRobotConnectSignal();
-//
-//  A listener may subscribe to a signal with a callback function like so.
-//
-//      auto cbRobotConnectSignal = [this](RobotID_t robotID, bool successful) {
-//        this->HandleRobotConnectSignal(robotID, successful);
-//      };
-//      _signalHandles.emplace_back( CozmoEngineSignals::GetRobotConnectSignal().ScopedSubscribe(cbRobotConnectSignal));
-//
-//      where _signalHandles is a container for the SmartHandle that ScopedSubscribe() returns.
-//      In this case, _signalHandles should be a member of the same class as HandleRobotConnectSignal().
-//      See simpleSignal.hpp for more details.
-//
-#define DECL_SIGNAL(__SIGNAME__, ...) \
-using __SIGNAME__##Signal = Signal::Signal<void (__VA_ARGS__)>; \
-static __SIGNAME__##Signal& Get##__SIGNAME__##Signal();
-
-
 namespace Anki {
   namespace Cozmo {
       
     class CozmoEngineSignals {
-      
+    
     public:
-
-      DECL_SIGNAL(RobotConnected, RobotID_t robotID, bool successful)
-      DECL_SIGNAL(RobotAvailable, RobotID_t robotID)
       
-      // Robot signals
-      DECL_SIGNAL(PlaySoundForRobot, RobotID_t robotID, u32 soundID, u8 numLoops, u8 volume)
-      DECL_SIGNAL(StopSoundForRobot, RobotID_t robotID)
+      // 1. Initial include just defines the definition modes for use below
+      #include "anki/cozmo/basestation/signals/cozmoEngineSignals.def"
+    
+      #define SIGNAL_CLASS_NAME CozmoEngineSignals
       
+      // 2. Define all the signal types
+      #define SIGNAL_DEFINITION_MODE SIGNAL_CLASS_DECLARATION_MODE
+      #include "anki/cozmo/basestation/signals/cozmoEngineSignals.def"
       
-      // Vision signals
-      DECL_SIGNAL(RobotImageAvailable,
-                  uint8_t robotID)
-      DECL_SIGNAL(DeviceDetectedVisionMarker,
-                  uint8_t engineID, uint32_t markerType,
-                  float x_upperLeft,  float y_upperLeft,
-                  float x_lowerLeft,  float y_lowerLeft,
-                  float x_upperRight, float y_upperRight,
-                  float x_lowerRight, float y_lowerRight)
-      DECL_SIGNAL(RobotObservedObject,
-                  uint8_t robotID, uint32_t objectID,
-                  float x_upperLeft,  float y_upperLeft,
-                  float width,  float height)
-      DECL_SIGNAL(RobotObservedFace,
-                  uint8_t robotID,
-                  float x_upperLeft, float y_upperLeft,
-                  float width, float height)
     };
     
     
