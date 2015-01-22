61c937264882fca79e3018ea1b3c8fa994c82429
Daniel Casner <daniel@anki.com>
2015-01-22 09:10:20 -0800

    Slightly less stubby stubs
    Switch camera script to raw bayer from YUYV, let the torpedo ISP handle
    the conversion to YUYV.

--- robot/som/cam-mjpeg.sh
+++ robot/som/cam-mjpeg.sh
@@ -5,6 +5,6 @@ yavta -w '0x00980911 720' /dev/v4l-subdev8
 yavta -w '0x0098090e 125' /dev/v4l-subdev8
 yavta -w '0x0098090f 175' /dev/v4l-subdev8
 
-media-ctl -v -f '"mt9p031":0 [SGRBG8 1298x970 (664,541)/1298x970], "OMAP3 ISP CCDC":2 [SGRBG10 1298x970], "OMAP3 ISP preview":1 [UYVY 1298x970], "OMAP3 ISP resizer":1 [UYVY 640x480]'
+media-ctl -v -f '"ov2686":0 [SGRBG8 1600x1200 (0,0)/1600x1200], "OMAP3 ISP CCDC":2 [SGRBG10 1600x1200], "OMAP3 ISP preview":1 [UYVY 1600x1200], "OMAP3 ISP resizer":1 [UYVY 1600x1200]'
 
 gst-launch v4l2src device=/dev/video6 ! ffmpegcolorspace ! jpegenc ! udpsink host=$1 port=$2
--- robot/som/ov2686/vm-build.ps1
+++ robot/som/ov2686/vm-build.ps1
@@ -1,4 +1,3 @@
+clear
 scp ov2686.c vm:dev_bsp/ov2686/ov2686.c
 ssh --% vm 'cd dev_bsp; ./ltib -m shell <<< "cd ov2686 && make && exit"'
-scp vm:dev_bsp/ov2686/ov2686.ko ./ov2686.ko
-scp ov2686.ko torpedo:ov2686.ko
