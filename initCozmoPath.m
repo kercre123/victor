d17e46f656376d07e29d6d46ce1a996d97cc36b7
Andrew Stein <andrew@anki.com>
2015-01-23 15:47:49 -0800

    Adding matlab initCozmoPath for cozmo-game

---
+++ initCozmoPath.m
@@ -0,0 +1,26 @@
+function initCozmoPath
+
+cozmoDir = fileparts(which(mfilename)); % excludes 'matlab'
+addpath(genpath(fullfile(cozmoDir, 'matlab')));
+addpath(fullfile(cozmoDir, 'build', 'lib', 'anki', 'cozmo-engine', 'mex'));
+% addpath(genpath(fullfile(cozmoDir, 'systemTests')));
+
+% coretechLibs = dir(fullfile(cozmoDir, 'coretech'));
+% coretechLibs(~[coretechLibs.isdir]) = []; % keep only directories
+% coretechLibs(1:2) = []; % remove . and .. entries
+% coretechLibs = {coretechLibs.name};
+
+% for i = 1:length(coretechLibs)
+%   matlabDir = fullfile(cozmoDir, 'coretech', coretechLibs{i}, 'matlab');
+%   if isdir(matlabDir)
+%     addpath(genpath(matlabDir));
+%   end
+% end
+
+% For now, coretech-external is special and lives outside this repo, 
+% assumed to be in ../coretech-external
+%run(fullfile(cozmoDir, '..', 'coretech-external', 'matlab', 'initExternalPath.m'));
+
+run(fullfile(cozmoDir, 'lib', 'anki', 'cozmo-engine', 'matlab', 'initCozmoPath.m'));
+
+end % initCozmoPath()
