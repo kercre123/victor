8476baa38580f3f7fd9eed4e9df394ea1654685b
Dante Ratto <dante.ratto@anki.com>
2015-02-03 13:41:25 -0800

    ADDED: singleton to robot engine manager

--- unity/Cozmo/Assets/Scripts/RobotEngineManager.cs
+++ unity/Cozmo/Assets/Scripts/RobotEngineManager.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class RobotEngineManager : MonoBehaviour {
 
+	public static RobotEngineManager instance;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Awake () {
+		instance = this;
 	}
 	
 	// Update is called once per frame
