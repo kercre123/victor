43003985610fb13c839ca160113ed3a9d6d451ba
Dante Ratto <dante.ratto@anki.com>
2015-02-03 11:25:11 -0800

    ADDED: intro

---
+++ unity/Cozmo/Assets/Scripts/Intro.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Intro : MonoBehaviour
+{
+	[SerializeField]
+	protected InputField id;
+	[SerializeField]
+	protected InputField ip;
+	[SerializeField]
+	protected Toggle simulated;
+	[SerializeField]
+	protected Button play;
+
+	protected void Awake()
+	{
+		play.onClick.AddListener( Play );
+	}
+
+	protected void Play()
+	{
+		Debug.Log( "ID: " + id.text );
+		Debug.Log( "IP: " + ip.text );
+		Debug.Log( "Simulated: " + simulated.isOn );
+	}
+}
