50b3342174f4454760155aa26a1797235b9a0703
Kevin Yoon <kevin@anki.com>
2015-02-09 09:35:38 -0800

    Setting to Cozmo3 by default

--- include/anki/cozmo/shared/cozmoConfig.h
+++ include/anki/cozmo/shared/cozmoConfig.h
@@ -10,7 +10,7 @@ namespace Anki {
   namespace Cozmo {
     
     // Uncomment if using COZMO v2
-    #define COZMO2
+    //#define COZMO2
 
     /***************************************************************************
      *
