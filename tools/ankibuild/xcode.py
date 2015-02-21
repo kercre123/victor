9bcfc517b888bafe485a733e44a7dec138a98cfa
Greg Nagel <Greg.Nagel@anki.com>
2015-02-20 18:08:03 -0800

    more work on schemes

--- tools/ankibuild/xcode.py
+++ tools/ankibuild/xcode.py
@@ -14,8 +14,12 @@ class XcodeWorkspace(object):
     def add_project(self, project_path):
         self.projects.append(project_path)
     
-    def add_scheme(self, mode, project_path, target):
-    	scheme = dict(name='MODE_{0}'.format(mode.upper()), mode=mode, project_path=project_path, target=target)
+    def add_scheme_cmake(self, name, project_path):
+    	scheme = dict(template=SCHEME_TEMPLATE_CMAKE, name=name, project_path=project_path)
+    	self.schemes.append(scheme)
+    
+    def add_scheme_ios(self, mode, project_path):
+    	scheme = dict(template=SCHEME_TEMPLATE_IOS, name='MODE_{0}'.format(mode.upper()), mode=mode, project_path=project_path)
     	self.schemes.append(scheme)
 
     def generate(self, path, derived_data_path):
@@ -52,9 +56,9 @@ class XcodeWorkspace(object):
         	util.File.mkdir_p(scheme_dir)
         	for scheme in self.schemes:
         		scheme_path = os.path.join(scheme_dir, '{0}.xcscheme'.format(scheme['name']))
-        		scheme_data = SCHEME_TEMPLATE.format(**scheme)
+        		scheme_data = scheme['template'].format(**scheme)
         		util.File.write(scheme_path, scheme_data)
-
+	
     def generate_file_ref(self, path, tag='group'):
         location = "%s:%s" % (tag, path)
         xml = "<FileRef location = \"%s\"></FileRef>" % location
@@ -112,7 +116,7 @@ def build(
     
     util.File.execute(arguments)
 
-SCHEME_TEMPLATE = '''\
+SCHEME_TEMPLATE_CMAKE = '''\
 <?xml version="1.0" encoding="UTF-8"?>
 <Scheme
    LastUpgradeVersion = "0600"
@@ -120,16 +124,6 @@ SCHEME_TEMPLATE = '''\
    <BuildAction
       parallelizeBuildables = "YES"
       buildImplicitDependencies = "YES">
-      <PreActions>
-         <ExecutionAction
-            ActionType = "Xcode.IDEStandardExecutionActionsCore.ExecutionActionType.ShellScriptAction">
-            <ActionContent
-               title = "Run Script"
-               scriptText = "export ANKI_BUILD_MODE=&quot;{mode}&quot;"
-               shellToInvoke = "/bin/bash">
-            </ActionContent>
-         </ExecutionAction>
-      </PreActions>
       <BuildActionEntries>
          <BuildActionEntry
             buildForTesting = "YES"
@@ -140,8 +134,8 @@ SCHEME_TEMPLATE = '''\
             <BuildableReference
                BuildableIdentifier = "primary"
                BlueprintIdentifier = "6F942CD0023C40FDBB818758"
-               BuildableName = "{target}"
-               BlueprintName = "{target}"
+               BuildableName = "ALL_BUILD"
+               BlueprintName = "ALL_BUILD"
                ReferencedContainer = "container:{project_path}">
             </BuildableReference>
          </BuildActionEntry>
@@ -168,8 +162,8 @@ SCHEME_TEMPLATE = '''\
          <BuildableReference
             BuildableIdentifier = "primary"
             BlueprintIdentifier = "6F942CD0023C40FDBB818758"
-            BuildableName = "{target}"
-            BlueprintName = "{target}"
+            BuildableName = "ALL_BUILD"
+            BlueprintName = "ALL_BUILD"
             ReferencedContainer = "container:{project_path}">
          </BuildableReference>
       </MacroExpansion>
@@ -186,11 +180,134 @@ SCHEME_TEMPLATE = '''\
          <BuildableReference
             BuildableIdentifier = "primary"
             BlueprintIdentifier = "6F942CD0023C40FDBB818758"
-            BuildableName = "{target}"
-            BlueprintName = "{target}"
+            BuildableName = "ALL_BUILD"
+            BlueprintName = "ALL_BUILD"
             ReferencedContainer = "container:{project_path}">
          </BuildableReference>
       </MacroExpansion>
+   </ProfileAction>
+   <AnalyzeAction
+      buildConfiguration = "Debug">
+   </AnalyzeAction>
+   <ArchiveAction
+      buildConfiguration = "Release"
+      revealArchiveInOrganizer = "YES">
+   </ArchiveAction>
+</Scheme>
+'''
+
+SCHEME_TEMPLATE_IOS = '''\
+<?xml version="1.0" encoding="UTF-8"?>
+<Scheme
+   LastUpgradeVersion = "0600"
+   version = "1.3">
+   <BuildAction
+      parallelizeBuildables = "YES"
+      buildImplicitDependencies = "YES">
+      <PreActions>
+         <ExecutionAction
+            ActionType = "Xcode.IDEStandardExecutionActionsCore.ExecutionActionType.ShellScriptAction">
+            <ActionContent
+               title = "Run Script"
+               scriptText = "export ANKI_BUILD_MODE=&quot;{mode}&quot;"
+               shellToInvoke = "/bin/bash">
+            </ActionContent>
+         </ExecutionAction>
+      </PreActions>
+      <BuildActionEntries>
+         <BuildActionEntry
+            buildForTesting = "YES"
+            buildForRunning = "YES"
+            buildForProfiling = "YES"
+            buildForArchiving = "YES"
+            buildForAnalyzing = "YES">
+            <BuildableReference
+               BuildableIdentifier = "primary"
+               BlueprintIdentifier = "1D6058900D05DD3D006BFB54"
+               BuildableName = "cozmo.app"
+               BlueprintName = "Unity-iPhone"
+               ReferencedContainer = "container:{project_path}">
+            </BuildableReference>
+         </BuildActionEntry>
+         <BuildActionEntry
+            buildForTesting = "YES"
+            buildForRunning = "YES"
+            buildForProfiling = "NO"
+            buildForArchiving = "NO"
+            buildForAnalyzing = "YES">
+            <BuildableReference
+               BuildableIdentifier = "primary"
+               BlueprintIdentifier = "5623C57217FDCB0800090B9E"
+               BuildableName = "cozmo.octest"
+               BlueprintName = "Unity-iPhone Tests"
+               ReferencedContainer = "container:{project_path}">
+            </BuildableReference>
+         </BuildActionEntry>
+      </BuildActionEntries>
+   </BuildAction>
+   <TestAction
+      selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
+      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
+      shouldUseLaunchSchemeArgsEnv = "YES"
+      buildConfiguration = "Debug">
+      <Testables>
+         <TestableReference
+            skipped = "NO">
+            <BuildableReference
+               BuildableIdentifier = "primary"
+               BlueprintIdentifier = "5623C57217FDCB0800090B9E"
+               BuildableName = "cozmo.octest"
+               BlueprintName = "Unity-iPhone Tests"
+               ReferencedContainer = "container:{project_path}">
+            </BuildableReference>
+         </TestableReference>
+      </Testables>
+      <MacroExpansion>
+         <BuildableReference
+            BuildableIdentifier = "primary"
+            BlueprintIdentifier = "1D6058900D05DD3D006BFB54"
+            BuildableName = "cozmo.app"
+            BlueprintName = "Unity-iPhone"
+            ReferencedContainer = "container:{project_path}">
+         </BuildableReference>
+      </MacroExpansion>
+   </TestAction>
+   <LaunchAction
+      selectedDebuggerIdentifier = "Xcode.DebuggerFoundation.Debugger.LLDB"
+      selectedLauncherIdentifier = "Xcode.DebuggerFoundation.Launcher.LLDB"
+      launchStyle = "0"
+      useCustomWorkingDirectory = "NO"
+      buildConfiguration = "Debug"
+      ignoresPersistentStateOnLaunch = "NO"
+      debugDocumentVersioning = "YES"
+      allowLocationSimulation = "YES">
+      <BuildableProductRunnable>
+         <BuildableReference
+            BuildableIdentifier = "primary"
+            BlueprintIdentifier = "1D6058900D05DD3D006BFB54"
+            BuildableName = "cozmo.app"
+            BlueprintName = "Unity-iPhone"
+            ReferencedContainer = "container:{project_path}">
+         </BuildableReference>
+      </BuildableProductRunnable>
+      <AdditionalOptions>
+      </AdditionalOptions>
+   </LaunchAction>
+   <ProfileAction
+      shouldUseLaunchSchemeArgsEnv = "YES"
+      savedToolIdentifier = ""
+      useCustomWorkingDirectory = "NO"
+      buildConfiguration = "Release"
+      debugDocumentVersioning = "YES">
+      <BuildableProductRunnable>
+         <BuildableReference
+            BuildableIdentifier = "primary"
+            BlueprintIdentifier = "1D6058900D05DD3D006BFB54"
+            BuildableName = "cozmo.app"
+            BlueprintName = "Unity-iPhone"
+            ReferencedContainer = "container:{project_path}">
+         </BuildableReference>
+      </BuildableProductRunnable>
    </ProfileAction>
    <AnalyzeAction
       buildConfiguration = "Debug">
