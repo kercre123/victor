78c780b738901e271334ca7687165b01bcd4d080
Donovan Drane <donovan.drane@anki.com>
2015-02-26 12:03:52 -0800

    fix to engine ip pref save.

--- unity/Cozmo/Assets/Scripts/Intro.cs
+++ unity/Cozmo/Assets/Scripts/Intro.cs
@@ -103,7 +103,7 @@ public class Intro : MonoBehaviour {
 
 	protected void SaveData() {
 		lastIp = ip.text;
-		lastId = engineIP.text;
+		lastEngineIp = engineIP.text;
 		lastVisualizerIp = visualizerIP.text;
 	}
 
