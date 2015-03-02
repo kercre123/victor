5af5384a5105892c1bc9ec7e202e95766a1daee7
peter-anki <peter@anki.com>
2015-03-02 10:26:10 -0800

    * Added headlights examples to test_rgb.m * Updated test file format

--- matlab/recognition/test_rgb.m
+++ matlab/recognition/test_rgb.m
@@ -17,29 +17,17 @@ function [accuracy, results] = test_rgb(varargin)
     %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/carRgb7/images_%05d.png', [0,99], [1,2,3], [5,5,2]},...
     %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/carRgb8/images_%05d.png', [0,99], [1,2,3], [5,5,2]}};
     
-    redBlueOnly = true;
-    filenamePatterns = {...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue1/images_%05d.png', [0,99], [1,2], [6,6]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue2/images_%05d.png', [0,99], [1,2], [6,6]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue3/images_%05d.png', [0,99], [1,2], [6,6]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue4/images_%05d.png', [0,99], [1,2], [6,6]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue5/images_%05d.png', [0,99], [1,2], [6,6]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue6/images_%05d.png', [0,99], [1,2], [6,6]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue7/images_%05d.png', [0,99], [2,1], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue8/images_%05d.png', [0,99], [2,1], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue9/images_%05d.png', [0,99], [2,1], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [0,99], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [100,199], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [200,299], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [300,399], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [400,499], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [500,599], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [600,699], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [700,799], [1,2], [8,4]},...
-        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [800,899], [1,2], [8,4]}};
-    
 %     redBlueOnly = true;
 %     filenamePatterns = {...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue1/images_%05d.png', [0,99], [1,2], [6,6]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue2/images_%05d.png', [0,99], [1,2], [6,6]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue3/images_%05d.png', [0,99], [1,2], [6,6]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue4/images_%05d.png', [0,99], [1,2], [6,6]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue5/images_%05d.png', [0,99], [1,2], [6,6]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue6/images_%05d.png', [0,99], [1,2], [6,6]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue7/images_%05d.png', [0,99], [2,1], [8,4]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue8/images_%05d.png', [0,99], [2,1], [8,4]},...
+%         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue9/images_%05d.png', [0,99], [2,1], [8,4]},...
 %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [0,99], [1,2], [8,4]},...
 %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [100,199], [1,2], [8,4]},...
 %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [200,299], [1,2], [8,4]},...
@@ -49,15 +37,24 @@ function [accuracy, results] = test_rgb(varargin)
 %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [600,699], [1,2], [8,4]},...
 %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [700,799], [1,2], [8,4]},...
 %         {'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [800,899], [1,2], [8,4]}};
-
+    
 %     redBlueOnly = true;
 %     filenamePatterns = {{'~/Documents/Anki/drive-ar-large-files/blinkingLights/redBlue10/images_%05d.png', [0,999], [1,2], [8,4]}};
+    
+    redBlueOnly = true;
+    filenamePatterns = {...
+        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/headlights1/images_%05d.png', [0,99], [1,2], [10,2]},...
+        {'~/Documents/Anki/drive-ar-large-files/blinkingLights/headlights2/images_%05d.png', [0,99], [1,2], [10,2]}};
 
+    displayIndividualResults = false;
+    
     if ispc()
         for i = 1:length(filenamePatterns)
             filenamePatterns{i}{1} = strrep(filenamePatterns{i}{1}, '~/Documents/', 'c:/');
         end
     end
+    
+    findHeadlights = true;
     
     testCExecutable = true;
     cExecutable = 'C:/Anki/ARGarage/parseLedCode/parseLedCode.msvc2012/Release/test_parseLedCode.exe ';
@@ -78,6 +75,9 @@ function [accuracy, results] = test_rgb(varargin)
     results = cell(length(filenamePatterns), length(parsingTypes), length(alignmentTypes));
     accuracy = cell(length(filenamePatterns), length(parsingTypes), length(alignmentTypes));
     
+    ledSearch_roi = [30, 20, 21, 21]; % opencv format
+    ledSearch_scale = 0.5;
+    
     %     colorNames = {'red', 'green', 'blue'};
     if redBlueOnly
         colorNames = {'R', 'B'};
@@ -88,8 +88,8 @@ function [accuracy, results] = test_rgb(varargin)
     %     for iAlignmentType = length(alignmentTypes):-1:1
     for iAlignmentType = length(alignmentTypes)
         for iParsingType = length(parsingTypes):-1:1
-            processingSize = [120,160];
-            lightSquareWidth = [80] * (processingSize(1) / 240);
+%             processingSize = [60,80];
+%             lightSquareWidth = [80] * (processingSize(1) / 240);
             
             for iFilenamePattern = 1:length(filenamePatterns)
                 %             for iFilenamePattern = 7:9
@@ -121,7 +121,7 @@ function [accuracy, results] = test_rgb(varargin)
                     
                     assert(numFrames == length(whichFirstFrames));
                     
-                    numbers = reshape(numbers(2:end), [8, numFrames]);
+                    numbers = reshape(numbers(2:end), [10, numFrames]);
                     numbers(2:3, :) = numbers(2:3, :) + 1;
                     numbers(6:8, :) = numbers(6:8, :) + 1;
                     
@@ -129,7 +129,21 @@ function [accuracy, results] = test_rgb(varargin)
                     numPositives = numbers(4:5, :);
                     locationX = numbers(6, :);
                     locationY = numbers(7, :);
-                    locationScale = numbers(8, :);
+                    locationScaleIndex = numbers(8, :);
+                    locationScale = numbers(9, :);
+                    firstIndex = numbers(10, :);
+                    
+                    if findHeadlights
+                        for iImage = filenamePatterns{iFilenamePattern}{2}(1):filenamePatterns{iFilenamePattern}{2}(2)
+                            tmpIm = imread(sprintf(filenamePatterns{iFilenamePattern}{1}, iImage));
+                            ims = zeros([size(tmpIm), 15], 'uint8');
+                            for di = 0:14
+                                ims(:,:,:,di+1) = imread(sprintf(filenamePatterns{iFilenamePattern}{1}, iImage+di));
+                            end
+                            
+                            parseLedCode_findHeadlights(ims, whichColors(:, iImage+1), numPositives(:, iImage+1), locationX(iImage+1), locationY(iImage+1), locationScale(iImage+1), firstIndex(iImage+1), ledSearch_roi, ledSearch_scale);
+                        end
+                    end % if findHeadlights
                 else % if testCExecutable
                     if redBlueOnly
                         whichColors = -ones(2, length(whichFirstFrames));
@@ -170,7 +184,9 @@ function [accuracy, results] = test_rgb(varargin)
                                 colorNames{whichColors(3,iFirstFrame)}, numPositives(3,iFirstFrame))];
                         end
                         
-                        disp(outString);
+                        if displayIndividualResults
+                            disp(outString);
+                        end
                     end % for iFirstFrame = 1:length(whichFirstFrames)
                 end % if testCExecutable
                 
@@ -222,8 +238,9 @@ function [accuracy, results] = test_rgb(varargin)
                         outString = [outString, ' ZZZZZ']; %#ok<AGROW>
                     end
                     
-                    disp(outString)
-                    
+                    if displayIndividualResults
+                        disp(outString)
+                    end
                 end % for iFirstFrame = 1:size(whichColors,2)
                 
                 figureIndex = length(parsingTypes)*(iAlignmentType-1) + iParsingType;
@@ -288,12 +305,18 @@ function [accuracy, results] = test_rgb(varargin)
         for iParsingType = 1:length(parsingTypes)
             for iFilenamePattern = 1:length(filenamePatterns)
                 numGood = length(find(accuracy{iFilenamePattern}{iParsingType}{iAlignmentType}<=1)) ;
+                numSkipped = length(find(accuracy{iFilenamePattern}{iParsingType}{iAlignmentType} == 20));
                 numErrors = length(find(accuracy{iFilenamePattern}{iParsingType}{iAlignmentType} ~= 20 & accuracy{iFilenamePattern}{iParsingType}{iAlignmentType} > 1));
                 numTotal = length(accuracy{iFilenamePattern}{iParsingType}{iAlignmentType});
                 
                 percentCorrect = numGood / numTotal;
+                percentSkipped = numSkipped / numTotal;
                 percentBadErrors = numErrors / numTotal;
-                disp(sprintf('%d %d %d) good:%d/%d=%0.2f%% veryBad:%d/%d=%0.2f%%', iAlignmentType, iParsingType, iFilenamePattern, numGood, numTotal, 100*percentCorrect, numErrors, numTotal, 100*percentBadErrors));
+                disp(sprintf('%d %d %d) good:%d/%d=%0.2f%%    \tskipped:%d/%d=%0.2f%%   \tveryBad:%d/%d=%0.2f%%',...
+                    iAlignmentType, iParsingType, iFilenamePattern,...
+                    numGood, numTotal, 100*percentCorrect,...
+                    numSkipped, numTotal, 100*percentSkipped,...
+                    numErrors, numTotal, 100*percentBadErrors));
             end
         end
     end
