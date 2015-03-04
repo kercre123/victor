a6b11bb1f02bfa5ab76f1a72f135281b13b7bd5f
Andrew Stein <andrew@anki.com>
2015-03-04 15:51:41 -0800

    Now emitting a signal when place on ground action completes

--- basestation/src/cozmoActions.cpp
+++ basestation/src/cozmoActions.cpp
@@ -869,6 +869,8 @@ namespace Anki {
         
       } // if robot is not picking/placing or moving
       
+      CozmoEngineSignals::RobotCompletedPlaceObjectOnGroundActionSignal().emit(robot.GetID(), actionResult == SUCCESS);
+      
       return actionResult;
       
     } // CheckIfDone()
