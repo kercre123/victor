6c0cb86deab9fd6d27c4edadb9b4981f42fbaae6
Dante Ratto <dante.ratto@anki.com>
2015-03-05 17:13:53 -0800

    FIXED: action buttons now change text based on the action

--- unity/Cozmo/Assets/Scripts/CozmoVision/CozmoVision1.cs
+++ unity/Cozmo/Assets/Scripts/CozmoVision/CozmoVision1.cs
@@ -57,7 +57,26 @@ public class CozmoVision1 : CozmoVision
 			for( int i = 0; i < actionButtons.Length; ++i )
 			{
 				// if no object selected or being actioned or holding block
-				actionButtons[i].gameObject.SetActive( ( i == 0 && robot.status == Robot.StatusFlag.IS_CARRYING_BLOCK ) || robot.selectedObject > -1 );
+				actionButtons[i].button.gameObject.SetActive( ( i == 0 && robot.status == Robot.StatusFlag.IS_CARRYING_BLOCK ) || robot.selectedObject > -1 );
+
+				if( i == 0 )
+				{
+					if( robot.status == Robot.StatusFlag.IS_CARRYING_BLOCK )
+					{
+						if( robot.selectedObject > -1 )
+						{
+							actionButtons[i].text.text = "Stack";
+						}
+						else
+						{
+							actionButtons[i].text.text = "Drop";
+						}
+					}
+					else
+					{
+						actionButtons[i].text.text = "Pick Up";
+					}
+				}
 			}
 
 			distancePairs.Clear();
--- unity/Cozmo/Assets/Scripts/CozmoVision/CozmoVision2.cs
+++ unity/Cozmo/Assets/Scripts/CozmoVision/CozmoVision2.cs
@@ -18,7 +18,26 @@ public class CozmoVision2 : CozmoVision
 			for( int i = 0; i < actionButtons.Length; ++i )
 			{
 				// if no object selected or being actioned
-				actionButtons[i].gameObject.SetActive( ( i == 0 && robot.status == Robot.StatusFlag.IS_CARRYING_BLOCK ) || robot.selectedObject > -1 );
+				actionButtons[i].button.gameObject.SetActive( ( i == 0 && robot.status == Robot.StatusFlag.IS_CARRYING_BLOCK ) || robot.selectedObject > -1 );
+
+				if( i == 0 )
+				{
+					if( robot.status == Robot.StatusFlag.IS_CARRYING_BLOCK )
+					{
+						if( robot.selectedObject > -1 )
+						{
+							actionButtons[i].text.text = "Stack";
+						}
+						else
+						{
+							actionButtons[i].text.text = "Drop";
+						}
+					}
+					else
+					{
+						actionButtons[i].text.text = "Pick Up";
+					}
+				}
 			}
 
 			for( int i = 0; i < maxObservedObjects; ++i )
