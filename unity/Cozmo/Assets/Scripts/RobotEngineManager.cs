dc7c86af39831dc896f602f6f2f90a0e9491b89a
Donovan Drane <donovan.drane@anki.com>
2015-03-17 12:15:07 -0700

    more interaction slider work.  basics of pick up and stacking/dropping all work.  button stays in search mode by default, for easy canceling on release.  sliding it up part way chooses the first action, sliding all the way up chooses the second action.  assisted actions allow manual speed control while pressed, all automatable actions trigger on release.
    fixes to slalom controller logic.  coz can now score aptly as many times as we like!  additional debug info displayed.

--- unity/Cozmo/Assets/Scripts/RobotEngineManager.cs
+++ unity/Cozmo/Assets/Scripts/RobotEngineManager.cs
@@ -618,7 +618,7 @@ public class RobotEngineManager : MonoBehaviour {
 		
 		U2G_PickAndPlaceObject message = new U2G_PickAndPlaceObject();
 		message.objectID = current.selectedObject;
-		message.usePreDockPose = 1;
+		message.usePreDockPose = 0;
 		message.useManualSpeed = 1;
 		
 		channel.Send( new U2G_Message{ PickAndPlaceObject = message } );
