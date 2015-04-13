48f76d461f483cca0e6dedb478f7b3eed5f1ef96
Donovan Drane <donovan.drane@anki.com>
2015-04-13 16:04:09 -0700

    WIP build instructions logic

--- unity/Cozmo/Assets/Scripts/UI/ScreenMessage.cs
+++ unity/Cozmo/Assets/Scripts/UI/ScreenMessage.cs
@@ -6,6 +6,8 @@ public class ScreenMessage : MonoBehaviour {
 
 	[SerializeField] protected Text text;
 
+	IEnumerator coroutine = null;
+
 	// Use this for initialization
 	void Start () {
 		text.gameObject.SetActive (true);
@@ -17,9 +19,11 @@ public class ScreenMessage : MonoBehaviour {
 
 	public void ShowMessageForDuration(string message, float time_in_seconds, Color color)
 	{
+		if(coroutine != null) StopCoroutine(coroutine);
+
 		//text.text = G2U_Message;
 		ShowMessage (message, color);
-		StartCoroutine(TurnOffText(time_in_seconds));
+		coroutine = StartCoroutine(TurnOffText(time_in_seconds));
 	}
 
 	public void ShowMessage(string message, Color color)
