a4fc6c6e41282b6d53ae38db1e4b2294fe15f695
Andrew Stein <andrew@anki.com>
2015-04-22 17:33:57 -0700

    Checking in CheckIntraLibraryDistances.m on master since I accidentally made changes before switching to nearest neighbor branch

---
+++ coretech/vision/matlab/CheckIntraLibraryDistances.m
@@ -0,0 +1,41 @@
+% Perform all pairwise comparisons to check how distinct elements in the
+% library are
+function d = CheckIntraLibraryDistances(nnLibrary)
+
+N = size(nnLibrary.probeValues,2);
+X = single(nnLibrary.probeValues);
+if isfield(nnLibrary, 'gradMagWeights')
+  W = single(nnLibrary.gradMagWeights);
+else
+  W = ones(size(X), 'single');
+end
+sumW = sum(W,1);
+
+d = nan(N);
+for i = 1:N
+
+  whichCols = (i+1):N;
+  d(i,(i+1):end) = sum(W(:,whichCols).*single(imabsdiff(X(:,i*ones(1,N-i)), X(:,whichCols))),1) ./ sumW(whichCols);
+  
+end
+
+if nargout==0
+  imagesc(d), axis image
+  clear d
+end
+
+end
+
+
+function helper
+%%
+[iSame,jSame] = find(d < 10);
+for i = 1:length(iSame)
+  subplot 121, imagesc(reshape(nnLibrary.probeValues(:,iSame(i)), 32,32)), axis image 
+  title(sprintf('%d of %d', i, length(iSame)))
+  subplot 122, imagesc(reshape(nnLibrary.probeValues(:,jSame(i)), 32,32)), axis image
+  title(d(iSame(i),jSame(i)))
+  pause
+end
+
+end
