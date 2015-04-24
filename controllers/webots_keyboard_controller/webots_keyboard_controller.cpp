b2113565e6010efcac9efcfed7209eeaa45bfd6e
Kevin Yoon <kevin@anki.com>
2015-04-23 19:03:17 -0700

    Support for head nodding test in webots_keyboard_controller

--- controllers/webots_keyboard_controller/webots_keyboard_controller.cpp
+++ controllers/webots_keyboard_controller/webots_keyboard_controller.cpp
@@ -661,7 +661,8 @@ namespace Anki {
                   break;
                 case TM_HEAD:
                   p1 = root_->getField("headTest_flags")->getSFInt32();
-                  p2 = 25;
+                  p2 = root_->getField("headTest_nodCycleTimeMS")->getSFInt32();  // Nodding cycle time in ms (if HTF_NODDING flag is set)
+                  p3 = 25;
                   break;
                 case TM_LIGHTS:
                   // p1: flags (See LightTestFlags)
