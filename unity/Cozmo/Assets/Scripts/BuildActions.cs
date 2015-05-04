99ba81d1bbb540879035efc30bd9799d77494ac7
Donovan Drane <donovan.drane@anki.com>
2015-05-04 12:13:37 -0700

    gamecontrollers now manage an array of GameActions for its GameStates.  BuildActions added and set for both games as the action type during Build phase.

---
+++ unity/Cozmo/Assets/Scripts/BuildActions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildActions : GameActions {
+
+	public override void Drop( bool onRelease, ObservedObject selectedObject )
+	{
+		if( !onRelease ) return;
+
+		//GameLayoutTracker.instance
+		//run validation prediction
+		// if it will fail layout contraints, abort here and throw audio and text about why its a bad drop
+
+		ActionButtonClick();
+		base.Drop(onRelease, selectedObject);
+	}
+
+	public override void Stack( bool onRelease, ObservedObject selectedObject )
+	{
+		if( robot == null || !onRelease ) return;
+
+		//run validation prediction
+		// if it will fail layout contraints, abort here and throw audio and text about why its a bad drop
+
+		ActionButtonClick();
+		base.Stack(onRelease, selectedObject);
+	}
+
+}
