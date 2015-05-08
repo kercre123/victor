778f9ecbbb68c9a2fa6d3d154fac99e8f2d044d9
Adam Ruja <adam.ruja@anki.com>
2015-05-08 14:54:20 -0700

    added score notification sounds

--- unity/Cozmo/Assets/Scripts/Games/GoldRushController.cs
+++ unity/Cozmo/Assets/Scripts/Games/GoldRushController.cs
@@ -23,6 +23,8 @@ public class GoldRushController : GameController {
 	[SerializeField] protected AudioClip newHighScore;
 
 	[SerializeField] protected AudioClip timeExtension;
+	[SerializeField] protected AudioClip[] scoreSounds;
+	[SerializeField] protected AudioClip points;
 	public float detectRangeDelayFar = 2.0f;
 	public float detectRangeDelayClose = .2f;
 	public float light_messaging_delay = .05f;
@@ -768,11 +770,22 @@ public class GoldRushController : GameController {
 		if( goldExtractingObject != null ) goldExtractingObject.SetActiveObjectLEDs (0);
 		// PlayNotificationAudio (collectedSound);
 		// award points
+
+		robot.isBusy = false;
+
+		yield return new WaitForSeconds(depositTrimTime);
+
+		PlayNotificationAudio(scoreSounds[numDrops]);
+		yield return new WaitForSeconds(scoreSounds[numDrops].length+.05f);
+
 		numDrops++;
 		scores[0]+= 10 * numDrops;
 
-		robot.isBusy = false;
-		yield return new WaitForSeconds(depositTrimTime);
+		if( scores < 2760 )
+		{
+			PlayNotificationAudio(points);
+			yield return new WaitForSeconds(points.length+.05f);
+		}
 
 		if( scores[0] > oldHighScore && !setHighScore )
 		{
