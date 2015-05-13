4b916be33369fd8841b18553b73bbdb499618037
Dante Ratto <dante.ratto@anki.com>
2015-05-13 12:59:07 -0700

    ADDED: celebration lights ADDED: we are the champions

--- unity/Cozmo/Assets/Scripts/Games/SlalomController.cs
+++ unity/Cozmo/Assets/Scripts/Games/SlalomController.cs
@@ -156,7 +156,6 @@ public class SlalomController : GameController {
 		if(textObservedCount != null) {
 			textObservedCount.text = "obstacles: " +obstacles.Count.ToString();
 		}
-
 	}
 
 	protected override void Enter_PRE_GAME() {
@@ -283,7 +282,8 @@ public class SlalomController : GameController {
 		base.Exit_PLAYING();
 
 		text_finalTime.text = "Final Time: " + stateTimer.ToString("f2") + " seconds";
-		ClearLights();
+		//ClearLights();
+		CelebrationLights();
 	}
 
 	protected override bool IsGameReady() {
@@ -299,7 +299,7 @@ public class SlalomController : GameController {
 		return false;
 	}
 
-	Vector2 GetCornerVector(ActiveBlock obstacle, int cornerIndex, bool clock, ActiveBlock previous, bool debug=false) {
+	private Vector2 GetCornerVector(ActiveBlock obstacle, int cornerIndex, bool clock, ActiveBlock previous, bool debug=false) {
 
 		float angle = idealCornerAngles[cornerIndex] * (clock ? -1f : 1f);
 
@@ -329,7 +329,7 @@ public class SlalomController : GameController {
 
 	int lastLapCount = 1;
 
-	void AdvanceCorner() {
+	private void AdvanceCorner() {
 
 		//trigger lap score and sound when we cross our first corner again
 		if(currentCorner == 0 && lastLapCount != currentLap) {
@@ -378,7 +378,7 @@ public class SlalomController : GameController {
 		return corner;
 	}
 
-	public void UpdateCornerLights(int corner, int next, bool nextCornerOnNextObstacle, bool debug=false)
+	private void UpdateCornerLights(int corner, int next, bool nextCornerOnNextObstacle, bool debug=false)
 	{
 		if(robot == null) return;
 		if(currentObstacle == null) return;
@@ -421,7 +421,7 @@ public class SlalomController : GameController {
 			}
 
 			//refresh light settings
-			for(int i = 0; i < 8; ++i) {
+			for(int i = 0; i < obstacle.lights.Length; ++i) {
 
 				if(i == currentTopLightIndex || i == currentBottomLightIndex) {
 					obstacle.lights[i].onColor = currentColor_unit;
@@ -448,13 +448,13 @@ public class SlalomController : GameController {
 
 	}
 
-	public void ClearLights()
+	/*private void ClearLights()
 	{
 
 		for(int obstacleIndex=0; obstacleIndex < obstacles.Count; obstacleIndex++) {
 			ActiveBlock obstacle = obstacles[obstacleIndex];
 			obstacle.relativeMode = 0;
-			for(int i = 0; i < 8; ++i) {
+			for(int i = 0; i < obstacle.lights.Length; ++i) {
 				obstacle.lights[i].onColor = 0;
 				obstacle.lights[i].onPeriod_ms = 1000;
 				obstacle.lights[i].offPeriod_ms = 0;
@@ -463,8 +463,7 @@ public class SlalomController : GameController {
 				obstacle.lights[i].transitionOffPeriod_ms = 0;
 			}
 		}
-	}
-
+	}*/
 
 	public int testLightIndex = 0;
 	public void TestLights()
@@ -473,7 +472,7 @@ public class SlalomController : GameController {
 			ActiveBlock obstacle = obstacles[obstacleIndex];
 			obstacle.relativeMode = 0;
 
-			for(int i = 0; i < 8; ++i) {
+			for(int i = 0; i < obstacle.lights.Length; ++i) {
 				
 				if(i == testLightIndex) {
 					obstacle.lights[i].onColor = currentColor_unit;
@@ -491,5 +490,27 @@ public class SlalomController : GameController {
 				obstacle.lights[i].transitionOffPeriod_ms = 0;
 			}
 		}
+	}
+
+	private void CelebrationLights()
+	{
+		for(int obstacleIndex = 0; obstacleIndex < obstacles.Count; ++obstacleIndex) {
+			ActiveBlock obstacle = obstacles[obstacleIndex];
+			obstacle.relativeMode = 0;
+			
+			for(int i = 0; i < obstacle.lights.Length; ++i) {
+				obstacle.lights[i].onColor = CycleColors(i);
+				obstacle.lights[i].onPeriod_ms = 125;
+				obstacle.lights[i].offPeriod_ms = 125;
+				obstacle.lights[i].offColor =  CycleColors(i + 3);
+				obstacle.lights[i].transitionOnPeriod_ms = 0;
+				obstacle.lights[i].transitionOffPeriod_ms = 0;
+			}
+		}
+	}
+
+	private uint CycleColors(int i) // cycle through all colors, but don't use black
+	{
+		return CozmoPalette.instance.GetUIntColorForActiveBlockMode(++i < (int)ActiveBlock.Mode.Count? (ActiveBlock.Mode)i : (ActiveBlock.Mode)(i+1 - (int)ActiveBlock.Mode.Count));
 	}
 }
