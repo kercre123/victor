6d4c6ab668c71b659d87222dd98e65cbf1c4e1f4
Andrew Stein <astein@neocitrus.com>
2015-05-19 16:14:19 -0700

    Updated NN library

--- matlab/createRotatedVisionMarkers.m
+++ matlab/createRotatedVisionMarkers.m
@@ -1,5 +1,5 @@
-rootPath = '~/Box Sync/Cozmo SE/VisionMarkers/lightCubeD/withFiducials';
-fnames = getfnames(rootPath, '*.png');
+rootPath = '~/Code/cozmo-game/lib/anki/products-cozmo-assets/VisionMarkers/symbols/withThinFiducials';
+fnames = getfnames(rootPath, 'goalFlag.png');
 if ~isdir(fullfile(rootPath, 'rotated'))
     mkdir(fullfile(rootPath, 'rotated'))
 end
