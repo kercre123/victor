1e4f586dedf0966e867fd57e4d4a88534eef0798
Adam Ruja <adam.ruja@anki.com>
2015-06-03 10:22:54 -0700

    proper cancel for drive to object

--- unity/Cozmo/Assets/Scripts/Games/GoldRushController.cs
+++ unity/Cozmo/Assets/Scripts/Games/GoldRushController.cs
@@ -950,8 +950,8 @@ public class GoldRushController : GameController {
 			UpdateReturning(true);
 			yield return 0;
 		}
-
-		robot.CancelAction(RobotActionType.DRIVE_TO_AND_TRAVERSE_OBJECT);
+		Debug.LogError("should be cancelling action");
+		robot.CancelAction(RobotActionType.DRIVE_TO_OBJECT);
 		EnterPlayState(PlayState.DEPOSITING);
 
 
