3d1e9c09c2b9e15371522a0153d30fc435c9ba57
Daniel Casner <daniel@anki.com>
2015-06-09 11:26:48 -0700

    Explicitly invoke python interpreter for Espressif burn script.

--- robot/espressif/burn.sh
+++ robot/espressif/burn.sh
@@ -4,4 +4,4 @@ ESPTOOL=../../../coretech-external/espressif/esptool/esptool.py
 
 
 
-$ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/eagle.flash.bin 0x40000 bin/eagle.irom0text.bin
+python $ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/eagle.flash.bin 0x40000 bin/eagle.irom0text.bin
