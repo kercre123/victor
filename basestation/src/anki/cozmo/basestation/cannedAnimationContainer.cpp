e4d28d73c499f4502bdd06fdb02f161b570c4725
Kevin Yoon <kevin@anki.com>
2015-06-16 17:12:38 -0700

    Re-enabled sending of animations to robot. Commented out eyeController stuff because the EyeController.Disabled messages were clogging up the robot's send buffer. (His face is completely different now anyway so the EyeController will be replaced by a FaceController or something.)

--- basestation/src/anki/cozmo/basestation/cannedAnimationContainer.cpp
+++ basestation/src/anki/cozmo/basestation/cannedAnimationContainer.cpp
@@ -115,11 +115,6 @@ namespace Cozmo {
    
   Result CannedAnimationContainer::Send(RobotID_t robotID, IRobotMessageHandler* msgHandler)
   {
-    // TODO: Skipping loading of animations because wifi issues are currently aggravated
-    //        by the barrage of messages on connection.
-    return RESULT_OK;
-    
-    
     Result lastResult = RESULT_OK;
     
     PRINT_STREAM_INFO("CannedAnimationContainer.Send",
