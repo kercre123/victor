f9e445a4ef63f0cfb66c7123c10a341a353686ee
Daniel Casner <daniel@anki.com>
2015-06-18 14:36:57 -0700

    Put flash speed back down, sigh.

--- robot/espressif/burn.sh
+++ robot/espressif/burn.sh
@@ -2,7 +2,7 @@
 
 ESPTOOL=../../../coretech-external/espressif/esptool/esptool.py
 
-python2 $ESPTOOL --port $1 --baud 460800 write_flash 0x00000 bin/boot_v1.2.bin \
+python2 $ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/boot_v1.2.bin \
                                                      0x01000 bin/upgrade/user1.512.new.0.bin \
                                                      0x41000 bin/upgrade/user2.512.new.0.bin \
                                                      0x7c000 bin/esp_init_data_default.bin \
