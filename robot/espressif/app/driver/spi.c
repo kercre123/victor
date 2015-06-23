d72507af06b8e8ac3f65a40eaccb2d9c1f2aa6a5
Daniel Casner <daniel@anki.com>
2015-06-23 08:04:21 -0700

    Working on SPI driver appropriate for our use.

--- robot/espressif/app/driver/spi.c
+++ robot/espressif/app/driver/spi.c
@@ -1,40 +1,33 @@
-
 #include "driver/spi.h"
 #include "driver/spi_overlap.h"
 
-#define CACHE_FLASH_CTRL_REG 0x3ff0000C
-#define CACHE_FLUSH_START_BIT BIT0
-#define CACHE_EMPTY_FLAG_BIT BIT1
-/******************************************************************************
- * FunctionName : cache_flush
- * Description  : clear all the cpu cache data for stability test.
-*******************************************************************************/
-void cache_flush(void)
-{
-   while(READ_PERI_REG(CACHE_FLASH_CTRL_REG)&CACHE_EMPTY_FLAG_BIT) {
-      CLEAR_PERI_REG_MASK(CACHE_FLASH_CTRL_REG, CACHE_FLUSH_START_BIT);
-      SET_PERI_REG_MASK(CACHE_FLASH_CTRL_REG, CACHE_FLUSH_START_BIT);
-   }
-   while(!(READ_PERI_REG(CACHE_FLASH_CTRL_REG)&CACHE_EMPTY_FLAG_BIT));
-   	
-   CLEAR_PERI_REG_MASK(CACHE_FLASH_CTRL_REG, CACHE_FLUSH_START_BIT);
-}
-/******************************************************************************
- * FunctionName : spi_master_init
- * Description  : SPI master initial function for common byte units transmission
- * Parameters   : uint8 spi_no - SPI module number, Only "SPI" and "HSPI" are valid
-*******************************************************************************/
-void ICACHE_FLASH_ATTR
-    spi_master_init(uint8 spi_no)
-{
-	uint32 regvalue; 
 
-	if(spi_no>1) 		return; //handle invalid input number
-	
+uint32 ICACHE_FLASH_ATTR spi_master_init(const SPIBus spi_no, uint32 frequency)
+{
+	uint32 regvalue;
+
+	if(spi_no>1) return; //handle invalid input number
+
+	// Calculate the actual frequency
+	if (frequency > 40000000)
+	{
+		frequency = 80000000;
+	}
+
+
+  // Setup the GPIO PIN_FUNC_SELECT
+  if (spi_no == SPI)
+  {
+		WRITE_PERI_REG(PERIPHS_IO_MUX, 0x005 )
+  }
+  else
+
+  }
+
 	SET_PERI_REG_MASK(SPI_USER(spi_no), SPI_CS_SETUP|SPI_CS_HOLD|SPI_USR_COMMAND);
 	CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_FLASH_MODE);
 
-	WRITE_PERI_REG(SPI_CLOCK(spi_no), 
+	WRITE_PERI_REG(SPI_CLOCK(spi_no),
 					((3&SPI_CLKCNT_N)<<SPI_CLKCNT_N_S)|
 					((1&SPI_CLKCNT_H)<<SPI_CLKCNT_H_S)|
 					((3&SPI_CLKCNT_L)<<SPI_CLKCNT_L_S)); //clear bit 31,set SPI clock div
@@ -52,16 +45,16 @@ void ICACHE_FLASH_ATTR
 	uint32 regvalue;
 	uint8 bytetemp;
 	if(spi_no>1) 		return; //handle invalid input number
-	
+
 	if(high_bit)		bytetemp=(low_8bit>>1)|0x80;
 	else				bytetemp=(low_8bit>>1)&0x7f;
-	
-	regvalue= ((8&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S)|((uint32)bytetemp);		//configure transmission variable,9bit transmission length and first 8 command bit 
+
+	regvalue= ((8&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S)|((uint32)bytetemp);		//configure transmission variable,9bit transmission length and first 8 command bit
 	if(low_8bit&0x01) 	regvalue|=BIT15;        //write the 9th bit
 	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);		//waiting for spi module available
 	WRITE_PERI_REG(SPI_USER2(spi_no), regvalue);				//write  command and command length into spi reg
 	SET_PERI_REG_MASK(SPI_CMD(spi_no), SPI_USR);		//transmission start
-//	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);	
+//	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);
 }
 /******************************************************************************
  * FunctionName : spi_mast_byte_write
@@ -81,11 +74,11 @@ void ICACHE_FLASH_ATTR
 
 	//SPI_FLASH_USER2 bit28-31 is cmd length,cmd bit length is value(0-15)+1,
 	// bit15-0 is cmd value.
-	WRITE_PERI_REG(SPI_USER2(spi_no), 
+	WRITE_PERI_REG(SPI_USER2(spi_no),
 					((7&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S)|((uint32)data));
 	SET_PERI_REG_MASK(SPI_CMD(spi_no), SPI_USR);
-	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);	
- }  
+	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);
+ }
 
 /******************************************************************************
  * FunctionName : spi_byte_write_espslave
@@ -109,7 +102,7 @@ void ICACHE_FLASH_ATTR
 	//SPI_FLASH_USER2 bit28-31 is cmd length,cmd bit length is value(0-15)+1,
 	// bit15-0 is cmd value.
 	//0x70000000 is for 8bits cmd, 0x04 is eps8266 slave write cmd value
-	WRITE_PERI_REG(SPI_USER2(spi_no), 
+	WRITE_PERI_REG(SPI_USER2(spi_no),
 					((7&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S)|4);
 	WRITE_PERI_REG(SPI_W0(spi_no), (uint32)(data));
 	SET_PERI_REG_MASK(SPI_CMD(spi_no), SPI_USR);
@@ -136,10 +129,10 @@ void ICACHE_FLASH_ATTR
 		//SPI_FLASH_USER2 bit28-31 is cmd length,cmd bit length is value(0-15)+1,
 	// bit15-0 is cmd value.
 	//0x70000000 is for 8bits cmd, 0x06 is eps8266 slave read cmd value
-	WRITE_PERI_REG(SPI_USER2(spi_no), 
+	WRITE_PERI_REG(SPI_USER2(spi_no),
 					((7&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S)|6);
 	SET_PERI_REG_MASK(SPI_CMD(spi_no), SPI_USR);
-	
+
 	while(READ_PERI_REG(SPI_CMD(spi_no))&SPI_USR);
 	*data=(uint8)(READ_PERI_REG(SPI_W0(spi_no))&0xff);
  }
@@ -154,7 +147,7 @@ void ICACHE_FLASH_ATTR
 void ICACHE_FLASH_ATTR
     spi_slave_init(uint8 spi_no,uint8 data_len)
 {
-    uint32 regvalue; 
+    uint32 regvalue;
     uint32 data_bit_len;
     if(spi_no>1)
         return; //handle invalid input number
@@ -168,29 +161,29 @@ void ICACHE_FLASH_ATTR
     ////WRITE_PERI_REG(PERIPHS_IO_MUX, 0x105); //clear bit9//TEST
     if(spi_no==SPI){
         PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_CLK_U, 1);//configure io to spi mode
-        PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_CMD_U, 1);//configure io to spi mode	
-        PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA0_U, 1);//configure io to spi mode	
-        PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA1_U, 1);//configure io to spi mode	
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_CMD_U, 1);//configure io to spi mode
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA0_U, 1);//configure io to spi mode
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_SD_DATA1_U, 1);//configure io to spi mode
     }else if(spi_no==HSPI){
         PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, 2);//configure io to spi mode
-        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, 2);//configure io to spi mode	
-        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, 2);//configure io to spi mode	
-        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, 2);//configure io to spi mode	
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, 2);//configure io to spi mode
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTMS_U, 2);//configure io to spi mode
+        PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDO_U, 2);//configure io to spi mode
     }
 
     //regvalue=READ_PERI_REG(SPI_FLASH_SLAVE(spi_no));
     //slave mode,slave use buffers which are register "SPI_FLASH_C0~C15", enable trans done isr
     //set bit 30 bit 29 bit9,bit9 is trans done isr mask
-    SET_PERI_REG_MASK(	SPI_SLAVE(spi_no), 
+    SET_PERI_REG_MASK(	SPI_SLAVE(spi_no),
     						SPI_SLAVE_MODE|SPI_SLV_WR_RD_BUF_EN|
                                          	SPI_SLV_WR_BUF_DONE_EN|SPI_SLV_RD_BUF_DONE_EN|
                                          	SPI_SLV_WR_STA_DONE_EN|SPI_SLV_RD_STA_DONE_EN|
                                          	SPI_TRANS_DONE_EN);
-    //disable general trans intr 
+    //disable general trans intr
     //CLEAR_PERI_REG_MASK(SPI_SLAVE(spi_no),SPI_TRANS_DONE_EN);
 
     CLEAR_PERI_REG_MASK(SPI_USER(spi_no), SPI_FLASH_MODE);//disable flash operation mode
-    SET_PERI_REG_MASK(SPI_USER(spi_no),SPI_USR_MISO_HIGHPART);//SLAVE SEND DATA BUFFER IN C8-C15 
+    SET_PERI_REG_MASK(SPI_USER(spi_no),SPI_USR_MISO_HIGHPART);//SLAVE SEND DATA BUFFER IN C8-C15
 
 
 //////**************RUN WHEN SLAVE RECIEVE*******************///////
@@ -200,12 +193,12 @@ void ICACHE_FLASH_ATTR
     WRITE_PERI_REG(SPI_CLOCK(spi_no), 0);
 
 
-    
-/////***************************************************//////	
 
-    //set 8 bit slave command length, because slave must have at least one bit addr, 
-    //8 bit slave+8bit addr, so master device first 2 bytes can be regarded as a command 
-    //and the  following bytes are datas, 
+/////***************************************************//////
+
+    //set 8 bit slave command length, because slave must have at least one bit addr,
+    //8 bit slave+8bit addr, so master device first 2 bytes can be regarded as a command
+    //and the  following bytes are datas,
     //32 bytes input wil be stored in SPI_FLASH_C0-C7
     //32 bytes output data should be set to SPI_FLASH_C8-C15
     WRITE_PERI_REG(SPI_USER2(spi_no), (0x7&SPI_USR_COMMAND_BITLEN)<<SPI_USR_COMMAND_BITLEN_S); //0x70000000
@@ -216,15 +209,15 @@ void ICACHE_FLASH_ATTR
                                                                                         ((0x7&SPI_SLV_STATUS_BITLEN)<<SPI_SLV_STATUS_BITLEN_S)|
                                                                                        ((0x7&SPI_SLV_WR_ADDR_BITLEN)<<SPI_SLV_WR_ADDR_BITLEN_S)|
                                                                                        ((0x7&SPI_SLV_RD_ADDR_BITLEN)<<SPI_SLV_RD_ADDR_BITLEN_S));
-    
-    SET_PERI_REG_MASK(SPI_PIN(spi_no),BIT19);//BIT19   
 
-    //maybe enable slave transmission liston 
+    SET_PERI_REG_MASK(SPI_PIN(spi_no),BIT19);//BIT19
+
+    //maybe enable slave transmission liston
     SET_PERI_REG_MASK(SPI_CMD(spi_no),SPI_USR);
     //register level2 isr function, which contains spi, hspi and i2s events
     ETS_SPI_INTR_ATTACH(spi_slave_isr_handler,NULL);
     //enable level2 isr, which contains spi, hspi and i2s events
-    ETS_SPI_INTR_ENABLE(); 
+    ETS_SPI_INTR_ENABLE();
 }
 
 
@@ -248,7 +241,7 @@ void ICACHE_FLASH_ATTR
  /******************************************************************************
  * FunctionName : hspi_master_readwrite_repeat
  * Description  : SPI master test  function for reading and writing esp8266 slave buffer,
- 			the function uses HSPI module 
+ 			the function uses HSPI module
 *******************************************************************************/
 os_timer_t timer2;
 
@@ -271,7 +264,7 @@ void hspi_master_readwrite_repeat(void)
 /******************************************************************************
  * FunctionName : spi_slave_isr_handler
  * Description  : SPI interrupt function, SPI HSPI and I2S interrupt can trig this function
- 			   some basic operation like clear isr flag has been done, 
+ 			   some basic operation like clear isr flag has been done,
  			   and it is availible	for adding user coder in the funtion
  * Parameters  : void *para- function parameter address, which has been registered in function spi_slave_init
 *******************************************************************************/
@@ -293,7 +286,7 @@ os_event_t * spiQueue;
 #define DATA_ERROR 6
 #define STATUS_R_IN_RD 7
 //init the two intr line of slave
-//gpio0: wr_ready ,and  
+//gpio0: wr_ready ,and
 //gpio2: rd_ready , controlled by slave
 void ICACHE_FLASH_ATTR
     gpio_init()
@@ -315,32 +308,32 @@ void spi_slave_isr_handler(void *para)
     	static uint8 state =0;
 	uint32 recv_data,send_data;
 
-	if(READ_PERI_REG(0x3ff00020)&BIT4){		
+	if(READ_PERI_REG(0x3ff00020)&BIT4){
         //following 3 lines is to clear isr signal
         	CLEAR_PERI_REG_MASK(SPI_SLAVE(SPI), 0x3ff);
     	}else if(READ_PERI_REG(0x3ff00020)&BIT7){ //bit7 is for hspi isr,
         	regvalue=READ_PERI_REG(SPI_SLAVE(HSPI));
-         	CLEAR_PERI_REG_MASK(SPI_SLAVE(HSPI),  
+         	CLEAR_PERI_REG_MASK(SPI_SLAVE(HSPI),
 								SPI_TRANS_DONE_EN|
 								SPI_SLV_WR_STA_DONE_EN|
 								SPI_SLV_RD_STA_DONE_EN|
 								SPI_SLV_WR_BUF_DONE_EN|
 								SPI_SLV_RD_BUF_DONE_EN);
         	SET_PERI_REG_MASK(SPI_SLAVE(HSPI), SPI_SYNC_RESET);
-        	CLEAR_PERI_REG_MASK(SPI_SLAVE(HSPI),  
+        	CLEAR_PERI_REG_MASK(SPI_SLAVE(HSPI),
 								SPI_TRANS_DONE|
 								SPI_SLV_WR_STA_DONE|
 								SPI_SLV_RD_STA_DONE|
 								SPI_SLV_WR_BUF_DONE|
-								SPI_SLV_RD_BUF_DONE); 
-		SET_PERI_REG_MASK(SPI_SLAVE(HSPI),  
+								SPI_SLV_RD_BUF_DONE);
+		SET_PERI_REG_MASK(SPI_SLAVE(HSPI),
 								SPI_TRANS_DONE_EN|
 								SPI_SLV_WR_STA_DONE_EN|
 								SPI_SLV_RD_STA_DONE_EN|
 								SPI_SLV_WR_BUF_DONE_EN|
 								SPI_SLV_RD_BUF_DONE_EN);
 
-		if(regvalue&SPI_SLV_WR_BUF_DONE){ 
+		if(regvalue&SPI_SLV_WR_BUF_DONE){
             		GPIO_OUTPUT_SET(0, 0);
             		idx=0;
             		while(idx<8){
@@ -361,7 +354,7 @@ void spi_slave_isr_handler(void *para)
 			//system_os_post(USER_TASK_PRIO_1,WR_RD,regvalue);
 
         	}
-    
+
     }else if(READ_PERI_REG(0x3ff00020)&BIT9){ //bit7 is for i2s isr,
 
     }
@@ -413,19 +406,19 @@ void ICACHE_FLASH_ATTR
 		break;
 	case STATUS_W:
 		os_printf("SW ERR,Reg:%08x\n",e->par);
-		break;	
+		break;
 	case TR_DONE_ALONE:
 		os_printf("TD ALO ERR,Reg:%08x\n",e->par);
-		break;	
+		break;
 	case WR_RD:
 		os_printf("WR&RD ERR,Reg:%08x\n",e->par);
-		break;	
+		break;
 	case DATA_ERROR:
 		os_printf("Data ERR,Reg:%08x\n",e->par);
 		break;
 	case STATUS_R_IN_RD :
 		os_printf("SR ERR in RDPR,Reg:%08x\n",e->par);
-		break;	
+		break;
         default:
             break;
     }
@@ -453,12 +446,10 @@ void ICACHE_FLASH_ATTR
     os_printf("spi miso init\n\r");
     set_miso_data();
 #endif
-    
+
     //os_timer_disarm(&spi_timer_test);
     //os_timer_setfn(&spi_timer_test, (os_timer_func_t *)set_miso_data, NULL);//wjl
     //os_timer_arm(&spi_timer_test,50,1);
 }
 
 #endif
-
-
--- robot/espressif/app/include/driver/spi.h
+++ robot/espressif/app/include/driver/spi.h
@@ -12,19 +12,50 @@
 #define IODATA_START_ADDR                 BIT0
 #define SPI_BUFF_BYTE_NUM                    32
 
-/*SPI number define*/
-#define SPI 			0
-#define HSPI			1
+// SPI number define
+typedef enum {
+  SPI  = 0, ///< Shared with program flash
+  HSPI = 1, ///< Independent SPI bus
+} SPIBus;
 
-void cache_flush(void);
-//spi master init funtion
-void spi_master_init(uint8 spi_no);
+/** Initalize the specified SPI peripheral for master mode
+ * @param spi_no Which SPI bus to initalize
+ * @param frequency SPI clock frequency to configure for in HZ
+ * @return The frequency actually selected
+ */
+uint32 spi_master_init(const SPIBus spi_no, uint32 frequency);
 
-//lcd drive function
-void spi_lcd_9bit_write(uint8 spi_no,uint8 high_bit,uint8 low_8bit);
-//use spi send 8bit data
-void spi_mast_byte_write(uint8 spi_no,uint8 data);
+/** Setup an SPI transaction to be followed by data
+ * @param spi_no Which SPI bus to use
+ * @param cmd_bits The number of bits of command data @range 0-16
+ * @param cmd_data Command data, only the lower cmd_bits of data will be used
+ * @param addr_bits The number of bits of address data @rante 0-32
+ * @param addr_data Address data, only the lower addr_bits will be used.
+ * @param mosi_bits The number of bits of master out / slave in data to transmit @range 0-512
+ * @param miso_bits The number of bits of master in / slave out data to receive. @range 0-512
+ * @param dummy_bits The number of dummy bits (extra clock cycles) to introduce. If miso_bits is > 0 then the dummy bits
+ *                   will be between the mosi_data and the miso_data. Otherwise it will be between the address data and
+ *                   the mosi_data. @range 0-255
+ */
+void spi_mast_start_transaction(const SPIBus spi_no, const uint8 cmd_bits, const uint16 cmd_data, \
+                                const uint8 addr_bits, const uint32 addr_data, \
+                                const uint16 mosi_bits, const uint16 miso_bits, const uint16 dummy_bits);
 
+/** Queue a 32 bit word for transmission during the mosi phase of an SPI transaction
+ * @param spi_no Which SPI bus to use
+ * @param data A 32 bit words to transmit.
+ * @return true if the word was added to the FIFO. False if there was no room
+ */
+bool spi_mast_write(const SPIBus spi_no, const uint32 data);
+
+/** Read a received 32 bit word from the FIFO
+ * @param spi_no Which SPI bus to use
+ * @return The read word. @note if the FIFO is empty 0 will be returned.
+ */
+uint32 spi_mast_read(const SPIBus spi_no);
+
+//esp8266 slave mode initial
+void spi_slave_init(uint8 spi_no,uint8 data_len);
 //transmit data to esp8266 slave buffer,which needs 16bit transmission ,
 //first byte is master command 0x04, second byte is master data
 void spi_byte_write_espslave(uint8 spi_no,uint8 data);
@@ -32,19 +63,8 @@ void spi_byte_write_espslave(uint8 spi_no,uint8 data);
 //first byte is master command 0x06, second byte is to read slave data
 void spi_byte_read_espslave(uint8 spi_no,uint8 *data);
 
- //esp8266 slave mode initial
-void spi_slave_init(uint8 spi_no,uint8 data_len);
-  //esp8266 slave isr handle funtion,tiggered when any transmission is finished.
-  //the function is registered in spi_slave_init.
-void spi_slave_isr_handler(void *para); 
-
-
-//hspi test function, used to test esp8266 spi slave
-void hspi_master_readwrite_repeat(void);
-
-
-void spi_test_init(void);
-
+//esp8266 slave isr handle funtion,tiggered when any transmission is finished.
+//the function is registered in spi_slave_init.
+void spi_slave_isr_handler(void *para);
 
 #endif
-
