ff3aa976347c10b590abf1d7fa2d0db3f5c4ff08
Daniel Casner <daniel@anki.com>
2015-06-29 10:23:24 -0700

    @dstulic Doesn't like ponies

--- robot/espressif/burn.sh
+++ robot/espressif/burn.sh
@@ -2,6 +2,4 @@
 
 ESPTOOL=../../../coretech-external/espressif/esptool/esptool.py
 
-
-
-python $ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/eagle.flash.bin 0x40000 bin/eagle.irom0text.bin
+python2 $ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/eagle.flash.bin 0x40000 bin/eagle.irom0text.bin
