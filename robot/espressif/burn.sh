02189068ba5da903d0de68c3528777dbeca5b343
Daniel Casner <daniel@anki.com>
2015-06-29 11:46:02 -0700

    Make burn.sh deal with more different environments. It now uses the CORETECH_EXTERNAL_DIR environmental variable and attempts to detect if it is in a python2/3 environment.

--- robot/espressif/burn.sh
+++ robot/espressif/burn.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
-ESPTOOL=$CORETECH_EXTERNAL_DIR/espressif/esptool/esptool.py
+if [[ -d $CORETECH_EXTERNAL_DIR ]]; then
+  ESPTOOL=$CORETECH_EXTERNAL_DIR/espressif/esptool/esptool.py
+else
+  echo "You must have CORETECH_EXTERNAL_DIR defined" 1>&2
+  exit 1
+fi
 
-python $ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/eagle.flash.bin 0x40000 bin/eagle.irom0text.bin
+if which python3; then
+  PYTHON=python2
+else
+  PYTHON=python
+fi
+
+$PYTHON $ESPTOOL --port $1 --baud 230400 write_flash 0x00000 bin/eagle.flash.bin 0x40000 bin/eagle.irom0text.bin
