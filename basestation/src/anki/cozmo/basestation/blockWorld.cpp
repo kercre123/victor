ab14e8c57dc4189bc3f1e6b1eace2a45e5ed1ed3
Andrew Stein <astein@neocitrus.com>
2015-07-02 15:48:52 -0700

    Adding LightCube4 to BlockWorld

--- basestation/src/anki/cozmo/basestation/blockWorld.cpp
+++ basestation/src/anki/cozmo/basestation/blockWorld.cpp
@@ -148,7 +148,7 @@ namespace Anki
       _objectLibrary[ObjectFamily::ACTIVE_BLOCKS].AddObject(new ActiveCube(Block::Type::LIGHTCUBE1));
       _objectLibrary[ObjectFamily::ACTIVE_BLOCKS].AddObject(new ActiveCube(Block::Type::LIGHTCUBE2));
       _objectLibrary[ObjectFamily::ACTIVE_BLOCKS].AddObject(new ActiveCube(Block::Type::LIGHTCUBE3));
-      //_objectLibrary[ObjectFamily::ACTIVE_BLOCKS].AddObject(new ActiveCube(Block::Type::LIGHTCUBE4));
+      _objectLibrary[ObjectFamily::ACTIVE_BLOCKS].AddObject(new ActiveCube(Block::Type::LIGHTCUBE4));
 
       
       //////////////////////////////////////////////////////////////////////////
