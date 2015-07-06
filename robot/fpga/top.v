e97dc6199bcd358d30c1efe9a2d45dd2790828a2
Daniel Casner <daniel@anki.com>
2015-07-06 16:37:33 -0700

    Checkpoint.

--- robot/fpga/top.v
+++ robot/fpga/top.v
@@ -5,37 +5,46 @@
 
 module top (
   // Pins
-  inout wire sio,  ///< SPI data in and data out in SIO mode
-  input wire sck,  ///< SPI clock
-  input wire scsn, ///< SPI chip select, active low
-  input wire xclk, ///< Camera clock input (12MHz?)
-  input wire pclk, ///< Camera pixel clock (>12MHz)
-  input wire [5:0] camD; ///< Camera pixel data
+  //inout  wire sio,  ///< SPI data in and data out in SIO mode
+  input  wire mosi; ///< Master out slave in
+  output reg miso; ///< Master in slave out
+  input  wire sck,  ///< SPI clock
+  input  wire scsn, ///< SPI chip select, active low
+  input  wire xclk, ///< Camera clock input (12MHz?)
+  input  wire pclk, ///< Camera pixel clock (>12MHz)
+  input  wire [5:0] camD ///< Camera pixel data
   );
 
 //////////////// SPI Testing /////////////////////
 
-  reg inNOut; ///< SIO direction is in / not out
+  reg  inNOut; ///< SIO direction is in / not out
+  //reg  miso;   ///< Master in slave out data register for when SIO is an output
+  //wire mosi;   ///< Master out slave in mirror of SIO when it's an input
+
   reg [31:0] testFifo;
 
+  //assign sio  = (inNOut) ? 1'bz : miso; // Assign output if outputing, otherwise tristate SIO
+  //assign mosi = (inNOut) ? sio  : 1'bx; // Assign input if inputting, otherwise tristate internal logic.
+
   initial begin
-    sio <= 1'bz;
+    miso   <= 1'b0;
     inNOut <= 1'b1;
     testFifo <= 32'h00000000;
   end
 
   always @(posedge scsn) begin
-    sio    <= 1'bz; // Make SIO an input
     inNOut <= ! inNOut; // Reverse direction for next transaction
   end
 
-  always @(posedge sclk and scsn == 1'b0) begin
-    if (inNOut == 1'b1) begin // Receiving data
-      testFifo <= {testFifo[30:0], sio};
-    end
-    else begin // Transmistting data
-      sio <= testFifo[31];
-      testFifo <= {testFifo[30:0], 0};
+  always @(posedge sck) begin
+    if (scsn == 1'b0) begin
+      if (inNOut == 1'b1) begin // Receiving data
+        testFifo <= {testFifo[30:0], mosi};
+      end
+      else begin // Transmistting data
+        miso <= testFifo[31];
+        testFifo <= {testFifo[30:0], 0};
+      end
     end
   end
 
