41cbeb0cb23bf4fbc7181ce1d0c8e452bb4c5805
Adam Ruja <adam.ruja@anki.com>
2015-07-08 19:12:36 -0700

    wip

---
+++ unity/Cozmo/Assets/Scripts/Robot/CozmoEmotionMachine.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public struct CozmoAnimation
+{
+	public string animName;
+	public CozmoEmotionManager.EmotionFlag emotionType;
+	public CozmoEmotionManager.EmotionIntensity emotionIntensity;
+	
+}
+
+[Serializable]
+public struct CozmoEmotionState
+{
+	public string stateName;
+	public CozmoEmotionManager.EmotionFlag emotionType;
+	public CozmoEmotionManager.EmotionIntensity emotionIntensity;
+	public List<CozmoEmotionManager.EmotionFlag> incrementEmotions;
+	public List<CozmoEmotionManager.EmotionFlag> decrementEmotions;
+	
+}
+
+public class CozmoEmotionMachine : MonoBehaviour {
+
+	public List<CozmoEmotionState> emotionStates;
+
+	// initailization
+	public void StartMachine()
+	{
+		CozmoEmotionManager.instance.RegisterMachine(this);
+		InitializeMachine();
+	}
+
+	public virtual void InitializeMachine() {}
+
+	public virtual void UpdateMachine(){}
+	
+	// clean-up
+	public void CloseMachine()
+	{
+		CozmoEmotionManager.instance.UnregisterMachine();
+	}
+
+	public virtual void CleanUp(){}
+	
+}
