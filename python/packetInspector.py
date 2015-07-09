47bfa77323f036500a5fc6ec7101d580955bba82
Daniel Casner <daniel@anki.com>
2015-07-09 11:29:35 -0700

    Adding more debuging tools.

--- python/packetInspector.py
+++ python/packetInspector.py
@@ -4,12 +4,13 @@ A little script with some useful functions for inspecting dumps of packets betwe
 """
 __author__ = "Daniel Casner <daniel@anki.com>"
 
-import sys, os, struct
+import sys, os, struct, serial
 
 UnreliablePacketPrefix = b"COZ\x03"
 ReliablePacketPrefix = b"RE\x01"
 ReliablePacketHeader = struct.Struct("3sBHHH")
-
+SerialPacketPrefix = b"\xbe\xef"
+SerialPacketHeader = struct.Struct("2sHBB")
 
 def loadPackets(file_path_name, has_unreliable_header=True):
     "Loads a binary dump of packets and returns the individual packets stripped of unreliable transport headers."
@@ -24,5 +25,15 @@ def parseReliableHeader(packet):
     try:
         return ReliablePacketHeader.unpack(packet[:ReliablePacketHeader.size])
     except:
-        return packet[:10] + b"..."
-    
+        return packet[:10] + b".."
+
+def captureSerialBytes(dev, length):
+    "Opens a serial port and captures bytes from it."
+    port = serial.Serial(dev, 5000000)
+    ret  = port.read(length)
+    port.close()
+    return ret
+
+def parseSerialPackets(capt):
+    "Split serial pacture into packets"
+    return capt.split(SerialPacketPrefix)
