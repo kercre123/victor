c95d9845ea7a6aec4d588a23bf6aec98b69ecd8c
Kevin Yoon <kevin@anki.com>
2015-07-20 16:01:54 -0700

    Added ADPCM encoder from Bryon. Using it to dole out encoded samples at animation play time. The engine can now just read in any mono, 24KHz, 16-bit signed lpcm wav file for playback on the robot.

---
+++ basestation/src/anki/cozmo/basestation/adpcm.cpp
@@ -0,0 +1,99 @@
+#include "adpcm.h"
+
+
+
+const static int8_t ima_index_table[] = {
+	-1, -1, -1, -1, 2, 4, 6, 8,
+	-1, -1, -1, -1, 2, 4, 6, 8
+};
+
+const static uint16_t stepsize_table[] = {
+	7, 8, 9, 10, 11, 12, 13, 14, 16, 17,
+	19, 21, 23, 25, 28, 31, 34, 37, 41, 45,
+	50, 55, 60, 66, 73, 80, 88, 97, 107, 118,
+	130, 143, 157, 173, 190, 209, 230, 253, 279, 307,
+	337, 371, 408, 449, 494, 544, 598, 658, 724, 796,
+	876, 963, 1060, 1166, 1282, 1411, 1552, 1707, 1878, 2066,
+	2272, 2499, 2749, 3024, 3327, 3660, 4026, 4428, 4871, 5358,
+	5894, 6484, 7132, 7845, 8630, 9493, 10442, 11487, 12635, 13899,
+	15289, 16818, 18500, 20350, 22385, 24623, 27086, 29794, 32767
+};
+
+static uint8_t next_nibble(int &index, int &valpred, int16_t delta) {
+	int step = stepsize_table[index];
+	int diff = (int)delta - valpred;
+	int code;
+
+	if (diff >= 0) {
+		code = 0;
+	} else {
+		code = 8;
+		diff = -diff;
+	}
+
+	int tempstep = step;
+
+	if (diff >= tempstep) {
+		code = (code | 4);
+		diff = diff - tempstep;
+	}
+
+	tempstep = tempstep >> 1;
+
+	if (diff >= tempstep) {
+		code = (code | 2);
+		diff = diff - tempstep;
+	}
+
+	tempstep = tempstep >> 1;
+
+	if (diff >= tempstep) {
+		code = (code | 1);
+	}
+
+	int diffq = step >> 3;
+	
+	if (code & 4) {
+		diffq += step;
+	} else if (code & 2) {
+		diffq += step >> 1;
+	} else if (code & 1) {
+		diffq += step >> 2;
+	}
+
+	bool sign = code & 8;
+
+	if (sign) {
+		valpred -= diffq;
+	} else {
+		valpred += diffq;
+	}
+
+	if (valpred > 32767) {
+		valpred = 32767;
+	} else if (valpred < -32768) {
+		valpred = -32768;
+	}
+	
+	index += ima_index_table[code];
+	
+	if (index < 0) {
+		index = 0;
+	} else if (index > 88) {
+		index = 88;
+	}
+  
+  return code;
+}
+
+// First block should starts with index + predictor = 0
+// Resulting values are the state for the next block
+// Length is in input samples (800)
+
+void encodeADPCM(int &index, int &predictor, int16_t *in, uint8_t *out, int length) {
+	for(;length > 0; length -= 2) {
+		uint8_t lo = next_nibble(index, predictor, *(in++)) & 0xF;
+		uint8_t hi = next_nibble(index, predictor, *(in++)) & 0xF;
+		*(out++) = (hi << 4) | (lo);
+	}
+}
