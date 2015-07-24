325196afb015f6830623e483a16f34b56ffe61f7
Donovan Drane <donovan.drane@anki.com>
2015-07-24 12:22:20 -0700

    RobotAnimTester

---
+++ unity/Cozmo/Assets/Scripts/Robot/RobotAnimTester.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using U2G = Anki.Cozmo.U2G;
+
+/// <summary>
+/// simple component for directly previewing animations on cozmo
+/// </summary>
+public class RobotAnimTester : MonoBehaviour {
+	
+	[SerializeField] ComboBox combo_selectAnim = null;
+	[SerializeField] Toggle toggle_loop = null;
+
+	private string[] animNames = null;
+
+	private U2G.PlayAnimation PlayAnimationMessage = new U2G.PlayAnimation();
+
+	void Awake() {
+		if(RobotEngineManager.instance == null) {
+			gameObject.SetActive(false);
+			return;
+		}
+
+		animNames = RobotEngineManager.instance.robotAnimationNames.ToArray();
+		combo_selectAnim.AddItems(animNames);
+	}
+
+	public void PlayAnimation () {
+		if(RobotEngineManager.instance == null) return;
+		if(RobotEngineManager.instance.current == null) return;
+		if(animNames == null) return;
+		if(combo_selectAnim == null) return;
+		if(toggle_loop == null) return;
+
+		PlayAnimationMessage.animationName = animNames[combo_selectAnim.SelectedIndex];
+		PlayAnimationMessage.numLoops = (uint)(toggle_loop.isOn ? 0 : 1);
+		PlayAnimationMessage.robotID = RobotEngineManager.instance.current.ID;
+		RobotEngineManager.instance.Message.PlayAnimation = PlayAnimationMessage;
+		RobotEngineManager.instance.SendMessage();
+	}
+}
