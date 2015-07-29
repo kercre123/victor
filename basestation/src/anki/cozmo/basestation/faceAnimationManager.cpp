3f37ab1f863766a31c1489f45a5107cfacd1613d
Andrew Stein <astein@neocitrus.com>
2015-07-29 13:14:55 -0700

    Missing const

--- basestation/src/anki/cozmo/basestation/faceAnimationManager.cpp
+++ basestation/src/anki/cozmo/basestation/faceAnimationManager.cpp
@@ -291,7 +291,7 @@ namespace Cozmo {
     u32 currColorCount = 0;
     
     for(s32 i=0; i<nrows; i++) {
-      u8* img_i = (u8*)img.ptr(i);
+      const u8* img_i = img.ptr(i);
       for(s32 j=0; j<ncols; j++, ++pixCount) {
         u8 pixel = img_i[j];
     
