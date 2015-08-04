ca314d2894713b14728b3c06722fea84fcdd3d01
Andrew Stein <astein@neocitrus.com>
2015-08-03 18:36:19 -0700

    Adding and just beginning to flesh out EmotionManager class

---
+++ basestation/include/anki/cozmo/basestation/emotionManager.h
@@ -0,0 +1,103 @@
+/**
+ * File: emotionManager.h
+ *
+ * Author: Andrew Stein
+ * Date:   8/3/15
+ *
+ *
+ * Description: Defines a class to manage Cozmo's emotional state / mood.
+ *
+ * Copyright: Anki, Inc. 2015
+ **/
+
+#ifndef ANKI_COZMO_EMOTION_MANAGER_H
+#define ANKI_COZMO_EMOTION_MANAGER_H
+
+#include "anki/common/types.h"
+#include "json/json.h"
+
+// TODO: This needs to be switched to some kind of _internal_ signals definition
+#include "messageEngineToGame.h"
+
+#include <array>
+
+namespace Anki {
+namespace Cozmo {
+  
+  // Forward declaration
+  class Robot;
+  
+  class EmotionManager
+  {
+  public:
+    
+    // The underlying value for emotions
+    using Value = f32;
+    
+    static const Value MAX_VALUE;
+    static const Value MIN_VALUE;
+    
+    enum Emotion {
+      ANXIOUS = 0,
+      SCARED,
+      SAD,
+      LONELY,
+      NUM_MOODS
+    };
+    
+    EmotionManager(Robot &robot, const Json::Value& value);
+    
+    // Get the value for a particular emotion
+    Value GetEmotion(Emotion emotion) const;
+    
+    // Directly adjust a particular emotion by a given value
+    // Clips to min/max values.
+    void AdjustEmotion(Emotion emotion, Value adjustment);
+    
+    // Set a particular emotion to a specified value
+    // Clips to min/max values.
+    void SetEmotion(Emotion emotion, Value newValue);
+    
+    // Update the current emotions as a function of time
+    Result Update(const float currentTime_sec);
+    
+  private:
+    
+    // Event handlers
+    void HandleRobotObservedFace(const ExternalInterface::RobotObservedObject& msg);
+    
+    Robot& _robot;
+    Json::Value _config;
+    
+    std::array<Value, Emotion::NUM_MOODS>  _emoState;
+    
+    // Functinon prototype for how emotion values change over time, given current
+    // value and current time in seconds.
+    using EmotionEvolutionFunction = std::function<Value(Value,f32)>;
+    std::map<Emotion, EmotionEvolutionFunction> _evolutionFunctions;
+    
+    void SetEvolutionFunction(Emotion emotion, EmotionEvolutionFunction fcn);
+    
+  }; // class EmotionManager
+  
+  
+  //
+  // Inlined Implementations
+  //
+  
+  inline EmotionManager::Value EmotionManager::GetEmotion(Emotion emotion) const {
+    return _emoState[emotion];
+  }
+  
+  inline void EmotionManager::AdjustEmotion(Emotion emotion, Value adjustment) {
+    SetEmotion(emotion, _emoState[emotion] + adjustment); // pass through to handle clipping
+  }
+  
+  inline void EmotionManager::SetEmotion(Emotion emotion, Value newValue) {
+    _emoState[emotion] = std::max(MIN_VALUE, std::min(MAX_VALUE, newValue));
+  }
+  
+} // namespace Cozmo
+} // namespace Anki
+
+#endif // ANKI_COZMO_EMOTION_MANAGER_H
