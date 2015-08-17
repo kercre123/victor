b1ff2d90386fca0e5d8ce433248ca661a2707953
Pohung Chen <pohung@anki.com>
2015-08-17 13:42:54 -0700

    hypnosis controller

---
+++ unity/Cozmo/Assets/Scripts/Games/HypnosisController.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HypnosisController : GameController {
+
+  protected override void OnEnable() {
+    base.OnEnable();
+  }
+
+  protected override void OnDisable() {
+    base.OnDisable();
+  }
+
+  protected override void Enter_BUILDING() {
+    base.Enter_BUILDING();
+  }
+
+  protected override void Exit_BUILDING() {
+    base.Exit_BUILDING();
+  }
+
+  protected override void Update_BUILDING() {
+    base.Update_BUILDING();
+  }
+
+  protected override void Enter_PRE_GAME() {
+    base.Enter_PRE_GAME();
+  }
+
+  protected override void Update_PRE_GAME() {
+    base.Update_PRE_GAME();
+  }
+
+  protected override void Exit_PRE_GAME() {
+    base.Exit_PRE_GAME();
+  }
+
+  protected override void Enter_PLAYING() {
+    base.Enter_PLAYING();
+  }
+
+  protected override void Update_PLAYING() {
+    base.Update_PLAYING();
+  }
+
+  protected override void Exit_PLAYING(bool overrideStars = false) {
+    base.Exit_PLAYING();
+  }
+
+  protected override void Enter_RESULTS() {
+    base.Enter_RESULTS();
+  }
+
+  protected override void Update_RESULTS() {
+    base.Update_RESULTS();
+  }
+
+  protected override void Exit_RESULTS() {
+    base.Exit_RESULTS();
+  }
+
+  protected override bool IsPreGameCompleted() {
+    return base.IsPreGameCompleted();
+  }
+
+  protected override bool IsGameReady() {
+    return base.IsGameReady();
+  }
+
+  protected override bool IsGameOver() {
+    return base.IsGameOver();
+  }
+
+  protected override void RefreshHUD() {
+    base.RefreshHUD();
+  }
+
+}
