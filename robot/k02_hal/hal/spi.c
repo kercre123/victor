029787fc1ef535fef164e80c4de69646b4c81bf9
Bryon Vandiver <unicdk@gmail.com>
2015-08-17 12:03:52 -0700

    Checkpoint

--- robot/k02_hal/hal/spi.c
+++ robot/k02_hal/hal/spi.c
@@ -6,9 +6,6 @@
 
 #include "spi.h"
 
-#define TXCTR ((SPI0_SR & SPI_SR_TXCTR_MASK) >> SPI_SR_TXCTR_SHIFT)
-#define RXCTR ((SPI0_SR & SPI_SR_RXCTR_MASK) >> SPI_SR_RXCTR_SHIFT)
-
 void spi_init(void) {
   // Turn on power to DMA, PORTC and SPI0
   SIM_SCGC6 |= SIM_SCGC6_SPI0_MASK | SIM_SCGC6_DMAMUX_MASK;
@@ -24,28 +21,29 @@ void spi_init(void) {
   // Configure perf
   SPI0_SR = SPI0_SR;  // Clear all status flags
   SPI0_MCR = SPI_MCR_MSTR_MASK | SPI_MCR_DCONF(0) | SPI_MCR_SMPL_PT(0) | SPI_MCR_CLR_TXF_MASK | SPI_MCR_CLR_RXF_MASK;
-  SPI0_CTAR0 = SPI_CTAR_LSBFE_MASK | SPI_CTAR_BR(7) | SPI_CTAR_FMSZ(15);
+  SPI0_CTAR0 = SPI_CTAR_LSBFE_MASK | SPI_CTAR_BR(7) | SPI_CTAR_FMSZ(15) /*| SPI_CTAR_CPOL_MASK | SPI_CTAR_CPHA_MASK*/;
 
   const int size = 256;
   uint16_t spi_buff[size]; // 512 bytes
   int tx_idx = 0, rx_idx = 0;
-  
-  for(;;) {
-    if (SPI0_SR & SPI_SR_TFFF_MASK)
-    {
-      SPI0_SR = SPI_SR_TFFF_MASK;
-      SPI0_PUSHR = SPI_PUSHR_PCS((tx_idx & 1) ? 2 : 0) | spi_buff[tx_idx];
-      tx_idx = (tx_idx + 1) % size;
-    }
 
-    if (SPI0_SR & SPI_SR_RFDF_MASK)
+  for(;;) {
+    //while (SPI0_SR & SPI_SR_RFDF_MASK)
     {
-      SPI0_SR = SPI_SR_RFDF_MASK;
       spi_buff[rx_idx] = SPI0_POPR;
       rx_idx = (rx_idx + 1) % size;
+
+      SPI0_SR = SPI_SR_RFDF_MASK;
+    }
+
+    //while (SPI0_SR & SPI_SR_TFFF_MASK)
+    {
+      SPI0_PUSHR = SPI_PUSHR_CONT_MASK | SPI_PUSHR_PCS((tx_idx & 1) ? 2 : 0) | 0x5500; //spi_buff[tx_idx];
+      tx_idx = (tx_idx + 1) % size;
+
+      SPI0_SR = SPI_SR_TFFF_MASK;
     }
     
-    for (int i = 0; i < 4; i++)
-    BOARD_I2C_DELAY;
+    for (int i = 0; i < 4; i++) BOARD_I2C_DELAY ;
   }
 }
