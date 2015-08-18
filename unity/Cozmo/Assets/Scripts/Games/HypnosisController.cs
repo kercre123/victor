bcc0598eb6205b700472fc64b9b8bd8f3cdebfe6
Pohung Chen <pohung@anki.com>
2015-08-18 11:05:39 -0700

    hypnosis game states, also block color turn on!

--- unity/Cozmo/Assets/Scripts/Games/HypnosisController.cs
+++ unity/Cozmo/Assets/Scripts/Games/HypnosisController.cs
@@ -1,16 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class HypnosisController : GameController {
 
   bool gameOver = false;
 
+  public enum HypnosisState {
+    WANDER,
+    SEARCH,
+    TRANCE
+  }
+
+  private HypnosisState currentState = HypnosisState.WANDER;
+  private int mostRecentTappedID = -1;
+  private float lastInTranceTime = 0.0f;
+  private ObservedObject tranceTarget = null;
+
   protected override void OnEnable() {
     base.OnEnable();
+    ActiveBlock.TappedAction += BlockTapped;
   }
 
   protected override void OnDisable() {
     base.OnDisable();
+    ActiveBlock.TappedAction -= BlockTapped;
   }
 
   protected override void Enter_BUILDING() {
@@ -39,11 +53,27 @@ public class HypnosisController : GameController {
 
   protected override void Enter_PLAYING() {
     base.Enter_PLAYING();
+
   }
 
   protected override void Update_PLAYING() {
     base.Update_PLAYING();
-    robot.DriveWheels(10.0f, 0.0f);
+    foreach (KeyValuePair<int, ActiveBlock> activeBlock in robot.activeBlocks) {
+      for (int j = 0; j < activeBlock.Value.lights.Length; ++j) {
+        activeBlock.Value.lights[j].onColor = CozmoPalette.ColorToUInt(Color.green);
+      }
+    }
+    switch (currentState) {
+    case HypnosisState.WANDER:
+      RobotWander();
+      break;
+    case HypnosisState.SEARCH:
+      RobotSearch();
+      break;
+    case HypnosisState.TRANCE:
+      RobotTrance();
+      break;
+    }
   }
 
   protected override void Exit_PLAYING(bool overrideStars = false) {
@@ -76,6 +106,47 @@ public class HypnosisController : GameController {
 
   protected override void RefreshHUD() {
     base.RefreshHUD();
+  }
+
+  private void BlockTapped(int blockID, int numTapped) {
+    mostRecentTappedID = blockID;
+  }
+
+  private void RobotWander() {
+    if (mostRecentTappedID != -1) {
+      currentState = HypnosisState.SEARCH;
+    }
+  }
+
+  private void RobotSearch() {
+    robot.DriveWheels(15.0f, 0.0f);
+    bool tappedBlockFound = false;
+
+    for (int i = 0; i < robot.observedObjects.Count; ++i) {
+      if (robot.observedObjects[i].ID == mostRecentTappedID) {
+        tappedBlockFound = true;
+        tranceTarget = robot.observedObjects[i];
+      }
+    }
+
+    if (tappedBlockFound) {
+      lastInTranceTime = Time.time;
+      currentState = HypnosisState.TRANCE;
+    }
+  }
+
+  private void RobotTrance() {
+
+    // follow observed object
+    if (tranceTarget != null) {
+      robot.GotoObject(tranceTarget, 80.0f);
+    }
+
+    if (Time.time - lastInTranceTime > 10.0f) {
+      currentState = HypnosisState.WANDER;
+      mostRecentTappedID = -1;
+      tranceTarget = null;
+    }
   }
 
 }
