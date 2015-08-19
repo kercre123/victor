12c4ab99e239fd7825502e1491a58d9ff5cc35dd
Kevin Yoon <kevin@anki.com>
2015-08-19 15:01:58 -0700

    Renamed some cozmo sim test files

--- simulator/controllers/webotsCtrlBuildServerTest/webotsCtrlBuildServerTest.cpp
+++ simulator/controllers/webotsCtrlBuildServerTest/webotsCtrlBuildServerTest.cpp
@@ -1,150 +1,61 @@
 /**
-* File: webotsCtrlBuildServerTest.cpp
+* File: main.cpp
 *
-* Author: damjan stulic
-* Created: 8/12/15
+* Author: Kevin Yoon
+* Created: 8/18/2015
 *
-* Description: 
+* Description: Main for running build server tests
 *
 * Copyright: Anki, inc. 2015
 *
 */
 
-#include "anki/cozmo/simulator/game/cozmoSimTestController.h"
+#include "anki/cozmo/shared/cozmoConfig.h"
+#include "anki/cozmo/shared/cozmoEngineConfig.h"
+#include "anki/cozmo/shared/cozmoTypes.h"
+
+#include "util/logging/logging.h"
+#include "util/logging/printfLoggerProvider.h"
+#include "util/ptree/ptreeTools.h"
 #include <stdio.h>
 #include <string.h>
+#include <fstream>
+
+#include "anki/cozmo/simulator/game/cozmoSimTestController.h"
 
 
-namespace Anki {
-namespace Cozmo {
-  
-  enum class TestState {
-    WaitingForInit,
-    ReadyForNextCommand,
-    ExecutingTestAnimation,
-    TestDone
-  };
-  
-  // ============ Test class declaration ============
-  class CST_Animations : public CozmoSimTestController {
-  public:
-    CST_Animations();
-    
-  private:
-    const u32 MIN_NUM_ANIMS_REQUIRED = 10;
-    const u32 NUM_ANIMS_TO_PLAY = 2;
-    
-    virtual s32 UpdateInternal() override;
-    
-    virtual void HandleRobotCompletedAction(const ExternalInterface::RobotCompletedAction& msg) override;
-    virtual void HandleAnimationAvailable(ExternalInterface::AnimationAvailable const& msg) override;
-    
-    TestState _testState = TestState::WaitingForInit;
+using namespace Anki;
+using namespace Anki::Cozmo;
 
-    std::vector<std::string> _availableAnims;
-    s32 _currentAnimIdx;
-    
-    int _result;
-  };
+int main(int argc, char **argv)
+{
+  Anki::Util::PrintfLoggerProvider loggerProvider;
+  loggerProvider.SetMinLogLevel(0);
+  Anki::Util::gLoggerProvider = &loggerProvider;
   
-  // Register class with factory
-  REGISTER_COZMO_SIM_TEST_CLASS(CST_Animations);
+  // Get the last position of '/'
+  std::string aux(argv[0]);
+  size_t pos = aux.rfind('/');
   
-  
-  // =========== Test class implementation ===========
-  
-  CST_Animations::CST_Animations() :
-  CozmoSimTestController()
-  {
-    _result = 0;
-    _testState = TestState::WaitingForInit;
-    _currentAnimIdx = -1;
-  }
-  
-
-  s32 CST_Animations::UpdateInternal()
-  {
-    switch (_testState) {
-
-      case TestState::WaitingForInit:
-        if (GetSupervisor()->getTime() > 2) {   // TODO: Better way to wait for ready. Ready message to game?
-          // Check that there were atleast a few animations defined
-          // TODO: Should animFailedToLoad be an EngineToGame message? Or do we test this is EngineTestController (which doesn't exist yet) that can listen to events.
-          
-          if (_availableAnims.size() < MIN_NUM_ANIMS_REQUIRED) {
-            PRINT_NAMED_INFO("BST.UpdateInternal.InsufficientAnimsLoaded", "%lu anims loaded", _availableAnims.size());
-            _result = -1;
-            _testState = TestState::TestDone;
-          } else {
-            _testState = TestState::ReadyForNextCommand;
-          }
-        }
-        break;
-
-      case TestState::ReadyForNextCommand:
-      {
-        if (_currentAnimIdx == NUM_ANIMS_TO_PLAY - 1) {
-          _testState = TestState::TestDone;
-          PRINT_NAMED_INFO("TestController.Update", "all tests completed (Result = %d)", _result);
-          break;
-        }
-        ++_currentAnimIdx;
-        SendAnimation(_availableAnims[_currentAnimIdx].c_str(), 1);
-        _testState = TestState::ExecutingTestAnimation;
-      }
-        break;
-
-      case TestState::ExecutingTestAnimation:
-        break;
-
-      case TestState::TestDone:
-        //QuitWebots(_result);
-        break;
-    }
-    
-    
-    return _result;
-  }
+  // Get the path and the name
+  std::string path = aux.substr(0,pos+1);
+  std::string resourcePath = path;
+  std::string filesPath = path + "temp";
+  std::string cachePath = path + "temp";
+  std::string externalPath = path + "temp";
+  Data::DataPlatform dataPlatform(filesPath, cachePath, externalPath, resourcePath);
 
   
+  // Create specified test controller
+  auto cstCtrl = CozmoSimTestFactory::getInstance()->Create("CST_Animations");
+  cstCtrl->SetDataPlatform(&dataPlatform);
+  cstCtrl->Init();
   
-  // ================ Message handler callbacks ==================
   
-  void CST_Animations::HandleRobotCompletedAction(const ExternalInterface::RobotCompletedAction& msg)
-  {
-    switch((RobotActionType)msg.actionType)
-    {
-      case RobotActionType::PLAY_ANIMATION:
-        printf("Robot %d finished playing animation %s. [Tag=%d]\n",
-               msg.robotID, msg.completionInfo.animName.c_str(), msg.idTag);
-        if ((_testState == TestState::ExecutingTestAnimation) &&
-            (_currentAnimIdx >= 0) &&
-            (msg.completionInfo.animName.compare(_availableAnims[_currentAnimIdx]) == 0)) {
-          if (msg.result == ActionResult::SUCCESS) {
-            _testState = TestState::ReadyForNextCommand;
-          } else {
-            _result = -1;
-            _testState = TestState::TestDone;
-          }
-        }
-        break;
-   
-      default:
-        break;
-    }
-   
-  }
-   
-  void CST_Animations::HandleAnimationAvailable(ExternalInterface::AnimationAvailable const& msg)
-  {
-    _availableAnims.push_back(msg.animName);
-    PRINT_NAMED_INFO("HandleAnimationAvailable", "Animation available: %s", msg.animName.c_str());
-  }
-   
-  // ============== End of message handlers =================
-
+  // Run update loop
+  while (cstCtrl->Update() == 0) {}
   
-
-} // end namespace Cozmo
-} // end namespace Anki
+  Anki::Util::gLoggerProvider = nullptr;
+  return 0;
+}
 
