67768dd000115cda9ccf09728de872d83270c046
Bryon Vandiver <unicdk@gmail.com>
2015-08-19 13:58:14 -0700

    Started porting over battery charge code

--- robot/syscon/hal/battery.cpp
+++ robot/syscon/hal/battery.cpp
@@ -15,17 +15,15 @@ const u32 V_SCALE       = 0x3ff; // 10 bit ADC
 const Fixed VEXT_SCALE  = TO_FIXED(2.0); // Cozmo 4.1 voltage divider
 const Fixed VBAT_SCALE  = TO_FIXED(4.0); // Cozmo 4.1 voltage divider
 
-/*
 const Fixed VBAT_CHGD_HI_THRESHOLD = TO_FIXED(4.05); // V
 const Fixed VBAT_CHGD_LO_THRESHOLD = TO_FIXED(3.70); // V
 const Fixed VBAT_EMPTY_THRESHOLD   = TO_FIXED(2.90); // V
-const Fixed VUSB_DETECT_THRESHOLD  = TO_FIXED(4.40); // V
+const Fixed VEXT_DETECT_THRESHOLD  = TO_FIXED(4.40); // V
 
 // Read battery dead state N times before we believe it is dead
 const u8 BATTERY_DEAD_CYCLES = 60;
 // Read charger contact state N times before we believe it changed
 const u8 CONTACT_DEBOUNCE_CYCLES = 30;
-*/
 
 // Are we currently on charge contacts?
 bool Battery::onContacts = false;
@@ -97,7 +95,6 @@ void Battery::powerOn()
 {
   // Let power drain out - 10ms is plenty long enough
   MicroWait(10000);
-  
   nrf_gpio_pin_clear(PIN_nVDDs_EN);
 }
 
@@ -110,34 +107,9 @@ void Battery::update()
   switch (m_pinIndex)
   {
     case ANALOG_V_BAT_SENSE:
-      g_dataToHead.VBat = calcResult(VBAT_SCALE);
-      startADCsample(ANALOG_V_EXT_SENSE);
-
-      // todo: LOGIC HERE
-      break ;
-    case ANALOG_V_EXT_SENSE:
-      g_dataToHead.VExt = calcResult(VEXT_SCALE);
-      startADCsample(ANALOG_V_BAT_SENSE);
-    
-      // todo: LOGIC HERE
-      break ;
-  }
-
-  /*
-  // Have a new result
-  switch (m_pinIndex)
-  {
-    case ANALOG_I_SENSE: // Battery current
-    {
-      g_dataToHead.IBat = FIXED_MUL(FIXED_DIV(TO_FIXED(NRF_ADC->RESULT * V_REFERNCE_MV * V_PRESCALE / V_SCALE), TO_FIXED(1000)), IBAT_SCALE);
-      startADCsample(ANALOG_V_BAT_SENSE);
-      break;
-    }
-    case ANALOG_V_BAT_SENSE: // Battery voltage
-    {
-      g_dataToHead.VBat = FIXED_MUL(FIXED_DIV(TO_FIXED(NRF_ADC->RESULT * V_REFERNCE_MV * V_PRESCALE / V_SCALE), TO_FIXED(1000)), VBAT_SCALE);
       static u8 debounceBattery = 0;
-      
+      g_dataToHead.VBat = calcResult(VBAT_SCALE);
+
       // Is battery dead AND we are not on the contacts
       if (!onContacts && g_dataToHead.VBat < VBAT_EMPTY_THRESHOLD)
       {
@@ -146,27 +118,25 @@ void Battery::update()
           debounceBattery++;
         } else {
           // Shut everything down
-          nrf_gpio_pin_clear(PIN_VBATs_EN);
-          nrf_gpio_pin_clear(PIN_VDDs_EN);
-          nrf_gpio_pin_clear(PIN_VDD_EN);
+          nrf_gpio_pin_set(PIN_nVDDs_EN);
+          nrf_gpio_pin_clear(PIN_PWR_EN);
         }
       } else {
         debounceBattery = 0;
       }
-      
-      startADCsample(ANALOG_V_USB_SENSE);
-      break;
-    }
-    case ANALOG_V_USB_SENSE:   // Charge contacts
-    {
-      g_dataToHead.Vusb = FIXED_MUL(FIXED_DIV(TO_FIXED(NRF_ADC->RESULT * V_REFERNCE_MV * V_PRESCALE / V_SCALE), TO_FIXED(1000)), VUSB_SCALE);
 
-      // Are we on external power?
-      bool onContacts = (g_dataToHead.Vusb > VUSB_DETECT_THRESHOLD);
+      startADCsample(ANALOG_V_EXT_SENSE);
+      break ;
+
+    case ANALOG_V_EXT_SENSE:
       static u8 debounceContacts = 0;
+      g_dataToHead.VExt = calcResult(VEXT_SCALE);
+   
+      // Are we on external power?
+      bool isOnCharger = (g_dataToHead.VExt > VEXT_DETECT_THRESHOLD);
 
       // If contact state hasn't changed, do nothing
-      if (onContacts == onContacts)
+      if (isOnCharger == onContacts)
       {
         debounceContacts = 0;   // Reset debounce time
         
@@ -178,26 +148,22 @@ void Battery::update()
       } else {
         debounceContacts = 0;   // Reset debounce time
        
-        Battery::onContacts = onContacts;
+        onContacts = isOnCharger;
         
         // If we are now on contacts, start charging
-        if (onContacts)
+        if (isOnCharger)
         {
-          // MUST disable VBATs (and thus head power) while charging, to protect battery
-          nrf_gpio_pin_clear(PIN_VBATs_EN);   // Off
-          nrf_gpio_pin_set(PIN_VDDs_EN);      // Off            
-          nrf_gpio_pin_set(PIN_VUSBs_EN);  // Enable charging
-          
+          nrf_gpio_pin_set(PIN_nVDDs_EN);   // Turn off motors
+          MicroWait(10000); // Wait 10ms before enabling charging
+          nrf_gpio_pin_set(PIN_CHARGE_EN);  // Enable charging
         // If we are now off contacts, stop charging and reboot
         } else {
-          nrf_gpio_pin_clear(PIN_VUSBs_EN);    // Disable charging
-          PowerOn();
+          nrf_gpio_pin_clear(PIN_CHARGE_EN);  // Disable charging
+          powerOn();
         }
       }
-      
-      startADCsample(ANALOG_I_SENSE);
-      break;
-    }
+
+      startADCsample(ANALOG_V_BAT_SENSE);
+      break ;
   }
-  */
 }
