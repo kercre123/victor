876cf1fabdf6051099311a001e40f138788a4307
Daniel Casner <daniel@danielcasner.org>
2015-08-25 14:16:05 -0700

    Update build script etc. for xtensa-lx106 tools in build-tools.

--- robot/espressif/build.sh
+++ robot/espressif/build.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
 
+export XTENSA_DIR=$PWD/../../tools/anki-util/tools/build-tools/tools/xtensa-lx106
+if [[ `uname` == "Darwin" ]]; then
+    #export PATH=$PWD/../../../esp-open-sdk/xtensa-lx106-elf/bin/:$PATH
+    export PATH=$XTENSA_DIR/osx/xtensa-lx106-elf/bin/:$PATH
+elif [[ `uname` == "Linux" ]]; then
+    export PATH=$XTENSA_DIR/linux/xtensa-lx106-elf/bin/:$PATH
+fi
+
 cd app
+
 clear
+
 make COMPILE=gcc BOOT=new APP=1 SPI_SPEED=20 SPI_MODE=QIO SPI_SIZE_MAP=3 && \
 make clean && \
 make COMPILE=gcc BOOT=new APP=2 SPI_SPEED=20 SPI_MODE=QIO SPI_SIZE_MAP=3 && \
