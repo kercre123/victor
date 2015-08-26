ee492beaa882341c93bb2a83a820e477fa5fb576
Bryon Vandiver <unicdk@gmail.com>
2015-08-26 16:01:14 -0700

    Checkpoint

--- robot/k02_hal/hal/spi.c
+++ robot/k02_hal/hal/spi.c
@@ -6,6 +6,8 @@
 
 #include "spi.h"
 
+void setup_dma(void);
+  
 void spi_init(void) {
   // Turn on power to DMA, PORTC and SPI0
   SIM_SCGC6 |= SIM_SCGC6_SPI0_MASK | SIM_SCGC6_DMAMUX_MASK;
@@ -13,16 +15,13 @@ void spi_init(void) {
   SIM_SCGC7 |= SIM_SCGC7_DMA_MASK;
 
   // Configure SPI pins
-  PORTC_PCR3 = PORT_PCR_MUX(2); // SPI0_PCS1
+  //PORTC_PCR3 = PORT_PCR_MUX(2); // SPI0_PCS1
   PORTC_PCR5 = PORT_PCR_MUX(2); // SPI0_SCK
   PORTC_PCR6 = PORT_PCR_MUX(2); // SPI0_SOUT
   PORTC_PCR7 = PORT_PCR_MUX(2); // SPI0_SIN
 
-  // Configure perf
-  SPI0_SR = SPI0_SR;  // Clear all status flags
-  SPI0_MCR = SPI_MCR_MSTR_MASK | SPI_MCR_DCONF(0) | SPI_MCR_SMPL_PT(0) | SPI_MCR_CLR_TXF_MASK | SPI_MCR_CLR_RXF_MASK;
-  SPI0_CTAR0 = SPI_CTAR_LSBFE_MASK | SPI_CTAR_BR(7) | SPI_CTAR_FMSZ(15) /*| SPI_CTAR_CPOL_MASK | SPI_CTAR_CPHA_MASK*/;
-
+  setup_dma();
+  
   const int size = 256;
   uint16_t spi_buff[size]; // 512 bytes
   int tx_idx = 0, rx_idx = 0;
@@ -38,7 +37,7 @@ void spi_init(void) {
 
     //while (SPI0_SR & SPI_SR_TFFF_MASK)
     {
-      SPI0_PUSHR = SPI_PUSHR_CONT_MASK | SPI_PUSHR_PCS((tx_idx & 1) ? 2 : 0) | 0x5500; //spi_buff[tx_idx];
+      SPI0_PUSHR = 0x5500; //spi_buff[tx_idx];
       tx_idx = (tx_idx + 1) % size;
 
       SPI0_SR = SPI_SR_TFFF_MASK;
@@ -46,4 +45,13 @@ void spi_init(void) {
     
     for (int i = 0; i < 4; i++) BOARD_I2C_DELAY ;
   }
+}
+
+void __attribute__((section("RW_IRAM2_PROGRAM_SCRATCH"))) setup_dma(void) {
+  // Configure perf
+  SPI0_SR = SPI0_SR;  // Clear all status flags
+  SPI0_MCR = SPI_MCR_MSTR_MASK | SPI_MCR_DCONF(0) | SPI_MCR_SMPL_PT(0) | SPI_MCR_CLR_TXF_MASK | SPI_MCR_CLR_RXF_MASK;
+  SPI0_CTAR0 = SPI_CTAR_LSBFE_MASK | SPI_CTAR_BR(7) | SPI_CTAR_FMSZ(15);
+
+  // TODO!
 }
