13c9b6abefd98f6cb6f397665a63974792197f4a
Bryon Vandiver <unicdk@gmail.com>
2015-09-01 15:49:50 -0700

    Checkpoint

--- robot/k02_hal/hal/spi.c
+++ robot/k02_hal/hal/spi.c
@@ -13,7 +13,11 @@
   so it doesn't keep clocking the Espressif.
 */
 
+
 void spi_init(void) {
+  const int size = 96;
+  uint32_t spi_buff[size]; // 512 bytes
+
   // Turn on power to DMA, PORTC and SPI0
   SIM_SCGC6 |= SIM_SCGC6_SPI0_MASK | SIM_SCGC6_DMAMUX_MASK;
   SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
@@ -27,42 +31,75 @@ void spi_init(void) {
 
   // Configure SPI perf to the magical value of magicalness
   SPI0_MCR = SPI_MCR_MSTR_MASK |
-             SPI_MCR_CONT_SCKE_MASK |
              SPI_MCR_DCONF(0) | 
              SPI_MCR_SMPL_PT(0) | 
              SPI_MCR_CLR_TXF_MASK | 
              SPI_MCR_CLR_RXF_MASK;
-  SPI0_CTAR0 = SPI_CTAR_BR(0) | 
+  SPI0_CTAR0 = SPI_CTAR_BR(6) | 
                SPI_CTAR_CPOL_MASK |
                SPI_CTAR_CPHA_MASK |
                SPI_CTAR_FMSZ(15);
+  int x = 0;
+  x = x;
   
+  SPI0_RSER = SPI_RSER_EOQF_RE_MASK;
+
   // Clear all status flags
   SPI0_SR = SPI0_SR;
 
-  const int size = 256;
-  uint16_t spi_buff[size]; // 512 bytes
-  int tx_idx = 0, rx_idx = 0;
+  // Enable IRQs
+  NVIC_SetPriority(SPI0_IRQn, 0);
+  NVIC_EnableIRQ(SPI0_IRQn);
 
-  for(;;) {
+  for (int i = 0; i < size; i++) {
+    spi_buff[i] = 
+        SPI_PUSHR_CONT_MASK | 
+        SPI_PUSHR_PCS(~i & 1 ? ~0: 0) | 
+        (i == (size-1) ? SPI_PUSHR_EOQ_MASK : 0) |
+        (~i & 0xFFFF);
+  }
+ 
+  for(int i = 0; i < size; ) {
     while (SPI0_SR & SPI_SR_RFDF_MASK)
     {
-      spi_buff[rx_idx] = SPI0_POPR;
-      rx_idx = (rx_idx + 1) % size;
+      SPI0_POPR;
 
       SPI0_SR = SPI_SR_RFDF_MASK;
     }
 
-    static int toggle = 0;
-
+    // Transmit 256 words, and send an EOQ on the last word
     while (SPI0_SR & SPI_SR_TFFF_MASK)
     {
-      SPI0_PUSHR = SPI_PUSHR_CONT_MASK | SPI_PUSHR_PCS(toggle) | spi_buff[tx_idx];
-      
-      toggle = ~toggle;
-      tx_idx = (tx_idx + 1) % size;
-
+      SPI0_PUSHR = 
+        SPI_PUSHR_CONT_MASK | 
+        SPI_PUSHR_PCS(~i & 1 ? ~0: 0) | 
+        (i++ & 0xFFFF);
       SPI0_SR = SPI_SR_TFFF_MASK;
+
+      /*
+      SPI0_PUSHR = spi_buff[i];
+      SPI0_SR = SPI_SR_TFFF_MASK;
+      
+      if (i == 0) {
+        BOARD_I2C_DELAY;
+        BOARD_I2C_DELAY;
+        BOARD_I2C_DELAY;
+        BOARD_I2C_DELAY;
+        BOARD_I2C_DELAY;
+        // attempt to silence early clocking
+        SPI0_MCR |= SPI_MCR_CONT_SCKE_MASK;
+      }
+      */
     }
   }
 }
+
+void SPI0_IRQHandler(void) {
+    // When the EOQ flag is set, clear our continuous clock mode.
+    while (SPI0_SR & SPI_SR_EOQF_MASK)
+    {
+      SPI0_MCR &= ~SPI_MCR_CONT_SCKE_MASK;
+      SPI0_SR = SPI_SR_EOQF_MASK;
+    }
+}
+
