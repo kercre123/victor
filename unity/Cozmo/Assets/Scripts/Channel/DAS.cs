1ebc6df9e29d40f13d4570c04acc477c14c4b785
Pohung Chen <pohung@anki.com>
2015-09-01 09:19:25 -0700

    DAS log context

--- unity/Cozmo/Assets/Scripts/Channel/DAS.cs
+++ unity/Cozmo/Assets/Scripts/Channel/DAS.cs
@@ -4,43 +4,43 @@ using UnityEngine;
 
 public static class DAS {
 
-  public static void Event(string eventName, string eventValue) {
+  public static void Event(string eventName, string eventValue, UnityEngine.Object context = null) {
     #if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)
     Unity_DAS_Event(eventName, eventValue);
     #else
-    UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 3, eventName, eventValue));
+    UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 3, eventName, eventValue), context);
     #endif
   }
 
-  public static void Error(string eventName, string eventValue) {
+  public static void Error(string eventName, string eventValue, UnityEngine.Object context = null) {
     #if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)
     Unity_DAS_LogE(eventName, eventValue);
     #else
-    UnityEngine.Debug.LogError(string.Format("DAS [{0}] {1} - {2}", 5, eventName, eventValue));
+    UnityEngine.Debug.LogError(string.Format("DAS [{0}] {1} - {2}", 5, eventName, eventValue), context);
     #endif
   }
 
-  public static void Warn(string eventName, string eventValue) {
+  public static void Warn(string eventName, string eventValue, UnityEngine.Object context = null) {
     #if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)
     Unity_DAS_LogW(eventName, eventValue);
     #else
-    UnityEngine.Debug.LogWarning(string.Format("DAS [{0}] {1} - {2}", 4, eventName, eventValue));
+    UnityEngine.Debug.LogWarning(string.Format("DAS [{0}] {1} - {2}", 4, eventName, eventValue), context);
     #endif
   }
 
-  public static void Info(string eventName, string eventValue) {
+  public static void Info(string eventName, string eventValue, UnityEngine.Object context = null) {
     #if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)
     Unity_DAS_LogI(eventName, eventValue);
     #else
-    UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 2, eventName, eventValue));
+    UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 2, eventName, eventValue), context);
     #endif
   }
 
-  public static void Debug(string eventName, string eventValue) {
+  public static void Debug(string eventName, string eventValue, UnityEngine.Object context = null) {
     #if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)
     Unity_DAS_LogD(eventName, eventValue);
     #else
-    UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 1, eventName, eventValue));
+    UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 1, eventName, eventValue), context);
     #endif
   }
 
--- unity/Cozmo/Assets/Scripts/Games/ShakeShakeController.cs
+++ unity/Cozmo/Assets/Scripts/Games/ShakeShakeController.cs
@@ -7,15 +7,29 @@ public class ShakeShakeController : GameController {
 
   private bool gameOver = false;
   private bool gameReady = false;
+  private int currentScore = 0;
+
+  private class BlockData {
+    public float lastColorChangeTime;
+    public Color currentColor;
+
+    public void SetNewColor(Color color) {
+      currentColor = color;
+      lastColorChangeTime = Time.time;
+    }
+  }
+
+  private Dictionary<int, BlockData> blockData = new Dictionary<int, BlockData>();
 
   protected override void OnEnable() {
     base.OnEnable();
     robot.VisionWhileMoving(true);
-    robot.SetLiftHeight(0.0f);
+    ActiveBlock.TappedAction += BlockTapped;
   }
 
   protected override void OnDisable() {
     base.OnDisable();
+    ActiveBlock.TappedAction -= BlockTapped;
   }
 
   protected override void Enter_BUILDING() {
@@ -24,7 +38,7 @@ public class ShakeShakeController : GameController {
 
   protected override void Update_BUILDING() {
     base.Update_BUILDING();
-    robot.DriveWheels(15.0f, -10.0f);
+    robot.DriveWheels(15.0f, -15.0f);
 
     foreach (KeyValuePair<int, ActiveBlock> activeBlock in robot.activeBlocks) {
       for (int j = 0; j < activeBlock.Value.lights.Length; ++j) {
@@ -56,11 +70,28 @@ public class ShakeShakeController : GameController {
 
   protected override void Enter_PLAYING() {
     base.Enter_PLAYING();
+    // spawn UI
+    // create block game states
+    foreach (KeyValuePair<int, ActiveBlock> activeBlock in robot.activeBlocks) {
+      blockData.Add(activeBlock.Key, new BlockData());
+    }
   }
 
   protected override void Update_PLAYING() {
     base.Update_PLAYING();
+    // update UI
 
+    // update block game states
+    int greenBlockCount = 0;
+    int blueBlockCount = 0;
+
+    // update block colors
+    foreach (KeyValuePair<int, BlockData> bData in blockData) {
+      for (int j = 0; j < robot.activeBlocks[bData.Key].lights.Length; ++j) {
+        robot.activeBlocks[bData.Key].lights[j].onColor = CozmoPalette.ColorToUInt(bData.Value.currentColor);
+      }
+    }
+    DAS.Debug("ShakeShakeController", "current score: " + currentScore);
   }
 
   protected override void Exit_PLAYING(bool overrideStars = false) {
@@ -93,6 +124,21 @@ public class ShakeShakeController : GameController {
 
   protected override void RefreshHUD() {
     base.RefreshHUD();
+  }
+
+  private void BlockTapped(int blockID, int numTapped) {
+    BlockData bData = null;
+    if (blockData.TryGetValue(blockID, out bData)) {
+      if (bData.currentColor == Color.green) {
+        currentScore++;
+      }
+      else if (bData.currentColor == Color.red) {
+        currentScore = currentScore - 10;
+        if (currentScore < 0) {
+          currentScore = 0;
+        }
+      }
+    }
   }
 
 }
