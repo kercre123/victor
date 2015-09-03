faf685840ac10134aafc20dfb4d2e834592b3232
Pohung Chen <pohung@anki.com>
2015-09-03 09:51:42 -0700

    trying to fix iOS build errors...

--- unity/Cozmo/Assets/Scripts/Channel/DAS.cs
+++ unity/Cozmo/Assets/Scripts/Channel/DAS.cs
@@ -2,51 +2,27 @@
 using System.Runtime.InteropServices;
 using UnityEngine;
 
-public static partial class DAS {
-
+#if !UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)
+public static class DAS {
+   
   public static void Event(string eventName, string eventValue, UnityEngine.Object context = null) {
-    if (Application.isEditor) {
-      UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 3, eventName, eventValue), context);
-    }
-    else {
-      Unity_DAS_Event(eventName, eventValue);
-    }
+    Unity_DAS_Event(eventName, eventValue);
   }
 
   public static void Error(string eventName, string eventValue, UnityEngine.Object context = null) {
-    if (Application.isEditor) {
-      UnityEngine.Debug.LogError(string.Format("DAS [{0}] {1} - {2}", 5, eventName, eventValue), context);
-    }
-    else {
-      Unity_DAS_LogE(eventName, eventValue);
-    }
+    Unity_DAS_LogE(eventName, eventValue); 
   }
 
   public static void Warn(string eventName, string eventValue, UnityEngine.Object context = null) {
-    if (Application.isEditor) {
-      UnityEngine.Debug.LogWarning(string.Format("DAS [{0}] {1} - {2}", 4, eventName, eventValue), context);
-    }
-    else {
-      Unity_DAS_LogW(eventName, eventValue);
-    }
+    Unity_DAS_LogW(eventName, eventValue);
   }
 
   public static void Info(string eventName, string eventValue, UnityEngine.Object context = null) {
-    if (Application.isEditor) {
-      UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 2, eventName, eventValue), context);
-    }
-    else {
-      Unity_DAS_LogI(eventName, eventValue);
-    }
+    Unity_DAS_LogI(eventName, eventValue);
   }
 
   public static void Debug(string eventName, string eventValue, UnityEngine.Object context = null) {
-    if (Application.isEditor) {
-      UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 1, eventName, eventValue), context);
-    }
-    else {
-      Unity_DAS_LogD(eventName, eventValue);
-    }
+    Unity_DAS_LogD(eventName, eventValue);
   }
 
   [DllImport("__Internal")]
@@ -65,3 +41,4 @@ public static partial class DAS {
   private static extern void Unity_DAS_LogD(string eventName, string eventValue);
 
 }
+#endif
