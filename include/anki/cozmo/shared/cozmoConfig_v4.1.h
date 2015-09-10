a47d866f8ce0883423944338636de50c24c09818
Kevin Yoon <kevin@anki.com>
2015-09-09 18:23:22 -0700

    Consolidated many cozmoConfig params and put them into common. Moved others down to robot. Added lift and head calibration offsets for physical robot.

--- include/anki/cozmo/shared/cozmoConfig_v4.1.h
+++ include/anki/cozmo/shared/cozmoConfig_v4.1.h
@@ -1,18 +1,9 @@
-
 /***************************************************************************
  *
  *                          Physical Robot Geometry
  *
  **************************************************************************/
-
-const f32 WHEEL_DIAMETER_MM  = 29.f;
-const f32 HALF_WHEEL_CIRCUM  = WHEEL_DIAMETER_MM * M_PI_2;
-const f32 WHEEL_RAD_TO_MM    = WHEEL_DIAMETER_MM / 2.f;  // or HALF_WHEEL_CIRCUM / PI;
-const f32 WHEEL_DIST_MM      = 47.7f; // approx distance b/w the center of the front treads
-const f32 WHEEL_DIST_HALF_MM = WHEEL_DIST_MM / 2.f;
-const f32 WHEEL_BASE_MM      = 48.f;
-
-
+/*
 // The x-offset from robot origin that the robot's drive center is
 // located for the treaded robot when not carrying a block.
 // (If you were to model the treaded robot as a two-wheel robot,
@@ -31,13 +22,6 @@ const f32 LIFT_HEIGHT_LOWDOCK  = 33.f;
 const f32 LIFT_HEIGHT_HIGHDOCK = 83.f; // 76.f;
 const f32 LIFT_HEIGHT_CARRY    = 97.f; // 92.f;
 const f32 LIFT_HEIGHT_LOW_ROLL = 73.f; // 68.f;
-
-// Distance between the robot origin and the distance along the robot's x-axis
-// to the lift when it is in the low docking position.
-const f32 ORIGIN_TO_LOW_LIFT_DIST_MM = 20.f;
-const f32 ORIGIN_TO_HIGH_LIFT_DIST_MM = 17.f;
-const f32 ORIGIN_TO_HIGH_PLACEMENT_DIST_MM = 20.f;
-const f32 ORIGIN_TO_LOW_ROLL_DIST_MM = 13.f;
 
 // Distance between the lift shoulder joint and the lift "wrist" joint where arm attaches to fork assembly
 const f32 LIFT_ARM_LENGTH = 66.f;
@@ -67,28 +51,5 @@ const f32 LIFT_BASE_POSITION[3]  = {-41.0f, 0.f, 31.3f + WHEEL_RAD_TO_MM}; // re
 
 // IMU position relative to neck joint
 const f32 IMU_POSITION[3] = {5.8f, 0.f, -13.5f};
-
-
-
-/***************************************************************************
- *
- *                          Camera Calibration
- *
- **************************************************************************/
-
-static const u8 NUM_RADIAL_DISTORTION_COEFFS = 4;
-
-// Calibration values from Sept 1, 2015 - on 4.1 robot headboard with SSID 3a97
-const u16 HEAD_CAM_CALIB_WIDTH  = 400;
-const u16 HEAD_CAM_CALIB_HEIGHT = 296;
-const f32 HEAD_CAM_CALIB_FOCAL_LENGTH_X = 278.065116921f;
-const f32 HEAD_CAM_CALIB_FOCAL_LENGTH_Y = 278.867229568f;
-const f32 HEAD_CAM_CALIB_CENTER_X       = 197.801561858f;
-const f32 HEAD_CAM_CALIB_CENTER_Y       = 151.672492176f;
-const f32 HEAD_CAM_CALIB_DISTORTION[NUM_RADIAL_DISTORTION_COEFFS] = {
-  0.11281163f,
-  -0.31673507f,
-  -0.00226334f,
-  0.00200109f
-};
+*/
 
