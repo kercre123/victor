69fd950f1594f010aed44fdeb723bf06d85b69a3
Bryon Vandiver <unicdk@gmail.com>
2015-09-16 15:39:20 -0700

    i2c work, RST line is bonkers

--- robot/k02_hal/hal/oled.cpp
+++ robot/k02_hal/hal/oled.cpp
@@ -4,18 +4,17 @@
 #include "board.h"
 #include "oled.h"
 #include "anki/cozmo/robot/hal.h"
+#include "hal/portable.h"
 
-#define SOFT_RESET
+const uint8_t SLAVE_ADDRESS     = 0x78; // Or 0x78 if SA is 0.
 
-const uint8_t SLAVE_ADDRESS     = 0x7A; // Or 0x78 if SA is 0.
+const uint8_t I2C_READ          = 0x01;
+const uint8_t I2C_WRITE         = 0x00;
 
-const uint8_t I2S_READ          = 0x01;
-const uint8_t I2S_WRITE         = 0x00;
-
-const uint8_t I2S_COMMAND       = 0x00;
-const uint8_t I2S_DATA          = 0x40;
-const uint8_t I2S_CONTINUATION  = 0x00;
-const uint8_t I2S_SINGLE        = 0x80;
+const uint8_t I2C_COMMAND       = 0x00;
+const uint8_t I2C_DATA          = 0x40;
+const uint8_t I2C_CONTINUATION  = 0x00;
+const uint8_t I2C_SINGLE        = 0x80;
 
 
 // FIFO junk
@@ -24,12 +23,12 @@ const int MAX_QUEUE = 16;
 typedef void (*i2c_callback)(const void *data, int count);
 
 enum I2C_Direction {
-  I2C_READ,
-  I2C_WRITE
+  I2C_DIR_READ,
+  I2C_DIR_WRITE
 };
 
 typedef struct {
-  int direction;
+  I2C_Direction direction;
   i2c_callback cb;
   
   const void* data;
@@ -46,49 +45,47 @@ static bool _active;
 
 // Display constants
 static const uint8_t InitDisplay[] = {
-  I2S_COMMAND | I2S_SINGLE, DISPLAYOFF,
-  I2S_COMMAND | I2S_SINGLE, SETDISPLAYCLOCKDIV, 
-  I2S_COMMAND | I2S_SINGLE, 0xF0, 
-  I2S_COMMAND | I2S_SINGLE, SETMULTIPLEX, 
-  I2S_COMMAND | I2S_SINGLE, 0x3F, 
-  I2S_COMMAND | I2S_SINGLE, SETDISPLAYOFFSET, 
-  I2S_COMMAND | I2S_SINGLE, 0x0, 
-  I2S_COMMAND | I2S_SINGLE, SETSTARTLINE | 0x0, 
-  I2S_COMMAND | I2S_SINGLE, CHARGEPUMP,
-  I2S_COMMAND | I2S_SINGLE, 0x14, 
-  I2S_COMMAND | I2S_SINGLE, MEMORYMODE,
-  I2S_COMMAND | I2S_SINGLE, 0x01, 
-  I2S_COMMAND | I2S_SINGLE, SEGREMAP | 0x1,
-  I2S_COMMAND | I2S_SINGLE, COMSCANDEC,
-  I2S_COMMAND | I2S_SINGLE, SETCOMPINS,
-  I2S_COMMAND | I2S_SINGLE, 0x12, 
-  I2S_COMMAND | I2S_SINGLE, SETCONTRAST,
-  I2S_COMMAND | I2S_SINGLE, 0xCF,
-  I2S_COMMAND | I2S_SINGLE, SETPRECHARGE,
-  I2S_COMMAND | I2S_SINGLE, 0xF1,
-  I2S_COMMAND | I2S_SINGLE, SETVCOMDETECT,
-  I2S_COMMAND | I2S_SINGLE, 0x40,
-  I2S_COMMAND | I2S_SINGLE, DISPLAYALLON_RESUME,
-  I2S_COMMAND | I2S_SINGLE, NORMALDISPLAY,
-  I2S_COMMAND | I2S_SINGLE, DISPLAYON
+  I2C_COMMAND | I2C_SINGLE, DISPLAYOFF,
+  I2C_COMMAND | I2C_SINGLE, SETDISPLAYCLOCKDIV, 
+  I2C_COMMAND | I2C_SINGLE, 0xF0, 
+  I2C_COMMAND | I2C_SINGLE, SETMULTIPLEX, 
+  I2C_COMMAND | I2C_SINGLE, 0x3F, 
+  I2C_COMMAND | I2C_SINGLE, SETDISPLAYOFFSET, 
+  I2C_COMMAND | I2C_SINGLE, 0x0, 
+  I2C_COMMAND | I2C_SINGLE, SETSTARTLINE | 0x0, 
+  I2C_COMMAND | I2C_SINGLE, CHARGEPUMP,
+  I2C_COMMAND | I2C_SINGLE, 0x14, 
+  I2C_COMMAND | I2C_SINGLE, MEMORYMODE,
+  I2C_COMMAND | I2C_SINGLE, 0x01, 
+  I2C_COMMAND | I2C_SINGLE, SEGREMAP | 0x1,
+  I2C_COMMAND | I2C_SINGLE, COMSCANDEC,
+  I2C_COMMAND | I2C_SINGLE, SETCOMPINS,
+  I2C_COMMAND | I2C_SINGLE, 0x12, 
+  I2C_COMMAND | I2C_SINGLE, SETCONTRAST,
+  I2C_COMMAND | I2C_SINGLE, 0xCF,
+  I2C_COMMAND | I2C_SINGLE, SETPRECHARGE,
+  I2C_COMMAND | I2C_SINGLE, 0xF1,
+  I2C_COMMAND | I2C_SINGLE, SETVCOMDETECT,
+  I2C_COMMAND | I2C_SINGLE, 0x40,
+  I2C_COMMAND | I2C_SINGLE, DISPLAYALLON_RESUME,
+  I2C_COMMAND | I2C_SINGLE, NORMALDISPLAY,
+  I2C_COMMAND | I2C_SINGLE, DISPLAYON
 };
 
 static const int FramePrefixLength = 13;
 static const int FrameBufferLength = 128*64/8;
 
 static uint8_t ResetCursor[FramePrefixLength+FrameBufferLength] = {
-  I2S_COMMAND | I2S_SINGLE, COLUMNADDR,
-  I2S_COMMAND | I2S_SINGLE, 0,
-  I2S_COMMAND | I2S_SINGLE, 127,
-  I2S_COMMAND | I2S_SINGLE, PAGEADDR,
-  I2S_COMMAND | I2S_SINGLE, 0,
-  I2S_COMMAND | I2S_SINGLE, 7,
-  I2S_DATA | I2S_CONTINUATION
+  I2C_COMMAND | I2C_SINGLE, COLUMNADDR,
+  I2C_COMMAND | I2C_SINGLE, 0,
+  I2C_COMMAND | I2C_SINGLE, 127,
+  I2C_COMMAND | I2C_SINGLE, PAGEADDR,
+  I2C_COMMAND | I2C_SINGLE, 0,
+  I2C_COMMAND | I2C_SINGLE, 7,
+  I2C_DATA | I2C_CONTINUATION
 };
 
 static uint8_t *FrameBuffer = &ResetCursor[FramePrefixLength];
-
-const uint32_t OLED_RST  = GPIO_MAKE_PIN(HW_PORTC, 10);
 
 void DMA_TX_Init(const void* source_addr, void* dest_addr, uint32_t num_bytes) {
   // Disable DMA
@@ -122,15 +119,15 @@ void dequeue() {
   I2C_Queue *active = &i2c_queue[_fifo_end];
 
   switch (active->direction) {
-    case I2C_WRITE:
+    case I2C_DIR_WRITE:
       // Enable i2C circuit as TX master, which will send a start condition
       I2C0_C1 |= I2C_C1_TX_MASK | I2C_C1_MST_MASK | I2C_C1_DMAEN_MASK;
-      I2C0_D =  SLAVE_ADDRESS | I2S_WRITE;
+      I2C0_D =  SLAVE_ADDRESS | I2C_WRITE;
 
       // Setup DMA for the future *ghost noises here*
       DMA_TX_Init(active->data, (void*)&(I2C0_D), active->count);  
       break ;
-    case I2C_READ:
+    case I2C_DIR_READ:
       // TODO
       break ;
   }
@@ -138,8 +135,21 @@ void dequeue() {
   _active = true;
 }
 
+static void i2c_reset(void) {
+  GPIO_PIN_SOURCE(OLED_RST, PTA,  19);
+  GPIO_OUT(GPIO_OLED_RST, PIN_OLED_RST);
+  PORTA_PCR19  = PORT_PCR_MUX(1);
+
+  for (;;) {
+    Anki::Cozmo::HAL::MicroWait(80);
+    GPIO_RESET(GPIO_OLED_RST, PIN_OLED_RST);
+    Anki::Cozmo::HAL::MicroWait(80);
+    GPIO_SET(GPIO_OLED_RST, PIN_OLED_RST);
+  }
+}
+
 // I2C calls
-bool i2s_write(const uint8_t *bytes, int len, i2c_callback cb) {
+bool i2c_write(const uint8_t *bytes, int len, i2c_callback cb) {
   // Queue is full.  Sorry 'bout it.
   if (_fifo_count >= MAX_QUEUE) {
     return false;
@@ -153,7 +163,7 @@ bool i2s_write(const uint8_t *bytes, int len, i2c_callback cb) {
   active->cb = cb;
   active->count = len;
   active->data = bytes;
-  active->direction = I2C_WRITE;
+  active->direction = I2C_DIR_WRITE;
 
   if (!_active) {
     dequeue();
@@ -163,7 +173,7 @@ bool i2s_write(const uint8_t *bytes, int len, i2c_callback cb) {
   return true;
 }
 
-bool i2s_read(const uint8_t *bytes, int len, i2c_callback cb) {
+bool i2c_read(const uint8_t *bytes, int len, i2c_callback cb) {
   // Queue is full.  Sorry 'bout it.
   if (_fifo_count >= MAX_QUEUE) {
     return false;
@@ -177,7 +187,7 @@ bool i2s_read(const uint8_t *bytes, int len, i2c_callback cb) {
   active->cb = cb;
   active->count = len;
   active->data = bytes;
-  active->direction = I2C_READ;
+  active->direction = I2C_DIR_READ;
 
   if (!_active) {
     dequeue();
@@ -196,31 +206,21 @@ void invert(const void *data, int length) {
 }
 
 void FlipOLED(void) {
-  i2s_write(ResetCursor, sizeof(ResetCursor), invert);
+  i2c_write(ResetCursor, sizeof(ResetCursor), invert);
 }
 
 void i2c_init()
 {
-  // AHHHHHH DO NO USE!
-  #ifdef SOFT_RESET
-  // Toggle the reset bit
-  PORT_HAL_SetMuxMode(PORTC_BASE, 10, kPortMuxAsGpio);
-  GPIO_DRV_WritePinOutput(OLED_RST, 0);
-  GPIO_DRV_SetPinDir(OLED_RST, kGpioDigitalOutput); 
-  Anki::Cozmo::HAL::MicroWait(80);
-  GPIO_DRV_WritePinOutput(OLED_RST, 1);
-  #endif
-
-  // Enable clocking on I2C, PortB, and DMA
+   // Enable clocking on I2C, PortB, PortE, and DMA
   SIM_SCGC4 |= SIM_SCGC4_I2C0_MASK;
-  SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+  SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTE_MASK;
   SIM_SCGC6 |= SIM_SCGC6_DMAMUX_MASK;
   SIM_SCGC7 |= SIM_SCGC7_DMA_MASK;
 
   // Configure port mux for i2c
-  PORTB_PCR2 = PORT_PCR_MUX(2); //I2C0_SDA
-  PORTB_PCR3 = PORT_PCR_MUX(2); //I2C0_SCL
-  
+  PORTB_PCR1 = PORT_PCR_MUX(2);   //I2C0_SDA
+  PORTE_PCR24 = PORT_PCR_MUX(5);  //I2C0_SCL
+
   // Configure i2c
   I2C0_F  = I2C_F_ICR(0x0D) | I2C_F_MULT(1);
   I2C0_C1 = I2C_C1_IICEN_MASK | I2C_C1_IICIE_MASK;
@@ -232,6 +232,8 @@ void i2c_init()
   NVIC_SetPriority(DMA1_IRQn, 3);
   NVIC_EnableIRQ(DMA1_IRQn);
 
+  i2c_reset();
+
   // Clear our FIFO
   _fifo_start = 0;
   _fifo_end = 0;
@@ -240,7 +242,7 @@ void i2c_init()
   _active = false;
  
   // Init Display the proper way
-  i2s_write(InitDisplay, sizeof(InitDisplay), NULL);
+  i2c_write(InitDisplay, sizeof(InitDisplay), NULL);
   FlipOLED();
 }
 
