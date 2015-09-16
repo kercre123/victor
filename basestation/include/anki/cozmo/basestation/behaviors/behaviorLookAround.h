2735f524f34ceba71d0303217f34d65901d6dc15
Lee Crippen <lee@anki.com>
2015-09-15 18:14:02 -0700

    Reducing LookAround behavior cooldown

--- basestation/include/anki/cozmo/basestation/behaviors/behaviorLookAround.h
+++ basestation/include/anki/cozmo/basestation/behaviors/behaviorLookAround.h
@@ -63,7 +63,7 @@ private:
   };
   
   // How long to wait before we trying to look around again (among other factors)
-  constexpr static f32 kLookAroundCooldownDuration = 11;
+  constexpr static f32 kLookAroundCooldownDuration = 7;
   // How fast to rotate when looking around
   constexpr static f32 kDegreesRotatePerSec = 25;
   // The default radius (in mm) we assume exists for us to move around in
