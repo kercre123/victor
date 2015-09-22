42c0bf1541652ab0dc555baa3ed39f3a385ca63c
Brad Neuman <brad@anki.com>
2015-09-22 16:33:45 -0700

    Enabled printf logger in coretech unit tests

--- coretech/common/basestation/test/run_coreTechCommonTests.cpp
+++ coretech/common/basestation/test/run_coreTechCommonTests.cpp
@@ -1,15 +1,24 @@
 #include "util/helpers/includeGTest.h" // Used in place of gTest/gTest.h directly to suppress warnings in the header
 #include <iostream>
+#include "util/logging/logging.h"
+#include "util/logging/printfLoggerProvider.h"
+
+Anki::Util::PrintfLoggerProvider* loggerProvider = nullptr;
 
 GTEST_API_ int main(int argc, char * argv[])
 {
+  //LEAKING HERE
+  loggerProvider = new Anki::Util::PrintfLoggerProvider();
+  loggerProvider->SetMinLogLevel(0);
+  Anki::Util::gLoggerProvider = loggerProvider;
+
   char *path=NULL;
   size_t size = 0;
   path=getcwd(path,size);
-  std::cout<<"hello world! I am " << argv[0] << "\nRunning in " << path << "\nLet the testing commence\n\n";
+  std::cout<<"hello world! I am " << argv[0] << "\nRunning in " << path
+           << "\nLet the (CoreTechCommon) testing commence\n\n";
   free(path);
   ::testing::InitGoogleTest(&argc, argv);
  
-  return RUN_ALL_TESTS();
-  
+  return RUN_ALL_TESTS();  
 }
