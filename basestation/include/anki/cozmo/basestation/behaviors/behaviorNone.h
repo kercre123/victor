cee220ca85e60b4fd9ec77bcd9d6642d61058a57
Lee Crippen <lee@anki.com>
2015-10-01 17:32:34 -0700

    Creating new none behavior and adding to demoBehaviorChooser

---
+++ basestation/include/anki/cozmo/basestation/behaviors/behaviorNone.h
@@ -0,0 +1,48 @@
+/**
+ * File: behaviorNone.h
+ *
+ * Author: Lee Crippen
+ * Created: 10/01/15
+ *
+ * Description: Behavior to do nothing.
+ *
+ * Copyright: Anki, Inc. 2015
+ *
+ **/
+
+#ifndef __Cozmo_Basestation_Behaviors_BehaviorNone_H__
+#define __Cozmo_Basestation_Behaviors_BehaviorNone_H__
+
+#include "anki/cozmo/basestation/behaviors/behaviorInterface.h"
+
+namespace Anki {
+namespace Cozmo {
+  
+class BehaviorNone: public IBehavior
+{
+public:
+  BehaviorNone(Robot& robot, const Json::Value& config) : IBehavior(robot, config) { _name = "None"; }
+  virtual ~BehaviorNone() { }
+  
+  //
+  // Abstract methods to be overloaded:
+  //
+  virtual bool IsRunnable(double currentTime_sec) const override { return true; }
+  virtual Result Init(double currentTime_sec) override { _isInterrupted = false; return Result::RESULT_OK; }
+  virtual Status Update(double currentTime_sec) override
+  {
+    Status retval = _isInterrupted ? Status::Complete : Status::Running;
+    return retval;
+  }
+  virtual Result Interrupt(double currentTime_sec) override { _isInterrupted = true; return Result::RESULT_OK; }
+  virtual bool GetRewardBid(Reward& reward) override { return true; }
+  
+protected:
+  bool _isInterrupted = false;
+};
+  
+
+} // namespace Cozmo
+} // namespace Anki
+
+#endif // __Cozmo_Basestation_Behaviors_BehaviorNone_H__
