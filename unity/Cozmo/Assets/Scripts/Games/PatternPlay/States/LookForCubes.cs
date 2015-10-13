e995e9f6015e7b8155b3b5194094dda5060f107d
Pohung Chen <pohung@anki.com>
2015-10-13 16:48:29 -0700

    renaming Place ground message more states

--- unity/Cozmo/Assets/Scripts/Games/PatternPlay/States/LookForCubes.cs
+++ unity/Cozmo/Assets/Scripts/Games/PatternPlay/States/LookForCubes.cs
@@ -2,5 +2,21 @@
 using System.Collections;
 
 public class LookForCubes : State {
+  int cubeSeenCount;
+
+  public override void Enter() {
+    base.Enter();
+  }
+
+  public override void Update() {
+    base.Update();
+    if (cubeSeenCount > 0) {
+      stateMachine.SetNextState(new PickUpCube());
+    }
+  }
+
+  public override void Exit() {
+    base.Exit();
+  }
 
 }
---
+++ unity/Cozmo/Assets/Scripts/Games/PatternPlay/States/PickUpCube.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpCube : State {
+
+  public override void Enter() {
+    base.Enter();
+    //robot.PickAndPlaceObject()
+  }
+
+  void PickUpDone() {
+    
+  }
+}
