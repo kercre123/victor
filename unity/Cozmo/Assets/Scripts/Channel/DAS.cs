488a41ad94a25f7b45635e3c959538727d8cc878
Pohung Chen <pohung@anki.com>
2015-10-26 16:02:15 -0700

    DAS line ending changes

--- unity/Cozmo/Assets/Scripts/Channel/DAS.cs
+++ unity/Cozmo/Assets/Scripts/Channel/DAS.cs
@@ -6,40 +6,50 @@ using UnityEngine;
 public static class DAS {
    
   public static void Event(string eventName, string eventValue, UnityEngine.Object context = null) {
+
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
+
 #else
     Unity_DAS_Event(eventName, eventValue);
 #endif
   }
-
+ 
   public static void Error(string eventName, string eventValue, UnityEngine.Object context = null) {
+
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
+
 #else
     Unity_DAS_LogE(eventName, eventValue);
 #endif
   }
 
   public static void Warn(string eventName, string eventValue, UnityEngine.Object context = null) {
+
 #if ANIMATION_TOOL
       Console.Out.WriteLine(eventName + ": " + eventValue);
+
 #else
       Unity_DAS_LogW(eventName, eventValue);
 #endif
     }
 
     public static void Info(string eventName, string eventValue, UnityEngine.Object context = null) {
+
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
+
 #else
     Unity_DAS_LogI(eventName, eventValue);
 #endif
   }
 
   public static void Debug(string eventName, string eventValue, UnityEngine.Object context = null) {
+
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
+
 #else
     Unity_DAS_LogD(eventName, eventValue);
 #endif
