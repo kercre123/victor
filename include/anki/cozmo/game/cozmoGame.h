7e49f009b4dd4b26f7a6cdee2f67ce487ed30546
Daniel Casner <daniel@anki.com>
2015-10-28 13:50:01 -0700

    Revert "Updates to cozmo game to match 4.1 firmware merge (PR #217)"
    This reverts commit eddd8bff5ea4bc3112fae5a68508f00a7d4ab2b4.
    
    Didn't mean to push this to master!

--- include/anki/cozmo/game/cozmoGame.h
+++ include/anki/cozmo/game/cozmoGame.h
@@ -14,6 +14,7 @@
 
 #include "anki/common/types.h"
 #include "json/json.h"
+#include "anki/cozmo/shared/cozmoTypes.h"
 #include "clad/externalInterface/messageEngineToGame.h"
 
 namespace Anki {
@@ -107,3 +108,4 @@ namespace Cozmo {
 } // namespace Anki
 
 #endif // ANKI_COZMO_GAME_HOST_H
+
