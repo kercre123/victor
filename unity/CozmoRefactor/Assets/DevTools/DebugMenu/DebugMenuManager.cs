6ba581482da1f2d6c28bd01f6f7a2d8f03bab558
Ivy Ngo <ivy@anki.com>
2015-10-30 15:15:53 -0700

    Debug menu prefab set-up

--- unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuManager.cs
+++ unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuManager.cs
@@ -3,13 +3,16 @@ using System.Collections;
 
 public class DebugMenuManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	[SerializeField]
+	private DebugMenuDialog debugMenuDialogPrefab_;
+  private DebugMenuDialog debugMenuDialogInstance_;
+
+  void Start(){
+    // TODO: Pragma out this code for production
+    GameObject debugMenuDialogInstance = GameObject.Instantiate(debugMenuDialogPrefab_.gameObject);
+    Transform dialogTransform = debugMenuDialogInstance.transform;
+    dialogTransform.SetParent(this.transform);
+    dialogTransform.localPosition = Vector3.zero;
+    debugMenuDialogInstance_ = debugMenuDialogInstance.GetComponent<DebugMenuDialog>();
+  }
 }
