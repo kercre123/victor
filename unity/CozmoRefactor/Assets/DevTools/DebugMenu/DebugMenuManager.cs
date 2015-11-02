0c67a02383f6b965e7589758b2f2ccdb7f067e8d
Ivy Ngo <ivy@anki.com>
2015-11-02 10:40:47 -0800

    Setting up testing functions

--- unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuManager.cs
+++ unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuManager.cs
@@ -18,7 +18,7 @@ public class DebugMenuManager : MonoBehaviour {
   public void OnDebugMenuButtonTap(){
     CreateDebugDialog();
   }
-
+  
   private void CreateDebugDialog(){
     GameObject debugMenuDialogInstance = GameObject.Instantiate(debugMenuDialogPrefab_.gameObject);
     Transform dialogTransform = debugMenuDialogInstance.transform;
@@ -26,4 +26,29 @@ public class DebugMenuManager : MonoBehaviour {
     dialogTransform.localPosition = Vector3.zero;
     debugMenuDialogInstance_ = debugMenuDialogInstance.GetComponent<DebugMenuDialog>();
   }
+
+  private void OnAddInfo(string eventName, string eventValue){
+  }
+ 
+  #region TESTING
+	public void OnAddInfoTap(){
+    DAS.Info("DebugMenuManager", "Manually Adding Info Info Info");
+	}
+
+	public void OnAddWarningTap(){
+    DAS.Warn("DebugMenuManager", "Manually Adding Warn Warn Warn");
+	}
+
+	public void OnAddErrorTap(){
+    DAS.Error("DebugMenuManager", "Manually Adding Error Error Error");
+	}
+
+	public void OnAddEventTap(){
+    DAS.Event("DebugMenuManager", "Manually Adding Event Event Event");
+	}
+
+	public void OnAddDebugTap(){
+    DAS.Debug("DebugMenuManager", "Manually Adding Debug Debug Debug");
+	}
+  #endregion
 }
