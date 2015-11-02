7a769599de8e958b1df323852978f82c04935d16
Ivy Ngo <ivy@anki.com>
2015-11-02 15:52:40 -0800

    Adding events to DAS so that we can show logs in the debug menu

--- UnityDASDll/UnityDASDll/DAS.cs
+++ UnityDASDll/UnityDASDll/DAS.cs
@@ -4,24 +4,46 @@ using UnityEngine;
 
 public static partial class DAS {
 
+  public delegate void DASHandler(string eventName, string eventValue, UnityEngine.Object context);
+  public static event DASHandler OnEventLogged;
+  public static event DASHandler OnErrorLogged;
+  public static event DASHandler OnWarningLogged;
+  public static event DASHandler OnInfoLogged;
+  public static event DASHandler OnDebugLogged;
+
   public static void Event(string eventName, string eventValue, UnityEngine.Object context = null) {
     UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 3, eventName, eventValue), context);
+    if (OnEventLogged != null) {
+      OnEventLogged(eventName, eventValue, context);
+    }
   }
 
   public static void Error(string eventName, string eventValue, UnityEngine.Object context = null) {
     UnityEngine.Debug.LogError(string.Format("DAS [{0}] {1} - {2}", 5, eventName, eventValue), context);
+    if (OnErrorLogged != null) {
+      OnErrorLogged(eventName, eventValue, context);
+    }
   }
 
   public static void Warn(string eventName, string eventValue, UnityEngine.Object context = null) {
     UnityEngine.Debug.LogWarning(string.Format("DAS [{0}] {1} - {2}", 4, eventName, eventValue), context);
+    if (OnWarningLogged != null) {
+      OnWarningLogged(eventName, eventValue, context);
+    }
   }
 
   public static void Info(string eventName, string eventValue, UnityEngine.Object context = null) {
     UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 2, eventName, eventValue), context);
+    if (OnInfoLogged != null) {
+      OnInfoLogged(eventName, eventValue, context);
+    }
   }
 
   public static void Debug(string eventName, string eventValue, UnityEngine.Object context = null) {
     UnityEngine.Debug.Log(string.Format("DAS [{0}] {1} - {2}", 1, eventName, eventValue), context);
+    if (OnDebugLogged != null) {
+      OnDebugLogged(eventName, eventValue, context);
+    }
   }
   
 }
