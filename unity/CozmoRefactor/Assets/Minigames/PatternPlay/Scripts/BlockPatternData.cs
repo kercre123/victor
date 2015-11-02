5da38306818d4b00740e87ca48cfb753f13cec81
Pohung Chen <pohung@anki.com>
2015-11-02 12:50:32 -0800

    pattern play code son reader

---
+++ unity/CozmoRefactor/Assets/Minigames/PatternPlay/Scripts/BlockPatternData.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockPatternData {
+
+  public BlockPatternData(BlockLights lights, float frameZAccel, float timeTapped) {
+    blockLightsLocalSpace = lights;
+    lastFrameZAccel = frameZAccel;
+    lastTimeTapped = timeTapped;
+  }
+
+  public BlockLights blockLightsLocalSpace;
+  public float lastFrameZAccel;
+  public float lastTimeTapped;
+  // for phone input
+  public float lastTimeTouched;
+  public bool moving = false;
+  public float seenAccumulator = 0.0f;
+
+  public bool BlockActiveTimeTouched() {
+    return Time.time - lastTimeTouched < 1.2f;
+  }
+}
+
--- unity/CozmoRefactor/Assets/Minigames/PatternPlay/Scripts/PatternPlayGame.cs
+++ unity/CozmoRefactor/Assets/Minigames/PatternPlay/Scripts/PatternPlayGame.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PatternPlayGame : GameBase {
 
   StateMachineManager patternPlayStateMachineManager_ = new StateMachineManager();
   StateMachine patternPlayStateMachine_ = new StateMachine();
+
+  private Dictionary<int, BlockPatternData> blockPatternData = new Dictionary<int, BlockPatternData>();
+  private PatternMemory memoryBank = new PatternMemory();
 
   void Start() {
     patternPlayStateMachine_.SetGameRef(this);
