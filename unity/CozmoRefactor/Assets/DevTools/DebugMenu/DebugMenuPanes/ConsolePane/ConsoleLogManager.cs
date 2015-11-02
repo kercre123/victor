5e999705b98872cb04033e432c7886ed6451e3a7
Ivy Ngo <ivy@anki.com>
2015-11-02 15:52:06 -0800

    Reorganizing DebugMenu folder

---
+++ unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuPanes/ConsolePane/ConsoleLogManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ConsoleLogManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
