50b0c26515437edc70d4311759f67be3900a9b27
Pohung Chen <pohung@anki.com>
2015-11-03 14:50:05 -0800

    porting speed tap

--- unity/Cozmo/Assets/Minigames/SpeedTap/Scripts/SpeedTapGame.cs
+++ unity/Cozmo/Assets/Minigames/SpeedTap/Scripts/SpeedTapGame.cs
@@ -1,15 +1,59 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class SpeedTapGame : GameBase {
 
+  public ActiveBlock cozmoBlock_;
+  public ActiveBlock playerBlock_;
+  public int cozmoScore_;
+  public int playerScore_;
+
+  public event Action PlayerTappedBlockEvent;
+
+
+  [SerializeField]
+  private UnityEngine.UI.Text cozmoScoreField;
+
+  [SerializeField]
+  private UnityEngine.UI.Text playerScoreField;
+
+  [SerializeField]
+  private AudioClip rollSound_;
+
   // Use this for initialization
-  void Start() {
+  void Start() { 
     DAS.Info("SpeedTapGame", "Game Created");
+
+    robot.VisionWhileMoving(true);
+    ActiveBlock.TappedAction += BlockTapped;
+    robot.StopFaceAwareness();
+    robot.SetBehaviorSystem(false);
+
+    UpdateUI();
   }
-	
-  // Update is called once per frame
-  void Update() {
-	
+
+  public void UpdateUI() {
+    if (cozmoScoreField != null) {
+      cozmoScoreField.text = cozmoScore_.ToString();
+    }
+    if (playerScoreField != null) {
+      playerScoreField.text = playerScore_.ToString();
+    }
   }
+
+  public void RollingBlocks() {
+    AudioManager.PlayAudioClip(rollSound_);
+  }
+
+
+  private void BlockTapped(int blockID, int tappedTimes) {
+    Debug.Log("Ima tapped:" + blockID);
+    if (playerBlock_ != null && playerBlock_.ID == blockID) {
+      if (PlayerTappedBlockEvent != null) {
+        PlayerTappedBlockEvent();
+      }
+    }
+  }
+
 }
---
+++ unity/Cozmo/Assets/Minigames/SpeedTap/Scripts/States/SpeedTapStateGoToCube.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+public class SpeedTapStateGoToCube : State {
+
+  private SpeedTapGame speedTapGame_ = null;
+  private bool drivingToBlock_ = false;
+
+  public override void Enter() {
+    base.Enter();
+    speedTapGame_ = stateMachine_.GetGame() as SpeedTapGame;
+    robot.SetLiftHeight(1.0f);
+    robot.SetHeadAngle();
+  }
+
+  void DriveToBlock() {
+    if (drivingToBlock_)
+      return;
+    if (speedTapGame_.cozmoBlock_.MarkersVisible) {
+      drivingToBlock_ = true;
+      robot.GotoObject(speedTapGame_.cozmoBlock_, 50.0f, (bool success) => {
+        drivingToBlock_ = false;
+        if (success) {
+          stateMachine_.SetNextState(new SpeedTapStateBeginMatch());
+        }
+        else {
+          
+        }
+      });
+    }
+  }
+
+  public override void Update() {
+    base.Update();
+    DriveToBlock();
+  }
+}
+
+
