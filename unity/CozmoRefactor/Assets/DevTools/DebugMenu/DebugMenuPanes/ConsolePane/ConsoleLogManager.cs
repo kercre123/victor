abcba0627dfec75d0c5e25f85220079fdda4b848
Ivy Ngo <ivy@anki.com>
2015-11-03 10:36:53 -0800

    Factoring out testing to its own prefab

--- unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuPanes/ConsolePane/ConsoleLogManager.cs
+++ unity/CozmoRefactor/Assets/DevTools/DebugMenu/DebugMenuPanes/ConsolePane/ConsoleLogManager.cs
@@ -121,29 +121,6 @@ public class ConsoleLogManager : MonoBehaviour {
     }
     return sb.ToString();
   }
-
-  // TODO: Pragma out for production
-  #region TESTING
-  public void OnAddInfoTap(){
-    DAS.Info("DebugMenuManager", "Manually Adding Info Info Info");
-  }
-  
-  public void OnAddWarningTap(){
-    DAS.Warn("DebugMenuManager", "Manually Adding Warn Warn Warn");
-  }
-  
-  public void OnAddErrorTap(){
-    DAS.Error("DebugMenuManager", "Manually Adding Error Error Error");
-  }
-  
-  public void OnAddEventTap(){
-    DAS.Event("DebugMenuManager", "Manually Adding Event Event Event");
-  }
-  
-  public void OnAddDebugTap(){
-    DAS.Debug("DebugMenuManager", "Manually Adding Debug Debug Debug");
-  }
-  #endregion
 }
 
 public class LogPacket {
