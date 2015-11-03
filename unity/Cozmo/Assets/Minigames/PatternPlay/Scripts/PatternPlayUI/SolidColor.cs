4057c2cade18d137b347b5115c5484e4ecf9f85f
Ivy Ngo <ivy@anki.com>
2015-11-03 15:33:15 -0800

    Hooking the font back up and getting rid of some annoying errors because SolidColor was not handling materials nicely

--- unity/Cozmo/Assets/Minigames/PatternPlay/Scripts/PatternPlayUI/SolidColor.cs
+++ unity/Cozmo/Assets/Minigames/PatternPlay/Scripts/PatternPlayUI/SolidColor.cs
@@ -4,14 +4,29 @@ using System.Collections;
 [ExecuteInEditMode]
 public class SolidColor : MonoBehaviour {
 
-  public Color ObjectColor;
-
-  private Color currentColor;
-
-	// Update is called once per frame
-	void Update () {
-    if (ObjectColor != currentColor) {
-      gameObject.GetComponent<Renderer>().material.color = currentColor = ObjectColor;
+  public Color ObjectColor {
+    get {
+      return lightMaterial_.color;
     }
-	}
+    set {
+      lightMaterial_.color = value;
+    }
+  }
+
+  private Material lightMaterial_;
+
+  private void Awake(){
+    Renderer renderer = gameObject.GetComponent<Renderer>();
+    lightMaterial_ = new Material(Shader.Find("Diffuse"));
+    lightMaterial_.color = ObjectColor;
+
+    // Let unity know we'll handle destroying this material
+    lightMaterial_.hideFlags = HideFlags.HideAndDontSave;
+
+    renderer.sharedMaterial = lightMaterial_;
+  }
+
+  private void OnDestroy(){
+    DestroyImmediate(lightMaterial_);
+  }
 }
