f9cbf709efaa13aa0f8ba7d76401dceb1466f148
Pohung Chen <pohung@anki.com>
2015-11-04 14:47:37 -0800

    starting energy hunt game

---
+++ unity/Cozmo/Assets/Minigames/EnergyHunt/EnergyHuntGame.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnergyHuntGame : GameBase {
+
+  void Start() {
+    
+  }
+
+  void Update() {
+    
+  }
+
+  public override void HandleHubWorldDestroyed() {
+
+  }
+}
