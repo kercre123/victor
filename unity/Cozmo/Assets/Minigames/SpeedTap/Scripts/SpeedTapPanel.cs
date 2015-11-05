73ff53398c413a8fe4a9b9503a4d7bb5d37e9d6a
Pohung Chen <pohung@anki.com>
2015-11-05 14:06:01 -0800

    on screen tap button

--- unity/Cozmo/Assets/Minigames/SpeedTap/Scripts/SpeedTapPanel.cs
+++ unity/Cozmo/Assets/Minigames/SpeedTap/Scripts/SpeedTapPanel.cs
@@ -4,17 +4,34 @@ using System.Collections;
 public class SpeedTapPanel : BaseDialog {
 
   [SerializeField]
-  private UnityEngine.UI.Text cozmoScoreField;
+  private UnityEngine.UI.Text cozmoScoreField_;
 
   [SerializeField]
-  private UnityEngine.UI.Text playerScoreField;
+  private UnityEngine.UI.Text playerScoreField_;
+
+  [SerializeField]
+  private UnityEngine.UI.Button tapButton_;
+
+  public delegate void TapButtonPressedHandler();
+
+  public TapButtonPressedHandler TapButtonPressed;
+
+  void Start() {
+    tapButton_.onClick.AddListener(OnTapped);
+  }
+
+  private void OnTapped() {
+    if (TapButtonPressed != null) {
+      TapButtonPressed();
+    }
+  }
 
   public void SetScoreText(int cozmoScore, int playerScore) {
-    if (cozmoScoreField != null) {
-      cozmoScoreField.text = "cozmo: " + cozmoScore.ToString();
+    if (cozmoScoreField_ != null) {
+      cozmoScoreField_.text = "cozmo: " + cozmoScore.ToString();
     }
-    if (playerScoreField != null) {
-      playerScoreField.text = "player: " + playerScore.ToString();
+    if (playerScoreField_ != null) {
+      playerScoreField_.text = "player: " + playerScore.ToString();
     }
   }
 
