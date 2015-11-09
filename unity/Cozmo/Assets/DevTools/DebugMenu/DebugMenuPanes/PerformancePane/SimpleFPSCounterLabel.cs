72fd7cd9d0b3a887465675515faf452050a94410
Ivy Ngo <ivy@anki.com>
2015-11-09 13:53:12 -0800

    Implemented fps counter so we can determine framerate on device

---
+++ unity/Cozmo/Assets/DevTools/DebugMenu/DebugMenuPanes/PerformancePane/SimpleFPSCounterLabel.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SimpleFPSCounterLabel : MonoBehaviour {
+
+  [SerializeField]
+  private Text fpsLabel_;
+
+  [SerializeField]
+  private Text fpsAvgLabel_;
+
+  [SerializeField]
+  private Button closeButton_;
+
+	// Use this for initialization
+	private void Start () {
+    closeButton_.onClick.AddListener(OnCloseButtonClick);
+	}
+  
+  private void OnDestroy () {
+    closeButton_.onClick.RemoveListener(OnCloseButtonClick);
+  }
+
+  public void SetFPS(float newFPS){
+    fpsLabel_.text = newFPS.ToString();
+  }
+
+  public void SetAvgFPS(float newFPS){
+    fpsAvgLabel_.text = newFPS.ToString("F2");
+  }
+
+  private void OnCloseButtonClick(){
+    Destroy(gameObject);
+  }
+}
