5773619957f70dd97a53228cedd4dcf171ff3a03
Pohung Chen <pohung@anki.com>
2015-11-12 15:12:23 -0800

    fleshing out TrySuccessDock

--- unity/Cozmo/Assets/Minigames/DockTraining/Scripts/States/TrySuccessDockState.cs
+++ unity/Cozmo/Assets/Minigames/DockTraining/Scripts/States/TrySuccessDockState.cs
@@ -4,16 +4,38 @@ using System.Collections;
 namespace DockTraining {
 
   public class TrySuccessDockState : State {
+    LightCube _CurrentTarget;
+    DockTrainingGame _DockTrainingGame;
+
     public override void Enter() {
       base.Enter();
+      _DockTrainingGame = _StateMachine.GetGame() as DockTrainingGame;
+      _CurrentTarget = _DockTrainingGame.GetCurrentTarget();
+
+      if (_CurrentTarget == null) {
+        _StateMachine.SetNextState(new AngryTargetChangedState());
+      }
+      else {
+        _CurrentRobot.AlignWithObject(_CurrentTarget, 20.0f, AlignDone);
+      }
     }
 
     public override void Update() {
       base.Update();
+
     }
 
     public override void Exit() {
       base.Exit();
+    }
+
+    private void AlignDone(bool success) {
+      if (success) {
+        _StateMachine.SetNextState(new CelebrateState());
+      }
+      else {
+        _StateMachine.SetNextState(new AngryTargetChangedState());
+      }
     }
   }
 
