33f93858bb54d7d3b8a8634545e184f6ad03992f
Pohung Chen <pohung@anki.com>
2015-11-11 16:15:05 -0800

    vision training first pass

---
+++ unity/Cozmo/Assets/Minigames/VisionTraining/Scripts/States/RecognizeCubeState.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace VisionTraining {
+
+  public class RecognizeCubeState : State {
+
+    private float _CubeInRectTime = 0.0f;
+    private int _SelectedCubeId = -1;
+
+    private Rect _VisionRect = new Rect(new Vector2(100.0f, 0.0f), new Vector2(50.0f, 50.0f));
+
+    public override void Enter() {
+      base.Enter();
+      _SelectedCubeId = (_StateMachine.GetGame() as VisionTrainingGame).PickCube();
+    }
+
+    public override void Update() {
+      base.Update();
+
+      bool cubeInRectNow = false;
+
+      for (int i = 0; i < _CurrentRobot.VisibleObjects.Count; ++i) {
+        if (_CurrentRobot.VisibleObjects[i].ID == _SelectedCubeId) {
+          if (_VisionRect.Contains((Vector2)_CurrentRobot.VisibleObjects[i].WorldPosition)) {
+            cubeInRectNow = true;
+          }
+        }
+      }
+
+      if (cubeInRectNow) {
+        _CubeInRectTime += Time.deltaTime;
+      }
+      else {
+        _CubeInRectTime -= Time.deltaTime;
+        if (_CubeInRectTime < 0.0f) {
+          _CubeInRectTime = 0.0f;
+        }
+      }
+
+      if (CubeInRect()) {
+        _CurrentRobot.SendAnimation("majorWin", AnimationDone);
+      }
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+
+    void AnimationDone(bool success) {
+      _StateMachine.SetNextState(new RecognizeCubeState());
+    }
+
+    public bool CubeInRect() {
+      return _CubeInRectTime > 3.0f;
+    }
+  }
+
+}
---
+++ unity/Cozmo/Assets/Minigames/VisionTraining/Scripts/VisionTrainingGame.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace VisionTraining {
+
+  public class VisionTrainingGame : GameBase {
+
+    private StateMachineManager _StateMachineManager = new StateMachineManager();
+    private StateMachine _StateMachine = new StateMachine();
+
+    void Start() {
+      _StateMachine.SetGameRef(this);
+      _StateMachineManager.AddStateMachine("FollowCubeStateMachine", _StateMachine);
+      InitialCubesState initCubeState = new InitialCubesState();
+      initCubeState.InitialCubeRequirements(new RecognizeCubeState(), 3, InitialCubesDone);
+      _StateMachine.SetNextState(initCubeState);
+      robot.StopFaceAwareness();
+
+      CreateDefaultQuitButton();
+    }
+
+    void Update() {
+
+    }
+
+    private void InitialCubesDone() {
+      
+    }
+
+    public int PickCube() {
+      int index = Random.Range(0, robot.LightCubes.Count);
+      int i = 0;
+      foreach (KeyValuePair<int, LightCube> lightCube in robot.LightCubes) {
+        if (index == i) {
+          return lightCube.Key;
+        }
+        i++;
+      }
+      return -1;
+    }
+
+    public override void CleanUp() {
+      DestroyDefaultQuitButton();
+    }
+  }
+
+}
