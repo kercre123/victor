e15132e0c40d99b7275151d79e4e6b001128b27d
Molly Jameson <molly@anki.com>
2015-11-12 14:09:35 -0800

    vortex stubs

--- unity/Cozmo/Assets/Minigames/Vortex/Scripts/VortexGame.cs
+++ unity/Cozmo/Assets/Minigames/Vortex/Scripts/VortexGame.cs
@@ -4,16 +4,44 @@ using System.Collections;
 namespace Vortex {
 
   public class VortexGame : GameBase {
+
+    [SerializeField]
+    private VortexPanel _GamePanelPrefab;
+
+    private VortexPanel _GamePanel;
+
+    private StateMachineManager _StateMachineManager = new StateMachineManager();
+    private StateMachine _StateMachine = new StateMachine();
+
     void Start() {
+
+      _StateMachine.SetGameRef(this);
+      _StateMachineManager.AddStateMachine("VortexStateMachine", _StateMachine);
+      DAS.Info("VortexGame", "VortexGame::Start");
+      InitialCubesState initCubeState = new InitialCubesState();
+      initCubeState.InitialCubeRequirements(new StateIntro(), 1, OnIntroComplete);
+      _StateMachine.SetNextState(initCubeState);
+
+      _GamePanel = UIManager.OpenDialog(_GamePanelPrefab).GetComponent<VortexPanel>();
+      CreateDefaultQuitButton();
 
     }
 
     void Update() {
-
+      _StateMachineManager.UpdateAllMachines();
     }
 
     public override void CleanUp() {
+      
+      if (_GamePanel != null) {
+        UIManager.CloseDialogImmediately(_GamePanel);
+      }
+      DestroyDefaultQuitButton();
 
+    }
+
+    public void OnIntroComplete() {
+      DAS.Info("VortexGame", "VortexGame::OnIntroComplete");
     }
   }
 
