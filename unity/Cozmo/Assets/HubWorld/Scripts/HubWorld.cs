a7e22f9db5affc05c40bb84e40b3eb48cc15d1c1
Pohung Chen <pohung@anki.com>
2015-11-13 13:01:49 -0800

    stubbing out hub world

---
+++ unity/Cozmo/Assets/HubWorld/Scripts/HubWorld.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HubWorld : HubWorldBase {
+
+  [SerializeField]
+  private HubWorldDialog _HubWorldDialogPrefab;
+  private HubWorldDialog _HubWorldDialogInstance;
+
+  private GameBase _MiniGameInstance;
+
+  public override bool LoadHubWorld() {
+    ShowHubWorldDialog();
+    return true;
+  }
+
+  public override bool DestroyHubWorld() {
+    // Deregister events
+    // Destroy dialog if it exists
+    if (_HubWorldDialogInstance != null) {
+      _HubWorldDialogInstance.OnButtonClicked -= OnDevButtonClicked;
+      _HubWorldDialogInstance.CloseDialogImmediately();
+    }
+
+    CloseMiniGame();
+    return true;
+  }
+
+  private void ShowHubWorldDialog() {
+
+    // Create dialog with the game prefabs
+    _HubWorldDialogInstance = UIManager.OpenDialog(_HubWorldDialogPrefab) as HubWorldDialog;
+
+  }
+
+  private void OnDevButtonClicked(GameBase miniGameClicked) {
+    _HubWorldDialogInstance.OnButtonClicked -= OnDevButtonClicked;
+    _HubWorldDialogInstance.CloseDialog();
+
+    GameObject newMiniGameObject = GameObject.Instantiate(miniGameClicked.gameObject);
+    _MiniGameInstance = newMiniGameObject.GetComponent<GameBase>();
+    _MiniGameInstance.OnMiniGameQuit += HandleMiniGameQuit;
+  }
+
+  private void HandleMiniGameQuit() {
+    CloseMiniGame();
+    ShowHubWorldDialog();
+  }
+
+  private void CloseMiniGame() {
+    // Destroy game if it exists
+    if (_MiniGameInstance != null) {
+      _MiniGameInstance.CleanUp();
+      Destroy(_MiniGameInstance.gameObject);
+    }
+  }
+
+}
---
+++ unity/Cozmo/Assets/HubWorld/Scripts/UI/HubWorldDialog.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HubWorldDialog : BaseDialog {
+
+  public delegate void ButtonClickedHandler(GameBase miniGameClicked);
+
+  public event ButtonClickedHandler OnButtonClicked;
+
+  protected override void CleanUp() {
+
+  }
+
+  protected override void ConstructCloseAnimation(DG.Tweening.Sequence closeAnimation) {
+
+  }
+
+  private void HandleOnDevButtonClicked(GameBase minigame) {
+    if (OnButtonClicked != null) {
+      OnButtonClicked(minigame);
+    }
+  }
+}
