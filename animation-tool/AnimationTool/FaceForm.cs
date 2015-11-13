138676b20382581fbe30ebfa31b6427e6bb19cba
Lee Crippen <lee@anki.com>
2015-11-13 13:59:42 -0800

    Updating faceForm to also live update based on face sliders

--- animation-tool/AnimationTool/FaceForm.cs
+++ animation-tool/AnimationTool/FaceForm.cs
@@ -97,6 +97,10 @@ namespace AnimationTool
                 }
             }
 
+            faceAngleNew.OnChanged += OnChanged;
+            faceScale.OnChanged += OnChanged;
+            faceCenter.OnChanged += OnChanged;
+
             Reset();
         }
 
