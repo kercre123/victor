f3788804349c8f68125539576817da6897121395
Trevor Dasch <trevor@anki.com>
2015-11-16 17:13:16 -0800

    Modified DAS to accept an object, type, or string, which will be used to determine the eventName. Also created an IDAS interface so an instance of the logger can be retrieved by a static class to simplify logging code.

--- unity/Cozmo/Assets/Scripts/Channel/DAS.cs
+++ unity/Cozmo/Assets/Scripts/Channel/DAS.cs
@@ -3,6 +3,19 @@ using System.Runtime.InteropServices;
 using UnityEngine;
 
 #if (!UNITY_EDITOR && (UNITY_ANDROID || UNITY_IPHONE || UNITY_STANDALONE)) || ANIMATION_TOOL
+
+public interface IDAS {
+  void Event(string eventValue, UnityEngine.Object context = null);
+
+  void Error(object eventObject, string eventValue, UnityEngine.Object context = null);
+
+  void Warn(object eventObject, string eventValue, UnityEngine.Object context = null);
+
+  void Info(object eventObject, string eventValue, UnityEngine.Object context = null);
+
+  void Debug(object eventObject, string eventValue, UnityEngine.Object context = null);
+}
+
 public static class DAS {
 
   public delegate void DASHandler(string eventName, string eventValue, UnityEngine.Object context);
@@ -12,8 +25,8 @@ public static class DAS {
   public static event DASHandler OnInfoLogged;
   public static event DASHandler OnDebugLogged;
 
-  public static void Event(string eventName, string eventValue, UnityEngine.Object context = null) {
-
+  public static void Event(object eventObject, string eventValue, UnityEngine.Object context = null) {
+    string eventName = GetEventName(eventObject);
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
 
@@ -25,8 +38,8 @@ public static class DAS {
 #endif
   }
  
-  public static void Error(string eventName, string eventValue, UnityEngine.Object context = null) {
-
+  public static void Error(object eventObject, string eventValue, UnityEngine.Object context = null) {
+    string eventName = GetEventName(eventObject);
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
 
@@ -38,8 +51,8 @@ public static class DAS {
 #endif
   }
 
-  public static void Warn(string eventName, string eventValue, UnityEngine.Object context = null) {
-
+  public static void Warn(object eventObject, string eventValue, UnityEngine.Object context = null) {
+    string eventName = GetEventName(eventObject);
 #if ANIMATION_TOOL
       Console.Out.WriteLine(eventName + ": " + eventValue);
 
@@ -51,8 +64,8 @@ public static class DAS {
 #endif
     }
 
-    public static void Info(string eventName, string eventValue, UnityEngine.Object context = null) {
-
+  public static void Info(object eventObject, string eventValue, UnityEngine.Object context = null) {
+    string eventName = GetEventName(eventObject);
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
 
@@ -64,8 +77,8 @@ public static class DAS {
 #endif
   }
 
-  public static void Debug(string eventName, string eventValue, UnityEngine.Object context = null) {
-
+  public static void Debug(object eventObject, string eventValue, UnityEngine.Object context = null) {
+    string eventName = GetEventName(eventObject);
 #if ANIMATION_TOOL
     Console.Out.WriteLine(eventName + ": " + eventValue);
 
@@ -76,6 +89,60 @@ public static class DAS {
     }
 #endif
   }
+
+  private static string GetEventName(object eventObject)
+  {
+    if (eventObject == null) {
+      return string.Empty;
+    }
+    var eventString = eventObject as string;
+    if (eventString != null) {
+      return eventString;
+    }
+
+    var eventType = eventObject as System.Type;
+    if (eventType != null) {
+      return eventType.Name;
+    }
+
+    return eventType.GetType().Name;
+  }
+
+  public static IDAS GetInstance(System.Type type) {
+    // this could be modified to do a dictionary lookup if it was a concern
+    // that multiple instance for the same type would be retrieved
+    return new DASInstance(type);
+  }
+
+  private sealed class DASInstance : IDAS
+  {
+    private string _EventName;
+
+    public DASInstance(System.Type type) {
+      _EventName = type.Name;
+    }
+
+    public void Event(string eventValue, UnityEngine.Object context = null) {
+      DAS.Event(_EventName, eventValue, context);
+    }
+
+    public void Error(object eventObject, string eventValue, UnityEngine.Object context = null) {
+      DAS.Error(_EventName, eventValue, context);      
+    }
+
+    public void Warn(object eventObject, string eventValue, UnityEngine.Object context = null) {
+      DAS.Warn(_EventName, eventValue, context);      
+    }
+
+    public void Info(object eventObject, string eventValue, UnityEngine.Object context = null) {
+      DAS.Info(_EventName, eventValue, context);      
+    }
+
+    public void Debug(object eventObject, string eventValue, UnityEngine.Object context = null) {
+      DAS.Debug(_EventName, eventValue, context);            
+    }
+  }
+
 
   // TODO: __Internal only works on certain platforms (iOS, but not Android, etc)
   [DllImport("__Internal")]
