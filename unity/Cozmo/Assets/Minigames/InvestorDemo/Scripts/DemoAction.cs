32bcaec19839554696f1dd3aa8e07eae2d3df459
Pohung Chen <pohung@anki.com>
2015-11-17 19:26:48 -0800

    using the new challenge system

---
+++ unity/Cozmo/Assets/Minigames/InvestorDemo/Scripts/DemoAction.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace InvestorDemo {
+  public class DemoAction {
+    public DemoAction(string animationName, Anki.Cozmo.BehaviorType behavior) {
+      AnimationName = animationName;
+      Behavior = behavior;
+    }
+
+    public string AnimationName { get; set; }
+
+    public Anki.Cozmo.BehaviorType Behavior { get; set; }
+
+  }
+}
--- unity/Cozmo/Assets/Minigames/InvestorDemo/Scripts/InvestorDemoGame.cs
+++ unity/Cozmo/Assets/Minigames/InvestorDemo/Scripts/InvestorDemoGame.cs
@@ -3,76 +3,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 
-public class InvestorDemoGame : GameBase {
+namespace InvestorDemo {
 
-  public class DemoAction {
-    public DemoAction(string animationName, Anki.Cozmo.BehaviorType behavior) {
-      AnimationName = animationName;
-      Behavior = behavior;
+  public class InvestorDemoGame : GameBase {
+
+    private List<DemoAction> _DemoActions = new List<DemoAction>();
+    private int _ActionIndex = -1;
+
+    [SerializeField]
+    private UnityEngine.UI.Text _CurrentActionText;
+
+    [SerializeField]
+    private Button _NextButton;
+
+    [SerializeField]
+    private Button _PrevButton;
+
+    void Start() {
+      CreateDefaultQuitButton();
+
+      _DemoActions.Add(new DemoAction(AnimationName.kInvestorDemo1, Anki.Cozmo.BehaviorType.NoneBehavior));
+      _DemoActions.Add(new DemoAction(AnimationName.kInvestorDemo2, Anki.Cozmo.BehaviorType.NoneBehavior));
+      _DemoActions.Add(new DemoAction("", Anki.Cozmo.BehaviorType.LookAround));
+
+      NextAction();
+
+      _NextButton.onClick.AddListener(NextAction);
+      _NextButton.onClick.AddListener(PrevAction);
     }
 
-    public string AnimationName { get; set; }
+    private void OnAnimationDone(bool success) {
+      NextAction();
+    }
 
-    public Anki.Cozmo.BehaviorType Behavior { get; set; }
+    private void NextAction() {
+      _ActionIndex++;
+      DoCurrentAction();
+    }
 
-  }
+    private void PrevAction() {
+      _ActionIndex--;
+      DoCurrentAction();
+    }
 
-  private List<DemoAction> _DemoActions = new List<DemoAction>();
-  private int _ActionIndex = -1;
+    private void DoCurrentAction() {
+      if (_ActionIndex > 0 && _ActionIndex < _DemoActions.Count) {
+        if (!_DemoActions[_ActionIndex].AnimationName.Equals("")) {
+          CurrentRobot.SendAnimation(_DemoActions[_ActionIndex].AnimationName, OnAnimationDone);
+          CurrentRobot.ExecuteBehavior(Anki.Cozmo.BehaviorType.NoneBehavior);
+          _CurrentActionText.text = "Playing Animation: " + _DemoActions[_ActionIndex].AnimationName;
 
-
-  [SerializeField]
-  private UnityEngine.UI.Text _CurrentActionText;
-
-  [SerializeField]
-  private Button _NextButton;
-
-  [SerializeField]
-  private Button _PrevButton;
-
-  void Start() {
-    CreateDefaultQuitButton();
-
-    _DemoActions.Add(new DemoAction(AnimationName.kInvestorDemo1, Anki.Cozmo.BehaviorType.NoneBehavior));
-    _DemoActions.Add(new DemoAction(AnimationName.kInvestorDemo2, Anki.Cozmo.BehaviorType.NoneBehavior));
-    _DemoActions.Add(new DemoAction("", Anki.Cozmo.BehaviorType.LookAround));
-
-    NextAction();
-
-    _NextButton.onClick.AddListener(NextAction);
-    _NextButton.onClick.AddListener(PrevAction);
-  }
-
-  private void OnAnimationDone(bool success) {
-    NextAction();
-  }
-
-  private void NextAction() {
-    _ActionIndex++;
-    DoCurrentAction();
-  }
-
-  private void PrevAction() {
-    _ActionIndex--;
-    DoCurrentAction();
-  }
-
-  private void DoCurrentAction() {
-    if (_ActionIndex > 0 && _ActionIndex < _DemoActions.Count) {
-      if (!_DemoActions[_ActionIndex].AnimationName.Equals("")) {
-        CurrentRobot.SendAnimation(_DemoActions[_ActionIndex].AnimationName, OnAnimationDone);
-        CurrentRobot.ExecuteBehavior(Anki.Cozmo.BehaviorType.NoneBehavior);
-        _CurrentActionText.text = "Playing Animation: " + _DemoActions[_ActionIndex].AnimationName;
-
+        }
+        else {
+          CurrentRobot.ExecuteBehavior(_DemoActions[_ActionIndex].Behavior);
+          _CurrentActionText.text = "Doing: " + _DemoActions[_ActionIndex].Behavior.ToString();
+        }
       }
-      else {
-        CurrentRobot.ExecuteBehavior(_DemoActions[_ActionIndex].Behavior);
-        _CurrentActionText.text = "Doing: " + _DemoActions[_ActionIndex].Behavior.ToString();
-      }
+    }
+
+    public override void CleanUp() {
+      DestroyDefaultQuitButton();
     }
   }
 
-  public override void CleanUp() {
-    DestroyDefaultQuitButton();
-  }
 }
