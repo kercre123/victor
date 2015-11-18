3cd09ffece4ef47092e4d023f6d510393463b6d1
Ivy Ngo <ivy@anki.com>
2015-11-18 15:42:50 -0800

    Setting up data for HubWorld to show

---
+++ unity/Cozmo/Assets/Minigames/VisionTraining/Scripts/VisionTrainingConfig.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisionTrainingConfig : MinigameConfigBase {
+
+  // Use this for initialization
+  void Start() {
+	
+  }
+	
+  // Update is called once per frame
+  void Update() {
+	
+  }
+}
