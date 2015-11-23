2fec199382de80398d557fc6767a62b0227d48c1
Pohung Chen <pohung@anki.com>
2015-11-23 09:48:26 -0800

    increase forgiveness

--- unity/Cozmo/Assets/Minigames/FollowCube/Scripts/FollowCubeConfig.cs
+++ unity/Cozmo/Assets/Minigames/FollowCube/Scripts/FollowCubeConfig.cs
@@ -3,13 +3,4 @@ using System.Collections;
 
 public class FollowCubeConfig : MinigameConfigBase {
 
-  // Use this for initialization
-  void Start() {
-	
-  }
-	
-  // Update is called once per frame
-  void Update() {
-	
-  }
 }
