42124205044a3fe2e5c15a13246a1143649dd699
Ivy Ngo <ivy@anki.com>
2015-11-25 11:15:09 -0800

    Adding bob animation to hub world buttons

--- unity/Cozmo/Assets/HubWorld/Scripts/UI/HubWorldUnlockedButton.cs
+++ unity/Cozmo/Assets/HubWorld/Scripts/UI/HubWorldUnlockedButton.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using DG.Tweening;
 
 namespace Cozmo.HubWorld {
   public class HubWorldUnlockedButton : HubWorldButton {
@@ -20,6 +21,22 @@ namespace Cozmo.HubWorld {
           _ChallengeIconImage.gameObject.SetActive(false);
         }
       }
+
+      StartCoroutine(DelayedBobAnimation());
+    }
+
+    private IEnumerator DelayedBobAnimation() {
+      float delay = Random.Range(0f, 1f);
+      yield return new WaitForSeconds(delay);
+
+      // Start a bobbing animation that plays forever
+      float duration = Random.Range(1.5f, 2f);
+      float yOffset = Random.Range(10f, 15f);
+      yOffset = Random.Range(0f, 1f) > 0.5f ? yOffset : -yOffset;
+      Sequence bobbingSequence = DOTween.Sequence();
+      bobbingSequence.SetLoops(-1, LoopType.Yoyo); 
+      bobbingSequence.Append(transform.DOLocalMoveY(transform.localPosition.y - yOffset, duration).SetEase(Ease.InOutSine));
+      bobbingSequence.Play();
     }
   }
 }
