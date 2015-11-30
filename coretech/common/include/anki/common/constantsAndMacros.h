ef8fab5e39d2cdeb34377c372afb0abcc8ab12b8
Molly Jameson <molly@anki.com>
2015-11-30 10:26:16 -0800

    adding back an empty header so less files need to change

---
+++ coretech/common/include/anki/common/constantsAndMacros.h
@@ -0,0 +1,12 @@
+/**
+* trying to reduce this to one file without breaking everything
+*
+**/
+
+#ifndef ANKICORETECH_COMMON_CONSTANTSANDMACROSOLD_H_
+#define ANKICORETECH_COMMON_CONSTANTSANDMACROSOLD_H_
+
+#include "util/math/constantsAndMacros.h"
+
+
+#endif /* ANKICORETECH_COMMON_CONSTANTSANDMACROSOLD_H_ */
