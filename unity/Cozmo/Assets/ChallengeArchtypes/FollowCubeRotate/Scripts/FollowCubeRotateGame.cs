14768ecf8ef4f7ae3dc463e93eee173f53ed0ffc
Pohung Chen <pohung@anki.com>
2015-12-03 10:27:49 -0800

    stubbing out the new rotation challenge

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/FollowCubeRotate/Scripts/FollowCubeRotateGame.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FollowCubeRotate {
+  public class FollowCubeRotateGame : GameBase {
+
+    private StateMachineManager _StateMachineManager = new StateMachineManager();
+    private StateMachine _StateMachine = new StateMachine();
+
+    public override void LoadMinigameConfig(MinigameConfigBase minigameConfig) {
+
+    }
+
+    void Start() {
+      _StateMachine.SetGameRef(this);
+      _StateMachineManager.AddStateMachine("ForwardBackwardStateMachine", _StateMachine);
+      InitialCubesState initCubeState = new InitialCubesState();
+      initCubeState.InitialCubeRequirements(new RotateWithTarget(), 1, null);
+      _StateMachine.SetNextState(initCubeState);
+      CurrentRobot.SetVisionMode(Anki.Cozmo.VisionMode.DetectingFaces, false);
+      CurrentRobot.SetVisionMode(Anki.Cozmo.VisionMode.DetectingMarkers, true);
+
+      CreateDefaultQuitButton();
+    }
+
+    void Update() {
+      _StateMachineManager.UpdateAllMachines();
+    }
+
+    public LightCube LightCubeTarget() {
+      foreach (ObservedObject visibleObj in CurrentRobot.VisibleObjects) {
+        if (visibleObj is LightCube) {
+          return (LightCube)visibleObj;
+        }
+      }
+      return null;
+    }
+
+    public override void CleanUp() {
+      DestroyDefaultQuitButton();
+    }
+  }
+
+}
---
+++ unity/Cozmo/Assets/ChallengeArchtypes/FollowCubeRotate/Scripts/States/RotateWithTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace FollowCubeRotate {
+  public class RotateWithTarget : State {
+    public override void Enter() {
+      base.Enter();
+    }
+
+    public override void Update() {
+      base.Update();
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+  }
+}
