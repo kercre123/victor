02edb299ccb38af292da7ae8edc162d03240c9e4
Pohung Chen <pohung@anki.com>
2015-12-08 13:46:25 -0800

    DEM COLORS!

--- unity/Cozmo/Assets/ChallengeArchtypes/FollowCube/Scripts/States/FollowCubeTurnLeftState.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/FollowCube/Scripts/States/FollowCubeTurnLeftState.cs
@@ -24,6 +24,7 @@ namespace FollowCube {
 
       if (_CurrentTarget == null && _CurrentRobot.VisibleObjects.Count > 0) {
         _CurrentTarget = _CurrentRobot.VisibleObjects[0] as LightCube;
+        _CurrentTarget.SetLEDs(CozmoPalette.ColorToUInt(Color.white));
       }
 
       _CurrentRobot.DriveWheels(0.0f, 0.0f);
