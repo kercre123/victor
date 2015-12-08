ab6cd19b87f8cd086b837136def4b6dcc88ec0a5
Trevor Dasch <trevor@anki.com>
2015-12-07 19:07:35 -0800

    Created New State HelpCozmoPickupState, modified HelpCozmoStackState so you have to guide cozmo to the cube you intend to drop on.

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/StackTraining/Scripts/States/HelpCozmoPickupState.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace StackTraining {
+  public class HelpCozmoPickupState : State {
+
+    private Bounds _BottomCubeBounds = new Bounds(new Vector3(100f, 0f, CozmoUtil.kBlockLengthMM * 0.5f), new Vector3(100f, 20f, 20f));
+
+    private bool _Moving = false;
+
+    private StackTrainingGame _Game;
+
+    public override void Enter() {
+      base.Enter();
+      _Game = _StateMachine.GetGame() as StackTrainingGame;
+
+      _CurrentRobot.SetHeadAngle(0f);
+      _CurrentRobot.SetLiftHeight(0f);
+    }
+
+    public override void Update() {
+      base.Update();
+
+      var bottomCube = _Game.BottomCube;
+      var topCube = _Game.TopCube;
+
+      if (bottomCube == null || topCube == null) {
+        _Game.RaiseMiniGameLose();
+        return;
+      }
+
+      Vector3 bottomCubePosition = _CurrentRobot.WorldToCozmo(bottomCube.WorldPosition);
+
+      if (_Moving) {
+        return;
+      }
+
+      if (_BottomCubeBounds.Contains(bottomCubePosition)) {
+        bottomCube.SetLEDs(Color.white);
+
+        _Moving = true;
+        _CurrentRobot.PickupObject(bottomCube, callback: (success) => {
+          if(success) {
+            _StateMachine.SetNextState(new HelpCozmoStackState());
+          }
+          else {
+            if(_Game.TryDecrementAttempts()) {
+              _CurrentRobot.SetLiftHeight(0.0f);
+            }
+            else {
+              HandleFailed();
+            }
+          }
+          _Moving = false;
+        });
+      }
+      else {
+        bottomCube.SetLEDs(Color.red);
+        topCube.TurnLEDsOff();
+      }
+    }
+
+    private void HandleFailed() {
+      AnimationState animState = new AnimationState();
+      animState.Initialize(AnimationName.kShocked, HandleLoseComplete);
+      _StateMachine.SetNextState(animState);
+    }
+      
+    private void HandleLoseComplete(bool success) {
+      _Game.RaiseMiniGameLose();
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+
+  }
+
+}
