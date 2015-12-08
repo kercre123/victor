18e767a0c1c56ddd261327729737cc90bf22924a
Trevor Dasch <trevor@anki.com>
2015-12-07 16:19:26 -0800

    Removed bounds from cube lifting challenge, added end state where cozmo picks up cube

--- unity/Cozmo/Assets/ChallengeArchtypes/CubeLifting/Scripts/States/FollowCubeUpDownState.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/CubeLifting/Scripts/States/FollowCubeUpDownState.cs
@@ -9,16 +9,11 @@ namespace CubeLifting {
     private int _SelectedCubeId = -1;
 
     private float _CubeInvisibleTimer = 0f;
-    private float _CubeOutOfBoundsTimer = 0f;
 
     private bool _StartedLifting = false;
 
-    private Bounds _VisionBounds = new Bounds(new Vector3(150.0f, 0.0f, 100.0f ), new Vector3(50.0f, 50.0f, 200.0f));
-
     private bool _Up;
     private bool _RaiseLift;
-
-    private float _LastHeight;
 
     private List<CubeLiftingSetting> _Settings;
     private int _Index;
@@ -38,7 +33,6 @@ namespace CubeLifting {
       if (_SelectedCubeId == -1) {
         _SelectedCubeId = (_StateMachine.GetGame() as CubeLiftingGame).PickCube();
       }
-      _LastHeight = _Up ? 0 : 1;
     }
 
     public override void Update() {
@@ -59,41 +53,30 @@ namespace CubeLifting {
       else {
         _CubeInvisibleTimer += Time.deltaTime;
       }
-
-
+        
       var cube = _CurrentRobot.LightCubes[_SelectedCubeId];
 
       float height = Mathf.InverseLerp(30, 150, cube.WorldPosition.z);
 
-      if (_CubeInvisibleTimer > 1.0f) {
+      if (_CubeInvisibleTimer > 0.5f) {
         cube.SetLEDs(CozmoPalette.ColorToUInt(Color.white));
-      }
-      else if (_VisionBounds.Contains(_CurrentRobot.WorldToCozmo(cube.WorldPosition))) { 
-        if (ValidateHeightChange(height)) {
-          cube.SetLEDs(CozmoPalette.ColorToUInt(_Up ? Color.blue : Color.red));
-
-          if (_RaiseLift) {
-            // go to 80% so it doesn't block our view
-            _CurrentRobot.SetLiftHeight(height * 0.8f);
-          }
-
-          _CurrentRobot.SetHeadAngle(height);
-
-          _StartedLifting = true;
-        }
-        else {
-          cube.SetLEDs(CozmoPalette.ColorToUInt(Color.yellow));
-        }
-        _CubeOutOfBoundsTimer = 0f;
       }
       else {
-        cube.SetLEDs(CozmoPalette.ColorToUInt(Color.white));
-        _CubeOutOfBoundsTimer += Time.deltaTime;
+        cube.SetLEDs(CozmoPalette.ColorToUInt(_Up ? Color.blue : Color.red));
+
+        if (_RaiseLift) {
+          // go to 80% so it doesn't block our view
+          _CurrentRobot.SetLiftHeight(height * 0.8f);
+        }
+
+        _CurrentRobot.SetHeadAngle(height);
+
+        _StartedLifting = true;
       }
+        
+      if (_CubeInvisibleTimer > 5.0f && _StartedLifting) {
 
-      if ((_CubeInvisibleTimer > 5.0f || _CubeOutOfBoundsTimer > 5.0f) && _StartedLifting) {
-
-        var game = (CubeLiftingGame)_StateMachine.GetGame();
+        var game = _StateMachine.GetGame();
 
         if (game.TryDecrementAttempts()) {
           AnimationState animState = new AnimationState();
@@ -111,10 +94,7 @@ namespace CubeLifting {
       if (ReachedMoveEnd()) {
 
         if (_Settings == null || _Index + 1 >= _Settings.Count) {
-          _CurrentRobot.LightCubes[_SelectedCubeId].TurnLEDsOff();
-          AnimationState animState = new AnimationState();
-          animState.Initialize(AnimationName.kMajorWin, HandleWinAnimationDone);
-          _StateMachine.SetNextState(animState);
+          _StateMachine.SetNextState(new PickupCubeState(_SelectedCubeId));
         }
         else {
           AnimationState animState = new AnimationState();
@@ -125,26 +105,8 @@ namespace CubeLifting {
       }
     }
 
-    private bool ValidateHeightChange(float height) {
-      bool result = false;
-      if (_Up) {
-        result = height >= _LastHeight - 0.05f && height <= _LastHeight + 0.2f;
-      }
-      else {
-        result = height <= _LastHeight + 0.05f && height >= _LastHeight - 0.2f;
-      }
-      if (result) {
-        _LastHeight = height;
-      }
-      return result;
-    }
-
     private bool ReachedMoveEnd() {
-      return (_Up && _CurrentRobot.HeadAngle > CozmoUtil.kMaxHeadAngle * 0.9f * Mathf.Deg2Rad) || (!_Up && _CurrentRobot.HeadAngle < 0.05f);
-    }
-
-    private void HandleWinAnimationDone(bool success) {
-      _StateMachine.GetGame().RaiseMiniGameWin();
+      return (_Up && _CurrentRobot.HeadAngle > CozmoUtil.kMaxHeadAngle * 0.9f * Mathf.Deg2Rad) || (!_Up && _CurrentRobot.HeadAngle < 0.07f);
     }
 
     private void HandleStateCompleteAnimationDone(bool success) {
