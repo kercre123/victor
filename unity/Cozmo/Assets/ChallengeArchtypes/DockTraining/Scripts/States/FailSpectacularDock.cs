21ec40487b2a3e50d220d924df5776c60bf459e5
Pohung Chen <pohung@anki.com>
2015-12-08 16:57:18 -0800

    Dock Training Return to starting pose on fail

--- unity/Cozmo/Assets/ChallengeArchtypes/DockTraining/Scripts/States/FailSpectacularDock.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/DockTraining/Scripts/States/FailSpectacularDock.cs
@@ -4,13 +4,22 @@ using System.Collections;
 namespace DockTraining {
 
   public class FailSpectacularDock : State {
+
+    private DockTrainingGame _GameInstance;
+
     public override void Enter() {
       base.Enter();
+      _GameInstance = _StateMachine.GetGame() as DockTrainingGame;
     }
 
     public override void Update() {
       base.Update();
       _CurrentRobot.DriveWheels(25.0f, 10.0f);
+    }
+
+    public override void Exit() {
+      base.Exit();
+      _CurrentRobot.DriveWheels(0.0f, 0.0f);
     }
 
     private void PlayShockedAnimation() {
@@ -20,7 +29,10 @@ namespace DockTraining {
     }
 
     private void HandleAnimationDone(bool success) {
-      _CurrentRobot.DriveWheels(0.0f, 0.0f);
+      _CurrentRobot.GotoPose(_GameInstance.StartingPosition(), _GameInstance.StartingRotation(), HandleBackToStartPos);
+    }
+
+    private void HandleBackToStartPos(bool success) {
       _StateMachine.SetNextState(new WaitForTargetState());
     }
   }
