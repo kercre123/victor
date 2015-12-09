69e4911d67b286a450e017b2df3a79c6ea5f89ab
Ivy Ngo <ivy@anki.com>
2015-12-08 17:24:54 -0800

    Removing tweening from progress bar and using our own

--- unity/Cozmo/Assets/SharedAssets/Scripts/UI/BarWidgets/ProgressBar.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/BarWidgets/ProgressBar.cs
@@ -1,10 +1,11 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using DG.Tweening;
 
 namespace Cozmo {
   namespace UI {
     public class ProgressBar : MonoBehaviour {
+
+      private const float kTweenDuration = 3.25f;
 
       [SerializeField]
       private Image _FilledForegroundImage;
@@ -18,54 +19,48 @@ namespace Cozmo {
       [SerializeField]
       private Color _IncreasingColor;
 
+      private float _StartProgress = 0f;
       private float _TargetProgress = 0f;
-
-      private Sequence _TweenToTargetSequence;
-      private bool _IsDirty = false;
+      private float _TimePassedSeconds = 0f;
 
       private void Start() {
         // Force filled type so that the progress bar works.
         _FilledForegroundImage.type = Image.Type.Filled;
-
-        if (!_IsDirty) {
-          ResetProgress();
-        }
+        _StartProgress = 0;
+        _FilledForegroundImage.fillAmount = 0;
+        _TimePassedSeconds = 0;
       }
 
       private void Update() {
-        if (_IsDirty) {
-          _IsDirty = false;
+        if (_FilledForegroundImage.fillAmount != _TargetProgress) {
+          _TimePassedSeconds += Time.deltaTime;
+          if (_TimePassedSeconds > kTweenDuration) {
+            _TimePassedSeconds = kTweenDuration;
+          }
+          _FilledForegroundImage.fillAmount = EaseOutQuad(_TimePassedSeconds, _StartProgress, _TargetProgress, kTweenDuration);
 
-          KillTweens();
-
-          if (_TargetProgress < _FilledForegroundImage.fillAmount) {
+          if (_TargetProgress > _FilledForegroundImage.fillAmount) {
+            _FilledForegroundImage.color = _IncreasingColor;
+          }
+          else if (_TargetProgress < _FilledForegroundImage.fillAmount) {
             _FilledForegroundImage.color = _DecreasingColor;
           }
           else {
-            _FilledForegroundImage.color = _IncreasingColor;
+            _FilledForegroundImage.color = _NormalColor;
+            _StartProgress = _FilledForegroundImage.fillAmount;
+            _TimePassedSeconds = 0;
           }
-
-          // TODO: Don't hardcode this
-          _TweenToTargetSequence = DOTween.Sequence();
-          Tweener tween = _FilledForegroundImage.DOFillAmount(_TargetProgress, 0.25f);
-          tween.SetEase(Ease.OutQuad);
-          _TweenToTargetSequence.Append(tween);
-          _TweenToTargetSequence.OnComplete(HandleTweenFinishedCallback);
-          _TweenToTargetSequence.Play();
         }
       }
 
-      private void HandleTweenFinishedCallback() {
-        _FilledForegroundImage.color = _NormalColor;
-      }
-
-      private void OnDestroy() {
-        KillTweens();
+      private float EaseOutQuad(float currentTimeSeconds, float startValue, float changeInValue, float duration) {
+        currentTimeSeconds /= duration;
+        return -changeInValue * currentTimeSeconds * (currentTimeSeconds - 2) + startValue;
       }
 
       public void ResetProgress() {
-        KillTweens();
-        _IsDirty = false;
+        _TimePassedSeconds = 0;
+        _StartProgress = 0;
         _TargetProgress = 0;
         _FilledForegroundImage.fillAmount = 0;
       }
@@ -78,14 +73,6 @@ namespace Cozmo {
         float newProgress = Mathf.Clamp(progress, 0f, 1f);
         if (newProgress != _TargetProgress) {
           _TargetProgress = newProgress;
-          _IsDirty = true;
-        }
-      }
-
-      private void KillTweens() {
-        if (_TweenToTargetSequence != null) {
-          _TweenToTargetSequence.Kill();
-          _TweenToTargetSequence = null;
         }
       }
     }
