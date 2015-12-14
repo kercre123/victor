f82969307292f85002e7ee4245d802845addbf9a
Pohung Chen <pohung@anki.com>
2015-12-14 12:07:11 -0800

    result stages

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/SpeedTap/Scripts/States/SpeedTapPlayerLoses.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpeedTap {
+
+  public class SpeedTapPlayerLoses : State {
+
+    private SpeedTapGame _SpeedTapGame = null;
+
+    public override void Enter() {
+      base.Enter();
+      _SpeedTapGame = _StateMachine.GetGame() as SpeedTapGame;
+      _SpeedTapGame.PlayerBlock.SetLEDs(Color.red);
+      _SpeedTapGame.CozmoBlock.SetLEDs(Color.black);
+    }
+
+    public override void Update() {
+      base.Update();
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+  }
+
+}
---
+++ unity/Cozmo/Assets/ChallengeArchtypes/SpeedTap/Scripts/States/SpeedTapPlayerWins.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+namespace SpeedTap {
+
+  public class SpeedTapPlayerWins : State {
+
+    private SpeedTapGame _SpeedTapGame = null;
+
+    public override void Enter() {
+      base.Enter();
+      _SpeedTapGame = _StateMachine.GetGame() as SpeedTapGame;
+      _SpeedTapGame.PlayerBlock.SetLEDs(Color.white);
+      _SpeedTapGame.CozmoBlock.SetLEDs(Color.black);
+    }
+
+    public override void Update() {
+      base.Update();
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+  }
+
+}
