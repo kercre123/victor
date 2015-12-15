c1889b366439ff8f6a7bf5bdea706c270e637fd6
Jordan Rivas <jordan@anki.com>
2015-12-14 16:33:38 -0800

    Add music functionality & update sound banks
    - Update C# AudioClinet
    - Add SetMusicState() methods to easily set state
    - Add PlayMusic Action to Scripted Sequence
    - Update audio assets SVN commit version

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/ScriptedSequences/Actions/PlayMusic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Anki.Cozmo.Audio;
+
+namespace ScriptedSequences.Actions {
+  public class PlayMusic : ScriptedSequenceAction {
+
+    public MusicGroupStates MusicState;
+
+    public bool WaitToEnd = true;
+
+    public override ISimpleAsyncToken Act() {
+
+      GameAudioClient.SetMusicState(MusicState);
+
+      SimpleAsyncToken token = new SimpleAsyncToken();
+      token.Succeed();
+      return token;
+    }
+  }
+}
+
