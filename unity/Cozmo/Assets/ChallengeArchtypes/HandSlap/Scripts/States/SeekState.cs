486d5ec139c8ef99a2b816ca5aa6f9247560ad5a
Ryan Anderson <ryan@anki.com>
2015-12-14 17:17:01 -0800

    Stubs for game states

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/HandSlap/Scripts/States/SeekState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HandSlap {
+  public class SeekState : State {
+
+    private HandSlapGame _HandSlapGame;
+
+    public override void Enter() {
+      base.Enter();
+      //_HandSlapGame = (_StateMachine.GetGame() as HandSlapGame);
+    }
+
+    public override void Update() {
+      base.Update();
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+  }
+}
+
