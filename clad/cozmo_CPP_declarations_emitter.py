cade5448c2e8cba151a0bf1361d687150e83e6d8
Daniel Casner <daniel@anki.com>
2015-12-14 17:08:50 -0800

    Updates for sanitizing CLAD The specialized emitters for Cozmo are now in the Cozmo Engine repository where they belong.
    * tools/anki-util 40dcb50...76c58f9 (2):
     * Merge pull request #178 from anki/sanitize-clad
     * Merge pull request #177 from anki/FilenameHasSuffix

---
+++ clad/cozmo_CPP_declarations_emitter.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+from __future__ import absolute_import
+from __future__ import print_function
+
+import inspect
+import os
+import sys
+import textwrap
+
+def _modify_path():
+    currentpath = os.path.dirname(inspect.getfile(inspect.currentframe()))
+    searchpath = os.path.normpath(os.path.abspath(os.path.join(currentpath, '..', '..')))
+    searchpath = os.path.normpath(os.path.abspath(os.path.realpath(searchpath)))
+    if searchpath not in sys.path:
+        sys.path.insert(0, searchpath)
+    sys.path.insert(0, os.path.join('..', 'tools', 'anki-util', 'tools', 'message-buffers'))
+_modify_path()
+
+from clad import ast
+from clad import clad
+from emitters import CPP_emitter
+
+class UnionDeclarationEmitter(ast.NodeVisitor):
+    "An emitter that generates the handler declaration."
+    
+    def __init__(self, output=sys.stdout, include_extension=None):
+        self.output = output
+    
+    def visit_UnionDecl(self, node):
+        for member in node.members():
+            self.output.write('void Process_{member_name}(const {member_type}& msg);\n'.format(
+                member_name=member.name, member_type=CPP_emitter.cpp_value_type(member.type)))
+
+if __name__ == '__main__':
+    from clad import emitterutil
+    from emitters import CPP_emitter
+    
+    emitterutil.c_main(language='C++', extension='_declarations.def',
+        emitter_types=[UnionDeclarationEmitter],
+        use_inclusion_guards=False)
