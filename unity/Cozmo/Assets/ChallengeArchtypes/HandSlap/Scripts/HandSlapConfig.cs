eb1ce9b1778ac5cfdca69dfbb51403e5c7a1f962
Ryan Anderson <ryan@anki.com>
2015-12-14 17:04:12 -0800

    Added in stubs for Handslap game and config

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/HandSlap/Scripts/HandSlapConfig.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HandSlap {
+
+  public class HandSlapConfig : MinigameConfigBase {
+    public float MinSlapDelay;
+    public float MaxSlapDelay;
+    public int MaxAttempts;
+    public int SuccessGoal;
+  }
+}
---
+++ unity/Cozmo/Assets/ChallengeArchtypes/HandSlap/Scripts/HandSlapGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace HandSlap {
+  
+  public class HandSlapGame : GameBase {
+
+    protected override void Initialize(MinigameConfigBase minigameConfig) {
+    }
+
+    protected override void CleanUpOnDestroy() {
+      
+    }
+  }
+}
