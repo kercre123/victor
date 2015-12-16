5dc8139c6d22e64d914eb75c52058b05ccee6a6f
Bryon Vandiver <unicdk@gmail.com>
2015-12-16 14:51:15 -0800

    Reduce the total space by not providing a full print function inside of the k02, test mode will now only display a number when there is a failure

--- robot/k02_hal/hal/test.cpp
+++ robot/k02_hal/hal/test.cpp
@@ -49,25 +49,17 @@ extern "C" void LeaveFacePrintf(void);
 
 void StartupSelfTest(void) {
   using namespace Anki::Cozmo::HAL;
-  
+
   int result = RunTests();
   
   if (result != ERROR_NONE) {
-    FacePrintf("Fail %03d", (int) result);
-  } else {
-    const uint16_t* SSID = (uint16_t*) 0xFFC;
-    FacePrintf("               OK%2x", *SSID);
+    OLED::ErrorCode(result);
   }
-  
-  TestPause(1000/5);
-  LeaveFacePrintf();
 }
 
 int RunTests(void) {
   using namespace Anki::Cozmo::HAL;
 
-  FacePrintf("Self Test    v08");
-  
   uint8_t imuid = IMU::ReadID();
   
   if (imuid != 0xD1) {
