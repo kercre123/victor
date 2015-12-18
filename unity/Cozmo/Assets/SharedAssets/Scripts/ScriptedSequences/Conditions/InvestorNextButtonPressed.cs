ab970444708f35c66e0198005d6c301b5f302431
Pohung Chen <pohung@anki.com>
2015-12-18 09:40:28 -0800

    button feedback for when waiting for a loop animation to finish

--- unity/Cozmo/Assets/SharedAssets/Scripts/ScriptedSequences/Conditions/InvestorNextButtonPressed.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/ScriptedSequences/Conditions/InvestorNextButtonPressed.cs
@@ -9,12 +9,15 @@ namespace ScriptedSequences.Conditions {
     protected override void EnableChanged(bool enabled) {
       GameObject investorNextButton = ObjectTagRegistryManager.Instance.GetObjectByTag("InvestorNextButton");
       investorNextButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(HandleOnButtonClicked);
+      investorNextButton.transform.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "NEXT";
     }
 
     #endregion
 
     private void HandleOnButtonClicked() {
       IsMet = true;
+      GameObject investorNextButton = ObjectTagRegistryManager.Instance.GetObjectByTag("InvestorNextButton");
+      investorNextButton.transform.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "Waiting...";
     }
   }
 }
