b38db6658209a75881467336d3226fae5c9c1455
Pohung Chen <pohung@anki.com>
2015-12-21 10:50:45 -0800

    investor demo text fixes

--- unity/Cozmo/Assets/SharedAssets/Scripts/ScriptedSequences/Conditions/InvestorNextButtonPressed.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/ScriptedSequences/Conditions/InvestorNextButtonPressed.cs
@@ -7,17 +7,25 @@ namespace ScriptedSequences.Conditions {
     #region implemented abstract members of ScriptedSequenceCondition
 
     protected override void EnableChanged(bool enabled) {
+
       GameObject investorNextButton = ObjectTagRegistryManager.Instance.GetObjectByTag("InvestorNextButton");
-      investorNextButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(HandleOnButtonClicked);
-      investorNextButton.transform.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "NEXT";
+
+      if (enabled) {
+        investorNextButton.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(HandleOnButtonClicked);
+        investorNextButton.transform.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "NEXT";
+      }
+      else {
+        investorNextButton.GetComponent<UnityEngine.UI.Button>().onClick.RemoveListener(HandleOnButtonClicked);
+      }
+
     }
 
     #endregion
 
     private void HandleOnButtonClicked() {
-      IsMet = true;
       GameObject investorNextButton = ObjectTagRegistryManager.Instance.GetObjectByTag("InvestorNextButton");
       investorNextButton.transform.Find("Text").GetComponent<UnityEngine.UI.Text>().text = "Waiting...";
+      IsMet = true;
     }
   }
 }
