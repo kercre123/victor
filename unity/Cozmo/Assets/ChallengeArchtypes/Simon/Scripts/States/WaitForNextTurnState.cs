36427b17ba794c1b957bcfddc1301f247a5e3bb9
Pohung Chen <pohung@anki.com>
2015-12-21 15:22:49 -0800

    simon continue button

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/Simon/Scripts/States/WaitForNextTurnState.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Simon {
+  public class WaitForNextTurnState : State {
+
+    private SimonGame _GameInstance;
+    private int _SequenceCount;
+
+    public WaitForNextTurnState(int sequenceCount) {
+      _SequenceCount = sequenceCount;
+    }
+
+    public override void Enter() {
+      base.Enter();
+      _GameInstance = _StateMachine.GetGame() as SimonGame;
+      SimonGamePanel continueButton = _GameInstance.ShowHowToPlaySlide("ContinueButton").GetComponent<SimonGamePanel>();
+      continueButton.EnableContinueButton(true);
+      continueButton.OnContinueButtonPressed += HandleContinuePressed;
+    }
+
+    private void HandleContinuePressed() {
+      _StateMachine.SetNextState(new CozmoSetSimonState(_SequenceCount));
+    }
+
+    public override void Exit() {
+      base.Exit();
+    }
+
+  }
+}
