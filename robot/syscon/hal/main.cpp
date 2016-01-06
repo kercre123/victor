8426cc75351651f8bd158a8e8dd473ffc58dfa7e
Bryon Vandiver <unicdk@gmail.com>
2016-01-06 13:15:53 -0800

    Missing files, added flashlight suport

--- robot/syscon/hal/main.cpp
+++ robot/syscon/hal/main.cpp
@@ -50,6 +50,8 @@ void MotorsUpdate(void* userdata) {
     {
       Motors::setPower(i, g_dataToBody.motorPWM[i]);
     }
+
+    Battery::setHeadlight(g_dataToBody.flags & BODY_FLASHLIGHT);
   }
 }
 
