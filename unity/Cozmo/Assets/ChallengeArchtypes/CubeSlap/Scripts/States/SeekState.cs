7a140b3fcaad6f9a0f44df7b8a64303e6a6e5aab
Pohung Chen <pohung@anki.com>
2016-01-07 09:54:38 -0800

    cube slap will drive up to the cube to start

--- unity/Cozmo/Assets/ChallengeArchtypes/CubeSlap/Scripts/States/SeekState.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/CubeSlap/Scripts/States/SeekState.cs
@@ -6,7 +6,7 @@ namespace CubeSlap {
   public class SeekState : State {
 
     private CubeSlapGame _CubeSlapGame;
-    private float _ConfirmDelay = 1.0f;
+    private float _ConfirmDelay = 0.5f;
     public float _FirstSeenTimestamp = -1;
 
     public override void Enter() {
@@ -39,6 +39,10 @@ namespace CubeSlap {
               _StateMachine.SetNextState(new SlapGameState());
             }
           }
+          else if (Vector3.Dot(_CurrentRobot.Forward, (target.WorldPosition - _CurrentRobot.WorldPosition).normalized) > 0.92f) {
+            // the robot is lined up but not close enough to drive toward it to start the game.
+            _CurrentRobot.DriveWheels(15.0f, 15.0f);
+          }
         }
       }
       if (isBad) {
@@ -55,6 +59,7 @@ namespace CubeSlap {
 
     public override void Exit() {
       base.Exit();
+      _CurrentRobot.DriveWheels(0.0f, 0.0f);
     }
   }
 }
