195a2ab6649f5bc8a67fe1ecaa0c2636988e1978
Ryan Anderson <ryan@anki.com>
2016-01-07 18:06:18 -0800

    Initial work on Daily Goals panel and radial Goal Badges

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/DailyGoalsPanel/DailyGoalPanel.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Cozmo.UI;
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DailyGoalPanel : BaseView {
+
+  // TODO: Position GoalBadges procedurally as they are added to the list.
+  private List<GoalBadge> _GoalUIBadges;
+  [SerializeField]
+  private GoalBadge _GoalBadgePrefab;
+
+	// Use this for initialization
+	void Start () {
+    _GoalUIBadges = new List<GoalBadge>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void CreateGoalBadge(string name, int target) {
+    GoalBadge newBadge = UIManager.CreateUIElement(_GoalBadgePrefab.gameObject, this.transform).GetComponent<GoalBadge>();
+    newBadge.Initialize(name,target,0);
+    _GoalUIBadges.Add(newBadge);
+
+  }
+
+  protected override void CleanUp() {
+    for (int i = 0; i < _GoalUIBadges.Count; i++) {
+      //TODO: Dismiss GoalBadges through themselves
+      Destroy(_GoalUIBadges[i].gameObject);
+    }
+    _GoalUIBadges.Clear();
+    
+  }
+}
---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/DailyGoalsPanel/GoalBadge.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Cozmo.UI;
+using Anki.UI;
+using DG.Tweening;
+using System.Collections.Generic;
+
+namespace Cozmo {
+  namespace UI {
+    public class GoalBadge : MonoBehaviour  {
+      [SerializeField]
+      private ProgressBar _GoalProgressBar;
+
+      private int _GoalTarget;
+      private int _GoalCurrent;
+
+      [SerializeField]
+      private AnkiTextLabel _GoalLabel;
+
+      [SerializeField]
+      private Image _GoalIcon;
+
+      public string GoalLabelText {
+        get {
+          return _GoalLabel.text;
+        }
+        set {
+          _GoalLabel.text = value;
+        }
+      }
+
+      public void ResetProgress() {
+        _GoalProgressBar.ResetProgress();
+      }
+
+      public void SetProgress(float progress) {
+        _GoalCurrent = (int)((float)_GoalTarget * progress);
+        _GoalProgressBar.SetProgress(progress);
+      }
+
+      public void SetProgress(int progress) {
+        if (progress > _GoalTarget) {
+          _GoalCurrent = _GoalTarget;
+        }
+        else {
+          _GoalCurrent = progress;
+        }
+        _GoalProgressBar.SetProgress((float)_GoalCurrent/(float)_GoalTarget);
+      }
+
+      public void Initialize(string name, int goal, int currProg) {
+        GoalLabelText = name;
+        _GoalTarget = goal;
+        SetProgress(currProg);
+      }
+
+    	// Use this for initialization
+    	void Start () {
+    	
+    	}
+    	
+    	// Update is called once per frame
+    	void Update () {
+    	
+    	}
+    }
+  }
+}
