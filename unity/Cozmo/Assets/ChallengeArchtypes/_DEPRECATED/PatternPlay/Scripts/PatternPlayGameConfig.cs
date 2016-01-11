4889d032c31d82083a3796365dfbd89c21146949
Ivy Ngo <ivy@anki.com>
2016-01-11 15:27:05 -0800

    Deleting old archetypes

--- unity/Cozmo/Assets/ChallengeArchtypes/_DEPRECATED/PatternPlay/Scripts/PatternPlayGameConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿public class PatternPlayGameConfig : MinigameConfigBase {
-  public override int NumCubesRequired() {
-    return 1;
-  }
-
-  public override int NumPlayersRequired() {
-    return 1;
-  }
-}
