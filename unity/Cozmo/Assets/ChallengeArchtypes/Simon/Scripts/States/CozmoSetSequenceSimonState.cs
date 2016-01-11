b30a5ff07186b32e8042836a0666b118576628be
Ivy Ngo <ivy@anki.com>
2016-01-11 13:26:08 -0800

    Adding delay when Cozmo is picking the same cube

--- unity/Cozmo/Assets/ChallengeArchtypes/Simon/Scripts/States/CozmoSetSequenceSimonState.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/Simon/Scripts/States/CozmoSetSequenceSimonState.cs
@@ -11,6 +11,8 @@ namespace Simon {
     private IList<int> _CurrentSequence;
     private int _SequenceLength;
     private SimonGameSequencePanel _SequenceDisplay;
+    private const float kSequenceWaitDelay = 0.3f;
+    private float _LastSequenceTime = -1;
 
     public override void Enter() {
       base.Enter();
@@ -39,12 +41,27 @@ namespace Simon {
 
     public override void Update() {
       base.Update();
-      _CurrentSequenceIndex++;
-      if (_CurrentSequenceIndex == _CurrentSequence.Count) {
-        _StateMachine.SetNextState(new WaitForPlayerGuessSimonState());
-        return;
+      if (_CurrentSequenceIndex == -1) {
+        // First in sequence
+        LightUpNextCube();
       }
+      else if (_CurrentSequenceIndex >= _CurrentSequence.Count - 1) {
+        // Last in sequence
+        _StateMachine.SetNextState(new WaitForPlayerGuessSimonState());
+      }
+      else {
+        bool differentCube = (_CurrentSequence[_CurrentSequenceIndex]
+                             == _CurrentSequence[_CurrentSequenceIndex + 1]);
+        if (differentCube || Time.time - _LastSequenceTime > kSequenceWaitDelay) {
+          LightUpNextCube();
+        }
+      }
+    }
+
+    private void LightUpNextCube() {
+      _CurrentSequenceIndex++;
       _SequenceDisplay.SetSequenceText(_CurrentSequenceIndex + 1, _SequenceLength);
+      _LastSequenceTime = Time.time;
       _StateMachine.PushSubState(new CozmoTurnToCubeSimonState(GetCurrentTarget(), true));
     }
 
