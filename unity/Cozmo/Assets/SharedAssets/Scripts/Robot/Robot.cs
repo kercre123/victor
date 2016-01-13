34e6e3234ed0631ba539fcdc7a1db17a872e0752
Pohung Chen <pohung@anki.com>
2016-01-13 14:52:35 -0800

    improving progression/friends API

--- unity/Cozmo/Assets/SharedAssets/Scripts/Robot/Robot.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Robot/Robot.cs
@@ -157,8 +157,6 @@ public class Robot : IDisposable {
 
   public List<Face> Faces { get; private set; }
 
-  public int[] ProgressionStats { get; private set; }
-
   public int FriendshipPoints { get; private set; }
 
   public int FriendshipLevel { get; private set; }
@@ -216,6 +214,8 @@ public class Robot : IDisposable {
   }
 
   private int _LastHeadTrackingObjectID;
+
+  private int[] ProgressionStats = new int[(int)Anki.Cozmo.ProgressionStatType.Count];
 
   public string CurrentBehaviorString { get; set; }
 
@@ -545,6 +545,21 @@ public class Robot : IDisposable {
     ComputeFriendshipLevel();
   }
 
+  public string GetFriendshipLevelName(int friendshipLevel) {
+    FriendshipLevelConfig levelConfig = RobotEngineManager.Instance.GetFriendshipLevelConfig();
+    return levelConfig.FriendshipLevels[friendshipLevel].FriendshipLevelName;
+  }
+
+  public int GetFriendshipLevelByName(string friendshipName) {
+    FriendshipLevelConfig levelConfig = RobotEngineManager.Instance.GetFriendshipLevelConfig();
+    for (int i = 0; i < levelConfig.FriendshipLevels.Length; ++i) {
+      if (levelConfig.FriendshipLevels[i].FriendshipLevelName == friendshipName) {
+        return i;
+      }
+    }
+    return -1;
+  }
+
   private void ComputeFriendshipLevel() {
     FriendshipLevelConfig levelConfig = RobotEngineManager.Instance.GetFriendshipLevelConfig();
     if (FriendshipLevel == levelConfig.FriendshipLevels.Length) {
@@ -565,6 +580,10 @@ public class Robot : IDisposable {
   #endregion
 
   #region Progression and Mood Stats
+
+  public int GetProgressionStat(Anki.Cozmo.ProgressionStatType index) {
+    return ProgressionStats[(int)index];
+  }
 
   public void AddToProgressionStat(Anki.Cozmo.ProgressionStatType index, int deltaValue) {
     ProgressionStatMessage.robotID = ID;
