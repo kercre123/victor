bc776af70e1a8186c626e8cbd23551cbf54a2198
Trevor Dasch <trevor@anki.com>
2016-01-19 12:12:27 -0800

    Added some comments to DailySummaryPanel.

--- unity/Cozmo/Assets/SharedAssets/Scripts/UI/DailySummaryPanel/DailySummaryPanel.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/DailySummaryPanel/DailySummaryPanel.cs
@@ -8,16 +8,14 @@ using Anki.Cozmo;
 
 public class DailySummaryPanel : MonoBehaviour {
 
+  // Animation Names
   private const string kIdleAnimationName = "Idle";
-
   private const string kLevelReachedAnimationName = "LevelReached";
-
   private const string kMaxLevelAnimationName = "MaxLevel";
 
+  // B versions put the current level label on the bottom
   private const string kIdleAnimationNameB = "IdleB";
-
   private const string kLevelReachedAnimationNameB = "LevelReachedB";
-
   private const string kMaxLevelAnimationNameB = "MaxLevelB";
 
 
@@ -124,7 +122,7 @@ public class DailySummaryPanel : MonoBehaviour {
   }
 
   private IEnumerator FriendshipBarFill(float start, float end) {
-    // this is pretty bad
+    // yay magic numbers
     const float initialPoint = 0.14f;
     const float finalPoint = 0.86f;
     const float timeLimit = 1.42f;
@@ -154,10 +152,14 @@ public class DailySummaryPanel : MonoBehaviour {
 
     int startingPoints = data.StartingFriendshipPoints;
     float pointsRequired, startingPercent, endingPercent;
+
+    // first go through all levels that we reached
     for(int level = data.StartingFriendshipLevel; 
           level < data.EndingFriendshipLevel; level++) {
+      // play the idle which displays the labels we want
       _FriendBarAnimator.Play(level % 2 == 0 ? kIdleAnimationName : kIdleAnimationNameB);
 
+      // set up the labels
       SetFriendshipTexts(
         levelConfig.FriendshipLevels[level].FriendshipLevelName,
         levelConfig.FriendshipLevels[level + 1].FriendshipLevelName,
@@ -170,23 +172,30 @@ public class DailySummaryPanel : MonoBehaviour {
 
       startingPercent = startingPoints / pointsRequired;
 
+      // fill the progress bar to the top
       yield return StartCoroutine(FriendshipBarFill(startingPercent, 1f));
 
+      // if there is another level after the next, play the animation
+      // where the next dot slides to where the current dot is
       if (level + 2 < levelConfig.FriendshipLevels.Length) {
         _FriendBarAnimator.Play(level % 2 == 0 ? kLevelReachedAnimationName : kLevelReachedAnimationNameB);
 
         yield return StartCoroutine(FriendshipBarFill(1, 0));
       }
       else {
+        // otherwise, we are at max level. fill the next circle and keep it there
         _FriendBarAnimator.Play(level % 2 == 0 ? kMaxLevelAnimationName : kMaxLevelAnimationNameB);
         yield break;
       }
 
+      // our next fill will start at point 0
       startingPoints = 0;
     }
 
+    // after filling all completed levels, we want to animate our progress on the current level
     int endingPoints = data.EndingFriendshipPoints;
 
+    // if this isn't the max level, show our progress toward our next level
     if (data.EndingFriendshipLevel + 1 < levelConfig.FriendshipLevels.Length) {
 
       _FriendBarAnimator.Play(data.EndingFriendshipLevel % 2 == 0 ? kIdleAnimationName : kIdleAnimationNameB);
@@ -203,6 +212,7 @@ public class DailySummaryPanel : MonoBehaviour {
 
     }
     else {
+      // if we are at max level, just play the max level animation (leaves the end dot full)
       startingPercent = endingPercent = 1f;
 
       _FriendBarAnimator.Play(data.EndingFriendshipLevel % 2 == 1 ? kMaxLevelAnimationName : kMaxLevelAnimationNameB);
@@ -216,10 +226,16 @@ public class DailySummaryPanel : MonoBehaviour {
     yield return StartCoroutine(FriendshipBarFill(startingPercent, endingPercent));
   }
 
+  // sets the labels on the friendship timeline
   private void SetFriendshipTexts(string current, string next, bool even, string nextNext = null) {
 
+    // not sure what setting text to null does
     nextNext = nextNext ?? string.Empty;
     // TODO: Localize
+
+    // for even levels, we show the top label for the current and the bottom for the next
+    // we set the unshown labels to the next value, so when the animation ends and
+    // switches them, they will already be correct regardless of synchronization with code
     _CurrentFriendshipLevelTop.text = even ? current : next;
     _CurrentFriendshipLevelBottom.text = even ? next : current;
     _NextFriendshipLevelTop.text = even ? nextNext : next;
