885ff3ef4e0d70090154e7d4a4cb9b6ddd530162
Trevor Dasch <trevor@anki.com>
2016-01-20 16:15:03 -0800

    Added Win and Lose buttons to the debug menu.

--- unity/Cozmo/Assets/HomeHub/Scripts/HomeHub.cs
+++ unity/Cozmo/Assets/HomeHub/Scripts/HomeHub.cs
@@ -21,6 +21,12 @@ namespace Cozmo.HomeHub {
 
     private GameBase _MiniGameInstance;
 
+    public GameBase MiniGameInstance { 
+      get {        
+        return _MiniGameInstance;
+      }
+    }
+
     private CompletedChallengeData _CurrentChallengePlaying;
 
     public override bool LoadHubWorld() {
