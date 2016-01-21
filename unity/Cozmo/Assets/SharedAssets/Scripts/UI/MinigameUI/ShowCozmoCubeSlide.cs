8ed652c28da611afe44ac95bc2e2e677684b4f14
Ivy Ngo <ivy@anki.com>
2016-01-21 14:58:04 -0800

    Setup now uses graphics to show cubes

--- unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/ShowCozmoCubeSlide.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/ShowCozmoCubeSlide.cs
@@ -3,18 +3,14 @@ using System.Collections;
 
 public class ShowCozmoCubeSlide : MonoBehaviour {
 
-  // INGO TODO: Using a text field for now and then adding
-  // functionality to show images later
   [SerializeField]
-  private Anki.UI.AnkiTextLabel _NumCubesText;
-  private int _NumCubesToShow;
+  private Cozmo.UI.SegmentedBar _NumCubesBar;
 
   public void Initialize(int numCubesToShow) {
-    _NumCubesText.text = "0 / " + numCubesToShow;
-    _NumCubesToShow = numCubesToShow;
+    _NumCubesBar.SetMaximumSegments(numCubesToShow);
   }
 
   public void LightUpCubes(int numberCubes) {
-    _NumCubesText.text = numberCubes + " / " + _NumCubesToShow;
+    _NumCubesBar.SetCurrentNumSegments(numberCubes);
   }
 }
