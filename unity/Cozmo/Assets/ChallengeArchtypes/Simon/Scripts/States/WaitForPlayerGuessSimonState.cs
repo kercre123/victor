ec75a85e0f579d7b830b3e21bd22c013fc496db3
Pohung Chen <pohung@anki.com>
2016-01-22 14:40:31 -0800

    modifying simon tap delay

--- unity/Cozmo/Assets/ChallengeArchtypes/Simon/Scripts/States/WaitForPlayerGuessSimonState.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/Simon/Scripts/States/WaitForPlayerGuessSimonState.cs
@@ -93,7 +93,7 @@ namespace Simon {
 
     private void OnBlockTapped(int id, int times) {
       _CurrentRobot.SetHeadAngle(Random.Range(-0.6f, 0f));
-      if (Time.time - _LastTappedTime < 0.8f || _StartLightBlinkTime != -1) {
+      if (Time.time - _LastTappedTime < 0.4f || _StartLightBlinkTime != -1) {
         return;
       }
 
