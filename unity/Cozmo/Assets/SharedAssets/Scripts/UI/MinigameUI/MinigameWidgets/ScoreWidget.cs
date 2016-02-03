f94394b563d681d65ce8b5bec786dee94be0cf17
Ivy Ngo <ivy@anki.com>
2016-02-03 14:29:08 -0800

    WIP: Score widget for minigame UI

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/MinigameWidgets/ScoreWidget.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using DG.Tweening;
+using Anki.UI;
+using Cozmo.UI;
+
+namespace Cozmo {
+  namespace MinigameWidgets {
+    public class ScoreWidget : MonoBehaviour, IMinigameWidget {
+
+      [SerializeField]
+      private float _AnimationXOffset = 600f;
+
+      [SerializeField]
+      private LayoutElement _ScoreContainer;
+
+      [SerializeField]
+      private AnkiTextLabel _ScoreCountLabel;
+
+      [SerializeField]
+      private LayoutElement _RoundContainer;
+
+      [SerializeField]
+      private SegmentedBar _RoundCountBar;
+
+      private void Start() {
+      }
+
+      public void DestroyWidgetImmediately() {
+        Destroy(gameObject);
+      }
+
+      // TODO: Don't hardcode this
+      public Sequence OpenAnimationSequence() {
+        Sequence open = DOTween.Sequence();
+        open.Append(this.transform.DOLocalMove(new Vector3(this.transform.localPosition.x + _AnimationXOffset, 
+          this.transform.localPosition.y, this.transform.localPosition.z),
+          0.25f).From().SetEase(Ease.OutQuad));
+        return open;
+      }
+
+      // TODO: Don't hardcode this
+      public Sequence CloseAnimationSequence() {
+        Sequence close = DOTween.Sequence();
+        close.Append(this.transform.DOLocalMove(new Vector3(this.transform.localPosition.x + _AnimationXOffset, 
+          this.transform.localPosition.y, this.transform.localPosition.z),
+          0.25f).SetEase(Ease.OutQuad));
+        return close;
+      }
+    }
+  }
+}
