969915c2fb25c534b62fd7c8fa0b277ead47c155
Trevor Dasch <trevor@anki.com>
2016-02-04 13:10:15 -0800

    When the player has to tap the cube, show 4 different colors.

--- unity/Cozmo/Assets/ChallengeArchtypes/SpeedTap/Scripts/States/SpeedTapPlayerConfirm.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/SpeedTap/Scripts/States/SpeedTapPlayerConfirm.cs
@@ -12,7 +12,12 @@ namespace SpeedTap {
     public override void Enter() {
       base.Enter();
       _SpeedTapGame = _StateMachine.GetGame() as SpeedTapGame;
-      _SpeedTapGame.PlayerBlock.SetLEDs(Color.white);
+
+      _SpeedTapGame.PlayerBlock.Lights[0].OnColor = Color.red.ToUInt();
+      _SpeedTapGame.PlayerBlock.Lights[1].OnColor = Color.green.ToUInt();
+      _SpeedTapGame.PlayerBlock.Lights[2].OnColor = Color.blue.ToUInt();
+      _SpeedTapGame.PlayerBlock.Lights[3].OnColor = Color.yellow.ToUInt();
+
       LightCube.TappedAction += HandleTap;
       _SpeedTapGame.ShowGameStateSlide(kSlideName);
 
