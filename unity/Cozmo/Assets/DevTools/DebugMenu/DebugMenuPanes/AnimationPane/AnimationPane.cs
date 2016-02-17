30f193618436aea6f50c257cb59dd44b26b3a36a
Pohung Chen <pohung@anki.com>
2016-02-17 13:27:02 -0800

    finishing animation pane debug thing

--- unity/Cozmo/Assets/DevTools/DebugMenu/DebugMenuPanes/AnimationPane/AnimationPane.cs
+++ unity/Cozmo/Assets/DevTools/DebugMenu/DebugMenuPanes/AnimationPane/AnimationPane.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class AnimationPane : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  [SerializeField]
+  private Dropdown _Dropdown;
+
+  [SerializeField]
+  private Button _PlayButton;
+
+  // Use this for initialization
+  void Start() {
+    List<string> robotAnimationNames = RobotEngineManager.Instance.GetRobotAnimationNames();
+    robotAnimationNames.Sort();
+    for (int i = 0; i < robotAnimationNames.Count; ++i) {
+      Dropdown.OptionData optionData = new Dropdown.OptionData();
+      optionData.text = robotAnimationNames[i];
+      _Dropdown.options.Add(optionData);
+    }
+    _Dropdown.value = 0;
+    _PlayButton.onClick.AddListener(() => RobotEngineManager.Instance.CurrentRobot.SendAnimation(robotAnimationNames[_Dropdown.value]));
+  }
 }
