52806e3dce6e7fb59dd54cc32f64caa22e0b8812
Trevor Dasch <trevor@anki.com>
2016-02-16 17:07:40 -0800

    Updated pivot on ChallengeEndedReward so it is in the center and matches up with the target.

--- unity/Cozmo/Assets/HomeHub/Scripts/DailyGoalScripts/DailyGoalPanel.cs
+++ unity/Cozmo/Assets/HomeHub/Scripts/DailyGoalScripts/DailyGoalPanel.cs
@@ -48,7 +48,7 @@ public class DailyGoalPanel : MonoBehaviour {
   private RectTransform _RectTransform;
 
   private GoalCell[] _GoalCellsByStat;
-  private List<Sequence> _RewardTweens;
+  private readonly List<Sequence> _RewardTweens = new List<Sequence>();
 
   void Awake() {
     _RectTransform = GetComponent<RectTransform>();
@@ -159,22 +159,12 @@ public class DailyGoalPanel : MonoBehaviour {
     yield return new WaitForSeconds(0.1f);
 
     // TODO: For each reward, tween it to the target goal
-    _RewardTweens = new List<Sequence>();
-    Sequence rewardTweenSequence;
-    Tweener rewardIconTween;
-    Transform target;
+    _RewardTweens.Clear();
+
     for (int stat = 0; stat < (int)ProgressionStatType.Count; stat++) {
       if (rewardIconsByStat[stat] != null) {
         if (_GoalCellsByStat[stat] != null) {
-          rewardTweenSequence = DOTween.Sequence();
-          target = rewardIconsByStat[stat];
-          rewardIconTween = rewardIconsByStat[stat].DOMove(_GoalCellsByStat[stat].transform.position, 1.5f)
-            .SetDelay(_RewardTweens.Count * 0.1f).SetEase(Ease.InOutBack).OnComplete(() => {
-            Destroy(target.gameObject);
-          });
-          rewardTweenSequence.Join(rewardIconTween);
-          rewardTweenSequence.Play();
-          _RewardTweens.Add(rewardTweenSequence);
+          CreateSequenceForRewardIcon(rewardIconsByStat[stat], _GoalCellsByStat[stat].transform);
         }
         else {
           DAS.Error(this, string.Format("Could not find GoalCell for stat {0} when tweening Rewards!", 
@@ -182,5 +172,16 @@ public class DailyGoalPanel : MonoBehaviour {
         }
       }
     }
+  }
+
+  private void CreateSequenceForRewardIcon(Transform target, Transform goalCell) {
+    var rewardTweenSequence = DOTween.Sequence();
+    var rewardIconTween = target.DOMove(goalCell.position, 1.5f)
+      .SetDelay(_RewardTweens.Count * 0.1f).SetEase(Ease.InCubic).OnComplete(() => {
+        Destroy(target.gameObject);
+      });
+    rewardTweenSequence.Join(rewardIconTween);
+    rewardTweenSequence.Play();
+    _RewardTweens.Add(rewardTweenSequence);
   }
 }
