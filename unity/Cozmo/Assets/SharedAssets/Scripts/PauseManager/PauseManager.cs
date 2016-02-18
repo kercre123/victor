a178c0de9df5c606f41d038f8817130e712ea7a2
Pohung Chen <pohung@anki.com>
2016-02-18 11:19:44 -0800

    stubbing out pause manager

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/PauseManager/PauseManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseManager : MonoBehaviour {
+  private void OnApplicationPause(bool pauseStatus) {
+    DAS.Info(this, "Pause Triggered: " + pauseStatus);
+  }
+}
