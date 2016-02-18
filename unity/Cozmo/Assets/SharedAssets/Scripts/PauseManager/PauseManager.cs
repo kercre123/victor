0a6e55e965e47e632e97f649bc0f344ffa8bdcd4
Pohung Chen <pohung@anki.com>
2016-02-18 14:46:09 -0800

    this seems way more consistent with what we want

--- unity/Cozmo/Assets/SharedAssets/Scripts/PauseManager/PauseManager.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/PauseManager/PauseManager.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class PauseManager : MonoBehaviour {
-  private void OnApplicationPause(bool pauseStatus) {
-    DAS.Info(this, "Pause Triggered: " + pauseStatus);
+  private void OnApplicationFocus(bool focusStatus) {
+    DAS.Info(this, "App Focussed: " + focusStatus);
   }
 }
