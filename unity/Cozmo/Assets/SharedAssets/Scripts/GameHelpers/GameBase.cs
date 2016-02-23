5115249cbcfcb691d36e0a46f6c476f3f56e9c8c
Ryan Anderson <ryan@anki.com>
2016-02-23 15:15:05 -0800

    Added vision Mode into GameBase's Robot Reset. All games should enable Cozmo seeing faces again. Bug fixes for conflicting animations

--- unity/Cozmo/Assets/SharedAssets/Scripts/GameHelpers/GameBase.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/GameHelpers/GameBase.cs
@@ -122,6 +122,9 @@ public abstract class GameBase : MonoBehaviour {
       CurrentRobot.ResetRobotState(() => {
         RobotEngineManager.Instance.CurrentRobot.SetBehaviorSystem(true);
         RobotEngineManager.Instance.CurrentRobot.ActivateBehaviorChooser(Anki.Cozmo.BehaviorChooserType.Demo);
+        CurrentRobot.SetVisionMode(VisionMode.DetectingFaces, true);
+        CurrentRobot.SetVisionMode(VisionMode.DetectingMarkers, true);
+        CurrentRobot.SetVisionMode(VisionMode.DetectingMotion, true);
       });
     }
     if (_SharedMinigameViewInstance != null) {
