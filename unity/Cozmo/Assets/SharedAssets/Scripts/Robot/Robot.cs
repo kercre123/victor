c36ee6aef440bfd9c9b532dbedf0c0cca23cffa4
Ivy Ngo <ivy@anki.com>
2016-02-25 11:45:33 -0800

    Revert "Update Robot Volume message"
    This reverts commit fc37b67d67981cfbdb85e8dda8d596c73fbfacb0.

--- unity/Cozmo/Assets/SharedAssets/Scripts/Robot/Robot.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Robot/Robot.cs
@@ -989,12 +989,12 @@ public class Robot : IRobot {
       callback, 
       queueActionPosition);
   }
-    
+
   public void SetRobotVolume(float volume) {
     DAS.Debug(this, "Set Robot Volume " + volume);
 
     RobotEngineManager.Instance.Message.SetRobotVolume = 
-      Singleton<SetRobotVolume>.Instance.Initialize(ID, volume);
+      Singleton<SetRobotVolume>.Instance.Initialize(volume);
     RobotEngineManager.Instance.SendMessage();
   }
 
