894c375b5413ecfb3d2b40ea1e890004b5cbc828
Bryon Vandiver <unicdk@gmail.com>
2016-02-24 17:13:58 -0800

    Finish up with cubes for now

--- robot/syscon/hal/main.cpp
+++ robot/syscon/hal/main.cpp
@@ -61,6 +61,14 @@ void MotorsUpdate(void* userdata) {
 
     Battery::setHeadlight(g_dataToBody.flags & BODY_FLASHLIGHT);
   }
+
+  // Prevent overheating
+  if (Battery::onContacts) {
+    for (int i = 0; i < MOTOR_COUNT; i++)
+    {
+      Motors::setPower(i, 0);
+    }
+  }
 }
 
 int main(void)
@@ -89,6 +97,7 @@ int main(void)
   // Only use Head/body if tests are not enabled
   Head::init();
   RTOS::schedule(MotorsUpdate);   
+
 
   // Start the scheduler
   RTOS::run();
