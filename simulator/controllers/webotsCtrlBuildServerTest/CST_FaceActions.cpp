dcaeb7f4a9a5e99ed3fa82f992ab3b04c93e0176
Al Chaussee <al@anki.com>
2016-03-01 09:50:28 -0800

    Webots test for TurnTowardsLastFacePoseAction

---
+++ simulator/controllers/webotsCtrlBuildServerTest/CST_FaceActions.cpp
@@ -0,0 +1,140 @@
+/**
+ * File: CST_FaceActions.cpp
+ *
+ * Author: Al Chaussee
+ * Created: 2/29/16
+ *
+ * Description: See TestStates below
+ *
+ * Copyright: Anki, inc. 2016
+ *
+ */
+
+#include "anki/cozmo/simulator/game/cozmoSimTestController.h"
+#include "anki/common/basestation/math/point_impl.h"
+#include "anki/cozmo/basestation/actions/basicActions.h"
+#include "anki/cozmo/basestation/robot.h"
+
+
+namespace Anki {
+  namespace Cozmo {
+    
+    enum class TestState {
+      TurnToFace,
+      TurnAwayFromFace,
+      TurnBackToFace,
+      TestDone
+    };
+    
+    // ============ Test class declaration ============
+    class CST_FaceActions : public CozmoSimTestController {
+      
+    private:
+      
+      virtual s32 UpdateInternal() override;
+      
+      TestState _testState = TestState::TurnToFace;
+      
+      bool _lastActionSucceeded = false;
+      
+      // Message handlers
+      virtual void HandleRobotCompletedAction(const ExternalInterface::RobotCompletedAction& msg) override;
+    };
+    
+    // Register class with factory
+    REGISTER_COZMO_SIM_TEST_CLASS(CST_FaceActions);
+    
+    
+    // =========== Test class implementation ===========
+    
+    s32 CST_FaceActions::UpdateInternal()
+    {
+      switch (_testState) {
+        case TestState::TurnToFace:
+        {
+          SendMoveHeadToAngle(MAX_HEAD_ANGLE, 100, 100);
+          
+          ExternalInterface::QueueSingleAction m;
+          m.robotID = 1;
+          m.position = QueueActionPosition::AT_END;
+          m.idTag = 2;
+          m.action.Set_turnInPlace(ExternalInterface::TurnInPlace(-PI/2, DEG_TO_RAD(100), 0, false, 1));
+          ExternalInterface::MessageGameToEngine message;
+          message.Set_QueueSingleAction(m);
+          SendMessage(message);
+          _testState = TestState::TurnAwayFromFace;
+          break;
+        }
+        case TestState::TurnAwayFromFace:
+        {
+          // Verify robot has turned and has seen the face
+          IF_CONDITION_WITH_TIMEOUT_ASSERT(!IsRobotStatus(RobotStatusFlag::IS_MOVING) &&
+                                           NEAR(GetRobotHeadAngle_rad(), MAX_HEAD_ANGLE, HEAD_ANGLE_TOL) &&
+                                           NEAR(GetRobotPose().GetRotation().GetAngleAroundZaxis().getDegrees(), -90, 10) &&
+                                           GetLastObservedFaceID() == -1, 10)
+          {
+            SendMoveHeadToAngle(0, 20, 20);
+            
+            ExternalInterface::QueueSingleAction m;
+            m.robotID = 1;
+            m.position = QueueActionPosition::AT_END;
+            m.idTag = 3;
+            m.action.Set_turnInPlace(ExternalInterface::TurnInPlace(-PI/2, DEG_TO_RAD(100), 0, false, 1));
+            ExternalInterface::MessageGameToEngine message;
+            message.Set_QueueSingleAction(m);
+            SendMessage(message);
+            _testState = TestState::TurnBackToFace;
+          }
+          break;
+        }
+        case TestState::TurnBackToFace:
+        {
+          // Verify robot has turned away from face
+          IF_CONDITION_WITH_TIMEOUT_ASSERT(!IsRobotStatus(RobotStatusFlag::IS_MOVING) &&
+                                           NEAR(GetRobotHeadAngle_rad(), 0, HEAD_ANGLE_TOL) &&
+                                           (NEAR(GetRobotPose().GetRotation().GetAngleAroundZaxis().getDegrees(), -180, 10) ||
+                                           NEAR(GetRobotPose().GetRotation().GetAngleAroundZaxis().getDegrees(), 180, 10)), 10)
+          {
+            ExternalInterface::QueueSingleAction m;
+            m.robotID = 1;
+            m.position = QueueActionPosition::NOW;
+            m.idTag = 10;
+            // Turn towards the last face pose
+            m.action.Set_turnTowardsLastFacePose(ExternalInterface::TurnTowardsLastFacePose(PI, 0, 0, 0, 0, 0, 0, 1));
+            ExternalInterface::MessageGameToEngine message;
+            message.Set_QueueSingleAction(m);
+            SendMessage(message);
+            _testState = TestState::TestDone;
+          }
+          break;
+        }
+        case TestState::TestDone:
+        {
+          // Verify robot has turned back towards the face
+          IF_CONDITION_WITH_TIMEOUT_ASSERT(!IsRobotStatus(RobotStatusFlag::IS_MOVING) &&
+                                           NEAR(GetRobotHeadAngle_rad(), DEG_TO_RAD(37), DEG_TO_RAD(2)) &&
+                                           NEAR(GetRobotPose().GetRotation().GetAngleAroundZaxis().getDegrees(), -90, 10) &&
+                                           GetLastObservedFaceID() == -2, 10)
+          {
+            CST_EXIT();
+          }
+          break;
+        }
+      }
+      return _result;
+    }
+    
+    
+    // ================ Message handler callbacks ==================
+    void CST_FaceActions::HandleRobotCompletedAction(const ExternalInterface::RobotCompletedAction& msg)
+    {
+      if (msg.result == ActionResult::SUCCESS) {
+        _lastActionSucceeded = true;
+      }
+    }
+    
+    // ================ End of message handler callbacks ==================
+    
+  } // end namespace Cozmo
+} // end namespace Anki
+
