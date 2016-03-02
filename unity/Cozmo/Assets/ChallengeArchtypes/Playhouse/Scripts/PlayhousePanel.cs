e5495ad3a17693c879e1e1d760139e3124ac58fe
Pohung Chen <pohung@anki.com>
2016-03-02 15:38:23 -0800

    DAS Stuff

--- unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhousePanel.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhousePanel.cs
@@ -24,6 +24,8 @@ namespace Playhouse {
       PopulateAnimations(_Dropdown2);
       PopulateAnimations(_Dropdown3);
       _StartPlayButton.onClick.AddListener(startPlayCallback);
+      _StartPlayButton.DASEventButtonName = "playhouse_start_button";
+      _StartPlayButton.DASEventViewController = "playhouse_panel";
     }
 
     private void PopulateAnimations(UnityEngine.UI.Dropdown dropdown) {
