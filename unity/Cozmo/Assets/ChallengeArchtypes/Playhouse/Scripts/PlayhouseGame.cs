004ecf4ae3ff372cfff2e5c1ae79c9e73c935f7e
Pohung Chen <pohung@anki.com>
2016-03-02 13:09:56 -0800

    logic for UI stuff

--- unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhouseGame.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhouseGame.cs
@@ -5,8 +5,12 @@ using System.Collections.Generic;
 namespace Playhouse {
   public class PlayhouseGame : GameBase {
 
+    [SerializeField]
+    private PlayhousePanel _PlayhousePanelPrefab;
+    private PlayhousePanel _PlayhousePanel = null;
+
     private PlayhouseGameConfig _Config;
-    private List<string> _AnimationSequence = new List<string>();
+    private List<string> _AnimationSequence;
     private int _SequenceIndex = -1;
 
     protected override void Initialize(MinigameConfigBase minigameConfig) {
@@ -19,15 +23,17 @@ namespace Playhouse {
     }
 
     protected override void CleanUpOnDestroy() {
-
+      UIManager.CloseView(_PlayhousePanel);
     }
 
     public void RequestAnimationDone() {
       // pop up UI for creating animation sequence.
+      _PlayhousePanel = UIManager.OpenView(_PlayhousePanelPrefab) as PlayhousePanel;
+      _PlayhousePanel._StartPlayButton.onClick.AddListener(HandleRunSequence);
     }
 
-
     private void HandleRunSequence() {
+      _AnimationSequence = _PlayhousePanel.GetAnimationList();
       CurrentRobot.SendAnimation(AnimationName.kCodeBreakerNewIdea, OnSequenceAnimationDone);
       _SequenceIndex = -1;
     }
@@ -45,6 +51,7 @@ namespace Playhouse {
     private void ResetGame() {
       _StateMachine.SetNextState(new RequestPlay());
       _AnimationSequence.Clear();
+      UIManager.CloseView(_PlayhousePanel);
     }
   }
 }
---
+++ unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhousePanel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Cozmo.UI;
+
+namespace Playhouse {
+  public class PlayhousePanel : BaseView {
+
+    [SerializeField]
+    public Anki.UI.AnkiButton _StartPlayButton;
+
+    [SerializeField]
+    public UnityEngine.UI.Dropdown _Dropdown1;
+
+    [SerializeField]
+    public UnityEngine.UI.Dropdown _Dropdown2;
+
+    [SerializeField]
+    public UnityEngine.UI.Dropdown _Dropdown3;
+
+    public List<string> GetAnimationList() {
+      List<string> animationList = new List<string>();
+      return animationList;
+    }
+
+    protected override void CleanUp() {
+
+    }
+  }
+
+}
