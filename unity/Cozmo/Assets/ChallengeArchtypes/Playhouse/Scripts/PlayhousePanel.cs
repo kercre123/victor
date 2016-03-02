6578c83e14caed076f8ad11f9df5f31a6af82ba5
Pohung Chen <pohung@anki.com>
2016-03-02 13:43:50 -0800

    populating strings

--- unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhousePanel.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhousePanel.cs
@@ -18,8 +18,29 @@ namespace Playhouse {
     [SerializeField]
     public UnityEngine.UI.Dropdown _Dropdown3;
 
+    public void Initialize() {
+      // populate dropdowns with animations.
+      PopulateAnimations(_Dropdown1);
+      PopulateAnimations(_Dropdown2);
+      PopulateAnimations(_Dropdown3);
+    }
+
+    private void PopulateAnimations(UnityEngine.UI.Dropdown dropdown) {
+      List<string> robotAnimationNames = RobotEngineManager.Instance.GetRobotAnimationNames();
+      robotAnimationNames.Sort();
+      for (int i = 0; i < robotAnimationNames.Count; ++i) {
+        UnityEngine.UI.Dropdown.OptionData optionData = new UnityEngine.UI.Dropdown.OptionData();
+        optionData.text = robotAnimationNames[i];
+        dropdown.options.Add(optionData);
+      }
+      dropdown.value = 0;
+    }
+
     public List<string> GetAnimationList() {
       List<string> animationList = new List<string>();
+      animationList.Add(_Dropdown1.options[_Dropdown1.value]);
+      animationList.Add(_Dropdown2.options[_Dropdown2.value]);
+      animationList.Add(_Dropdown3.options[_Dropdown3.value]);
       return animationList;
     }
 
