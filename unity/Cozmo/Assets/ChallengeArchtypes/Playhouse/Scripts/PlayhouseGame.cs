83fe19b4f01588fc1f16170aa7cb3a9fc65b8235
Pohung Chen <pohung@anki.com>
2016-03-01 16:12:29 -0800

    stubbing out playhouse

---
+++ unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/PlayhouseGame.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Playhouse {
+  public class PlayhouseGame : GameBase {
+
+    private PlayhouseGameConfig _Config;
+    private List<string> _AnimationSequence = new List<string>();
+    private int _SequenceIndex = -1;
+
+    protected override void Initialize(MinigameConfigBase minigameConfig) {
+      _Config = (PlayhouseGameConfig)minigameConfig;
+
+      State nextState = new RequestPlay();
+      InitialCubesState initCubeState = new InitialCubesState(new HowToPlayState(nextState), _Config.NumCubesRequired());
+      _StateMachine.SetNextState(initCubeState);
+
+    }
+
+    protected override void CleanUpOnDestroy() {
+
+    }
+
+    public void RequestAnimationDone() {
+      // pop up UI for creating animation sequence.
+    }
+
+
+    private void HandleRunSequence() {
+      CurrentRobot.SendAnimation(AnimationName.kCodeBreakerNewIdea, OnSequenceAnimationDone);
+      _SequenceIndex = -1;
+    }
+
+    private void OnSequenceAnimationDone(bool success) {
+      _SequenceIndex++;
+      if (_SequenceIndex < _AnimationSequence.Count) {
+        CurrentRobot.SendAnimation(_AnimationSequence[_SequenceIndex], OnSequenceAnimationDone);
+      }
+      else {
+        ResetGame();
+      }
+    }
+
+    private void ResetGame() {
+      _StateMachine.SetNextState(new RequestPlay());
+      _AnimationSequence.Clear();
+    }
+  }
+}
---
+++ unity/Cozmo/Assets/ChallengeArchtypes/Playhouse/Scripts/States/RequestPlay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Playhouse {
+  public class RequestPlay : State {
+
+    private PlayhouseGame _GameInstance;
+
+    public override void Enter() {
+      base.Enter();
+      RobotEngineManager.Instance.CurrentRobot.SendAnimation(AnimationName.kEnjoyPattern, AnimationDone);
+      _GameInstance = _StateMachine.GetGame() as PlayhouseGame;
+    }
+
+    private void AnimationDone(bool success) {
+      _GameInstance.RequestAnimationDone();
+    }
+
+  }
+
+}
