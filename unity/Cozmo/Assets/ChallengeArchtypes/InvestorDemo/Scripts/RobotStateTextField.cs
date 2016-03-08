95ce23e37445cfc0a46d53c212ef03649617eed5
Molly Jameson <molly@anki.com>
2016-03-07 18:15:14 -0800

    Debug String can show behaviors or anime

--- unity/Cozmo/Assets/ChallengeArchtypes/InvestorDemo/Scripts/RobotStateTextField.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/InvestorDemo/Scripts/RobotStateTextField.cs
@@ -1,17 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Anki.UI;
+using Anki.Cozmo.Viz;
 
 public class RobotStateTextField : MonoBehaviour {
 
   [SerializeField]
   private AnkiTextLabel _RobotStateLabel;
 
+  private static bool _useAnimString = false;
+
+  public static void ToggleUseAnimString() {
+    _useAnimString = !_useAnimString;
+  }
+
   private void Update() {
     if (RobotEngineManager.Instance.CurrentRobot != null) {
-      IRobot robot = RobotEngineManager.Instance.CurrentRobot;
-      if (robot.CurrentBehaviorString != _RobotStateLabel.text) {
-        _RobotStateLabel.text = robot.CurrentBehaviorString;
+      if (_useAnimString) {
+        if (VizManager.Instance.AnimationName != _RobotStateLabel.text) {
+          _RobotStateLabel.text = VizManager.Instance.AnimationName;
+        }
+      }
+      else {
+        IRobot robot = RobotEngineManager.Instance.CurrentRobot;
+        if (robot.CurrentBehaviorString != _RobotStateLabel.text) {
+          _RobotStateLabel.text = robot.CurrentBehaviorString;
+        }
       }
     }
   }
