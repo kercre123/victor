6349fdcadebafbe0ea885eefe27ff4b61adec8a2
Daniel Casner <daniel@anki.com>
2016-03-08 17:08:51 -0800

    Minor patches to try and make robot message passing more robust.

--- robot/k02_hal/hal/wifi.cpp
+++ robot/k02_hal/hal/wifi.cpp
@@ -108,7 +108,7 @@ namespace HAL {
       }
       else
       {
-        AnkiError( 132, "WiFi.ReceiveMessage", 398, "No buffer available to receive clad message %x[%d]", 2, data[0], length);
+        AnkiError( 132, "WiFi.ReceiveMessage", 398, "No buffer available to receive clad message %x[%d], buffer %x %x %d", 5, data[0], length, rxBuf[rind], rxBuf[rind+1], RX_BUF_SIZE - ((rind - wind) & RX_BUF_SIZE_MASK));
         return false;
       }
     }
