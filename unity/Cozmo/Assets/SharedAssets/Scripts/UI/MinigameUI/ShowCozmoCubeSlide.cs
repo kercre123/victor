c6ca5b7ffbb887db332d43acf9d57e4dd398d2a7
Ivy Ngo <ivy@anki.com>
2016-03-10 13:59:45 -0800

    Using new sprites in the setup flow

--- unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/ShowCozmoCubeSlide.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/ShowCozmoCubeSlide.cs
@@ -1,21 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using Anki.UI;
 using System.Collections;
 
 public class ShowCozmoCubeSlide : MonoBehaviour {
 
   [SerializeField]
-  private Cozmo.UI.SegmentedBar _NumCubesBar;
+  private VerticalLayoutGroup _CubeContainer;
 
   [SerializeField]
-  private Anki.UI.AnkiTextLabel _ShowCozmoCubesLabel;
+  private Image _CubePrefab;
+
+  [SerializeField]
+  private Sprite _CubeOff;
+
+  [SerializeField]
+  private Sprite _CubeOn;
+
+  [SerializeField]
+  private AnkiTextLabel _ShowCozmoCubesLabel;
+
+  private Image[] _CubeImages;
 
   public void Initialize(int numCubesToShow) {
-    _NumCubesBar.SetMaximumSegments(numCubesToShow);
+    CreateCubes(numCubesToShow);
+    LightUpCubes(0);
     _ShowCozmoCubesLabel.text = Localization.GetWithArgs(LocalizationKeys.kMinigameLabelShowCubes,
       numCubesToShow);
   }
 
   public void LightUpCubes(int numberCubes) {
-    _NumCubesBar.SetCurrentNumSegments(numberCubes);
+    for (int i = 0; i < _CubeImages.Length; i++) {
+      _CubeImages[i].sprite = (i < numberCubes) ? _CubeOn : _CubeOff;
+    }
+  }
+
+  private void CreateCubes(int numCubesToShow) {
+    _CubeImages = new Image[numCubesToShow];
+    for (int i = 0; i < _CubeImages.Length; i++) {
+      _CubeImages[i] = UIManager.CreateUIElement(_CubePrefab, _CubeContainer.transform).GetComponent<Image>();
+    }
   }
 }
