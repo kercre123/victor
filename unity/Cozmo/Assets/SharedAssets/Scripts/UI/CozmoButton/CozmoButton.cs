a5e3cbb0ff51323ecf17a86e10ebc4e09ad88200
Pohung Chen <pohung@anki.com>
2016-03-11 09:21:29 -0800

    starting CozmoButton

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/CozmoButton/CozmoButton.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Anki.UI;
+
+namespace Cozmo.UI {
+  public class CozmoButton : AnkiButton {
+
+    [SerializeField]
+    private AnkiAnimateGlint _GlintAnimator;
+
+    [SerializeField]
+    private Anki.Cozmo.Audio.AudioEventParameter _UISoundEvent = Anki.Cozmo.Audio.AudioEventParameter.DefaultClick;
+
+    public Anki.Cozmo.Audio.AudioEventParameter SoundEvent {
+      get { return _UISoundEvent; }
+      set { _UISoundEvent = value; }
+    }
+
+    protected override void Start() {
+      base.Start();
+      base.onClick.AddListener(HandleOnPress);
+    }
+
+    protected override void OnDestroy() {
+      base.OnDestroy();
+      base.onClick.RemoveListener(HandleOnPress);
+    }
+
+    private void HandleOnPress() {
+      Anki.Cozmo.Audio.GameAudioClient.PostAudioEvent(_UISoundEvent);
+    }
+
+    private void ShowGlint(bool show) {
+      if (_GlintAnimator != null) {
+        _GlintAnimator.EnableGlint(show);
+      }
+    }
+  }
+
+}
