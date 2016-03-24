fdaa24d8e7a586fa473a6165eb7ffd07ad13f332
Pohung Chen <pohung@anki.com>
2016-03-24 14:49:19 -0700

    handling of messages

--- unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
+++ unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
@@ -9,6 +9,9 @@ public class FactoryIntroManager : MonoBehaviour {
   private Intro _DevConnectDialog;
   private GameObject _DevConnectDialogInstance;
 
+  [SerializeField]
+  private UnityEngine.UI.Image _Background;
+
   void Start() {
     ShowDevConnectDialog();
     RobotEngineManager.Instance.RobotConnected += HandleConnected;
@@ -28,12 +31,19 @@ public class FactoryIntroManager : MonoBehaviour {
   }
 
   private void TestFailed() {
-    
+    _Background.color = Color.red;
+  }
+
+  private void TestPassed() {
+    _Background.color = Color.green;
   }
 
   private void FactoryResult(FactoryTestResult result) {
-    if (result.resultCode > 0) {
+    if (result.resultCode != 0) {
       TestFailed();
+    }
+    else {
+      TestPassed();
     }
   }
 
