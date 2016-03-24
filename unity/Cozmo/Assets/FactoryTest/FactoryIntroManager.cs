f6138dffdb37ed809777c62c0a6c67be065b4e6d
Pohung Chen <pohung@anki.com>
2016-03-24 14:14:34 -0700

    stubbing out factory test app scene

---
+++ unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactoryIntroManager : MonoBehaviour {
+
+  [SerializeField]
+  private Intro _DevConnectDialog;
+  private GameObject _DevConnectDialogInstance;
+
+  void Start() {
+    ShowDevConnectDialog();
+    RobotEngineManager.Instance.RobotConnected += HandleConnected;
+    RobotEngineManager.Instance.DisconnectedFromClient += HandleDisconnectedFromClient;
+  }
+
+  private void HandleConnected(int robotID) {
+    HideDevConnectDialog();
+    Debug.Log("Factory App Connected");
+  }
+
+  private void HandleDisconnectedFromClient(DisconnectionReason obj) {
+    ShowDevConnectDialog();
+  }
+
+  private void ShowDevConnectDialog() {
+    if (_DevConnectDialogInstance == null && _DevConnectDialog != null) {
+      _DevConnectDialogInstance = UIManager.CreateUIElement(_DevConnectDialog.gameObject);
+    }
+    _DevConnectDialogInstance.GetComponent<Intro>().Play(false);
+  }
+
+  private void HideDevConnectDialog() {
+    if (_DevConnectDialogInstance != null) {
+      Destroy(_DevConnectDialogInstance);
+    }
+  }
+}
