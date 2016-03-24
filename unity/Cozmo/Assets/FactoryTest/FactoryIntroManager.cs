799bd5c380d25f2830901d0f2ed76e6a50ff065a
Pohung Chen <pohung@anki.com>
2016-03-24 16:18:40 -0700

    hooking up UI stuff aww yeah

--- unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
+++ unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
@@ -12,11 +12,29 @@ public class FactoryIntroManager : MonoBehaviour {
   [SerializeField]
   private UnityEngine.UI.Image _Background;
 
+  [SerializeField]
+  private UnityEngine.UI.Button _LogsButton;
+
+  [SerializeField]
+  private UnityEngine.UI.Button _SettingsButton;
+
+  [SerializeField]
+  private UnityEngine.UI.Button _RestartButton;
+
+  [SerializeField]
+  private FactoryLogPanel _FactoryLogPanelPrefab;
+  private FactoryLogPanel _FactoryLogPanelInstance;
+
+  [SerializeField]
+  private Canvas _Canvas;
+
   void Start() {
     ShowDevConnectDialog();
     RobotEngineManager.Instance.RobotConnected += HandleConnected;
     RobotEngineManager.Instance.DisconnectedFromClient += HandleDisconnectedFromClient;
     RobotEngineManager.Instance.OnFactoryResult += FactoryResult;
+    _RestartButton.gameObject.SetActive(false);
+    _LogsButton.onClick.AddListener(HandleLogButtonClick);
   }
 
   private void HandleConnected(int robotID) {
@@ -24,6 +42,21 @@ public class FactoryIntroManager : MonoBehaviour {
     Debug.Log("Factory App Connected To Robot");
     RobotEngineManager.Instance.Message.StartFactoryTest = Singleton<StartFactoryTest>.Instance.Initialize(0);
     RobotEngineManager.Instance.SendMessage();
+    SOSLogManager.Instance.CreateListener();
+    RobotEngineManager.Instance.CurrentRobot.SetEnableSOSLogging(true);
+    SOSLogManager.Instance.RegisterListener(HandleNewSOSLog);
+  }
+
+  private void HandleLogButtonClick() {
+    if (_FactoryLogPanelInstance != null) {
+      GameObject.Destroy(_FactoryLogPanelInstance.gameObject);
+    }
+    _FactoryLogPanelInstance = GameObject.Instantiate(_FactoryLogPanelPrefab).GetComponent<FactoryLogPanel>();
+    _FactoryLogPanelInstance.transform.SetParent(_Canvas.transform, false);
+  }
+
+  private void HandleNewSOSLog(string log_entry) {
+    
   }
 
   private void HandleDisconnectedFromClient(DisconnectionReason obj) {
@@ -31,11 +64,16 @@ public class FactoryIntroManager : MonoBehaviour {
   }
 
   private void TestFailed() {
+    if (_Background == null) {
+      return; // quit case
+    }
     _Background.color = Color.red;
+    _RestartButton.gameObject.SetActive(true);
   }
 
   private void TestPassed() {
     _Background.color = Color.green;
+    _RestartButton.gameObject.SetActive(true);
   }
 
   private void FactoryResult(FactoryTestResult result) {
---
+++ unity/Cozmo/Assets/FactoryTest/FactoryLogPanel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FactoryLogPanel : MonoBehaviour {
+  [SerializeField]
+  UnityEngine.UI.Text _LogTextField;
+
+  [SerializeField]
+  private UnityEngine.UI.ScrollRect _TextScrollRect;
+
+  [SerializeField]
+  private UnityEngine.UI.Button _CloseButton;
+
+  void Start() {
+    string test = "sdfsdf";
+    for (int i = 0; i < 100; ++i) {
+      test += i + "wee\n";
+    }
+    _LogTextField.text = test;
+    _TextScrollRect.verticalNormalizedPosition = 0;
+    _CloseButton.onClick.AddListener(ClosePanel);
+  }
+
+  private void ClosePanel() {
+    GameObject.Destroy(gameObject);
+  }
+}
