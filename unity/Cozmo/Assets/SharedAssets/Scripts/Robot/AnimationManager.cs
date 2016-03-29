e4066302e113f9f0305b5ad5403c1993dc433d2a
Ryan Anderson <ryan@anki.com>
2016-03-29 15:25:18 -0700

    Moving to a more Publisher->Subscriber based model

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Robot/AnimationManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using UnityEngine;
+using Anki.Cozmo;
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+public class AnimationManager {
+  
+  private static AnimationManager _Instance = null;
+
+  public static AnimationManager Instance { 
+    get { 
+      if (_Instance == null) { 
+        _Instance = new AnimationManager();
+      }
+      return _Instance; 
+    } 
+  }
+
+  private IRobot _CurrRobot = null;
+
+  // Map Animation Group Names to Event Enums
+  public Dictionary<CozmoEvent, string> AnimationGroupDict = new Dictionary<CozmoEvent, string>();
+
+
+  public AnimationManager() {
+    CozmoEventManager.Instance.CozmoEventReceived += CozmoEventRecieved;
+    _CurrRobot = RobotEngineManager.Instance.CurrentRobot;
+  }
+
+  public void CozmoEventRecieved(CozmoEvent cozEvent) {
+    if (AnimationGroupDict.ContainsKey(cozEvent)) {
+      _CurrRobot.SendAnimationGroup(AnimationGroupDict[cozEvent]);
+    }
+  }
+
+  public bool SetAnimationForEvent(CozmoEvent cozEvent, string animGroupName) {
+    if (AnimationGroupDict.ContainsKey(cozEvent)) {
+      DAS.Error(this, "CozmoEvent already has an Animation Assigned, only one Animation Group per Event");
+      return false;
+    }
+    AnimationGroupDict.Add(cozEvent, animGroupName);
+    return true;
+  }
+
+  public bool RemoveAnimationForEvent(CozmoEvent cozEvent, string animGroupName) {
+    if (AnimationGroupDict.ContainsKey(cozEvent)) {
+      AnimationGroupDict.Remove(cozEvent);
+      return true;
+    }
+    DAS.Error(this, "CozmoEvent doesn't have an Animation Assigned, can't remove what doesn't exist");
+    return false;
+  }
+
+}
--- unity/Cozmo/Assets/SharedAssets/Scripts/Robot/CozmoEventManager.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Robot/CozmoEventManager.cs
@@ -5,6 +5,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
+// TODO: Move all of this into clad
+public enum CozmoEvent : byte {
+  SPEEDTAP_TAP,
+  SPEEDTAP_WIN_HAND,
+  SPEEDTAP_WIN_ROUND,
+  SPEEDTAP_WIN_SESSION,
+  SPEEDTAP_LOSE_HAND,
+  SPEEDTAP_LOSE_ROUND,
+  SPEEDTAP_LOSE_SESSION,
+  SPEEDTAP_IDLE,
+  SPEEDTAP_FAKE,
+  COUNT
+}
+
 public class CozmoEventManager {
   
   private static CozmoEventManager _Instance = null;
@@ -18,41 +32,19 @@ public class CozmoEventManager {
     } 
   }
 
-  // TODO: Replace this with CLAD generated enums to match engine message
-  public enum CozmoEvent : byte {
-    SPEEDTAP_TAP,
-    SPEEDTAP_WIN_HAND,
-    SPEEDTAP_WIN_ROUND,
-    SPEEDTAP_WIN_SESSION,
-    SPEEDTAP_LOSE_HAND,
-    SPEEDTAP_LOSE_ROUND,
-    SPEEDTAP_LOSE_SESSION,
-    SPEEDTAP_IDLE,
-    SPEEDTAP_FAKE,
-    COUNT
-  }
-
-  // Dictionary that maps CLAD Events to Actions
-  // Other Tools add their specific actions to the CozmoEvent
-  // For example, The AnimationTool would add SendAnimationGroup calls to CozmoEvents
-  public Dictionary<CozmoEvent,Action> CozmoEventDict = new Dictionary<CozmoEvent, Action>();
+  // Event that fires and notifes all listeners of a CozmoEvent being recieved,
+  // other managers listen to this and use that to map enum based events to things
+  public event Action<CozmoEvent> CozmoEventReceived;
 
   public CozmoEventManager() {
     // TODO : Constructor?
-    // RobotEngineManager.Instance.WhateverTheEventMessageThingyIs += CozmoEventReceived;
+    // Listen to whatever message is giving us the CozmoEvent message.
+    // RobotEngineManager.Instance.WhateverTheEventMessageThingyIs += RaiseCozmoEvent;
   }
 
-  // TODO : Ok so the CozmoEvent Action has been fired, how does that action send the proper animation?
-  // AnimationTool has a CozmoEvent to String mapping for GroupNames?
-  // Fire the Action for the specific CozmoEvent enum
-  public bool CozmoEventRecieved(CozmoEvent cozEvent) {
-    Action cozAction;
-    if (CozmoEventDict.TryGetValue(cozEvent, out cozAction)) {
-      if (cozAction != null) {
-        cozAction.Invoke();
-        return true;
-      }
+  public void RaiseCozmoEvent(CozmoEvent cozEvent) {
+    if (CozmoEventReceived != null) {
+      CozmoEventReceived(cozEvent);
     }
-    return false;
   }
 }
