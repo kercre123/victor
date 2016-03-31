40bfbb26812011dbcfb4ce47f77368b5258136ae
Pohung Chen <pohung@anki.com>
2016-03-31 09:28:54 -0700

    starting data model for unlock system

---
+++ unity/Cozmo/Assets/CozmoUnlocks/Scripts/UnlockableInfo.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum UnlockableType {
+  Game,
+  Action
+}
+
+public class UnlockableInfo {
+  public readonly uint _Id;
+
+  public readonly UnlockableType _UnlockableType;
+
+  public readonly uint[] _Prerequisites;
+
+  // true = any prereq filled will make this unlock available
+  // false = all prereqs must be filled to make this unlock available.
+  public readonly bool _AnyPrereqUnlock;
+
+  public readonly int _CubesRequired;
+
+  public readonly int _TreatCost;
+}
