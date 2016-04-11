be269180a56466ef2d9746124647b69b131b8503
Bryon Vandiver <unicdk@gmail.com>
2016-04-11 14:22:06 -0700

    Missing files

---
+++ robot/robot_boot/hal/spi.h
@@ -0,0 +1,22 @@
+#ifndef __SPI_H
+#define __SPI_H
+
+namespace Anki
+{
+  namespace Cozmo
+  {
+    namespace HAL
+    {
+			namespace SPI
+			{
+				void espInit(void);
+				void init(void);
+				void sync(void);
+				
+				uint16_t readWord(void);
+			}
+		}
+	}
+}
+
+#endif
