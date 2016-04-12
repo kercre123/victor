f78bbe82a1c069b4536580ac1b96545a1c97cc90
Kevin Yoon <kevin@anki.com>
2016-04-11 20:21:15 -0700

    Hooked up NVStorageComponent to new nvStorage messages

--- basestation/include/anki/cozmo/basestation/components/nvStorageComponent.h
+++ basestation/include/anki/cozmo/basestation/components/nvStorageComponent.h
@@ -20,7 +20,8 @@
 
 #include <vector>
 #include <queue>
-#include <map>
+#include <unordered_map>
+#include <unordered_set>
 
 namespace Anki {
 namespace Cozmo {
@@ -55,13 +56,18 @@ public:
   // Save data to robot under the given tag.
   // Returns true if data passes checks to be sent to robot.
   // Caller should expect a MessageEngineToGame indicating success/failure of write.
-  bool Save(NVStorage::NVEntryTag tag, u8* data, int size);
+  bool Write(NVStorage::NVEntryTag tag, u8* data, int size);
   
-  // Request data stored on robot under the given tag to be written to data.
+  // Erase the given entry from robot flash
+  void Erase(NVStorage::NVEntryTag tag);
+  
+  // Request data stored on robot under the given tag.
+  // Executes specified callback when data is received.
+  // Copies data into specified data vector if non-null.
   // Returns true if request was successfully sent.
   // TODO: Some other message gets sent when the data is ready.
-  using NVStorageBlobReceiveCallback = std::function<void(u8* data, size_t size)>;
-  bool Request(NVStorage::NVEntryTag tag, std::vector<u8>& data, NVStorageBlobReceiveCallback callback = {});
+  using NVStorageReadCallback = std::function<void(u8* data, size_t size)>;
+  bool Read(NVStorage::NVEntryTag tag, NVStorageReadCallback callback = {}, std::vector<u8>* data = nullptr);
 
   void Update();
 
@@ -77,33 +83,49 @@ private:
   void HandleGameEvents(const AnkiEvent<ExternalInterface::MessageGameToEngine>& event);
   
   std::vector<Signal::SmartHandle> _signalHandles;
-  
+
+  // Whether or not this tag is composed of (potentially) multiple blobs
   bool IsMultiBlobEntryTag(u32 tag) const;
+  
+  // Given any tag, returns the assumed base tag
   u32 GetBaseEntryTag(u32 tag) const;
+  
+  // Given any tag, returns the assumed end-of-range tag
+  u32 GetTagRangeEnd(u32 startTag) const;
   
   struct SendDataObject {
     SendDataObject(u32 tag, std::vector<u8>&& dataVec) {
+      baseTag = tag;
       nextTag = tag;
       sendIndex = 0;
       data = dataVec;
     }
-    
+    u32 baseTag;
     u32 nextTag;
     u32 sendIndex;
     std::vector<u8> data;
   };
   
   struct RecvDataObject {
+    ~RecvDataObject() {
+      if (deleteVectorWhenDone)
+        delete data;
+    }
+    
     std::vector<u8> *data;
-    NVStorageBlobReceiveCallback callback;
+    NVStorageReadCallback callback;
+    bool deleteVectorWhenDone;
   };
+  
   
   // Queue of pairs of (nextSendIndex, data) to be sent to robot for saving
   std::queue<SendDataObject> _dataToSendQueue;
   
+  // Map of all tags sent for writing the specified NVEntryTag
+  std::unordered_map<u32, std::unordered_set<u32> > _sentWriteTags;
+  
   // Map of requested tag from robot to vector where the data should be stored
-//  std::map<NVStorage::NVEntryTag, std::vector<u8>* > _recvDataMap;
-    std::map<NVStorage::NVEntryTag, RecvDataObject > _recvDataMap;
+  std::unordered_map<u32, RecvDataObject> _recvDataMap;
 
   static constexpr u32 _kMaxNvStorageBlobSize        = 1024;
   static constexpr u32 _kMaxNumBlobsInMultiBlobEntry = 40;
--- basestation/src/anki/cozmo/basestation/components/nvStorageComponent.cpp
+++ basestation/src/anki/cozmo/basestation/components/nvStorageComponent.cpp
@@ -20,6 +20,8 @@
 
 #include "util/logging/logging.h"
 
+#define DEBUG_NVSTORAGE_COMPONENT 0
+
 namespace Anki {
 namespace Cozmo {
   
@@ -67,8 +69,16 @@ u32 NVStorageComponent::GetBaseEntryTag(u32 tag) const {
   }
   return baseTag;
 }
+  
+u32 NVStorageComponent::GetTagRangeEnd(u32 startTag) const {
+  if (IsMultiBlobEntryTag(startTag)) {
+    return startTag | 0xffff;
+  } else {
+    return startTag;
+  }
+}
 
-bool NVStorageComponent::Save(NVStorage::NVEntryTag tag, u8* data, int size) 
+bool NVStorageComponent::Write(NVStorage::NVEntryTag tag, u8* data, int size)
 {
   // Copy data locally and break up into as many messages as needed
   _dataToSendQueue.emplace((u32)tag, std::vector<u8>(data,data+size));
@@ -76,25 +86,52 @@ bool NVStorageComponent::Save(NVStorage::NVEntryTag tag, u8* data, int size)
   return true;
 }
   
-bool NVStorageComponent::Request(NVStorage::NVEntryTag tag, std::vector<u8>& data, NVStorageBlobReceiveCallback callback)
+void NVStorageComponent::Erase(NVStorage::NVEntryTag tag)
 {
-  u32 endTag = (u32)tag;
-  if (IsMultiBlobEntryTag((u32)tag)) {
-    endTag |= 0x0000ffff;
-  }
+  NVStorage::NVStorageWrite eraseMsg;
+  eraseMsg.reportTo = NVStorage::NVReportDest::ENGINE;
+  eraseMsg.writeNotErase = false;
+  eraseMsg.reportEach = false;
+  eraseMsg.reportDone = true;
+  eraseMsg.rangeEnd = GetTagRangeEnd((u32)tag);
+  eraseMsg.entry.tag = (u32)tag;
+  _robot.SendMessage(RobotInterface::EngineToRobot(std::move(eraseMsg)));
+}
   
-  // TODO: Request message from tag to to endTag
-  // ...
-  
-  
-  
-  // Save RecvDataObject
-  if (_recvDataMap.find(tag) != _recvDataMap.end()) {
-    PRINT_NAMED_WARNING("NVStorageComponent.Request.TagAlreadyRequested", "Ignoring tag %d", (u32)tag);
+bool NVStorageComponent::Read(NVStorage::NVEntryTag tag, NVStorageReadCallback callback, std::vector<u8>* data)
+{
+  // Check if tag already requested
+  u32 entryTag = (u32)tag;
+  if (_recvDataMap.find(entryTag) != _recvDataMap.end()) {
+    PRINT_NAMED_WARNING("NVStorageComponent.Read.TagAlreadyRequested", "Ignoring tag 0x%x", (u32)tag);
     return false;
   }
-  _recvDataMap[tag].data = &data;
-  _recvDataMap[tag].callback = callback;
+  
+  // Request message from tag to to endTag
+  NVStorage::NVStorageRead readMsg;
+  readMsg.tag = entryTag;
+  readMsg.tagRangeEnd = GetTagRangeEnd(entryTag);
+  readMsg.to = NVStorage::NVReportDest::ENGINE;
+  _robot.SendMessage(RobotInterface::EngineToRobot(std::move(readMsg)));
+  
+  
+  if (DEBUG_NVSTORAGE_COMPONENT) {
+    PRINT_NAMED_DEBUG("NVStorageComponent.Read.SendingRead",
+                      "StartTag: 0x%x, EndTag: 0x%x, reportTo: %s",
+                      readMsg.tag,
+                      readMsg.tagRangeEnd,
+                      EnumToString(readMsg.to));
+  }
+  
+  // Create RecvDataObject
+  if (nullptr == data) {
+    _recvDataMap[entryTag].data = new std::vector<u8>();
+    _recvDataMap[entryTag].deleteVectorWhenDone = true;
+  } else {
+    _recvDataMap[entryTag].data = data;
+    _recvDataMap[entryTag].deleteVectorWhenDone = false;
+  }
+  _recvDataMap[entryTag].callback = callback;
   
   return true;
 }
@@ -111,14 +148,31 @@ void NVStorageComponent::Update()
     u32 bytesToSend = MIN(bytesLeftToSend, _kMaxNvStorageBlobSize);
     assert(bytesToSend > 0);
     
-    NVStorage::NVStorageBlob blobMsg;
-    blobMsg.tag = sendData->nextTag;
-    blobMsg.blob = std::vector<u8>(sendData->data.begin() + sendData->sendIndex,
-                                   sendData->data.begin() + sendData->sendIndex + bytesToSend);
-    _robot.SendMessage(RobotInterface::EngineToRobot(std::move(blobMsg)));
-    
+    NVStorage::NVStorageWrite writeMsg;
+    writeMsg.reportTo = NVStorage::NVReportDest::ENGINE;
+    writeMsg.writeNotErase = true;
+    writeMsg.reportEach = false;
+    writeMsg.reportDone = true;
+    writeMsg.rangeEnd = (u32)NVStorage::NVEntryTag::NVEntry_Invalid;
+    writeMsg.entry.tag = sendData->nextTag;
+    writeMsg.entry.blob = std::vector<u8>(sendData->data.begin() + sendData->sendIndex,
+                                          sendData->data.begin() + sendData->sendIndex + bytesToSend);
+    _robot.SendMessage(RobotInterface::EngineToRobot(std::move(writeMsg)));
+
+    _sentWriteTags[sendData->baseTag].insert(sendData->nextTag);
     ++sendData->nextTag;
     sendData->sendIndex += bytesToSend;
+
+
+    
+    if (DEBUG_NVSTORAGE_COMPONENT) {
+      PRINT_NAMED_DEBUG("NVStorageComponent.Update.SendingWriteMsg",
+                        "tag: 0x%x, write: %d, bytesSent: %d",
+                        writeMsg.entry.tag,
+                        writeMsg.writeNotErase,
+                        bytesToSend);
+    }
+
     
     // Sent last blob?
     if (bytesToSend < _kMaxNvStorageBlobSize) {
@@ -130,30 +184,42 @@ void NVStorageComponent::Update()
 
 void NVStorageComponent::HandleNVData(const AnkiEvent<RobotInterface::RobotToEngine>& message)
 {
-  NVStorage::NVStorageBlob nvBlob = message.GetData().Get_nvData().blob;
+  RobotInterface::NVReadResultToEngine nvReadResult = message.GetData().Get_nvData();
+  NVStorage::NVStorageBlob& nvBlob = nvReadResult.blob;
+
   
   // Check that this was actually requested data
-  if (_recvDataMap.find((NVStorage::NVEntryTag)nvBlob.tag) == _recvDataMap.end()) {
-    PRINT_NAMED_WARNING("NVStorageComponent.HandleNVData.TagNotRequested", "tag %d", nvBlob.tag);
+  u32 baseTag = GetBaseEntryTag(nvBlob.tag);
+  if (_recvDataMap.find(baseTag) == _recvDataMap.end()) {
+    PRINT_NAMED_WARNING("NVStorageComponent.HandleNVData.TagNotRequested", "tag 0x%x", nvBlob.tag);
     return;
   }
   
+  if (DEBUG_NVSTORAGE_COMPONENT) {
+    PRINT_NAMED_DEBUG("NVStorageComponent.HandleNVData.DataRecvd",
+                      "tag: 0x%x", nvBlob.tag);
+  }
   
   if (IsMultiBlobEntryTag(nvBlob.tag)) {
     // Make sure receive vector is large enough to hold data
-    u32 baseTag = GetBaseEntryTag(nvBlob.tag);
     u32 blobIndex = nvBlob.tag - baseTag;
     u32 blobSize = (u32)nvBlob.Size();
     u32 startWriteIndex = blobIndex *_kMaxNvStorageBlobSize;
     u32 newTotalEntrySize = startWriteIndex + blobSize;
 
-    std::vector<u8>* vec = _recvDataMap[(NVStorage::NVEntryTag)baseTag].data;
+    std::vector<u8>* vec = _recvDataMap[baseTag].data;
     if (vec->size() < newTotalEntrySize) {
       vec->resize(newTotalEntrySize);
     }
     
-    // Store data
+    // Store data at appropriate in receive vector
     std::copy(nvBlob.blob.begin(), nvBlob.blob.end(), vec->begin() + startWriteIndex);
+    
+    if (DEBUG_NVSTORAGE_COMPONENT) {
+      PRINT_NAMED_DEBUG("NVStorageComponent.HandelNVData.ReceivedBlob",
+                        "Tag: 0x%x, BaseEntryTag: 0x%x, BlobSize %d, StartWriteIndex: %d",
+                        nvBlob.tag, baseTag, blobSize, startWriteIndex);
+    }
     
   } else {
     
@@ -161,54 +227,83 @@ void NVStorageComponent::HandleNVData(const AnkiEvent<RobotInterface::RobotToEng
     // TODO: Or should I expect a NvAllBlobsSent message even for single blob messages?
     
     PRINT_NAMED_INFO("NVStorageComponent.HandleNVData.MsgRecvd", "Executing callback");
-    _recvDataMap[(NVStorage::NVEntryTag)nvBlob.tag].callback(nvBlob.blob.data(), nvBlob.blob.size());
+    _recvDataMap[baseTag].callback(nvBlob.blob.data(), nvBlob.blob.size());
+    _recvDataMap.erase(baseTag);
   }
-  
-  
-  /*
-  switch((NVStorage::NVEntryTag)nvBlob.tag)
-  {
-    case NVStorage::NVEntryTag::NVEntry_CameraCalibration:
-    {
-      PRINT_NAMED_INFO("Robot.HandleNVData.CameraCalibration","");
-      
-      CameraCalibration payload;
-      payload.Unpack(nvBlob.blob.data(), nvBlob.blob.size());
-      PRINT_NAMED_INFO("RobotMessageHandler.CameraCalibration",
-                       "Received new %dx%d camera calibration from robot.", payload.ncols, payload.nrows);
-      
-      // Convert calibration message into a calibration object to pass to the robot
-      Vision::CameraCalibration calib(payload.nrows,
-                                      payload.ncols,
-                                      payload.focalLength_x,
-                                      payload.focalLength_y,
-                                      payload.center_x,
-                                      payload.center_y,
-                                      payload.skew);
-      
-      _robot.GetVisionComponent().SetCameraCalibration(calib);
-      _robot.SetPhysicalRobot(payload.isPhysicalRobots);
-      break;
-    }
-    case NVStorage::NVEntryTag::NVEntry_APConfig:
-    case NVStorage::NVEntryTag::NVEntry_StaConfig:
-    case NVStorage::NVEntryTag::NVEntry_SDKModeUnlocked:
-    case NVStorage::NVEntryTag::NVEntry_Invalid:
-      PRINT_NAMED_WARNING("Robot.HandleNVData.UnhandledTag", "TODO: Implement handler for tag %d", nvBlob.tag);
-      break;
-    default:
-      PRINT_NAMED_WARNING("Robot.HandleNVData.InvalidTag", "Invalid tag %d", nvBlob.tag);
-      break;
-  }
-   */
-  
-  
 }
 
 void NVStorageComponent::HandleNVOpResult(const AnkiEvent<RobotInterface::RobotToEngine>& message)
 {
-  NVStorage::NVOpResult payload = message.GetData().Get_nvResult();
-  PRINT_NAMED_INFO("Robot.HandleNVOpResult","Tag: %d, Result: %d, write: %d", payload.tag, (int)payload.result, payload.write);
+  RobotInterface::NVOpResultToEngine payload = message.GetData().Get_nvResult();
+  if (DEBUG_NVSTORAGE_COMPONENT) {
+    PRINT_NAMED_DEBUG("NVStorageComponent.HandleNVOpResult.Recvd",
+                      "RobotAddr: %u.%u.%u.%u, Tag: 0x%x, Result: %s, write: %d",
+                      (payload.robotAddress & 0xf000) >> 24,
+                      (payload.robotAddress & 0x0f00) >> 16,
+                      (payload.robotAddress & 0x00f0) >> 8,
+                      (payload.robotAddress & 0x000f),
+                      payload.report.tag,
+                      EnumToString(payload.report.result),
+                      payload.report.write);
+  }
+  
+
+  
+  u32 tag = (u32)payload.report.tag;   // TODO: Make report.tag u32?
+  u32 baseTag = GetBaseEntryTag(tag);
+  if (payload.report.write) {
+    // Check that this was actually written data
+    if (_sentWriteTags.find(baseTag) == _sentWriteTags.end()) {
+      PRINT_NAMED_WARNING("NVStorageComponent.HandleNVOpResult.AckdTagBaseTagWasNeverSent",
+                          "tag 0x%x, baseTag 0x%x", tag, baseTag);
+      return;
+    }
+    
+    if (_sentWriteTags[baseTag].find(tag) == _sentWriteTags[baseTag].end()) {
+      PRINT_NAMED_WARNING("NVStorageComponent.HandleNVOpResult.AckdTagWasNeverSent",
+                          "tag 0x%x, baseTag 0x%x", tag, baseTag);
+      return;
+    }
+
+    _sentWriteTags[baseTag].erase(tag);
+    if (_sentWriteTags[baseTag].size() == 0) {
+      PRINT_NAMED_INFO("NVStorageComponent.HandleNVOpResult.MsgWriteConfirmed", "%s", EnumToString((NVStorage::NVEntryTag)baseTag));
+    }
+
+    
+    if (payload.report.result != NVStorage::NVResult::NV_OKAY) {
+      PRINT_NAMED_WARNING("NVStorageComponent.HandleNVOpResult.WriteOpFailed",
+                          "Tag: 0x%x, write: %d, result: %s",
+                          tag, payload.report.write, EnumToString(payload.report.result));
+      return;
+    }
+
+    
+  } else {
+    // Check that this was actually requested data
+    if (_recvDataMap.find(baseTag) == _recvDataMap.end()) {
+      PRINT_NAMED_WARNING("NVStorageComponent.HandleNVOpResult.AckdTagNeverRequested", "tag 0x%x", tag);
+      return;
+    }
+    
+    if (payload.report.result != NVStorage::NVResult::NV_OKAY) {
+      PRINT_NAMED_WARNING("NVStorageComponent.HandleNVOpResult.ReadOpFailed",
+                          "Tag: 0x%x, write: %d, result: %s",
+                          tag, payload.report.write, EnumToString(payload.report.result));
+      return;
+    }
+
+    
+    // Execute callback
+    if ( _recvDataMap[baseTag].callback ) {
+      PRINT_NAMED_DEBUG("NVStorageComponent.HandleNVOpResult.MultiBlobReadComplete", "Executing callback");
+      _recvDataMap[baseTag].callback(_recvDataMap[baseTag].data->data(), _recvDataMap[baseTag].data->size());
+    }
+    
+    // Delete recvData object
+    _recvDataMap.erase(baseTag);
+  }
+
 }
 
 
