29b6f8cd44cf39b37ae0855998e81a06fe23d777
Ivy Ngo <ivy@anki.com>
2016-04-13 14:11:26 -0700

    COZMO-1114: Adding menu item so that we can check for duplicate asset references in bundles

--- unity/Cozmo/Assets/SharedAssets/Scripts/Startup/CozmoAssetBundleNames.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Startup/CozmoAssetBundleNames.cs
@@ -1,7 +1,6 @@
 ﻿public static class CozmoAssetBundleNames {
+  // TODO: Don't hardcode reference
   public const string BasicUIPrefabsBundleName = "basic-ui-prefabs";
   public const string GameMetadataBundleName = "game-metadata";
-
   public const string MinigameUIPrefabsBundleName = "minigame-ui-prefabs";
-  // Helper methods to load ScriptableObjects?
 }
