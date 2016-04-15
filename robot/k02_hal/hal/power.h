f1d5afb94a6557d1b8474e64b5b3b9ff8157bdaa
Bryon Vandiver <unicdk@gmail.com>
2016-04-15 13:02:59 -0700

    Checkpoint

---
+++ robot/k02_hal/hal/power.h
@@ -0,0 +1,15 @@
+#ifndef __POWER_H
+#define __POWER_H
+
+namespace Anki {
+  namespace Cozmo {
+    namespace HAL {
+      namespace Power {
+        void enableEspressif(void);
+        void disableEspressif(void);
+      }
+    }
+  }
+}
+
+#endif
