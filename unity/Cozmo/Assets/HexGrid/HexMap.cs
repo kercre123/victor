cfab0d028a195b6080bc820460da71945549eeab
Pohung Chen <pohung@anki.com>
2016-04-18 16:10:55 -0700

    renaming and comments

--- unity/Cozmo/Assets/HexGrid/HexMap.cs
+++ unity/Cozmo/Assets/HexGrid/HexMap.cs
@@ -6,10 +6,14 @@ public class HexMap {
   private Dictionary<Coord, HexItem> _HexItems = new Dictionary<Coord, HexItem>();
   private HexSet _Map;
 
+  // determines if hexItem at coord can fit inside of _Map.
   public bool CanAdd(HexItem hexItem, Coord coord) {
     return false;
   }
 
+  // actually attempts to add hexItem at coord. returns false
+  // if it does not fit. returns true when it has been successfully
+  // added to _Map
   public bool TryAdd(HexItem hexItem, Coord coord) {
     if (!CanAdd(hexItem, coord)) {
       return false;
--- unity/Cozmo/Assets/HexGrid/HexSet.cs
+++ unity/Cozmo/Assets/HexGrid/HexSet.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 
 public class HexSet : ScriptableObject {
   [SerializeField]
-  private List<Coord> HexList = new List<Coord>();
+  private List<Coord> CoordsList = new List<Coord>();
 
   public HashSet<Coord> HexSetData = new HashSet<Coord>();
 
+  // actually put the stuff in the serialized coordinates list into the hex set.
   public HexSet() {
-    for (int i = 0; i < HexList.Count; ++i) {
-      HexSetData.Add(HexList[i]);
+    for (int i = 0; i < CoordsList.Count; ++i) {
+      HexSetData.Add(CoordsList[i]);
     }
   }
 
