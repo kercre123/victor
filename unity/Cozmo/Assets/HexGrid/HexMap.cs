85b0bcf801d4cc41c17a0bf2612bbac24f104568
Pohung Chen <pohung@anki.com>
2016-04-19 16:40:45 -0700

    cleaned up code a bit and added comments

--- unity/Cozmo/Assets/HexGrid/HexMap.cs
+++ unity/Cozmo/Assets/HexGrid/HexMap.cs
@@ -6,21 +6,36 @@ public class HexMap {
   private Dictionary<Coord, PuzzlePiece> _OccupancyMap = new Dictionary<Coord, PuzzlePiece>();
   private HexSet _Map;
 
-  public bool TryRemove(Coord mapCoord) {
+  // tries to remove the PuzzlePiece at mapCoord. returns true if a piece was successfully removed.
+  // returns false if mapCoord was an available cell.
+  public bool TryRemove(Coord mapCoord, out PuzzlePiece removedPiece) {
     if (!_OccupancyMap.ContainsKey(mapCoord)) {
+      removedPiece = null;
       return false;
     }
 
+    // save the puzzle piece we are removing
+    removedPiece = _OccupancyMap[mapCoord];
+
+    // get the coordinates to remove in local puzzle piece space
     HashSet<Coord> toRemoveLocalCoords = _OccupancyMap[mapCoord].PieceData.HexSet.HexSetData;
     foreach (Coord localCoord in toRemoveLocalCoords) {
+      // convert to mapCoord space by adding the map position of the puzzle piece we are
+      // removing
       _OccupancyMap.Remove(localCoord + _OccupancyMap[mapCoord].MapPosition);
     }
+
     return true;
   }
 
   public bool CanAdd(PuzzlePiece hexItem, Coord mapCoord) {
     foreach (Coord localCoord in hexItem.PieceData.HexSet.HexSetData) {
+      // check to see if the coordinate has been occupied by another piece.
       if (_OccupancyMap.ContainsKey(mapCoord + localCoord)) {
+        return false;
+      }
+      // check to see if the coordinate exists in the map hex set.
+      if (_Map.HexSetData.Contains(mapCoord + localCoord) == false) {
         return false;
       }
     }
@@ -31,6 +46,8 @@ public class HexMap {
     if (!CanAdd(hexItem, mapCoord)) {
       return false;
     }
+
+    // update the occupancy map
     foreach (Coord localCoord in hexItem.PieceData.HexSet.HexSetData) {
       _OccupancyMap.Add(mapCoord + localCoord, hexItem);
     }
