042e172df2b62f88cff5b343a07965ff0290f361
Pohung Chen <pohung@anki.com>
2016-04-22 13:48:01 -0700

    UI upgrades

--- unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
+++ unity/Cozmo/Assets/FactoryTest/FactoryIntroManager.cs
@@ -42,6 +42,9 @@ public class FactoryIntroManager : MonoBehaviour {
   [SerializeField]
   private Canvas _Canvas;
 
+  [SerializeField]
+  private UnityEngine.UI.Image _InProgressSpinner;
+
   private int _StationNumber = 0;
   private int _TestNumber = 0;
   private bool _IsSim = false;
@@ -49,7 +52,7 @@ public class FactoryIntroManager : MonoBehaviour {
   private List<string> _LogList = new List<string>();
 
   void Start() {
-    
+    _RestartButton.onClick.AddListener(() => RestartTestApp());
     SetStatusText("Not Connected");
     RobotEngineManager.Instance.RobotConnected += HandleConnected;
     RobotEngineManager.Instance.DisconnectedFromClient += HandleDisconnectedFromClient;
@@ -58,6 +61,7 @@ public class FactoryIntroManager : MonoBehaviour {
     _LogsButton.onClick.AddListener(HandleLogButtonClick);
     _OptionsButton.onClick.AddListener(HandleOptionsButtonClick);
     _StartButton.onClick.AddListener(HandleStartButtonClick);
+    _InProgressSpinner.gameObject.SetActive(false);
   }
 
   private void HandleStationNumberUpdate(int update) {
@@ -78,6 +82,7 @@ public class FactoryIntroManager : MonoBehaviour {
   private void HandleStartButtonClick() {
     ShowDevConnectDialog();
     _StartButton.gameObject.SetActive(false);
+    _InProgressSpinner.gameObject.SetActive(true);
   }
 
   private void HandleConnected(int robotID) {
@@ -93,7 +98,6 @@ public class FactoryIntroManager : MonoBehaviour {
     RobotEngineManager.Instance.CurrentRobot.SetEnableSOSLogging(true);
     SOSLogManager.Instance.RegisterListener(HandleNewSOSLog);
     _RestartButton.gameObject.SetActive(true);
-    _RestartButton.onClick.AddListener(() => RestartTestApp());
   }
 
   private void HandleLogButtonClick() {
@@ -148,10 +152,14 @@ public class FactoryIntroManager : MonoBehaviour {
       return; // quit case
     }
     _Background.color = Color.red;
+    _InProgressSpinner.gameObject.SetActive(false);
+    _RestartButton.gameObject.SetActive(true);
   }
 
   private void TestPassed() {
     _Background.color = Color.green;
+    _InProgressSpinner.gameObject.SetActive(false);
+    _RestartButton.gameObject.SetActive(true);
   }
 
   private void RestartTestApp() {
