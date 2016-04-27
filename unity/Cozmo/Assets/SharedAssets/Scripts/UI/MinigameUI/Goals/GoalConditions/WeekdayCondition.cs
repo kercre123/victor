be34faa2e5b9933dbb16518f6ddb3af2d3d13e4b
Ryan Anderson <ryan@anki.com>
2016-04-26 17:09:26 -0700

    Use Enum.Parse instead of map for DayOfWeek

--- unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/Goals/GoalConditions/WeekdayCondition.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/Goals/GoalConditions/WeekdayCondition.cs
@@ -15,34 +15,27 @@ namespace Anki {
     [System.Serializable]
     public class WeekdayCondition : GoalCondition {
       [SerializeField]
-      public string dayName;
-      DayOfWeek day;
+      private string _DayName;
+      private DayOfWeek _Day;
 
       // Returns true if day of the week is the desired day of the week
       public override bool ConditionMet() {
-        return (DateTime.UtcNow.DayOfWeek.ToString() == dayName);
+        return (DateTime.UtcNow.DayOfWeek.ToString() == _DayName);
       }
 
       #if UNITY_EDITOR
       public override void DrawControls() {
-        if (day.ToString() != dayName) {
-          if (!strToDay.TryGetValue(dayName, out day)) {
-            day = DayOfWeek.Sunday;
+        if (_Day.ToString() != _DayName) {
+          try {
+            _Day = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), _DayName);
+          }
+          catch (ArgumentException) {
+            _Day = DayOfWeek.Sunday;
           }
         }
-        day = (DayOfWeek)EditorGUILayout.EnumPopup("Day", day);
-        dayName = day.ToString();
+        _Day = (DayOfWeek)EditorGUILayout.EnumPopup("Day", _Day);
+        _DayName = _Day.ToString();
       }
-      // Map strings to DayOfWeek because DayOfWeek is the worst... THE WORST
-      Dictionary<string, DayOfWeek> strToDay = new Dictionary<string, DayOfWeek>() {
-        { "Monday", DayOfWeek.Monday },
-        { "Tuesday", DayOfWeek.Tuesday },
-        { "Wednesday", DayOfWeek.Wednesday },
-        { "Thursday", DayOfWeek.Thursday },
-        { "Friday", DayOfWeek.Friday },
-        { "Saturday", DayOfWeek.Saturday },
-        { "Sunday", DayOfWeek.Sunday }
-      };
       #endif
     }
   }
