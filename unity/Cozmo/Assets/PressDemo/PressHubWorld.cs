670a1a4183dfb455aeab178f62b59934b1abf1f4
Pohung Chen <pohung@anki.com>
2016-04-27 14:59:50 -0700

    more press demo setup

---
+++ unity/Cozmo/Assets/PressDemo/PressHubWorld.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PressHubWorld : HubWorldBase {
+
+  private GameBase _MiniGameInstance;
+
+  [SerializeField]
+  private ChallengeData _SpeedTapChallengeData;
+
+  [SerializeField]
+  private ChallengeData _FaceEnrollmentChallengeData;
+
+  public override bool LoadHubWorld() {
+    StartSpeedTapGame();
+    return true;
+  }
+
+  public override bool DestroyHubWorld() {
+    return true;
+  }
+
+  private void StartFaceEnrollmentActivity() {
+    PlayMinigame(_FaceEnrollmentChallengeData);
+  }
+
+  private void StartSpeedTapGame() {
+    PlayMinigame(_SpeedTapChallengeData);
+  }
+
+  private void PlayMinigame(ChallengeData challengeData) {
+    GameObject newMiniGameObject = GameObject.Instantiate(challengeData.MinigamePrefab);
+    _MiniGameInstance = newMiniGameObject.GetComponent<GameBase>();
+    _MiniGameInstance.InitializeMinigame(challengeData);
+    _MiniGameInstance.OnMiniGameQuit += HandleMiniGameQuit;
+    _MiniGameInstance.OnMiniGameWin += HandleMinigameOver;
+    _MiniGameInstance.OnMiniGameLose += HandleMinigameOver;
+  }
+
+  private void HandleMinigameOver(Transform[] rewards) {
+    HandleMiniGameQuit();
+  }
+
+  private void HandleMiniGameQuit() {
+    
+  }
+
+}
