8b7e2f0779cf3e531cf9e8ef3790205dd313f26a
Bryon Vandiver <unicdk@gmail.com>
2016-04-27 15:54:43 -0700

    Added radio RSSI tester

---
+++ robot/syscon/radio_test/radio_tester.cpp
@@ -0,0 +1,74 @@
+extern "C" {
+  #include "nrf.h"
+  #include "nrf_gpio.h"
+  #include "nrf_sdm.h"
+}
+
+static const uint32_t TX_PIN = 17;
+static const uint32_t RX_PIN = 18;
+
+static uint8_t measure(uint8_t frequency) {
+  NRF_RADIO->FREQUENCY = frequency;
+  
+  NRF_RADIO->EVENTS_READY = 0;
+  NRF_RADIO->TASKS_RXEN = 1;
+  while(NRF_RADIO->EVENTS_READY == 0);
+  
+  NRF_RADIO->EVENTS_END = 0;
+  NRF_RADIO->TASKS_START = 1;
+
+  NRF_RADIO->EVENTS_RSSIEND = 0;
+  NRF_RADIO->TASKS_RSSISTART = 1;
+  while(NRF_RADIO->EVENTS_RSSIEND == 0);
+
+  uint8_t data = NRF_RADIO->RSSISAMPLE;
+
+  NRF_RADIO->EVENTS_DISABLED = 0;    
+  NRF_RADIO->TASKS_DISABLE   = 1;
+
+  while(NRF_RADIO->EVENTS_DISABLED == 0) ;
+
+  return data;
+}
+
+int main(void)
+{
+  // Start 16 MHz crystal oscillator.
+  NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+  NRF_CLOCK->TASKS_HFCLKSTART    = 1;
+
+  // Wait for the external oscillator to start up.
+  while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) ;
+  
+  // Enable and configure the uart
+  nrf_gpio_cfg_input(RX_PIN, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_output(TX_PIN);
+
+  NRF_UART0->POWER = 1;
+
+  NRF_UART0->CONFIG = 0;
+  NRF_UART0->ENABLE = UART_ENABLE_ENABLE_Enabled << UART_ENABLE_ENABLE_Pos;
+    
+  NRF_UART0->BAUDRATE = UART_BAUDRATE_BAUDRATE_Baud1M;
+  NRF_UART0->PSELTXD = TX_PIN;
+  NRF_UART0->PSELRXD = RX_PIN;
+
+  NRF_UART0->TASKS_STARTRX = 1;
+  NRF_UART0->TASKS_STARTTX = 1;
+
+  // Power our radio
+  NRF_RADIO->POWER = 1;
+
+  // Run forever, because we are awesome.
+  uint8_t channel = 71;
+  for (;;) {    
+    while (!NRF_UART0->EVENTS_RXDRDY) ;
+    uint8_t channel = NRF_UART0->RXD;
+    NRF_UART0->EVENTS_RXDRDY = 0;
+    uint8_t reading = measure(channel);
+
+    
+    NRF_UART0->TXD = channel;
+    NRF_UART0->TXD = reading;
+  }
+}
