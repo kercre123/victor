a6cefd3981bb24dc52a49bb334f05709a39730c5
Molly Jameson <MollyJameson@users.noreply.github.com>
2016-04-29 04:34:24 -0700

    COZMO-1455 game side of showing latency. (#1051)
    * COZMO-1455 game side of showing latency.
    
    * Real engine pointer on master
    
    * Really killing progression system
    
    * Engine pointer bump
    
    * ProgressionStatType moved to unity since it's not part of engine anymore
    
    * iOS project revert

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/CozmoStats/ProgressionStatType.cs
@@ -0,0 +1,16 @@
+﻿namespace Anki {
+
+  namespace Cozmo {
+
+    public enum ProgressionStatType : byte {
+      Time,
+      Novelty,
+      Excitement,
+      Count}
+
+    ;
+
+  }
+  // namespace Cozmo
+
+} // namespace Anki
