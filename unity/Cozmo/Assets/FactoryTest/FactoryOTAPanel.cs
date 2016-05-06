e5811df6f2524d2bf6b69bea45a2f6cd17b3d442
Kevin Yoon <kevin@anki.com>
2016-05-06 11:42:14 +0800

    Body stage removed from OTA

--- unity/Cozmo/Assets/FactoryTest/FactoryOTAPanel.cs
+++ unity/Cozmo/Assets/FactoryTest/FactoryOTAPanel.cs
@@ -20,12 +20,12 @@ public class FactoryOTAPanel : MonoBehaviour {
 
   private void OnFirmwareUpdateProgress(Anki.Cozmo.ExternalInterface.FirmwareUpdateProgress message) {
     _OTAStatus.text = "InProgress: Robot " + message.robotID + " Stage: " + message.stage + ":" + message.subStage + " " + message.percentComplete + "%"
-    + "\nWifi = " + message.fwSigWifi + "\nRTIP = " + message.fwSigRTIP + "\nBody = " + message.fwSigBody;
+    + "\nWifi = " + message.fwSigWifi + "\nRTIP = " + message.fwSigRTIP;
   }
 
   private void OnFirmwareUpdateComplete(Anki.Cozmo.ExternalInterface.FirmwareUpdateComplete message) {
     _OTAStatus.text = "Complete: Robot " + message.robotID + " Result: " + message.result
-    + "\nWifi = " + message.fwSigWifi + "\nRTIP = " + message.fwSigRTIP + "\nBody = " + message.fwSigBody;
+    + "\nWifi = " + message.fwSigWifi + "\nRTIP = " + message.fwSigRTIP;
 
     _RestartButton.gameObject.SetActive(true);
 
