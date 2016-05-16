9a9763fc11a165e063a58ddfc5fe7e74f5469a03
Pohung Chen <pohung@anki.com>
2016-05-16 14:23:27 -0700

    spinner UI assets and code

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/Spinner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spinner : MonoBehaviour {
+  [SerializeField]
+  private UnityEngine.UI.Image _ImageToSpin;
+
+  [Tooltip("in radians per second")]
+  [SerializeField]
+  private float _SpinSpeed;
+
+  void Update() {
+    _ImageToSpin.transform.Rotate(new Vector3(0.0f, 0.0f, _SpinSpeed * Time.deltaTime));
+  }
+}
