d3770a0f19d55b1b1f317ef58660194a180e957e
Bryon Vandiver <unicdk@gmail.com>
2016-05-25 21:08:57 -0700

    Added reversal function for diffie hellman

--- crypto/aes.h
+++ crypto/aes.h
@@ -7,15 +7,19 @@ extern "C" {
 }
 
 static const int AES_KEY_LENGTH = 16;
-static const int AES_BLOCK_LENGTH = 16;
 
 struct ecb_data_t
 {
   uint8_t key[AES_KEY_LENGTH];
-  uint8_t cleartext[AES_BLOCK_LENGTH];
-  uint8_t ciphertext[AES_BLOCK_LENGTH];
+  uint8_t cleartext[AES_KEY_LENGTH];
+  uint8_t ciphertext[AES_KEY_LENGTH];
 };
 
+// Do not use these directly (software fallbacks)
+void AES128_ECB_encrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_ECB_decrypt(uint8_t* input, const uint8_t* key, uint8_t *output);
+void AES128_CBC_encrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
+void AES128_CBC_decrypt_buffer(uint8_t* output, uint8_t* input, uint32_t length, const uint8_t* key, const uint8_t* iv);
 
 void aes_key_init();
 void aes_ecb(ecb_data_t* ecb);
