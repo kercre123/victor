c0b0e59df3c3e9058d61fc87f5a3a7bffc3513fd
Andrew Stein <andrew@anki.com>
2016-05-31 15:23:02 -0700

    Green lights for interacting light state

--- basestation/src/anki/cozmo/basestation/components/lightsComponent.cpp
+++ basestation/src/anki/cozmo/basestation/components/lightsComponent.cpp
@@ -154,7 +154,7 @@ void LightsComponent::SetLights(ObjectID object, CubeLightsState state)
     case CubeLightsState::Interacting:
       _robot.SetObjectLights(object,
                              WhichCubeLEDs::ALL,
-                             NamedColors::ORANGE, NamedColors::ORANGE,
+                             NamedColors::GREEN, NamedColors::GREEN,
                              1000, 0,
                              500, 500,
                              true,
