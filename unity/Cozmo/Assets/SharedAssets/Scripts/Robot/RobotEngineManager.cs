ea7157ab3df4ae51c125f5945e2a326cdcb082d8
Ivy Ngo <ivy@anki.com>
2016-06-06 15:01:09 -0700

    COZMO-936: Fixing hockeyapp crash by demoting it to a warning

--- unity/Cozmo/Assets/SharedAssets/Scripts/Robot/RobotEngineManager.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Robot/RobotEngineManager.cs
@@ -648,11 +648,18 @@ public class RobotEngineManager : MonoBehaviour {
     }
 
     if (actionType == RobotActionType.PLAY_ANIMATION) {
-      // Reset cozmo's face
-      string animationCompleted = message.completionInfo.animationCompleted.animationName;
+      if (message.completionInfo.GetTag() == ActionCompletedUnion.Tag.animationCompleted) {
+        // Reset cozmo's face
+        string animationCompleted = message.completionInfo.animationCompleted.animationName;
 
-      if (RobotCompletedAnimation != null) {
-        RobotCompletedAnimation(success, animationCompleted);
+        if (RobotCompletedAnimation != null) {
+          RobotCompletedAnimation(success, animationCompleted);
+        }
+      }
+      else {
+        DAS.Warn("RobotEngineManager.ReceivedSpecificMessage(G2U.RobotCompletedAction message)", 
+          string.Format("Message is of type RobotActionType.PLAY_ANIMATION but message.completionInfo has tag {0} instead of ActionCompletedUnion.Tag.animationCompleted! idTag={1} result={2}", 
+            message.completionInfo.GetTag(), message.idTag, message.result));
       }
     }
 
