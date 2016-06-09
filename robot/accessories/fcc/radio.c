97a52d5c30abce5d578657eac3ab9da121cf6618
Nathan Monson <nathan@anki.com>
2016-06-09 14:11:20 -0700

    Fixture c23 - hacks for low-power FCC testing.

--- robot/accessories/fcc/radio.c
+++ robot/accessories/fcc/radio.c
@@ -39,13 +39,13 @@ void delayms(u8 delay);
 
 // Test mode configuration: CONFIG (RX/TX), RF_SETUP (power/data rate), CHAN, unused
 u8 code TESTS[] = {
-  PRIM_RX|PWR_UP, RF_PWR1|PLL_LOCK, 2,  0,
+  PRIM_RX|PWR_UP, RF_PWR1, 2,  0,
   
-  PWR_UP,         RF_PWR1|PLL_LOCK, 2,  0,
-  PWR_UP,         RF_PWR1|PLL_LOCK, 42, 0,
-  PWR_UP,         RF_PWR1|PLL_LOCK, 81, 0,
+  PWR_UP,         RF_PWR1, 2,  0,
+  PWR_UP,         RF_PWR1, 42, 0,
+  PWR_UP,         RF_PWR1, 81, 0,
   
-  PRIM_RX|PWR_UP, RF_PWR1|PLL_LOCK, 81, 0,
+  PRIM_RX|PWR_UP, RF_PWR1, 81, 0,
   
   PWR_UP,         RF_PWR1|PLL_LOCK|CONT_WAVE, 2,  0,
   PWR_UP,         RF_PWR1|PLL_LOCK|CONT_WAVE, 42, 0,
@@ -74,42 +74,40 @@ void TransmitData()
 
   writeReg(SETUP_AW, 2);   // 4 bytes
   
-  // Receive length is 32 bytes (on both pipes)
-  writeReg(RX_PW_P0, 32);
-  writeReg(RX_PW_P1, 32);
-
-  // Clear radio IRQ flags and RRF
-  writeReg(STATUS, RX_DR | TX_DS | MAX_RT);
-  RFF = 0;  // The "8051 way" to acknowledge an untaken interrupt
-
-  // Flush the last packet we received
-  writeReg(FLUSH_RX, 0);
-  
   // Power up in receive or transmit mode
   writeReg(CONFIG, TESTS[i+0]);
-  delayms(2);
+  delayms(5);
 
-  // Enable radio
-  RFCE = 1;
-  
   // Tests 1-3, send packets forever
   if (_whichTest && !(_whichTest & 4))
   {
-    writeMultiReg(W_TX_PAYLOAD_NOACK, _radioPayload, 17);   // Buffer one extra
+    // Continuous: writeMultiReg(W_TX_PAYLOAD_NOACK, _radioPayload, 17);   // Buffer one extra
     while (1)
     {
+      delayms(5);
+      i++;
       LedTest(_whichTest);
-      writeMultiReg(W_TX_PAYLOAD_NOACK, _radioPayload, 17);
+      if (i == 7)
+      {
+        // Our craziest case is 3 packets per 35ms on same channel - or 9 packets hopping!
+        i = 0;
+        writeMultiReg(W_TX_PAYLOAD_NOACK, _radioPayload, 32);
+        writeMultiReg(W_TX_PAYLOAD_NOACK, _radioPayload, 32);
+        writeMultiReg(W_TX_PAYLOAD_NOACK, _radioPayload, 32);
 
-      // Clear radio IRQ flags and RRF
-      writeReg(STATUS, RX_DR | TX_DS | MAX_RT);
-      RFF = 0;  // The "8051 way" to acknowledge an untaken interrupt
-
-      // Wait for radio to signal transmit complete
-      while (!RFF)
-        ;
+        // Clear radio IRQ flags and RRF
+        writeReg(STATUS, RX_DR | TX_DS | MAX_RT);
+        
+        RFF = 0;  // The "8051 way" to acknowledge an untaken interrupt
+        RFCE = 1; // Kick off radio and come back in 5ms when it's done
+      } else {
+        RFCE = 0;
+      }
     }
   }
+  
+  // Enable radio
+  RFCE = 1;
   
   // Else just wait here
   while (1)
