21d78829ea240f7b16ea11d6030984703b69a19a
Ivy Ngo <ivy@anki.com>
2016-06-14 14:58:54 -0700

    COZMO-2238: How To Play label updates

--- unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/Slides/AnimationSlide.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/UI/MinigameUI/Slides/AnimationSlide.cs
@@ -7,16 +7,24 @@ namespace Cozmo {
     public class AnimationSlide : MonoBehaviour {
 
       [SerializeField]
+      private Anki.UI.AnkiTextLabel _HeaderTextLabel;
+
+      [SerializeField]
       private Anki.UI.AnkiTextLabel _InfoTextLabel;
 
       [SerializeField]
       private RectTransform _AnimationContainer;
 
-      public void Initialize(GameObject animationPrefab, string infoLocKey) {
+      public void Initialize(GameObject animationPrefab, string infoLocKey, string headerLocKey = null) {
         if (animationPrefab != null) {
           UIManager.CreateUIElement(animationPrefab, _AnimationContainer);
         }
         _InfoTextLabel.text = Localization.Get(infoLocKey);
+
+        _HeaderTextLabel.gameObject.SetActive(headerLocKey != null);
+        if (headerLocKey != null) {
+          _HeaderTextLabel.text = Localization.Get(headerLocKey);
+        }
       }
     }
   }
