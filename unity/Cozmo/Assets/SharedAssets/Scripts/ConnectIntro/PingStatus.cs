ed77cae7ad8549516a7e805ff71f63c9fc341fca
dcerpa-anki <dcerpa@anki.com>
2016-06-14 17:28:44 -0700

    COZMO-2167: Investigate a ping solution for the connect button (#125)
    * Reimplemented the ping functionality to use coroutines like it is suggested in the documentation.
    After a successful ping, now we continue pinging, although less often, to catch the case when we lose connectivity to the robot.
    
    * Commented out the Ping functionality because it seems to be leaking threads. The issue has been reported to Unity. In the meantime, we'll use the WiFi IP address to know if we are in the robot subnetwork.
    This allowed me to find a bug on the function that finds the WiFi address. On devices with multiple interfaces (like a laptop), it was returning the first IPv4 address which may not be the right one. Similar code on engine look for the en0 interface so I'm following suit here.

--- unity/Cozmo/Assets/SharedAssets/Scripts/ConnectIntro/PingStatus.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/ConnectIntro/PingStatus.cs
@@ -3,51 +3,87 @@ using System.Collections;
 
 public class PingStatus : MonoBehaviour {
 
-  //private UnityEngine.Ping _Ping;
-  private bool _PingSuccess = true;
-  //private float _LastPingTime = 0.0f;
+  private const float kWaitBetweenSuccesfulPings = 3.0f;
+  private const float kWaitBetweenFailedPings = 2.0f;
+  private const float kKillPingTime = 10.0f;
+  private const float kLogTime = 2.0f;
+
+  private UnityEngine.Ping _Ping;
+  private Coroutine _coroutine;
+  private bool _PingCompleted = true;
+  private bool _PingSuccessful = false;
+//  private float _LastLogTime = 0.0f;
+  private float _LastPingTime = 0.0f;
 
   public bool GetPingStatus() {
-    return _PingSuccess;
+    return _PingSuccessful;
   }
 
-  // Use this for initialization
   void Start() {
-    //SendPing();
+    _coroutine = StartCoroutine(PingCoroutine());
+  }
+
+  void OnDestroy() {
+    if (_coroutine != null) {
+      DAS.Debug(this, "Stopping ping coroutine");
+      StopCoroutine(_coroutine);
+      _coroutine = null;
+    }
   }
 	
-  // Update is called once per frame
   void Update() {
-    /*if (Time.time - _LastPingTime > 2.0f) {
-      // it's been 2 seconds since we've heard anything. try again.
-      // this handles the edge case of Ping throwing a routing exception
-      // unity is dumb and doesn't mark isDone as true if it throws an exception so there's
-      // no other way for us to tell
-      SendPing();
+    if (_PingCompleted) {
+      if (!_PingSuccessful) {
+        if (Time.time - _LastPingTime > kWaitBetweenFailedPings) {
+          _coroutine = StartCoroutine(PingCoroutine());
+          _LastPingTime = Time.time;
+        }
+      } else {
+        if (Time.time - _LastPingTime > kWaitBetweenSuccesfulPings) {
+          _coroutine = StartCoroutine(PingCoroutine());
+          _LastPingTime = Time.time;
+        }
+      }
     }
-    if (_Ping.isDone) {
-      if (_Ping.time >= 0) {
-        _PingSuccess = true;
-      }
-      else {
-        _PingSuccess = false;
-      }
-      try {
-        SendPing();
-      }
-      catch {
-        _PingSuccess = false;
-        SendPing();
-      }
-
-    }*/
   }
 
-  private void SendPing() {
-    /*_LastPingTime = Time.time;
-    if (_Ping != null) {
-      _Ping.DestroyPing();
-    }
-    _Ping = new Ping(RobotEngineManager.kRobotIP);*/
+  private IEnumerator PingCoroutine() {
+//    DAS.Debug(this, "Pinging " + RobotEngineManager.kRobotIP);
+//    _Ping = new Ping(RobotEngineManager.kRobotIP);
+//    _PingCompleted = false;
+//
+//    float startTime = Time.time;
+//    _LastLogTime = Time.time;
+//
+//    // Cancel the ping if it is not comleted in a certain amount of time to make sure it doesn't keep running forever
+//    while (!_Ping.isDone && ((Time.time - startTime) < kKillPingTime)) {
+//      if ((Time.time - _LastLogTime) > kLogTime) {
+//        DAS.Debug(this, "Still pinging " + RobotEngineManager.kRobotIP);
+//        _LastLogTime = Time.time;
+//      }
+//      yield return null;
+//    }
+//
+//    if (_Ping.isDone) {
+//      DAS.Debug(this, "Ping completed. Time is = " + _Ping.time);
+//    } else {
+//      DAS.Debug(this, "Ping failed");
+//    }
+//
+//    _PingSuccessful = _Ping.time > 0;
+//    _Ping.DestroyPing();
+//    _Ping = null;
+//    _PingCompleted = true;
+
+    // Disabling using Ping since it is leaking threads on device which
+    // is causing multiple issues especially in the factory test app.
+    // See https://fogbugz.unity3d.com/default.asp?804440_vdqj6tngkifda521
+    // While we wait for this to be fixed in Unity, we have decided to use the
+    // WiFi address as an indication of whether we are connected to the right 
+    // network.
+    System.Net.IPAddress ipAddress = RobotUdpChannel.GetLocalIPv4();
+    _PingSuccessful = (ipAddress != null) && ipAddress.ToString().StartsWith("172.31");
+    _PingCompleted = true;
+    yield break;
   }
 }
