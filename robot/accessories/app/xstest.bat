7460fce6d1facb3f273c26372d3d5e43b0c756b7
Bryon Vandiver <unicdk@gmail.com>
2016-06-15 17:36:18 -0700

    Avoid this for now

--- robot/accessories/app/xstest.bat
+++
@@ -1,6 +0,0 @@
-"c:\cygwin\bin\sed" -n '1,106p' ../boot/build/xsboot.hex > build/xstest.hex
-echo :0D3FEE000AB01EAB11CA01FFFFEF04FF0077 >> build/xstest.hex
-"c:\cygwin\bin\sed" -n '2,100p' build/xspatch.hex >> build/xstest.hex
-"C:\Program Files (x86)\Nordic Semiconductor\nRFgo Studio\nrfgostudio" -defaultsettings
-"C:\Program Files (x86)\Nordic Semiconductor\nRFgo Studio\nrfgocli" -p build/xstest.hex -b 1 -t isp -mt le1
-pause
