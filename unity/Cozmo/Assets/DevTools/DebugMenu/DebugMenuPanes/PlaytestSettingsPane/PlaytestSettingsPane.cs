36b6e7f1d647393a0164373467dbebc5d1caede2
Pohung Chen <pohung@anki.com>
2016-06-16 13:31:03 -0700

    playtest reset all the things

---
+++ unity/Cozmo/Assets/DevTools/DebugMenu/DebugMenuPanes/PlaytestSettingsPane/PlaytestSettingsPane.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using DataPersistence;
+
+public class PlaytestSettingsPane : MonoBehaviour {
+  [SerializeField]
+  UnityEngine.UI.Button _ResetPlaytestSession;
+
+  private void Start() {
+    _ResetPlaytestSession.onClick.AddListener(ResetSession);
+  }
+
+  private void ResetSession() {
+    RobotEngineManager.Instance.CurrentRobot.EraseAllEnrolledFaces();
+    SkillSystem.Instance.DebugEraseStorage();
+    // use reflection to change readonly field
+    typeof(DataPersistenceManager).GetField("Data").SetValue(DataPersistenceManager.Instance, new SaveData());
+    DataPersistenceManager.Instance.Save();
+    Application.Quit();
+  }
+
+}
