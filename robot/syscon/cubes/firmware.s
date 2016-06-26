f933e1b579fdf0bd287a95713728a92cca674ad0
Nathan Monson <nathan@anki.com>
2016-06-26 16:51:42 +0800

    Pilot firmware should use Pilot cubes.

--- robot/syscon/cubes/firmware.s
+++ robot/syscon/cubes/firmware.s
@@ -5,6 +5,7 @@
                 EXPORT  CUBE_FIRMWARE_LENGTH
 
 CUBE_UPDATE
+                INCBIN  ../../accessories/releases/xs6-p2.safe
 CUBE_FIRMWARE_LENGTH
                 DCD     CUBE_FIRMWARE_LENGTH - CUBE_UPDATE
 
