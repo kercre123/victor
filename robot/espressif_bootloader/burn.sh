a4bd69a94073afe0a5fa66d6eb2739228499315d
Daniel Casner <daniel@anki.com>
2016-07-06 16:47:35 -0700

    Fix burn script for new factory.safe location

--- robot/espressif_bootloader/burn.sh
+++ robot/espressif_bootloader/burn.sh
@@ -5,7 +5,7 @@ esptool.py --port $1 --baud 230400 write_flash --flash_size 16m --flash_freq 80m
         0x000000 firmware/cboot.bin \
         0x001000 firmware/sn.bin \
         0x080000 ../staging/esp.factory.bin \
-        0x0c5000 ../staging/factory.safe \
+        0x0c8000 ../staging/factory.safe \
         0x1fc000 ../espressif/bin/esp_init_data_default.bin \
         0x1fe000 ../espressif/bin/blank.bin \
         0x003000 ../espressif/bin/blank.bin \
