42a553c5589a224d423f42311bffd52fc264e0fb
Nathan Monson <nathan@anki.com>
2016-07-14 18:18:01 -0700

    Patch p3 improves SPI margin.

--- robot/syscon/cubes/firmware.s
+++ robot/syscon/cubes/firmware.s
@@ -7,6 +7,6 @@
 __CUBE_XS
                 INCBIN  ../../accessories/releases/xs-p5.safe
 __CUBE_XS6
-                INCBIN  ../../accessories/releases/xs6-p2.safe
+                INCBIN  ../../accessories/releases/xs6-p3.safe
 
                 END
