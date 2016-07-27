c2cdab305cd1ea0d8dd6426a9898825c28894d85
Kevin Yoon <kevin@anki.com>
2016-07-27 11:51:32 +0800

    Corrected encoder scale factor for 4T head encoders in MP robots.

--- robot/syscon/hal/motors.cpp
+++ robot/syscon/hal/motors.cpp
@@ -64,9 +64,9 @@ const u32 METERS_PER_TICK = TO_FIXED_0_32((0.948 * 0.125 * 0.0292 * 3.1415926535
 // compute the radians per tick on the lift as:
 const Fixed RADIANS_PER_LIFT_TICK = TO_FIXED((0.25 * 3.14159265359) / 172.68);
 
-// Given a gear ratio of 348.77:1 and 8 encoder ticks per revolution, we
+// Given a gear ratio of 348.77:1 and 4 encoder ticks per revolution, we
 // compute the radians per tick on the head as:
-const Fixed RADIANS_PER_HEAD_TICK = TO_FIXED((0.125 * 3.14159265359) / 348.77);
+const Fixed RADIANS_PER_HEAD_TICK = TO_FIXED((0.25 * 3.14159265359) / 348.77);
 
 // Pre-production hardware had 8 ticks
 const Fixed RADIANS_PER_HEAD_TICK_PRE_PROD = TO_FIXED((0.125 * 3.14159265359) / 348.77);
