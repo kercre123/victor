dbd1e02300a0eca5f4787806341bbe3f66faa360
Wesley <wesleyiyue@gmail.com>
2016-07-30 17:24:16 -0700

    Demangle backtrace symbols on callstack dump COZMO-3327 (#673)
    Make the callstack dump readable and demangle things like ZN4Anki4Util14sDumpCallstackERKNSt3_112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE into Anki::Util::sDumpCallstack(std::__1::basic_string, std::__1::allocator > const&)

--- lib/util/source/anki/util/logging/callstack.cpp
+++ lib/util/source/anki/util/logging/callstack.cpp
@@ -11,14 +11,20 @@
  **/
 #include "callstack.h"
 #include "util/logging/logging.h"
+#include "util/string/stringHelpers.h"
 
 #include <cstdlib>
+
+#include <string>
+#include <sstream>
+#include <vector>
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // appropriate headers per platform
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 #if defined(__MACH__) && defined(__APPLE__)
 #include <execinfo.h>
+#include <cxxabi.h>
 #elif defined(ANDROID) || defined(LINUX)
 
 #else
@@ -28,6 +34,43 @@
 namespace Anki {
 namespace Util {
 
+#if defined(__MACH__) && defined(__APPLE__)
+std::string DemangleBacktraceSymbols(const std::string& backtraceFrame) {
+  std::vector<std::string> splitFrame = SplitString(backtraceFrame, ' ');
+
+  // -4 because the valid statuses from __cxa_demangle() is 0, -1, -2, -3. If we got -4 after
+  // calling __cxa_demangle() something very wrong happened.
+  int status = -4;
+
+  // Make sure that when the backtrace frame string is split 
+  // by spaces there is at least 3 items in there.
+  if (splitFrame.size() > 3) {
+    // A backtraceFrame looks like:
+    // "0   webotsCtrlGameEngine                0x000000010cc84894 _ZN4Anki4Util14sDumpCallstackERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE + 52"
+    // The mangled symbol is the third element from the back, when split by ' '
+    splitFrame.end()[-3] = abi::__cxa_demangle(splitFrame.end()[-3].c_str(), 0, 0, &status);
+  } else {
+    PRINT_NAMED_WARNING("Callback.DemangleBacktraceSymbols",
+                        "Something is wrong with the format of the backtrace frame. It should look "
+                        "something like "
+                        "'0   webotsCtrlGameEngine                0x000000010cc84894 _ZN4Anki4Util14sDumpCallstackERKNSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEE + 52'");
+  }
+
+  if (status != 0) {
+    // Demangle didn't work, just print the raw name.
+    PRINT_NAMED_WARNING("Callback.DemangleBacktraceSymbols",
+                        "Couldn't demangle the symbol, __cxa_demangle returned status = %i", status);
+  }
+
+  std::ostringstream os;
+  for (const std::string& part : splitFrame) {
+    os << part << " ";
+  }
+
+  return os.str();
+}
+#endif  // defined(__MACH__) && defined(__APPLE__)
+
 void sDumpCallstack(const std::string& name)
 {
   #if defined(__MACH__) && defined(__APPLE__)
@@ -35,9 +78,8 @@ void sDumpCallstack(const std::string& name)
     void* callstack[128];
     int frames = backtrace(callstack, 128);
     char** strs = backtrace_symbols(callstack, frames);
-    for (int i = 0; i < frames; ++i)
-    {
-      PRINT_NAMED_INFO(name.c_str(), "%s", strs[i]);
+    for (int i = 0; i < frames; ++i) {
+      PRINT_NAMED_INFO(name.c_str(), "%s", DemangleBacktraceSymbols(strs[i]).c_str());
     }
     free(strs);
   }
