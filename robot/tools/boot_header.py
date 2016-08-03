eb3cb4070cfe11d6e133d57c08647a6505732722
Daniel Casner <daniel.t.casner@ieee.org>
2016-08-03 11:17:10 -0700

    Deal with changes in python zlib.crc32 between python 3 and python 2, why is anyone still using 2? (#732)

--- robot/tools/boot_header.py
+++ robot/tools/boot_header.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 
-from sys import argv
+from sys import argv, version_info
 from struct import pack
 from elfInfo import rom_info, HEADER_LENGTH
 import zlib
@@ -13,8 +13,10 @@ def fix_header(fn):
 	with open(fn, "rb+") as fo:
 		checksum = zlib.crc32(rom_data[HEADER_LENGTH:])
 
+		fmt = "<IIi" if version_info.major < 3 else "<III" # Zlib crc32 output changed from signed to unsigned
+
 		fo.seek(magic_location)
-		fo.write(pack("<IIi", base_addr+HEADER_LENGTH, len(rom_data) - HEADER_LENGTH, checksum))
+		fo.write(pack(fmt, base_addr+HEADER_LENGTH, len(rom_data) - HEADER_LENGTH, checksum))
 
 	return 
 
