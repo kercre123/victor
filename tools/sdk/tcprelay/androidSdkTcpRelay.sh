eabe3aec09e53b54a55175b22377f168f3e0c28e
Mark Wesley <mwesley@anki.com>
2016-08-15 17:43:54 -0700

    Add Android support for Cozmo SDK and reorganize tcprelay folders (#948)
    Renamed sdk -> oldSdk (it's legacy, but still useful for cli.py and some tkinter and similar example usage that's not in new sdk yet)
    Renamed sdk.next -> sdk (it's the primary sdk now, and this folder is the one that gets zipped up as an artifact)
    Moved oldSdk/tcprelay_usbmux_py to sdk/tcprelay (as it's now not specific to usbmux)
    Added sdk/tcprelay/androidSdkTcpRelay.sh (using adb forward)
    Renamed sdk/tcprelay/openSdkTcpRelay.sh to sdk/tcprelay/iosSdkTcpRelay.sh (to clarify iOS specific), and added call to remove adb forward first to ease switching between iOS and Android

---
+++ tools/sdk/tcprelay/androidSdkTcpRelay.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Forward a tcp connection tcp:LOCAL_PORT tcp:REMOTE_PORT
+# REMOTE_PORT: SDK_ON_DEVICE_TCP_PORT   - on app running on attached Android device over USB
+# LOCAL_PORT:  SDK_ON_COMPUTER_TCP_PORT - on local PC
+# Note: Ports can be the same number - they're on different devices
+#       This requires that an Android device is attached (otherwise you'll see "error: no devices/emulators found")
+#       The forward is removed by adb whenever the device is lost, so you must rerun the script again
+
+# remove existing adb forward, then bind, then list to verify it succeeded
+adb forward --remove tcp:5106
+adb forward --no-rebind tcp:5106 tcp:5106
+adb forward --list
--- tools/sdk/tcprelay/iosSdkTcpRelay.sh
+++ tools/sdk/tcprelay/iosSdkTcpRelay.sh
@@ -3,4 +3,8 @@
 # TO_PORT:   SDK_ON_DEVICE_TCP_PORT   - on app running on attached iOS device over USB
 # FROM_PORT: SDK_ON_COMPUTER_TCP_PORT - on local PC
 # Note: Ports can be the same number - they're on different devices
+
+# First remove any adb forwarding on this port (you can't connect to iOS and Android on same port at the same time)
+adb forward --remove tcp:5106
+
 ./tcprelay.py 5106:5106
