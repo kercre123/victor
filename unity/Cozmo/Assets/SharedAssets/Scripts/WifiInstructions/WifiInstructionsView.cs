77e9e9ff2db24b66ab89e10d9fd31a3cff64ae39
Pohung Chen <pohungc@gmail.com>
2016-08-25 16:59:45 -0700

    COZMO-4181 removing wifi timeout replace on charger view (#1135)

--- unity/Cozmo/Assets/SharedAssets/Scripts/WifiInstructions/WifiInstructionsView.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/WifiInstructions/WifiInstructionsView.cs
@@ -9,32 +9,15 @@ public class WifiInstructionsView : Cozmo.UI.BaseView {
   private Cozmo.UI.BaseView _GetHelpViewPrefab;
   private Cozmo.UI.BaseView _GetHelpViewInstance;
 
-  [SerializeField]
-  private Cozmo.UI.BaseView _ReplaceCozmoOnChargerViewPrefab;
-  private Cozmo.UI.BaseView _ReplaceCozmoOnChargerViewInstance;
-
-  [SerializeField]
-  private float _ReplaceCozmoViewTimeout = 25.0f;
-
   private void Awake() {
     _HelpButton.Initialize(() => {
       _GetHelpViewInstance = UIManager.OpenView(_GetHelpViewPrefab);
     }, "help_button", this.DASEventViewName);
-
-    Invoke("OpenReplaceCozmoView", _ReplaceCozmoViewTimeout);
-  }
-
-  private void OpenReplaceCozmoView() {
-    _ReplaceCozmoOnChargerViewInstance = UIManager.OpenView(_ReplaceCozmoOnChargerViewPrefab);
   }
 
   protected override void CleanUp() {
     if (_GetHelpViewInstance != null) {
       UIManager.CloseViewImmediately(_GetHelpViewInstance);
-    }
-
-    if (_ReplaceCozmoOnChargerViewInstance != null) {
-      UIManager.CloseViewImmediately(_ReplaceCozmoOnChargerViewInstance);
     }
   }
 }
