ac7fc7fdaf1332f2f154f9e4c5b0818bd420b86a
seanlevatino <sean@anki.com>
2016-09-13 10:45:03 -0700

    Cozmo 4855 upgrade detail view polish (#1540)
    * add custom "ask Cozmo to do X" button descriptions for each upgrade.
    
    * In the 'Available' upgrade state, set the Upgrade Icon alpha instead of making it a Dark tint (alpha should dim to: 128).
    
    * pluralize Sparks where necessary on Spark Button (Currently says "4 Spark" instead of "4 Sparks").
    
    * Unify the ‘not enough sparks’ message and the ‘not enough bits’ message so that they both say “Earn more by completing Daily Goals”
    
    * fixing whitespace weirdness

--- unity/Cozmo/Assets/CozmoUnlocks/Scripts/UnlockableInfo.cs
+++ unity/Cozmo/Assets/CozmoUnlocks/Scripts/UnlockableInfo.cs
@@ -33,6 +33,8 @@ public class UnlockableInfo : ScriptableObject, IComparable {
 
   public string TitleKey;
 
+  public string SparkButtonDescription;
+
   [Cozmo.ItemId]
   public string UpgradeCostItemId;
   public int UpgradeCostAmountNeeded;
