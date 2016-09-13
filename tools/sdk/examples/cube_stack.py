de9a9a78e0c38c32c048db12cc93d44e77237cf7
Michelle Sintov <michelle@anki.com>
2016-09-13 10:55:57 -0700

    Cozmo 4749 fix cube stack example (#1541)
    COZMO-4749: Modify SDK cube_stack script look around
    
    Correct check for number of cubes found and print error

--- tools/sdk/examples/cube_stack.py
+++ tools/sdk/examples/cube_stack.py
@@ -8,10 +8,17 @@ He will pick up the first one he sees, and place it on the second one.'''
 def run(coz_conn):
     coz = coz_conn.wait_for_robot()
 
+    lookaround = coz.start_behavior(cozmo.behavior.BehaviorTypes.LookAround)
+
     cubes = coz.world.wait_until_observe_num_objects(num=2, object_type=cozmo.objects.LightCube, timeout=30)
-    
-    coz.pickup_object(cubes[0]).wait_for_completed()
-    coz.place_on_object(cubes[1]).wait_for_completed()
+
+    lookaround.stop()
+
+    if len(cubes) < 2:
+  	  print("Error: need 2 Cubes but only found", len(cubes), "Cube(s)")
+    else:
+      coz.pickup_object(cubes[0]).wait_for_completed()
+      coz.place_on_object(cubes[1]).wait_for_completed()
 
 if __name__ == '__main__':
     cozmo.setup_basic_logging()
