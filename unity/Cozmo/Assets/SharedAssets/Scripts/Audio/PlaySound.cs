baf945cc9c02d2ec2bec4340ea6fb066d4362bbf
Charlie Huguenard <charliehuge@users.noreply.github.com>
2016-09-14 10:19:32 -0700

    COZMO-4618 Add sound for reward particle (#1583)
    A sound trigger for when the reward particle
    was played was requested, so this is a simple
    script add to allow us to attach a sound to a
    GameObject being created or destroyed.
    
    NOTE: this goes through no trouble to limit
    the number of sounds triggered across instances.
    Voice limiting will be left to the sound design
    team via the Wwise authoring tool.

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Audio/PlaySound.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Anki {
+  namespace Cozmo {
+    namespace Audio {
+      /// <summary>
+      /// Attach this script to a GameObject to play a sound.
+      /// </summary>
+      public class PlaySound : MonoBehaviour {
+
+        /// <summary>
+        /// Specifies a time we would want to play a sound.
+        /// Add more here (and their corresponding handlers) if other needs arise.
+        /// </summary>
+        public enum PlayEventType
+        {
+          Awake,
+          Start,
+          OnEnable,
+          OnDisable
+        }
+
+        [SerializeField]
+        private AudioEventParameter _AudioEventParameter = AudioEventParameter.InvalidEvent;
+
+        [SerializeField]
+        private PlayEventType _PlayEvent = PlayEventType.Awake;
+
+        private void Awake() {
+          if (PlayEventType.Awake == _PlayEvent) {
+            Play();
+          }
+        }
+
+        private void Start() {
+          if (PlayEventType.Start == _PlayEvent) {
+            Play();
+          }
+        }
+
+        private void OnEnable() {
+          if (PlayEventType.OnEnable == _PlayEvent) {
+            Play();
+          }
+        }
+
+        private void OnDisable() {
+          if (PlayEventType.OnDisable == _PlayEvent) {
+            Play();
+          }
+        }
+
+        private void Play() {
+          if (_AudioEventParameter.IsInvalid()) {
+            DAS.Warn("PlaySound.Play", "_AudioEventParameter is invalid.");
+            return;
+          }
+
+          GameAudioClient.PostAudioEvent(_AudioEventParameter);
+        }
+      }
+    }
+  }
+}
