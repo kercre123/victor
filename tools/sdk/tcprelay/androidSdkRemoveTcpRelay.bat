560c34c9a7db5300f9a61cad9d0f976437b163fe
Michelle Sintov <michelle@anki.com>
2016-09-14 15:22:26 -0700

    Windows iOS SDK iosSdkTcpRelay script improvements
    Turn off 'adb forward --remove' calls from iOSSdkTcpRelay.sh/.bat. Will add to documentation instead. Move these calls to new scripts androidSdkRemoveTcpRelay.bat/.sh
    
    For Windows iOS correct Python version that calls tcprelay.py.
    
    Rename androidSdkTcpRelay.* to androidSdkAddTcpRelay.*

---
+++ tools/sdk/tcprelay/androidSdkRemoveTcpRelay.bat
@@ -0,0 +1,2 @@
+REM Remove any adb forwarding on this port. (Note: you can't connect to iOS and Android on same port at the same time.)
+adb forward --remove tcp:5106
--- tools/sdk/tcprelay/iosSdkTcpRelay.sh
+++ tools/sdk/tcprelay/iosSdkTcpRelay.sh
@@ -4,7 +4,4 @@
 # FROM_PORT: SDK_ON_COMPUTER_TCP_PORT - on local PC
 # Note: Ports can be the same number - they're on different devices
 
-# First remove any adb forwarding on this port (you can't connect to iOS and Android on same port at the same time)
-adb forward --remove tcp:5106
-
 ./tcprelay.py 5106:5106
