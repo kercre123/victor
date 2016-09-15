3bf55f859310bb5bec17b99319009cd5d98ad366
ilngo <ivy.le.ngo@gmail.com>
2016-09-15 15:48:30 -0700

    COZMO-3033: Fixing formatting of Cozmo serial number (#1669)

--- unity/Cozmo/Assets/HomeHub/Scripts/Settings/SettingsVersionsPanel.cs
+++ unity/Cozmo/Assets/HomeHub/Scripts/Settings/SettingsVersionsPanel.cs
@@ -56,7 +56,7 @@ namespace Cozmo.Settings {
       _CozmoVersionLabel.FormattingArgs = new object[] { ShortenData(robot.FirmwareVersion.ToString()) };
 
       // Fill out Serial number args
-      _SerialNumberLabel.FormattingArgs = new object[] { ShortenData(robot.SerialNumber.ToString()) };
+      _SerialNumberLabel.FormattingArgs = new object[] { ShortenData(robot.SerialNumber.ToString("X6")) };
 
       string dasEventViewName = "settings_version_panel";
       _AcknowledgementsLinkButton.Initialize(HandleAcknowledgementsLinkButtonTapped, "acknowledgements_link", dasEventViewName);
