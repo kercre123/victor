b216781bca32c3c84f2f952309b341464c520c57
Pohung Chen <pohungc@gmail.com>
2016-09-16 16:30:02 -0700

    COZMO-5060 more android specific wifi instruction hacks (#1745)

---
+++ unity/Cozmo/Assets/SharedAssets/Scripts/ConnectIntro/Wifi/WifiSlide4.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WifiSlide4 : MonoBehaviour {
+
+  [SerializeField]
+  UnityEngine.UI.Image _HomeScreenImage;
+
+  [SerializeField]
+  Sprite _AndroidSpecificSprite;
+
+  private void Awake() {
+#if UNITY_ANDROID
+    _HomeScreenImage.overrideSprite = _AndroidSpecificSprite;
+#endif
+  }
+}
