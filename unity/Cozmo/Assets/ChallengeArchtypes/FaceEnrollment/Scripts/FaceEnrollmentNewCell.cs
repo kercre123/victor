06146f516fbdb215cb499f3d562a81919ed3d65d
Pohung Chen <pohungc@gmail.com>
2016-09-16 17:00:53 -0700

    COZMO-4963 design changes to Meet Cozmo unlock images (#1747)

--- unity/Cozmo/Assets/ChallengeArchtypes/FaceEnrollment/Scripts/FaceEnrollmentNewCell.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/FaceEnrollment/Scripts/FaceEnrollmentNewCell.cs
@@ -11,6 +11,12 @@ public class FaceEnrollmentNewCell : MonoBehaviour {
   [SerializeField]
   private Cozmo.UI.CozmoButton _CreateNewButtonFace;
 
+  [SerializeField]
+  private Sprite _DisabledLockSprite;
+
+  [SerializeField]
+  private UnityEngine.UI.Image _MainCellImage;
+
   private void Awake() {
     _CreateNewButtonFace.Initialize(HandleEnrollNewFaceButton, "create_new_face_button", "face_enrollment_new_cell");
     _CreateNewButton.Initialize(HandleEnrollNewFaceButton, "create_new_button", "face_enrollment_new_cell");
@@ -19,6 +25,12 @@ public class FaceEnrollmentNewCell : MonoBehaviour {
   public void EnableCell(bool enable) {
     _CreateNewButtonFace.Interactable = enable;
     _CreateNewButton.gameObject.SetActive(enable);
+    if (!enable) {
+      _MainCellImage.overrideSprite = _DisabledLockSprite;
+    }
+    else {
+      _MainCellImage.overrideSprite = null;
+    }
   }
 
   private void HandleEnrollNewFaceButton() {
