459a16923702ed003f667d538815a94751693d68
David Woods <dwoods@anki.com>
2016-10-03 16:01:09 -0700

    Updated version to 1.1.0 (#2180)

--- cozmoAPI/include/anki/cozmo/buildVersion.h
+++ cozmoAPI/include/anki/cozmo/buildVersion.h
@@ -16,7 +16,7 @@ namespace Anki {
 namespace Cozmo {
 
   
-constexpr const char* const kBuildVersion = "00000.00007.00000";
+constexpr const char* const kBuildVersion = "00001.00001.00000";
 
   
 } // namespace Cozmo
