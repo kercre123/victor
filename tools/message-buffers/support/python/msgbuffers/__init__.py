a5b091d588fd88253c524429023f543b0545db61
Gareth Watts <gareth@omnipotent.net>
2016-10-12 13:08:38 -0700

    COZMO-5948 Update SDK to Apache license (#2301)
    * COZMO-5948 Update SDK to Apache license

--- tools/message-buffers/support/python/msgbuffers/__init__.py
+++ tools/message-buffers/support/python/msgbuffers/__init__.py
@@ -1,4 +1,17 @@
-# Copyright (c) 2016 Anki, Inc. All rights reserved. See LICENSE.txt for details.
+# Copyright (c) 2016 Anki, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License in the file LICENSE.txt or at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """
 Message buffer support module for generated code.
 """
@@ -32,22 +45,22 @@ class ReadError(Exception):
 
 class BinaryReader(object):
     "Used to read in a stream of binary data a buffer, keeping track of the current position."
-    
+
     def __init__(self, buffer):
         self._buffer = buffer
         self._index = 0
-    
+
     def __len__(self):
         return len(self._buffer)
-    
+
     def tell(self):
         "Returns the current stream position as an offset within the buffer."
         return self._index
-    
+
     def read(self, format):
         "Reads in a single value of the given format."
         return self.read_farray(format, 1)[0]
-    
+
     def read_farray(self, format, length):
         "Reads in a fixed-length array of the given format and length."
         reader = _get_struct(format, length)
@@ -57,96 +70,96 @@ class BinaryReader(object):
         result = reader.unpack_from(self._buffer, self._index)
         self._index += reader.size
         return result
-    
+
     def read_varray(self, data_format, length_format):
         "Reads in a variable-length array with the given length format and data format."
         length = self.read(length_format)
         return self.read_farray(data_format, length)
-    
+
     def read_string(self, length_format):
         "Reads in a variable-length string with the given length format."
         length = self.read(length_format)
         bytes = self.read_farray('s', length)[0]
         return bytes.decode('utf_8')
-    
+
     def read_string_farray(self, string_length_format, array_length):
         "Reads in a fixed-length array of variable-length strings with the given length format."
         return [self.read_string(string_length_format) for i in xrange(array_length)]
-    
+
     def read_string_varray(self, string_length_format, array_length_format):
         "Reads in a variable-length array of variable-length strings with the given length format."
         array_length = self.read(array_length_format)
         return [self.read_string(string_length_format) for i in xrange(array_length)]
-    
+
     def read_object(self, unpack_from_method):
         "Reads in an object according to the given method."
         return unpack_from_method(self)
-    
+
     def read_object_farray(self, unpack_from_method, length):
         "Reads in a fixed-length object sequence according to the given method."
         return [unpack_from_method(self) for i in xrange(length)]
-    
+
     def read_object_varray(self, unpack_from_method, length_format):
         "Reads in a variable-length object sequence according to the given method."
         length = self.read(length_format)
         return [unpack_from_method(self) for i in xrange(length)]
-    
+
 class BinaryWriter(object):
     "Used to write out a stream of binary data."
-    
+
     def __init__(self):
         self._buffer = []
-    
+
     def clear(self):
         del self._buffer[:]
-    
+
     def dumps(self):
         return b''.join(self._buffer)
-    
+
     def write(self, value, format):
         "Writes out a single value of the given format."
         self.write_farray((value,), format, 1)
-    
+
     def write_farray(self, value, format, length):
         "Writes out a fixed-length array of the given format and length."
         writer = _get_struct(format, length)
         self._buffer.append(writer.pack(*value))
-    
+
     def write_varray(self, value, data_format, length_format):
         "Writes out a variable-length array with the given length format and data format."
         self.write(len(value), length_format)
         self.write_farray(value, data_format, len(value))
-    
+
     def write_string(self, value, length_format):
         "Writes out a variable-length string with the given length format."
         bytes = value.encode('utf_8')
         self.write(len(bytes), length_format)
         self.write_farray((bytes,), 's', len(bytes))
-    
+
     def write_string_farray(self, value, string_length_format, array_length):
         "Writes out a fixed-length array of variable-length strings with the given length format."
         if len(value) != array_length:
             raise ValueError('The given fixed-length sequence has the wrong length.')
         for element in value:
             self.write_string(element, string_length_format)
-    
+
     def write_string_varray(self, value, string_length_format, array_length_format):
         "Writes out a variable-length array of variable-length strings with the given length format."
         self.write(len(value), array_length_format)
         for element in value:
             self.write_string(element, string_length_format)
-    
+
     def write_object(self, value):
         "Writes out an object that supports a pack_to method."
         value.pack_to(self)
-    
+
     def write_object_farray(self, value, length):
         "Writes out a fixed-length object sequence that supports a pack_to method."
         if len(value) != length:
             raise ValueError('The given fixed-length sequence has the wrong length.')
         for element in value:
             element.pack_to(self)
-    
+
     def write_object_varray(self, value, length_format):
         "Writes out a variable-length object sequence that supports a pack_to method."
         self.write(len(value), length_format)
@@ -341,10 +354,10 @@ def shorten_string(value):
         return repr(unicode('{0}...'.format(value[:29])))
     else:
         return repr(value)
-    
+
 class Namespace(argparse.Namespace):
     "A simple object that pretty prints when converted to string."
-    
+
     def deep_clone(self):
         namespace = Namespace()
         for key, value in self._get_kwargs():
@@ -352,7 +365,7 @@ class Namespace(argparse.Namespace):
                 value = value.deep_clone()
             setattr(namespace, key, value)
         return namespace
-    
+
     def update(self, other_namespace):
         for key, value in other_namespace._get_kwargs():
             if isinstance(value, Namespace):
