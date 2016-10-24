9c4d78625da8e0074702ac564ba7d0fee55eb847
Pohung Chen <pohungc@gmail.com>
2016-10-24 16:05:41 -0700

    COZMO-6253 turn to last face when entering cozmo says (#2383)
    * COZMO-6253 turn to last face when entering cozmo says
    
    * imposing cozmo says character limit
    
    * fix up ios project
    
    * more xcode bs...

--- unity/Cozmo/Assets/ChallengeArchtypes/CozmoSays/Scripts/CozmoSaysGame.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/CozmoSays/Scripts/CozmoSaysGame.cs
@@ -8,6 +8,7 @@ namespace CozmoSays {
     private SayTextSlide _SayTextSlidePrefab;
 
     protected override void InitializeGame(MinigameConfigBase minigameConfig) {
+      CurrentRobot.TurnTowardsLastFacePose(90.0f);
       SharedMinigameView.ShowFullScreenGameStateSlide(_SayTextSlidePrefab.gameObject, "say_text_slide");
     }
 
