d1a190fbf0fc0ec882c40ce4e8936c0be2691f60
Ivy Ngo <ivy.le.ngo@gmail.com>
2016-11-17 23:15:12 -0800

    COZMO-5886: Fixing null refs in AnimatorUtil (#2759)

--- unity/Cozmo/Assets/SharedAssets/Scripts/Util/AnimatorUtil.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/Util/AnimatorUtil.cs
@@ -3,33 +3,41 @@
 public static class AnimatorUtil {
   public static void ListenForAnimationStart(this Animator animator,
                                              AnimationEventStateMachineBehavior.AnimationHandler callback) {
-    AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
-    for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
-      progressBarAnimatorSMBs[i].OnAnyAnimationStarted += callback;
+    if (animator != null) {
+      AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
+      for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
+        progressBarAnimatorSMBs[i].OnAnyAnimationStarted += callback;
+      }
     }
   }
 
   public static void StopListenForAnimationStart(this Animator animator,
                                              AnimationEventStateMachineBehavior.AnimationHandler callback) {
-    AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
-    for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
-      progressBarAnimatorSMBs[i].OnAnyAnimationStarted -= callback;
+    if (animator != null) {
+      AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
+      for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
+        progressBarAnimatorSMBs[i].OnAnyAnimationStarted -= callback;
+      }
     }
   }
 
   public static void ListenForAnimationEnd(this Animator animator,
                                              AnimationEventStateMachineBehavior.AnimationHandler callback) {
-    AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
-    for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
-      progressBarAnimatorSMBs[i].OnAnyAnimationEnded += callback;
+    if (animator != null) {
+      AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
+      for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
+        progressBarAnimatorSMBs[i].OnAnyAnimationEnded += callback;
+      }
     }
   }
 
   public static void StopListenForAnimationEnd(this Animator animator,
                                              AnimationEventStateMachineBehavior.AnimationHandler callback) {
-    AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
-    for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
-      progressBarAnimatorSMBs[i].OnAnyAnimationEnded -= callback;
+    if (animator != null) {
+      AnimationEventStateMachineBehavior[] progressBarAnimatorSMBs = animator.GetBehaviours<AnimationEventStateMachineBehavior>();
+      for (int i = 0; i < progressBarAnimatorSMBs.Length; i++) {
+        progressBarAnimatorSMBs[i].OnAnyAnimationEnded -= callback;
+      }
     }
   }
 }
