eb3ddada4e0c736ca34a9fb7f04765cb6f933ae7
Stuart Eichert <seichert@anki.com>
2016-11-29 15:42:01 -0800

    COZMO-7333 ./start_sim.py can talk to real robots
    Allow the user of this script to pass "-r" to indicate we are talking to
    a real robot instead of a sim.

--- tools/sdk_devonly/start_sim.py
+++ tools/sdk_devonly/start_sim.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2016 Anki, Inc. All rights reserved. See LICENSE.txt for details.
 
+import argparse
 import cozmo
 import time
 
@@ -9,7 +10,7 @@ import time
 
 
 _is_engine_loaded = False
-
+_options = None
 
 def handle_engine_loading_state(evt, *, msg):
   if msg.ratioComplete == 1.0:
@@ -25,6 +26,7 @@ def handle_robot_found(evt, **kw):
 
 
 def start_sim(coz_conn):
+    global _options
     "start sim - starts the engine and connects to a sim robot"
     
     coz_conn.add_event_handler(cozmo._clad._MsgEngineLoadingDataStatus, handle_engine_loading_state)
@@ -34,8 +36,9 @@ def start_sim(coz_conn):
     coz_conn.send_msg(start_engine_msg)
     
     time_waiting_for_engine_load = 0
+    max_time_waiting_for_engine_load = 60.0 if _options and _options.is_real_robot else 6.0
     global _is_engine_loaded
-    while (not _is_engine_loaded) and (time_waiting_for_engine_load < 6.0):
+    while (not _is_engine_loaded) and (time_waiting_for_engine_load < max_time_waiting_for_engine_load):
       sleep_time = 0.5
       time.sleep(sleep_time)
       time_waiting_for_engine_load += sleep_time
@@ -47,9 +50,15 @@ def start_sim(coz_conn):
       print("Took %s seconds to load engine" % time_waiting_for_engine_load)
     else:
       print("Timed out after %s seconds waiting to load engine" % time_waiting_for_engine_load)
-    
-    ip_address = "127.0.0.1\0\0\0\0\0\0\0".encode('ascii')
-    connect_robot_msg = cozmo._clad._clad_to_engine_iface.ConnectToRobot(ipAddress=ip_address, robotID=1, isSimulated=1)
+
+    is_simulated = 0 if _options and _options.is_real_robot else 1
+
+    if is_simulated != 0:
+      ip_address = "127.0.0.1\0\0\0\0\0\0\0".encode('ascii')
+    else:
+      ip_address = "172.31.1.1\0\0\0\0\0\0".encode('ascii')
+
+    connect_robot_msg = cozmo._clad._clad_to_engine_iface.ConnectToRobot(ipAddress=ip_address, robotID=1, isSimulated=is_simulated)
     coz_conn.send_msg(connect_robot_msg)
 
 
@@ -61,5 +70,13 @@ def run(coz_conn):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-r', '--real-robot',
+                        dest='is_real_robot',
+                        default=False,
+                        action='store_const',
+                        const=True,
+                        help='Run with a real robot instead of a sim')
+    _options = parser.parse_args()
     cozmo.setup_basic_logging()
     cozmo.connect(run)
