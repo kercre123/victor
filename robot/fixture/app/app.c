d8cba0952f6c942cd0bf2a856eb3a3182cb7d521
Craig <crohe@anki.com>
2016-12-21 17:17:37 -0800

    v80 release: EP1 v1.5 for christmas factory build

--- robot/fixture/app/app.c
+++ robot/fixture/app/app.c
@@ -18,8 +18,9 @@
 
 #include "app/tests.h"
 
-u8 g_fixtureReleaseVersion = 71;
-const char* BUILD_INFO = "EP1 v1.5 beta-006";
+u8 g_fixtureReleaseVersion = 80;
+//const char* BUILD_INFO = "EP1 v1.5 beta-007";
+const char* BUILD_INFO = "EP1 v1.5";
 
 BOOL g_isDevicePresent = 0;
 const char* FIXTYPES[FIXTURE_DEBUG+1] = FIXTURE_TYPES;
