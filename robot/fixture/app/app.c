92721e4477e07699394a6746b0baa64d51c6e27b
Craig <crohe@anki.com>
2017-01-05 16:33:49 -0800

    rev to avoid confusion

--- robot/fixture/app/app.c
+++ robot/fixture/app/app.c
@@ -18,14 +18,14 @@
 
 #include "app/tests.h"
 
-u8 g_fixtureReleaseVersion = 81;
+u8 g_fixtureReleaseVersion = 82;
 #define BUILD_INFO_PREFIX "EP1 v1.5"
 
 //Set this flag to modify display info - indicates a debug/test build
 #define NOT_FOR_FACTORY 1
 
 #if NOT_FOR_FACTORY > 0
-#define BUILD_INFO (BUILD_INFO_PREFIX ## " beta-013")
+#define BUILD_INFO (BUILD_INFO_PREFIX ## " beta-014")
 #else
 #define BUILD_INFO (BUILD_INFO_PREFIX)
 #endif
