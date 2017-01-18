468e30c7e8389cf0b0d0aaa3fc3af735afa0c355
Brad Neuman <brad@anki.com>
2017-01-17 22:34:10 -0800

    COZMO-8952 Disable simulated latency
    - Workaround for webots loading crash (but doesn't fully resolve
      possible issues)
    
    - COZMO-8952 appears to have been caused because the code in
      multiClientComms was not thread safe. Specifically, adding elements to
      the deque inside sendMsgPackets_ can cause a vector-resize which needs
      to deallocate memory, which was causing a memory crash when called
      from one of the loading threads in robotDataLoader. I suspect that
      this is because this operation is not thread safe, so another thread
      may have left this in a bad state (although I couldn't prove
      that. Other threads were running and loading, but none was clearly in
      the middle of logging).
    
    - The latency simulation was set to 0 seconds prior to this commit, so
      the only effect this should have is sending out messages immediately
      instead of at the end of a tick. This also avoids the storing the
      messages here in a data structure which was not thread-safe for
      appending, which seems to have fixed the problem after running 10 runs
      of each webots test
    
    - Note that there may be other threading bugs lurking in this code or
      other code called in the logger providers
    
    - This bug should not have been happening in production because we don't
      use the "unity logger" when ANKI_DEV_CHEATS is disabled

--- basestation/include/anki/cozmo/basestation/multiClientComms.h
+++ basestation/include/anki/cozmo/basestation/multiClientComms.h
@@ -27,7 +27,8 @@
 // beyond the actual latency of TCP.
 // Note that the resolution of these latencies is currently equal to
 // the Basestation frequency since that's what defines how often Update() is called.
-#define DO_SIM_COMMS_LATENCY 1
+
+#define DO_SIM_COMMS_LATENCY 0
 #define SIM_RECV_LATENCY_SEC 0.0 // 0.03
 #define SIM_SEND_LATENCY_SEC 0.0 // 0.03
 
