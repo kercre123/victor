2503ff5ee6dc2e7184d051f3157756f1a94ee98a
baustinanki <baustinanki@users.noreply.github.com>
2017-01-19 15:14:03 -0800

    COZMO-9057 Increase delay time before showing wifi instructions (#3058)

--- unity/Cozmo/Assets/SharedAssets/Scripts/ConnectIntro/AndroidFindNetworks.cs
+++ unity/Cozmo/Assets/SharedAssets/Scripts/ConnectIntro/AndroidFindNetworks.cs
@@ -3,6 +3,9 @@
 // Screen to wait on until we successfully scan a Cozmo to try to connect to
 public class AndroidFindNetworks : AndroidConnectionFlowStage {
 
+  // how long to wait before displaying the "Turn Cozmo on..." instructions
+  private const float kInstructionsDelaySeconds = 5.0f;
+
   [SerializeField]
   private Anki.UI.AnkiButton _CancelButton;
 
@@ -16,7 +19,7 @@ public class AndroidFindNetworks : AndroidConnectionFlowStage {
     // register listeners to get scan results from Java, or be notified if we get a permissions issue
     var receiver = AndroidConnectionFlow.Instance.GetMessageReceiver();
     RegisterJavaListener(receiver, "scanResults", HandleScanResults);
-    Invoke("ShowAnimations", 2.5f);
+    Invoke("ShowAnimations", kInstructionsDelaySeconds);
   }
 
   private void HandleScanResults(string[] ssids) {
