219dcda26f48b068eca7302ed93a4f5a4811f7b2
ashelly <ashelly@users.noreply.github.com>
2017-02-03 14:02:00 -0800

    Cozmo-7836: put psk info in factory flash (#3175)

--- robot/fixture/hal/espressif.c
+++ robot/fixture/hal/espressif.c
@@ -448,6 +448,10 @@ bool ESPFlashLoad(uint32_t address, int length, const uint8_t *data, bool dat_st
   return true;
 }
 
+
+extern int generateCozmoPassword(char* pwOut, int len);
+
+
 void ProgramEspressif(int serial)
 {
   SlowPrintf("ESP Syncronizing...\n");
@@ -479,16 +483,18 @@ void ProgramEspressif(int serial)
     // Set serial number, model number, random data in Espressif
     if (!setserial) {
       const int DATALEN = 2+16;
-      u32 data[DATALEN];
+      const int STRLEN = 20;
+      u32 data[DATALEN+STRLEN/sizeof(u32)];
       data[0] = serial; // Serial
       data[1] = 0;      // Model
       
       // Add 64 bytes of random gibberish, at Daniel's request
       for (int i = 2; i < DATALEN; i++)
         data[i] = GetRandom();
-      
+
+      generateCozmoPassword((char*)data+DATALEN, STRLEN);
       SlowPrintf("Load SERIAL data: ");
-      for (int i = 0; i < DATALEN; i++)
+      for (int i = 0; i < sizeof(data); i++)
         SlowPrintf("%08x,", data[i]);
       SlowPrintf("\n");
       ESPFlashLoad(0x1000, sizeof(data), (uint8_t*)&data, false);
