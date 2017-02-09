26aac6e490f8798cc7b642af4121d1e490b4d1e0
Craig <crohe@anki.com>
2017-02-09 14:46:02 -0800

    change ID0 pin. Verify future ID1,2 pins are NC/Float

--- robot/fixture/hal/board.c
+++ robot/fixture/hal/board.c
@@ -155,17 +155,23 @@ static pinstate_e _test_pin_state(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 board_rev_t GetBoardRev(void)
 {
   pinstate_e board_id0 = _test_pin_state(GPIOC, GPIOC_BOARD_ID0);
+  pinstate_e board_id1 = _test_pin_state(GPIOC, GPIOC_BOARD_ID1);
+  pinstate_e board_id2 = _test_pin_state(GPIOC, GPIOC_BOARD_ID2);
   
-  //fixture 1.0 rev1,2,3 did not have a board revision check. pins were NC/float.
+  //fixture 1.0 rev1,2,3 did not have a board revision check. pins are NC/float.
   //fixture 1.5+ implemented this check
   if( board_id0 == Z )
     return BOARD_REV_1_0_REV3; //or rev1,2. Can't tell.
   else if( board_id0 == LOW )
     return BOARD_REV_1_5_0;
-  else
+  else //no revision currently pulls this pin high.
     assert(0);
   
-  return BOARD_REV_1_0_REV1;
+  //additional ID pins are NC/float. crash and burn if firmware is older than fixture hardware.
+  assert( board_id1 == Z );
+  assert( board_id2 == Z );
+  
+  return BOARD_REV_1_0_REV1; //silly compiler. we'll never make it here.
 }
 
 u8 GetBoardID(void)
