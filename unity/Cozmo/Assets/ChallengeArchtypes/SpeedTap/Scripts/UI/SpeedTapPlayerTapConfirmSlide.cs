4bbd9f38218fa143cebec7190b9c74778d1925a3
Molly Jameson <MollyJameson@users.noreply.github.com>
2017-02-10 14:17:19 -0800

    COZMO-9454 MP QT: Make clear which player is which (#3274)
    * COZMO-9449 [Quick Tap] Cube Lights Should be Flashing Red When Tap Incorrect Match
    * Review feedback

--- unity/Cozmo/Assets/ChallengeArchtypes/SpeedTap/Scripts/UI/SpeedTapPlayerTapConfirmSlide.cs
+++ unity/Cozmo/Assets/ChallengeArchtypes/SpeedTap/Scripts/UI/SpeedTapPlayerTapConfirmSlide.cs
@@ -17,15 +17,22 @@ namespace SpeedTap {
     [SerializeField]
     private GameObject[] _Player2OnlyContent;
 
+    [SerializeField]
+    private GameObject[] _ConfirmContent;
+
     public void Init(int numPlayers, int playerIndex = 0) {
       _SPContainer.SetActive(numPlayers <= 2);
       _MPContainer.SetActive(numPlayers > 2);
       if (numPlayers > 2) {
+        // If playerIndex is invalid ( -1 ) then show the final step...
+        for (int i = 0; i < _ConfirmContent.Length; ++i) {
+          _ConfirmContent[i].SetActive(playerIndex <= 0);
+        }
         for (int i = 0; i < _Player2OnlyContent.Length; ++i) {
-          _Player2OnlyContent[i].SetActive(playerIndex > 1);
+          _Player2OnlyContent[i].SetActive(playerIndex == 2);
         }
         for (int i = 0; i < _Player1OnlyContent.Length; ++i) {
-          _Player1OnlyContent[i].SetActive(playerIndex <= 1);
+          _Player1OnlyContent[i].SetActive(playerIndex == 1);
         }
       }
     }
