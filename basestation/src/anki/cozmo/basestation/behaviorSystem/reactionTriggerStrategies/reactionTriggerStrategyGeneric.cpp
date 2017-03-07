01cd7aef5d850ba133974e16ed7ce4d1fbbe7735
mmichini <mmichini@users.noreply.github.com>
2017-03-07 13:47:12 -0800

    COZMO-9887 Cozmo performing 2 cliff reactions back to back after discovering cliff (#3475)
    - For `ReactionTriggerStrategyGeneric`, do not set `_shouldTrigger` if `CanInterruptSelf()` is `false` and the current running behavior was triggered by the current reaction trigger strategy.
    - Before this, `_shouldTrigger` could get set to `true` while the triggered behavior was running, causing the behavior to run again once the first one finished.

--- basestation/src/anki/cozmo/basestation/behaviorSystem/reactionTriggerStrategies/reactionTriggerStrategyGeneric.cpp
+++ basestation/src/anki/cozmo/basestation/behaviorSystem/reactionTriggerStrategies/reactionTriggerStrategyGeneric.cpp
@@ -14,6 +14,7 @@
 #include "anki/cozmo/basestation/behaviorSystem/reactionTriggerStrategies/reactionTriggerStrategyGeneric.h"
 #include "anki/cozmo/basestation/behaviors/iBehavior.h"
 #include "anki/cozmo/basestation/behaviorSystem/behaviorPreReqs/behaviorPreReqRobot.h"
+#include "anki/cozmo/basestation/behaviorManager.h"
 #include "anki/cozmo/basestation/messageHelpers.h"
 #include "anki/cozmo/basestation/robot.h"
 #include "util/logging/logging.h"
@@ -104,6 +105,14 @@ bool ReactionTriggerStrategyGeneric::ShouldTriggerBehavior(const Robot& robot, c
 // should occur whenever a relevant event is handled.
 void ReactionTriggerStrategyGeneric::AlwaysHandleInternal(const EngineToGameEvent& event, const Robot& robot)
 {
+  // Don't do anything if this trigger can't interrupt itself and this reaction trigger
+  //  caused the current behavior to run.
+  if (!CanInterruptSelf() &&
+      robot.GetBehaviorManager().GetCurrentReactionTrigger() == GetReactionTrigger())
+  {
+    return;
+  }
+  
   if (_relevantEvents.find(event.GetData().GetTag()) == _relevantEvents.end())
   {
     PRINT_NAMED_ERROR("ReactionTriggerStrategyGeneric.AlwaysHandleInternal.BadEventType",
