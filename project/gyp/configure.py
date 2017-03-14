bec26d78c5b3d626877fd4f5f92a0ca9dd30b910
Mark Wesley <mwesley@anki.com>
2017-03-14 15:35:17 -0700

    COZMO-10141 Generate Hash for C# CLAD (#3578)
    This is a step towards providing the generated C# from CLAD to allow people to more easily build a C# SDK.
    Adds an error on startup for the case when anyone has out of sync C# and C++ Engine code too.

--- project/gyp/configure.py
+++ project/gyp/configure.py
@@ -321,7 +321,7 @@ def main(scriptArgs):
     return True
 
   #run clad's make
-  unityGeneratedPath=os.path.join(projectRoot, 'unity/Cozmo/Assets/Scripts/Generated')
+  unityGeneratedPath=os.path.join(projectRoot, 'unity/Cozmo/Assets/Scripts/Generated/')
   shutil.rmtree(unityGeneratedPath, True)
   if (subprocess.call(['make', '--silent', 'OUTPUT_DIR_CSHARP=' + unityGeneratedPath, 'csharp'],
     cwd=os.path.join(options.cozmoEnginePath, 'clad')) != 0):
