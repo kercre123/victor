f7d346f3df02b08c05699b8de613978692b0e6b4
Al Chaussee <alchaussee@users.noreply.github.com>
2017-03-21 12:53:25 -0700

    COZMO-10276 FaceWorld.AddOrUpateFace.BadPoseParent error (#3635)
    * COZMO-10276 FaceWorld.AddOrUpateFace.BadPoseParent error
    
    Removing the error since the code already handles the conditions that cause the error to print

--- DEPS
+++ DEPS
@@ -35,7 +35,7 @@
                 "build_type_id": "CozmoOne_Experiments_CollectFirmware",
                 "extension": "tar.gz",
                 "package_name": "firmware",
-                "version": "1850"
+                "version": "1861"
             },
             "old_firmware": {
                 "build_type_id": "CozmoOne_Experiments_CollectFirmware",
--- basestation/src/anki/cozmo/basestation/faceWorld.cpp
+++ basestation/src/anki/cozmo/basestation/faceWorld.cpp
@@ -191,14 +191,6 @@ namespace Cozmo {
   
   Result FaceWorld::AddOrUpdateFace(const Vision::TrackedFace& face)
   {
-    // The incoming TrackedFace should be w.r.t. the arbitrary historical world origin.
-    if(face.GetHeadPose().GetParent() == nullptr || !face.GetHeadPose().GetParent()->IsOrigin())
-    {
-      PRINT_NAMED_ERROR("FaceWorld.AddOrUpdateFace.BadPoseParent",
-                        "TrackedFace's head pose parent should be an origin.");
-      return RESULT_FAIL;
-    }
-
     // Head pose is stored w.r.t. historical world origin, but needs its parent
     // set up to be the robot's world origin here:
     Pose3d headPoseWrtWorldOrigin;
