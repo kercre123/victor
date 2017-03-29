cf8bd59e767df26b23daeb2713c20f32f30637e4
Pohung Chen <pohungc@gmail.com>
2017-03-29 14:53:15 -0700

    Fixes an issue where theme jsons are not properly loaded in editor time (#3729)

--- unity/Cozmo/Assets/DevTools/Editor/CozmoThemeSystem/CozmoThemeSystemInitialize.cs
+++ unity/Cozmo/Assets/DevTools/Editor/CozmoThemeSystem/CozmoThemeSystemInitialize.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
 using UnityEditor;
+using Anki.Core.UI.Components;
 
 [InitializeOnLoad]
 public class CozmoThemeSystemInitialize : MonoBehaviour {
   static CozmoThemeSystemInitialize() {
     CozmoThemeSystemUtils.Initialize();
     CozmoThemeSystemEditorUtils.Initialize();
+
+    ThemesJson.LoadJson();
+    ThemeSystemConfigJson.LoadJson();
   }
 }
