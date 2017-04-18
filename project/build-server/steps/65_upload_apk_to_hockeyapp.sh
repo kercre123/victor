53c66f7ff5507ca757aa469c2ed030f690f5234a
Stuart Eichert <seichert@anki.com>
2017-04-17 19:44:46 -0700

    COZMO-2889 Nix BUILT_PRODUCTS_PATH where not used
    BUILT_PRODUCTS_PATH is not used in 65_upload_apk_to_hockeyapp.sh, so I
    removed it.  Also deleted commented code around declaration of
    APK_FILE_NAME.

--- project/build-server/steps/65_upload_apk_to_hockeyapp.sh
+++ project/build-server/steps/65_upload_apk_to_hockeyapp.sh
@@ -23,21 +23,12 @@ if [ -z ${APP_ID+x} ] && [ ! -z ${ANKI_BUILD_HOCKEYAPP_APP_ID_ANDROID+x} ]; then
     APP_ID=${ANKI_BUILD_HOCKEYAPP_APP_ID_ANDROID}
 fi
 
-# Set OVERRIDE before calling script to fetch apk's from a different location.
-if [ ! -z ${BUILD_PRODUCTS_PATH_OVERRIDE+x} ]; then
-    BUILT_PRODUCTS_PATH=${BUILD_PRODUCTS_PATH_OVERRIDE}
-fi
-
 if [ -z ${APP_ID+x} ]; then
     echo "ERROR: Missing HOCKEYAPP_APP_ID_ANDROID"
     exit 1
 fi
 
-#if [ ${ANKI_BUILD_TYPE} == "Release" ]; then
-  APK_FILE_NAME="Cozmo"
-#else
-#  APK_FILE_NAME="Cozmo-${ANKI_BUILD_TYPE}"
-#fi
+APK_FILE_NAME="Cozmo"
 
 echo "${APP_ID}"
 ${ANKI_BUILD_TOOLS_ROOT}/upload-to-hockeyapp.sh ${1} ${APP_ID} "${ANKI_BUILD_ROOT}/android/${APK_FILE_NAME}.apk" "${ANKI_BUILD_ROOT}/android/${APK_FILE_NAME}.apk-symbols.zip"
