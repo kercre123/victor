71d06ef153bc94f48b4b43dd6544fa46ab3deaff
baustinanki <baustinanki@users.noreply.github.com>
2017-04-28 15:15:06 -0700

    COZMO-10980 Fix surgery script to be less invasive (#4012)

--- tools/android/apkSplashScreenFix.sh
+++ tools/android/apkSplashScreenFix.sh
@@ -6,32 +6,29 @@
 INFILE=$1
 OUTFILE=$2
 
-TEMPDIR=$(basename $INFILE)_temp
-EDITED_APK_NAME=$(basename $INFILE)_edited.apk
-EDITED_APK=$TEMPDIR/$EDITED_APK_NAME
-
-rm -rf $TEMPDIR
+APK_FILE=$(basename $OUTFILE)
+SETTINGS_FILE=assets/bin/Data/settings.xml
 
 echo "$INFILE before processing:" $(stat -f%z $INFILE)
 
 # unpack the APK into $TEMPDIR
 set -x
-unzip -q $INFILE -d $TEMPDIR
+cp $INFILE $OUTFILE
+pushd $(dirname $OUTFILE)
 
+# extract settings.xml
+unzip $APK_FILE $SETTINGS_FILE
 # change the "showSplash" option from True to False
-sed -i '' -e 's/showSplash">True/showSplash">False/g' $TEMPDIR/assets/bin/Data/settings.xml
+sed -i '' -e 's/showSplash">True/showSplash">False/g' $SETTINGS_FILE
+# put our modified file back in the apk
+zip -u $APK_FILE $SETTINGS_FILE
 
-# re-pack the APK file
-pushd $TEMPDIR
-zip -r -q $EDITED_APK_NAME . -x META-INF/\*
-popd
+# remove META-INF folder from zip (jarsigner will replace it and requires it not be there)
+zip -d $APK_FILE META-INF/\*
 
 # sign the new APK with the debug android keystore, output to $OUTFILE
-jarsigner -keystore ~/.android/debug.keystore -storepass android -keypass android $EDITED_APK androiddebugkey -sigfile CERT -signedjar $OUTFILE
+jarsigner -keystore ~/.android/debug.keystore -storepass android -keypass android $APK_FILE androiddebugkey -sigfile CERT -signedjar $APK_FILE
 set +x
 
-echo "temporary $EDITED_APK size:" $(stat -f%z $EDITED_APK)
-echo "$OUTFILE after processing:" $(stat -f%z $EDITED_APK)
-
-# remove our temporary folder
-rm -rf $TEMPDIR
+echo "$OUTFILE after processing:" $(stat -f%z $OUTFILE)
+popd
