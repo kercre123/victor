81c7d08cccb867f0ea86291727404a92e555b368
Craig <crohe@anki.com>
2017-05-03 10:45:37 -0700

    rev to v97

--- robot/fixture/app/app.c
+++ robot/fixture/app/app.c
@@ -21,7 +21,7 @@
 #include "app/tests.h"
 #include "nvReset.h"
 
-u8 g_fixtureReleaseVersion = 96;
+u8 g_fixtureReleaseVersion = 97;
 #define BUILD_INFO "PVT v1.5"
 
 //Set this flag to modify display info - indicates a debug/test build
