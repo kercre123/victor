8f377a1d74b6e24e85a3bcfba97e2ae3b55b5e05
Pohung Chen <pohungc@gmail.com>
2017-05-05 04:42:16 -0700

    COZMO-11150 Fix Slow ChallengeConfig Editing (#4075)

--- unity/Cozmo/Assets/Scripts/SharedScripts/Util/SerializableEnum/Editor/SerializableEnumEditor.cs
+++ unity/Cozmo/Assets/Scripts/SharedScripts/Util/SerializableEnum/Editor/SerializableEnumEditor.cs
@@ -1,79 +1,69 @@
 ﻿// from http://www.scottwebsterportfolio.com/blog/2015/3/29/enum-serialization-in-unity
 
 using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 using UnityEditor;
-using System;
+
+public class SerializableEnumDrawingHelper {
+  public static void PopulateEnumStringToValueMap(SerializedProperty property, Dictionary<string, int> _EnumStringToValueMap) {
+    SerializedProperty enumProperty = property.FindPropertyRelative("_EnumValue");
+    for (int nameIndex = 0; nameIndex < enumProperty.enumNames.Length; nameIndex++) {
+      _EnumStringToValueMap.Add(enumProperty.enumNames[nameIndex], nameIndex);
+    }
+  }
+
+  public static void OnGUIHelper(Rect position, SerializedProperty property, GUIContent label, Dictionary<string, int> enumStringToValueMap, bool enumStringToValueMapPopulated) {
+    if (!enumStringToValueMapPopulated) {
+      PopulateEnumStringToValueMap(property, enumStringToValueMap);
+    }
+
+    EditorGUI.BeginProperty(position, label, property);
+
+    position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+    SerializedProperty enumProperty = property.FindPropertyRelative("_EnumValue");
+    SerializedProperty enumStringProperty = property.FindPropertyRelative("_EnumValueAsString");
+
+    enumProperty.enumValueIndex = enumStringToValueMap[enumStringProperty.stringValue];
+
+    enumProperty.enumValueIndex = EditorGUI.Popup(position, enumProperty.enumValueIndex, enumProperty.enumNames);
+    enumStringProperty.stringValue = enumProperty.enumNames[enumProperty.enumValueIndex];
+    EditorGUI.EndProperty();
+  }
+}
 
 [CustomPropertyDrawer(typeof(UnlockableInfo.SerializableUnlockIds))]
 public class SerializableEnumEditor : PropertyDrawer {
 
+  private Dictionary<string, int> _EnumStringToValueMap = new Dictionary<string, int>();
+  private bool _EnumStringToValueMapPopulated = false;
+
   public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-    EditorGUI.BeginProperty(position, label, property);
-
-    position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-    SerializedProperty enumProperty = property.FindPropertyRelative("_EnumValue");
-    SerializedProperty enumStringProperty = property.FindPropertyRelative("_EnumValueAsString");
-
-    for (int nameIndex = 0; nameIndex < enumProperty.enumNames.Length; nameIndex++) {
-      if (enumProperty.enumNames[nameIndex] == enumStringProperty.stringValue) {
-        enumProperty.enumValueIndex = nameIndex;
-        break;
-      }
-    }
-
-    enumProperty.enumValueIndex = EditorGUI.Popup(position, enumProperty.enumValueIndex, enumProperty.enumNames);
-    enumStringProperty.stringValue = enumProperty.enumNames[enumProperty.enumValueIndex];
-
-    EditorGUI.EndProperty();
+    SerializableEnumDrawingHelper.OnGUIHelper(position, property, label, _EnumStringToValueMap, _EnumStringToValueMapPopulated);
+    _EnumStringToValueMapPopulated = true;
   }
 }
 
 [CustomPropertyDrawer(typeof(SerializableGameEvents))]
 public class SerializableGameEventEnumEditor : PropertyDrawer {
 
+  private Dictionary<string, int> _EnumStringToValueMap = new Dictionary<string, int>();
+  private bool _EnumStringToValueMapPopulated = false;
+
   public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-    EditorGUI.BeginProperty(position, label, property);
-
-    position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-
-    SerializedProperty enumProperty = property.FindPropertyRelative("_EnumValue");
-    SerializedProperty enumStringProperty = property.FindPropertyRelative("_EnumValueAsString");
-
-    for (int nameIndex = 0; nameIndex < enumProperty.enumNames.Length; nameIndex++) {
-      if (enumProperty.enumNames[nameIndex] == enumStringProperty.stringValue) {
-        enumProperty.enumValueIndex = nameIndex;
-        break;
-      }
-    }
-
-    enumProperty.enumValueIndex = EditorGUI.Popup(position, enumProperty.enumValueIndex, enumProperty.enumNames);
-    enumStringProperty.stringValue = enumProperty.enumNames[enumProperty.enumValueIndex];
-
-    EditorGUI.EndProperty();
+    SerializableEnumDrawingHelper.OnGUIHelper(position, property, label, _EnumStringToValueMap, _EnumStringToValueMapPopulated);
+    _EnumStringToValueMapPopulated = true;
   }
 }
 
 [CustomPropertyDrawer(typeof(SerializableAnimationTrigger))]
 public class SerializableAnimationTriggerEnumEditor : PropertyDrawer {
 
+  private Dictionary<string, int> _EnumStringToValueMap = new Dictionary<string, int>();
+  private bool _EnumStringToValueMapPopulated = false;
+
   public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
-    EditorGUI.BeginProperty(position, label, property);
-
-    position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
-    SerializedProperty enumProperty = property.FindPropertyRelative("_EnumValue");
-    SerializedProperty enumStringProperty = property.FindPropertyRelative("_EnumValueAsString");
-    for (int nameIndex = 0; nameIndex < enumProperty.enumNames.Length; nameIndex++) {
-      if (enumProperty.enumNames[nameIndex] == enumStringProperty.stringValue) {
-        enumProperty.enumValueIndex = nameIndex;
-        break;
-      }
-    }
-
-    enumProperty.enumValueIndex = EditorGUI.Popup(position, enumProperty.enumValueIndex, enumProperty.enumNames);
-    enumStringProperty.stringValue = enumProperty.enumNames[enumProperty.enumValueIndex];
-    EditorGUI.EndProperty();
+    SerializableEnumDrawingHelper.OnGUIHelper(position, property, label, _EnumStringToValueMap, _EnumStringToValueMapPopulated);
+    _EnumStringToValueMapPopulated = true;
   }
 }
 
