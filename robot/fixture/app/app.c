ac7e0023547f9c77eb7f5fd568479a5140454424
Craig <crohe@anki.com>
2017-05-08 17:44:13 -0700

    release v98

--- robot/fixture/app/app.c
+++ robot/fixture/app/app.c
@@ -25,7 +25,7 @@ u8 g_fixtureReleaseVersion = 98;
 #define BUILD_INFO "PVT v1.5"
 
 //Set this flag to modify display info - indicates a debug/test build
-#define NOT_FOR_FACTORY 1
+#define NOT_FOR_FACTORY 0
 
 //other global dat
 app_reset_dat_t g_app_reset;
