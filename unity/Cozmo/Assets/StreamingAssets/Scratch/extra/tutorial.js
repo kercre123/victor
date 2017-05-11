3fd6e5e1b9e47b559cfe0ab81bd95a18f648f6dd
Michelle Sintov <michelle@anki.com>
2017-05-10 17:06:53 -0700

    COZMO-10983 Save/load and tutorial fixes (#4148)
    Fixes bugs:
    COZMO-10983: removes highlight on delete project button when clicked
    COZMO-11294: fixes ghosts under tutorial slide images
    COZMO-11192: use info icon for tutorials link in Save/Load screen
    COZMO-11162: Adds tooltip sample to 2nd page of tutorial
    
    Code from Adam/Code Refactory.

--- unity/Cozmo/Assets/StreamingAssets/Scratch/extra/tutorial.js
+++ unity/Cozmo/Assets/StreamingAssets/Scratch/extra/tutorial.js
@@ -7,6 +7,7 @@ window.addEventListener('DOMContentLoaded', function() {
 
   setText('#tutorial-slide-1 .slide-label', 'Code Lab lets you code Cozmo.');
   setText('#tutorial-slide-2 .slide-label', 'You can tap a code block to see what it does.');
+  setText('#tutorial-slide-2 #text-tap-to-learn-more', 'Turn left' );
   setText('#tutorial-slide-3 .slide-label', 'Drag code blocks together to make a program.');
   setText('#tutorial-slide-4 .slide-label', 'Always place a green flag BLOCK first. That way, the green flag BUTTON starts your program!');
 
