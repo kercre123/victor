e616f3d4202015ac91cd8c10d6fa557612d3723d
Michelle Sintov <michelle@anki.com>
2017-05-18 19:16:52 -0700

    COZMO-11526 Code Lab: Add build script to build blocks compressed js (#4241)
    This PR does two things:
    
    1. Scratch/lib/blocks/build.sh can be used to generate compressed js after code in Scratch/lib/blocks has changed.
    
    2. While developing code in Scratch/lib/blocks, it is helpful to be able to test that code before compressing. In index.html, I've added the scripts (commented out) that should be turned on to test the uncompressed files. When using those uncompressed files, also need to copy tools/scratch/closure-library to Scratch/lib/closure-library (2000+ files).

---
+++ unity/Cozmo/Assets/StreamingAssets/Scratch/lib/blocks/build.sh
@@ -0,0 +1,37 @@
+# After modifying files in Scratch/lib/blocks, run this file
+# to generate the compressed js files.
+
+rm -rf ../closure-library
+
+ln -s $(npm root)/google-closure-library ../closure-library
+
+echo npm install
+npm install
+
+echo removing node_modules.meta incase Unity made one
+rm -f node_modules.meta
+
+echo removing npm-*log*.meta incase Unity made one
+rm -f npm-debug.log.meta
+
+echo removing dist.meta incase Unity made one
+rm -f dist.meta
+
+echo removing closure-library symlink
+rm ../closure-library
+
+echo reverting gh-pages
+git checkout -- gh-pages
+git clean -fd gh-pages
+
+echo reverting i18n
+git checkout -- i18n
+git clean -fd i18n
+
+echo reverting node_modules
+rm -rf node_modules
+
+echo reverting msg
+git checkout -- msg
+git clean -fd msg
+
