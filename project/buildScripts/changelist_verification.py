76ea1f9cf45131cdbd6a1d9c2d389b5cc14eeb11
David Woods <dwoods@anki.com>
2017-07-03 09:49:59 -0700

    BI-444 add pull_request_branch param to match %teamcity_branch% (#4755)

--- project/buildScripts/changelist_verification.py
+++ project/buildScripts/changelist_verification.py
@@ -19,6 +19,8 @@ def parse_args(argv=[]):
                         help='Mandatory Github authentication token. Create one if you want to run this locally.')
     parser.add_argument('-p', '--pull_request_number', type=int, action='store', default='0',
                         help='Enter Pull request number you want to compare')
+    parser.add_argument('-b', '--pull_request_branch', action='store', default=None,
+                        help='Enter Pull request branch you want to compare. Expected format=1234/head')
     parser.add_argument('-r', '--repository_name', action='store', default='anki/cozmo-one',
                         help='Enter the repository to verify PRs from. Default=anki/cozmo-one')
     args = parser.parse_args(argv)
@@ -37,7 +39,11 @@ def verify_regex(file_list, reg="\Arobot\/"):
 def main(argv):
     args = parse_args(argv[1:])
     if args.auth_token is not None:
-        file_list = anki_github.get_file_change_list(args.auth_token, args.pull_request_number,
+        if args.pull_request_branch is not None:
+            branch_number = int(filter(str.isdigit, args.pull_request_branch))
+        else:
+            branch_number = args.pull_request_number
+        file_list = anki_github.get_file_change_list(args.auth_token, branch_number,
                                                      args.verbose, args.repository_name)
         if verify_regex(file_list) is False:
             sys.exit(os.linesep + "Pull Request was triggered erroneously from Teamcity plugin. "
