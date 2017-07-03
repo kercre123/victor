8ebd309a6ed9321c5af2a41f176ee83e3941a2f0
David Woods <dwoods@anki.com>
2017-07-03 01:16:19 -0700

    BI-444: Rewrite changelist_verification.py (#4730)
    * BI-444: Rewrite changelist_verification.py
    
    - Now calls github to figure out base branch.
    - Looks for changes to firmware in all commits related to PR.
    
    * updated after pr comments.
    
    * updated after pr comments.

--- project/buildScripts/anki_github.py
+++ project/buildScripts/anki_github.py
@@ -2,12 +2,19 @@
 
 #############################################
 #                                           #
+# Anki_Github                               #
 # Library for Anki Specific Github funcions #
 #                                           #
 #############################################
 
-from github import Github
+from github import Github, GithubException
+import subprocess
+import dependencies
+import sys
+import os
 
+
+DEFAULT_REPO = "anki/cozmo-one"
 
 def get_branch_name(auth_token, pull_request_number, repo_name="anki/cozmo-one"):
     """
@@ -20,11 +27,51 @@ def get_branch_name(auth_token, pull_request_number, repo_name="anki/cozmo-one")
     """
 
     # TO-DO: initialize of instance should become its own function if this grows larger.
-    github_inst = Github(auth_token)
-    repo = github_inst.get_repo(repo_name)
     try:
+        github_inst = Github(auth_token)
+        repo = github_inst.get_repo(repo_name)
         for pr in repo.get_pulls():
             if pr.number == pull_request_number:
-                return(pr.raw_data['head']['ref'])
-    except:
-	print("Exception thrown because of {0}".format(repo))
+                return (pr.raw_data['head']['ref'])
+    except GithubException, e:
+        print("Exception thrown because of {0}".format(repo))
+        sys.exit("GITHUB ERROR: {0}".format(e.data['message']))
+
+
+def get_file_change_list(auth_token, pull_request_number, verbose=False, repo_name=DEFAULT_REPO):
+    """
+    Returns the list of changed files from a github PR.
+
+    :param auth_token: string
+    :param pull_request_number: int
+    :param verbose: boolean
+    :param repo_name: string
+    :return: list
+    """
+
+    if dependencies.is_tool("git") is False:
+        sys.exit("[ERROR] Could not find git")
+
+    try:
+        github_inst = Github(auth_token)
+        repo = github_inst.get_repo(repo_name)
+        pr = repo.get_pull(pull_request_number)
+    except GithubException, e:
+        sys.exit("GITHUB ERROR: {0} {1}".format(e.data['message'], e.status))
+
+    if verbose:
+        print("Comparing {0} at {1}".format(pr.base.ref, pr.base.sha))
+        print("against {0} at {1}".format(pr.head.ref, pr.head.sha))
+
+    git_cmd = ['git', 'diff', '--name-only', '{0}...{1}'.format(pr.base.sha, pr.head.sha), '--submodule']
+    p = subprocess.Popen(git_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdout, stderr) = p.communicate()
+    status = p.poll()
+
+    if status != 0:
+        print "ERROR: {0}".format(stderr)
+        return None
+    if verbose:
+        print(os.linesep + "Files Changed:" + os.linesep)
+        print(stdout)
+        return stdout.splitlines()
--- project/buildScripts/changelist_verification.py
+++ project/buildScripts/changelist_verification.py
@@ -4,37 +4,46 @@ import subprocess
 import dependencies
 import sys
 import re
+import anki_github
+import os
+import argparse
+
+
+def parse_args(argv=[]):
+
+    parser = argparse.ArgumentParser(description='Verify that pr changes list is not going to need a firmware build.')
+
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='print verbose output')
+    parser.add_argument('-a', '--auth_token', action='store', default=None,
+                        help='Mandatory Github authentication token. Create one if you want to run this locally.')
+    parser.add_argument('-p', '--pull_request_number', type=int, action='store', default='0',
+                        help='Enter Pull request number you want to compare')
+    parser.add_argument('-r', '--repository_name', action='store', default='anki/cozmo-one',
+                        help='Enter the repository to verify PRs from. Default=anki/cozmo-one')
+    args = parser.parse_args(argv)
+    return args
 
 
 def verify_regex(file_list, reg="\Arobot\/"):
-    for f in file_list:
+    if file_list is not None:
         prog = re.compile(reg)
-        result = prog.match(f)
-        if result is not None:
-            return True
+        for f in file_list:
+            result = prog.match(f)
+            if result is not None:
+                return True
     return False
 
-# Will be rewritten using the github api.
-# Why? If the initial opening of the PR has multiple commits there is a changes for a false negative.
-# However this is far better than the false positives that are currently happening.
-def extract_file_list():
-    if dependencies.is_tool("git") is False:
-        sys.exit("[ERROR] Could not find git")
-    git_cmd = ["git", "diff", "--name-only", "HEAD", "HEAD^"]
-    p = subprocess.Popen(git_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    (stdout, stderr) = p.communicate()
-    status = p.poll()
-    if status != 0:
-        return None
-    return stdout.splitlines()
-
-
-def main():
-    file_list = extract_file_list()
-    print( "File(s) changed:\n")
-    print( file_list )
-    if verify_regex(file_list) == False:
-	sys.exit("\nPR was trigger erroneously from Teamcity.")
+def main(argv):
+    args = parse_args(argv[1:])
+    if args.auth_token is not None:
+        file_list = anki_github.get_file_change_list(args.auth_token, args.pull_request_number,
+                                                     args.verbose, args.repository_name)
+        if verify_regex(file_list) is False:
+            sys.exit(os.linesep + "Pull Request was triggered erroneously from Teamcity plugin. "
+                                  "Termination of build is intentional.")
+    else:
+        sys.exit("ERROR: Authentication token is mandatory to connect to Github.")
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
