f41dce14424d42fc6ef739f152efb7afd37b5d46
frgmike <mike@freerangegames.com>
2017-07-21 19:45:23 -0700

    Mike/cozmo 12600 fix freeplay card (#5058)
    Move Freeplay card from Discover mode into Play mode.  Add a scroll view to SparksView so the freeplay card can be scrolled offscreen.  Fixes COZMO-12600.

--- unity/Cozmo/Assets/Scripts/NeedsHub/SparksView/FreeplayCard.cs
+++ unity/Cozmo/Assets/Scripts/NeedsHub/SparksView/FreeplayCard.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using DataPersistence;
 
-namespace Cozmo.Needs.Activities.UI {
+namespace Cozmo.Needs.Sparks.UI {
   public class FreeplayCard : MonoBehaviour {
 
     [SerializeField]
@@ -15,29 +15,32 @@ namespace Cozmo.Needs.Activities.UI {
     [SerializeField]
     private CozmoButton _SlideRight;
 
-    private ActivitiesView _ActivityView;
+    private SparksView _SparksView;
 
-    public void Initialize(ActivitiesView activityView) {
-      _ActivityView = activityView;
+    public void Initialize(SparksView sparksView) {
+      _SparksView = sparksView;
       _SlideLeft.Initialize(HandleSlideLeftClicked, "freeplay_slide_button", "play_freeplay_card");
       _SlideRight.Initialize(HandleSlideRightClicked, "freeplay_slide_button", "play_freeplay_card");
     }
 
-    // used by activity view to toggle arrow as it scrolls, more meaningful when both arrows were in the same place 
-    internal void SlideArrowFaceLeft(bool left) {
-      _SlideLeft.gameObject.SetActive(left);
-      _SlideRight.gameObject.SetActive(!left);
+    internal void ShowSlideLeftArrow() {
+      _SlideLeft.gameObject.SetActive(true);
+      _SlideRight.gameObject.SetActive(false);
+    }
+
+    internal void ShowSlideRightArrow() {
+      _SlideLeft.gameObject.SetActive(false);
+      _SlideRight.gameObject.SetActive(true);
     }
 
     private void HandleSlideLeftClicked() {
       DataPersistenceManager.Instance.Data.DefaultProfile.HideFreeplayCard = false;
-      _ActivityView.ShowFreeplayCard();
+      _SparksView.ShowFreeplayCard();
     }
 
     private void HandleSlideRightClicked() {
       DataPersistenceManager.Instance.Data.DefaultProfile.HideFreeplayCard = true;
-      _ActivityView.HideFreeplayCard();
+      _SparksView.HideFreeplayCard();
     }
-
   }
 }
