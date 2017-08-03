8a0e84c3bb706ff362598fd306223fac47d5a7f5
Molly Jameson <MollyJameson@users.noreply.github.com>
2017-08-03 12:32:52 -0700

    COZMO-13016 Reset bonus tokens in onboarding if redone. (#5016)
    * COZMO-13016	[Inventory Bonus Token] - 2 Tokens - One Bonus token is lost when player goes through Onboarding again
    
    * COZMO-13647	[UI] Needs Meters displays wrongly after skipping Onboarding
    COZMO-13666 [Onboarding] Getting bonus token award too early

--- unity/Cozmo/Assets/Scripts/NeedsHub/NeedsHubUI/NeedsHubView.cs
+++ unity/Cozmo/Assets/Scripts/NeedsHub/NeedsHubUI/NeedsHubView.cs
@@ -281,6 +281,13 @@ namespace Cozmo.Needs.UI {
       OnboardingManager.Instance.OnOnboardingAnimEvent.Invoke(param);
     }
     public void OnboardingSkipped() {
+
+      // They've completed everything really.
+      RobotEngineManager.Instance.Message.RegisterOnboardingComplete =
+                 new Anki.Cozmo.ExternalInterface.RegisterOnboardingComplete(
+                      System.Enum.GetNames(typeof(OnboardingManager.OnboardingPhases)).Length - 1, true);
+      RobotEngineManager.Instance.SendMessage();
+
       PopLatestBracketAndUpdateButtons();
       if (_MetersWidget != null) {
         _MetersWidget.OnboardingSkipped();
