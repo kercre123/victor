e7c3b141dd3526ea11c48df9bdf717021ec1ac35
Molly Jameson <MollyJameson@users.noreply.github.com>
2017-09-07 18:22:36 -0700

    COZMO-14494 put in screen space clamped border for tooltips so they don't show too close to the edge. (#5766)
    Put in DAS events for tooltips showing
    COZMO-14499 delayed showing tooltips for onboarding even more

--- unity/Cozmo/Assets/Scripts/SharedScripts/UI/Tooltips/TooltipNeedsMeterData.cs
+++ unity/Cozmo/Assets/Scripts/SharedScripts/UI/Tooltips/TooltipNeedsMeterData.cs
@@ -7,17 +7,17 @@ namespace Cozmo.UI {
     private Anki.Cozmo.NeedId _NeedId;
 
     protected override void HandlePointerDown(UnityEngine.EventSystems.BaseEventData data) {
-      if (OnboardingManager.Instance.IsOnboardingRequired(OnboardingManager.OnboardingPhases.NurtureIntro)) {
+      if (OnboardingManager.Instance.IsOnboardingRequired(OnboardingManager.OnboardingPhases.RewardBox)) {
         return;
       }
       base.HandlePointerDown(data);
     }
 
-    protected override string GetBodyString() {
+    protected override string GetBodyLocKey() {
       // Get only based on which need ID
       Needs.NeedsValue needsVal = Needs.NeedsStateManager.Instance.GetCurrentDisplayValue(_NeedId);
       int index = (int)needsVal.Bracket;
-      return _BodyLocKeys.Length > index ? Localization.Get(_BodyLocKeys[index]) : base.GetBodyString();
+      return _BodyLocKeys.Length > index ? _BodyLocKeys[index] : base.GetBodyLocKey();
     }
 
   }
