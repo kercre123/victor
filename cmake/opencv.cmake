d404f9d547369e8fe74567efaeabb34b9c7eba5f
Andrew Stein <andrew@anki.com>
2017-09-18 10:21:24 -0700

    More cleanup of opencv.cmake

--- cmake/opencv.cmake
+++ cmake/opencv.cmake
@@ -13,7 +13,6 @@ else()
   set(OPENCV_LIB_DIR ${CORETECH_EXTERNAL_DIR}/build/${OPENCV_DIR}/lib/Release)
   set(OPENCV2_INCLUDE_PATH ${CORETECH_EXTERNAL_DIR}/build/opencv-${OPENCV_VERSION})
 endif()
-
                                                                                                     
 set(OPENCV_LIBS
     calib3d
@@ -28,8 +27,6 @@ set(OPENCV_LIBS
     ml
     videoio
     dnn)
-
-set(OPENCV_MODULES_DIR ${CORETECH_EXTERNAL_DIR}/OPENCV_DIR/modules)
 
 set(OPENCV_INCLUDE_PATHS ${OPENCV2_INCLUDE_PATH})
 
@@ -50,41 +47,6 @@ foreach(OPENCV_MODULE ${OPENCV_LIBS})
 
   list(APPEND OPENCV_INCLUDE_PATHS ${MODULE_INCLUDE_PATH})
 endforeach()
-
-# TODO: Remove this 
-if(0)
-    if (ANDROID)
-      message(STATUS "opencv.cmake add_library: " ${OPENCV_MODULE})
-      add_library(${OPENCV_MODULE} STATIC IMPORTED)
-      set(MODULE_INCLUDE_PATH "${CORETECH_EXTERNAL_DIR}/build/opencv-android/OpenCV-android-sdk/sdk/native/jni/include/opencv2/${OPENCV_MODULE}")
-      set(include_paths
-          ${MODULE_INCLUDE_PATH}
-          ${OPENCV_INCLUDE_PREFIX}
-          ${OPENCV2_INCLUDE_PATH})
-      set_target_properties(${OPENCV_MODULE} PROPERTIES
-          IMPORTED_LOCATION
-          ${CORETECH_EXTERNAL_DIR}/build/opencv-android/OpenCV-android-sdk/sdk/native/libs/armeabi-v7a/libopencv_${OPENCV_MODULE}.a
-          INTERFACE_INCLUDE_DIRECTORIES
-          "${include_paths}")
-      list(APPEND OPENCV_INCLUDE_PATHS ${MODULE_INCLUDE_PATH})
-    elseif (MACOSX)
-      message(STATUS "opencv.cmake add_library: " ${OPENCV_MODULE})
-      add_library(${OPENCV_MODULE} STATIC IMPORTED)
-      set(MODULE_INCLUDE_PATH "${CORETECH_EXTERNAL_DIR}/${OPENCV_DIR}/modules/${OPENCV_MODULE}/include")
-      set(include_paths
-          ${MODULE_INCLUDE_PATH}
-          ${OPENCV_INCLUDE_PREFIX}
-          ${OPENCV2_INCLUDE_PATH})
-      set_target_properties(${OPENCV_MODULE} PROPERTIES
-          IMPORTED_LOCATION
-          ${CORETECH_EXTERNAL_DIR}/build/${OPENCV_DIR}/lib/Release/libopencv_${OPENCV_MODULE}.a
-          INTERFACE_INCLUDE_DIRECTORIES
-          "${include_paths}")
-      list(APPEND OPENCV_INCLUDE_PATHS ${MODULE_INCLUDE_PATH})
-    else()
-      include_directories(${OPENCV_MODULES_DIR}/${OPENCV_MODULE}/include)
-    endif()
-endif()
 
 add_library(opencv_interface INTERFACE IMPORTED)
 set_target_properties(opencv_interface PROPERTIES
@@ -133,20 +95,4 @@ if (MACOSX)
   list(APPEND OPENCV_LIBS ${ACCELERATE} ${APPKIT} ${OPENCL})
 endif()
 
-#if (ANDROID)
-#  add_library(libjpeg STATIC IMPORTED)
-#  set_target_properties(libjpeg PROPERTIES IMPORTED_LOCATION
-#    ${CORETECH_EXTERNAL_DIR}/libjpeg-turbo/android_armv7_libs/libjpeg.so)
-#  add_library(libturbojpeg STATIC IMPORTED)
-#  set_target_properties(libturbojpeg PROPERTIES IMPORTED_LOCATION
-#    ${CORETECH_EXTERNAL_DIR}/libjpeg-turbo/android_armv7_libs/libturbojpeg.so)
-#elseif (MACOSX)
-#  add_library(libjpeg STATIC IMPORTED)
-#  set_target_properties(libjpeg PROPERTIES IMPORTED_LOCATION
-#    ${CORETECH_EXTERNAL_DIR}/libjpeg-turbo/mac_libs/libjpeg.a)
-#  add_library(libturbojpeg STATIC IMPORTED)
-#  set_target_properties(libturbojpeg PROPERTIES IMPORTED_LOCATION
-#    ${CORETECH_EXTERNAL_DIR}/libjpeg-turbo/mac_libs/libturbojpeg.a)
-#  list(APPEND OPENCV_LIBS libturbojpeg)
-#endif()
 
