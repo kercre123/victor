b980c1ec0b6baa2fc3c5c349529aad6963970207
Adam Shelly <ashelly@anki.com>
2017-10-03 18:14:25 -0700

    Helper: display. mostly working but rotated.

--- robot2/core/inc/core/lcd.h
+++ robot2/core/inc/core/lcd.h
@@ -8,6 +8,15 @@ typedef struct LcdFrame_t {
   uint16_t data[LCD_FRAME_WIDTH*LCD_FRAME_HEIGHT];
 } LcdFrame;
 
+enum LcdColor {
+  lcd_BLACK = 0x0000,
+  lcd_BLUE  = 0x001F,
+  lcd_GREEN = 0x07E0,
+  lcd_RED   = 0xF800,
+  lcd_WHITE = 0xFFFF,
+};
+
+
 int lcd_init(void);
 void lcd_draw_frame(LcdFrame* frame);
 void lcd_shutdown(void);
