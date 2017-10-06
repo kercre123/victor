ce5e7a811f80b232d670cfb94455a11bb5f2f76b
David C Mudie <dmudie@anki.com>
2017-10-05 21:27:28 -0700

    VIC-12: Enable coretech unit tests (#77)
    * VIC-12: Enable cti_common_test and cti_planning_test
    
    Add cmake targets for existing unit tests under coretech/common and coretech/planning.
    
    Unit tests build under cmake and run under ctest.
    
    Update wrapper scripts to invoke ctest on build server.
    
    * VIC-12: Fix target for cti_common tests
    
    * VIC-12: Enable cti vision tests

--- project/buildServer/steps/unittestsEngine.sh
+++ project/buildServer/steps/unittestsEngine.sh
@@ -1,95 +1,48 @@
 #!/usr/bin/env bash
+#
 set -e
 
-# change dir to the project dir, no matter where script is executed from
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-CUT=`which cut`
-GREP=`which grep`
+# change dir to the script dir, no matter where we are executed from
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Entering directory \`${SCRIPTDIR}'"
+cd $SCRIPTDIR
+
 GIT=`which git`
 if [ -z $GIT ];then
   echo git not found
   exit 1
 fi
-TOPLEVEL=`$GIT rev-parse --show-toplevel`
+
+: ${TOPLEVEL:=`$GIT rev-parse --show-toplevel`}
+
+# configure
+PROJECT=${TOPLEVEL}/test
+PLATFORM=mac
+CONFIG=Debug
+BUILDPATH=${TOPLEVEL}/_build/${PLATFORM}/${CONFIG}/test
+LOG=cozmoEngineGoogleTest.log
+LOGZIP=cozmoEngineGoogleTest.tar.gz
+
+echo "Entering directory \`${BUILDPATH}'"
+cd ${BUILDPATH}
+
+# clean
+rm -rf ${LOG} ${LOGZIP} ${PROJECT}/${LOGZIP}
 
 # prepare
-TESTNAME=cozmoEngine
-PROJECT=$TOPLEVEL/generated/mac
-BUILD_TYPE="Debug"
-GTEST=$TOPLEVEL/lib/util/libs/framework/
-BUILDTOOLS=$TOPLEVEL/tools/build
-
-: ${NO_BUILD:=0}
-
-if [ $NO_BUILD -eq 0 ]; then
-    # build
-    xcodebuild \
-    -workspace $PROJECT/CozmoWorkspace_mac.xcworkspace \
-    -scheme BUILD_WORKSPACE \
-    -sdk macosx \
-    -configuration $BUILD_TYPE \
-    build
-fi
-
-BUILD_DIR=`xcodebuild \
--workspace $PROJECT/CozmoWorkspace_mac.xcworkspace \
--scheme BUILD_WORKSPACE \
--sdk macosx \
--configuration $BUILD_TYPE \
--showBuildSettings | $GREP TARGET_BUILD_DIR | $CUT -d " " -f 7`
-
-set -o pipefail
-set +e
-
-# clean output
-rm -rf $BUILD_DIR/${TESTNAME}GoogleTest*
-rm -rf $BUILD_DIR/case*
-rm -f $BUILD_DIR/*.txt
-
-DUMP_OUTPUT=0
-ARGS=""
-while (( "$#" )); do
-
-    if [[ "$1" == "-x" ]]; then
-        DUMP_OUTPUT=1
-    else
-        if [[ "$ARGS" == "" ]]; then
-            ARGS="--gtest_filter=$1"
-        else
-            ARGS="$ARGS $1"
-        fi
-    fi
-    shift
-done
-
-if (( \! $DUMP_OUTPUT )); then
-    ARGS="$ARGS --silent"
-fi
+mkdir -p testdata
 
 # execute
-$BUILDTOOLS/tools/ankibuild/multiTest.py \
---path $BUILD_DIR \
---gtest_path "$GTEST" \
---work_path "$BUILD_DIR/" \
---config_path "$BUILD_DIR/" \
---gtest_output "xml:$BUILD_DIR/${TESTNAME}GoogleTest_.xml" \
---executable ${TESTNAME}UnitTest \
---stdout_file \
---xml_dir "$BUILD_DIR" \
---xml_basename ${TESTNAME}GoogleTest_ \
-$ARGS
+set +e
+set -o pipefail
+ctest -V -O ${LOG}
 
 EXIT_STATUS=$?
 set -e
 
-if (( $DUMP_OUTPUT )); then
-    cat $BUILD_DIR/*.txt
-fi
+#  publish results
+tar czf ${LOGZIP} ${LOG}
+mv ${LOGZIP} ${PROJECT}
 
-#tarball files together
-cd $BUILD_DIR
-tar czf ${TESTNAME}GoogleTest.tar.gz ${TESTNAME}GoogleTest_*
-mv ${TESTNAME}GoogleTest.tar.gz $TOPLEVEL/build/${TESTNAME}GoogleTest.tar.gz
 # exit
 exit $EXIT_STATUS
