3f52fd3cffc52bc813d647d666a32b7e6304b3af
Bryan Austin <baustin@anki.com>
2017-10-24 11:15:11 -0700

    add more build infrastructure

--- tools/build/cmake/anki_build_go.cmake
+++ tools/build/cmake/anki_build_go.cmake
@@ -46,3 +46,51 @@ macro(anki_build_go_c_library target_name gensrc_var srclist_dir)
   set_property(TARGET ${target_name} PROPERTY IMPORTED_LOCATION ${_gobuild_out})
 
 endmacro()
+
+# build a go executable
+# usage: anki_build_go_executable(mytarget "path/to/source/directory" "path/to/GOPATH/directory" ${ANKI_SRCLIST_DIR})
+macro(anki_build_go_executable target_name target_basedir gopath srclist_dir)
+  __anki_build_absolute_source_list(${target_name} ${srclist_dir})
+
+  set(__gobuild_out "${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${target_name}")
+  get_filename_component(__gobuild_gopath "${gopath}" ABSOLUTE)
+  get_filename_component(__gobuild_basedir "${target_basedir}" ABSOLUTE)
+  file(RELATIVE_PATH __gobuild_basedir ${CMAKE_CURRENT_BINARY_DIR} "${__gobuild_basedir}")
+
+  set(__go_get_env "GOPATH=${__gobuild_gopath}")
+  set(__go_build_flags "")
+  set(__go_deps "")
+
+  list(APPEND __go_build_flags "-i")
+  list(APPEND __go_build_flags "-o" "${__gobuild_out}")
+
+  if(ANDROID)
+    # set android flags for `go get`
+    list(APPEND __go_get_env "GOOS=android")
+    list(APPEND __go_get_env "GOARCH=arm")
+    list(APPEND __go_get_env "GOARM=7")
+  endif()
+
+  if (ANDROID)
+    # set android flags for `go build`
+    list(APPEND __go_compile_env "CC=${ANDROID_TOOLCHAIN_CC}")
+    list(APPEND __go_compile_env "CXX=${ANDROID_TOOLCHAIN_CXX}")
+    list(APPEND __go_compile_env "CGO_ENABLED=1")
+    list(APPEND __go_compile_env "CGO_FLAGS=\"-march=armv7-a\"")
+    set(__gobuild_pkgdir )
+    list(APPEND __go_build_flags "-pkgdir" "${CMAKE_CURRENT_BINARY_DIR}/pkgdir")
+    list(APPEND __go_deps android_toolchain)
+  endif()
+
+  # `go build` should share `go get` environment
+  list(APPEND __go_compile_env "${__go_get_env}")
+
+  add_custom_command(
+    OUTPUT ${__gobuild_out}
+    COMMAND ${CMAKE_COMMAND} -E env ${__go_get_env} go get -d ${__gobuild_basedir}
+    COMMAND ${CMAKE_COMMAND} -E env ${__go_compile_env} go build ${__go_build_flags} ${SRCS} ${_ab_PLATFORM_SRCS}
+    DEPENDS ${SRCS} ${_ab_PLATFORM_SRCS} ${__go_deps}
+  )
+  add_custom_target(${target_name} DEPENDS ${__gobuild_out})
+
+endmacro()
