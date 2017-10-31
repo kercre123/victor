0fec3929362e1275a1cbe465ece2357725b10218
Brad Neuman <brad@anki.com>
2017-10-31 16:12:11 -0700

    Port RandomVectorSampler from Overdrive-one
    * Util class which contains a vector of arbitrary-type objects (templated) and weights. Then can be sampled to
      return a randomly chosen (by weight) item from the vector
    
    * Adapted slightly from OD:
    ** Uses floats instead of doubles
    ** pass in RNG to sample function rather than relying on global singleton
    ** doesn't throw exceptions (undefined behavior + a printed error instead...)

---
+++ lib/util/source/anki/util/random/randomVectorSampler-forwards.h
@@ -0,0 +1,25 @@
+/**
+ * File: randomVectorSampler-forwards.h
+ *
+ * Author: Brad Neuman
+ * Created: 2017-10-31
+ *
+ * Description: Forward declarations for random vector sampler
+ *
+ * Copyright: Anki, Inc. 2017
+ *
+ **/
+
+#ifndef __Util_Random_RandomVectorSampler-Forwards_H__
+#define __Random_RandomVectorSampler-Forwards_H__
+
+namespace Anki{
+namespace Util {
+
+template<typename T> class RandomVectorSampler;
+
+}
+}
+
+
+#endif
