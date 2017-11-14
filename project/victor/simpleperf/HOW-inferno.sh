086bc28d61794e61f78ffdac872b5943b875ea61
David C Mudie <dmudie@anki.com>
2017-11-14 10:54:29 -0800

    VIC-81: Add simpleperf helper scripts (#242)
    * VIC-81: Add simpleperf executables
    
    Code drop from https://android.googlesource.com/platform/prebuilts/simpleperf/+/master
    
    * VIC-81: simpleperf -> lib/util/tools
    
    * VIC-81: Add simpleperf helper scripts
    
    Revised version of scripts to demonstrate simpleperf profile & reporting options.
    These scripts pass required arguments on command line so there is no need for customized victor_profiler.py or cozmoengined.config.
    
    If profiling scripts prove useful to other developers, I can add command-line options for duration and process name.
    As is, the wrapper scripts are easy to modify if you need to change parameters.
    
    * VIC-81: Add env vars to act as command params
    
    Allow customization of profile parameters without editing scripts.
    Add README.md to document same.
    
    * VIC-81: Fix comment oops

---
+++ project/victor/simpleperf/HOW-inferno.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -eu
+
+# Where is this script?
+SCRIPTDIR=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))           
+
+# What do we want to profile?
+: ${ANKI_PROFILE_PROCNAME:="cozmoengined"}
+
+# How long do we capture? (in seconds)
+: ${ANKI_PROFILE_DURATION:="10"}
+
+# How often do we sample? (in usec)
+: ${ANKI_PROFILE_FREQUENCY:="4000"}
+
+# Where is symbol cache?
+: ${ANKI_PROFILE_SYMBOLCACHE:="${SCRIPTDIR}/symbol_cache"}
+
+# Where is top level?
+: ${TOPLEVEL:="`git rev-parse --show-toplevel`"}
+
+# Where is simpleperf?
+: ${SIMPLEPERF:="${TOPLEVEL}/lib/util/tools/simpleperf"}
+
+if [ ! -d ${ANKI_PROFILE_SYMBOLCACHE} ] ; then
+  bash ${SCRIPTDIR}/make_symbol_cache.sh ${ANKI_PROFILE_SYMBOLCACHE}
+fi
+
+#
+# Invoke inferno.sh to collect data and generate html report
+#
+${SIMPLEPERF}/inferno.sh \
+  -np ${ANKI_PROFILE_PROCNAME} \
+  -t ${ANKI_PROFILE_DURATION} \
+  -f ${ANKI_PROFILE_FREQUENCY} \
+  --symfs ${ANKI_PROFILE_SYMBOLCACHE}
+
