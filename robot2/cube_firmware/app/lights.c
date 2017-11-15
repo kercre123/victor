407760a095f6d5673d1a9469769946ac0dd8b948
Bryon Vandiver <unicdk@gmail.com>
2017-11-15 11:59:31 -0800

    Checkpoint

--- robot2/cube_firmware/app/lights.c
+++ robot2/cube_firmware/app/lights.c
@@ -16,20 +16,23 @@ typedef struct {
 static void SWTIM_IRQHandler(void);
 
 static const LED_DEF pin_assignment[] = {
-  { GPIO_R_REGISTER(2), 1 << 2,  0 },
-  { GPIO_R_REGISTER(2), 1 << 9,  3 },
-  { GPIO_R_REGISTER(2), 1 << 6,  6 },
-  { GPIO_R_REGISTER(2), 1 << 3,  9 },
+  // Green
+  { GPIO_R_REGISTER(2), 1 << 1,  2 },
+  { GPIO_R_REGISTER(2), 1 << 7,  5 },
+  { GPIO_R_REGISTER(2), 1 << 5,  8 },
+  { GPIO_R_REGISTER(2), 1 << 4, 11 },
 
+  // Red
   { GPIO_R_REGISTER(0), 1 << 7,  1 },
   { GPIO_R_REGISTER(2), 1 << 8,  4 },
   { GPIO_R_REGISTER(1), 1 << 0,  7 },
   { GPIO_R_REGISTER(1), 1 << 1, 10 },
 
-  { GPIO_R_REGISTER(2), 1 << 1,  2 },
-  { GPIO_R_REGISTER(2), 1 << 7,  5 },
-  { GPIO_R_REGISTER(2), 1 << 5,  8 },
-  { GPIO_R_REGISTER(2), 1 << 4, 11 }
+  // Blue
+  { GPIO_R_REGISTER(2), 1 << 2,  0 },
+  { GPIO_R_REGISTER(2), 1 << 9,  3 },
+  { GPIO_R_REGISTER(2), 1 << 6,  6 },
+  { GPIO_R_REGISTER(2), 1 << 3,  9 },
 };
 
 static const uint16_t SET_R0 = (1 << 7);
@@ -37,7 +40,10 @@ static const uint16_t SET_R1 = (1 << 1) | (1 << 0);
 static const uint16_t SET_R2 = (1 << 1) | (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5)
                              | (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
 
+static const int LED_COUNT = 12;
+
 static IrqCallback swtim_irq;
+static uint8_t intensity[LED_COUNT];
 
 // These are mass pin set values
 void hal_led_init(void) {
@@ -50,7 +56,7 @@ void hal_led_init(void) {
   // Setup Timer0
   SetWord16(TIMER0_RELOAD_N_REG, 0);
   SetWord16(TIMER0_RELOAD_M_REG, 0);
-  SetWord16(TIMER0_ON_REG, TICKS_PER_FRAME / 12);
+  SetWord16(TIMER0_ON_REG, TICKS_PER_FRAME / LED_COUNT);
 
   // Configure the IRQ
   swtim_irq = IRQ_Vectors[SWTIM_IRQn];
@@ -94,7 +100,7 @@ static void SWTIM_IRQHandler(void) {
 
   hal_led_on(led++);
 
-  if (led >= LED_WRAP) {
+  if (led >= LED_COUNT) {
     led = 0;
   }
 }
