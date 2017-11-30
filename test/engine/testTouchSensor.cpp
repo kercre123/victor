c9c1e29f1e0a3af740f8417bf87cb0f2b78a1200
Arjun Menon <ajn.menon@gmail.com>
2017-11-30 15:33:39 -0800

    VIC-720 Analyze Touch Idle DVT (#318)
    unit test as a script to analyze touch sensor data from playpen trials

--- test/engine/testTouchSensor.cpp
+++ test/engine/testTouchSensor.cpp
@@ -12,9 +12,9 @@
  **/
 
 // note: uncomment this line to build the parameter sweeping code
-// #define ENABLE_PARAMETER_SWEEP
+// #define ENABLE_PROTOTYPING_ANALYSIS_TESTS
 
-#ifdef ENABLE_PARAMETER_SWEEP
+#ifdef ENABLE_PROTOTYPING_ANALYSIS_TESTS
 #define private public
 #define protected public
 #endif
@@ -339,8 +339,8 @@ TEST_F(TouchSensorTest, CalibrateAndClassifyMultirobotTests)
   }
 }
 
-#ifdef ENABLE_PARAMETER_SWEEP
-TEST_F(TouchSensorTest, ParameterSweepTouchDetect)
+#ifdef ENABLE_PROTOTYPING_ANALYSIS_TESTS
+TEST_F(TouchSensorTest, DISABLED_ParameterSweepTouchDetect)
 {
   using namespace Anki::Cozmo::ExternalInterface;
 
@@ -497,4 +497,255 @@ TEST_F(TouchSensorTest, ParameterSweepTouchDetect)
   }
   Util::FileUtils::WriteFile(basePath+"results.csv", resultSS.str());
 }
-#endif // ifdef ENABLE_PARAMETER_SWEEP
+
+// An analysis script for parsing multiple logs of touch-sensor data
+// from DVT1 robots produced for Victor//
+// NOTE: All touch sensor streams are indicating idle touch (no contact)
+//
+// Inputs:
+// Json dumps of touch sensor data, annotated by what the robot was doing
+// at the time of data collection. The robot could either be:
+// - on charger and stationary
+// - off charger and stationary
+// - off charger and mobile
+//
+// Outputs:
+// + a csv with a line item for each trial (a trial consists of robot+playpen attempt)
+//    where the column headers for the csv are:
+//      * robot condition or scenario (charger/mobility state)
+//      * mean touch sensor reading for the whole range
+//      * standard deviation for the touch sensor readings
+//      * the calibrated mean when using the stream as input to the touch sensor
+//      * the calibration standard deviation when using the stream as above
+//      * index in the touch sensor stream when the calibration completes
+//      * whether or not any false positive touches were detected during calib
+//
+// + a sanity check which calibrates the robot with one touch-sensor stream
+//    and then looks for false positives in a differently conditioned touch-sensor
+//    stream (on the same robot).
+//
+//    This check is framed as an aggregate accross all trials where it outputs
+//    the percentage of trials that correctly "do not result in false positives"
+TEST_F(TouchSensorTest, DISABLED_AnalyzePlayPenTestsDec17)
+{
+  using namespace Anki::Cozmo::ExternalInterface;
+  
+  const std::string mfgDataDir = cozmoContext->GetDataPlatform()->pathToResource(Util::Data::Scope::Resources,"dvt_dec17");
+  
+  PRINT_NAMED_INFO("AnalyzePlayPenTests","%s",mfgDataDir.c_str());
+  
+  ASSERT_TRUE( Util::FileUtils::DirectoryExists(mfgDataDir) );
+  
+  enum EScenarioType : uint8_t
+  {
+    OffChargerStationary,
+    OnChargerStationary,
+    OffChargerMobile,
+    Count
+  };
+  
+  typedef std::vector<uint16_t> TouchSensorStream;
+  
+  // helper struct -- each trial has a condition and touch sensor reading set
+  struct PlaypenTrialEntry
+  {
+    TouchSensorStream readingsByScenario[EScenarioType::Count];
+  };
+  
+  // helper lambda -- converts Json::Array into list of integers
+  const auto convertJsonToVector = [](const Json::Value& json)->TouchSensorStream
+  {
+    TouchSensorStream ss;
+    
+    for(Json::Value::const_iterator it = json.begin(); it!=json.end(); ++it) {
+      ss.push_back(it->asUInt());
+    }
+    
+    return ss;
+  };
+  
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // analysis: csv of characteristics of each robot trial, for the purposes of
+  //            plotting and visualization for detecting trends and confirming
+  //            assumptions.
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
+  std::vector<std::string> jsonFileList = Util::FileUtils::FilesInDirectory(mfgDataDir,
+                                                                            true,
+                                                                            "json",
+                                                                            true);
+  
+  PRINT_NAMED_INFO("AnalyzePlayPenTests", "Found %zd jsons", jsonFileList.size());
+  
+  // load all the data from disk
+  std::vector<PlaypenTrialEntry> allTouchSensorTrialData;
+  for(const auto& jsonFilename : jsonFileList) {
+    PRINT_NAMED_INFO("AnalyzePlayPenTests", "Processing '%s'",
+                     Util::FileUtils::GetFileName(jsonFilename).c_str());
+    std::string fileContents{ Util::FileUtils::ReadFile(jsonFilename) };
+    
+    Json::Value trialJsonData;
+    Json::Reader reader;
+    bool success = reader.parse(fileContents, trialJsonData);
+    ASSERT_TRUE(success);
+    
+    Json::Value& offChrgStat = trialJsonData["TouchSensor_PlaypenSoundCheck"];
+    Json::Value& onChrgStat = trialJsonData["TouchSensor_PlaypenDriftCheck"];
+    Json::Value& offChrgMobl = trialJsonData["TouchSensor_PlaypenPickupCube"];
+    
+    ASSERT_TRUE(offChrgStat.isArray());
+    ASSERT_TRUE(onChrgStat.isArray());
+    ASSERT_TRUE(offChrgMobl.isArray());
+    
+    allTouchSensorTrialData.push_back(
+                                      PlaypenTrialEntry{
+                                        .readingsByScenario = {
+                                          convertJsonToVector(offChrgStat),
+                                          convertJsonToVector(onChrgStat),
+                                          convertJsonToVector(offChrgMobl)
+                                        }
+                                      });
+  }
+  
+  PRINT_NAMED_INFO("AnalyzePlayPenTests", "Parsed %zd touch sensor streams", allTouchSensorTrialData.size());
+  
+  // compute and organize the data for the CSV dump
+  std::stringstream resultStream;
+  for(const auto& trial : allTouchSensorTrialData) {
+    
+    for(uint8_t i=0; i<EScenarioType::Count; ++i) {
+      
+      auto condition = (EScenarioType)i;
+      auto readings = trial.readingsByScenario[condition];
+      
+      float resultFullMean = 0.0f;
+      float resultFullStdev = 0.0f;
+      for(const auto& reading : readings) {
+        resultFullMean += reading;
+      }
+      resultFullMean /= readings.size();
+      
+      for(const auto& reading : readings) {
+        const float tmp = reading - resultFullMean;
+        resultFullStdev += tmp*tmp;
+      }
+      resultFullStdev /= readings.size() - 1;
+      resultFullStdev = sqrt(resultFullStdev);
+      
+      
+      float resultCalibMean;
+      float resultCalibStdev;
+      int resultIndexWhenCalib = -1;
+      int resultCountFalsePosCalib = 0;
+      {
+        _msgMonitor->ResetCountTouchButtonEvent();
+        TouchSensorComponent tscomp(*_robot);
+        
+        resultIndexWhenCalib = -1;
+        for(int i=0; i<readings.size(); ++i) {
+          const auto reading = readings[i];
+          Anki::Cozmo::RobotState msg = _robot->GetDefaultRobotState();
+          msg.backpackTouchSensorRaw = reading;
+          tscomp.Update(msg);
+          
+          if(resultIndexWhenCalib<0 && tscomp._baselineCalib.IsCalibrated()) {
+            resultIndexWhenCalib = i;
+          } else if(resultIndexWhenCalib>=0) {
+            EXPECT_TRUE(tscomp._baselineCalib.IsCalibrated());
+          }
+        }
+        
+        resultCalibMean = tscomp._baselineCalib.GetFilteredTouchMean();
+        resultCalibStdev = tscomp._baselineCalib.GetFilteredTouchStdev();
+        resultCountFalsePosCalib = _msgMonitor->GetCountTouchButtonEvent();
+      }
+      
+      
+      resultStream << resultFullMean << ", ";
+      resultStream << resultFullStdev << ", ";
+      resultStream << resultCalibMean << ", ";
+      resultStream << resultCalibStdev << ", ";
+      resultStream << resultIndexWhenCalib << ", ";
+      resultStream << resultCountFalsePosCalib << std::endl;
+    }
+  }
+  
+  Util::FileUtils::WriteFile(mfgDataDir+"/results.csv", resultStream.str());
+  
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  // analysis: sanity check the false positive rate for the condition when
+  //  calibrated under different conditions than when testing
+  //
+  // i.e. when calibrated on the charger, but now receiving off-charger input
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
+  // helper lambda -- convert scenario type to string
+  const auto scenToString = [](const EScenarioType t)->std::string{
+    const std::string map[EScenarioType::Count] =
+    {
+      "OffChrgSt",
+      "OnChrgSt",
+      "OffChrgMob"
+    };
+    return map[t];
+  };
+  
+  // helper struct -- testing table for no false positives
+  struct TestingEntry
+  {
+    EScenarioType calibCond; // calibrate under this condition
+    EScenarioType testCond; // test under this condition
+  };
+  
+  // table of reasonable testing skews that could be
+  // representative of real world conditions
+  std::vector<TestingEntry> testTable =
+  {
+    {OffChargerStationary,  OnChargerStationary   },
+    {OffChargerStationary,  OffChargerMobile      },
+    {OnChargerStationary,   OffChargerStationary  },
+    {OnChargerStationary,   OffChargerMobile      },
+  };
+  
+  // compute an aggregate score (the false positive rate) for
+  // each testing skew across all the robot trials
+  for(int i=0; i<testTable.size(); ++i) {
+    size_t numFalsePositives = 0;
+    for(const auto& trial : allTouchSensorTrialData) {
+      const auto& calibrationReadings = trial.readingsByScenario[ testTable[i].calibCond ];
+      const auto& testingReadings = trial.readingsByScenario[ testTable[i].testCond ];
+      
+      TouchSensorComponent tscomp(*_robot);
+      
+      // (1) calibrate
+      for(int i=0; i<calibrationReadings.size(); ++i) {
+        const auto reading = calibrationReadings[i];
+        Anki::Cozmo::RobotState msg = _robot->GetDefaultRobotState();
+        msg.backpackTouchSensorRaw = reading;
+        tscomp.Update(msg);
+      }
+      
+      ASSERT_TRUE(tscomp._baselineCalib.IsCalibrated());
+      
+      // (2) check for false positive touch detections
+      _msgMonitor->ResetCountTouchButtonEvent();
+      for(int i=0; i<testingReadings.size(); ++i) {
+        const auto reading = testingReadings[i];
+        Anki::Cozmo::RobotState msg = _robot->GetDefaultRobotState();
+        msg.backpackTouchSensorRaw = reading;
+        tscomp.Update(msg);
+      }
+      
+      numFalsePositives += _msgMonitor->GetCountTouchButtonEvent()>0;
+    }
+    float successRate = 100.0f*float(allTouchSensorTrialData.size() - numFalsePositives)/allTouchSensorTrialData.size();
+    PRINT_NAMED_INFO("AnalyzePlayPenTests",
+                     "%10s -> %10s (%2zd / %2zd) = %5.2f %%",
+                     scenToString(testTable[i].calibCond).c_str(),
+                     scenToString(testTable[i].testCond).c_str(),
+                     allTouchSensorTrialData.size() - numFalsePositives,
+                     allTouchSensorTrialData.size(),
+                     successRate);
+  }
+}
+#endif // ifdef ENABLE_PROTOTYPING_ANALYSIS_TESTS
