3f93a4313a57657211eaab749521078c69c5ad39
Bryon Vandiver <unicdk@gmail.com>
2017-12-06 11:35:45 -0800

    Cube stuff

--- robot2/cube_firmware/sdk/platform/core_modules/rwip/src/rwip.c
+++ robot2/cube_firmware/sdk/platform/core_modules/rwip/src/rwip.c
@@ -145,7 +145,7 @@ uint32_t dbg_sleep_dur_last     __RETAINED;     // last calculated sleep_duratio
 uint32_t dbg_sleep_dur_avg      __RETAINED;     // average sleep_duration of the previous 10 sleeps
 uint32_t dbg_sleep_dur_sum      __RETAINED;     // sum of the sleep_duration of the previous 10 sleeps (used for averaging)
 uint32_t dbg_sleep_dur_meas     __RETAINED;     // counter to know when we have 10 samples available for averaging
-uint32_t dbg_remaining_time_min __RETAINED;     // minimum time remaining for sleep() and the SLP ISR 
+uint32_t dbg_remaining_time_min __RETAINED;     // minimum time remaining for sleep() and the SLP ISR
 
 
 void pow_profile_exec1(uint32_t sleep_duration)
@@ -153,7 +153,7 @@ void pow_profile_exec1(uint32_t sleep_duration)
     struct lld_evt_tag *evt = (struct lld_evt_tag *)co_list_pick(&lld_evt_env.evt_prog);
 
     dbg_sleep_times[dbg_evt_timer_wr_ptr] = sleep_duration;
-        
+
     dbg_sleep_dur_last = sleep_duration;
     if (dbg_sleep_dur_meas < 10)
     {
@@ -166,7 +166,7 @@ void pow_profile_exec1(uint32_t sleep_duration)
         dbg_sleep_dur_sum = 0;
         dbg_sleep_dur_meas = 0;
     }
-    
+
     do
     {
         if (evt == NULL)
@@ -174,13 +174,13 @@ void pow_profile_exec1(uint32_t sleep_duration)
 
         if (evt->prog)
             break;
-        
+
         dbg_evt_times[dbg_evt_timer_wr_ptr] = evt->time;
         if (sleep_duration > dbg_sleep_dur_avg)
             dbg_sleep_flag[dbg_evt_timer_wr_ptr] = 0xFF;
         else
             dbg_sleep_flag[dbg_evt_timer_wr_ptr] = 0;
-    } while (0);        
+    } while (0);
 }
 
 
@@ -189,9 +189,9 @@ void pow_profile_exec2(void)
     dbg_remaining_times[dbg_evt_timer_wr_ptr] = ble_finetimecnt_get();
     if (dbg_remaining_time_min > dbg_remaining_times[dbg_evt_timer_wr_ptr])
         dbg_remaining_time_min = dbg_remaining_times[dbg_evt_timer_wr_ptr];
-    
+
     dbg_evt_timer_wr_ptr++;
-    if (dbg_evt_timer_wr_ptr == PROFILE_LOG_SZ) 
+    if (dbg_evt_timer_wr_ptr == PROFILE_LOG_SZ)
         dbg_evt_timer_wr_ptr = 0;
 }
 
@@ -228,12 +228,12 @@ void pow_profile_exec2(void)
 
 
 #else
-    
+
     #define POWER_PROFILE_INIT {}
     #define POWER_PROFILE_CHECKS_COMPLETED {}
     #define POWER_PROFILE_SLEEP_TIMES {}
     #define POWER_PROFILE_REMAINING_TIME {}
-    
+
 #endif // (USE_POWER_OPTIMIZATIONS) && (POWER_OPT_PROFILING)
 
 
@@ -250,13 +250,13 @@ static uint32_t rwip_slot_2_lpcycles(uint32_t slot_cnt)
 
     // Compute the low power clock cycles - case of a 32.768kHz clock
     lpcycles = (slot_cnt << 11)/100;
-    
+
     if(lpcycles)
     {
        lpcycles--;
     }
 
-    
+
     return(lpcycles);
 }
 
@@ -269,7 +269,7 @@ static uint32_t rwip_slot_2_lpcycles_rcx(uint32_t slot_cnt)
     ASSERT_ERR(slot_cnt < 1000000);
 
     lpcycles = (uint32_t)((uint64_t)slot_cnt * rcx_slot_duration_num) / rcx_slot_duration_den;
-    
+
     return(lpcycles);
 }
 
@@ -283,13 +283,13 @@ sleep_mode_t rwip_sleep(void)
     uint32_t twext_value;
 
     uint32_t sleep_duration = jump_table_struct[max_sleep_duration_external_wakeup_pos];//MAX_SLEEP_DURATION_EXTERNAL_WAKEUP;
-    
+
     // We may lower the clock now while we are waiting the BLE to go to sleep...
     bool slow_system_clk = false;
 
     uint32_t xtal16m_settling_cycles;
     uint32_t tmp_dur;
-    
+
 #if (!DEVELOPMENT_DEBUG)
     uint32_t sleep_lp_cycles;
 #endif
@@ -306,7 +306,7 @@ sleep_mode_t rwip_sleep(void)
         if (rwip_env.ext_wakeup_enable == 2)
             sleep_duration = 0;
     }
-        
+
     do
     {
         /************************************************************************
@@ -317,20 +317,20 @@ sleep_mode_t rwip_sleep(void)
         // Do not allow sleep if system is in startup period
         if (check_sys_startup_period())
             break;
-        
+
         /************************************************************************
          **************            CHECK KERNEL EVENTS             **************
          ************************************************************************/
         // Check if some kernel processing is ongoing
         if (!ke_sleep_check())
             break;
-        
+
         // Processor sleep can be enabled
         proc_sleep = mode_idle;
 
         DBG_SWDIAG(SLEEP, ALGO, 1);
 
-        
+
 
         /************************************************************************
          **************             CHECK ENABLE FLAG              **************
@@ -339,7 +339,7 @@ sleep_mode_t rwip_sleep(void)
         if(!rwip_env.sleep_enable)
             break;
 
-        
+
         /************************************************************************
          **************           CHECK RADIO POWER DOWN           **************
          ************************************************************************/
@@ -359,7 +359,7 @@ sleep_mode_t rwip_sleep(void)
 
         DBG_SWDIAG(SLEEP, ALGO, 2);
 
-        
+
         /************************************************************************
          **************           CHECK EXT WAKEUP FLAG            **************
          ************************************************************************/
@@ -385,7 +385,7 @@ sleep_mode_t rwip_sleep(void)
                 // If not, there's no reason to verify / ensure the available time for SLP...
                 tmp_dur = sleep_duration;
             }
-    
+
 
         /************************************************************************
          **************            CHECK KERNEL TIMERS             **************
@@ -393,12 +393,12 @@ sleep_mode_t rwip_sleep(void)
         // Compute the duration up to the next software timer expires
         if (!ke_timer_sleep_check(&sleep_duration, rwip_env.wakeup_delay))
             break;
-         
-        // Check if a kernel timer has expired before the above ke_timer_sleep_check() 
-        // function call. If that happens the ke_timer_sleep_check() will incorrectly 
-        // allow the system to enter sleep, because the timeout of the first timer is 
-        // already in the past, and consequently the timer event will not be serviced 
-        // until the next system wake up (the rwip_sleep() function is being called 
+
+        // Check if a kernel timer has expired before the above ke_timer_sleep_check()
+        // function call. If that happens the ke_timer_sleep_check() will incorrectly
+        // allow the system to enter sleep, because the timeout of the first timer is
+        // already in the past, and consequently the timer event will not be serviced
+        // until the next system wake up (the rwip_sleep() function is being called
         // while the interrupts are disabled).
         if (ble_intrawstat_get() & BLE_GROSSTGTIMINTRAWSTAT_BIT)
             break;
@@ -414,7 +414,7 @@ sleep_mode_t rwip_sleep(void)
             if (!lld_sleep_check(&sleep_duration, rwip_env.wakeup_delay))
                 break;
         }
-        
+
         DBG_SWDIAG(SLEEP, ALGO, 4);
 
         if (HCIC_ITF)
@@ -423,13 +423,13 @@ sleep_mode_t rwip_sleep(void)
          ************************************************************************/
         {
             if((BLE_APP_PRESENT == 0) || (BLE_INTEGRATED_HOST_GTL == 1 ))
-            {       
+            {
                 // Try to switch off HCI
                 if (!hci_enter_sleep())
                     break;
             }
         }
-        
+
         if (GTL_ITF)
         /************************************************************************
          **************                 CHECK TL                   **************
@@ -444,15 +444,15 @@ sleep_mode_t rwip_sleep(void)
         }
 
         DBG_SWDIAG(SLEEP, ALGO, 6);
-        
+
         if (USE_POWER_OPTIMIZATIONS)
         {
             /************************************************************************
              ******      BLOCK UNTIL THERE'S TIME FOR sleep() AND SLP ISR      ******
              ************************************************************************/
-            
+
             bool rcx_duration_corr = false;
-            
+
             // Restore sleep_duration
             sleep_duration = tmp_dur;
             /*
@@ -462,8 +462,8 @@ sleep_mode_t rwip_sleep(void)
             if ( arch_clk_is_RCX20() )
             {
                 xtal16m_settling_cycles = lld_sleep_us_2_lpcycles_sel_func(XTAL16M_SETTLING_IN_USEC);
-                
-                while ( (ble_finetimecnt_get() < 550) && (ble_finetimecnt_get() > 200) ); 
+
+                while ( (ble_finetimecnt_get() < 550) && (ble_finetimecnt_get() > 200) );
                 // If we are close to the end of this slot then the actual sleep entry will
                 // occur during the next one. But the sleep_duration will have been calculated
                 // based on the current slot...
@@ -472,9 +472,9 @@ sleep_mode_t rwip_sleep(void)
             }
             else if ( arch_clk_is_XTAL32( ) )
             {
-                while (ble_finetimecnt_get() < 300);  
+                while (ble_finetimecnt_get() < 300);
             }
-            
+
             /************************************************************************
              *                                                                      *
              *                   CHECK DURATION UNTIL NEXT EVENT                    *
@@ -482,7 +482,7 @@ sleep_mode_t rwip_sleep(void)
              *                                                                      *
              ************************************************************************/
             bool sleep_check = false;
-            
+
             do
             {
                 /************************************************************************
@@ -491,12 +491,12 @@ sleep_mode_t rwip_sleep(void)
                 // Compute the duration up to the next software timer expires
                 if (!ke_timer_sleep_check(&sleep_duration, rwip_env.wakeup_delay))
                     break;
-                
-                // Check if a kernel timer has expired before the above ke_timer_sleep_check() 
-                // function call. If that happens the ke_timer_sleep_check() will incorrectly 
-                // allow the system to enter sleep, because the timeout of the first timer is 
-                // already in the past, and consequently the timer event will not be serviced 
-                // until the next system wake up (the rwip_sleep() function is being called 
+
+                // Check if a kernel timer has expired before the above ke_timer_sleep_check()
+                // function call. If that happens the ke_timer_sleep_check() will incorrectly
+                // allow the system to enter sleep, because the timeout of the first timer is
+                // already in the past, and consequently the timer event will not be serviced
+                // until the next system wake up (the rwip_sleep() function is being called
                 // while the interrupts are disabled).
                 if (ble_intrawstat_get() & BLE_GROSSTGTIMINTRAWSTAT_BIT)
                     break;
@@ -512,33 +512,33 @@ sleep_mode_t rwip_sleep(void)
                     if (!lld_sleep_check(&sleep_duration, rwip_env.wakeup_delay))
                         break;
                 }
-                
+
                 sleep_check = true;
-                
+
             } while(0);
-            
+
             if (!sleep_check)
             {
                 if((BLE_APP_PRESENT == 0) || (BLE_INTEGRATED_HOST_GTL == 1 ))
                 {
-                    if (BLE_HOST_PRESENT)		
+                    if (BLE_HOST_PRESENT)
                             gtl_eif_init();
                     else
                             hci_eif_init();
-                    
+
                 }
                 // sleep is aborted and serial i/f communication is restored
                 break;
             }
-            
+
             if (sleep_duration && rcx_duration_corr)
                 sleep_duration--;
-            
+
             DBG_SWDIAG(SLEEP, ALGO, 4);
         }
 
         POWER_PROFILE_CHECKS_COMPLETED;
-        
+
         /************************************************************************
          **************          PROGRAM CORE DEEP SLEEP           **************
          ************************************************************************/
@@ -547,9 +547,9 @@ sleep_mode_t rwip_sleep(void)
             if (!USE_POWER_OPTIMIZATIONS)
             {
                twirq_set_value = lld_sleep_us_2_lpcycles_sel_func(XTAL_TRIMMING_TIME_USEC);
-                
+
                 twirq_reset_value = TWIRQ_RESET_VALUE;
-                
+
                 // TWEXT setting
                 twext_value = TWEXT_VALUE_RCX;
             }
@@ -558,19 +558,20 @@ sleep_mode_t rwip_sleep(void)
                 // Calculate the time we need to wake-up before "time 0" to do XTAL16 settling,
                 // call periph_init() and power-up the BLE core.
                 uint32_t lpcycles = lld_sleep_us_2_lpcycles_sel_func(LP_ISR_TIME_USEC);
-                
+
                 // Set TWIRQ_SET taking into account that some LP cycles are needed for the power up FSM.
                 twirq_set_value = RCX_POWER_UP_TIME + lpcycles;
                 if (sleep_env.slp_state == ARCH_DEEP_SLEEP_ON)
                     twirq_set_value += RCX_OTP_COPY_OVERHEAD;
-                
+
                 // BOOST mode + RCX is not supported
-                if (GetBits16(ANA_STATUS_REG, BOOST_SELECTED) == 1)
-                    ASSERT_WARNING(0);
+                // Re: Email from Ian Morris
+                //if (GetBits16(ANA_STATUS_REG, BOOST_SELECTED) == 1)
+                //    ASSERT_WARNING(0);
 
                 // Program LP deassertion to occur when the XTAL16M has settled
                 twirq_reset_value = lpcycles - xtal16m_settling_cycles;
-                
+
                 // TWEXT setting
                 twext_value = lpcycles;
              }
@@ -583,16 +584,16 @@ sleep_mode_t rwip_sleep(void)
                 twirq_reset_value = TWIRQ_RESET_VALUE;
                 twext_value = TWEXT_VALUE_XTAL32;
             }
-            else       
+            else
             {
                 // The time we need to wake-up before "time 0" to do XTAL16 settling,
                 // call periph_init() and power-up the BLE core is LP_ISR_TIME_XTAL32_CYCLES in this case.
-                
+
                 // Set TWIRQ_SET taking into account that some LP cycles are needed for the power up FSM.
                 twirq_set_value = XTAL32_POWER_UP_TIME + LP_ISR_TIME_XTAL32_CYCLES;
                 if (sleep_env.slp_state == ARCH_DEEP_SLEEP_ON)
                     twirq_set_value += XTAL32_OTP_COPY_OVERHEAD;
-                
+
                 // Adjust TWIRQ_SET in case of BOOST mode, if needed
                 if (set_boost_low_vbat1v_overhead == APPLY_OVERHEAD)
                     twirq_set_value += BOOST_POWER_UP_OVERHEAD;
@@ -600,13 +601,13 @@ sleep_mode_t rwip_sleep(void)
 
                 // Program LP deassertion to occur when the XTAL16M has settled
                 twirq_reset_value = LP_ISR_TIME_XTAL32_CYCLES - XTAL16M_SETTLING_IN_XTAL32_CYCLES;
-                
+
                 // TWEXT setting
                 twext_value = LP_ISR_TIME_XTAL32_CYCLES;
             }
         }
 
-       
+
         //Prepare BLE_ENBPRESET_REG for next sleep cycle
         SetBits32(BLE_ENBPRESET_REG, TWIRQ_RESET, twirq_reset_value);   // TWIRQ_RESET
         SetBits32(BLE_ENBPRESET_REG, TWIRQ_SET, twirq_set_value);       // TWIRQ_SET
@@ -614,13 +615,13 @@ sleep_mode_t rwip_sleep(void)
 
         //Everything ready for sleep!
         proc_sleep = mode_sleeping;
-        
+
         if (USE_POWER_OPTIMIZATIONS)
         {
             // Eliminate any additional delays.
-            if (sleep_duration) 
+            if (sleep_duration)
                 sleep_duration += SLEEP_DURATION_CORR;
-            
+
             POWER_PROFILE_SLEEP_TIMES;
         }
 
@@ -631,41 +632,41 @@ sleep_mode_t rwip_sleep(void)
                 sleep_lp_cycles = rwip_slot_2_lpcycles(sleep_duration);
             else if ( arch_clk_is_RCX20() )
                 sleep_lp_cycles = rwip_slot_2_lpcycles_rcx(sleep_duration);
-            
+
             lld_sleep_enter(sleep_lp_cycles, rwip_env.ext_wakeup_enable);
         }
-     
+
         DBG_SWDIAG(SLEEP, SLEEP, 1);
 
         /************************************************************************
          **************               SWITCH OFF RF                **************
-         ************************************************************************/        
+         ************************************************************************/
         POWER_PROFILE_REMAINING_TIME;
-        
+
         rwip_rf.sleep();
-        
+
         if (USE_POWER_OPTIMIZATIONS)
         {
-            
-            if (BLE_APP_PRESENT)            
+
+            if (BLE_APP_PRESENT)
             {
                 if ( app_use_lower_clocks_check() )
                 {
                     // It will save some power if you lower the clock while waiting for STAT...
                     SetBits16(CLK_AMBA_REG, PCLK_DIV, 3);  // lowest is 2MHz (div 8, source is @16MHz)
                     SetBits16(CLK_AMBA_REG, HCLK_DIV, 3);
-                    
+
                     slow_system_clk = true;
                 }
             }
         }
-                
+
         while(!ble_deep_sleep_stat_getf());
-                
+
         //check and wait till you may disable the radio. 32.768KHz XTAL must be running!
         //(debug note: use BLE_CNTL2_REG:MON_LP_CLK bit to check (write 0, should be set to 1 by the BLE))
         while ( !(GetWord32(BLE_CNTL2_REG) & RADIO_PWRDN_ALLOW) ) {};
-        
+
         if (USE_POWER_OPTIMIZATIONS)
         {
             if (slow_system_clk)
@@ -677,9 +678,9 @@ sleep_mode_t rwip_sleep(void)
 
         ble_regs_push();    // push the ble ret.vars to retention memory
 //        smpc_regs_push();   // push smpc ble ret.vars to retention memory
-        
+
         //BLE CLK must be turned off when DEEP_SLEEP_STAT is set
-        SetBits16(CLK_RADIO_REG, BLE_ENABLE, 0);   												
+        SetBits16(CLK_RADIO_REG, BLE_ENABLE, 0);
 
     } while(0);
 
@@ -696,7 +697,7 @@ void rwip_init(uint32_t error)
 
     // Reset RW environment
     memset(&rwip_env, 0, sizeof(rwip_env));
-	
+
 	ke_mem_heaps_used = KE_MEM_BLOCK_MAX;
 
     #if (KE_SUPPORT)
@@ -705,9 +706,9 @@ void rwip_init(uint32_t error)
     // Initialize memory heap used by kernel.
     #if (KE_MEM_RW)
     // Memory allocated for environment variables
-    ke_mem_init(KE_MEM_ENV,(uint8_t*)(jump_table_struct[rwip_heap_env_pos]),     jump_table_struct[rwip_heap_env_size]);	
+    ke_mem_init(KE_MEM_ENV,(uint8_t*)(jump_table_struct[rwip_heap_env_pos]),     jump_table_struct[rwip_heap_env_size]);
     // Memory allocated for Attribute database
-    ke_mem_init(KE_MEM_ATT_DB,(uint8_t*)(jump_table_struct[rwip_heap_db_pos]),      jump_table_struct[rwip_heap_db_size]);	        
+    ke_mem_init(KE_MEM_ATT_DB,(uint8_t*)(jump_table_struct[rwip_heap_db_pos]),      jump_table_struct[rwip_heap_db_size]);
     // Memory allocated for kernel messages
     ke_mem_init(KE_MEM_KE_MSG,(uint8_t*)(jump_table_struct[rwip_heap_msg_pos]),     jump_table_struct[rwip_heap_msg_size]);
     // Non Retention memory block
