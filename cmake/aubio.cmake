d46da96bee82ffc935722e130cf3a573192bdb92
Matt Michini <mmichini@anki.com>
2017-12-19 12:04:20 -0800

    cmake changes for android build

--- cmake/aubio.cmake
+++ cmake/aubio.cmake
@@ -1,23 +1,20 @@
-set(AUBIO_HOME "/Users/matt/github/aubio")
-
-set(AUBIO_INCLUDE_PATH
-   "${AUBIO_HOME}/build/dist/usr/local/include"
-)
-
-set(AUBIO_LIBS
-  aubio
-)
-
 
 if (MACOSX)
-  set(AUBIO_LIB_PATH "${AUBIO_HOME}/build/src")
+  set(AUBIO_INCLUDE_PATH "/usr/local/Cellar/aubio/0.4.6/include")
+  set(AUBIO_LIB_PATH "/usr/local/Cellar/aubio/0.4.6/lib/libaubio.dylib")
+elseif (ANDROID)
+  set(AUBIO_INCLUDE_PATH "/Users/matt/github/aubio/dist-android-19-arm/usr/local/include")
+  set(AUBIO_LIB_PATH "/Users/matt/github/aubio/dist-android-19-arm/usr/local/lib/libaubio.so")
 endif()
+
+set(AUBIO_LIBS aubio)
 
 foreach(LIB ${AUBIO_LIBS})
   add_library(${LIB} SHARED IMPORTED)
   set_target_properties(${LIB} PROPERTIES
     IMPORTED_LOCATION
-    "${AUBIO_LIB_PATH}/lib${LIB}.dylib"
+    "${AUBIO_LIB_PATH}"
     INTERFACE_INCLUDE_DIRECTORIES
     "${AUBIO_INCLUDE_PATH}")
 endforeach()
+
