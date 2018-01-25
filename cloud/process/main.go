949f2e1500a619163ea2f66f039ddf991186a370
baustinanki <baustinanki@users.noreply.github.com>
2018-01-25 13:32:01 -0800

    Add verbose logging to cloud process (#498)
    * Add verbose logging to cloud process
    
    If started with the "-verbose" flag, the cloud process will add
    more logging about when it receives data from the mic, when that
    data is uploaded, and when an intent is received from the server.
    It does not (yet) add more logging to Chipper, which would probably
    be needed to get a complete picture of networking costs.
    
    * Improve voice capture testing program
    
    - Fix a bug that caused the written wave file to not have any
      samples in it (audio callback was tossing samples after upload)
    - Put upload on a separate routine behind a buffered channel to
      prevent audio recording from getting blocked while waiting for
      upload to happen
    - Add voice sample to be used in future
    
    * Add mechanism to test cloud client on-robot w/o mics
    
    - Add wavtest program, which will read in a wav file (specified on
      command line) and feed the samples inside it to the cloud client
      through a test socket as if the data was being recorded in real
      time
    - Add test server socket to cloud client to accept connections from
      wav tester
    
    * Temporarily disable wavtest build target
    
    Having multiple go targets built by default causes issues with
    `go get` running in parallel and fetching the same remote
    repositories to put into the same location. Until a better fix for
    this is added, we don't need to build wavtest by default.

--- cloud/process/main.go
+++ cloud/process/main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"anki/cloudproc"
 	"anki/ipc"
+	"flag"
 	"fmt"
 	"time"
 )
+
+var verbose bool
 
 func getSocketWithRetry(name string, client string) ipc.Conn {
 	for {
@@ -19,11 +22,40 @@ func getSocketWithRetry(name string, client string) ipc.Conn {
 	}
 }
 
+func testReader(serv ipc.Server, ch chan []byte) {
+	for conn := range serv.NewConns() {
+		go func(conn ipc.Conn) {
+			for {
+				msg := conn.ReadBlock()
+				if msg == nil || len(msg) == 0 {
+					conn.Close()
+					return
+				}
+				ch <- msg
+			}
+		}(conn)
+	}
+}
+
 func main() {
 	fmt.Println("Hello, world!")
-	micSock := getSocketWithRetry("/dev/mic_sock", "cp_mic")
-	aiSock := getSocketWithRetry("/dev/ai_sock", "cp_ai")
+	micSock := getSocketWithRetry(ipc.GetSocketPath("mic_sock"), "cp_mic")
+	defer micSock.Close()
+	aiSock := getSocketWithRetry(ipc.GetSocketPath("ai_sock"), "cp_ai")
+	defer aiSock.Close()
+	testSock, err := ipc.NewUnixgramServer(ipc.GetSocketPath("cp_test"))
+	if err != nil {
+		fmt.Println("Server create error:", err)
+	}
+	defer testSock.Close()
 	fmt.Println("Sockets successfully created")
 
-	cloudproc.RunProcess(micSock, aiSock, nil)
+	testChan := make(chan []byte)
+	defer close(testChan)
+	go testReader(testSock, testChan)
+
+	flag.BoolVar(&verbose, "verbose", false, "enable verbose logging")
+	flag.Parse()
+	cloudproc.SetVerbose(verbose)
+	cloudproc.RunProcess(micSock, aiSock, testChan, nil)
 }
