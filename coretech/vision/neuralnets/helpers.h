69c1f5fce7119632bcbdadb274cb8c0b024dee77
Andrew Stein <andrew@anki.com>
2018-02-06 19:03:34 -0800

    Actually print errors with std::cerr

---
+++ coretech/vision/neuralnets/helpers.h
@@ -0,0 +1,48 @@
+#ifndef __Anki_ObjectDetector_TensorFlow_Helpers_H__
+#define __Anki_ObjectDetector_TensorFlow_Helpers_H__
+
+#include <iostream>
+#include <sys/stat.h>
+
+#define QUOTE_HELPER(__ARG__) #__ARG__
+#define QUOTE(__ARG__) QUOTE_HELPER(__ARG__)
+
+// TODO: Use coretech shared types.h? Or replicate full enum here?
+using Result = int;
+const Result RESULT_OK = 0;
+const Result RESULT_FAIL = 1;
+
+using TimeStamp_t = uint32_t;
+
+// Define local versions of FulLFilePath and FileExists to avoid needing to include 
+// all of Anki::Util
+static inline std::string FullFilePath(const std::string& path, const std::string& filename)
+{
+  return path + "/" + filename;
+}
+
+static inline bool FileExists(const std::string& filename)
+{
+  struct stat st;
+  const int statResult = stat(filename.c_str(), &st);
+  return (statResult == 0);
+}
+
+static inline void PRINT_NAMED_ERROR(const char* eventName, const char* format, ...)
+{
+  const size_t kMaxStringBufferSize = 1024;
+
+  // parse string
+  va_list args;
+  char logString[kMaxStringBufferSize]{0};
+  va_start(args, format);
+  vsnprintf(logString, kMaxStringBufferSize, format, args);
+  va_end(args);
+
+  // TODO: log it
+  //LogError(eventName, keyValues, logString);
+
+  std::cerr << "ERROR: " << logString << std::endl;
+}
+
+#endif /* __Anki_ObjectDetector_TensorFlow_Helpers_H__ */
