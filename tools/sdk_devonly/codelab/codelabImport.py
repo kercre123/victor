bd3f3510c6fc45dc5bc2df72cfaaa539b8975b53
Sheldon <msheldon@anki.com>
2018-02-09 11:00:10 -0800

    COZMO-16257 Simplify Cozmo Code Lab Implementation Flow (#6714)
    COZMO-16257 Simplify Cozmo Code Lab Implementation Flow
    
    Quick script with GUI to simplify the process of featuring a Code Lab script. This pair of scripts adds both the featured project data, and the what's new modal popup data, and the required strings and images for both. But does not run scan_project, or automatically do any git commands.

---
+++ tools/sdk_devonly/codelab/codelabImport.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python3
+
+COZMO_DIR = "../../../"
+
+Paths = {
+    "SCRATCH_DIR": COZMO_DIR + "unity/Cozmo/Assets/StreamingAssets/Scratch/",
+    "STRINGS_DIR": COZMO_DIR + "unity/Cozmo/Assets/StreamingAssets/LocalizedStrings/en-US",
+    "WHATS_NEW_DIR": COZMO_DIR + "unity/Cozmo/Assets/AssetBundles/Shared/Managers-Bundle/",
+    "WHATS_NEW_ART_DIR": COZMO_DIR + "unity/Cozmo/Assets/AssetBundles/NeedsHub/Art/WhatsNewIconSprites/",
+}
+
+import os, sys, io
+import urllib.parse, json
+from collections import OrderedDict
+from shutil import copyfile
+from datetime import datetime
+
+def UnderscoreName(name):
+    return name.lower().replace(" ","_")
+    
+def CamelCaseName(name, firstLower=False):
+    name = name.title().replace(" ","")
+    if firstLower:
+        name = name[0].lower() + name[1:]
+    return name
+
+log_text = print
+
+## Shamelessly taken from codelab.py. Might break in the future. Probably not.
+CODELAB_FILE_HEADER = "CODELAB:"
+def load_codelab_file(file_contents):
+    unencoded_contents = urllib.parse.unquote_plus(file_contents)
+
+    if not unencoded_contents.startswith(CODELAB_FILE_HEADER):
+        log_text("Error: load_codelab_file - file doesn't start with %s" % CODELAB_FILE_HEADER)
+        return None
+
+    unencoded_contents = unencoded_contents[len(CODELAB_FILE_HEADER):]
+
+    try:
+        json_contents = json.loads(unencoded_contents)
+    except json.decoder.JSONDecodeError as e:
+        log_text("load_codelab_file: Decode error: %s" % e)
+        log_text("unencoded_contents = [%s]" % unencoded_contents)
+        return None
+
+    return json_contents
+## End of shame
+
+def readCodelabFile(codelabFilePath):
+    with open(codelabFilePath) as f:
+        codelabJson = load_codelab_file(f.read())
+        uuid = codelabJson["ProjectUUID"]
+        projectJson = json.loads(codelabJson["ProjectJSON"], object_pairs_hook=OrderedDict)
+        log_text('uuid: {}'.format(uuid))
+
+    return (uuid, projectJson)
+
+def createFeaturedProjectFile(name, uuid, projectJson):
+    featuredProjectFilePath = "{SCRATCH_DIR}/featuredProjects/{}_en-us.json".format(UnderscoreName(name), **Paths)
+    with open(featuredProjectFilePath, "w", encoding="utf8") as f:
+        json.dump(OrderedDict([("ProjectUUID",uuid), ("ProjectJSON",projectJson)]), f, sort_keys=False, indent=4, separators=(',', ': '))
+    log_text('Created featured project file: {}'.format(featuredProjectFilePath))
+
+def addFeaturedProject(uuid, name, desc, instructions, bgColor, textColor, startDate):
+    featuredProjectsJsonPath = "{SCRATCH_DIR}/featured-projects.json".format(**Paths)
+    with open(featuredProjectsJsonPath, encoding="utf8") as f:
+        featuredProjects = json.load(f, object_pairs_hook=OrderedDict)
+
+    featuredProjects.append(
+        OrderedDict([
+            ("ProjectUUID", uuid),
+            ("ProjectName", "codeLabFeatured{}.projectName".format(CamelCaseName(name))),
+            ("DASProjectName", CamelCaseName(name)),
+            ("VersionNum", "3"),
+            ("IsVertical", "true"),
+            ("DisplayOrder", "11"),
+            ("FeaturedProjectDescription", "codeLabFeatured{}.projectDescription".format(CamelCaseName(name))),
+            ("FeaturedProjectInstructions", "codeLabFeatured{}.projectInstructions".format(CamelCaseName(name))),
+            ("FeaturedProjectImageName", CamelCaseName(name)),
+            ("FeaturedProjectBackgroundColor", bgColor),
+            ("FeaturedProjectTitleTextColor", textColor),
+            ("ProjectJSONFile", UnderscoreName(name)),
+            ("StartDate", OrderedDict([
+                ("Month", str(startDate.month)),
+                ("Day", str(startDate.day)),
+                ("Year", str(startDate.year))
+            ]))
+        ]))
+
+    with open(featuredProjectsJsonPath, "w", encoding="utf8") as f:
+        json.dump(featuredProjects, f, sort_keys=False, indent=4, separators=(',', ': '))
+
+def addCodeLabStrings(name, desc, instructions):
+    stringsPath = "{STRINGS_DIR}/CodeLabStrings.json".format(**Paths)
+    with open(stringsPath, encoding="utf8") as f:
+        strings = json.load(f, object_pairs_hook=OrderedDict)
+
+    strings["codeLabFeatured{}.projectName".format(CamelCaseName(name))] = {"translation": name}
+    strings["codeLabFeatured{}.projectDescription".format(CamelCaseName(name))] = {"translation": desc}
+    strings["codeLabFeatured{}.projectInstructions".format(CamelCaseName(name))] = {"translation": instructions}
+
+    with open(stringsPath, "w", encoding="utf8") as f:
+        jsondump = json.dumps(strings, sort_keys=False, indent=2, separators=(',', ': '), ensure_ascii=False)
+        f.write(jsondump)
+
+def copyImages(name, framingCardPath, activeIconPath, idleIconPath):
+    framingCardDestPath = "{SCRATCH_DIR}/extra/images/featured/framing_card{}_feat.png".format(CamelCaseName(name), **Paths)
+    copyfile(framingCardPath, framingCardDestPath)
+    
+    activeIconDestPath = "{SCRATCH_DIR}/images/ui/play-now-modal/icon_{}_active.jpg".format(CamelCaseName(name).lower(), **Paths)
+    copyfile(activeIconPath, activeIconDestPath)
+    
+    idleIconDestPath = "{SCRATCH_DIR}/images/ui/play-now-modal/icon_{}_idle.jpg".format(CamelCaseName(name).lower(), **Paths)
+    copyfile(idleIconPath, idleIconDestPath)
+
+def addWhatsNewModalData(name, uuid, leftTint, rightTint, startDate, endDate):
+    whatsNewModelDataJsonPath = "{WHATS_NEW_DIR}/whatsNewModalData.json".format(**Paths)
+    with open(whatsNewModelDataJsonPath, encoding="utf8") as f:
+        modalData = json.load(f, object_pairs_hook=OrderedDict)
+
+    modalData.append(
+        OrderedDict([
+            ("FeatureID", "code_lab_{}".format(UnderscoreName(name))),
+            ("DASEventFeatureID", "code_lab_{}".format(UnderscoreName(name))),
+            ("StartDate", OrderedDict([
+                ("Month", str(startDate.month)),
+                ("Day", str(startDate.day)),
+                ("Year", str(startDate.year))
+            ])),
+            ("EndDate", OrderedDict([
+                ("Month", str(endDate.month)),
+                ("Day", str(endDate.day)),
+                ("Year", str(endDate.year))
+            ])),
+            ("TitleKey", "whatsNew.CodeLab_{}.title".format(CamelCaseName(name))),
+            ("DescriptionKey", "whatsNew.CodeLab_{}.description".format(CamelCaseName(name))),
+            ("IconAssetBundleName", "whats_new_icon_sprites"),
+            ("IconAssetName", "icon_{}".format(CamelCaseName(name, firstLower=True))),
+            ("LeftTintColor", leftTint),
+            ("RightTintColor", rightTint),
+            ("CodeLabData", OrderedDict([
+                ("ShowButton", True),
+                ("ProjectGuid", uuid)
+            ]))
+        ]))
+
+    with open(whatsNewModelDataJsonPath, "w", encoding="utf8") as f:
+        json.dump(modalData, f, sort_keys=False, indent=2, separators=(',', ': '))
+
+def addWhatsNewStrings(name, desc):
+    stringsPath = "{STRINGS_DIR}/WhatsNewStrings.json".format(**Paths)
+    with open(stringsPath, encoding="utf8") as f:
+        strings = json.load(f, object_pairs_hook=OrderedDict)
+
+    strings["whatsNew.CodeLab_{}.title".format(CamelCaseName(name))] = {"translation": name}
+    strings["whatsNew.CodeLab_{}.description".format(CamelCaseName(name))] = {"translation": desc}
+
+    with open(stringsPath, "w", encoding="utf8") as f:
+        jsondump = json.dumps(strings, sort_keys=False, indent=2, separators=(',', ': '), ensure_ascii=False)
+        f.write(jsondump)
+
+def copyWhatsNewImage(name, whatsNewIconPath):
+    destPath = "{WHATS_NEW_ART_DIR}/whats_new_icon_sprites-SD/Packed/icon_{}.png".format(CamelCaseName(name, firstLower=True), **Paths)
+    copyfile(whatsNewIconPath, destPath)
+
+    destPath = "{WHATS_NEW_ART_DIR}/whats_new_icon_sprites-HD/Packed/icon_{}.png".format(CamelCaseName(name, firstLower=True), **Paths)
+    copyfile(whatsNewIconPath, destPath)
+
+    destPath = "{WHATS_NEW_ART_DIR}/whats_new_icon_sprites-UHD/Packed/icon_{}.png".format(CamelCaseName(name, firstLower=True), **Paths)
+    copyfile(whatsNewIconPath, destPath)
+
+
+def importCodelabFile(projectName, codelabFilePath, description, instructions, bgColor, textColor, framingCardPath, activeIconPath, idleIconPath,
+                      leftTint, rightTint, whatsNewIconPath, startDate, endDate):
+
+    try:
+        log_text('Reading codelab file...')
+        (uuid, projectJson) = readCodelabFile(codelabFilePath)
+        log_text('Creating featured project file...')
+        createFeaturedProjectFile(projectName, uuid, projectJson)
+        log_text('Adding featured project to featured-projects.json...')
+        addFeaturedProject(uuid, projectName, description, instructions, bgColor, textColor, startDate)
+        log_text('Adding CodeLab strings...')
+        addCodeLabStrings(projectName, description, instructions)
+        log_text('Copying CodeLab images...')
+        copyImages(projectName, framingCardPath, activeIconPath, idleIconPath)
+
+        log_text('Adding new modal to whatsNewModalData.json...')
+        addWhatsNewModalData(projectName, uuid, leftTint, rightTint, startDate, endDate)
+        log_text('Adding WhatsNew strings...')
+        addWhatsNewStrings(projectName, description)
+        log_text('Copying WhatsNew images...')
+        copyWhatsNewImage(projectName, whatsNewIconPath)
+        log_text('Success!')
+    except Exception as e:
+        log_text(str(e))
---
+++ tools/sdk_devonly/codelab/codelabImportGUI.py
@@ -0,0 +1,156 @@
+#! /usr/bin/env python3
+
+import os
+from datetime import datetime
+import json
+
+from tkinter import *
+from tkinter.scrolledtext import ScrolledText
+from tkinter import filedialog
+
+import codelabImport
+
+root = Tk()
+
+rootFrame = Frame(root)
+rootFrame.pack()
+
+bottomFrame = Frame(root)
+bottomFrame.pack()
+
+log = ScrolledText(bottomFrame, height=6, wrap=WORD, takefocus=0, relief='sunken',bd=1)
+log.pack()
+
+def log_text(text):
+    log.insert(END, text + '\n')
+    log.see(END)
+
+def TextEntry(name, default='', **options):
+    frame = Frame(rootFrame)
+    Label(frame, text="{}:".format(name), width=20, anchor=E).pack(side=LEFT)
+    field = Entry(frame, **options)
+    field.insert(0,default)
+    field.pack(side=LEFT)
+    frame.pack(fill=X)
+    return field
+
+def MultilineTextEntry(name, default='', **options):
+    frame = Frame(rootFrame)
+    Label(frame, text="{}:".format(name), width=20, anchor=E).pack(side=LEFT)
+    field = ScrolledText(frame, bd=1, relief='sunken', **options)
+    field.pack(side=LEFT)
+    frame.pack(fill=X)
+    return field
+    
+def FileEntry(name, filetypes):
+    frame = Frame(rootFrame)
+    Label(frame, text="{}:".format(name), width=20, anchor=E).pack(side=LEFT)
+    field = Entry(frame, width=50)
+    field.pack(side=LEFT)
+    def SelectFile():
+        field.delete(0,END)
+        field.insert(0, filedialog.askopenfilename(filetypes = filetypes))
+    button = Button(frame, text='Open...', command=SelectFile)
+    button.pack(side=LEFT)
+    frame.pack(fill=X)
+    return field
+    
+def ListBoxEntry(name, choices):
+    frame = Frame(rootFrame)
+    frame.pack(fill=X)
+    Label(frame, text="{}:".format(name), width=20, anchor=E).pack(side=LEFT)
+    var = StringVar(frame)
+    var.set(choices[0])
+    field = OptionMenu(frame, var, *choices)
+    field.pack(side=LEFT)
+    return var
+
+def DateEntry(name):
+    frame = Frame(rootFrame)
+    frame.pack(fill=X)
+    Label(frame, text="{}:".format(name), width=20, anchor=E).pack(side=LEFT)
+    now = datetime.now()
+    year = Entry(frame, width=4)
+    year.insert(0, str(now.year))
+    year.pack(side=LEFT)
+    month = Entry(frame, width=2)
+    month.insert(0, str(now.month))
+    month.pack(side=LEFT)
+    day = Entry(frame, width=2)
+    day.insert(0, str(now.day))
+    day.pack(side=LEFT)
+    return (year, month, day)
+
+# Setup UI
+nameField = TextEntry('Project Name', width=25)
+codelabFile = FileEntry('.codelab File', (("codelab files", "*.codelab"),))
+description = MultilineTextEntry('Description', width=50, height=6)
+instructions = MultilineTextEntry('Instructions', width=50, height=6)
+bgColor = TextEntry('BG Color', default='#ffe700')
+textColor = TextEntry('Text Color', default='#696b68')
+framingCardPath = FileEntry('Framing Card Image', (("Image files", "*.png"),))
+activeIconPath = FileEntry('Active Icon Image', (("Image files", "*.jpg"),))
+idleIconPath = FileEntry('Idle Icon Image', (("Image files", "*.jpg"),))
+whatsNewIconPath = FileEntry('Whats New Icon Image', (("Image files", "*.png"),))
+whatsNewTint = ListBoxEntry('Whats New Tint', ['yellow', 'purple', 'red', 'green'])
+startYear, startMonth, startDay = DateEntry('Start Date')
+endYear, endMonth, endDay = DateEntry('End Date')
+
+def testEmpty(name, value):
+    if len(value) == 0:
+        log_text('Error: {} field is empty.'.format(name))
+        return 1
+    return 0
+
+def Import():
+    errors = 0
+    errors += testEmpty('Project Name', nameField.get())
+    errors += testEmpty('.codelab File', codelabFile.get())
+    errors += testEmpty('Description', codelabFile.get())
+    errors += testEmpty('Instructions', codelabFile.get())
+    errors += testEmpty('BG Color', codelabFile.get())
+    errors += testEmpty('Text Color', codelabFile.get())
+    errors += testEmpty('Framing Card Image', codelabFile.get())
+    errors += testEmpty('Active Icon Image', codelabFile.get())
+    errors += testEmpty('Idle Icon Image', codelabFile.get())
+    errors += testEmpty('Whats New Icon Image', codelabFile.get())
+
+    if(errors > 0):
+        log_text('Import aborted, please fix errors above and try again.')
+        return
+    
+    tints = {
+        'yellow':([212,172,3], [255,220,26]),
+        'purple':([130,31,183], [150,73,190]),
+        'red':([195,50,50], [253,78,93]),
+        'green':([93,197,21], [157,227,53])
+    }
+    
+    tint = tints[whatsNewTint.get()]
+    
+    startDate = datetime(int(startYear.get()), int(startMonth.get()), int(startDay.get()))
+    endDate = datetime(int(endYear.get()), int(endMonth.get()), int(endDay.get()))
+    
+    codelabImport.log_text = log_text # Route codelabImport output to GUI
+    codelabImport.importCodelabFile(nameField.get().rstrip(),
+                                    codelabFile.get(),
+                                    description.get(1.0, END).rstrip(),
+                                    instructions.get(1.0, END).rstrip(),
+                                    bgColor.get(),
+                                    textColor.get(),
+                                    framingCardPath.get(),
+                                    activeIconPath.get(),
+                                    idleIconPath.get(),
+                                    tint[0], tint[1],
+                                    whatsNewIconPath.get(),
+                                    startDate, endDate)
+
+Button(rootFrame, text='Import', command=Import).pack()
+
+log_text('Codelab Importer\nFill in all fields, then click the Import button.\nErrors will be reported in this box.')
+
+root.lift()
+root.attributes('-topmost',True)
+root.after_idle(root.attributes,'-topmost',False)
+
+root.mainloop()
