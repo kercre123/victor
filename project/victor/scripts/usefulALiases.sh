7be93370ab6cfbef9a497a6cbbf4383aa13e081f
Lee Crippen <lee@anki.com>
2018-02-14 20:12:08 -0800

    VIC-1292 Add scripts for aliases

--- project/victor/scripts/usefulALiases.sh
+++ project/victor/scripts/usefulALiases.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 # Handy Victor Aliases from Al Chaussee
 # =====================================
 # They can be called from anywhere within the git project to build, deploy, and run on physical robots.
@@ -6,25 +8,17 @@
 
 alias GET_GIT_ROOT='export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`'
 
-alias victor_restart='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/systemctl.sh restart anki-robot.target'
-alias victor_start='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/systemctl.sh start anki-robot.target'
-alias victor_stop='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/systemctl.sh stop anki-robot.target'
+alias victor_restart='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_restart.sh'
+alias victor_start='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_start.sh'
+alias victor_stop='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_stop.sh'
 
-alias victor_build='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/build-victor.sh -c Release'
-alias victor_deploy='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/deploy.sh -c Release'
-alias victor_deploy_run="GET_GIT_ROOT;\
-    victor_stop\
-    ${GIT_PROJ_ROOT}/project/victor/scripts/deploy.sh -c Release &&\
-    victor_restart"
+alias victor_build='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_build.sh'
+alias victor_deploy='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_deploy.sh'
+alias victor_deploy_run='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_deploy_run.sh'
+alias victor_build_run='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_build_run.sh'
+alias victor_assets='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_assets.sh'
+alias victor_assets_force='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/victor_assets_force.sh'
 
-alias victor_build_run="GET_GIT_ROOT;\
-    victor_stop &&\
-    ${GIT_PROJ_ROOT}/project/victor/build-victor.sh -I -c Release &&\
-    ${GIT_PROJ_ROOT}/project/victor/scripts/deploy.sh -c Release &&\
-    victor_restart"
-
-alias victor_assets='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/deploy-assets.sh ${GIT_PROJ_ROOT}/_build/android/Release/assets'
-alias victor_assets_force='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/project/victor/scripts/deploy-assets.sh -f ${GIT_PROJ_ROOT}/_build/android/Release/assets'
 alias victor_log='adb logcat mm-camera:S mm-camera-intf:S mm-camera-eztune:S mm-camera-sensor:S mm-camera-img:S cnss-daemon:S cozmoengined:S ServiceManager:S chatty:S'
 alias victor_ble='GET_GIT_ROOT; node ${GIT_PROJ_ROOT}/tools/victor-ble-cli/index.js'
 
---
+++ project/victor/scripts/victor_restart.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
+${GIT_PROJ_ROOT}/project/victor/scripts/systemctl.sh restart anki-robot.target
