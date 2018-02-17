b850e906015539dace01bb28e9fd5f4ced152548
Paul Aluri <paluri@anki.com>
2018-02-16 17:50:53 -0800

    VIC-1364 Initial build script commits

---
+++ cmake/sodium.cmake
@@ -0,0 +1,14 @@
+if (ANDROID)
+  set(LIBSODIUM_INCLUDE_PATH "${CORETECH_EXTERNAL_DIR}/build/libsodium/android-armv7a/include")
+  set(LIBSODIUM_LIB_PATH "${CORETECH_EXTERNAL_DIR}/build/libsodium/android-armv7a/lib")
+elseif (MACOSX)
+  set(LIBSODIUM_INCLUDE_PATH "${CORETECH_EXTERNAL_DIR}/build/libsodium/mac/include")
+  set(LIBSODIUM_LIB_PATH "${CORETECH_EXTERNAL_DIR}/build/libsodium/mac/lib")
+endif()
+
+set(SODIUM_LIBS sodium)
+
+add_library(sodium STATIC IMPORTED)
+set_target_properties(sodium PROPERTIES
+  IMPORTED_LOCATION "${LIBSODIUM_LIB_PATH}/libsodium.a"
+  INTERFACE_INCLUDE_DIRECTORIES "${LIBSODIUM_INCLUDE_PATH}")
