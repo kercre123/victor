fee0375e9e5c3223ac9cdbf2a40868e26e4984f6
Robert Cosgriff <robert@anki.com>
2018-02-21 15:22:54 -0800

    Add first pass of eye contact behavior

---
+++ engine/aiComponent/behaviorComponent/behaviors/basicWorldInteractions/behaviorEyeContact.cpp
@@ -0,0 +1,63 @@
+/**
+* File: behaviorEyeContact.cpp
+*
+* Author: Robert Cosgriff
+* Created: 2/5/18
+*
+* Description: Simple behavior to react to eye contact
+*
+* Copyright: Anki, Inc. 2017
+*
+**/
+
+// TODO these need to stripped down, let's get it compiling 
+// and then start to get rid of the one that aren't needed
+#include "engine/aiComponent/behaviorComponent/behaviors/basicWorldInteractions/behaviorEyeContact.h"
+
+#include "engine/actions/animActions.h"
+#include "engine/actions/basicActions.h"
+#include "engine/aiComponent/behaviorComponent/behaviorExternalInterface/behaviorExternalInterface.h"
+#include "engine/faceWorld.h"
+
+#include "clad/externalInterface/messageGameToEngine.h"
+#include "clad/types/imageTypes.h"
+
+#include "coretech/common/engine/utils/data/dataPlatform.h"
+#include "coretech/common/engine/jsonTools.h"
+#include "coretech/common/engine/utils/timer.h"
+
+#include "engine/aiComponent/behaviorComponent/behaviorExternalInterface/beiRobotInfo.h"
+#include "engine/components/bodyLightComponent.h"
+#include "engine/components/movementComponent.h"
+#include "engine/components/sensors/touchSensorComponent.h"
+#include "engine/components/visionComponent.h"
+#include "engine/cozmoContext.h"
+#include "engine/externalInterface/externalInterface.h"
+
+#include "util/fileUtils/fileUtils.h"
+
+namespace Anki {
+namespace Cozmo {
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+bool BehaviorEyeContact::WantsToBeActivatedBehavior() const
+{
+  return GetBEI().GetFaceWorld().IsMakingEyeContact();
+}
+
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorEyeContact::OnBehaviorActivated()
+{
+  DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::HikingSquintStart));
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorEyeContact::OnBehaviorDeactivated()
+{
+  // TODO not sure what to do here, try to
+  // find another example
+}
+  
+} // namespace Cozmo
+} // namespace Anki
---
+++ engine/aiComponent/behaviorComponent/behaviors/basicWorldInteractions/behaviorEyeContact.h
@@ -0,0 +1,54 @@
+/**
+* File: behaviorEyeContact.h
+*
+* Author: Robert Cosgriff
+* Created: 2/5/18
+*
+* Description: Simple behavior to react to eye contact
+*
+* Copyright: Anki, Inc. 2018
+*
+**/
+
+#ifndef __Cozmo_Basestation_Behaviors_BehaviorEyeContact_H__
+#define __Cozmo_Basestation_Behaviors_BehaviorEyeContact_H__
+
+#include "engine/aiComponent/behaviorComponent/behaviors/iCozmoBehavior.h"
+
+namespace Anki {
+namespace Cozmo {
+  
+class BehaviorEyeContact : public ICozmoBehavior
+{
+protected:
+  // Enforce creation through BehaviorContainer
+  friend class BehaviorContainer;
+  BehaviorEyeContact(const Json::Value& config);
+  
+public:
+  virtual ~BehaviorEyeContact() override {}
+  virtual bool WantsToBeActivatedBehavior() const override;
+  // virtual bool WantsToBeActivatedBehavior() const override
+  // {
+  // PRINT_NAMED_INFO("BehaviorEyeContact.WantsToBeActivatedBehavior","");
+  //  return true;
+  // }
+  
+protected:
+  // TODO review modifiers
+  virtual void GetBehaviorOperationModifiers(BehaviorOperationModifiers& modifiers) const override {
+    modifiers.wantsToBeActivatedWhenOffTreads = true;
+    modifiers.wantsToBeActivatedWhenOnCharger = true;
+    // modifiers.behaviorAlwaysDelegates = false;
+  }
+
+  virtual void OnBehaviorActivated() override;
+  virtual void OnBehaviorDeactivated() override;
+  
+};
+  
+
+} // namespace Cozmo
+} // namespace Anki
+
+#endif // __Cozmo_Basestation_Behaviors_BehaviorEyeContact_H__
