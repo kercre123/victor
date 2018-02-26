12d2432b62b60a01ff383d4d5dbc86f62dd15d50
David C Mudie <dmudie@anki.com>
2018-02-26 11:32:30 -0800

    Add new helper script vic-init.sh (#753)
    1. Add new vic-init.sh to perform cleanup tasks specified by developer
    
    2. Update platform/config cmake to deploy (1)
    
    As is, vic-init.sh can be run by hand on robot.
    Future versions of the OS will provide a service definition to run this automatically.

---
+++ platform/config/bin/vic-init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh -f
+#
+# platform/config/bin/vic-init.sh
+#
+# Perform any cleanup & init tasks not handled by the OS.
+#
+
+# VIC-1369: Older OS images wrote journal to root partition
+echo "Vacuum journal"
+/bin/journalctl --vacuum-size=10M
+
+# VIC-1504: Older OS images do not provide init_debuggerd.service
+active=`/bin/systemctl is-active init_debuggerd`
+if [ "$active" != "active" ]; then
+  echo "Enable tombstones"
+  /bin/mkdir -p /data/tombstones
+  pid=`/bin/pgrep debuggerd`
+  if [ "$pid" == "" ]; then
+    echo "Start debuggerd"
+    /usr/bin/debuggerd > /dev/null 2>&1 &
+  fi
+fi
+
