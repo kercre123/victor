f7dfdb105c0beab46c2bde4924ea9dc2d74d748c
Bryon Vandiver <unicdk@gmail.com>
2018-02-28 18:30:23 -0800

    Final encryption

--- robot/cube_firmware/src/user_app/user_custs1_impl.c
+++ robot/cube_firmware/src/user_app/user_custs1_impl.c
@@ -32,16 +32,11 @@
 static bool app_valid = false;
 static bool app_running = false;
 static timer_hnd app_timer = EASY_TIMER_INVALID_TIMER;
-static ApplicationMap* const app_current = (ApplicationMap*) 0x20005800;
-static uint8_t* app_write_address = (uint8_t*) app_current;
+static const int app_location = 0x20005800;
+static ApplicationMap* const app_current = (ApplicationMap*) app_location;
+static uint8_t* app_write_address = (uint8_t*) app_location;
 static const uint8_t xxtea_key[16] = {0x88, 0xdb, 0x37, 0x96, 0x76, 0x8f, 0x86, 0x91, 0x42, 0x24, 0xf4, 0x35, 0xc1, 0xfd, 0x7f, 0xd3};
-
-typedef __packed struct {
-  uint16_t address;
-  uint16_t sum;
-  uint8_t data[15];
-  uint32_t _unused;
-} OTA_Packet;
+static const uint8_t xxtea_nonce[16] = {0x3d, 0x70, 0x7a, 0x86, 0x10, 0x3c, 0xef, 0x4f, 0x83, 0x2f, 0xa8, 0xa6, 0x64, 0x20, 0xba, 0xdf};
 
 void app_send_version() {
   int length = app_valid ? sizeof(app_current->version) : 0;
@@ -132,7 +127,7 @@ void user_custs1_disconnected() {
 #define DELTA 0x9e3779b9
 #define MX (((z>>5^y<<2) + (y>>3^z<<4)) ^ ((sum^y) + (key[(p&3)^e] ^ z)))
 
-void btea(uint32_t *v, int n, const uint32_t* key) {
+void xxtea(uint32_t *v, int n, const uint32_t* key) {
   uint32_t y, z, sum;
   unsigned p, rounds, e;
 
@@ -156,37 +151,34 @@ void user_custs1_ota_target_wr_ind_handler(ke_msg_id_t const msgid,
                                       ke_task_id_t const dest_id,
                                       ke_task_id_t const src_id)
 {
-  static int expected_address = 0;
-  static OTA_Packet packet;
+  static int offset = 0;
+  int length = param->length & ~3;
 
-  memcpy(&packet, param->value, param->length);
-  btea((uint32_t*)&packet, param->length / sizeof(uint32_t), (const uint32_t*)&xxtea_key);
-
-  uint16_t sum = 0;
-  for (int i = 0; i < sizeof(packet.data); i++) sum += packet.data[i];
-
-  int address = packet.address & 0x7FFF;
-
-  if (address == 0) {
-    expected_address = 0;
+  if (offset + length > sizeof(ApplicationMap)) {
     app_erase();
-  } else if (sum != packet.sum || address != expected_address) {
-    expected_address = 0;
-    app_erase();
+    offset = 0;
     return ;
   }
   
-  expected_address += sizeof(packet.data);
+  memcpy(&app_write_address[offset], param->value, length);
+  offset += length;
 
-  // Cannot write past end of memory
-  if (address + sizeof(packet.data) > sizeof(ApplicationMap)) return ;
+  if (param->length & 3) {
+    // Erase upper memory (anti-hack)
+    memset(&app_write_address[offset], 0, sizeof(ApplicationMap) - offset);
+    xxtea((uint32_t*)app_location, offset / sizeof(uint32_t), (const uint32_t*)&xxtea_key);
 
-  memcpy(&app_write_address[address], packet.data, sizeof(packet.data));
+    if (memcmp(xxtea_nonce, app_current->nonce, sizeof(xxtea_nonce)) != 0) {
+      app_erase();
+      offset = 0;
+      return ;
+    }
 
-  if (packet.address & 0x8000) {
     app_valid = true;
     app_send_version();
     app_start();
+
+    return ;
   }
 }
 
--- robot/cube_firmware/tools/sign.py
+++ robot/cube_firmware/tools/sign.py
@@ -2,28 +2,14 @@ from struct import pack
 from xxtea import encrypt, decrypt
 import sys
 
-BLOCK_SIZE = 15
+BLOCK_SIZE = 20
 key = b'\x88\xdb\x37\x96\x76\x8f\x86\x91\x42\x24\xf4\x35\xc1\xfd\x7f\xd3'
 
-def chunk(fi):
-	offset = 0
-	while True:
-		block = fi.read(BLOCK_SIZE)
-
-		if not block:
-			break
-
-		block += b'\x00' * (BLOCK_SIZE - len(block))
-
-		yield offset, block
-
-		offset += BLOCK_SIZE
-
 with open(sys.argv[1], "rb") as fi:
-	all_chunks = [s for s in chunk(fi)]
-	all_chunks[-1] = all_chunks[-1][0] | 0x8000, all_chunks[-1][1]
+	original = fi.read()
+	original = original + b'\x00' * (4 - (len(original) & 4))
+
+	data = encrypt(original, key)
 
 	with open(sys.argv[2], "wb") as fo:
-		for offset, data in all_chunks:
-			encoded = encrypt(pack("<HH%is" % BLOCK_SIZE, offset, sum(data), data), key)
-			fo.write(encoded)
+		fo.write(data + b'\xff')
