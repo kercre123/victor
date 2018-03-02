3d3a123bd1dd80c5b9b7cacd166644df9bf3d38e
Bryon Vandiver <unicdk@gmail.com>
2018-03-02 12:56:29 -0800

    These are not here anymore

--- robot/cube_firmware/app/accel.h
+++
@@ -1,8 +0,0 @@
-#ifndef __ACCEL_H
-#define __ACCEL_H
-
-void hal_acc_init(void);
-void hal_acc_tick(void);
-void hal_acc_stop(void);
-
-#endif
--- robot/cube_firmware/app/uart.h
+++
@@ -1,7 +0,0 @@
-#ifndef __UART_H
-#define __UART_H
-
-void hal_uart_init(void);
-void hal_uart_stop(void);
-
-#endif
