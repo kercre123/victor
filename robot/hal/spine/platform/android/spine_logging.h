8fafb4f295fe1a261541ef64b315ddfd6f195ada
David C Mudie <dmudie@anki.com>
2018-03-02 12:17:55 -0800

    VIC-602: Revise victor log format for efficient filtering (#840)
    1. Move channel name from tag to message body.  This will allow efficient filtering by logcat.
    Alternative solution is to revise logcat to support prefix or regex matching on tags.
    
    2. Demote spine "invalid crc" from error to warning. This will allow us to discard verbose warnings without losing actual errors.
    
    3. Change log tag from "spine" to "vic-spine" because naming.

--- robot/hal/spine/platform/android/spine_logging.h
+++ robot/hal/spine/platform/android/spine_logging.h
@@ -2,10 +2,11 @@
 #define PLATFORM_LOGGING_H
 
 #include "android/log.h"
-static const char *TAG = "spine";
+static const char *TAG = "vic-spine";
 #define LOGI(fmt, args...) __android_log_print(ANDROID_LOG_INFO,  TAG, fmt, ##args)
 #define LOGD(fmt, args...) __android_log_print(ANDROID_LOG_DEBUG, TAG, fmt, ##args)
 #define LOGE(fmt, args...) __android_log_print(ANDROID_LOG_ERROR, TAG, fmt, ##args)
+#define LOGW(fmt, args...) __android_log_print(ANDROID_LOG_WARN, TAG, fmt, ##args)
 
 
 #endif//PLATFORM_LOGGING_H
