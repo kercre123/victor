09c4de7b28663560d20dc2abec4185db0f905445
Craig <crohe@anki.com>
2018-03-05 17:23:22 -0800

    take file as cmd arge (.bat) & sub out from package script.

--- robot/fixture/tools/release/update_firmware.bat
+++ robot/fixture/tools/release/update_firmware.bat
@@ -12,24 +12,31 @@ if %ERRORLEVEL% NEQ 0 (
   exit 2
 )
 
-REM manual step - copy updated firmware package to release path
-if not exist "fixture.safe" (
-  echo could not find a firmware package
+REM caller specify a firmware .safe file?
+set ARG1=%1
+if "%ARG1%" NEQ "" (
+  set FILENAME=%ARG1%
+) else (
+  set FILENAME=fixture.safe
+)
+if not exist %FILENAME% (
+  echo could not find firmware file: %FILENAME%
   exit 1
 )
+echo update firmware to: %FILENAME%
 
 REM create fixture directory, if it doesn't exist
 adb shell -x "mkdir -p data/local/fixture"
 
 REM load updated helper files
 adb push dfu data/local/fixture/
-adb push fixture.safe data/local/fixture/
+adb push %FILENAME% data/local/fixture/
 adb shell -x "cd data/local/fixture && chmod +x dfu"
 
 REM send firmware to bootloader
 echo updating firmware...
 adb shell -x "pkill helper"
-adb shell -x "cd data/local/fixture && ./dfu fixture.safe"
+adb shell -x "cd data/local/fixture && ./dfu %FILENAME%"
 
 REM echo restarting helper
 echo restarting helper
