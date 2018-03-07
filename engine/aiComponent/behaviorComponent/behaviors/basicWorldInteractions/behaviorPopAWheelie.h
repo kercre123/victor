0b945bd9b32dc8880b4a47bb22625a6b47e9ecf5
Kevin Karol <kevin.karol@anki.com>
2018-03-07 11:37:11 -0800

    Whitespace update: Fix pop a wheelie namespace indentation

--- engine/aiComponent/behaviorComponent/behaviors/basicWorldInteractions/behaviorPopAWheelie.h
+++ engine/aiComponent/behaviorComponent/behaviors/basicWorldInteractions/behaviorPopAWheelie.h
@@ -20,52 +20,52 @@
 #include <string>
 
 namespace Anki {
-  namespace Cozmo {
-    
-    class BlockWorldFilter;
-    class ObservableObject;
-    
-    class BehaviorPopAWheelie : public ICozmoBehavior
-    {
-    protected:
-      // Enforce creation through BehaviorFactory
-      friend class BehaviorFactory;
-      BehaviorPopAWheelie(const Json::Value& config);
-      
-      virtual void GetBehaviorOperationModifiers(BehaviorOperationModifiers& modifiers) const override {}
-      virtual void OnBehaviorActivated() override;
-      virtual void OnBehaviorDeactivated() override;
-      
-      virtual bool WantsToBeActivatedBehavior() const override;
+namespace Cozmo {
 
-    private:
-      
-      // TODO:(bn) a few behaviors have used this pattern now, maybe we should re-think having some kind of
-      // UpdateWhileNotRunning
-      mutable ObjectID _targetBlock;
-      ObjectID _lastBlockReactedTo;
-      
-      s32 _numPopAWheelieActionRetries = 0;
-      
-      bool _hasDisabledcliff = false;
-            
-      enum class DebugState {
-        ReactingToBlock,
-        PerformingAction
-      };
-            
-      void TransitionToReactingToBlock();
-      void TransitionToPerformingAction();
-      void TransitionToPerformingAction(bool isRetry);
-      
-      void SetupRetryAction(const ExternalInterface::RobotCompletedAction& msg);
-      
-      void ResetBehavior();
-      
-      virtual void UpdateTargetBlock() const;
-    };
-    
-  }
+class BlockWorldFilter;
+class ObservableObject;
+
+class BehaviorPopAWheelie : public ICozmoBehavior
+{
+protected:
+  // Enforce creation through BehaviorFactory
+  friend class BehaviorFactory;
+  BehaviorPopAWheelie(const Json::Value& config);
+  
+  virtual void GetBehaviorOperationModifiers(BehaviorOperationModifiers& modifiers) const override {}
+  virtual void OnBehaviorActivated() override;
+  virtual void OnBehaviorDeactivated() override;
+  
+  virtual bool WantsToBeActivatedBehavior() const override;
+
+private:
+  
+  // TODO:(bn) a few behaviors have used this pattern now, maybe we should re-think having some kind of
+  // UpdateWhileNotRunning
+  mutable ObjectID _targetBlock;
+  ObjectID _lastBlockReactedTo;
+  
+  s32 _numPopAWheelieActionRetries = 0;
+  
+  bool _hasDisabledcliff = false;
+        
+  enum class DebugState {
+    ReactingToBlock,
+    PerformingAction
+  };
+        
+  void TransitionToReactingToBlock();
+  void TransitionToPerformingAction();
+  void TransitionToPerformingAction(bool isRetry);
+  
+  void SetupRetryAction(const ExternalInterface::RobotCompletedAction& msg);
+  
+  void ResetBehavior();
+  
+  virtual void UpdateTargetBlock() const;
+};
+  
+}
 }
 
 #endif // __Cozmo_Basestation_Behaviors_BehaviorPopAWheelie_H__
