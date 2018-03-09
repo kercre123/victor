936efef5c1f352dda6c489ede0a2533aecd6d8ea
Craig <crohe@anki.com>
2018-03-09 14:00:40 -0800

    fix exclude param

--- robot/vmake.sh
+++ robot/vmake.sh
@@ -9,7 +9,7 @@ if [ "${1}" == "-b" ] || [ "${2}" == "-b" ]; then buildBoot=1; else buildBoot=0;
 if [ "${1}" == "clean" ] || [ "${2}" == "clean" ]; then echo Cleaning...; rm -rf ../$PRJ/syscon/build; fi
 
 echo Copying to anki-vm-keil:$HOSTNAME/$PRJ...
-rsync --delete -rte "ssh" --exclude /fixture/ ../$PRJ nathan@anki-vm-keil:$HOSTNAME
+rsync --delete --exclude=/fixture/ -rte "ssh" ../$PRJ nathan@anki-vm-keil:$HOSTNAME
 rsync --delete -rte "ssh" ../crypto nathan@anki-vm-keil:$HOSTNAME
 echo Building...
 ssh nathan@anki-vm-keil 'bash -lc "cd '"$HOSTNAME/$PRJ"';./convert_projx_to_uv4.sh";bash -lc "cd '"$HOSTNAME/$PRJ/syscon"';/cygdrive/c/Keil_v4/UV4/UV4.exe -j0 -b syscon.uvproj -o build.log;cat build.log;rm build.log"'
