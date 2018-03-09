1c333490865650d77b910e7d84b27ecaf965f65a
Craig <crohe@anki.com>
2018-03-09 15:05:09 -0800

    debug opts. excluded folder don't sync back.

--- robot/vmake.sh
+++ robot/vmake.sh
@@ -4,13 +4,18 @@ export PRJ=robot
 cd "${0%/*}"
 if [ ! -d ../$PRJ ]; then echo "Must run from project folder"; exit 1; fi
 
-#command line options: '-b' = build bootloader, 'clean' = clean build
-if [ "${1}" == "-b" ] || [ "${2}" == "-b" ]; then buildBoot=1; else buildBoot=0; fi
-if [ "${1}" == "clean" ] || [ "${2}" == "clean" ]; then echo Cleaning...; rm -rf ../$PRJ/syscon/build; fi
+#command line options:
+buildBoot=0
+PROGRESS=""
+for arg in "$@"; do
+  if [ "${arg}" == "-b" ]; then buildBoot=1; fi #'-b' build bootloader
+  if [ "${arg}" == "-p" ]; then PROGRESS="--progress"; fi #'-P' show rsync progress
+  if [ "${arg}" == "clean" ]; then echo Cleaning...; rm -rf ../$PRJ/syscon/build; fi #'clean' = clean build
+done
 
 echo Copying to anki-vm-keil:$HOSTNAME/$PRJ...
-rsync --delete --exclude=/fixture/ -rte "ssh" ../$PRJ nathan@anki-vm-keil:$HOSTNAME
-rsync --delete -rte "ssh" ../crypto nathan@anki-vm-keil:$HOSTNAME
+rsync $PROGRESS --delete --exclude=fixture/ -rte "ssh" ../$PRJ nathan@anki-vm-keil:$HOSTNAME
+rsync $PROGRESS --delete -rte "ssh" ../crypto nathan@anki-vm-keil:$HOSTNAME
 echo Building...
 ssh nathan@anki-vm-keil 'bash -lc "cd '"$HOSTNAME/$PRJ"';./convert_projx_to_uv4.sh";bash -lc "cd '"$HOSTNAME/$PRJ/syscon"';/cygdrive/c/Keil_v4/UV4/UV4.exe -j0 -b syscon.uvproj -o build.log;cat build.log;rm build.log"'
 if [ $buildBoot -gt 0 ]; then
@@ -18,7 +23,7 @@ if [ $buildBoot -gt 0 ]; then
   ssh nathan@anki-vm-keil 'bash -lc "cd '"$HOSTNAME/$PRJ/syscon"';/cygdrive/c/Keil_v4/UV4/UV4.exe -j0 -b sysboot.uvproj -o build.log;cat build.log;rm build.log"'
 fi
 echo Copying back...
-rsync -rte "ssh" nathan@anki-vm-keil:$HOSTNAME/$PRJ ..
+rsync $PROGRESS --exclude=fixture/ -rte "ssh" nathan@anki-vm-keil:$HOSTNAME/$PRJ ..
 ls -la syscon/build/syscon_raw.bin
 ls -la syscon/build/syscon.dfu
 if [ $buildBoot -gt 0 ]; then ls -la syscon/build/sysboot.bin; fi
