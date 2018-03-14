a14364a99f1e8f067481431265cdd8d4928ad5d6
Brian Chapados <chapados@anki.com>
2018-03-14 00:05:13 -0700

    Vic 1692 linux build support (#973)
    * Platform-specific scripts for fetching deps
    
    Linux systems do not have homebrew, so we need a different way to determine
    whether build deps are available and install them.  This change preserves the
    mac methods and adds simple checks for linux + instructions that might unblock
    building. We'll need a better solution, but this change should bootstrap the process
    of building on linux.
    
    * Get CMAKE_EXE path using cmake.py script
    
    Using cmake.py, we can install/determine the path of our baked-in
    cmake executable.  This replaces the mac-specific path that was previously used.
    
    * Search for flatc host executable in cte prebuilts
    
    * Update DEPS for flatc prebuilt support
    
    * Only use unbuffered output in dependencies.py
    
    * Explicitly name target_triple variable
    
    (cherry picked from commit 1e01d480d39d13711e9c3d3d5a504e334f5def61)

---
+++ project/victor/scripts/fetch-build-deps.linux.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+set -e
+set -u
+
+GIT=`which git`
+if [ -z $GIT ]; then
+  echo git not found
+  exit 1
+fi
+: ${TOPLEVEL:=`$GIT rev-parse --show-toplevel`}
+
+function vlog()
+{
+    echo "[fetch-build-deps] $*"
+}
+
+function check_dep()
+{
+    CHECK_CMD="$*"
+    eval $CHECK_CMD && return 0
+    
+    echo "Depdendency check failed: $CHECK_CMD"
+    echo "If you have apt-get, you should make sure you have the following deps"
+    echo ""
+    echo "sudo apt install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev \\"
+    echo "                 gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev \\"
+    echo "		   libx11-dev lib32z-dev libxml-simple-perl libc6-dev libgl1-mesa-dev tofrodos \\"
+    echo "                 python-markdown libxml2-utils xsltproc genisoimage"
+    echo ""
+    echo "sudo apt install gawk chrpath texinfo p7zip-full android-tools-fsutils"
+    echo ""
+    echo "sudo apt install libsndfile-dev ruby ninja-build subversion libssl-dev nodejs"
+    echo ""
+    exit 1
+}
+
+pushd "${TOPLEVEL}" > /dev/null 2>&1
+
+$GIT config --global url."git@github.com:".insteadOf https://github.com
+
+# Check for required programs
+check_dep which python2
+check_dep which python3
+check_dep which ninja
+check_dep pkg-config sndfile
+
+echo `pwd`
+
+vlog "Android SDK"
+# BRC: r3 is not supported on Linux. AndroidSDK is not required for build, but adb is currently required for deploy
+#./tools/build/tools/ankibuild/android.py --install-sdk r3
+
+vlog "Android NDK"
+./tools/build/tools/ankibuild/android.py --install-ndk r15b
+
+vlog "CMake"
+./tools/build/tools/ankibuild/cmake.py
+
+vlog "Go"
+./tools/build/tools/ankibuild/go.py
+
+vlog "Build output dirs"
+mkdir -p generated
+mkdir -p _build
+
+vlog "Fetch & extract external dependencies. This may take 1-5 min."
+./project/buildScripts/dependencies.py -v --deps-file DEPS --externals-dir EXTERNALS
+
+vlog "Configure audio library"
+./lib/audio/configure.py
+
+popd > /dev/null 2>&1
