b75d82759d56c73135474219da3633b417b350ef
David C Mudie <dmudie@anki.com>
2018-03-20 00:05:48 -0700

    VIC-1609: Watchdog and tombstone scripts (v3)(DVT3) (#1039) (#1071)
    1. New script vic-watchdog.sh will run as a service under systemd. 
     It will poll status of critical services (vic-robot, vic-anim, vic-engine) at regular intervals.
    If a critical service is not running, it will call vic-tombstone.sh to display an error code on the robot's face.
    
    2. New script vic-tombstone.sh will be called by vic-watchdog.sh to display error codes on the robot's face.  If the animation process is active, it will be stopped so that error codes will be visible.
    
    I put the tombstone display into a separate script so it can be used elsewhere if needed.

---
+++ project/victor/scripts/victor_enable_watchdog.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+set -eu
+
+GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
+systemctl=${GIT_PROJ_ROOT}/project/victor/scripts/systemctl.sh
+svc=vic-watchdog.service
+
+${systemctl} enable --now ${svc}
+
