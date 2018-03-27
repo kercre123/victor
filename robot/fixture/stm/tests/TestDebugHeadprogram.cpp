c7cb3bd2e7cfadfd016bcf8da2c2c7a890c78cba
Craig <crohe@anki.com>
2018-03-27 16:36:32 -0700

    linker: more space for app code

--- robot/fixture/stm/tests/TestDebugHeadprogram.cpp
+++ robot/fixture/stm/tests/TestDebugHeadprogram.cpp
@@ -41,7 +41,7 @@ void TestDebugInit(void)
 {
 }
 
-void TestDebugProcess(void)
+void TestDebugAlternateScriptTimeout(void)
 {
   static int ofs = -2;
   srand(Timer::get());
@@ -55,11 +55,31 @@ void TestDebugProcess(void)
   cmdSend(CMD_IO_HELPER, snformat(b,bz,"dutprogram %u %u", shell_timeout_s, script_delay_s), (shell_timeout_s+10)*1000, CMD_OPTS_DEFAULT | CMD_OPTS_ALLOW_STATUS_ERRS );
 }
 
+void TestDebugInspectArgs(void)
+{
+  const int shell_timeout_s = 5;
+  const uint32_t esn = 0xabcd1234;
+  char b[40]; const int bz = sizeof(b);
+  
+  snformat(b,bz,"dutprogram %u %08x", shell_timeout_s, esn);
+  cmdSend(CMD_IO_HELPER, b, (shell_timeout_s+10)*1000, CMD_OPTS_DEFAULT | CMD_OPTS_ALLOW_STATUS_ERRS );
+  
+  snformat(b,bz,"dutprogram %u %08x param1 param2 param3", shell_timeout_s, esn);
+  cmdSend(CMD_IO_HELPER, b, (shell_timeout_s+10)*1000, CMD_OPTS_DEFAULT | CMD_OPTS_ALLOW_STATUS_ERRS );
+  
+  snformat(b,bz,"dutprogram %u %08x   param1  param2  param3 ", shell_timeout_s, esn);
+  cmdSend(CMD_IO_HELPER, b, (shell_timeout_s+10)*1000, CMD_OPTS_DEFAULT | CMD_OPTS_ALLOW_STATUS_ERRS );
+}
+
 TestFunction* TestDebugGetTests(void)
 {
   static TestFunction m_tests[] = {
     TestDebugInit,
-    TestDebugProcess,
+    TestDebugInspectArgs,
+    //TestDebugInspectArgs,
+    //TestDebugInspectArgs,
+    //TestDebugAlternateScriptTimeout,
+    //TestDebugAlternateScriptTimeout,
     NULL
   };
   return m_tests;
