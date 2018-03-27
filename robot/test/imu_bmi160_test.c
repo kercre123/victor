8d436b07b90ab924681ede94eeba7645a7e66e21
jean-pierre sainfeld <jean-pierre@anki.com>
2018-03-27 12:21:06 -0700

    VIC-1648 Added test skeleton for iio imu_bmi160_test

---
+++ robot/test/imu_bmi160_test.c
@@ -0,0 +1,26 @@
+
+#define _GNU_SOURCE
+
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/dir.h>
+#include <linux/types.h>
+#include <string.h>
+#include <poll.h>
+#include <endian.h>
+#include <getopt.h>
+#include <inttypes.h>
+#include "iio_utils.h"
+
+
+int main(int argc, char **argv)
+{
+	printf("Nous somes au pied du mur\n");
+
+
+	return 0;
+}
