783b2130875f4746a3f2d648f1a738c6895c6584
Paul Terry <pterry@anki.com>
2018-03-29 13:41:45 -0700

    Pault/vic 2099 add ip override option (#1196)
    * VIC-2099 Deploy: Add option to force-copy rsync binary to robot
    
    - Add -r option to force-copy the rsync binary to robot, as apparently some
      people have had issues that this fixes
    
    * VIC-2099 Add robotIP override option
    
    - Add '-s <robotIP>' option to most of the victor scripts.  This allows one to
      override IP found in the 'robot_ip.txt' file (and ANKI_ROBOT_HOST
      environment variable).  Helpful to devs and QA working with multiple
      robots, to allow for specific per-command targeting.
    
    * VIC-2099 Add confirmation to destructive scripts
    
    - For two 'wipe' scripts, add a single-key confirmation
    
    * VIC-2099 final: update readme
    
    - Update the readme file to mention the '-s <robotIP>' option, and make things
      a little bit clearer
    
    * VIC-2099 PR feedback
    
    - Remove the interactive confirmations (2 scripts)
    - Reduce replicated script by pulling the new '-s' option handling into
      a sourced script where possible
    
    * VIC-2099 Mini: Remove '-s' option from lnav-log (temporarily)
    
    - Removing the '-s' option override from victor_log_lnav.sh, because this
      script is being changed in another PR and will need to be different.  So
      I will restore this functionality after the other PR gets merged.
    
    * VIC-2099 Add '-s' back to victor_lnav
    
    - After merging in KevinY's PR for improving victor_lnav script, I'm
      adding back the '-s <robotIP>' option because it had to be done
      slightly differently (not use shared script).

--- project/victor/scripts/systemctl.sh
+++ project/victor/scripts/systemctl.sh
@@ -5,8 +5,9 @@ set -u
 
 : ${INSTALL_ROOT:="/anki"}
 
-# Go to directory of this script                                                                    
+# Go to directory of this script
 SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))
+SCRIPT_NAME=$(basename ${0})
 GIT=`which git`
 if [ -z $GIT ]
 then
@@ -16,6 +17,8 @@ fi
 TOPLEVEL=`$GIT rev-parse --show-toplevel`
 
 source ${SCRIPT_PATH}/victor_env.sh
+
+source ${SCRIPT_PATH}/host_robot_ip_override.sh
 
 robot_set_host
 
--- project/victor/scripts/victor_log.sh
+++ project/victor/scripts/victor_log.sh
@@ -3,8 +3,9 @@
 set -e
 set -u
 
-# Get the directory of this script                                                                    
+# Get the directory of this script
 SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))
+SCRIPT_NAME=$(basename ${0})
 GIT=`which git`
 if [ -z $GIT ]
 then
@@ -14,6 +15,8 @@ fi
 TOPLEVEL=`$GIT rev-parse --show-toplevel`
 
 source ${SCRIPT_PATH}/victor_env.sh
+
+source ${SCRIPT_PATH}/host_robot_ip_override.sh
 
 robot_set_host
 
