ead1494ec6c564785083529c8fe9a9942a24e37d
Al Chaussee <al@anki.com>
2018-04-05 18:33:34 -0700

    Update build scripts to also build and deploy vicos

--- project/victor/scripts/victor_build.sh
+++ project/victor/scripts/victor_build.sh
@@ -2,3 +2,19 @@
 
 export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
 ${GIT_PROJ_ROOT}/project/victor/build-victor.sh "$@"
+
+# Build vicos
+if [ $? -eq 0 ]; then
+    # Remove any platform arguments
+    args=("$@")
+    for i in "${!args[@]}"
+    do
+	if [[ "${args[i]}" = "-p" ]]; then
+	    unset 'args[i]'
+	    unset 'args[i+1]'
+	fi
+    done
+
+    ${GIT_PROJ_ROOT}/project/victor/build-victor.sh -p vicos "${args[@]}"
+fi
+    
--- project/victor/scripts/victor_deploy.sh
+++ project/victor/scripts/victor_deploy.sh
@@ -2,3 +2,8 @@
 
 export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
 ${GIT_PROJ_ROOT}/project/victor/scripts/deploy.sh "$@"
+
+# Also deploy vicos
+if [ $? -eq 0 ]; then
+    PLATFORM_NAME=vicos ${GIT_PROJ_ROOT}/project/victor/scripts/deploy.sh "$@"
+fi
