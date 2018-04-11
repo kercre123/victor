352f5ecbb532172ac453dad96857d84dba3d3f57
Jordan Rivas <jordan@anki.com>
2018-04-11 14:21:43 -0700

    VIC-2307 Add Audio Aliases to project
    Add aliases to improve audio team’s workflow
    
    (cherry picked from commit 8d219f40870663038eb31e28261c04fb89403e6f)

---
+++ project/victor/scripts/usefulAudioAliases.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Handy Victor Audio Aliases from Jordan Rivas, inspired by Al Chaussee
+# =====================================
+# They can be called from anywhere within the git project to build, deploy, and run on physical robots.
+# Call source on this file from your .bash_profile if you always want the latest version of these aliases 
+# or copy its contents into your .bash_profile.
+#
+# NOTE: Must add VICTOR_WWISE_PROJECT_TRUNK_PATH to ~/.bash_profile above calling source on this file
+
+alias GET_GIT_ROOT='export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`'
+
+alias victor_audio_update_local_assets='GET_GIT_ROOT; ${GIT_PROJ_ROOT}/tools/audio/UseLocalAudioAssets.py $VICTOR_WWISE_PROJECT_TRUNK_PATH'
+alias victor_audio_1press_update_local_assets='GET_GIT_ROOT; victor_audio_update_local_assets -c Release -b --allow-missing'
+
+alias victor_audio_clean_local_assets='GET_GIT_ROOT; rm -rf ${GIT_PROJ_ROOT}/EXTERNALS/local-audio-assets'
+alias victor_audio_clean_project_assets='GET_GIT_ROOT; rm -rf $VICTOR_WWISE_PROJECT_TRUNK_PATH/VictorAudio/GeneratedSoundBanks'
