0f1b95fdc2b821bcf300b33acb935430427a123c
Paul Aluri <paul@aluri.org>
2018-04-12 15:59:10 -0700

    Update mac-client build script to brew install readline

--- apps/demos/ble-pairing/mac-client/build_client.sh
+++ apps/demos/ble-pairing/mac-client/build_client.sh
@@ -1,3 +1,6 @@
 output="_build"
+echo "Downloading readline library"
+brew install readline
+
 echo "Building mac-client in ./$output/mac-client"
 xcodebuild -scheme mac-client build CONFIGURATION_BUILD_DIR=./$output/
