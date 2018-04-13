dac78323b057f9014aa8ec7b47ce991582226f93
Al Chaussee <al@anki.com>
2018-04-13 11:12:14 -0700

    Fixes the LCD (#1348)
    Updates LCD register settings based on suggestions from Santek, the LCD manufacturer, and Sitronix, the driver chip manufacturer.
    
    Also follow the datasheet and wait 120ms after sending `Sleep out` and `Display on` commands

--- robot/core/src/lcd.c
+++ robot/core/src/lcd.c
@@ -34,33 +34,31 @@ typedef struct {
   uint8_t cmd;
   uint8_t data_bytes;
   uint8_t data[14];
+  uint32_t delay_ms;
 } INIT_SCRIPT;
 
-
 static const INIT_SCRIPT init_scr[] = {
-  { 0x11, 0 }, // Sleep Off
-  { 0x36, 1, { 0x00 } }, //0x00: RGB565, 0x08: BGR565
-  { 0xB0, 2, { 0x00, 0x08 } },  // Expect unswapped bytes in u16 pixel
-  { 0xB7, 1, { 0x72 } }, // Gate control (VGH 14.97, VGL -8.23)
-  { 0xBB, 1, { 0x36 } }, // VCOMS 1.45v
-  { 0xC0, 1, { 0x2C } },
-  { 0xC2, 1, { 0x01 } },
-  { 0xC3, 1, { 0x14 } }, // VRH 4.55v
-  { 0xC4, 1, { 0x20 } },
-  { 0xC6, 1, { 0x0F } },
-  { 0xD0, 2, { 0xA4, 0xA1 } }, // Power control 1
-  { 0xE0, 14, { 0xD0, 0x10, 0x16, 0x0A, 0x0A, 0x26, 0x3C, 0x53, 0x53, 0x18, 0x15, 0x12, 0x36, 0x3C } }, // +ve voltage gamma control
-  { 0xE1, 14, { 0xD0, 0x11, 0x19, 0x0A, 0x09, 0x25, 0x3D, 0x35, 0x54, 0x17, 0x15, 0x12, 0x36, 0x3C } }, // -ve voltage gamma control
-  { 0x3A, 1, { 0x55 } },
-  { 0x55, 1, { 0x03 } }, // Content Adaptive Brightness Control: 0x03 = Color Enhancement Off, Moving Image Mode
-  { 0x21, 0 },
-  { 0x2A, 4, { 0x00, RSHIFT, (LCD_FRAME_WIDTH + RSHIFT - 1) >> 8, (LCD_FRAME_WIDTH + RSHIFT - 1) & 0xFF } },
-  { 0x2B, 4, { 0x00, 0x00, (LCD_FRAME_HEIGHT -1) >> 8, (LCD_FRAME_HEIGHT -1) & 0xFF } },
-  { 0x26, 1, { 0x08 } }, // Gamma Curve Setting: 0x01=2.2, 0x02=1.8, 0x04=2.5, 0x08=1.0
-  //{ 0x53, 1, {0x24} },  // Brightness control: Brightness registers active, no dimming, backlight on
-  //{ 0x51, 1, {0x80} },  // Screen brightness value
-  { 0x29, 0 }, // Display On
-
+  { 0x11, 1, { 0x00 }, 120}, // Sleep out
+  { 0x2A, 4, { 0x00, RSHIFT, (LCD_FRAME_WIDTH + RSHIFT - 1) >> 8, (LCD_FRAME_WIDTH + RSHIFT - 1) & 0xFF } }, // Column address set
+  { 0x2B, 4, { 0x00, 0x00, (LCD_FRAME_HEIGHT -1) >> 8, (LCD_FRAME_HEIGHT -1) & 0xFF } }, // Row address set
+  { 0x36, 1, { 0x00 }, 0 }, // Memory data access control
+  { 0x3A, 1, { 0x55 }, 0 }, // Interface pixel format (16 bit/pixel 65k RGB data)
+  { 0xB0, 2, { 0x00, 0x08 } }, // RAM control (LSB first)
+  { 0xB2, 5, { 0x0C, 0x0C, 0x00, 0x33, 0x33 }, 0 }, // Porch setting
+  { 0xB7, 1, { 0x72 }, 0 }, // Gate control (VGH 14.97v, VGL -8.23v)
+  { 0xBB, 1, { 0x3B }, 0 }, // VCOMS setting (1.575v)
+  { 0xC0, 1, { 0x2C }, 0 }, // LCM control
+  { 0xC2, 1, { 0x01 }, 0 }, // VDV and VRH command enable
+  { 0xC3, 1, { 0x14 }, 0 }, // VRH set
+  { 0xC4, 1, { 0x20 }, 0 }, // VDV set
+  { 0xC6, 1, { 0x0F }, 0 }, // Frame rate control in normal mode (60hz)
+  { 0xD0, 2, { 0xA4, 0xA1 }, 0 }, // Power control 1
+  { 0xE0, 14, { 0xD0, 0x10, 0x16, 0x0A, 0x0A, 0x26, 0x3C, 0x53, 0x53, 0x18, 0x15, 0x12, 0x36, 0x3C }, 0 }, // Positive voltage gamma control
+  { 0xE1, 14, { 0xD0, 0x11, 0x19, 0x0A, 0x09, 0x25, 0x3D, 0x35, 0x54, 0x17, 0x15, 0x12, 0x36, 0x3C }, 0 }, // Negative voltage gamma control
+  { 0xE9, 3, { 0x05, 0x05, 0x01 }, 0 }, // Equalize time control
+  { 0x21, 1, { 0x00 }, 0 }, // Display inversion on
+  { 0x11, 1, { 0x00 }, 120 }, // Sleep out (again?)
+  { 0x29, 1, { 0x00 }, 120 }, // Display on
   { 0 }
 };
 
@@ -105,10 +103,11 @@ static void lcd_spi_transfer(int cmd, int bytes, const void* data) {
 
 static void lcd_device_init() {
   int idx;
-
+  
   for (idx = 0; init_scr[idx].cmd; idx++) {
     lcd_spi_transfer(TRUE, 1, &init_scr[idx].cmd);
     lcd_spi_transfer(FALSE, init_scr[idx].data_bytes, init_scr[idx].data);
+    milliwait(init_scr[idx].delay_ms);
   }
 }
 
