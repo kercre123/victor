108a23cbd9126e8e4646402081560755693ba3fb
David C Mudie <dmudie@anki.com>
2018-04-16 16:03:29 -0700

    VIC-2133: Untangle include files (#1384)
    Remove unnecessary #includes from compositeImageCache.h.
    Fixes erratic build failures that occur when animation code is compiled before engine CLAD has been generated.

--- animProcess/src/cozmoAnim/animation/compositeImageCache/compositeImageCache.h
+++ animProcess/src/cozmoAnim/animation/compositeImageCache/compositeImageCache.h
@@ -15,16 +15,13 @@
 #define __Cozmo_Basestation_AIComponent_CompositeImageCache_H__
 
 #include "clad/types/compositeImageTypes.h"
-#include "coretech/vision/engine/colorPixelTypes.h"
 #include "coretech/vision/shared/compositeImage/compositeImage.h"
-#include "engine/robotDataLoader.h"
-#include "engine/aiComponent/aiComponents_fwd.h"
 #include "util/helpers/noncopyable.h"
 
 namespace Anki {
 namespace Cozmo {
 
-  
+
 class CompositeImageCache : private Util::noncopyable
 {
 public:
@@ -33,14 +30,14 @@ public:
 
   // Pass in the template and a map of quadrant name to image name
   // If an image with the same name has been built and cached, only the quadrants
-  // different from the cached image will be re-drawn 
+  // different from the cached image will be re-drawn
   const Vision::ImageRGBA& BuildImage(const std::string& imageName,
                                       s32 imageWidth, s32 imageHeight,
                                       const Vision::CompositeImage& image);
 
   // Give behaviors a way to access the imagePathMap
-  const Util::CladEnumToStringMap<Vision::SpriteName>& GetSpriteMap(){ 
-    DEV_ASSERT(_spriteMap != nullptr, "CompositeImageCache.GetSpriteMap.Nullptr"); 
+  const Util::CladEnumToStringMap<Vision::SpriteName>& GetSpriteMap(){
+    DEV_ASSERT(_spriteMap != nullptr, "CompositeImageCache.GetSpriteMap.Nullptr");
     return *_spriteMap;
   }
 
@@ -50,7 +47,7 @@ private:
     : preAllocatedImage(imageHeight, imageWidth){
       preAllocatedImage.FillWith(Vision::PixelRGBA());
     }
-    
+
     Vision::CompositeImage compositeImage;
     Vision::ImageRGBA preAllocatedImage;
   };
