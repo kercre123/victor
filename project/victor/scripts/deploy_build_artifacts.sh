34fb3f8082c1e3e760c2eeeba6d22fa864fc4c61
Nishkar Grover <nishkar@anki.com>
2018-04-16 17:47:31 -0700

    Fix zip deployables (part 2) (#1386)
    Improving the change made in https://github.com/anki/victor/pull/1372 to qualify the path to the `Release` and `rsync` directories and for `TOPLEVEL` in `project/victor/scripts/deploy_build_artifacts.sh`

--- project/victor/scripts/deploy_build_artifacts.sh
+++ project/victor/scripts/deploy_build_artifacts.sh
@@ -5,4 +5,4 @@ set -u
 
 SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))
 
-TOPLEVEL="." STAGING_DIR="Release" RSYNC_BIN_DIR="rsync" ${SCRIPT_PATH}/deploy.sh -r -f "$@"
+TOPLEVEL="${SCRIPT_PATH}" STAGING_DIR="${SCRIPT_PATH}/Release" RSYNC_BIN_DIR="${SCRIPT_PATH}/rsync" ${SCRIPT_PATH}/deploy.sh -r -f "$@"
