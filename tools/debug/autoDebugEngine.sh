86111be02cf0a0bd3ea4af61f7bb6f1bbea71cf0
Brad Neuman <brad@anki.com>
2018-04-17 10:12:25 -0700

    Update auto-debug script to work with mac webots builds

--- tools/debug/autoDebugEngine.sh
+++ tools/debug/autoDebugEngine.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 while true; do
-    lldb $DIR/../../simulator/controllers/webotsCtrlGameEngine/webotsCtrlGameEngine -s $DIR/autoDebug.lldb
+    lldb $DIR/../../simulator/controllers/webotsCtrlGameEngine2/webotsCtrlGameEngine2 -s $DIR/autoDebug.lldb
     # sleep for a couple seconds so the user has time to hit ctrl+c
     echo "restarting lldb in 2 seconds, hit ctrl+c now to abort"
     sleep 2
