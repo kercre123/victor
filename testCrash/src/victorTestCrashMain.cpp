47fc728e87cfbe9e6b63d6f26022a036368f92f1
Paul Terry <pterry@anki.com>
2018-04-20 18:20:15 -0700

    Pault/vic 2568 test crash program (#1447)
    * VIC-2568 test crash program
    
    (This first commit is a cherry-pick from an earlier branch.)
    
    This commit adds a new program 'vic-testcrash' that runs on robot,
    takes command line arguments specifying a type of crash and
    optional delay and timeout, and does that crash
    
    - CMakeLists.txt: add vic-testcrash subdirectory
    - vic-runcrashtests.sh script to bulk-test crashes
    - Add env file
    - BUILD.in, CMakeLists.txt for vic-testcrash
    - victorTestCrashMain.cpp is the main test program
    
    * VIC-2568 test crash program
    
    (This commit is a cherry-pick from an earlier branch.)
    
    - CMake: include our existing breakpad stuff
    - test crash tool:  Integrate Google breakpad (note this is code copied
      from google_breakpad.cpp in our repo)
    - test crash tool:  Got the 'stackoverflow' crash working
    - test crash tool:  write minidump files on robot, and some cleanup
    
    * VIC-2568 test crash program
    
    - Mini cleanup in victor test crash program, including trying not
      to inline the signal-raising functions
    
    * VIC-2568 test crash program: pr feedback
    
    - Remove unneeded USE_DAS symbol definition

---
+++ testCrash/src/victorTestCrashMain.cpp
@@ -0,0 +1,359 @@
+/**
+* File: victorTestCrashMain.cpp
+*
+* Author: Paul Terry
+* Created: 04/03/18
+*
+* Description: Victor test crash tool for LE
+*
+* Copyright: Anki, inc. 2018
+*
+*/
+#include "anki/cozmo/shared/cozmoConfig.h"
+
+#include "util/fileUtils/fileUtils.h"
+#include "util/logging/logging.h"
+#include "util/logging/androidLogPrintLogger_android.h"
+
+#include <stdio.h>
+#include <chrono>
+#include <sstream>
+#include <iomanip>
+#include <thread>
+
+using namespace Anki;
+using namespace Anki::Cozmo;
+
+#define LOG_CHANNEL "VictorTestCrash"
+
+#if (defined(ANDROID) && defined(USE_GOOGLE_BREAKPAD))
+#include <client/linux/handler/exception_handler.h>
+#include <client/linux/handler/minidump_descriptor.h>
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#endif
+
+namespace GoogleBreakpad {
+
+#if (defined(ANDROID) && defined(USE_GOOGLE_BREAKPAD))
+// Google Breakpad setup
+namespace {
+
+static char dumpPath[1024] = {'\0'};
+static int fd = -1;
+static google_breakpad::ExceptionHandler* exceptionHandler;
+
+bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
+                  void* context, bool succeeded)
+{
+  LOG_INFO("GoogleBreakpad.DumpCallback",
+           "Dump path: '%s', fd = %d, context = %p, succeeded = %s",
+           dumpPath, descriptor.fd(), context, succeeded ? "true" : "false");
+  if (descriptor.fd() == fd && fd >= 0) {
+    (void) close(fd); fd = -1;
+  }
+  return succeeded;
+}
+
+} // anon namespace
+
+void InstallGoogleBreakpad(const char *path)
+{
+  (void) strlcpy(dumpPath, path, sizeof(dumpPath));
+  fd = open(path, O_WRONLY | O_CREAT | O_TRUNC | O_EXCL, 0600);
+  google_breakpad::MinidumpDescriptor descriptor(fd);
+  exceptionHandler = new google_breakpad::ExceptionHandler(descriptor, NULL, DumpCallback, NULL, true, -1);
+}
+
+void UnInstallGoogleBreakpad()
+{
+  delete exceptionHandler;
+  exceptionHandler = nullptr;
+  if (fd >= 0) {
+    (void) close(fd); fd = -1;
+  }
+  if (dumpPath[0] != '\0') {
+    struct stat dumpStat;
+    memset(&dumpStat, 0, sizeof(dumpStat));
+    int rc = stat(dumpPath, &dumpStat);
+    if (!rc && !dumpStat.st_size) {
+      (void) unlink(dumpPath);
+    }
+  }
+}
+
+#else
+
+void InstallGoogleBreakpad(const char *path) {}
+void UnInstallGoogleBreakpad() {}
+
+#endif
+
+} // namespace GoogleBreakpad
+
+namespace {
+  bool gShutdown = false;
+
+  enum
+  {
+    CT_NullPointer,
+    CT_Abort,
+    CT_StackOverflow,
+    CT_ExitFailure,
+    CT_SIGABRT,
+    CT_SIGFPE,
+    CT_SIGILL,
+    CT_SIGINT,
+    CT_SIGSEGV,
+    CT_SIGTERM,
+    NumCrashTypes
+  };
+
+  static const char* crashTypeNames[NumCrashTypes] = {
+    "null",
+    "abort",
+    "stackoverflow",
+    "exitfailure",
+    "SIGABRT",
+    "SIGFPE",
+    "SIGILL",
+    "SIGINT",
+    "SIGSEGV",
+    "SIGTERM"
+  };
+
+  static const float kDefaultCrashTime_s = 1.0f;
+  static const float kDefaultTimeout_s = 5.0f;
+}
+
+static void Shutdown(int signum)
+{
+  LOG_INFO("CozmoTestCrash.Shutdown", "Shutdown on signal %d", signum);
+  gShutdown = true;
+}
+
+static void DoNullPointer()
+{
+  volatile int* a = reinterpret_cast<volatile int*>(0);
+  *a = 42;
+}
+
+static void DoAbort()
+{
+  abort();
+}
+
+static int DoStackOverflow(int counter);
+static int StackOverflowFoo(int counter)
+{
+  static const int sizeOfArray = 268435456;
+  volatile int largeArray[sizeOfArray];
+  largeArray[counter % sizeOfArray] = counter;
+  return DoStackOverflow(counter);
+}
+static int DoStackOverflow(int counter)
+{
+  if (counter < 0) {
+    return counter;
+  }
+  static const int sizeOfArray = 268435456;
+  volatile int largeArray[sizeOfArray];
+  largeArray[counter % sizeOfArray] = counter;
+  return StackOverflowFoo(counter - 1);
+}
+
+static void DoExitFailure()
+{
+  exit(EXIT_FAILURE);
+}
+
+void DoSIGABRT() { raise(SIGABRT); } __attribute__((noinline))
+void DoSIGFPE()  { raise(SIGFPE); }  __attribute__((noinline))
+void DoSIGILL()  { raise(SIGILL); }  __attribute__((noinline))
+void DoSIGINT()  { raise(SIGINT); }  __attribute__((noinline))
+void DoSIGSEGV() { raise(SIGSEGV); } __attribute__((noinline))
+void DoSIGTERM() { raise(SIGTERM); } __attribute__((noinline))
+
+
+static void CrashMe(int crashType, int cmdLineSpecialCount)
+{
+  switch (crashType)
+  {
+    case CT_NullPointer:
+      {
+        DoNullPointer();
+      }
+      break;
+
+    case CT_Abort:
+      {
+        DoAbort();
+      }
+      break;
+
+    case CT_StackOverflow:
+      {
+        const int result = DoStackOverflow(cmdLineSpecialCount);
+        // Should not get here unless we fail to crash.  But
+        // we want something here to use 'result' so this all
+        // doesn't just get optimized away.
+        printf("stack overflow call returned %i\n", result);
+      }
+      break;
+
+    case CT_ExitFailure:
+      {
+        DoExitFailure();
+      }
+      break;
+
+    case CT_SIGABRT:  DoSIGABRT();  break;
+    case CT_SIGFPE:   DoSIGFPE();   break;
+    case CT_SIGILL:   DoSIGILL();   break;
+    case CT_SIGINT:   DoSIGINT();   break;
+    case CT_SIGSEGV:  DoSIGSEGV();  break;
+    case CT_SIGTERM:  DoSIGTERM();  break;
+  }
+}
+
+using ClockType = std::chrono::system_clock;
+
+std::string GetDateTimeString()
+{
+  const auto now = ClockType::now();
+  const auto numSecs = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch());
+  const auto millisLeft = std::chrono::duration_cast<std::chrono::milliseconds>((now - numSecs).time_since_epoch());
+  
+  const auto currTime_t = ClockType::to_time_t(now);
+  struct tm localTime; // This is local scoped to make it thread safe
+  // For some reason this is giving this error message: "__bionic_open_tzdata: couldn't find any tzdata when looking for GMT!"
+  localtime_r(&currTime_t, &localTime);
+  
+  // Use the old fashioned strftime for thread safety, instead of std::put_time
+  char formatTimeBuffer[256];
+  strftime(formatTimeBuffer, sizeof(formatTimeBuffer), "%FT%H-%M-%S-", &localTime);
+  
+  std::ostringstream stringStream;
+  stringStream << formatTimeBuffer << std::setfill('0') << std::setw(3) << millisLeft.count();
+
+  return stringStream.str();
+}
+  
+
+int main(int argc, char* argv[])
+{
+  signal(SIGTERM, Shutdown);
+
+  std::string path = "/data/data/com.anki.victor/cache/crashDumps/";
+  Util::FileUtils::CreateDirectory(path);
+
+  std::string crashFile = path + GetDateTimeString() + ".dmp";
+  GoogleBreakpad::InstallGoogleBreakpad(crashFile.c_str());
+
+  // - create and set logger
+  Util::AndroidLogPrintLogger logPrintLogger("vic-testcrash");
+  Util::gLoggerProvider = &logPrintLogger;
+
+  if (argc < 2)
+  {
+    printf("Usage:  vic-testcrash <crashType> [<secsBeforeCrash>] [<secsTimeout>]\n");
+    printf("crashType: can be one of:  ");
+    for (const auto& p : crashTypeNames)
+    {
+      printf("%s ", p);
+    }
+    printf("\nsecsBeforeCrash: float number of seconds to wait before crashing (defaults to %f)\n",
+           kDefaultCrashTime_s);
+    printf("secsTimeout: float number of seconds before program ends normally (defaults to %f)\n",
+           kDefaultTimeout_s);
+    exit(1);
+  }
+
+  int crashType = NumCrashTypes;
+  for (int i = 0; i < NumCrashTypes; i++)
+  {
+    if (!strcmp(crashTypeNames[i], argv[1]))
+    {
+      crashType = i;
+    }
+  }
+  if (crashType == NumCrashTypes)
+  {
+    printf("Unrecognized crash type \"%s\"\n", argv[1]);
+    exit(1);
+  }
+
+  float crashTime_s = kDefaultCrashTime_s;
+  if (argc > 2)
+  {
+    crashTime_s = std::stof(argv[2]);
+  }
+
+  float timeout_s = kDefaultTimeout_s;
+  if (argc > 3)
+  {
+    timeout_s = std::stof(argv[3]);
+  }
+
+  int cmdLineSpecialCount = 1 << 29;
+  if (argc > 4)
+  {
+    cmdLineSpecialCount = std::stoi(argv[4]);
+  }
+
+  using namespace std::chrono;
+  using TimeClock = steady_clock;
+
+  const auto runStart = TimeClock::now();
+
+  srand(time(NULL));
+
+  // Set the target time for the end of the first frame
+  static const uint32_t TEST_CRASH_TIME_STEP_US = 100'000;
+  auto targetEndFrameTime = runStart + (microseconds)(TEST_CRASH_TIME_STEP_US);
+  const auto crashTime_ms = static_cast<uint32_t>(crashTime_s * 1000.0f);
+  const auto crashTime = runStart + (milliseconds)(crashTime_ms);
+  const auto timeout_ms = static_cast<uint32_t>(timeout_s * 1000.0f);
+  const auto timeoutTime = runStart + (milliseconds)(timeout_ms);
+
+  while (!gShutdown)
+  {
+    // cozmoWebServer->Update();
+
+    const auto tickNow = TimeClock::now();
+    const auto remaining_us = duration_cast<microseconds>(targetEndFrameTime - tickNow);
+
+    // Now we ALWAYS sleep, but if we're overtime, we 'sleep zero' which still
+    // allows other threads to run
+    static const auto minimumSleepTime_us = microseconds((long)0);
+    std::this_thread::sleep_for(std::max(minimumSleepTime_us, remaining_us));
+
+    // Set the target end time for the next frame
+    targetEndFrameTime += (microseconds)(TEST_CRASH_TIME_STEP_US);
+
+    if (tickNow > crashTime)
+    {
+      printf("Crashing (%s)...\n", argv[1]);
+      CrashMe(crashType, cmdLineSpecialCount);
+      // We should not get here unless we fail to crash
+      break;
+    }
+
+    if (tickNow > timeoutTime)
+    {
+      printf("Timeout reached\n");
+      break;
+    }
+  }
+
+  LOG_INFO("victorTestCrashMain.exit", "exit(0)");
+  printf("Exiting victorTestCrashMain normally...no crash\n");
+  Util::gLoggerProvider = nullptr;
+
+  GoogleBreakpad::UnInstallGoogleBreakpad();
+
+  exit(0);
+}
