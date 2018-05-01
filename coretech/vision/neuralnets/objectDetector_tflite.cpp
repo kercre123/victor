29a44718c700e637661a073e32891c434454c6ab
Andrew Stein <andrew@anki.com>
2018-04-30 17:26:57 -0700

    Cleanup and go back to using opencv's resize

--- coretech/vision/neuralnets/objectDetector_tflite.cpp
+++ coretech/vision/neuralnets/objectDetector_tflite.cpp
@@ -6,7 +6,6 @@
 #include "tensorflow/contrib/lite/string_util.h"
 #include "tensorflow/contrib/lite/tools/mutable_op_resolver.h"
 #include "tensorflow/contrib/lite/interpreter.h"
-#include "tensorflow/contrib/lite/builtin_op_data.h"
 
 #include <cmath>
 #include <fstream>
@@ -238,17 +237,14 @@ void ObjectDetector::GetClassification(TimeStamp_t timestamp, std::list<Detected
 
   T maxScore = (T)(scoreScale * _params.min_score);
   int labelIndex = -1;
-  std::cout << "Scores: ";
   for(int i=0; i<_labels.size(); ++i)
   {
-    std::cout << (float)(output[i]) << " ";
     if(output[i] > maxScore)
     {
       maxScore = output[i];
       labelIndex = i;
     }
   }
-  std::cout << std::endl;
 
   if(labelIndex >= 0)
   {    
@@ -328,80 +324,14 @@ void ObjectDetector::GetDetectedObjects(TimeStamp_t timestamp, std::list<Detecte
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-template <class T>
-void resize(T* out, uint8_t* in, int image_height, int image_width,
-            int image_channels, int wanted_height, int wanted_width,
-            int wanted_channels, bool input_floating, float input_mean, float input_std) 
-{
-  int number_of_pixels = image_height * image_width * image_channels;
-  std::unique_ptr<tflite::Interpreter> interpreter(new tflite::Interpreter);
-
-  int base_index = 0;
-
-  // two inputs: input and new_sizes
-  interpreter->AddTensors(2, &base_index);
-  // one output
-  interpreter->AddTensors(1, &base_index);
-  // set input and output tensors
-  interpreter->SetInputs({0, 1});
-  interpreter->SetOutputs({2});
-
-  // set parameters of tensors
-  TfLiteQuantizationParams quant;
-  interpreter->SetTensorParametersReadWrite(
-      0, kTfLiteFloat32, "input",
-      {1, image_height, image_width, image_channels}, quant);
-  interpreter->SetTensorParametersReadWrite(1, kTfLiteInt32, "new_size", {2},
-                                            quant);
-  interpreter->SetTensorParametersReadWrite(
-      2, kTfLiteFloat32, "output",
-      {1, wanted_height, wanted_width, wanted_channels}, quant);
-
-  tflite::ops::builtin::BuiltinOpResolver resolver;
-  TfLiteRegistration* resize_op =
-      resolver.FindOp(tflite::BuiltinOperator_RESIZE_BILINEAR);
-  auto* params = reinterpret_cast<TfLiteResizeBilinearParams*>(
-      malloc(sizeof(TfLiteResizeBilinearParams)));
-  //params->align_corners = false;
-  interpreter->AddNodeWithParameters({0, 1}, {2}, nullptr, 0, params, resize_op,
-                                     nullptr);
-
-  interpreter->AllocateTensors();
-
-  // fill input image
-  // in[] are integers, cannot do memcpy() directly
-  auto input = interpreter->typed_tensor<float>(0);
-  for (int i = 0; i < number_of_pixels; i++) {
-    input[i] = in[i];
-  }
-
-  // fill new_sizes
-  interpreter->typed_tensor<int>(1)[0] = wanted_height;
-  interpreter->typed_tensor<int>(1)[1] = wanted_width;
-
-  interpreter->Invoke();
-
-  auto output = interpreter->typed_tensor<float>(2);
-  auto output_number_of_pixels =
-      wanted_height * wanted_height * wanted_channels;
-
-  for (int i = 0; i < output_number_of_pixels; i++) {
-    if (input_floating)
-      out[i] = (output[i] - input_mean) / input_std;
-    else
-      out[i] = (uint8_t)output[i];
-  }
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Result ObjectDetector::Detect(cv::Mat& img, std::list<DetectedObject>& objects)
 {
   if(_params.use_grayscale)
   {
     cv::cvtColor(img, img, CV_BGR2GRAY);
   }
-  
-  const auto kResizeMethod = CV_INTER_NN;
+
+  const auto kResizeMethod = CV_INTER_LINEAR;
   const char* typeStr = "UNKNOWN";
   const int inputIndex = _interpreter->inputs()[0];
   switch(_interpreter->tensor(inputIndex)->type)
@@ -410,38 +340,14 @@ Result ObjectDetector::Detect(cv::Mat& img, std::list<DetectedObject>& objects)
     {
       typeStr = "FLOAT";
       
-      assert(img.isContinuous());
-      resize<float>(_interpreter->typed_tensor<float>(inputIndex), img.ptr<uint8_t>(0), img.rows,
-                    img.cols, img.channels(), _params.input_height, _params.input_width,
-                    img.channels(), true, _params.input_shift, _params.input_scale);
-
-/*
       // TODO: Resize and convert directly into the interpreter's input tensor data
-      if(img.rows != _params.input_height || img.cols != _params.input_width)
-      {
-        cv::resize(img, img, cv::Size(_params.input_width,_params.input_height), 0, 0, kResizeMethod);
-      }
-      else if(_params.verbose)
-      {
-        std::cout << "Skipping actual resize: image already correct size" << std::endl;
-      }
-      assert(img.isContinuous());
+      cv::resize(img, img, cv::Size(_params.input_width,_params.input_height), 0, 0, kResizeMethod);
+      
+      cv::Mat cvTensor(_params.input_height, _params.input_width, CV_32FC(img.channels()),
+                       _interpreter->typed_tensor<float>(inputIndex));
 
-      img.convertTo(img, CV_32FC(img.channels()), 1.f/_params.input_scale, _params.input_shift);
-      memcpy(_interpreter->typed_tensor<float>(inputIndex), img.data, img.rows*img.cols*img.channels()*sizeof(float));
-*/
-      // float* tensor_data = image_tensor.tensor<float,4>().data();
-      // for(int i=0; i<_params.input_height; ++i)
-      // {
-      //   const int scaled_row = i/
-      //   const uint8_t* img_i = img.ptr<uint8_t>(i);
-
-      //   for(int j=0; j<_params.input_width*img.channels(); ++j)
-      //   {
-      //     tensor_data[j] = (float)(img_i[j]) / 255.f;
-      //     ++tensor_data;
-      //   }
-      // }
+      img.convertTo(cvTensor, CV_32FC(img.channels()), 1.f/_params.input_scale, _params.input_shift);
+      
       break;
     }
       
@@ -449,21 +355,15 @@ Result ObjectDetector::Detect(cv::Mat& img, std::list<DetectedObject>& objects)
     {
       typeStr = "UINT8";
       
-      assert(img.isContinuous());
-      resize<uint8_t>(_interpreter->typed_tensor<uint8_t>(inputIndex), img.ptr<uint8_t>(0), img.rows,
-                    img.cols, img.channels(), _params.input_height, _params.input_width,
-                    img.channels(), false, 0, 0);
-
-      /*
       // Resize input image directly into the interpreter's input tensor data
       cv::Mat cvTensor(_params.input_height, _params.input_width, CV_8UC(img.channels()),
                        _interpreter->typed_tensor<uint8_t>(inputIndex));
 
       cv::resize(img, cvTensor, cv::Size(_params.input_width,_params.input_height), 0, 0, kResizeMethod);
 
+      // NOTE: we don't scale/shift when in UINT8
       //cvTensor.convertTo(cvTensor, CV_8UC(img.channels()), 1.f/_params.input_scale, _params.input_shift);
-      */
-
+      
       break;
     }
       
