27e0cca5b337525bfb53425ebfafc10c87c15420
David C Mudie <dmudie@anki.com>
2018-05-03 08:31:44 -0700

    Tech Debt: Restore LatticePlanner unit tests (#1575)
    1. Put latticePlannerTest.cpp back into build
    
    2. AppendPointTurn() now expects starting angle as parameter.
    Tests pass if starting angle is always 0.f.

--- test/engine/latticePlannerTest.cpp
+++ test/engine/latticePlannerTest.cpp
@@ -18,7 +18,6 @@
 #define private public
 #define protected public
 
-#include "anki/common/types.h"
 #include "engine/latticePlanner.h"
 #include "engine/robot.h"
 #include "engine/cozmoContext.h"
@@ -47,7 +46,7 @@ protected:
     Util::SafeDelete(_robot);
   }
 
-  // "busy sleep" while suggesting that other threads run 
+  // "busy sleep" while suggesting that other threads run
   // for a small amount of time
   void little_sleep(std::chrono::microseconds us) {
     using namespace std::chrono;
@@ -81,7 +80,7 @@ protected:
         case EPlannerStatus::CompleteNoPlan:
           FAIL() << "planner finished with no plan! Should have plan";
           break;
-      
+
       }
 
       if(done)
@@ -95,10 +94,10 @@ protected:
     ASSERT_TRUE(done) << "planner didn't finish in " << ms << "ms. Wall time " << time << " sec";
 
     std::cout<<"planner took "<<ms<<"ms wall time " << time << " sec\n";
-  
+
     EXPECT_LT(ms, maxTimeMs) << "shouldn't use all the available time";
   }
-    
+
   Robot* _robot = nullptr;
   LatticePlanner* _planner = nullptr;
 
@@ -127,7 +126,7 @@ TEST_F(LatticePlannerTest, PlanOnceEmpty)
   ASSERT_TRUE(hasPath);
   EXPECT_EQ(selectedTargetIdx, 0) << "only one target, should have selected it";
 
-  EXPECT_GT(path.GetNumSegments(), 1) << "should be more than one action in the path"; 
+  EXPECT_GT(path.GetNumSegments(), 1) << "should be more than one action in the path";
 }
 
 TEST_F(LatticePlannerTest, PlanTwiceEmpty)
@@ -179,7 +178,7 @@ TEST_F(LatticePlannerTest, PlanWhilePlanning)
   // use planner thread for this test, but make it super slow
   _planner->SetIsSynchronous(false);
   _planner->SetArtificialPlannerDelay_ms(10000);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
   Pose3d goal1(0, Z_AXIS_3D(), Vec3f(20,1000,0) );
   Pose3d goal2(0, Z_AXIS_3D(), Vec3f(-520,400,0) );
@@ -188,7 +187,7 @@ TEST_F(LatticePlannerTest, PlanWhilePlanning)
   // NOTE: technically, if we context switch here and don't run this thread for really really long, this test
   // could break...
   EComputePathStatus ret2 = _planner->ComputePath(start, goal2);
-  
+
   EXPECT_EQ(ret1, EComputePathStatus::Running);
   EXPECT_EQ(ret2, EComputePathStatus::Error);
 
@@ -220,11 +219,11 @@ TEST_F(LatticePlannerTest, PlanWhilePlanning2)
 {
   // same test as above, but add an extra delay between the ComputePath calls to allow the worker thread to
   // run a bit
-  
+
   // use planner thread for this test, but make it super slow
   _planner->SetIsSynchronous(false);
   _planner->SetArtificialPlannerDelay_ms(10000);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
   Pose3d goal1(0, Z_AXIS_3D(), Vec3f(20,1000,0) );
   Pose3d goal2(0, Z_AXIS_3D(), Vec3f(-520,400,0) );
@@ -265,10 +264,10 @@ TEST_F(LatticePlannerTest, StopPlanning)
   // use planner thread for this test, but slow it down a bit
   _planner->SetIsSynchronous(false);
   _planner->SetArtificialPlannerDelay_ms(1000);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
   Pose3d goal(0, Z_AXIS_3D(), Vec3f(20,100,0) );
-  
+
   EComputePathStatus ret = _planner->ComputePath(start, goal);
   EXPECT_EQ(ret, EComputePathStatus::Running);
 
@@ -329,7 +328,7 @@ TEST_F(LatticePlannerTest, StopPlanning)
   _planner->SetArtificialPlannerDelay_ms(0);
   ret = _planner->ComputePath(start, goal);
   EXPECT_EQ(ret, EComputePathStatus::Running);
-    
+
   ExpectPlanCompleteInThread(1000);
 
   Planning::GoalID selectedTargetIdx = 100;
@@ -338,7 +337,7 @@ TEST_F(LatticePlannerTest, StopPlanning)
   ASSERT_TRUE(hasPath);
   EXPECT_EQ(selectedTargetIdx, 0) << "only one target, should have selected it";
 
-  EXPECT_GT(path.GetNumSegments(), 1) << "should be more than one action in the path"; 
+  EXPECT_GT(path.GetNumSegments(), 1) << "should be more than one action in the path";
 }
 
 
@@ -348,17 +347,17 @@ TEST_F(LatticePlannerTest, MotionProfileSimple1)
   motionProfile.speed_mmps = 50;
   motionProfile.accel_mmps2 = 10;
   motionProfile.decel_mmps2 = 10;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, 50, 0, 50, 10, 10);
   path.AppendLine(50, 0, 100, 0, 50, 10, 10);
   path.AppendLine(100, 0, 140, 0, 50, 10, 10);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {50, 42.42f, 28.28f, IPathPlanner::finalPathSegmentSpeed_mmps};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -374,17 +373,17 @@ TEST_F(LatticePlannerTest, MotionProfileSimple2)
   motionProfile.accel_mmps2 = 10;
   motionProfile.decel_mmps2 = 10;
   motionProfile.pointTurnSpeed_rad_per_sec = 2;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, 50, 0, 50, 10, 10);
-  path.AppendPointTurn(50, 0, DEG_TO_RAD(20), 1, 1, 1, DEG_TO_RAD(1), true);
+  path.AppendPointTurn(50, 0, 0.f, DEG_TO_RAD(20), 1, 1, 1, DEG_TO_RAD(1), true);
   path.AppendLine(50, 0, 90, 0, 50, 10, 10);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {50, 2, 50};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -399,20 +398,20 @@ TEST_F(LatticePlannerTest, MotionProfileMedium1)
   motionProfile.speed_mmps = 50;
   motionProfile.accel_mmps2 = 10;
   motionProfile.decel_mmps2 = 10;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, 50, 0, 50, 10, 10);
   path.AppendLine(50, 0, 80, 0, 50, 10, 10);
-  path.AppendPointTurn(80, 0, DEG_TO_RAD(20), 1, 1, 1, DEG_TO_RAD(1), true);
+  path.AppendPointTurn(80, 0, 0.f, DEG_TO_RAD(20), 1, 1, 1, DEG_TO_RAD(1), true);
   path.AppendLine(80, 0, 120, 0, 50, 10, 10);
-  path.AppendPointTurn(120, 0, DEG_TO_RAD(20), 1, 1, 1, DEG_TO_RAD(1), true);
+  path.AppendPointTurn(120, 0, 0.f, DEG_TO_RAD(20), 1, 1, 1, DEG_TO_RAD(1), true);
   path.AppendLine(120, 0, 140, 0, 50, 10, 10);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {24.49f, 24.49f, 2, 50, 2, 50};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -427,15 +426,15 @@ TEST_F(LatticePlannerTest, MotionProfileSimple3)
   motionProfile.speed_mmps = 50;
   motionProfile.accel_mmps2 = 10;
   motionProfile.decel_mmps2 = 10;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, 50, 0, 50, 10, 10);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {31.62f};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -452,17 +451,17 @@ TEST_F(LatticePlannerTest, MotionProfileMedium3)
   motionProfile.speed_mmps = 50;
   motionProfile.accel_mmps2 = 10;
   motionProfile.decel_mmps2 = 10;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, 50, 0, 50, 10, 10);
   path.AppendArc(-20, 50, 20, DEG_TO_RAD(180), DEG_TO_RAD(90), 1, 1, 1);
   path.AppendLine(-20, 70, -50, 70, 50, 10, 10);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {35.04f, 24.49f, IPathPlanner::finalPathSegmentSpeed_mmps};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -477,16 +476,16 @@ TEST_F(LatticePlannerTest, MotionProfileSplitLine)
   motionProfile.speed_mmps = 50;
   motionProfile.accel_mmps2 = 10;
   motionProfile.decel_mmps2 = 10;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, 50, 0, 50, 10, 10);
   path.AppendLine(50, 0, 200, 0, 50, 10, 10);
-  
+
   Pose3d start(0, Z_AXIS_3D(), Vec3f(0,0,0) );
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {50, 50, IPathPlanner::finalPathSegmentSpeed_mmps};
   f32 expectedLen[] = {50, 25, 125};
   for(int i=0; i<path2.GetNumSegments();i++)
@@ -503,16 +502,16 @@ TEST_F(LatticePlannerTest, MotionProfileMedium2)
   motionProfile.speed_mmps = 100;
   motionProfile.accel_mmps2 = 200;
   motionProfile.decel_mmps2 = 20;
-  
+
   Planning::Path path;
   path.AppendLine(-140.000000f, 340.000000f, -122.360680f, 340.000000f, 100, 200, 20);
   path.AppendArc(-122.360680f, 245.278641f, 94.721359f, 1.570796f, -0.463648f, 100, 200, 20);
   path.AppendLine(-80.000000f, 330.000000f, 262.111450f, 158.944275f, 100, 200, 20);
   path.AppendArc(300.000000f, 234.721359f, 84.721359f, -2.034444f, 0.463648f, 100, 200, 20);
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {100, 100, 100, 39.63f, IPathPlanner::finalPathSegmentSpeed_mmps};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -527,19 +526,19 @@ TEST_F(LatticePlannerTest, MotionProfileComplex1)
   motionProfile.speed_mmps = 100;
   motionProfile.accel_mmps2 = 200;
   motionProfile.decel_mmps2 = 20;
-  
+
   Planning::Path path;
-  path.AppendPointTurn(250, -80, -2.03444f, -2.5, 100, 100, DEG_TO_RAD(1), true);
+  path.AppendPointTurn(250, -80, 0.f, -2.03444f, -2.5, 100, 100, DEG_TO_RAD(1), true);
   path.AppendArc(210.000000f, -60.000000, 44.721359f, -0.463648f, -0.643501f, 100, 200, 20);
   path.AppendLine(230, -100, -210, -320, 100, 200, 20);
-  path.AppendPointTurn(-210, -320, -2.356194f, 2.5, 100, 100, DEG_TO_RAD(1), true);
+  path.AppendPointTurn(-210, -320, 0.f, -2.356194f, 2.5, 100, 100, DEG_TO_RAD(1), true);
   path.AppendLine(-210, -320, -212.928925f, -322.928925f, 100, 200, 20);
   path.AppendArc(-195.857864f, -340, 24.142136f, 2.356194f, 0.785398f, 100, 200, 20);
-  path.AppendPointTurn(-220, -340, 0, 2.5, 100, 100, DEG_TO_RAD(1), true);
-  
+  path.AppendPointTurn(-220, -340, 0.f, 0, 2.5, 100, 100, DEG_TO_RAD(1), true);
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {-2, 100, 100, 20, 2, 27.53f, 27.53f, 2};
   f32 expectedLen[] = {0, 28.77f, 241.93f, 249.99f, 0, 4.14f, 18.96f, 0};
   for(int i=0; i<path2.GetNumSegments();i++)
@@ -557,9 +556,9 @@ TEST_F(LatticePlannerTest, MotionProfileComplex2)
   motionProfile.speed_mmps = 100;
   motionProfile.accel_mmps2 = 200;
   motionProfile.decel_mmps2 = 20;
-  
+
   Planning::Path path;
-  path.AppendPointTurn(-80, -410, 0.463648f, 2, 100, 100, DEG_TO_RAD(1), true);
+  path.AppendPointTurn(-80, -410, 0.f, 0.463648f, 2, 100, 100, DEG_TO_RAD(1), true);
   path.AppendArc(-100.000000, -370.000000, 44.721359f, -1.107149f, 0.643501f, 100, 200, 20);
   path.AppendLine(-60.000000, -390.000000, 171.055725f, 72.111458f, 100, 200, 20);
   path.AppendArc(95.278641f, 110.000000, 84.721359f, -0.463648f, 0.463648f, 100, 200, 20);
@@ -567,10 +566,10 @@ TEST_F(LatticePlannerTest, MotionProfileComplex2)
   path.AppendArc(214.142136f, 245.857864f, 34.142136f, 3.141593f, -0.785398f, 100, 200, 20);
   path.AppendLine(190.000000, 270.000000, 192.928925f, 272.928925f, 100, 200, 20);
   path.AppendArc(210.000000, 255.857864f, 24.142136f, 2.356194f, -0.785398f, 100, 200, 20);
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {2, 100, 100, 94.88f, 86.20f, 44.68f, 30.39f, 27.53f, IPathPlanner::finalPathSegmentSpeed_mmps};
   f32 expectedLen[] = {0, 28.77f, 491.71f, 24.94f, 39.28f, 135.85f, 26.81f, 4.14f, 18.96f};
   for(int i=0; i<path2.GetNumSegments();i++)
@@ -587,15 +586,15 @@ TEST_F(LatticePlannerTest, MotionProfileZeroLengthSeg)
   motionProfile.speed_mmps = 100;
   motionProfile.accel_mmps2 = 200;
   motionProfile.decel_mmps2 = 500;
-  
+
   Planning::Path path;
   path.AppendLine(178.592758f, -99.807365f, 176.896408f, -80.959740f, 100, 200, 500);
   path.AppendLine(176.896408f, -80.959740f, 176.000000, -71.000000, 100, 200, 500);
-  path.AppendPointTurn(176.000000, -71.000000, 1.570796f, -2, 100, 500, DEG_TO_RAD(2.f), true);
-  
+  path.AppendPointTurn(176.000000, -71.000000, 0.f, 1.570796f, -2, 100, 500, DEG_TO_RAD(2.f), true);
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {100, 100, -2};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
@@ -610,17 +609,17 @@ TEST_F(LatticePlannerTest, MotionProfileBackwardsForwards)
   motionProfile.speed_mmps = 100;
   motionProfile.accel_mmps2 = 200;
   motionProfile.decel_mmps2 = 500;
-  
+
   Planning::Path path;
   path.AppendLine(0, 0, -10, 0, -100, 200, 500);
-  path.AppendPointTurn(-10, 0, -0.463648f, -2, 100, 500, DEG_TO_RAD(2.f), true);
+  path.AppendPointTurn(-10, 0, 0.f, -0.463648f, -2, 100, 500, DEG_TO_RAD(2.f), true);
   path.AppendLine(-10, 0, -30, 9.999996f, -100, 200, 500);
   path.AppendArc(10, 10, 40, DEG_TO_RAD(180), DEG_TO_RAD(40), 100, 200, 500);
   path.AppendLine(-20.641773f, -15.711510f, -33.541773f, -31.01151f, 100, 200, 500);
-  
+
   Planning::Path path2;
   _planner->ApplyMotionProfile(path, motionProfile, path2);
-  
+
   f32 expectedSpeeds[] = {-100, -2, -100, 100, 100, 20};
   for(int i=0; i<path2.GetNumSegments();i++)
   {
