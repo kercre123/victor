6a9df4e71879dd1d58ecd27015d59afeae05954a
Adam Shelly <ashelly@anki.com>
2018-05-31 20:12:51 -0700

    dfu makefile updates and menuman debugging

--- robot/fixture/helpware/display.c
+++ robot/fixture/helpware/display.c
@@ -362,7 +362,7 @@ void display_render(uint8_t layermask) {
 
 void display_draw_text(int layer, int line , uint16_t fg, uint16_t bg, const char* text, int len, bool centered)
 {
-  if( layer == DISPLAY_LAYER_LARGE && len > gFont[layer].CharsPerLine ) {
+  if( layer == DISPLAY_LAYER_LARGE && len > gFont[layer]->CharsPerLine ) {
     layer = DISPLAY_LAYER_LARGE_SKINNY;
   }
   const Font* font =  gFont[layer];
