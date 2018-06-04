e69c40c672a205fcd346428c2599f224d37a2b58
Brad Neuman <bradneuman@gmail.com>
2018-06-04 11:02:13 -0700

    Add a few misc stim events (#1988)
    - Added stim events for obstacle reaction, timer ringing, and unmatched/unclaimed/no voice intent

--- engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToVoiceCommand.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToVoiceCommand.cpp
@@ -602,10 +602,12 @@ void BehaviorReactToVoiceCommand::TransitionToIntentReceived()
     case EIntentStatus::IntentUnknown:
       PRINT_CH_DEBUG( "MicData", "BehaviorReactToVoiceCommand.Intent", "Heard user intent but could not understand it" );
       intentReaction = AnimationTrigger::VC_IntentNeutral;
+      GetBEI().GetMoodManager().TriggerEmotionEvent( "NoValidVoiceIntent" );
       break;
     case EIntentStatus::NoIntentHeard:
       PRINT_CH_DEBUG( "MicData", "BehaviorReactToVoiceCommand.Intent", "No user intent was heard" );
       intentReaction = AnimationTrigger::VC_IntentNeutral;
+      GetBEI().GetMoodManager().TriggerEmotionEvent( "NoValidVoiceIntent" );
       break;
   }
 
