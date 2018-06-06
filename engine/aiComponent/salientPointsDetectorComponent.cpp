5446702fb1f56d077022b206b685038296e41c71
Lorenzo Riano <lorenzo.riano@gmail.com>
2018-06-06 14:18:01 -0700

    Handling external messages

--- engine/aiComponent/salientPointsDetectorComponent.cpp
+++ engine/aiComponent/salientPointsDetectorComponent.cpp
@@ -1,42 +1,32 @@
 /**
- * File: PersonDetectorComponent
+ * File: salientPointDetectorComponent.cpp
  *
- * Author: Raul
- * Created: 03/25/16
+ * Author: Lorenzo Riano
+ * Created: 06/04/18
  *
- * Description: Whiteboard for behaviors to share information that is only relevant to them.
+ * Description: A component for salient points. Listens to messages and categorize them based on their type
  *
  * Copyright: Anki, Inc. 2016
  *
  **/
-#include "engine/aiComponent/salientPointsDetectorComponent.h"
 
-#include "coretech/common/engine/math/poseOriginList.h"
+#include "clad/externalInterface/messageEngineToGame.h"
+#include "coretech/common/engine/utils/timer.h"
 #include "engine/actions/animActions.h"
 #include "engine/actions/basicActions.h"
 #include "engine/activeObject.h"
 #include "engine/aiComponent/aiComponent.h"
 #include "engine/aiComponent/objectInteractionInfoCache.h"
+#include "engine/aiComponent/salientPointsDetectorComponent.h"
 #include "engine/ankiEventUtil.h"
 #include "engine/blockWorld/blockWorld.h"
 #include "engine/components/carryingComponent.h"
 #include "engine/components/dockingComponent.h"
 #include "engine/cozmoContext.h"
 #include "engine/drivingAnimationHandler.h"
-#include "engine/externalInterface/externalInterface.h"
-#include "engine/faceWorld.h"
 #include "engine/robot.h"
-
-#include "coretech/common/engine/math/point_impl.h"
-#include "coretech/common/engine/math/rotation.h"
-#include "coretech/common/engine/utils/timer.h"
-
-#include "clad/externalInterface/messageEngineToGame.h"
-
 #include "util/console/consoleInterface.h"
-#include "util/logging/logging.h"
 
-#define DEBUG_AI_WHITEBOARD_POSSIBLE_OBJECTS 0
 
 namespace Anki {
 namespace Cozmo {
@@ -65,17 +55,28 @@ void SalientPointsDetectorComponent::InitDependent(Cozmo::Robot* robot, const AI
   if ( _robot.HasExternalInterface() )
   {
     using namespace ExternalInterface;
-//    auto helper = MakeAnkiEventUtil(*_robot.GetExternalInterface(), *this, _signalHandles);
-    // TODO Subscribe to the messages we need
-//    helper.SubscribeEngineToGame<MessageEngineToGameTag::RobotObservedObject>();
-//    helper.SubscribeEngineToGame<MessageEngineToGameTag::RobotObservedPossibleObject>();
-//    helper.SubscribeEngineToGame<MessageEngineToGameTag::RobotOffTreadsStateChanged>();
+    auto helper = MakeAnkiEventUtil(*_robot.GetExternalInterface(), *this, _signalHandles);
+    helper.SubscribeEngineToGame<MessageEngineToGameTag::RobotObservedSalientPoint>();
+    // TODO what happens when helper goes out of scope here?
   }
   else {
-    PRINT_NAMED_WARNING("SalientPointsDetectorComponent.Init", "Initialized whiteboard with no external interface. Will miss events.");
+    PRINT_NAMED_WARNING("SalientPointsDetectorComponent.Init", "Initialized component with no external interface. "
+        "Will miss events.");
   }
 }
 
+template<>
+void SalientPointsDetectorComponent::HandleMessage(const ExternalInterface::RobotObservedSalientPoint& msg)
+{
+
+    PRINT_CH_INFO("Behaviors", "SalientPointsDetectorComponent.HandleMessage.NewMessageReceived", "");
+    const auto& salientPoint = msg.salientPoint;
+
+    if (salientPoint.salientType == Vision::SalientPointType::Person ) {
+      _lastPersonDetected = salientPoint;
+    }
+
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void SalientPointsDetectorComponent::UpdateDependent(const AICompMap& dependentComps)
@@ -88,37 +89,34 @@ bool SalientPointsDetectorComponent::PersonDetected() const
   // TODO Here I'm cheating: test if x seconds have passed and if so make the condition true
   float currentTickCount = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
   if ((currentTickCount - _timeSinceLastObservation) > 3 ) { // 3 seconds
+
+    _lastPersonDetected.timestamp = _robot.GetLastImageTimeStamp();
+
+    // Random image coordinates
+    _lastPersonDetected.y_img = float(_robot.GetContext()->GetRandom()->RandDbl());
+    _lastPersonDetected.x_img = float(_robot.GetContext()->GetRandom()->RandDbl());
+
     PRINT_CH_INFO("Behaviors", "SalientPointsDetectorComponent.PersonDetected.ConditionTrue", "");
     _timeSinceLastObservation = currentTickCount;
     return true;
   }
   else {
-    PRINT_CH_INFO("Behaviors", "SalientPointsDetectorComponent.PersonDetected.ConditionFalse", "");
+    PRINT_CH_DEBUG("Behaviors", "SalientPointsDetectorComponent.PersonDetected.ConditionFalse", "");
     return false;
   }
+
+  // TODO Here just check if a person has been detected, e.g. check that _lastPersonDetected is not empty
 }
 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-template<>
-void SalientPointsDetectorComponent::HandleMessage(const ExternalInterface::RobotObservedObject& msg)
+void SalientPointsDetectorComponent::GetLastPersonDetectedData(Vision::SalientPoint& salientPoint) const
 {
+  salientPoint = _lastPersonDetected;
+
+  // reset the person data, it's been "consumed"
+  _lastPersonDetected = Vision::SalientPoint();
 
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-template<>
-void SalientPointsDetectorComponent::HandleMessage(const ExternalInterface::RobotObservedPossibleObject& msg)
-{
-
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-template<>
-void SalientPointsDetectorComponent::HandleMessage(const ExternalInterface::RobotOffTreadsStateChanged& msg)
-{
-
-}
 
 
 } // namespace Cozmo
--- engine/aiComponent/salientPointsDetectorComponent.h
+++ engine/aiComponent/salientPointsDetectorComponent.h
@@ -1,54 +1,46 @@
 /**
- * File: AIWhiteboard
+ * File: salientPointDetectorComponent.h
  *
- * Author: Raul
- * Created: 03/25/16
+ * Author: Lorenzo Riano
+ * Created: 06/04/18
  *
- * Description: Whiteboard for behaviors to share information that is only relevant to them.
+ * Description: A component for salient points. Listens to messages and categorize them based on their type
  *
  * Copyright: Anki, Inc. 2016
  *
  **/
+
 #ifndef __Cozmo_Basestation_BehaviorSystem_ObjectDetectorComponent_H__
 #define __Cozmo_Basestation_BehaviorSystem_ObjectDetectorComponent_H__
 
-#include "engine/aiComponent/aiBeacon.h"
-
-#include "engine/aiComponent/aiComponents_fwd.h"
-#include "engine/externalInterface/externalInterface_fwd.h"
-
+#include "clad/types/behaviorComponent/postBehaviorSuggestions.h"
+#include "clad/types/objectFamilies.h"
+#include "clad/types/objectTypes.h"
+#include "clad/types/salientPointTypes.h"
 #include "coretech/common/engine/math/pose.h"
 #include "coretech/common/engine/objectIDs.h"
 #include "coretech/vision/engine/faceIdTypes.h"
-
-#include "clad/types/objectFamilies.h"
-#include "clad/types/objectTypes.h"
-#include "clad/types/behaviorComponent/postBehaviorSuggestions.h"
-
+#include "engine/aiComponent/aiBeacon.h"
+#include "engine/aiComponent/aiComponents_fwd.h"
+#include "engine/aiComponent/beiConditions/iBEIConditionEventHandler.h"
+#include "engine/externalInterface/externalInterface_fwd.h"
 #include "util/entityComponent/iDependencyManagedComponent.h"
 #include "util/helpers/noncopyable.h"
 #include "util/signals/simpleSignal_fwd.h"
 
-#include <list>
-#include <queue>
-#include <set>
-#include <vector>
-#include <unordered_map>
 
 namespace Anki {
 namespace Cozmo {
 
 // Forward declarations
-class BlockWorldFilter;  
-class ObservableObject;
 class Robot;
-class SmartFaceID;
+
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // SalientPointsDetectorComponent
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 class SalientPointsDetectorComponent : public IDependencyManagedComponent<AIComponentID>,
-                                private Util::noncopyable
+                                       private Util::noncopyable
 {
 public:
 
@@ -57,7 +49,7 @@ public:
   // IDependencyManagedComponent<AIComponentID> functions
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   void InitDependent(Cozmo::Robot* robot,
-                             const AICompMap& dependentComps) override;
+                     const AICompMap& dependentComps) override;
 
   
   // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -74,11 +66,8 @@ public:
   // end IDependencyManagedComponent<AIComponentID> functions
 
   bool PersonDetected() const;
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  // Events
-  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-  
-  // template for all events we subscribe to
+  void GetLastPersonDetectedData(Vision::SalientPoint& salientPoint) const;
+
   template<typename T>
   void HandleMessage(const T& msg);
 
@@ -92,6 +81,7 @@ private:
   Robot& _robot;
   // signal handles for events we register to. These are currently unsubscribed when destroyed
   std::vector<Signal::SmartHandle> _signalHandles;
+  mutable Vision::SalientPoint _lastPersonDetected; // TODO made mutable here for testing
 
   mutable float _timeSinceLastObservation = 0; // TODO this is temporary for testing
 
