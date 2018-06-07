fba33899d1c23b40e35b1997538812707e480a11
Richard Gale <richard@richardgale.org>
2018-06-07 13:07:01 -0700

    Fixes Xcode project name, back to victor instead of Okoa (#2011)
    Fixes flatbuffer license type

--- cmake/flatbuffers.cmake
+++ cmake/flatbuffers.cmake
@@ -17,5 +17,5 @@ foreach(LIB ${FLATBUFFERS_LIBS})
     "${FLATBUFFERS_LIB_PATH}/lib${LIB}.a"
     INTERFACE_INCLUDE_DIRECTORIES
     "${FLATBUFFERS_INCLUDE_PATH}")
-  anki_build_target_license(${LIB} "ANKI")
+  anki_build_target_license(${LIB} "Apache-2.0")
 endforeach()
