638a0a59f16ab5bdefd9d3aad38489dbde35cc6a
Stuart Eichert <seichert@anki.com>
2018-06-13 17:45:02 -0700

    VIC-1394 Make victor_build_* aliases call scripts
    For the Victor OS build of victor, we want to build with the same
    build flags that developers are using when they build for shipping,
    release, etc.  Unfortunately, aliases cannot be invoked in bitbake
    shell scripts (or any shell scripts for that matter).  To fix this, I
    have created new victor_build_*.sh build scripts that contain the
    build flags.  The victor_build_* aliases now just invoke the scripts.
    
    (cherry picked from commit a49d966ff1da5b6407d75ec24a0de102d580fc71)
    Signed-off-by: Stuart Eichert <seichert@anki.com>

---
+++ project/victor/scripts/victor_build_debug.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
+${GIT_PROJ_ROOT}/project/victor/build-victor.sh \
+		-c Debug
---
+++ project/victor/scripts/victor_build_xcode.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
+${GIT_PROJ_ROOT}/project/victor/build-victor.sh \
+		-c Debug \
+		-p mac \
+		-g Xcode \
+		-C
