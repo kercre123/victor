c49828c25843c9213545fe098b50e923ffb9ce6c
Nathan Monson <nathan@anki.com>
2018-06-16 16:31:43 -0700

    Add BLE TX support, fix FCC bugs
    Added btconfig tool from apps_proc/vendor/qcom/opensource/bluetooth/btconfig
    ankibluetoothd and fccify.bat are not used, so I removed them to prevent confusion
    Fix a broken path in Makefile
    Reorder menus to fit BLE
    Add channel 13 support
    Make sure ankibluetoothd is stopped to prevent false readings

--- robot/fixture/helpware/files/fccify.bat
+++
@@ -1,6 +0,0 @@
-adb wait-for-device
-adb shell "mount -o rw,remount / ; rm /anki/menuman"
-adb shell "mv /usr/bin/ankibluetoothd /usr/bin/ankiblues ; systemctl disable vic-engine ; systemctl disable vic-anim ; systemctl disable vic-robot ; systemctl disable anki-robot.target ; systemctl disable vic-watchdog"
-adb push ankiinit /etc/initscripts
-adb push square-05.wav sine-05.wav testsound.sh burn menuman system.sh menuman.sh burn.sh /anki
-adb shell "chmod +x /anki/* ; chmod +x /etc/initscripts/ankiinit ; sync ; reboot"
--- robot/fixture/helpware/files/system.sh
+++ robot/fixture/helpware/files/system.sh
@@ -1,19 +1,39 @@
 #!/bin/sh
-# Try to stop everything - if it fails, restart the module
+# Work around menuman crash by kicking off this work in the background and returning instantly
+if [ "$3" == "" ]; then
+  /anki/menuman/system.sh $1 $2 async &
+  exit 0
+fi
+
+# Try to stop everything - if it fails, restart the wlan module
+systemctl stop ankibluetoothd.service
 if ! iwpriv wlan0 tx 0; then
   rmmod wlan.ko
   insmod /usr/lib/modules/3.18.66-perf/extra/wlan.ko con_mode=5
   iwpriv wlan0 ftm 1
   iwpriv wlan0 ena_chain 2
 fi
-iwpriv wlan0 tx_cw_rf_gen 0
+iwpriv wlan0 tx_cw_rf_gen 0             # Carrier off
+/anki/menuman/btconfig --soc smd /dev/smd3 rawcmd 0x08 0x001F   # BLE off
 
 if [ $1 == STOP ]; then
   echo Stopped
   exit 0
 fi
+if [ $1 == BLE ]; then
+  echo BLE pseudo-random TX on BLE channel $2 - hex 0x0..0x27
+  rmmod wlan # Must stop WLAN before BLE test can take over
+  /anki/menuman/btconfig --soc smd /dev/smd3 rawcmd 0x08 0x001E $2 0x23 0x00  # See KBA-170629211501 - 0x23=60% duty cycle
+  exit 0
+fi
+if [ $1 == RX ]; then
+  echo BLE RX on BLE channel $2 - hex 0x0..0x27
+  rmmod wlan # Must stop WLAN before BLE test can take over
+  /anki/menuman/btconfig --soc smd /dev/smd3 rawcmd 0x08 0x001D $2 0x23 0x00  # See KBA-170629211501
+  exit 0
+fi
 
-echo Selecting channel $2
+echo Selecting WiFi channel $2
 iwpriv wlan0 set_channel $2              #select Wifi channel 1-11
 iwpriv wlan0 pwr_cntl_mode 1             #select SCPC control mode
 iwpriv wlan0 set_txpower 16              #SCPC power setting
