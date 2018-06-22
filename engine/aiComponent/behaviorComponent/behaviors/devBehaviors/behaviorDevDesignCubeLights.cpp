7b00e9b3898a14444b406ca7fc5b4ed385ce60cf
Kevin Karol <kevinmkarol@gmail.com>
2018-06-22 12:17:20 -0700

    Fix DevDesignCubeLights in Shipping (#2259)
    - Put variable usage behind ANKI_DEV_CHEATS flags

--- engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorDevDesignCubeLights.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorDevDesignCubeLights.cpp
@@ -173,114 +173,116 @@ void BehaviorDevDesignCubeLights::BehaviorUpdate()
     return;
   }
 
-  auto newLights = BuildLightsFromConsoleVars();
-  std::list<CubeLightAnimation::LightPattern> anim;
-  CubeLightAnimation::LightPattern pattern;
-  pattern.lights = newLights;
-  anim.push_back(pattern);
+  #if ANKI_DEV_CHEATS
+    auto newLights = BuildLightsFromConsoleVars();
+    std::list<CubeLightAnimation::LightPattern> anim;
+    CubeLightAnimation::LightPattern pattern;
+    pattern.lights = newLights;
+    anim.push_back(pattern);
 
-  if(ANKI_DEV_CHEATS && s_shouldSaveNextTick){
-    struct tm time;
-    WallTime::getInstance()->GetLocalTime(time);
-    const std::string filename = "DevLights_" + std::to_string(time.tm_hour) + ":" + std::to_string(time.tm_min) + ":" + std::to_string(time.tm_sec);
-    GetBEI().GetCubeLightComponent().SaveLightsToDisk(filename, anim);
-    s_shouldSaveNextTick = false;
-  }
+    if(s_shouldSaveNextTick){
+      struct tm time;
+      WallTime::getInstance()->GetLocalTime(time);
+      const std::string filename = "DevLights_" + std::to_string(time.tm_hour) + ":" + std::to_string(time.tm_min) + ":" + std::to_string(time.tm_sec);
+      GetBEI().GetCubeLightComponent().SaveLightsToDisk(filename, anim);
+      s_shouldSaveNextTick = false;
+    }
 
-  BlockWorldFilter filter;
-  filter.AddAllowedFamily(ObjectFamily::LightCube);
-  const ActiveObject* obj = GetBEI().GetBlockWorld().FindConnectedActiveMatchingObject(filter);
+    BlockWorldFilter filter;
+    filter.AddAllowedFamily(ObjectFamily::LightCube);
+    const ActiveObject* obj = GetBEI().GetBlockWorld().FindConnectedActiveMatchingObject(filter);
 
-  if((newLights != _dVars.currentLights) &&
-     (obj != nullptr)){
-    GetBEI().GetCubeLightComponent().StopLightAnimAndResumePrevious(_dVars.handle);
-    
+    if((newLights != _dVars.currentLights) &&
+      (obj != nullptr)){
+      GetBEI().GetCubeLightComponent().StopLightAnimAndResumePrevious(_dVars.handle);
+      
 
-    
-    GetBEI().GetCubeLightComponent().PlayLightAnim(obj->GetID(), anim, {}, "DevDesignCubeLights", _dVars.handle);
-    _dVars.currentLights = newLights;
-  }
-
+      
+      GetBEI().GetCubeLightComponent().PlayLightAnim(obj->GetID(), anim, {}, "DevDesignCubeLights", _dVars.handle);
+      _dVars.currentLights = newLights;
+    }
+  #endif
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 CubeLightAnimation::ObjectLights BehaviorDevDesignCubeLights::BuildLightsFromConsoleVars() const
 {
   CubeLightAnimation::ObjectLights lights;
+  #if ANKI_DEV_CHEATS
+    // Setup state 1 for LEDs
+    {
+      ColorRGBA led1(kLED1_s1_red, kLED1_s1_green, kLED1_s1_blue, kLED1_s1_alpha);
+      lights.onColors[0] = led1.AsRGBA();
 
-  // Setup state 1 for LEDs
-  {
-    ColorRGBA led1(kLED1_s1_red, kLED1_s1_green, kLED1_s1_blue, kLED1_s1_alpha);
-    lights.onColors[0] = led1.AsRGBA();
+      ColorRGBA led2(kLED2_s1_red, kLED2_s1_green, kLED2_s1_blue, kLED2_s1_alpha);
+      lights.onColors[1] = led2.AsRGBA();
 
-    ColorRGBA led2(kLED2_s1_red, kLED2_s1_green, kLED2_s1_blue, kLED2_s1_alpha);
-    lights.onColors[1] = led2.AsRGBA();
+      ColorRGBA led3(kLED3_s1_red, kLED3_s1_green, kLED3_s1_blue, kLED3_s1_alpha);
+      lights.onColors[2] = led3.AsRGBA();
 
-    ColorRGBA led3(kLED3_s1_red, kLED3_s1_green, kLED3_s1_blue, kLED3_s1_alpha);
-    lights.onColors[2] = led3.AsRGBA();
+      ColorRGBA led4(kLED4_s1_red, kLED4_s1_green, kLED4_s1_blue, kLED4_s1_alpha);
+      lights.onColors[3] = led4.AsRGBA();
+    }
 
-    ColorRGBA led4(kLED4_s1_red, kLED4_s1_green, kLED4_s1_blue, kLED4_s1_alpha);
-    lights.onColors[3] = led4.AsRGBA();
-  }
+    // Setup state 2 for LEDs
+    {
+      ColorRGBA led1(kLED1_s2_red, kLED1_s2_green, kLED1_s2_blue, kLED1_s2_alpha);
+      lights.offColors[0] = led1.AsRGBA();
 
-  // Setup state 2 for LEDs
-  {
-    ColorRGBA led1(kLED1_s2_red, kLED1_s2_green, kLED1_s2_blue, kLED1_s2_alpha);
-    lights.offColors[0] = led1.AsRGBA();
+      ColorRGBA led2(kLED2_s2_red,kLED2_s2_green,  kLED2_s2_blue, kLED2_s2_alpha);
+      lights.offColors[1] = led2.AsRGBA();
 
-    ColorRGBA led2(kLED2_s2_red,kLED2_s2_green,  kLED2_s2_blue, kLED2_s2_alpha);
-    lights.offColors[1] = led2.AsRGBA();
+      ColorRGBA led3(kLED3_s2_red, kLED3_s2_green, kLED3_s2_blue, kLED3_s2_alpha);
+      lights.offColors[2] = led3.AsRGBA();
 
-    ColorRGBA led3(kLED3_s2_red, kLED3_s2_green, kLED3_s2_blue, kLED3_s2_alpha);
-    lights.offColors[2] = led3.AsRGBA();
-
-    ColorRGBA led4(kLED4_s2_red, kLED4_s2_green, kLED4_s2_blue, kLED4_s2_alpha);
-    lights.offColors[3] = led4.AsRGBA();
-  }
-
-
-  // Setup state 1 hold
-  {
-    lights.onPeriod_ms[0] = kLED1_s1_hold_ms;
-    lights.onPeriod_ms[1] = kLED2_s1_hold_ms;
-    lights.onPeriod_ms[2] = kLED3_s1_hold_ms;
-    lights.onPeriod_ms[3] = kLED4_s1_hold_ms;
-  }
-
-  // Setup state 2 hold
-  {
-    lights.offPeriod_ms[0] = kLED1_s2_hold_ms;
-    lights.offPeriod_ms[1] = kLED2_s2_hold_ms;
-    lights.offPeriod_ms[2] = kLED3_s2_hold_ms;
-    lights.offPeriod_ms[3] = kLED4_s2_hold_ms;
-  }
+      ColorRGBA led4(kLED4_s2_red, kLED4_s2_green, kLED4_s2_blue, kLED4_s2_alpha);
+      lights.offColors[3] = led4.AsRGBA();
+    }
 
 
-  // Setup state 1 -> state 2 transition
-  {
-    lights.transitionOffPeriod_ms[0] = kLED1_s1_transition_s2_ms;
-    lights.transitionOffPeriod_ms[1] = kLED2_s1_transition_s2_ms;
-    lights.transitionOffPeriod_ms[2] = kLED3_s1_transition_s2_ms;
-    lights.transitionOffPeriod_ms[3] = kLED4_s1_transition_s2_ms;
-  }
+    // Setup state 1 hold
+    {
+      lights.onPeriod_ms[0] = kLED1_s1_hold_ms;
+      lights.onPeriod_ms[1] = kLED2_s1_hold_ms;
+      lights.onPeriod_ms[2] = kLED3_s1_hold_ms;
+      lights.onPeriod_ms[3] = kLED4_s1_hold_ms;
+    }
 
-  // Setup state 2 -> state 1 transition
-  {
-    lights.transitionOnPeriod_ms[0] = kLED1_s2_transition_s1_ms;
-    lights.transitionOnPeriod_ms[1] = kLED2_s2_transition_s1_ms;
-    lights.transitionOnPeriod_ms[2] = kLED3_s2_transition_s1_ms;
-    lights.transitionOnPeriod_ms[3] = kLED4_s2_transition_s1_ms;
-  }
+    // Setup state 2 hold
+    {
+      lights.offPeriod_ms[0] = kLED1_s2_hold_ms;
+      lights.offPeriod_ms[1] = kLED2_s2_hold_ms;
+      lights.offPeriod_ms[2] = kLED3_s2_hold_ms;
+      lights.offPeriod_ms[3] = kLED4_s2_hold_ms;
+    }
 
-  // Offsets for start
-  {
-    lights.offset[0] = kLED1_s1_hold_offset_ms;
-    lights.offset[1] = kLED2_s1_hold_offset_ms;
-    lights.offset[2] = kLED3_s1_hold_offset_ms;
-    lights.offset[3] = kLED4_s1_hold_offset_ms;
-  }
 
-  lights.rotate = kRotate;
+    // Setup state 1 -> state 2 transition
+    {
+      lights.transitionOffPeriod_ms[0] = kLED1_s1_transition_s2_ms;
+      lights.transitionOffPeriod_ms[1] = kLED2_s1_transition_s2_ms;
+      lights.transitionOffPeriod_ms[2] = kLED3_s1_transition_s2_ms;
+      lights.transitionOffPeriod_ms[3] = kLED4_s1_transition_s2_ms;
+    }
+
+    // Setup state 2 -> state 1 transition
+    {
+      lights.transitionOnPeriod_ms[0] = kLED1_s2_transition_s1_ms;
+      lights.transitionOnPeriod_ms[1] = kLED2_s2_transition_s1_ms;
+      lights.transitionOnPeriod_ms[2] = kLED3_s2_transition_s1_ms;
+      lights.transitionOnPeriod_ms[3] = kLED4_s2_transition_s1_ms;
+    }
+
+    // Offsets for start
+    {
+      lights.offset[0] = kLED1_s1_hold_offset_ms;
+      lights.offset[1] = kLED2_s1_hold_offset_ms;
+      lights.offset[2] = kLED3_s1_hold_offset_ms;
+      lights.offset[3] = kLED4_s1_hold_offset_ms;
+    }
+
+    lights.rotate = kRotate;
+  #endif
 
 
   return lights;
