49fe6ef235df6355fd1c79cfb10a9e586abe9af3
Andrew Stein <andrew@anki.com>
2018-06-29 12:22:19 -0700

    VIC-1887: Try/Catch for cv::boxFilter (#2378)
    Add try/catch around call to cv::boxFilter so we don't just die in case of cv::Exception and instead print an error (as with other opencv calls)

--- coretech/vision/engine/image.cpp
+++ coretech/vision/engine/image.cpp
@@ -478,7 +478,14 @@ namespace Vision {
   template<typename T>
   void ImageBase<T>::BoxFilter(ImageBase<T>& filtered, u32 size) const
   {
-    cv::boxFilter(this->get_CvMat_(), filtered.get_CvMat_(), -1, cv::Size(size, size));
+    try {
+      cv::boxFilter(this->get_CvMat_(), filtered.get_CvMat_(), -1, cv::Size(size, size));
+    }
+    catch (cv::Exception& e) {
+      PRINT_NAMED_ERROR("ImageBase.BoxFilter.OpenCvBoxFilterFailed",
+                        "%s", e.what());
+      this->CopyTo(filtered); // Just put the unfiltered source into the destination
+    }
   }
   
   template<typename T>
