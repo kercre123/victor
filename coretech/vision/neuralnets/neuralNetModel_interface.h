9ab8ffb87d0cb9028569b8a078ba3268bc94ad12
Andrew Stein <andrew@anki.com>
2018-07-06 16:30:44 -0700

    Adding TFLite support

---
+++ coretech/vision/neuralnets/neuralNetModel_interface.h
@@ -0,0 +1,84 @@
+/**
+ * File: neuralNetModel_interface.h
+ *
+ * Author: Andrew Stein
+ * Date:   7/2/2018
+ *
+ * Description: Defines interface and shared helpers for various NeuralNetModel implementations.
+ *
+ * Copyright: Anki, Inc. 2018
+ **/
+
+#ifndef __Anki_Vision_NeuralNetModel_Interface_H__
+#define __Anki_Vision_NeuralNetModel_Interface_H__
+
+#include "coretech/common/shared/types.h"
+#include "coretech/vision/neuralnets/neuralNetParams.h"
+
+#include "clad/types/salientPointTypes.h"
+#include "json/json.h"
+
+#include "opencv2/core/core.hpp"
+
+#include <list>
+#include <vector>
+
+namespace Anki {
+namespace Vision {
+    
+class INeuralNetModel
+{
+public:
+  
+  ~INeuralNetModel()
+  {
+    
+  }
+  
+  bool IsVerbose() const { return _params.verbose; }
+  
+  // Subclasses are expected to overload the LoadModel and Detect methods below.
+  // Note that we are not using virtual abstract methods here because we have no need for polymorphism.
+  // There will only ever be one "type" of NeuralNetModel present compiled into the system.
+  // There is no implementation of these methods in the .cpp file for this class as they should never
+  // be referenced, only overridden by subclasses.
+  
+  // Load the model/labels files specified in the config and set up assocated parameters
+  Result LoadModel(const std::string& modelPath, const Json::Value& config);
+  
+  // Run forward inference on the given image/timestamp and return any SalientPoints found
+  Result Detect(cv::Mat& img, const TimeStamp_t t, std::list<Vision::SalientPoint>& salientPoints);
+  
+protected:
+  
+  // Base model not meant to be directly instantiated
+  INeuralNetModel() {}
+  
+  // Helper to read simple text labels files (one label per line)
+  static Result ReadLabelsFile(const std::string& fileName, std::vector<std::string>& labels_out);
+  
+  // Helper to find the index of the a single output with the highest score, assumed to
+  // correspond to the matching label from the labels file
+  // Implemented for float and uint8 types
+  template<typename T>
+  void GetClassification(const T* outputData, TimeStamp_t timestamp, std::list<Vision::SalientPoint>& salientPoints);
+  
+  // Helper to return a set of localization boxes from a grid, assuming a binary classifcation
+  // (e.g. person / no-person in a 6x6 grid). Grid size is specified in JSON config
+  template<typename T>
+  void GetLocalizedBinaryClassification(const T* outputData, TimeStamp_t timestamp,
+                                        std::list<Vision::SalientPoint>& salientPoints);
+
+  NeuralNetParams                           _params;
+  std::vector<std::string>                  _labels;
+  
+  // For OutputType::BinaryLocalization
+  cv::Mat_<uint8_t>                         _detectionGrid;
+  cv::Mat_<int32_t>                         _labelsGrid;
+  
+};
+
+} // namespace Vision
+} // namespace Anki
+
+#endif /* __Anki_Vision_NeuralNetModel_Interface_H__ */
---
+++ coretech/vision/neuralnets/neuralNetModel_tflite.cpp
@@ -0,0 +1,225 @@
+/**
+ * File: neuralNetModel_tflite.cpp
+ *
+ * Author: Andrew Stein
+ * Date:   12/5/2017
+ *
+ * Description: Implementation of ObjectDetector Model class which wraps TensorFlow Lite.
+ *
+ * Copyright: Anki, Inc. 2017
+ **/
+
+// NOTE: this wrapper completely compiles out if we're using a different model (e.g. TensorFlow)
+#ifdef VIC_NEURALNETS_USE_TFLITE
+
+#include "coretech/vision/neuralnets/neuralNetModel_tflite.h"
+#include <list>
+#include <queue>
+
+#include "util/fileUtils/fileUtils.h"
+#include "util/logging/logging.h"
+
+#include "opencv2/imgcodecs/imgcodecs.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+#include "tensorflow/contrib/lite/kernels/register.h"
+#include "tensorflow/contrib/lite/model.h"
+#include "tensorflow/contrib/lite/string_util.h"
+
+namespace Anki {
+namespace Vision {
+
+#define LOG_CHANNEL "NeuralNets"
+  
+// TODO: Make this a parameter in config?
+constexpr int kNumThreads = 1;
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  
+// A TFLite error reporter that uses our error logging system
+struct TFLiteLogReporter : public tflite::ErrorReporter {
+  int Report(const char* format, va_list args) override
+  {
+    ::Anki::Util::sErrorV("NeuralNetModel.TFLiteErrorReporter", {}, format, args);
+    return 0;
+  }
+};
+
+static TFLiteLogReporter sLogReporter;
+  
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+NeuralNetModel::NeuralNetModel()
+{ 
+
+}
+  
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+NeuralNetModel::~NeuralNetModel()
+{
+  LOG_INFO("NeuralNetModel.Destructor", "");
+}
+  
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Result NeuralNetModel::LoadModel(const std::string& modelPath, const Json::Value& config)
+{
+  const Result paramsResult = _params.SetFromConfig(config);
+  if(RESULT_OK != paramsResult)
+  {
+    return paramsResult;
+  }
+   
+  DEV_ASSERT(!modelPath.empty(), "NeuralNetModel.LoadModel.EmptyModelPath");
+  
+  std::vector<int> sizes = {1, _params.inputHeight, _params.inputWidth, 3};
+  
+  const std::string graphFileName = Util::FileUtils::FullFilePath({modelPath,_params.graphFile});
+  
+  if(!Util::FileUtils::FileExists(graphFileName))
+  {
+    PRINT_NAMED_ERROR("NeuralNetModel.LoadModel.GraphFileDoesNotExist", "%s", graphFileName.c_str());
+    return RESULT_FAIL;
+  }
+  
+  _model = tflite::FlatBufferModel::BuildFromFile(graphFileName.c_str(), &sLogReporter);
+  
+  if (!_model)
+  {
+    PRINT_NAMED_ERROR("NeuralNetModel.LoadModel.FailedToBuildFromFile", "%s", graphFileName.c_str());
+    return RESULT_FAIL;
+  }
+  
+  LOG_INFO("NeuralNetModel.LoadModel.Success", "Loaded: %s",
+           graphFileName.c_str());
+  
+  //_model->error_reporter();
+  //LOG_INFO("NeuralNetModel.LoadModel.ResolvedReporter", "");
+  
+#ifdef TFLITE_CUSTOM_OPS_HEADER
+  tflite::MutableOpResolver resolver;
+  RegisterSelectedOps(&resolver);
+#else
+  tflite::ops::builtin::BuiltinOpResolver resolver;
+#endif
+  
+  tflite::InterpreterBuilder(*_model, resolver)(&_interpreter);
+  if (!_interpreter)
+  {
+    PRINT_NAMED_ERROR("NeuralNetModel.LoadModel.FailedToConstructInterpreter", "");
+    return RESULT_FAIL;
+  }
+  
+  if (kNumThreads != -1)
+  {
+    _interpreter->SetNumThreads(kNumThreads);
+  }
+  
+  const int input = _interpreter->inputs()[0];
+  _interpreter->ResizeInputTensor(input, sizes);
+  
+  if (_interpreter->AllocateTensors() != kTfLiteOk)
+  {
+    PRINT_NAMED_ERROR("NeuralNetModel.LoadModel.FailedToAllocateTensors", "");
+    return RESULT_FAIL;
+  }
+
+  // Read the label list
+  const std::string labelsFileName = Util::FileUtils::FullFilePath({modelPath, _params.labelsFile});
+  const Result readLabelsResult = ReadLabelsFile(labelsFileName, _labels);
+  if(RESULT_OK != readLabelsResult)
+  {
+    return readLabelsResult;
+  }
+  
+  return RESULT_OK;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void NeuralNetModel::ScaleImage(cv::Mat& img)
+{
+  DEV_ASSERT(_interpreter != nullptr, "NeuralNetModel.ScaleImage.NullInterpreter");
+
+  const int inputIndex = _interpreter->inputs()[0];
+  
+  const auto kResizeMethod = CV_INTER_LINEAR;
+  
+  if(_params.useFloatInput)
+  {
+    float* scaledInputData = _interpreter->typed_tensor<float>(inputIndex);
+    
+    // Resize uint8 image data, and *then* convert smaller image to float below
+    // TODO: Resize and convert directly into the scaledInputData (ideally using NEON?)
+    if(img.rows != _params.inputHeight || img.cols != _params.inputWidth)
+    {
+      cv::resize(img, img, cv::Size(_params.inputWidth,_params.inputHeight), 0, 0, kResizeMethod);
+    }
+    else if(_params.verbose)
+    {
+      LOG_INFO("NeuralNetModel.Detect.SkipResize", "Skipping actual resize: image already correct size");
+    }
+    DEV_ASSERT(img.isContinuous(), "NeuralNetModel.Detect.ImageNotContinuous");
+    
+    // Scale/shift resized image directly into the tensor data
+    const auto cvType = (img.channels() == 1 ? CV_32FC1 : CV_32FC3);
+    
+    cv::Mat cvTensor(_params.inputHeight, _params.inputWidth, cvType, scaledInputData);
+    
+    img.convertTo(cvTensor, cvType, 1.f/_params.inputScale, _params.inputShift);
+  }
+  else
+  {
+    uint8_t* scaledInputData = _interpreter->typed_tensor<uint8_t>(inputIndex);
+    
+    // Resize uint8 input image directly into the uint8 tensor data
+    cv::Mat cvTensor(_params.inputHeight, _params.inputWidth,
+                     (img.channels() == 1 ? CV_8UC1 : CV_8UC3),
+                     scaledInputData);
+    
+    cv::resize(img, cvTensor, cv::Size(_params.inputWidth, _params.inputHeight), 0, 0, kResizeMethod);
+  }
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Result NeuralNetModel::Detect(cv::Mat& img, const TimeStamp_t t, std::list<Vision::SalientPoint>& salientPoints)
+{
+  // Scale image, subtract mean, divide by standard deviation and store in the interpreter's input tensor
+  ScaleImage(img);
+  
+  const auto invokeResult = _interpreter->Invoke();
+  if (kTfLiteOk != invokeResult)
+  {
+    PRINT_NAMED_ERROR("NeuralNetModel.Run.FailedToInvoke", "");
+    return RESULT_FAIL;
+  }
+  
+  switch(_params.outputType)
+  {
+    case NeuralNetParams::OutputType::Classification:
+    {
+      const float* outputData = _interpreter->typed_output_tensor<float>(0);
+      GetClassification(outputData, t, salientPoints);
+      break;
+    }
+      
+    case NeuralNetParams::OutputType::BinaryLocalization:
+    {
+      const float* outputData = _interpreter->typed_output_tensor<float>(0);
+      GetLocalizedBinaryClassification(outputData, t, salientPoints);
+      break;
+    }
+      
+    case NeuralNetParams::OutputType::AnchorBoxes:
+      //GetDetectedObjects(outputTensors, t, salientPoints);
+    case NeuralNetParams::OutputType::Segmentation:
+    {
+      PRINT_NAMED_ERROR("NeuralNetModel.Detect.OutputTypeNotSupported", "TFLite model needs more output types implemented");
+      return RESULT_FAIL;
+    }
+  }
+  
+  return RESULT_OK;
+}
+  
+} // namespace Vision
+} // namespace Anki
+
+#endif // #if VIC_NEURALNETS_USE_TFLITE
