6bea700a8f6cc12e89baf4ab7167bf4a6438b07d
Andrew Stein <andrew@anki.com>
2018-07-06 16:37:06 -0700

    Commit mod'd files, not just additions

--- coretech/vision/neuralnets/neuralNetModel_tensorflow.cpp
+++ coretech/vision/neuralnets/neuralNetModel_tensorflow.cpp
@@ -9,6 +9,9 @@
  * Copyright: Anki, Inc. 2018
  **/
 
+// NOTE: this wrapper completely compiles out if we're using a different model (e.g. TFLite)
+#ifdef VIC_NEURALNETS_USE_TENSORFLOW
+
 #include "coretech/common/shared/types.h"
 #include "coretech/common/engine/math/polygon_impl.h"
 #include "coretech/common/engine/math/rect_impl.h"
@@ -36,51 +39,19 @@
 #include "util/helpers/quoteMacro.h"
 #include "util/logging/logging.h"
 
+#include "opencv2/imgcodecs/imgcodecs.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
 #include <cmath>
 #include <fstream>
 
 namespace Anki {
+namespace Vision {
 
 #define LOG_CHANNEL "NeuralNets"
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-// TODO: Could use JsonTools:: instead of most of these...
-
-static inline void SetFromConfigHelper(const Json::Value& json, int32_t& value) {
-  value = json.asInt();
-}
-
-static inline void SetFromConfigHelper(const Json::Value& json, float& value) {
-  value = json.asFloat();
-}
-
-static inline void SetFromConfigHelper(const Json::Value& json, bool& value) {
-  value = json.asBool();
-}
-
-static inline void SetFromConfigHelper(const Json::Value& json, std::string& value) {
-  value = json.asString();
-}
-
-static inline void SetFromConfigHelper(const Json::Value& json, std::vector<std::string>& values)
-{
-  if(json.isArray()) {
-    for(const auto& value : json) {
-      values.push_back(value.asString());
-    }
-  } 
-  else {
-    values.push_back(json.asString());
-  }
-}
-
-// static inline void SetFromConfigHelper(const Json::Value& json, uint8_t& value) {
-//   value = json.asUInt();
-// }
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 NeuralNetModel::NeuralNetModel()
-: _params{} 
 {
 
 }
@@ -103,7 +74,7 @@ NeuralNetModel::~NeuralNetModel()
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 Result NeuralNetModel::LoadModel(const std::string& modelPath, const Json::Value& config)
 {
-  const Result result = SetParamsFromConfig(config);
+  const Result result = _params.SetFromConfig(config);
   if(RESULT_OK != result) 
   {
     PRINT_NAMED_ERROR("NeuralNetModel.LoadModel.SetParamsFromConfigFailed", "");
@@ -249,362 +220,6 @@ Result NeuralNetModel::LoadModel(const std::string& modelPath, const Json::Value
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Result NeuralNetModel::SetParamsFromConfig(const Json::Value& config)
-{
-# define GetFromConfig(keyName) \
-  if(!config.isMember(QUOTE(keyName))) \
-  { \
-    PRINT_NAMED_ERROR("NeuralNetModel.SetParamsFromConfig.MissingConfig", QUOTE(keyName)); \
-    return RESULT_FAIL; \
-  } \
-  else \
-  { \
-    SetFromConfigHelper(config[QUOTE(keyName)], _params.keyName); \
-  }
-  
-  GetFromConfig(verbose);
-  GetFromConfig(labelsFile);
-  GetFromConfig(minScore);  
-  GetFromConfig(graphFile);
-  GetFromConfig(inputHeight);
-  GetFromConfig(inputWidth);
-  GetFromConfig(architecture);
-  GetFromConfig(memoryMapGraph);
-  GetFromConfig(benchmarkRuns);
-
-  if("ssd_mobilenet" == _params.architecture)
-  {
-    _params.inputLayerName = "image_tensor";
-    _params.outputLayerNames = {"detection_scores", "detection_classes", "detection_boxes", "num_detections"};
-    _params.useFloatInput = false;
-    _params.outputType = OutputType::AnchorBoxes;
-
-    if(config.isMember("outputType")) {
-      PRINT_NAMED_WARNING("NeuralNetModel.SetParamsFromConfig.IgnoringOutputType",
-                          "Ignoring outputType and using 'AnchorBoxes' because architecture='ssd_mobilenet' was specified");
-    }
-  }
-  else if(("mobilenet" == _params.architecture) || ("mobilenet_v1" == _params.architecture))
-  { 
-    _params.inputLayerName = "input";
-    _params.outputLayerNames = {"MobilenetV1/Predictions/Softmax"};
-    _params.useFloatInput = true;
-    _params.outputType = OutputType::Classification;
-
-    if(config.isMember("outputType")) {
-      PRINT_NAMED_WARNING("NeuralNetModel.SetParamsFromConfig.IgnoringOutputType",
-                          "Ignoring outputType and using 'Classification' because architecture='mobilenet' was specified");
-    }
-  }
-  else if("custom" == _params.architecture)
-  {
-    GetFromConfig(inputLayerName);
-    GetFromConfig(outputLayerNames);
-    GetFromConfig(useFloatInput);
-    
-    const Result result = SetOutputTypeFromConfig(config);
-    if(RESULT_OK != result) {
-      // SetOutputTypeFromConfig will print an error, just return
-      // up the chain
-      return result;
-    }
-    
-    if(config.isMember("useGrayscale"))
-    {
-      SetFromConfigHelper(config["useGrayscale"], _params.useGrayscale);
-    }
-  }
-  else
-  {
-    PRINT_NAMED_ERROR("NeuralNetModel.SetParamsFromConfig.UnrecognizedArchitecture", "%s",
-                      _params.architecture.c_str());
-    return RESULT_FAIL;
-  }
-
-  if(_params.verbose)
-  {
-    std::string outputNames;
-    for(auto const& outputLayerName : _params.outputLayerNames)
-    {
-      outputNames += outputLayerName + " ";
-    }    
-
-    LOG_INFO("NeuralNetModel.SetParamsFromConfig.Summary", "Arch: %s, %s Input: %s, Outputs: %s",
-             _params.architecture.c_str(), (_params.useGrayscale ? "Grayscale" : "Color"),
-             _params.inputLayerName.c_str(), outputNames.c_str());
-  }
-
-  if(_params.useFloatInput)
-  {
-    // NOTE: Only used when processing in floating point
-    GetFromConfig(inputShift);
-    GetFromConfig(inputScale);
-  }
-
-  return RESULT_OK;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Result NeuralNetModel::SetOutputTypeFromConfig(const Json::Value& config)
-{
-  // Convert outputType to enum and validate number of outputs
-  if(!config.isMember("outputType")) {
-    PRINT_NAMED_ERROR("NeuralNetModel.SetOutputTypeFromConfig.MissingOutputType",
-                      "Custom architecture requires outputType to be specified");
-    return RESULT_FAIL;
-  }
-
-  std::string outputTypeStr;
-  SetFromConfigHelper(config["outputType"], outputTypeStr);
-
-  struct OutputTypeEntry {
-    OutputType type;
-    int        numOutputs;
-  };
-  const std::map<std::string, OutputTypeEntry> kOutputTypeMap{
-    {"classification",        {OutputType::Classification,     1} }, 
-    {"binary_localization",   {OutputType::BinaryLocalization, 1} },
-    {"anchor_boxes",          {OutputType::AnchorBoxes,        4} },
-    {"segmentation",          {OutputType::Segmentation,       1} },
-  };
-
-  auto iter = kOutputTypeMap.find(outputTypeStr);
-  if(iter == kOutputTypeMap.end()) {
-    std::string validKeys;
-    for(auto const& entry : kOutputTypeMap) {
-      validKeys += entry.first;
-      validKeys += " ";
-    }
-    PRINT_NAMED_ERROR("NeuralNetModel.SetOutputTypeFromConfig.BadOutputType", "Valid types: %s", validKeys.c_str());
-    return RESULT_FAIL;
-  }
-  else {
-
-    if(_params.outputLayerNames.size() != iter->second.numOutputs)
-    {
-      PRINT_NAMED_ERROR("NeuralNetModel.SetOutputTypeFromConfig.WrongNumberOfOutputs",
-                        "OutputType %s requires %d outputs (%d provided)", 
-                        iter->first.c_str(), iter->second.numOutputs, (int)_params.outputLayerNames.size());
-      return RESULT_FAIL;
-    }
-    _params.outputType = iter->second.type;
-
-    if(OutputType::BinaryLocalization == _params.outputType)
-    {
-      GetFromConfig(numGridRows);
-      GetFromConfig(numGridCols);
-    }
-  }
-
-  return RESULT_OK;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-Result NeuralNetModel::ReadLabelsFile(const std::string& fileName, std::vector<std::string>& labels_out)
-{
-  std::ifstream file(fileName);
-  if (!file)
-  {
-    PRINT_NAMED_ERROR("NeuralNetModel.ReadLabelsFile.LabelsFileNotFound", "%s", fileName.c_str());
-    return RESULT_FAIL;
-  }
-  
-  labels_out.clear();
-  std::string line;
-  while (std::getline(file, line)) {
-    labels_out.push_back(line);
-  }
-  
-  LOG_INFO("NeuralNetModel.ReadLabelsFile.Success", "Read %d labels", (int)labels_out.size());
-
-  return RESULT_OK;
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void NeuralNetModel::GetClassification(const tensorflow::Tensor& outputTensor, TimeStamp_t timestamp,
-                                       std::list<Vision::SalientPoint>& salientPoints)
-{
-  const float* outputData = outputTensor.tensor<float, 2>().data();
-        
-  float maxScore = _params.minScore;
-  int labelIndex = -1;
-  for(int i=0; i<_labels.size(); ++i)
-  {
-    if(outputData[i] > maxScore)
-    {
-      maxScore = outputData[i];
-      labelIndex = i;
-    }
-  }
-  
-  const Rectangle<int32_t> imgRect(0.f,0.f,1.f,1.f);
-  const Poly2i imgPoly(imgRect);
-  
-  if(labelIndex >= 0)
-  {    
-    Vision::SalientPoint salientPoint(timestamp, 0.5f, 0.5f, maxScore, 1.f,
-                                      Vision::SalientPointType::Object,
-                                      (labelIndex < _labels.size() ? _labels.at((size_t)labelIndex) : "<UNKNOWN>"),
-                                      imgPoly.ToCladPoint2dVector());
-    
-    if(_params.verbose)
-    {
-      LOG_INFO("NeuralNetModel.GetClassification.ObjectFound", "Name: %s, Score: %f", salientPoint.description.c_str(), salientPoint.score);
-    }
-
-    salientPoints.push_back(std::move(salientPoint));
-  }
-  else if(_params.verbose)
-  {
-    LOG_INFO("NeuralNetModel.GetClassification.NoObjects", "MinScore: %f", _params.minScore);
-  }
-}
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-void NeuralNetModel::GetLocalizedBinaryClassification(const tensorflow::Tensor& outputTensor, TimeStamp_t timestamp,
-                                                      std::list<Vision::SalientPoint>& salientPoints)
-{
-  // Create a detection box for each grid cell that is above threshold
-
-  // This raw (Eigen) tensor data appears to be _column_ major (i.e. "not row major"). Ensure that remains true.
-  DEV_ASSERT( !(outputTensor.tensor<float, 2>().Options & Eigen::RowMajor), 
-             "NeuralNetModel.GetLocalizedBinaryClassification.OutputNotRowMajor");
-
-  const float* outputData = outputTensor.tensor<float, 2>().data();
-
-  _detectionGrid.create(_params.numGridRows, _params.numGridCols);
-  
-  bool anyDetections = false;
-  for(int i=0; i<_detectionGrid.rows; ++i)
-  {
-    uint8_t* detectionGrid_i = _detectionGrid.ptr(i);
-    for(int j=0; j<_detectionGrid.cols; ++j)
-    {
-      // Compute the column-major index to get data from the output tensor
-      const int outputIndex = j*_params.numGridRows + i;
-      const float score = outputData[outputIndex];
-      
-      // Create binary detection image
-      if(score > _params.minScore) {
-        anyDetections = true;
-        detectionGrid_i[j] = static_cast<uint8_t>(255.f * score);
-      }
-      else {
-        detectionGrid_i[j] = 0;
-      }
-      
-    }
-  }
-  
-  if(anyDetections)
-  {
-    // Someday if we ever link vic-neuralnets against coretech vision, we could use our own connected
-    // components API, but for now, rely directly on OpenCV. Because we want to get average score, 
-    // we'll do our own "stats" computation below instead of using connectedComponentsWithStats()
-    const s32 count = cv::connectedComponents(_detectionGrid, _labelsGrid);
-    DEV_ASSERT((_detectionGrid.rows == _labelsGrid.rows) && (_detectionGrid.cols == _labelsGrid.cols),
-               "NeuralNetModel.GetLocalizedBinaryClassification.MismatchedLabelsGridSize");
-    
-    if(_params.verbose)
-    {
-      LOG_INFO("NeuralNetModel.GetLocalizedBinaryClassification.FoundConnectedComponents",
-               "NumComponents: %d", count);
-    }
-    
-    // Compute stats for each connected component
-    struct Stat {
-      int32_t area; // NOTE: area will be number of grid squares, not area of bounding box shape
-      int32_t scoreSum;
-      Point2f centroid;
-      int xmin, xmax, ymin, ymax;
-    };
-    std::vector<Stat> stats(count, {0,0,{0.f,0.f},_detectionGrid.cols, -1, _detectionGrid.rows, -1});
-    
-    for(int i=0; i<_detectionGrid.rows; ++i)
-    {
-      const uint8_t* detectionGrid_i = _detectionGrid.ptr<uint8_t>(i);
-      const int32_t* labelsGrid_i    = _labelsGrid.ptr<int32_t>(i);
-      for(int j=0; j<_detectionGrid.cols; ++j)
-      {
-        const int32_t label = labelsGrid_i[j];
-        if(label > 0) // zero is background (not part of any connected component)
-        {
-          DEV_ASSERT(label < count, "NeuralNetModel.GetLocalizedBinaryClassification.BadLabel");
-          const int32_t score = detectionGrid_i[j];
-          Stat& stat = stats[label];
-          stat.scoreSum += score;
-          stat.area++;
-          
-          stat.centroid.x() += j;
-          stat.centroid.y() += i;
-          
-          stat.xmin = std::min(stat.xmin, j);
-          stat.xmax = std::max(stat.xmax, j);
-          stat.ymin = std::min(stat.ymin, i);
-          stat.ymax = std::max(stat.ymax, i);
-        }
-      }
-    }
-    
-    // Create a SalientPoint to return for each connected component (skipping background component 0)
-    const float widthScale  = 1.f / static_cast<float>(_detectionGrid.cols);
-    const float heightScale = 1.f / static_cast<float>(_detectionGrid.rows);
-    for(s32 iComp=1; iComp < count; ++iComp)
-    {
-      Stat& stat = stats[iComp];
-      
-      const float area = static_cast<float>(stat.area);
-      const float avgScore = (1.f/255.f) * ((float)stat.scoreSum / area);
-      
-      // Centroid currently contains a sum. Divide by area to get actual centroid.
-      stat.centroid *= 1.f/area;
-      
-      // Convert centroid to normalized coordinates
-      stat.centroid.x() = Util::Clamp(stat.centroid.x()*widthScale,  0.f, 1.f);
-      stat.centroid.y() = Util::Clamp(stat.centroid.y()*heightScale, 0.f, 1.f);
-      
-      // Use the single label (this is supposed to be a binary classifier after all) and
-      // convert it to a SalientPointType
-      DEV_ASSERT(_labels.size()==1, "ObjectDetector.GetLocalizedBinaryClassification.NotBinary");
-      Vision::SalientPointType type = Vision::SalientPointType::Unknown;
-      const bool success = SalientPointTypeFromString(_labels[0], type);
-      DEV_ASSERT(success, "ObjectDetector.GetLocalizedBinaryClassification.NoSalientPointTypeForLabel");
-
-      // Use the bounding box as the shape (in normalized coordinates)
-      // TODO: Create a more precise polygon that traces the shape of the connected component (cv::findContours?)
-      const float xmin = (static_cast<float>(stat.xmin)-0.5f) * widthScale;
-      const float ymin = (static_cast<float>(stat.ymin)-0.5f) * heightScale;
-      const float xmax = (static_cast<float>(stat.xmax)+0.5f) * widthScale;
-      const float ymax = (static_cast<float>(stat.ymax)+0.5f) * heightScale;
-      const Poly2f shape( Rectangle<float>(xmin, ymin, xmax-xmin, ymax-ymin) );
-      
-      Vision::SalientPoint salientPoint(timestamp,
-                                        stat.centroid.x(),
-                                        stat.centroid.y(),
-                                        avgScore,
-                                        area * (widthScale*heightScale), // convert to area fraction
-                                        type,
-                                        EnumToString(type),
-                                        shape.ToCladPoint2dVector());
-      
-      if(_params.verbose)
-      {
-        LOG_INFO("NeuralNetModel.GetLocalizedBinaryClassification.SalientPoint",
-                 "%d: %s score:%.2f area:%.2f [%s %s %s %s]",
-                 iComp, stat.centroid.ToString().c_str(), avgScore, area,
-                 shape[0].ToString().c_str(),
-                 shape[1].ToString().c_str(),
-                 shape[2].ToString().c_str(),
-                 shape[3].ToString().c_str());
-      }
-      
-      salientPoints.push_back(std::move(salientPoint));
-    }
-  }
-
-} 
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void NeuralNetModel::GetDetectedObjects(const std::vector<tensorflow::Tensor>& outputRensors, TimeStamp_t timestamp,
                                         std::list<Vision::SalientPoint>& salientPoints)
 {
@@ -745,22 +360,29 @@ Result NeuralNetModel::Detect(cv::Mat& img, const TimeStamp_t t, std::list<Visio
 
   switch(_params.outputType)
   {
-    case OutputType::Classification:
+    case NeuralNetParams::OutputType::Classification:
     {
-      GetClassification(outputTensors[0], t, salientPoints);
+      const float* outputData = outputTensor.tensor<float, 2>().data();
+      GetClassification(outputData, t, salientPoints);
       break;
     }
-    case OutputType::BinaryLocalization:
+    case NeuralNetParams::OutputType::BinaryLocalization:
     {
-      GetLocalizedBinaryClassification(outputTensors[0], t, salientPoints);
+      // This raw (Eigen) tensor data appears to be _column_ major (i.e. "not row major"). Ensure that remains true.
+      DEV_ASSERT( !(outputTensor.tensor<float, 2>().Options & Eigen::RowMajor),
+                 "NeuralNetModel.Detect.OutputNotRowMajor");
+      
+      const float* outputData = outputTensor.tensor<float, 2>().data();
+      
+      GetLocalizedBinaryClassification(outputData, t, salientPoints);
       break;
     }
-    case OutputType::AnchorBoxes:
+    case NeuralNetParams::OutputType::AnchorBoxes:
     {
       GetDetectedObjects(outputTensors, t, salientPoints);  
       break;
     }
-    case OutputType::Segmentation:
+    case NeuralNetParams::OutputType::Segmentation:
     {
       // TODO (robert) need to convert the segmenation response map
       // into salient points
@@ -844,4 +466,7 @@ Result NeuralNetModel::Run(tensorflow::Tensor imageTensor, std::vector<tensorflo
   }
 }
 
+} // namespace Vision
 } // namespace Anki
+
+#endif /* VIC_NEURALNETS_USE_TENSORFLOW */
