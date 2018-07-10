3107b062c6c4dfb7ecc859e304616b3b9f226251
Arjun Menon <ajn.menon@gmail.com>
2018-07-10 10:20:52 -0700

    VIC-4025 VIC-4026 habitat webviz (#2489)
    * display current habitat state in web viz
    * add button controls for habitat belief

--- engine/components/habitatDetectorComponent.h
+++ engine/components/habitatDetectorComponent.h
@@ -86,6 +86,10 @@ public:
   HabitatBeliefState GetHabitatBeliefState() const { return _habitatBelief; }
   
   bool IsProxObservingHabitatWall() const;
+  
+protected:
+  
+  void SendDataToWebViz() const;
 
 private:
   
@@ -108,6 +112,8 @@ private:
   
   // whether we've seen any white cliff data since being putdown
   bool _detectedWhiteFromCliffs = false;
+  
+  f32 _nextSendWebVizDataTime_sec = 0.0f;
 };
 
 } // end Cozmo
