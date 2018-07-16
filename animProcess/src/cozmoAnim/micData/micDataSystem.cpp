5cd70bed3016d2d90c10474344212fea9b7350fb
Matt Michini <mmichini@anki.com>
2018-07-16 14:56:01 -0700

    Get rid of "speaker latency" log spam (#2586)
    - Do not attempt to retrieve speaker latency value from AKAlsaSink plugin in sim.

--- animProcess/src/cozmoAnim/micData/micDataSystem.cpp
+++ animProcess/src/cozmoAnim/micData/micDataSystem.cpp
@@ -463,6 +463,7 @@ void MicDataSystem::Update(BaseStationTime_t currTime_nanosec)
   // Try to retrieve the speaker latency from the AkAlsaSink plugin. We
   // only need to actually call into the plugin once (or until we get a
   // nonzero latency), since the latency does not change during runtime.
+#ifndef SIMULATOR
   if (_speakerLatency_ms == 0) {
     if (_context != nullptr) {
       const auto* audioController = _context->GetAudioController();
@@ -477,6 +478,7 @@ void MicDataSystem::Update(BaseStationTime_t currTime_nanosec)
       }
     }
   }
+#endif
 }
 
 void MicDataSystem::ClearCurrentStreamingJob()
