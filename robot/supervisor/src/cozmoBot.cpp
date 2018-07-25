db64a46bb88779b41fcd4044fd0384bebeb6ee66
Stuart Eichert <seichert@anki.com>
2018-07-25 12:09:23 -0700

    VIC-2756 robot: Set TooLong threshold to 2000us
    We see `CozmoBot.MainCycleTimeError` reported in `logcat` more
    often than needed to diagnose problems.  The previous threshold of
    700 microseconds seems too low.  This change moves it up to 2000
    microseconds which should be more reasonable and reduce the
    frequency of these log messages.

--- robot/supervisor/src/cozmoBot.cpp
+++ robot/supervisor/src/cozmoBot.cpp
@@ -53,7 +53,7 @@ namespace Anki {
         u32 lastCycleStartTime_ = 0;
         u32 lastMainCycleTimeErrorReportTime_ = 0;
         const u32 MAIN_TOO_LATE_TIME_THRESH_USEC = ROBOT_TIME_STEP_MS * 1500;  // Normal cycle time plus 50% margin
-        const u32 MAIN_TOO_LONG_TIME_THRESH_USEC = 700;
+        const u32 MAIN_TOO_LONG_TIME_THRESH_USEC = 2000;
         const u32 MAIN_CYCLE_ERROR_REPORTING_PERIOD_USEC = 1000000;
       } // Robot private namespace
 
