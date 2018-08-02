2531cbbd338b3d54bda1c208ee875bef1a4ef69f
Shawn Blakesley <shawnblakesley@gmail.com>
2018-08-02 15:01:26 -0700

    Share gateway certs between vic-cloud and vic-gateway (#2891)

---
+++ cloud/go/src/anki/robot/gateway_cert_linux.go
@@ -0,0 +1,8 @@
+// +build linux
+
+package robot
+
+const (
+	GatewayKey  = "/data/etc/robot.pem"
+	GatewayCert = "/data/vic-gateway/gateway.cert"
+)
---
+++ cloud/go/src/anki/robot/gateway_cert_other.go
@@ -0,0 +1,8 @@
+// +build !linux
+
+package robot
+
+const (
+	GatewayKey  = "/tmp/anki/gateway/trust.key"
+	GatewayCert = "/tmp/anki/gateway/trust.cert"
+)
