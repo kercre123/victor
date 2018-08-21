a94f00a00cdc5ce6af47bdb0a2d71644eca6991f
Bryon Vandiver <unicdk@gmail.com>
2018-08-21 16:57:47 -0700

    Checkpoint

--- robot/syscon/common/flash.cpp
+++ robot/syscon/common/flash.cpp
@@ -47,10 +47,6 @@ void Flash::writeFlash(const void* targ, const void* src, int size) {
   lockFlash();
 }
 
-extern "C" void EraseFlash() {
-  Flash::eraseApplication();
-}
-
 void Flash::eraseApplication(void) {
   unlockFlash();
   FLASH->CR |= FLASH_CR_PER;
