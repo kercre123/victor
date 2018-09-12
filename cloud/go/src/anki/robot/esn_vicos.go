320a7c056be52a9ac399f09fedae0ad6b958af63
Brian Chapados <chapados@anki.com>
2018-09-12 16:40:01 -0700

    VIC-1951 VIC-6721 Safe readEsn + group dir access (#3516)
    * VIC-1951 Use `emr-cat` instead of reading raw disk
    
    When running as a non-root user, `vic-cloud` does not have permission
    to read from raw block partitions.  Instead, it should use the `emr-cat`
    utility, which was created for the purpose of accessing `emr` data in
    a consistent way for every program. This utility can have its sticky
    bit set to run with appropriate permissions to access the block partition
    and return data.
    
    * VIC-1951 Create dirs with group rwx privs
    
    All vic-* share a common location in /data (/data/data/com.anki.victor)
    where persistent or cache files live. When programs are running as non-root
    users, they will still belong to the `anki` group so that they can access
    any shared persistent data. This change ensures that the `persistent`,
    `cache` and other dirs created under our shared data dir are accessible to
    all programs that are members of the `anki` group.
    
    * VIC-1951 Add mode param to FileUtils::CreateDirectory
    
    The default value is S_IRWXU|S_IRWXG. Since this is intended to be used for anki code,
    which will be run by users that share a common group, it will grant all users in the
    `anki` group full access to any directories that might be created. Code that requires
    stricter access can change the param (such as dasmgr). Process that require stricter
    default values can set the `umask`, which will be used by the underlying `mkdir` call.
    
    (cherry picked from commit 24ce20ad604ac65deaf72ffea5e6004c1f20e303)
    Signed-off-by: David Woods <dwoods@anki.com>

--- cloud/go/src/anki/robot/esn_vicos.go
+++ cloud/go/src/anki/robot/esn_vicos.go
@@ -3,28 +3,25 @@
 package robot
 
 import (
-	"encoding/binary"
-	"fmt"
-	"os"
 	"os/exec"
 	"strings"
 )
 
+var esn string
+
 // ReadESN returns the ESN of a robot by reading it from
 // the robot's filesystem
 func ReadESN() (string, error) {
-	file, err := os.Open("/dev/block/bootdevice/by-name/emr")
+	if esn != "" {
+		return esn, nil
+	}
+
+	buf, err := exec.Command("/bin/emr-cat", "e").Output()
 	if err != nil {
 		return "", err
 	}
-	defer file.Close()
-
-	// ESN is first 4 bytes of the file
-	var numEsn uint32
-	if err := binary.Read(file, binary.LittleEndian, &numEsn); err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("%08x", numEsn), nil
+	esn = strings.TrimSpace(string(buf))
+	return esn, nil
 }
 
 // OSVersion returns a string representation of the OS version, like:
