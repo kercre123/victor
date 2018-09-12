d4d5a96b760dd9b7fe4a6265af750715960f8aa2
Jordan Rivas <jordan@anki.com>
2018-09-11 17:09:25 -0700

    VIC-6715 Don’t link unlicensed Wwise plugins in Shipping build
    In shipping build script we set the CMake flag `AUDIO_RELEASE=true`.  We use this flag to link the Wwise release libs, disable profiling and only link plugins we have licenses for. For normal non-shipping builds we link all plugins to allow the audio designers test and play with all options.
    
    (cherry picked from commit 351ae8fafb8d5a10b19d73c6d3910d6b29a0173d)
    Signed-off-by: David Woods <dwoods@anki.com>

--- cmake/audio.cmake
+++ cmake/audio.cmake
@@ -1,53 +1,100 @@
 #
 # Link Audio Plugins
 #
-set(ANKI_LIB_AUDIO_PLUGINS
-  # Effect Plugins
-  AE_AK_COMPRESSOR_FX
-  AE_AK_DELAY_FX
-  AE_AK_METER_FX
-  AE_AK_EXPANDER_FX
-  AE_AK_PARAMETRIC_EQ_FX
-  AE_AK_GAIN_FX
-  AE_AK_PEAK_LIMITER_FX
-  AE_AK_PITCH_SHIFTER_FX
-  AE_AK_TIME_STRETCH_FX
-  AE_AK_FLANGER_FX
-  AE_AK_TREMOLO_FX
-  AE_AK_HARMONIZER_FX
-  AE_AK_MATRIX_REVERB_FX
-  AE_AK_ROOM_VERB_FX
-  AE_AK_SOUND_SEED_IMPACT_FX
-  AE_AK_GUITAR_DISTORTION_FX
-  AE_AK_STEREO_DELAY_FX
-  AE_AK_CONVOLUTION_REVERB_FX
-  AE_AK_RECORDER_FX
-  AE_MCDSP_LIMITER_FX
-  AE_MCDSP_FUTZ_BOX_FX
-  AE_Krotos_Vocoder_FX
-  # Source Plugins	
-  AE_AK_SILENCE_SOURCE
-  AE_AK_SINE_GENERATOR_SOURCE
-  AE_AK_TONE_GENERATOR_SOURCE
-  AE_AK_SOUND_SEED_WOOSH_SOURCE
-  AE_AK_SOUND_SEED_WIND_SOURCE
-  AE_AK_SYNTH_ONE_SOURCE
-)
 
-if(VICOS)
-list(APPEND ANKI_LIB_AUDIO_PLUGINS
-  # Sink Plugins
-  AE_AK_ALSA_SINK
+option(AUDIO_RELEASE "Only link plugs we have licensed for" OFF)
 
-  # Linux specific plugins
-  AE_iZotope_HybridReverb_FX
-  AE_iZotope_TrashBoxModeler_FX
-  AE_iZotope_TrashDelay_FX
-  AE_iZotope_TrashDistortion_FX
-  AE_iZotope_TrashDynamics_FX
-  AE_iZotope_TrashFilters_FX
-  AE_iZotope_TrashMultibandDistortion_FX
-)
-endif()
+if(AUDIO_RELEASE)
+  # Only link plugins we have lisences for
+  message("AUDIO_RELEASE is ON - Only link licensed plugins")
+  set(ANKI_LIB_AUDIO_PLUGINS
+    # Effect Plugins
+    AE_AK_COMPRESSOR_FX
+    AE_AK_DELAY_FX
+    AE_AK_METER_FX
+    AE_AK_EXPANDER_FX
+    AE_AK_PARAMETRIC_EQ_FX
+    AE_AK_GAIN_FX
+    AE_AK_PEAK_LIMITER_FX
+    AE_AK_PITCH_SHIFTER_FX
+    AE_AK_TIME_STRETCH_FX
+    AE_AK_FLANGER_FX
+    AE_AK_TREMOLO_FX
+    AE_AK_HARMONIZER_FX
+    AE_AK_MATRIX_REVERB_FX
+    AE_AK_ROOM_VERB_FX
+    AE_AK_GUITAR_DISTORTION_FX
+    AE_AK_STEREO_DELAY_FX
+    AE_AK_RECORDER_FX
+    AE_MCDSP_LIMITER_FX
+    AE_Krotos_Vocoder_FX
+    # Source Plugins	
+    AE_AK_SILENCE_SOURCE
+    AE_AK_SINE_GENERATOR_SOURCE
+    AE_AK_TONE_GENERATOR_SOURCE
+    AE_AK_SYNTH_ONE_SOURCE
+  )
+
+  if(VICOS)
+  list(APPEND ANKI_LIB_AUDIO_PLUGINS
+    # Sink Plugins
+    AE_AK_ALSA_SINK
+  )
+  endif(VICOS)
+
+else()
+
+  # Link all availble plugins
+  message("AUDIO_RELEASE is OFF - Link all Wwise plugins")
+  set(ANKI_LIB_AUDIO_PLUGINS
+    # Effect Plugins
+    AE_AK_COMPRESSOR_FX
+    AE_AK_DELAY_FX
+    AE_AK_METER_FX
+    AE_AK_EXPANDER_FX
+    AE_AK_PARAMETRIC_EQ_FX
+    AE_AK_GAIN_FX
+    AE_AK_PEAK_LIMITER_FX
+    AE_AK_PITCH_SHIFTER_FX
+    AE_AK_TIME_STRETCH_FX
+    AE_AK_FLANGER_FX
+    AE_AK_TREMOLO_FX
+    AE_AK_HARMONIZER_FX
+    AE_AK_MATRIX_REVERB_FX
+    AE_AK_ROOM_VERB_FX
+    AE_AK_SOUND_SEED_IMPACT_FX
+    AE_AK_GUITAR_DISTORTION_FX
+    AE_AK_STEREO_DELAY_FX
+    AE_AK_CONVOLUTION_REVERB_FX
+    AE_AK_RECORDER_FX
+    AE_MCDSP_LIMITER_FX
+    AE_MCDSP_FUTZ_BOX_FX
+    AE_Krotos_Vocoder_FX
+    # Source Plugins	
+    AE_AK_SILENCE_SOURCE
+    AE_AK_SINE_GENERATOR_SOURCE
+    AE_AK_TONE_GENERATOR_SOURCE
+    AE_AK_SOUND_SEED_WOOSH_SOURCE
+    AE_AK_SOUND_SEED_WIND_SOURCE
+    AE_AK_SYNTH_ONE_SOURCE
+  )
+
+  if(VICOS)
+  list(APPEND ANKI_LIB_AUDIO_PLUGINS
+    # Sink Plugins
+    AE_AK_ALSA_SINK
+
+    # Linux specific plugins
+    AE_iZotope_HybridReverb_FX
+    AE_iZotope_TrashBoxModeler_FX
+    AE_iZotope_TrashDelay_FX
+    AE_iZotope_TrashDistortion_FX
+    AE_iZotope_TrashDynamics_FX
+    AE_iZotope_TrashFilters_FX
+    AE_iZotope_TrashMultibandDistortion_FX
+  )
+  endif(VICOS)
+
+endif(AUDIO_RELEASE)
 
 import(audio "lib/audio")
