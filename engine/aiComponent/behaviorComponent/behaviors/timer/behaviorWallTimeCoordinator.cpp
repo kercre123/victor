706e7a115be803241b482a1ccc242fc5b2830c58
Brad Neuman <brad@anki.com>
2018-09-18 15:12:28 -0700

    VIC-6748 fix TTS string formatting for time (#3649)
    - Use standard functions instead of buggy home-grown code
    
    - Added unit tests for conversion to string (which is sent into TTS)
    
    (cherry picked from commit 063038d041109546b6ee620ea93b2f284e288637)
    Signed-off-by: David Woods <dwoods@anki.com>

--- engine/aiComponent/behaviorComponent/behaviors/timer/behaviorWallTimeCoordinator.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/timer/behaviorWallTimeCoordinator.cpp
@@ -21,6 +21,8 @@
 #include "engine/faceWorld.h"
 #include "osState/wallTime.h"
 
+#include <iomanip>
+
 namespace Anki {
 namespace Vector {
   
@@ -186,29 +188,33 @@ void BehaviorWallTimeCoordinator::StartTTSGeneration()
 
   const auto& settingsManager = GetBEI().GetSettingsManager();
   const bool clockIs24Hour = settingsManager.GetRobotSettingAsBool(external_interface::RobotSetting::clock_24_hour);
-  const int divisor = clockIs24Hour ? 24 : 12;
 
-  const int currentMins = localTime.tm_min;
-  int currentHours = localTime.tm_hour % divisor;
+  auto textOfTime = GetTTSStringForTime(localTime, clockIs24Hour);
 
-  if( !clockIs24Hour && currentHours == 0 ) {
-    currentHours = 12;
-  }
+  const UtteranceTriggerType triggerType = UtteranceTriggerType::Manual;
+  const AudioTtsProcessingStyle style = AudioTtsProcessingStyle::Default_Processed;
 
   auto callback = [this](const UtteranceState& utteranceState)
   {
     _dVars.utteranceState = utteranceState;
   };
-  
-  const std::string hourStr = currentHours < 10 ? "0" + std::to_string(currentHours) : std::to_string(currentHours);
-  const std::string minStr  = currentMins  < 10 ? "0" + std::to_string(currentMins)  : std::to_string(currentMins);
-
-  std::string textOfTime = hourStr + ":" + minStr;
-  const UtteranceTriggerType triggerType = UtteranceTriggerType::Manual;
-  const AudioTtsProcessingStyle style = AudioTtsProcessingStyle::Default_Processed;
 
   _dVars.utteranceID = GetBEI().GetTextToSpeechCoordinator().CreateUtterance(textOfTime, triggerType, style,
                                                                              1.0f, callback);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+std::string BehaviorWallTimeCoordinator::GetTTSStringForTime(struct tm& localTime, const bool clockIs24Hour)
+{
+  std::stringstream ss;
+  if( clockIs24Hour ) {
+    ss << std::put_time(&localTime, "%H:%M");
+  }
+  else {
+    ss << std::put_time(&localTime, "%I:%M");
+  }
+
+  return ss.str();
 }
 
 
