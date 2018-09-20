b818819b3d69f1cc09cec0e55f489ae43fa41eb9
Stuart Eichert <seichert@anki.com>
2018-09-20 10:11:18 -0700

    VIC-7187 update-engine: Accept X.Y.Z.<build-num>
    We want to change to 4 number versions like 1.0.0.1995.  This change
    updates the regular expression that validates version numbers to accept
    a 3 or 4 number version.
    
    Later, once we are no longer producing builds with 3 number versions and
    we are not downgrading to them, we can tighten up this check to only
    accept 4 number versions.
    
    (cherry picked from commit 3669ce0188a38fa72dc008ea85dc4e4daa45843e)
    Signed-off-by: David Woods <dwoods@anki.com>

--- platform/update-engine/update-engine.py
+++ platform/update-engine/update-engine.py
@@ -513,7 +513,7 @@ def validate_new_os_version(current_os_version, new_os_version, cmdline):
     allow_downgrade = os.getenv("UPDATE_ENGINE_ALLOW_DOWNGRADE", "False") in TRUE_SYNONYMS
     if allow_downgrade and "anki.dev" in cmdline:
         return
-    os_version_regex = re.compile('^\d+\.\d+\.\d+(d|ud)?$')
+    os_version_regex = re.compile('^(?:\d+\.){2,3}\d+(d|ud)?$')
     m = os_version_regex.match(new_os_version)
     if not m:
         die(216, "OS version " + new_os_version + " does not match regular expression")
