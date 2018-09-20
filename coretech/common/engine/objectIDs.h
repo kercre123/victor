d6645db840b2b182402c59b18a3302cb4e529b0d
David C Mudie <dmudie@anki.com>
2018-09-20 16:52:03 -0700

    Tech Debt: Remove virtual from ObjectID cast operator (#3681)
    Xcode10 reports -Wdelete-nonvirtual-dtor warnings for Anki::ObjectID because the base class (UniqueEnumeratedValue) declares a virtual cast operator.
    We don't have any code that overrides this operator, so it doesn't need to be virtual.
    
    Incidental cleanup:
    
    1. Add virtual destructor in testPointerHandle.cpp to silence -Wdelete-nonvirtual-dtor.
    
    2. Remove unused variable in testBeiConditions.cpp to silence -Wunused-variable.

--- coretech/common/engine/objectIDs.h
+++ coretech/common/engine/objectIDs.h
@@ -22,20 +22,20 @@
 #include <utility>
 
 namespace Anki {
-  
+
   template<typename StorageType_>
   class UniqueEnumeratedValue
   {
   public:
     using StorageType = StorageType_;
-    
+
     UniqueEnumeratedValue() : _value(UNKNOWN_VALUE) { }
-    
+
     // Comparison and sorting (for use as key value)
     bool operator==(const UniqueEnumeratedValue& other) const { return _value == other._value; }
     bool operator!=(const UniqueEnumeratedValue& other) const { return _value != other._value; }
     bool operator< (const UniqueEnumeratedValue& other) const { return _value <  other._value; }
-    
+
     bool operator==(const StorageType_ value) const { return _value == value; }
     bool operator!=(const StorageType_ value) const { return _value != value; }
     bool operator< (const StorageType value) const { return _value <  value; }
@@ -47,42 +47,42 @@ namespace Anki {
     // assignment should only be used as a means of selecting/finding a specific
     // UniqueEnumeratedValue.
     //
-    
+
     // Assignment
     UniqueEnumeratedValue<StorageType>& operator=(const UniqueEnumeratedValue<StorageType>& other) {
       _value = other._value;
       return *this;
     }
-    
+
     UniqueEnumeratedValue<StorageType>& operator=(const StorageType value) {
       _value = value;
       return *this;
     }
-    
+
     template<typename FwdType>
     explicit UniqueEnumeratedValue(FwdType&& value) : _value(std::forward<FwdType>(value)) { }
-    
+
     bool IsUnknown() const { return _value == UNKNOWN_VALUE; }
-    
+
     bool IsSet() const { return _value != UNKNOWN_VALUE; }
-    
+
     void SetToUnknown() { _value = UNKNOWN_VALUE; }
-    
+
     void UnSet() { _value = UNKNOWN_VALUE; }
-    
+
     //void SetToAny() { _value = ANY_VALUE; }
-    
+
     StorageType GetValue() const { return _value; }
-    
-    virtual operator StorageType() const { return _value; }
-    
+
+    operator StorageType() const { return _value; }
+
   protected:
-    
+
     // Derived classes can choose whether to expose this ability or not
     void SetValue(StorageType newValue) { _value = newValue; }
-    
+
   private:
-    
+
     static const StorageType UNKNOWN_VALUE = -1;
     //static const StorageType ANY_VALUE     = std::numeric_limits<StorageType>::max();
 
@@ -96,11 +96,11 @@ namespace Anki {
   {
     friend void ResetObjectIDCounter();
   public:
-    
+
     void Set();
-    
+
     using UniqueEnumeratedValue<StorageType>::operator=;
-    
+
     ObjectID() = default;
     ObjectID(StorageType value) : UniqueEnumeratedValue<StorageType>(value) { }
 
@@ -108,7 +108,7 @@ namespace Anki {
     static StorageType UniqueIDCounter;
 
   }; // class ObjectID
-  
+
 } // namespace Anki
 
 #endif // __Anki_Common_ObjectIDs_H__
