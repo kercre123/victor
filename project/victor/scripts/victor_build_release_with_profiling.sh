d14cc693e02e30ad06471139736671988128c1b3
Stuart Eichert <seichert@anki.com>
2018-09-25 15:09:20 -0700

    VIC-7452 By default, disable profiling in release builds
    Often, these logs generated by profiling overwhelm the system and
    consume too much CPU.  With this change, a release build will have
    `ANKI_PROFILING_ENABLED=0` by default.  To build for release, but have
    `ANKI_PROFILING_ENABLED=1`, you can now use
    `victor_build_release_with_profiling`.
    
    (cherry picked from commit 880833eee9d329ffecc5294fc4f9b26fbe4066b1)

---
+++ project/victor/scripts/victor_build_release_with_profiling.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
+${GIT_PROJ_ROOT}/project/victor/scripts/victor_build_release.sh \
+		-DANKI_PROFILING_ENABLED=1 \
+		"$@"
