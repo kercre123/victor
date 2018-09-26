ab15d58522257dd329c705f4afa7dc89294f9aa2
Michael <mwillett@anki.com>
2018-09-25 17:45:06 -0700

    make convexIntersection statically typed

--- coretech/common/engine/math/convexIntersection.h
+++ coretech/common/engine/math/convexIntersection.h
@@ -5,7 +5,7 @@
  * Created: 2018-06-15
  *
  * Description: Defines an the intersection of an arbitrary number of bounded convex sets
- *                      S₁ ⋂ S₂ ⋂ ... ⋂ Sₓ  = { p ∈ ℝⁿ | p ∈ S₁, p ∈ S₂, p ∈ Sₓ } 
+ *                      S₁ ⋂ S₂ ⋂ ... ⋂ Sₓ  = { p ∈ ℝⁿ | p ∈ S₁, p ∈ S₂, ..., p ∈ Sₓ } 
  *
  * Copyright: Anki, Inc. 2018
  *
@@ -20,50 +20,82 @@
 
 namespace Anki {
 
-template <DimType N, typename T>
-class ConvexIntersection : public BoundedConvexSet<N,T> 
+template <DimType N, typename T, class S1, class S2, typename = typename std::enable_if<
+  (std::is_base_of<PointSet<N,T>, S1>::value && std::is_base_of<PointSet<N,T>, S2>::value)
+>::type>
+class ConvexIntersection : public BoundedConvexSet<N,T>
 {
 public:
-  ConvexIntersection() : _sets() {}
+  ConvexIntersection(S1 set1, S2 set2) : _set1(set1), _set2(set2) {}
   virtual ~ConvexIntersection() {}
 
-  template<class Derived, typename = typename std::enable_if<std::is_base_of<BoundedConvexSet<N,T>, Derived>::value>::type>
-  void AddSet(const Derived& p) {
-    _sets.push_back( std::make_unique<Derived>(p) );
-  }
-
-  virtual bool InHalfPlane(const Halfplane<N,T>& H) const override {
-    return std::all_of(_sets.begin(), _sets.end(), [&H] (const auto& S) { return S->InHalfPlane(H); });
-  }
-
   virtual bool Contains(const Point<N,T>& x) const override {
-    return std::all_of(_sets.begin(), _sets.end(), [&x] (const auto& S) { return S->Contains(x); });
+    return _set1.Contains(x) && _set2.Contains(x);
+  }
+  
+  bool ContainsHyperCube(const AxisAlignedHyperCube<N,T>& C) const {
+    // NOTE: this is merely a sufficient condition, but not necessary
+    return SetContains(_set1, C) && SetContains(_set2, C);
   }
 
   virtual bool Intersects(const AxisAlignedHyperCube<N,T>& C) const override {
-    return std::all_of(_sets.begin(), _sets.end(), [&C] (const auto& S) { return S->Intersects(C); });
+    return this->_set1.Intersects(C) && this->_set2.Intersects(C);
+  }
+
+  virtual bool InHalfPlane(const Halfplane<N,T>& H) const override {
+    return this->_set1.InHalfPlane(H) && this->_set2.InHalfPlane(H);
   }
 
   virtual AxisAlignedHyperCube<N,T> GetAxisAlignedBoundingBox() const override 
   {
-    Point<N,T> min(std::numeric_limits<T>::lowest());
-    Point<N,T> max(std::numeric_limits<T>::max());
-    for (const auto& S : _sets) {
-      const auto& sBoundingBox = S->GetAxisAlignedBoundingBox();
-      for (DimType dim = 0; dim < N; ++dim) {
-        if(sBoundingBox.GetMinVertex()[dim] > min[dim]) { min[dim] = sBoundingBox.GetMinVertex()[dim]; }
-        if(sBoundingBox.GetMaxVertex()[dim] < max[dim]) { max[dim] = sBoundingBox.GetMaxVertex()[dim]; }
-      }
+    // both Bounded types, so merge results from `GetAxisAlignedBoundingBox`
+    const auto& s1BoundingBox = this->_set1.GetAxisAlignedBoundingBox();
+    const auto& s2BoundingBox = this->_set2.GetAxisAlignedBoundingBox();
+    Point<N,T> min = s1BoundingBox.GetMinVertex();
+    Point<N,T> max = s1BoundingBox.GetMaxVertex();
+
+    for (DimType dim = 0; dim < N; ++dim) {
+      if(s2BoundingBox.GetMinVertex()[dim] > min[dim]) { min[dim] = s2BoundingBox.GetMinVertex()[dim]; }
+      if(s2BoundingBox.GetMaxVertex()[dim] < max[dim]) { max[dim] = s2BoundingBox.GetMaxVertex()[dim]; }
     }
 
     return AxisAlignedHyperCube<N,T>(min, max);
   }
 
-private:
-  std::vector< std::unique_ptr<BoundedConvexSet2f> > _sets;
+protected:
+  // helpers for choosing the correct Contains for Hypercubes
+  template<class Set, typename = typename std::enable_if<std::is_base_of<ConvexPointSet<N,T>, Set>::value>::type>
+  inline static bool SetContains(const Set& S, const AxisAlignedHyperCube<N,T>& C) {
+    return S.ContainsAll(C.GetVertices());
+  }
+
+  // fall through for Intersection types
+  template<class M1, class M2>
+  inline static bool SetContains(const ConvexIntersection<N,T,M1,M2>& S, const AxisAlignedHyperCube<N,T>& C) {
+    return S.ContainsHyperCube(C);
+  }
+
+  S1 _set1;
+  S2 _set2;
 };
 
-using ConvexIntersection2f = ConvexIntersection<2, float>;
+template <typename S1, typename S2>
+using ConvexIntersection2f = ConvexIntersection<2, float, S1, S2>;
+
+// helper for making intersections of multiple sets via recursion: (S₁ ⋂ (S₂ ⋂ (S₃ ⋂ (... ⋂ Sₓ))))
+template<DimType N, typename T, class S>
+S MakeIntersection(S v) { return v; };
+
+template<DimType N, typename T, class S, typename... Args>
+decltype(auto) MakeIntersection(S head, Args... tail) {
+  auto retv = MakeIntersection<N,T,Args...>(tail...);
+  return ConvexIntersection<N, T, S, decltype(retv)>(head, retv);
+};
+
+template <typename... Args>
+decltype(auto) MakeIntersection2f(Args... sets) {
+  return MakeIntersection<2, float, Args...>(sets...);
+}
 
 }
 
--- engine/components/sensors/proxSensorComponent.cpp
+++ engine/components/sensors/proxSensorComponent.cpp
@@ -310,11 +310,16 @@ void ProxSensorComponent::UpdateNavMap()
     const Point2f obstacleP4 = (objectPos.GetTransform() * Transform3d(id, obstacleOffset1 + obstacleOffset3)).GetTranslation();
 
     // clear sensor beam
-    ConvexIntersection2f intersection;
-    intersection.AddSet( FastPolygon({sensorCorner1, sensorCorner2, robotPose.GetTranslation()}) );   // sensor beam
-    intersection.AddSet( FastPolygon({clampCorner1, clampCorner2, obstacleP2, obstacleP1}) );         // clamp
+    // NOTE: making an intersection of a quad and a triangle here results in a 50%-100% speedup over using a
+    //       five sided polygon, so use that if we can't insert a triangle FP.
+    FastPolygon sensorCone({sensorCorner1, sensorCorner2, robotPose.GetTranslation()});
+    auto intersection = MakeIntersection2f( sensorCone, FastPolygon({clampCorner1, clampCorner2, obstacleP2, obstacleP1}) );
 
+    if (sensorBeamHalfWidth_mm < ROBOT_BOUNDING_Y *.25) {
+      _robot->GetMapComponent().ClearRegion( sensorCone,  lastTimestamp);
+    } else {
     _robot->GetMapComponent().ClearRegion( intersection,  lastTimestamp);
+    }
 
     // Add proxObstacle if detected and close to robot, and lift is not interfering
     if (_latestData.distance_mm <= kMaxObsThreshold_mm && !noObject && !IsLiftInFOV()) {
