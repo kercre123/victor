3ffdce6aeb403a1c1d86a044baa2a6f803f87a05
Arjun Menon <ajn.menon@gmail.com>
2018-09-26 15:18:24 -0700

    VIC-7033 restore react to motor calibration (#3708)
    When the robot has an automatically started calibration (not initiated by behaviors) then the robot will enter a reaction behavior to wait while the calibration completes

---
+++ engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToMotorCalibration.h
@@ -0,0 +1,56 @@
+/**
+ * File: BehaviorReactToMotorCalibration.h
+ *
+ * Author: Kevin Yoon
+ * Created: 11/2/2016
+ *
+ * Description: Behavior for reacting to automatic motor calibration
+ *
+ * Copyright: Anki, Inc. 2016
+ *
+ **/
+
+#ifndef __Cozmo_Basestation_Behaviors_BehaviorReactToMotorCalibration_H__
+#define __Cozmo_Basestation_Behaviors_BehaviorReactToMotorCalibration_H__
+
+#include "engine/aiComponent/behaviorComponent/behaviors/iCozmoBehavior.h"
+#include "clad/robotInterface/messageRobotToEngine.h"
+#include "util/signals/simpleSignal_fwd.h"
+
+namespace Anki {
+namespace Vector {
+
+  
+class BehaviorReactToMotorCalibration : public ICozmoBehavior
+{
+private:
+  using super = ICozmoBehavior;
+  
+  friend class BehaviorFactory;
+  BehaviorReactToMotorCalibration(const Json::Value& config);
+  
+public:  
+  virtual bool WantsToBeActivatedBehavior() const override;
+
+protected:
+  virtual void GetBehaviorOperationModifiers(BehaviorOperationModifiers& modifiers) const override {
+    modifiers.wantsToBeActivatedWhenCarryingObject = true;
+  }
+  virtual void GetBehaviorJsonKeys(std::set<const char*>& expectedKeys) const override {}
+  
+  virtual void OnBehaviorActivated() override;
+  virtual void OnBehaviorDeactivated() override;
+
+  virtual void HandleWhileInScopeButNotActivated(const EngineToGameEvent& event) override;
+  virtual void HandleWhileActivated(const EngineToGameEvent& event) override;
+
+  constexpr static f32 _kTimeout_sec = 5.;
+  
+  bool _headMotorCalibrationStarted = false;
+  bool _liftMotorCalibrationStarted = false;
+};
+  
+}
+}
+
+#endif // __Cozmo_Basestation_Behaviors_BehaviorReactToMotorCalibration_H__
