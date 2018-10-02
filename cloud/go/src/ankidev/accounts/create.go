059cdff8584967c844e017b2d06d0345c839963c
Joep van Gassel <joep.vangassel@anki.com>
2018-10-02 01:57:59 -0700

    More informative error messages for cloud account creation and login
    The account creation and login for only provide a status code and
    not the actual error message in the body of the HTTP resposnse.

--- cloud/go/src/ankidev/accounts/create.go
+++ cloud/go/src/ankidev/accounts/create.go
@@ -1,7 +1,6 @@
 package accounts
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 
@@ -28,7 +27,11 @@ func DoCreate(envName, email, password string) (apiclient.Json, error) {
 	if err != nil {
 		return nil, err
 	} else if resp.StatusCode != http.StatusOK {
-		return nil, errors.New(fmt.Sprint("http status ", resp.StatusCode))
+		json, err := resp.Json()
+		if err != nil || json == nil {
+			return nil, fmt.Errorf("http status %d: (no JSON body)", resp.StatusCode)
+		}
+		return nil, fmt.Errorf("http status %d: (%q)", resp.StatusCode, json["message"])
 	}
 
 	json, err := resp.Json()
