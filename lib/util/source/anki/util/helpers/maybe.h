2081b819e822a16b4026c7d6a67b2aab12840f5a
Michael <mwillett@anki.com>
2018-10-09 11:54:27 -0700

    implement maybe type

---
+++ lib/util/source/anki/util/helpers/maybe.h
@@ -0,0 +1,216 @@
+/**
+ * File: maybe.h
+ *
+ * Author: Michael Willett
+ * Created: 10/2/2018
+ *
+ * Description: 
+ * 
+ *      Implementation of c++ Maybe Monad
+ *      For a good tutorial on monadic types: 
+ *        http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
+ * 
+ * 
+ *    In general, this class can be used to encapsulate type safe calls to an object that
+ *    may or may not exist. Common C++ patterns often use pointers that must be checked
+ *    for null explicitly before derefernce, or otherwise require that the result to be
+ *    passed by mutable reference through the argument list, and the function returns bool.
+ *    Often times these patterns can degrade overtime as code is refactored, where the user
+ *    forgets to check the return type of a function, and falsely assume the resulting type
+ *    is valid. 
+ * 
+ *    While `std::optional` introduced in C++17 aims to provide a type signature
+ *    to help the developer with the 
+ *    
+ *    The Maybe typeclass encapsulates this behavior entirely within its own internal state
+ *    releiving the developer of the responsibility of checking the success of each step
+ *    until explicit derefernce performed by `ValueOr`.
+ * 
+ *    Operations on the Maybe type can be performed by the FMap and Bind operations.
+ *    FMap should be used in general applications of functions of the wrapped types, and 
+ *    Bind should be used when the result of the function is also a Maybe type
+ * 
+ *    IMPLEMENTATION NOTES:
+ *      -- input functions to FMap and Bind are not defined as std::function types for two
+ *         reasons. First, C++ lambda statements do not implicitly convert to std::function,
+ *         expression type, and limit the useability of `auto` specifier in this context.
+ *         Second, std::function adds additional runtime overhead when wrapping certain types.
+ * 
+ *    TODO:
+ *      -- Current implementation stores wrapped data inside std::shared_ptr, though it should
+ *         be possible to store this as a std::unique_ptr through more intiligent copy 
+ *         construtors
+ * 
+ *      -- currently only supports FMap and Bind operations with a single input argument T.
+ *         `std::forward<Func>(f)(*_data)` calls can be replaced with a deferred bind_first
+ *         operation once a working implementation of argument currying can be mind to work
+ *         with arbitrary function types, allowing for lazy evaluation upon dereference. 
+ * 
+ *      -- Could statically speedup `if(_valid)` checks by making distinct Just<T> and Nothing<T>
+ *         classes, though the lack of Algebraic Data Types in C++ makes it difficult for
+ *         a typename to map to two types concurrently
+ *
+ * 
+ * Copyright: Anki, Inc. 2018
+ *
+ **/
+
+#ifndef __UTIL_MAYBE_H__
+#define __UTIL_MAYBE_H__
+
+#include <type_traits>
+
+namespace Anki{ 
+namespace Util {
+
+template <typename T> class Maybe;
+
+// convenience operator forwards
+template <typename T> Maybe<T> Nothing() { return Maybe<T>::Nothing(); }
+template <typename T> Maybe<T> Just(T t) { return Maybe<T>::Just( t ); }
+template <typename T> Maybe<T> Just()    { return Maybe<T>::Just();  }
+
+
+// convenience prefix operation forwards
+template <typename Func, typename U, typename T = typename std::result_of_t<Func&(U&)> >
+Maybe<T> FMap(Func&& f, Maybe<U> u) { return u.FMap(f); }
+
+template <typename Func, typename U, typename RT = typename std::result_of_t<Func&(U&)> >
+RT Bind(Func&& f, Maybe<U> u) { return u.Bind(f); }
+
+// convenience infix operation forwards. 
+// NOTE: requires explicit `using namespace Anki::Util::MaybeOperators` for devleoper to use
+//       to prevent accidential operator overloading
+namespace MaybeOperators {
+  template <typename Func, typename U, typename T = typename std::result_of_t<Func&(U&)> >
+  Maybe<T> operator *=(Func&& f, Maybe<U> u) { return u.FMap(f); }
+
+  template <typename Func, typename U, typename RT = typename std::result_of_t<Func&(U&)> >
+  RT operator >>=(Func&& f, Maybe<U> u) { return u.Bind(f); }
+}
+
+namespace TypeOperators {
+  // check if object is of Maybe typeclass
+  template <typename T> struct MaybeT             : public std::false_type {};
+  template <typename T> struct MaybeT< Maybe<T> > : public std::true_type {};
+  template <typename T> constexpr bool IsMaybe() { return MaybeT<T>::value; }
+
+  // checking if a function is void type
+  template <typename Func, typename... Args>
+  constexpr bool IsVoidResult() { return std::is_void<std::result_of_t<Func&(Args&...)>>::value; }
+
+  // function handle for a static cast between two types
+  template <typename T, typename U>
+  constexpr T StaticCast(U u) { return static_cast<U>(u); }
+
+  template <typename Func>
+  using DerefResult = std::remove_reference_t< std::result_of_t<Func> >;
+} 
+
+template<typename T>
+class Maybe {
+  template <typename U> friend class Maybe;  // allow deferred binding of function parameter to result type
+
+public:
+  using Type = T;
+
+  // static constructor helpers
+  template<typename U>
+  static Maybe<T> Just(U val)                  { return Maybe<T>( val ); }
+  static Maybe<T> Nothing()                    { return Maybe<T>(); }
+
+  // test the state of Maybe object
+  bool IsJust()    const { return  _valid; }
+  bool IsNothing() const { return !_valid; }
+
+    
+  template <typename Func, typename RT = Maybe< TypeOperators::DerefResult<Func&(T&)> >>
+  RT FMap(Func&& f) const {
+    return (_valid) ? RT::Just( f, *_data ) : RT::Nothing(); 
+  }
+
+  // bind operation for mapping functions that already return a Maybe
+  template<typename Func, 
+           typename RT = std::result_of_t<Func&(T&)>,
+           typename    = std::enable_if_t<TypeOperators::IsMaybe<RT>()>>
+  RT Bind(Func&& f) const {
+    return (_valid) ? std::forward<Func>(f)(*_data) : RT::Nothing(); 
+  }
+  
+  // throw a specific error if we call bind with a function that does not return `Maybe<T>`
+  template<typename Func, bool IsMaybe = TypeOperators::IsMaybe<std::result_of_t<Func&(T&)>>(),
+           typename = typename std::enable_if_t<!IsMaybe>>
+  void Bind(Func&& f) const {
+    static_assert(IsMaybe, "Attempted to Bind a function that does not return a Maybe type. Did you mean FMap (infix `*=`)?"); 
+  }
+
+  template <typename U> T        ValueOr(U defaultVal)  const { return (_valid) ? *_data : defaultVal; }
+  template <typename U> Maybe<T> ThisOr(Maybe<U> other) const { return (_valid) ? *this : other.FMap( &TypeOperators::StaticCast<T,U> ); }
+
+protected:
+  // keep constructors private so construction must happen through `Just` or `Nothing`
+  template <typename U>
+  Maybe(U val) : _data( std::make_shared<T>(val) ), _valid(true) {} 
+  Maybe()      : _data(nullptr), _valid(false) {} 
+
+  // special constructor for forwarding function evaluation to result type construction 
+  template <typename Func, typename U> 
+  static Maybe<T> Just(Func&& f, U& d) { return Maybe<T>( std::forward<Func>(f)(d) ); }
+
+  // keep data under a shared_ptr so we can quickly copy this class
+  std::shared_ptr<T> _data;
+  bool               _valid;
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+// specialization for handling encaspulation of void type
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+template<>
+class Maybe<void> {
+  template <typename U> friend class Maybe;           // allow defering binding of function parameter to result type
+
+public:
+  using Type = void;
+
+  // static constructor helpers
+  static Maybe<void> Just()    { return Maybe<void>(true); }
+  static Maybe<void> Nothing() { return Maybe<void>(false); }
+
+  // test the state of Maybe object
+  bool IsJust()    const { return  _valid; }
+  bool IsNothing() const { return !_valid; }
+
+  template <typename Func, typename RT = Maybe< TypeOperators::DerefResult<Func&()> >>
+  RT FMap(Func&& f) const {
+    return (_valid) ? RT::Just( std::forward<Func>(f)() ) : RT::Nothing(); 
+  }
+
+  template<typename Func, 
+           typename RT = std::result_of_t<Func&()>,
+           typename    = std::enable_if_t< TypeOperators::IsMaybe<RT>() >>
+  RT Bind(Func&& f) const {
+    return (_valid) ? std::forward<Func>(f)() : RT::Nothing(); 
+  }
+
+  // NOTE: Cannot implement ValueOr since void cannot be a return type
+  // NOTE: ThisOr can only be used to bind other void types since no implicit cast to void is well-formed
+  Maybe<void> ThisOr(Maybe<void> other) const { return (_valid) ? *this : other; }
+
+protected:
+
+  Maybe(bool isJust = false) : _valid(isJust) {} 
+
+  // deferred constructor
+  template <typename Func, typename U, typename = typename std::enable_if_t< TypeOperators::IsVoidResult<Func, U>()> > 
+  static Maybe<void> Just(Func&& f, U& d) { 
+    std::forward<Func>(f)(d); 
+    return Maybe<void>(true); 
+  }
+
+  bool _valid;
+};
+
+} // Util
+} // Anki
+
+#endif
---
+++ lib/util/source/anki/utilUnitTest/tests/testMaybe.cpp
@@ -0,0 +1,369 @@
+/**
+ * File: testMaybe
+ *
+ * Author: Michael Willett
+ * Created: 08/11/15
+ *
+ * Description: Unit tests for math.h
+ *
+ * Copyright: Anki, Inc. 2015
+ *
+ **/
+
+#include "util/helpers/includeGTest.h"
+#include "util/helpers/maybe.h"
+#include "util/math/math.h"
+
+
+TEST(TestMaybe, NothingFMap)
+{
+  using namespace Anki::Util;
+
+  Maybe<int> notInt = Nothing<int>();
+
+  int plus2CalledCount = 0;
+  auto plus2 = [&plus2CalledCount](int x) -> int { 
+    ++plus2CalledCount;
+    return x+2; 
+  };
+
+  auto result = notInt.FMap(plus2);
+  
+  EXPECT_TRUE(notInt.IsNothing());
+  EXPECT_TRUE(result.IsNothing());
+  EXPECT_EQ(0, plus2CalledCount);
+
+  // test monadic chaining
+  auto result2 = result.FMap(plus2)
+                       .FMap(plus2)
+                       .FMap(plus2);
+
+  EXPECT_TRUE(result.IsNothing());
+  EXPECT_TRUE(result2.IsNothing());
+  EXPECT_EQ(0, plus2CalledCount);
+
+  // test infix format
+  using namespace Anki::Util::MaybeOperators;
+  
+  auto result3 = plus2 *= plus2 *= result;
+  auto result4 = FMap( plus2, FMap(plus2, Nothing<int>()) );
+
+  EXPECT_TRUE(result.IsNothing());
+  EXPECT_TRUE(result3.IsNothing());
+  EXPECT_TRUE(result4.IsNothing());
+  EXPECT_EQ(0, plus2CalledCount);
+}
+
+
+TEST(TestMaybe, JustFmap)
+{
+  using namespace Anki::Util;
+
+  Maybe<int> intM = Just(4);
+
+  int plus2CalledCount = 0;
+  auto plus2 = [&plus2CalledCount](int x) -> int { 
+    ++plus2CalledCount;
+    return x+2; 
+  };
+
+  auto result = intM.FMap(plus2);
+  int extractedVal = result.ValueOr(0);
+  
+  EXPECT_TRUE(intM.IsJust());
+  EXPECT_TRUE(result.IsJust());
+  EXPECT_EQ(6, extractedVal) << "calculated " << extractedVal << ", not 6";
+  EXPECT_EQ(1, plus2CalledCount) << "called " << plus2CalledCount << " times, should have been called once";
+
+  // test monadic chaining
+  auto result2 = result.FMap(plus2)
+                       .FMap(plus2)
+                       .FMap(plus2);
+
+
+  extractedVal = result2.ValueOr(0);
+
+  EXPECT_TRUE(result.IsJust());
+  EXPECT_TRUE(result2.IsJust());
+  EXPECT_EQ(12, extractedVal);
+  EXPECT_EQ(4, plus2CalledCount);
+
+  // test infix format
+  using namespace Anki::Util::MaybeOperators;
+
+  auto result3 = plus2 *= plus2 *= result;
+  auto result4 = FMap( plus2, FMap(plus2, Just(1)) );
+
+  EXPECT_TRUE(result3.IsJust());
+  EXPECT_TRUE(result4.IsJust());
+  EXPECT_EQ(10, result3.ValueOr(-1)) << "calculated " << result3.ValueOr(-1) << ", not 10";
+  EXPECT_EQ( 5, result4.ValueOr(-1)) << "calculated " << result4.ValueOr(-1) << ", not 10";
+}
+
+TEST(TestMaybe, NothingBind)
+{
+  using namespace Anki::Util;
+
+  Maybe<int> intM = Nothing<int>();
+
+  auto safeInv = [](float divisor) { return ( NEAR_ZERO(divisor) ) ? Nothing<float>() : Just(1/divisor); };
+
+  // double inverse should change the type, but keep the value
+  auto result = intM.Bind(safeInv)
+                    .Bind(safeInv);
+
+  float extractedVal = result.ValueOr(-1.2);
+  
+  EXPECT_TRUE(result.IsNothing());
+  EXPECT_TRUE( FLT_NEAR(-1.2f, extractedVal) ) << "calculated " << extractedVal << ", not -1.2";
+
+  // test infix format
+  using namespace Anki::Util::MaybeOperators;
+
+  auto result2 = safeInv >>= safeInv >>= intM;
+  auto result3 = Bind( safeInv, Bind(safeInv, intM) );
+
+  EXPECT_TRUE(result2.IsNothing());
+  EXPECT_TRUE(result3.IsNothing());
+}
+
+TEST(TestMaybe, JustBind)
+{
+  using namespace Anki::Util;
+
+  Maybe<int> intM = Just(4);
+
+  auto minus2  = [](int x)   { return x-2; };
+  auto safeInv = [](float divisor) { return ( NEAR_ZERO(divisor) ) ? Nothing<float>() : Just(1/divisor); };
+
+  // double inverse should change the type, but keep the value
+  auto result = intM.Bind(safeInv)
+                    .Bind(safeInv);
+
+  float extractedVal = result.ValueOr(0.);
+  
+  EXPECT_TRUE(result.IsJust());
+  EXPECT_TRUE( FLT_NEAR(4.f, extractedVal) ) << "calculated " << extractedVal << ", not 4.";
+
+  auto result2 = result.FMap(minus2).FMap(minus2);
+  extractedVal = result2.ValueOr(-5.f);
+  EXPECT_TRUE( NEAR_ZERO(extractedVal) ) << "calculated " << extractedVal << ", should be 0.f";
+
+  // alternative syntax test
+  auto result3 = result2.Bind(safeInv).Bind(safeInv);
+  EXPECT_TRUE( result3.IsNothing() ) << "safeInverse should return Nothing if inverting 0";
+
+
+  // test infix format
+  using namespace Anki::Util::MaybeOperators;
+
+  auto result4 = safeInv >>= safeInv >>= Just(4);
+  auto result5 = Bind( safeInv, Bind(safeInv, Just(4)) );
+
+  EXPECT_TRUE(result4.IsJust());
+  EXPECT_TRUE(result5.IsJust());
+  EXPECT_TRUE( FLT_NEAR(4.f, result4.ValueOr(-1)) ) << "calculated " << result4.ValueOr(-1) << ", not 4.";
+  EXPECT_TRUE( FLT_NEAR(4.f, result5.ValueOr(-1)) ) << "calculated " << result5.ValueOr(-1) << ", not 4.";
+
+  auto result6 = safeInv >>= safeInv >>= Just(0);
+  EXPECT_TRUE( result6.IsNothing() ) << "safeInverse should return Nothing if inverting 0";
+}
+
+TEST(TestMaybe, MutableClassMember)
+{
+  using namespace Anki::Util;
+
+  class NumWrapper {
+  public:
+    NumWrapper(float x) : _x(x) {}
+
+    void decr() { --_x; }
+
+    Maybe<void> invert() 
+    { 
+      if ( NEAR_ZERO(_x) ) {
+        return Nothing<void>(); 
+      } else { 
+        _x = 1/_x; 
+        return Just<void>();
+      }
+    }
+
+    float get() const { return _x; }
+
+  private:
+    float _x;
+  };
+
+  auto decrSelf   = [](NumWrapper& x) { x.decr(); };
+  auto invertSelf = [](NumWrapper& x) { return x.invert(); };
+
+  auto wrapM = Just<NumWrapper>(2.f);
+
+  wrapM.FMap(decrSelf);
+  wrapM.FMap(decrSelf);
+  auto failHere = wrapM.Bind(invertSelf);
+  wrapM.FMap(decrSelf);
+  auto succeedHere = wrapM.Bind(invertSelf);
+  auto extract = wrapM.FMap( [](auto w) { return w.get(); } );
+
+  EXPECT_TRUE(failHere.IsNothing());
+  EXPECT_TRUE(succeedHere.IsJust());
+  EXPECT_TRUE( FLT_NEAR(-1.f, extract.ValueOr(0.f)) ) << "should have inverted -1";
+}
+
+namespace {
+  template<typename T>
+  T SubtractAbs(T x, T y) { return fabs(x - y); }
+
+  template<typename T>
+  T Double(T x) { return x*2; }
+    
+  struct IntWrapper {
+    IntWrapper(int a) : x(a) {}
+    int x;
+    int& operator++() { return ++x; }
+    static int inc(int y) { return ++y; }
+  };
+}
+
+TEST(TestMaybe, FMapFunctionTypes)
+{
+  using namespace Anki::Util;
+
+  // these tests are just to ensure these calls are compiled without SFINAE errors.
+  // Other tests check proper execution
+
+  // std::function
+  float z = 2;
+  std::function<void(double)> a = [=](double x) { return z + x; };
+  Just(5).FMap(a);
+
+  // std::bind
+  auto b = std::bind(&SubtractAbs<int>, std::placeholders::_1, 5);
+  Just(2).FMap(b);
+
+  // lambda
+  auto c = [](float x) { return x / 2.f ; };
+  Just(5).FMap(c);
+
+  // anonymous
+  auto d = &Double<int>;
+  Just(2).FMap(d);
+
+  // static member function reference
+  auto e = &IntWrapper::inc;
+  Just(2).FMap(e);
+
+  // member function of existing type
+  auto f = std::bind(&IntWrapper::operator++, std::placeholders::_1);
+  Just<IntWrapper>(2).FMap(f);
+}
+
+TEST(TestMaybe, NothingVoid)
+{
+  using namespace Anki::Util;
+
+  // from Nothing<void>
+  auto notAnything = Nothing<void>();
+
+  int calledCount = 0;
+  auto get2 = [&calledCount]() -> int { 
+    ++calledCount;
+    return 2; 
+  };
+    
+  auto maybe2 = [&calledCount]() { 
+    ++calledCount;
+    return Just(2); 
+  };
+
+  auto resultFMap     = notAnything.FMap(get2);
+  auto resultBind     = notAnything.Bind(maybe2);
+  auto resultNotThis  = notAnything.ThisOr(Nothing<void>());
+  // NOTE: ValueOr does not exist for Maybe<void> since void cannot resolve to an instance
+  
+  EXPECT_TRUE(notAnything.IsNothing());
+  EXPECT_FALSE(notAnything.IsJust());
+  EXPECT_TRUE(resultFMap.IsNothing());
+  EXPECT_TRUE(resultBind.IsNothing());
+  EXPECT_EQ(0, calledCount) << "bind or fmap called the bound method even though the input was Nothing";
+
+  EXPECT_EQ(6, resultFMap.ValueOr(6)) << "Fmap result did not return default value in `ValueOr`";
+  EXPECT_TRUE(resultNotThis.IsNothing()) << "Bind result did not return default value in `ThisOr`";
+
+  // to Nothing<void>
+  auto notInt = Nothing<int>();
+  auto voidM  = notInt.FMap( [](auto x) { ++x; } );
+  auto voidM2 = Nothing<int>().FMap( [](auto x) { ++x; } );
+
+  EXPECT_TRUE(voidM.IsNothing());
+  EXPECT_TRUE(voidM2.IsNothing());
+  static_assert(std::is_same<decltype(voidM)::Type, void>(), "");
+}
+
+TEST(TestMaybe, JustVoid)
+{
+  using namespace Anki::Util;
+
+  // from Just<void>
+  auto justEmpty = Just<void>();
+
+  auto get2   = []() { return 2; };
+  auto maybe4 = []() { return Just(4); };
+
+  auto resultFMap = justEmpty.FMap(get2);
+  auto resultBind = justEmpty.Bind(maybe4);
+  auto resultThis = justEmpty.ThisOr(Nothing<void>());
+  // NOTE: ValueOr does not exist for Maybe<void> since void cannot resolve to an instance
+  
+  EXPECT_FALSE(justEmpty.IsNothing());
+  EXPECT_TRUE(justEmpty.IsJust());
+
+  EXPECT_TRUE(resultFMap.IsJust());
+  EXPECT_TRUE(resultBind.IsJust());
+  EXPECT_TRUE(resultThis.IsJust());
+
+  EXPECT_EQ(2, resultFMap.ValueOr(6)) << "Fmap result did not return current value in `ValueOr`";
+  EXPECT_EQ(4, resultBind.ValueOr(6)) << "Bind result did not return current value in `ValueOr`";
+
+  // to Just<void>
+  auto justInt = Just<int>(5);
+  auto voidM  = justInt.FMap( [](auto x) { ++x; } );
+
+  EXPECT_TRUE(voidM.IsJust());
+  static_assert(std::is_same<decltype(voidM)::Type, void>(), "");
+}
+
+TEST(TestMaybe, ValueOrTypeConversion) 
+{
+  using namespace Anki::Util;
+  auto intM = Just<int>(2); 
+  auto result = intM.ValueOr(.2);
+
+  static_assert(std::is_same<decltype(result), int>(), "ValueOr does maintain declaired type" );
+  EXPECT_TRUE( result == 2 ) << "calculated " << result << ", not 2.";
+
+  intM = Nothing<int>();
+  result = intM.ValueOr(.2);
+  EXPECT_TRUE( result == 0 ) << "calculated " << result << ", should of cast (float .2) to (int 0).";
+}
+
+TEST(TestMaybe, ThisOrInternalTypeConversion) 
+{
+  using namespace Anki::Util;
+
+  const float floatVal = -5.6;
+  auto intM = Just<int>(2); 
+  auto floatM = Just<float>(floatVal);
+  auto resultM = intM.ThisOr(floatM);
+  auto result = resultM.ValueOr(0);
+
+  static_assert(std::is_same<decltype(resultM), decltype(intM)>(), "ThisOr does maintain left declaired type" );
+  EXPECT_TRUE( result == 2 ) << "calculated " << result << ", not 2.";
+
+  intM = Nothing<int>();
+  resultM = intM.ThisOr(floatM);
+  result = resultM.ValueOr(0);
+  EXPECT_TRUE( result == ((int) floatVal) ) << "calculated " << result << ", should of cast (float " << floatVal << ") to (int " <<  (int) floatVal << ").";
+}
