969d9f2ad372983fbae6f987cb06ca8144007c04
Michael <mwillett@anki.com>
2018-10-09 11:17:17 -0700

    updated description

--- lib/util/source/anki/util/helpers/maybe.h
+++ lib/util/source/anki/util/helpers/maybe.h
@@ -4,11 +4,54 @@
  * Author: Michael Willett
  * Created: 10/2/2018
  *
- * Description: Implementation of Maybe Monad 
- *              for a good tutorial on monadic types: 
- *                  http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
+ * Description: 
+ * 
+ *      Implementation of c++ Maybe Monad
+ *      For a good tutorial on monadic types: 
+ *        http://adit.io/posts/2013-04-17-functors,_applicatives,_and_monads_in_pictures.html
+ * 
+ * 
+ *    In general, this class can be used to encapsulate type safe calls to an object that
+ *    may or may not exist. Common C++ patterns often use pointers that must be checked
+ *    for null explicitly before derefernce, or otherwise require that the result to be
+ *    passed by mutable reference through the argument list, and the function returns bool.
+ *    Often times these patterns can degrade overtime as code is refactored, where the user
+ *    forgets to check the return type of a function, and falsely assume the resulting type
+ *    is valid. 
+ * 
+ *    While `std::optional` introduced in C++17 aims to provide a type signature
+ *    to help the developer with the 
+ *    
+ *    The Maybe typeclass encapsulates this behavior entirely within its own internal state
+ *    releiving the developer of the responsibility of checking the success of each step
+ *    until explicit derefernce performed by `ValueOr`.
+ * 
+ *    Operations on the Maybe type can be performed by the FMap and Bind operations.
+ *    FMap should be used in general applications of functions of the wrapped types, and 
+ *    Bind should be used when the result of the function is also a Maybe type
+ * 
+ *    IMPLEMENTATION NOTES:
+ *      -- input functions to FMap and Bind are not defined as std::function types for two
+ *         reasons. First, C++ lambda statements do not implicitly convert to std::function,
+ *         expression type, and limit the useability of `auto` specifier in this context.
+ *         Second, std::function adds additional runtime overhead when wrapping certain types.
+ * 
+ *    TODO:
+ *      -- Current implementation stores wrapped data inside std::shared_ptr, though it should
+ *         be possible to store this as a std::unique_ptr through more intiligent copy 
+ *         construtors
+ * 
+ *      -- currently only supports FMap and Bind operations with a single input argument T.
+ *         `std::forward<Func>(f)(*_data)` calls can be replaced with a deferred bind_first
+ *         operation once a working implementation of argument currying can be mind to work
+ *         with arbitrary function types, allowing for lazy evaluation upon dereference. 
+ * 
+ *      -- Could statically speedup `if(_valid)` checks by making distinct Just<T> and Nothing<T>
+ *         classes, though the lack of Algebraic Data Types in C++ makes it difficult for
+ *         a typename to map to two types concurrently
  *
- * Copyright: Anki, Inc. 2014
+ *
+ * Copyright: Anki, Inc. 2018
  *
  **/
 
