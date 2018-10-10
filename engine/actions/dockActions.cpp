509bd66c599883e11c92f00d81e0a095e2df9255
Matt Michini <mmichini@anki.com>
2018-10-10 12:08:34 -0700

    VIC-5369 Beeline docking (#3903)
    - Add new docking method `HYBRID_DOCKING_BEELINE` which is just like normal hybrid docking, but instead of following the marker normal, it just follows a direct path from the robot to the cube. Make this method the default docking method for cube pickup.
    - Increase some of the docking error signal timeouts to account for the general slowness of vector's marker detection (as compared with Cozmo).
    - Do not do Hanns Maneuver checks if we are beeline docking, since the tolerances do not work when the approach is angled.
    - Comment out check for "no longer past point of no return", since this sometimes triggers spuriously and is not really needed (assuming the cube does not move during docking).
    - Convert some AnkiDebug -> AnkiInfo for easier debugging

--- engine/actions/dockActions.cpp
+++ engine/actions/dockActions.cpp
@@ -59,7 +59,7 @@ namespace Anki {
 
     // Which docking method actions should use
     CONSOLE_VAR(u32, kDefaultDockingMethod,"DockingMethod(B:0 T:1 H:2)", (u8)DockingMethod::BLIND_DOCKING);
-    CONSOLE_VAR(u32, kPickupDockingMethod, "DockingMethod(B:0 T:1 H:2)", (u8)DockingMethod::HYBRID_DOCKING);
+    CONSOLE_VAR(u32, kPickupDockingMethod, "DockingMethod(B:0 T:1 H:2)", (u8)DockingMethod::HYBRID_DOCKING_BEELINE);
     CONSOLE_VAR(u32, kRollDockingMethod,   "DockingMethod(B:0 T:1 H:2)", (u8)DockingMethod::BLIND_DOCKING);
     CONSOLE_VAR(u32, kStackDockingMethod,  "DockingMethod(B:0 T:1 H:2)", (u8)DockingMethod::BLIND_DOCKING);
 
