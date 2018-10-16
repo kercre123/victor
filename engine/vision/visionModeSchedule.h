d48f5f980e0ed63130ed762fd39ff21d979d6704
David C Mudie <dmudie@anki.com>
2018-10-16 14:31:52 -0700

    VIC-7013: Remove unused private member VisionModeSchedule _index (#3974)
    Fixes -Wunused warning reported by XCode 10 compiler.
    
    Review with 'w=1' to hide whitespace changes.

--- engine/vision/visionModeSchedule.h
+++ engine/vision/visionModeSchedule.h
@@ -34,17 +34,16 @@ public:
 
   // Whether or not the schedule is true at index indicating it is time to process
   bool IsTimeToProcess(u32 index) const;
-  
+
   // Returns whether the schedule will ever run (i.e. is not just "false" for all time)
   bool WillEverRun() const;
-  
+
 private:
   std::vector<bool> _schedule;
-  int               _index = 0;
-  
+
 }; // class VisionModeSchedule
-  
-  
+
+
 class AllVisionModesSchedule
 {
 public:
@@ -53,7 +52,7 @@ public:
   // If initWithDefaults=true, all modes' schedules are set to current defaults.
   // Otherwise, everything starts disabled.
   AllVisionModesSchedule(bool initWithDefaults = true);
-  
+
   // Initialize specified modes with given schedules, and initialize any unspecified
   // modes' schedules to the current defaults. (If useDefaultsForUnspecified=false,
   // any unspecified modes will be disabled.)
@@ -67,28 +66,28 @@ public:
   //
   AllVisionModesSchedule(const ModeScheduleList& schedules,
                          bool useDefaultsForUnspecified = true);
-  
+
   // Get the schedule for a specific mode
   VisionModeSchedule& GetScheduleForMode(VisionMode mode);
   const VisionModeSchedule& GetScheduleForMode(VisionMode mode) const;
-    
+
   // Returns whether it is time to process a mode
   bool IsTimeToProcess(VisionMode mode, u32 index) const;
-  
+
   // Change the defaults to use for unspecified modes
   static void SetDefaultSchedule(VisionMode mode, VisionModeSchedule&& schedule);
-  
+
 private:
-  
+
   using ScheduleArray = std::array<VisionModeSchedule, (size_t)VisionMode::Count>;
-  
+
   ScheduleArray _schedules;
-  
+
   static ScheduleArray sDefaultSchedules;
   static ScheduleArray InitDefaultSchedules();
 
 }; // class AllVisionModesSchedule
-  
+
 
 } // namespace Vector
 } // namespace Anki
