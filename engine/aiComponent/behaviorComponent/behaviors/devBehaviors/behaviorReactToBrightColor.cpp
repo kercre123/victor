701146e2b4794471bd332c25913dc1ea617d4c6a
Patrick Doran <patrick.doran@anki.com>
2018-10-18 15:58:34 -0700

    Code cleanup for finding a new color

--- engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorReactToBrightColor.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorReactToBrightColor.cpp
@@ -25,6 +25,9 @@
 #include "coretech/common/engine/jsonTools.h"
 #include "coretech/vision/engine/colorPixelTypes.h"
 
+#include <algorithm>
+#include <functional>
+
 // TODO: PDoran remove this
 #include <random>
 
@@ -33,15 +36,28 @@ namespace Vector {
 
 namespace {
 const char* const kEyeColorBehaviorKey = "eyeColorBehavior";
+const char* const kNumHuesKey = "numHues";
+
+/**
+ * @brief Modify the Vision::SalientPoint such that color is rounded to the nearest Hue
+ */
+void RoundToNearestHueImpl(u32 numHues, Vision::SalientPoint& pt) {
+  ColorRGBA color(pt.color_rgba);
+  Vision::PixelHSV hsv(Vision::PixelRGB(color.r(),color.g(),color.b()));
+  hsv.h() = std::roundf(hsv.h() * numHues)/numHues;
+  Vision::PixelRGB rgb = hsv.ToPixelRGB();
+  pt.color_rgba = ColorRGBA(rgb.r(),rgb.g(),rgb.b(),255).AsRGBA();
+}
 
 }
-  
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 BehaviorReactToBrightColor::InstanceConfig::InstanceConfig(const Json::Value& config)
 {
   const std::string& debugName = "BehaviorReactToBrightColor.InstanceConfig.LoadConfig";
   eyeColorBehaviorStr = JsonTools::ParseString(config, kEyeColorBehaviorKey, debugName);
+  numHues = JsonTools::ParseUInt32(config, kNumHuesKey, debugName);
+  DEV_ASSERT(numHues > 0,"Number of hues must be greater than 0");
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
@@ -109,6 +125,7 @@ void BehaviorReactToBrightColor::GetBehaviorJsonKeys(std::set<const char*>& expe
 #if 1
   const char* list[] = {
     kEyeColorBehaviorKey,
+    kNumHuesKey
   };
   expectedKeys.insert( std::begin(list), std::end(list) );
 #endif
@@ -125,60 +142,22 @@ void BehaviorReactToBrightColor::OnBehaviorActivated()
   PRINT_NAMED_ERROR("BehaviorReactToBrightColor.OnBehaviorActivated","PDORAN 1");
 
 
-  auto& component = GetAIComp<SalientPointsComponent>();
-  std::list<Vision::SalientPoint> latestBrightColors;
-  component.GetSalientPointSinceTime(latestBrightColors,Vision::SalientPointType::BrightColors,
-                                     _dVars.persistent.lastSeenTimeStamp);
-
-  // No bright colors since last time
-  if (latestBrightColors.empty())
+  // TODO: Replace this with std::optional like semantics
+  Vision::SalientPoint next;
+  bool found = FindNewColor(next);
+  if (!found)
   {
-    return;
-  }
-
-  // Color will fluctuate a lot, so we group BrightColor salient points into their nearest hue so we can act on
-  // those salient points as if they were the same object.
-  // TODO: move numSectores numSectors to InstanceConfig
-  float numSectors = 6.f;
-  auto ConvertToNearestHue = [numSectors](Vision::SalientPoint& pt) -> void {
-    ColorRGBA color(pt.color_rgba);
-    Vision::PixelHSV hsv(Vision::PixelRGB(color.r(),color.g(),color.b()));
-    hsv.h() = std::roundf(hsv.h() * numSectors)/numSectors;
-    Vision::PixelRGB rgb = hsv.ToPixelRGB();
-    pt.color_rgba = ColorRGBA(rgb.r(),rgb.g(),rgb.b(),255).AsRGBA();
-  };
-  std::for_each(latestBrightColors.begin(), latestBrightColors.end(), ConvertToNearestHue);
-
-  // TODO: In order to not repeatedly react to the same color, we put the color on cooldown and remove it from
-  // the list of SalientPoints that we're looking at.
-  auto IsOnCooldown = [](const Vision::SalientPoint& pt) -> bool {
-    return false;
-  };
-  std::remove_if(latestBrightColors.begin(), latestBrightColors.end(), IsOnCooldown);
-
-  // Finally choose the brightest SalientPoint to act on
-  auto IsBrighterThan = [](const Vision::SalientPoint& a, const Vision::SalientPoint& b) -> bool {
-    return a.score < b.score;
-  };
-
-  auto iter = std::max_element(latestBrightColors.begin(), latestBrightColors.end(), IsBrighterThan);
-  if (iter == latestBrightColors.end())
-  {
-    // TODO: Figure out how to get this to show up in webots at DEBUG level
     PRINT_NAMED_ERROR("BehaviorReactToBrightColor.OnBehaviorActivated","No new bright colors to act on");
     return;
   }
 
-
-  // DEBUGGING
-  {
-    ColorRGBA color(iter->color_rgba);
-    Vision::PixelHSV hsv(Vision::PixelRGB(color.r(),color.g(),color.b()));
-    // TODO: Figure out how to get this to show up in webots at DEBUG level
-    PRINT_NAMED_ERROR("BehaviorReactToBrightColor.OnBehaviorActivated","Reacting to bright color Hue: %d, Score: %d",
-                      static_cast<s32>(std::roundf(hsv.h()*360.f)),
-                      static_cast<s32>(std::roundf(iter->score)));
-  }
+  // Found a new color to react to, do something!
+  ColorRGBA color(next.color_rgba);
+  Vision::PixelHSV hsv(Vision::PixelRGB(color.r(),color.g(),color.b()));
+  // TODO: Figure out how to get this to show up in webots at DEBUG level
+  PRINT_NAMED_ERROR("BehaviorReactToBrightColor.OnBehaviorActivated","Reacting to bright color Hue: %d, Score: %d",
+                    static_cast<s32>(std::roundf(hsv.h()*360.f)),
+                    static_cast<s32>(std::roundf(next.score)));
 
 
   // TODO: Set color using the color of the salient point
@@ -238,6 +217,61 @@ void BehaviorReactToBrightColor::BehaviorUpdate()
   }
   // TODO: delete this function if you don't need it
   PRINT_NAMED_ERROR("BehaviorReactToBrightColor.BehaviorUpdate","PDORAN 2");
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorReactToBrightColor::RoundToNearestHue(std::list<Vision::SalientPoint>& points)
+{
+  std::for_each(points.begin(), points.end(), std::bind(RoundToNearestHueImpl, _iConfig.numHues, std::placeholders::_1));
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorReactToBrightColor::FindCurrentColor ()
+{
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+bool BehaviorReactToBrightColor::FindNewColor (Vision::SalientPoint& point)
+{
+  auto& component = GetAIComp<SalientPointsComponent>();
+  std::list<Vision::SalientPoint> latestBrightColors;
+  component.GetSalientPointSinceTime(latestBrightColors,Vision::SalientPointType::BrightColors,
+                                     _dVars.persistent.lastSeenTimeStamp);
+
+  // No bright colors since last time
+  if (latestBrightColors.empty())
+  {
+    return false;
+  }
+
+  // Color will fluctuate a lot, so we group BrightColor salient points into their nearest hue so we can act on
+  // those salient points as if they were the same object.
+  RoundToNearestHue(latestBrightColors);
+
+  // TODO: In order to not repeatedly react to the same color, we put the color on cooldown and remove it from
+  // the list of SalientPoints that we're looking at.
+  auto IsOnCooldown = [](const Vision::SalientPoint& pt) -> bool {
+    return false;
+  };
+  std::remove_if(latestBrightColors.begin(), latestBrightColors.end(), IsOnCooldown);
+
+  // Finally choose the brightest SalientPoint
+  auto IsScoreLessThan = [](const Vision::SalientPoint& a, const Vision::SalientPoint& b) -> bool {
+    return a.score < b.score;
+  };
+  auto iter = std::max_element(latestBrightColors.begin(), latestBrightColors.end(), IsScoreLessThan);
+
+  if (iter == latestBrightColors.end())
+  {
+    // No BrightColors to choose from
+    return false;
+  }
+  else
+  {
+    // Found a bright color!
+    point = *iter;
+    return true;
+  }
 }
 
 }
--- engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorReactToBrightColor.h
+++ engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorReactToBrightColor.h
@@ -16,6 +16,9 @@
 
 #include "engine/aiComponent/behaviorComponent/behaviors/iCozmoBehavior.h"
 #include "coretech/common/engine/robotTimeStamp.h"
+#include "clad/types/salientPointTypes.h"
+
+#include <list>
 
 namespace Anki {
 namespace Vector {
@@ -45,8 +48,13 @@ private:
 
   struct InstanceConfig {
     InstanceConfig(const Json::Value& config);
+
+     //! Name of the eye color behavior ID
     std::string eyeColorBehaviorStr;
     ICozmoBehaviorPtr eyeColorBehavior;
+
+    //! Number of Hues. This is used to round SalientPoint BrightColors to he nearest hue
+    u32 numHues;
   };
 
   struct DynamicVariables {
@@ -60,6 +68,26 @@ private:
     //! The previous eye color to transition back to
     u32 prevEyeColor;
   };
+
+  /**
+   * @brief Round the input SalientPoints that are BrightColors to the nearest hue
+   * @see InstanceConfig::numHueSectors
+   */
+  void RoundToNearestHue(std::list<Vision::SalientPoint>& points);
+
+  /**
+   * @brief Find a salient point for the current color the behavior is interested
+   * @return
+   */
+  void FindCurrentColor();
+
+  /**
+   * @brief Find a salient point for a new color
+   * @details Find a salient point for a new color that the behavior has not interacted with in a while.
+   * @return true if point has been set to a new BrightColor SalientPoint, false otherwise
+   * @todo Replace this structure with returning a std::optional<Vision::SalientPoint>
+   */
+  bool FindNewColor(Vision::SalientPoint& point);
 
   InstanceConfig _iConfig;
   DynamicVariables _dVars;
