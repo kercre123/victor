09dd1431b38397e92b5918e6c4a8b7b745756980
Kevin Yoon <kevin@anki.com>
2018-10-26 20:19:13 -0700

    VIC-7960: Follow-up PR to make sure syscon is in calm mode while on charger to minimize battery overheating. Added DAS messages to maybe shed some light on strange toggling of HAL::PowerSetDesiredMode observed by LG.

--- robot/supervisor/src/powerModeManager.cpp
+++ robot/supervisor/src/powerModeManager.cpp
@@ -20,11 +20,11 @@ namespace {
   // Whether or not motors should be calibrated when transitioning from calm to active
   bool calibOnEnable_ = false;
 
-  // Whether or not we should go into calm mode because of inactivity
-  // for INACTIVITY_TIMEOUT_TO_CALM_MS
+  // Whether or not we should go into calm mode because on charger
+  // in order to prevent battery overheating
   bool autoDisable_   = false;
-  TimeStamp_t lastActivityTime_ms_ = HAL::GetTimeStamp();
-  const u32 INACTIVITY_TIMEOUT_TO_CALM_MS = 30 * 60 * 1000;
+  TimeStamp_t autoDisableOnChargerTime_ms_ = 0;
+  const u32 TIME_TO_AUTODISABLE_ON_CHARGER_MS = 10 * 1000;
 
 } // "private" members
 
@@ -51,12 +51,20 @@ void Update()
 
   const bool isHALActiveModeEnabled = HAL::PowerGetDesiredMode() == HAL::POWER_MODE_ACTIVE;
   
-  if (((enable_ && !autoDisable_) || shouldEnableInternally) && !isHALActiveModeEnabled) {
+  const bool shouldEnable = (enable_ && !autoDisable_) || shouldEnableInternally;
+  if (shouldEnable && !isHALActiveModeEnabled) {
     // Active mode should be enabled, but it's not.
     // Enable it!
     AnkiInfo("PowerModeManager.EnableActiveMode", 
              "enable: %d (%d), autoDisable: %d, calib: %d", 
              enable_, shouldEnableInternally, autoDisable_, calibOnEnable_);
+    DASMSG(power_mode_manager_enable_active_mode, "PowerModeManager.EnableActiveMode", "Enabling syscon active mode");
+    DASMSG_SET(i1, enable_, "Enable (external)");
+    DASMSG_SET(i2, shouldEnableInternally, "Should enable internally because of commanded motors");
+    DASMSG_SET(i3, autoDisable_, "Auto calm mode");
+    DASMSG_SET(i4, calibOnEnable_, "Calibrate on enable");
+    DASMSG_SEND();
+    
     HAL::PowerSetDesiredMode(HAL::POWER_MODE_ACTIVE);
 
     if (calibOnEnable_ && enable_) {
@@ -64,39 +72,46 @@ void Update()
       LiftController::StartCalibrationRoutine(true, EnumToString(MotorCalibrationReason::PowerSaveEnteringActiveMode));
       HeadController::StartCalibrationRoutine(true, EnumToString(MotorCalibrationReason::PowerSaveEnteringActiveMode));
     }
-  } else if ((!enable_ || autoDisable_) && !shouldEnableInternally && isHALActiveModeEnabled) {
+  } else if (!shouldEnable && isHALActiveModeEnabled) {
+
     AnkiInfo("PowerModeManager.DisableActiveMode", 
              "enable: %d (%d), autoDisable: %d", 
              enable_, shouldEnableInternally, autoDisable_);
+    DASMSG(power_mode_manager_disable_active_mode, "PowerModeManager.DisableActiveMode", "Disabling syscon active mode");
+    DASMSG_SET(i1, enable_, "Enable (external)");
+    DASMSG_SET(i2, shouldEnableInternally, "Should enable internally because of commanded motors");
+    DASMSG_SET(i3, autoDisable_, "Auto calm mode");
+    DASMSG_SET(i4, calibOnEnable_, "Calibrate on enable");
+    DASMSG_SEND();
     HAL::PowerSetDesiredMode(HAL::POWER_MODE_CALM);
 
     if (autoDisable_ && enable_) {
-      AnkiWarn("PowerModeManager.AutoCalmMode", "This shouldn't be happening outside of DevDoNothing!");
-      DASMSG(power_mode_manager_auto_calm_mode, "power_mode_manager.auto_calm_mode", "Automatically going into calm mode due to inactivity");
+      AnkiWarn("PowerModeManager.AutoCalmMode", "Going into calm mode because on charger for TIME_TO_AUTODISABLE_ON_CHARGER_MS");
+      DASMSG(power_mode_manager_auto_calm_mode, "power_mode_manager.auto_calm_mode", "Automatically going into calm mode due to on charger");
       DASMSG_SEND_WARNING();
     }
   }
 
   // Check for any activity that should clear, and reset the timeout for, autoDisable 
-  static bool wasPickedUp        = false;
-  const bool  isPickedUp         = IMUFilter::IsPickedUp();
-  const bool  pickupStateChanged = isPickedUp != wasPickedUp;
-              wasPickedUp        = isPickedUp;
-  static bool wasOnCharger = false;
-  const bool  isOnCharger        = HAL::BatteryIsOnCharger();
+  static bool wasOnCharger          = false;
+  const bool  isOnCharger           = HAL::BatteryIsOnCharger();
   const bool  onChargerStateChanged = isOnCharger != wasOnCharger;
-              wasOnCharger       = isOnCharger;
-  const bool  buttonPressed      = HAL::GetButtonState(HAL::BUTTON_POWER);
-  const bool  activityDetected   = shouldEnableInternally || buttonPressed || pickupStateChanged || onChargerStateChanged;
+              wasOnCharger          = isOnCharger;
 
   auto now_ms = HAL::GetTimeStamp();
-  if (activityDetected || !enable_) {
-    lastActivityTime_ms_ = now_ms;
-    autoDisable_ = false;
-    calibOnEnable_ = true;
-  } else if (now_ms - lastActivityTime_ms_ > INACTIVITY_TIMEOUT_TO_CALM_MS) {
-    autoDisable_ = true;
+  if (onChargerStateChanged) {
+    if (isOnCharger) {
+      autoDisableOnChargerTime_ms_ = now_ms + TIME_TO_AUTODISABLE_ON_CHARGER_MS;
+    } else {
+      autoDisableOnChargerTime_ms_ = 0;
+      autoDisable_ = false;
+    }
   }
+  if (autoDisableOnChargerTime_ms_ > 0 && now_ms > autoDisableOnChargerTime_ms_) {
+    autoDisable_ = true;
+    calibOnEnable_ = false;
+  }
+
 }
 
 
