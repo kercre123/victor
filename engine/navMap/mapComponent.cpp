b98a7435ff685fb946e80209800058a8d32a89d3
David Woods <dwoods@anki.com>
2018-10-29 11:04:50 -0700

    VIC-7883 Use REMOTE_CONSOLE_ENABLED. (#4090)

--- engine/navMap/mapComponent.cpp
+++ engine/navMap/mapComponent.cpp
@@ -265,14 +265,14 @@ decltype(auto) GetHabitatRegion(const Pose3d& poseWRTRoot)
 // console var utility for testing out Visual extending cliffs
 static Robot* consoleRobot = nullptr;
 
-#if ANKI_DEV_CHEATS
+#if REMOTE_CONSOLE_ENABLED 
 void DevProcessOneFrameForVisionEdges( ConsoleFunctionContextRef context )
 {
   if(consoleRobot != nullptr) {
     consoleRobot->GetActionList().QueueAction(QueueActionPosition::NOW, new WaitForImagesAction(1, VisionMode::DetectingOverheadEdges));
   }
 }
-#endif //ANKI_DEV_CHEATS
+#endif //REMOTE_CONSOLE_ENABLED
 CONSOLE_FUNC( DevProcessOneFrameForVisionEdges, "MapComponent.VisualEdgeDetection" );
 
 };
