d7964280b7fe58f65210f28fa6f5ac243c107335
Jarrod Hatfield <jarrod@anki.com>
2018-10-29 18:24:53 -0700

    [VIC-11172] Hook up React to being shaken (dizzy) animations
    [VIC-11172][React to Motion] Hook up React to being shaken (dizzy) animations
    
    + created .json for dizzy behavior and hooked it up into the behavior tree
    + added new dizzy anim groups to the trigger map
    + updated dizzy behavior to use new anims, which wasn’t a 1to1 swap so did a little bit of restructuring
    + added ability to config shake thresholds from .json
    + updated the behavior to use callbacks rather than waiting for delegation to complete within the update function
    + creagted a new “active feature” associated with this behavior
    + since I’ll be the one having to work on this in the future, re-formatted a bunch of things to make me happy

--- engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToRobotShaken.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToRobotShaken.cpp
@@ -16,165 +16,284 @@
 #include "engine/aiComponent/aiComponent.h"
 #include "engine/aiComponent/behaviorComponent/behaviorExternalInterface/beiRobotInfo.h"
 #include "engine/actions/animActions.h"
-#include "engine/actions/basicActions.h"
 
+#include "coretech/common/engine/jsonTools.h"
 #include "coretech/common/engine/utils/timer.h"
+
+
+#define LOG_CHANNEL "Behaviors"
 
 namespace Anki {
 namespace Vector {
 
 namespace{
-// Accelerometer magnitude threshold corresponding to "no longer shaking"
-const float kAccelMagnitudeShakingStoppedThreshold = 13000.f;
-// Dizzy factor thresholds for playing the soft, medium, or hard reactions
-const float kShakenDurationThresholdHard   = 5.0f;
-const float kShakenDurationThresholdMedium = 2.5f;
+  // json keys ...
+  // Accelerometer magnitude threshold corresponding to "no longer shaking"
+  const char* kKey_ShakeThresholdStop         = "shakeThresholdStop";
+  // Dizzy factor thresholds for playing the soft, medium, or hard reactions
+  const char* kKey_ShakeThresholdMedium       = "shakeThresholdMedium";
+  const char* kKey_ShakeThresholdHard         = "shakeThresholdHard";
+
+  const float kDefaultShakeThresholdStop      = 13000.f;
+  const float kDefaultShakeThresholdMedium    = 18000.f;
+  const float kDefaultShakeThresholdHard      = 20000.f;
+//  const float kShakenDurationThresholdMedium  = 2.5f;
+//  const float kShakenDurationThresholdHard    = 5.0f;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+BehaviorReactToRobotShaken::InstanceConfig::InstanceConfig() :
+  shakeThresholdStop( kDefaultShakeThresholdStop ),
+  shakeThresholdMedium( kDefaultShakeThresholdMedium ),
+  shakeThresholdHard( kDefaultShakeThresholdHard )
+{
 
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-BehaviorReactToRobotShaken::BehaviorReactToRobotShaken(const Json::Value& config)
-: ICozmoBehavior(config)
+BehaviorReactToRobotShaken::BehaviorReactToRobotShaken(const Json::Value& config) :
+  ICozmoBehavior( config )
 {
+  JsonTools::GetValueOptional( config, kKey_ShakeThresholdStop, _iVars.shakeThresholdStop );
+  JsonTools::GetValueOptional( config, kKey_ShakeThresholdMedium, _iVars.shakeThresholdMedium );
+  JsonTools::GetValueOptional( config, kKey_ShakeThresholdHard, _iVars.shakeThresholdHard );
 }
 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorReactToRobotShaken::GetBehaviorJsonKeys(std::set<const char*>& expectedKeys) const
+{
+  expectedKeys.insert( kKey_ShakeThresholdStop );
+  expectedKeys.insert( kKey_ShakeThresholdMedium );
+  expectedKeys.insert( kKey_ShakeThresholdHard );
+//  expectedKeys.insert( kKey_ShakeDurationMedium );
+//  expectedKeys.insert( kKey_ShakeDurationHard );
+}
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void BehaviorReactToRobotShaken::OnBehaviorActivated()
 {
-  // Reset variables:
-  _maxShakingAccelMag = 0.f;
-  _shakingStartedTime_s = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
-  _shakenDuration_s = 0.f;
-  _reactionPlayed = EReaction::None;
+  _dVars = DynamicVariables();
+  _dVars.shakeStartTime = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
+  _dVars.shakeMaxMagnitude = GetBEI().GetRobotInfo().GetHeadAccelMagnitudeFiltered();
 
-  // Start the animations:
-  DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::DEPRECATED_DizzyShakeLoop, 0));
-
-  // Kick off the state machine:
-  _state = EState::Shaking;
-
-
+  DelegateIfInControl( new TriggerAnimationAction( AnimationTrigger::ReactToShake_GetIn ),
+                       &BehaviorReactToRobotShaken::PlayNextShakeReactionLoop );
 }
 
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 void BehaviorReactToRobotShaken::BehaviorUpdate()
 {
-  if(!IsActivated()){
+  if ( !IsActivated() )
+  {
     return;
   }
 
   // Master state machine:
-  switch(_state) {
+  switch ( _dVars.state )
+  {
+    case EState::ShakeGetIn:
     case EState::Shaking:
     {
-      const auto& robotInfo = GetBEI().GetRobotInfo();
-      const float accMag = robotInfo.GetHeadAccelMagnitudeFiltered();
-      _maxShakingAccelMag = std::max(_maxShakingAccelMag, accMag);
+      const float accMag = GetBEI().GetRobotInfo().GetHeadAccelMagnitudeFiltered();
+      _dVars.shakeMaxMagnitude = std::max( _dVars.shakeMaxMagnitude, accMag );
 
       // Done shaking? Then transition to the next state.
-      if (accMag < kAccelMagnitudeShakingStoppedThreshold) {
-        // Now that shaking has ended, determine how long it lasted:
-        const float now_s = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
-        _shakenDuration_s = now_s - _shakingStartedTime_s;
-        _state = EState::DoneShaking;
+      if ( ( EState::Shaking == _dVars.state ) && ( accMag < _iVars.shakeThresholdStop ) )
+      {
+        TransitionToDoneShaking();
       }
 
       break;
     }
-    case EState::DoneShaking:
-    {
-      CancelDelegates(false);
-      CompoundActionSequential* action = new CompoundActionSequential({new TriggerAnimationAction(AnimationTrigger::DEPRECATED_DizzyShakeStop),
-                                                                       new TriggerAnimationAction(AnimationTrigger::DEPRECATED_DizzyStillPickedUp)});
-      DelegateIfInControl(action);
 
-      _state = EState::WaitTilOnTreads;
-
-      break;
-    }
-    case EState::WaitTilOnTreads:
-    {
-      // Wait until on treads or the animations that are playing finish (timeout).
-      if (GetBEI().GetOffTreadsState() == OffTreadsState::OnTreads) {
-        _state = EState::ActDizzy;
-      } else if (!IsControlDelegated()) {
-        // The "DizzyStillPickedUp" reaction played to completion, so log that as the played reaction:
-        _reactionPlayed = EReaction::StillPickedUp;
-        _state = EState::Finished;
-      }
-
-      break;
-    }
-    case EState::ActDizzy:
-    {
-      CancelDelegates(false);
-      // Play appropriate reaction based on duration of shaking:
-      if (_shakenDuration_s > kShakenDurationThresholdHard) {
-        DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::DEPRECATED_DizzyReactionHard));
-        _reactionPlayed = EReaction::Hard;
-      } else if (_shakenDuration_s > kShakenDurationThresholdMedium) {
-        DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::DEPRECATED_DizzyReactionMedium));
-        _reactionPlayed = EReaction::Medium;
-      } else {
-        DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::DEPRECATED_DizzyReactionSoft));
-        _reactionPlayed = EReaction::Soft;
-      }
-
-      _state = EState::Finished;
-      break;
-    }
-    case EState::Finished:
-    {
-      if (!IsControlDelegated()) {
-        // Done
-        BehaviorObjectiveAchieved(BehaviorObjective::ReactedToRobotShaken);
-        CancelSelf();
-        return;
-      }
-    }
     default:
       break;
   }
 }
 
-
-void BehaviorReactToRobotShaken::OnBehaviorDeactivated()
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorReactToRobotShaken::PlayNextShakeReactionLoop()
 {
-  // Log some DAS stuff:
-  const int shakenDuration_ms = std::round(_shakenDuration_s * 1000.f);
-  const int maxShakenAccelMag = std::round(_maxShakingAccelMag);
-  // DAS event string: "<shakenDuration_ms>:<maxShakenAccelMag>"
-  const std::string& data = std::to_string(shakenDuration_ms) + ":" + std::to_string(maxShakenAccelMag);
-  Anki::Util::sInfo("robot.dizzy_reaction",
-                    {{DDATA, data.c_str()}},
-                    EReactionToString(_reactionPlayed));
+  switch ( _dVars.state )
+  {
+    case EState::ShakeGetIn:
+      _dVars.state = EState::Shaking;
+      // ... fall through
+    case EState::Shaking:
+    {
+      // we retrigger this looping animation each time through so that we can adjust the "level" in case the shaking
+      // magnitude increases
+      const AnimationTrigger reactionAnim = GetReactionAnimation( EReactionAnimation::Loop );
+      DelegateIfInControl( new TriggerAnimationAction( reactionAnim ), &BehaviorReactToRobotShaken::PlayNextShakeReactionLoop );
 
-  // Log human-readable completion info:
-  PRINT_CH_INFO("Behaviors", "BehaviorReactToRobotShaken.DizzyReaction",
-                   "shakenDuration = %.3fs, maxShakingAccelMag = %.1f, reactionPlayed = '%s'",
-                   _shakenDuration_s,
-                   _maxShakingAccelMag,
-                   EReactionToString(_reactionPlayed));
+      break;
+    }
+
+    default:
+      DEV_ASSERT( false, "BehaviorReactToRobotShaken.PlayNextShakeReactionLoop.InvalidState" );
+      break;
+  }
 }
 
-
-const char* BehaviorReactToRobotShaken::EReactionToString(EReaction reaction) const
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorReactToRobotShaken::TransitionToDoneShaking()
 {
-  switch(reaction) {
-    case EReaction::None:
-      return "None";
-    case EReaction::Soft:
+  _dVars.state = EState::DoneShaking;
+  
+  // we're done shaking, cache the end time so we know how long we were shaking
+  _dVars.shakeEndTime = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
+
+  // cancel the looping animation
+  CancelDelegates( false );
+
+  auto getOutCallbcak = [this]()
+  {
+    // now that we've had time to transition out of the shake anim, check if we're on the ground or still in hand and
+    // play the appropriate animation
+    const bool isOnTreads = ( GetBEI().GetOffTreadsState() == OffTreadsState::OnTreads );
+    const EReactionAnimation reactionType = ( isOnTreads ? EReactionAnimation::OnGround : EReactionAnimation::InHand );
+    const AnimationTrigger reactionAnim = GetReactionAnimation( reactionType );
+    DelegateIfInControl( new TriggerAnimationAction( reactionAnim ) );
+  };
+
+  const AnimationTrigger reactionAnim = GetReactionAnimation( EReactionAnimation::ShakeTransition );
+  DelegateIfInControl( new TriggerAnimationAction( reactionAnim ), getOutCallbcak );
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void BehaviorReactToRobotShaken::OnBehaviorDeactivated()
+{
+  BehaviorObjectiveAchieved( BehaviorObjective::ReactedToRobotShaken );
+
+  // Log some DAS stuff:
+  const float shakeDuration_s = ( _dVars.shakeEndTime - _dVars.shakeStartTime );
+  const int shakenDuration_ms = std::round( shakeDuration_s * 1000.f );
+  const int maxShakenAccelMag = std::round( _dVars.shakeMaxMagnitude );
+
+  const EReactionLevel level = GetReactionLevelFromMagnitude();
+  const std::string levelString = EReactionLevelToString( level );
+
+  // DAS event string: "<shakenDuration_ms>:<maxShakenAccelMag>"
+  const std::string& data = std::to_string( shakenDuration_ms ) + ":" + std::to_string( maxShakenAccelMag );
+  Anki::Util::sInfo( "robot.dizzy_reaction",
+                     {{DDATA, data.c_str()}},
+                     levelString.c_str() );
+
+  // Log human-readable completion info:
+  LOG_DEBUG( "BehaviorReactToRobotShaken.DizzyReaction",
+             "shakenDuration = %.3fs, maxShakingAccelMag = %.1f, reactionPlayed = '%s'",
+             shakeDuration_s,
+             _dVars.shakeMaxMagnitude,
+             levelString.c_str() );
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+BehaviorReactToRobotShaken::EReactionLevel BehaviorReactToRobotShaken::GetReactionLevelFromMagnitude() const
+{
+  EReactionLevel reaction = EReactionLevel::Soft;
+
+  if ( _dVars.shakeMaxMagnitude >= _iVars.shakeThresholdHard )
+  {
+    reaction = EReactionLevel::Hard;
+  }
+  else if ( _dVars.shakeMaxMagnitude >= _iVars.shakeThresholdMedium )
+  {
+    reaction = EReactionLevel::Medium;
+  }
+
+  return reaction;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+//BehaviorReactToRobotShaken::EReactionLevel BehaviorReactToRobotShaken::GetReactionLevelFromDuration() const
+//{
+//  EReactionLevel reaction = EReactionLevel::Soft;
+//
+//  // if we haven't stopped shaking yet, use the current time for our duration
+//  const float currentTime = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
+//  const float shakeEndTime = ( _dVars.shakeEndTime > 0.f ) ? _dVars.shakeEndTime : currentTime;
+//  const float shakeDuration_s = ( shakeEndTime - _dVars.shakeStartTime );
+//
+//  if ( shakeDuration_s > kShakenDurationThresholdHard )
+//  {
+//    reaction = EReactionLevel::Hard;
+//  }
+//  else if ( shakeDuration_s > kShakenDurationThresholdMedium )
+//  {
+//    reaction = EReactionLevel::Medium;
+//  }
+//
+//  return reaction;
+//}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+AnimationTrigger BehaviorReactToRobotShaken::GetReactionAnimation( EReactionAnimation type ) const
+{
+  const EReactionLevel level = GetReactionLevelFromMagnitude();
+  return GetReactionAnimation( level, type );
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+AnimationTrigger BehaviorReactToRobotShaken::GetReactionAnimation( EReactionLevel level, EReactionAnimation type ) const
+{
+  // Static animations for reactions ...
+
+  static AnimationTrigger kLoopReactions[] =
+  {
+    AnimationTrigger::ReactToShake_Lvl1Loop,
+    AnimationTrigger::ReactToShake_Lvl2Loop,
+    AnimationTrigger::ReactToShake_Lvl3Loop,
+  };
+
+  static AnimationTrigger kWaitReactions[] =
+  {
+    AnimationTrigger::ReactToShake_Lvl1Waiting,
+    AnimationTrigger::ReactToShake_Lvl2Waiting,
+    AnimationTrigger::ReactToShake_Lvl3Waiting,
+  };
+
+  static AnimationTrigger kInHandReactions[] =
+  {
+    AnimationTrigger::ReactToShake_Lvl1InHand,
+    AnimationTrigger::ReactToShake_Lvl2InHand,
+    AnimationTrigger::ReactToShake_Lvl3InHand,
+  };
+
+  static AnimationTrigger kOnGroundReactions[] =
+  {
+    AnimationTrigger::ReactToShake_Lvl1OnGround,
+    AnimationTrigger::ReactToShake_Lvl2OnGround,
+    AnimationTrigger::ReactToShake_Lvl3OnGround,
+  };
+  
+  static AnimationTrigger* kReactionAnims[] =
+  {
+    kLoopReactions,
+    kWaitReactions,
+    kInHandReactions,
+    kOnGroundReactions,
+  };
+
+  DEV_ASSERT_MSG( ( (uint8_t)level < 3 ), "BehaviorReactToRobotShaken.GetReactionAnimation",
+                                          "EReactionLevel enum too large [%d]", (uint8_t)level );
+  DEV_ASSERT_MSG( ( (uint8_t)type < 4 ), "BehaviorReactToRobotShaken.GetReactionAnimation",
+                                         "EReactionAnimation enum too large [%d]", (uint8_t)type );
+
+  return kReactionAnims[static_cast<uint8_t>(type)][static_cast<uint8_t>(level)];
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const char* BehaviorReactToRobotShaken::EReactionLevelToString( EReactionLevel reaction) const
+{
+  switch ( reaction )
+  {
+    case EReactionLevel::Soft:
       return "Soft";
-    case EReaction::Medium:
+    case EReactionLevel::Medium:
       return "Medium";
-    case EReaction::Hard:
+    case EReactionLevel::Hard:
       return "Hard";
-    case EReaction::StillPickedUp:
-      return "StillPickedUp";
-    default:
-      return "";
   }
 }
 
