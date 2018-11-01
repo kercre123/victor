43d37f6c2a6672c6305481b28fc59016658c6680
Matt Michini <mmichini@anki.com>
2018-11-01 11:41:14 -0700

    More victor_log options (#4120)
    - victor_log by default now shows everything in /var/log/messages instead of just the most recent 10 lines
    - added options to purge logs, as well as show older zipped logs in addition to newest logs

--- project/victor/scripts/victor_log.sh
+++ project/victor/scripts/victor_log.sh
@@ -3,18 +3,36 @@
 set -e
 set -u
 
-params="$*"
-
 # Get the directory of this script
 SCRIPT_PATH=$(dirname $([ -L $0 ] && echo "$(dirname $0)/$(readlink -n $0)" || echo $0))
 SCRIPT_NAME=$(basename ${0})
-GIT=`which git`
-if [ -z $GIT ]
-then
-    echo git not found
-    exit 1
-fi
-TOPLEVEL=`$GIT rev-parse --show-toplevel`
+
+function usage() {
+  echo "$SCRIPT_NAME [OPTIONS]"
+  echo "  -h         print this message and exit"
+  echo "  -c         purge logs"
+  echo "  -a         show _all_ previous logs (not just most recent)"
+}
+
+PURGE_LOGS=0
+SHOW_ALL_LOGS=0
+
+while getopts "hca" opt; do
+  case $opt in
+    h)
+      usage
+      exit 1
+      ;;
+    c)
+      PURGE_LOGS=1
+      ;;
+    a)
+      SHOW_ALL_LOGS=1
+      ;;
+    *)
+      exit 1
+  esac
+done
 
 source ${SCRIPT_PATH}/victor_env.sh ""
 
@@ -22,4 +40,16 @@ source ${SCRIPT_PATH}/host_robot_ip_override.sh ""
 
 robot_set_host
 
-robot_sh /bin/tail -F /var/log/messages
+LOG_ZIP=/var/log/messages.1.gz
+
+if [ $PURGE_LOGS -eq 1 ]; then
+  robot_sh rm -rf $LOG_ZIP
+  robot_sh '> /var/log/messages'
+  exit $?
+fi
+
+if [ $SHOW_ALL_LOGS -eq 1 ]; then
+  robot_sh ls $LOG_ZIP 2>/dev/null && robot_sh zcat $LOG_ZIP
+fi
+
+robot_sh /bin/tail -F -n +1 /var/log/messages
