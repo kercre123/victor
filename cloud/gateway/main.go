114fa6ceeb66f7a7808003533f2f927683648e6b
David Woods <dwoods@anki.com>
2018-11-02 10:37:27 -0700

    Revert "VIC-9900 Rate limit the Gateway jdocs AppTokens reads (#4083)"
    This reverts commit 2e00a2c28848da9b2b209bb6e965c1a0422747ff.

--- cloud/gateway/main.go
+++ cloud/gateway/main.go
@@ -38,8 +38,6 @@ var (
 	signalHandler      chan os.Signal
 	demoKeyPair        *tls.Certificate
 	demoCertPool       *x509.CertPool
-	cloudCheckLimiter  *MultiLimiter
-	debugLogLimiter    *MultiLimiter
 	switchboardManager SwitchboardIpcManager
 	engineProtoManager EngineProtoIpcManager
 	tokenManager       ClientTokenManager
