b6c8488eb1da8df38ab0612fc53d3ca55ca4b37e
Stuart Eichert <seichert@anki.com>
2018-11-13 10:53:44 -0800

    VIC-11708 switchboard: Use sudo for diagnostics
    With the changes for VIC-11362, `vic-switchboard` no longer runs as
    root.  When it executes the `/anki/bin/diagnostics-logger` script as the
    `net:anki` user, the script fails at various points to create
    directories and gather information.  `/anki/bin/diagnostics-logger`
    needs root privilege to gather all of the diagnostic information.  With
    this change, `vic-switchboard` will use `sudo` to run
    `/anki/bin/diagnostics-logger`.  `/anki/bin/diagnostics-logger` is also
    modified to make sure that `net:anki` is the owner of the created
    files.  This allows `vic-switchboard` to read them back and send them to
    `mac-client`.
    
    (cherry picked from commit 9f1ea28758eade66391e13e900320f69f89321ab)
    Signed-off-by: Stuart Eichert <seichert@anki.com>

--- platform/switchboard/switchboardd/rtsHandlerV4.cpp
+++ platform/switchboard/switchboardd/rtsHandlerV4.cpp
@@ -618,7 +618,7 @@ void RtsHandlerV4::HandleRtsLogRequest(const Vector::ExternalComms::RtsConnectio
     return;
   }
 
-  int exitCode = ExecCommand({"/anki/bin/diagnostics-logger"});
+  int exitCode = ExecCommand({"sudo", "/anki/bin/diagnostics-logger"});
 
   std::vector<uint8_t> logBytes;
 
