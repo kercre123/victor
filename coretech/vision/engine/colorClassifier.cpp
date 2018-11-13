33e4c50293d0a74772a8cee5abb201b42a300683
Patrick Doran <patrick.doran@anki.com>
2018-11-13 10:03:29 -0800

    Moved classifier files

--- coretech/vision/engine/colorClassifier.cpp
+++ coretech/vision/engine/colorClassifier.cpp
@@ -1,63 +1,199 @@
 /**
- * File: colorClassifier.cpp
+ * File: myClassifier.cpp
  *
  * Author: Patrick Doran
- * Date: 11/22/2018
+ * Date: 2018-11-08
  */
 
-#include "coretech/vision/engine/colorClassifier.h"
-#include "coretech/vision/engine/image.h"
-#include "util/fileUtils/fileUtils.h"
+#include "coretech/vision/engine/myClassifier.h"
+#include "util/math/math.h"
 
-#include <unordered_map>
-#include <iterator>
+#include "json/json.h"
+
+#include <opencv2/core.hpp>
+
+#include <algorithm>
+#include <cmath>
+#include <random>
+
+
+// TODO: Remove
+#include <iostream>
 #include <fstream>
+#include <unordered_map>
 
 namespace Anki
 {
 namespace Vision
 {
 
-const ColorClassifier::Label ColorClassifier::UNKNOWN_LABEL = { "background", PixelRGB(0,0,0) };
-
+#if 0
 namespace
 {
-
-float MahalanobisDistanceSquared(const cv::Vec3f& sample, const cv::Vec3f& mean, const cv::Matx33f inv_cov)
+std::ostream& operator<<(std::ostream& os, const MyClassifier::LabelStats& stats)
 {
-  cv::Vec3f diff = sample-mean;
-  return (diff.t() * inv_cov * diff).val[0];
+  os<<"Mean:            "<<stats.mean<<std::endl;
+  os<<"Covariance: "<<std::endl<<stats.covariance<<std::endl;
+  os<<"Covariance_inv: "<<std::endl<<stats.covariance_inv<<std::endl;
+  os<<"Determinant:     "<<stats.determinant<<std::endl;
+  os<<"Determinant_log: "<<stats.determinant_log<<std::endl;
+  os<<"Eigenvalues:     "<<stats.eigenvalues<<std::endl;
+  os<<"Eigenvectors:    "<<std::endl<<stats.eigenvectors<<std::endl;
+  return os;
+}
+}
+#endif
+
+MyClassifier::MyClassifier ()
+{
+  Reset();
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-/**
- * @brief Struct for containing label location
- */
-struct LabelRegion
+MyClassifier::~MyClassifier ()
 {
-  std::string label;
-  Anki::Rectangle<s32> rectangle;
-};
+}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-/**
- * @brief Struct for containing stats about the label
- */
-struct LabelStats
+void MyClassifier::Reset()
 {
-  std::string label;
-  std::vector<cv::Vec3f> samples;
-  cv::Vec3f mean;
-  cv::Matx33f cov;
-  cv::Matx33f inv_cov;
-};
+  _stats.clear();
+  _dimensionality = 0;
+}
 
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-/**
- * @brief Load labels from a Labelme json file
- */
-bool LoadLabels(const std::string& filename, std::unordered_map<std::string,std::vector<LabelRegion>>& labels)
+bool MyClassifier::Train (const std::vector<cv::Mat>& samples)
+{
+  if (samples.empty())
+    return false;
+
+  _stats.clear();
+  _stats.resize(static_cast<std::vector<LabelStats>::size_type>(samples.size()));
+
+  // Sanity check sample matrices
+  _dimensionality = samples[0].cols;
+  for (auto index = 1; index < samples.size(); ++index){
+    assert(_dimensionality == samples[index].cols);
+  }
+
+  for (auto index = 0; index < samples.size(); ++index)
+  {
+    LabelStats& stats = _stats[index];
+    stats.samples = samples[index];
+    cv::calcCovarMatrix(stats.samples, stats.covariance, stats.mean, CV_COVAR_NORMAL | CV_COVAR_ROWS);
+    stats.covariance_inv = stats.covariance.inv();
+    stats.determinant = cv::determinant(stats.covariance);
+    stats.denominator = std::sqrt(std::pow(2*M_PI, _dimensionality));
+    stats.determinant_log = log(stats.determinant);
+    cv::eigen(stats.covariance, stats.eigenvalues, stats.eigenvectors);
+
+#if 0
+    std::cerr<<" ---- Stats["<<index<<"] ---- "<<std::endl;
+    std::cerr<<stats<<std::endl;
+#endif
+  }
+
+  return false;
+}
+
+void MyClassifier::Predict (const cv::Mat& samples, cv::Mat& labels) const
+{
+  for (auto index = 0; index < _stats.size(); ++index)
+  {
+    const LabelStats& stats = _stats[index];
+    bool denominatorNearZero = Util::IsNearZero(stats.denominator);
+    for (auto row = 0; row  < samples.rows; ++row)
+    {
+      float probability = 0.f;
+      if (!denominatorNearZero)
+      {
+        const cv::Mat& sample = samples.row(row);
+        cv::Mat num = (-0.5f * (sample - stats.mean).t() * stats.covariance_inv * (sample - stats.mean));
+        float numerator = std::exp(num.at<double>(0));
+        probability = numerator / stats.denominator;
+      }
+      labels.at<double>(row,index) = probability;
+    }
+  }
+}
+
+void MyClassifier::Classify (const cv::Mat& samples, cv::Mat& labelings, s32 unknown) const
+{
+  // Calculate the Mahalanobis distance to each cluster
+  cv::Mat distances = cv::Mat::ones(samples.rows, static_cast<int>(_stats.size()), CV_64FC1);
+  for (auto row = 0; row  < samples.rows; ++row)
+  {
+    const cv::Mat& sample = samples.row(row);
+    for (auto index = 0; index < _stats.size(); ++index)
+    {
+      const LabelStats& stats = _stats[index];
+      cv::Mat tmp = ((sample - stats.mean) * stats.covariance_inv * (sample - stats.mean).t());
+      float distance = tmp.at<double>(0);
+      distances.at<double>(row,index) = distance;
+    }
+  }
+
+  // TODO: Make this be a  threshold computed from N where N means "N standard deviations away"
+  float threshold = 0.000002f; // (distance^2)
+
+  cv::Mat minDistances = cv::Mat::zeros(labelings.rows,labelings.cols,CV_64FC1);
+  cv::Mat maxDistances = cv::Mat::zeros(labelings.rows,labelings.cols,CV_64FC1);
+
+  for (auto row = 0; row < distances.rows; ++row)
+  {
+    const cv::Mat& distance = distances.row(row);
+
+    double minVal;
+    double maxVal;
+    cv::Point minLoc;
+    cv::Point maxLoc;
+    cv::minMaxLoc(distance, &minVal, &maxVal, &minLoc, &maxLoc);
+    minDistances.at<double>(row) = minVal;
+    maxDistances.at<double>(row) = maxVal;
+    // Minimum below threshold, useful if trying to find the minimum distance
+    if (minVal < threshold){
+      labelings.at<s32>(row) = minLoc.x;
+    } else {
+      labelings.at<s32>(row) = unknown;
+    }
+  }
+}
+
+void MyClassifier::Save (const std::string& filename)
+{
+  Json::Value root(Json::ValueType::objectValue);
+  Json::Value clusters(Json::ValueType::arrayValue);
+  for (auto label = 0; label < _stats.size(); ++label)
+  {
+    const LabelStats& stats = _stats[label];
+    Json::Value mean(Json::ValueType::arrayValue);
+    for (auto index = 0; index < stats.mean.cols; ++index)
+    {
+      mean.append(Json::Value(stats.mean.at<double>(index)));
+    }
+    Json::Value covariance(Json::ValueType::objectValue);
+    covariance["rows"] = Json::Value(stats.covariance.rows);
+    covariance["cols"] = Json::Value(stats.covariance.cols);
+    {
+      Json::Value data(Json::ValueType::arrayValue);
+      for (auto index = 0; index < stats.covariance.rows*stats.covariance.cols; ++index)
+      {
+        covariance["data"].append(Json::Value(stats.covariance.at<double>(index)));
+      }
+    }
+    Json::Value cluster(Json::ValueType::objectValue);
+    cluster["mean"] = mean;
+    cluster["covariance"] = covariance;
+
+    clusters.append(cluster);
+  }
+
+  root["dimensionality"] = Json::Value(_dimensionality);
+  root["clusters"] = clusters;
+
+  std::ofstream os(filename);
+  Json::StyledWriter writer;
+  os << writer.write(root);
+}
+
+void MyClassifier::Load (const std::string& filename)
 {
   Json::CharReaderBuilder rbuilder;
   rbuilder["collectComments"] = false;
@@ -67,263 +203,89 @@ bool LoadLabels(const std::string& filename, std::unordered_map<std::string,std:
 
   if (!Json::parseFromStream(rbuilder, is, &root, &errs))
   {
-    return false;
+    return; // fail
   }
 
-  Json::Value shapes = root.get("shapes",Json::Value::null);
-  if (shapes.isNull() || !shapes.isArray())
+  Json::Value jsonClusters = root.get("clusters",Json::Value::null);
+  if (jsonClusters.isNull() || jsonClusters.type() != Json::ValueType::arrayValue)
   {
-    return false;
+    return; // fail;
   }
 
-  for (auto ii = 0; ii < shapes.size(); ++ii)
+  Json::Value jsonDimensionality = root.get("dimensionality",Json::Value::null);
+  if (jsonDimensionality.isNull() || jsonDimensionality.type() != Json::ValueType::arrayValue)
   {
-    LabelRegion newLabel;
-
-    Json::Value shape = shapes[ii];
-
-    Json::Value type = shape.get("shape_type",Json::Value::null);
-    if (type.isNull() || !type.isString() || type.asString() != "rectangle")
-    {
-      continue;
-    }
-
-    Json::Value label = shape.get("label",Json::Value::null);
-    if (label.isNull() || !label.isString())
-    {
-      continue;
-    }
-    newLabel.label = label.asString();
-
-    Json::Value points = shape.get("points",Json::Value::null);
-    if (points.isNull() || !points.isArray() || points.size() != 2)
-    {
-      continue;
-    }
-    Json::Value p0 = points[0];
-    Json::Value p1 = points[1];
-    if (!p0.isArray() || p0.size() != 2 || !p1.isArray() || p1.size() != 2)
-    {
-      continue;
-    }
-
-    float x0 = p0[0].asInt();
-    float y0 = p0[1].asInt();
-    float x1 = p1[0].asInt();
-    float y1 = p1[1].asInt();
-
-    newLabel.rectangle = Anki::Rectangle<s32>(x0, y0, x1-x0, y1-y0);
-
-    auto iter = labels.find(newLabel.label);
-    if (iter == labels.end())
-    {
-      labels.emplace(newLabel.label, std::vector<LabelRegion>());
-    }
-    labels[newLabel.label].push_back(newLabel);
+    return; // fail;
   }
+  s32 dimensionality = jsonDimensionality.asInt();
 
-  return true;
-}
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-/**
- * @brief Get the samples from the image within the region
- */
-void GetSamples(const ImageRGB& image, LabelRegion& region, LabelStats& stats)
-{
-  ImageRGB subimage = image.GetROI(region.rectangle);
-  for (int row = 0; row < subimage.GetNumRows(); ++row)
+
+  std::vector<LabelStats> allStats;
+
+  for (const Json::Value& jsonCluster : jsonClusters)
   {
-    for (int col = 0; col < subimage.GetNumCols(); ++col)
+    LabelStats stats;
+
+    Json::Value jsonMean = jsonCluster.get("mean",Json::Value::null);
+    Json::Value jsonCovariance = jsonCluster.get("covariance",Json::Value::null);
+
+    if (jsonMean.isNull() || jsonMean.type() != Json::ValueType::arrayValue)
     {
-      PixelRGB& pixel = subimage(row,col);
-      cv::Vec3f rgb(pixel.r() / 255.f, pixel.g() / 255.f, pixel.b() / 255.f);
-      stats.samples.push_back(rgb);
+      return; // fail
     }
+    if (jsonCovariance.isNull() || jsonCovariance.type() != Json::ValueType::objectValue)
+    {
+      return; // fail
+    }
+    Json::Value jsonRows = jsonCovariance.get("rows",Json::Value::null);
+    if (jsonRows.isNull() || jsonRows.isInt())
+    {
+      return; //fail
+    }
+    int rows = jsonRows.asInt();
+
+    Json::Value jsonCols = jsonCovariance.get("cols",Json::Value::null);
+    if (jsonCols.isNull() || jsonCols.isInt())
+    {
+      return; //fail
+    }
+    int cols = jsonCols.asInt();
+
+    Json::Value jsonData = jsonCovariance.get("rows",Json::Value::null);
+    if (jsonData.isNull() || jsonData.type() != Json::ValueType::arrayValue || jsonData.size() != rows*cols)
+    {
+      return; // fail
+    }
+
+    for (const Json::Value& value : jsonMean)
+    {
+      stats.mean.push_back(value.asDouble());
+    }
+
+    for (const Json::Value& value : jsonCovariance)
+    {
+      stats.covariance.push_back(value.asDouble());
+    }
+
+    stats.mean.reshape(1,{1,cols});
+    stats.covariance.reshape(1,{rows,cols});
+
+    allStats.push_back(stats);
   }
-}
 
-void FillOutStats(LabelStats& stats)
-{
-  cv::Mat covar, mean;
-  int flags = CV_COVAR_NORMAL | CV_COVAR_ROWS;
-  int ctype = CV_32FC1;
-  // TODO figure out why the channels don't work
-  cv::calcCovarMatrix(cv::Mat((int)stats.samples.size(),3,CV_32F,stats.samples.data()), covar,mean,flags, ctype);
+  // Compute the secondary statistical information
+  for (LabelStats& stats : allStats)
+  {
+    stats.covariance_inv = stats.covariance.inv();
+    stats.determinant = cv::determinant(stats.covariance);
+    stats.denominator = std::sqrt(std::pow(2*M_PI, dimensionality));
+    stats.determinant_log = log(stats.determinant);
+    cv::eigen(stats.covariance, stats.eigenvalues, stats.eigenvectors);
+  }
 
-  stats.mean = mean;
-  stats.cov = covar;
-  stats.inv_cov = stats.cov.inv();
-
-  cv::Mat w,u,vt;
-  cv::SVD::compute(covar, w, u, vt, cv::SVD::FULL_UV);
-//  std::cerr<<"w: "<<std::endl<<w<<std::endl;
-//  std::cerr<<"u: "<<std::endl<<u<<std::endl;
-//  std::cerr<<"vt: "<<std::endl<<vt<<std::endl;
-}
-
-} /* anonymous namespace */
-
-ColorClassifier::ColorClassifier ()
-{
   Reset();
-}
-
-ColorClassifier::~ColorClassifier ()
-{
-}
-
-void ColorClassifier::Classify(const ImageRGB& image, Image& labels) const
-{
-  DEV_ASSERT(_trained,"Cannot classify, not trained");
-
-  for (int row = 0; row < labels.GetNumRows(); ++row)
-  {
-    for (int col = 0; col < labels.GetNumCols(); ++col)
-    {
-      labels.get_CvMat_()(row,col) = Classify(image.get_CvMat_()(row,col));
-    }
-  }
-}
-
-u8 ColorClassifier::Classify (PixelRGB color) const
-{
-  std::vector<LabelStats> statistics;
-  std::vector<float> distances(statistics.size());
-  float threshold = 2.f; // Mahalanobis distance threshold. How many standard deviations away.
-  threshold *= threshold; // Square the threshold because we're doing this with the distance squared
-
-  for (auto i = 0; i < statistics.size(); ++i)
-  {
-    LabelStats& stats = statistics[i];
-    cv::Vec3f rgb(color.r()/255.f, color.g()/255.f, color.b()/255.f);
-    distances[i] = MahalanobisDistanceSquared(rgb,stats.mean,stats.inv_cov);
-  }
-
-  // The index is of the min_element
-  auto iter = std::min_element(distances.begin(), distances.end());
-  if (iter == distances.end())
-    return UNKNOWN_INDEX;
-  u8 index = static_cast<u8>(std::distance(distances.begin(), iter));
-  if (distances[index] > threshold)
-    return UNKNOWN_INDEX;
-  else
-    return index;
-}
-
-void ColorClassifier::ColorImage (const Image& labels, ImageRGB& image) const
-{
-  DEV_ASSERT(_trained,"Cannot classify, not trained");
-
-  for (int row = 0; row < labels.GetNumRows(); ++row)
-  {
-    for (int col = 0; col < labels.GetNumCols(); ++col)
-    {
-      std::vector<Label>::size_type index = labels.get_CvMat_()(row,col);
-      image.get_CvMat_()(row,col) = _labels.at(index).color;
-    }
-  }
-}
-
-ColorClassifier::Label ColorClassifier::GetLabel(u8 index) const
-{
-  DEV_ASSERT(index == UNKNOWN_INDEX || (index >= 0 && index < _labels.size()), "Invalid index");
-
-  if (index == UNKNOWN_INDEX)
-    return UNKNOWN_LABEL;
-
-  return _labels[index];
-}
-
-void ColorClassifier::Reset()
-{
-  _labels.clear();
-  _trained = false;
-}
-
-void ColorClassifier::Train (const std::string& directory)
-{
-  Reset();
-
-  // Load the image and annotation files from the directory
-  std::vector<std::string> imageFilenames;
-  std::vector<std::string> labelFilenames;
-  {
-    // TODO: Better file checking
-    bool useFullPath = true;
-    bool recursive = true;
-    imageFilenames = Util::FileUtils::FilesInDirectory(directory, useFullPath, "jpg", recursive);
-    labelFilenames = Anki::Util::FileUtils::FilesInDirectory(directory, useFullPath, "json", recursive);
-    std::sort(imageFilenames.begin(), imageFilenames.end());
-    std::sort(labelFilenames.begin(), labelFilenames.end());
-
-    // TODO: Do a better check that the filenames match the images
-    DEV_ASSERT((int)imageFilenames.size() == (int)labelFilenames.size(),
-               "Not all images in directory have label files");
-  }
-
-  std::unordered_map<std::string, LabelStats> inputImageStats;
-  ImageRGB inputImage;
-  for (auto index = 0; index < imageFilenames.size(); ++index)
-  {
-    const std::string& imageFilename = imageFilenames[index];
-    const std::string& labelFilename = labelFilenames[index];
-
-    DEV_ASSERT(inputImage.Load(imageFilename) == Anki::Result::RESULT_OK, "Could not load file");
-
-    std::unordered_map<std::string,std::vector<LabelRegion>> labels;
-
-    bool loaded = LoadLabels(labelFilename, labels);
-    if (!loaded || labels.empty())
-    {
-      // Skip the ones without labels
-      continue;
-    }
-
-    // Get the samples for all the label regions
-    for (auto& kv : labels)
-    {
-      const std::string& label = kv.first;
-      std::vector<LabelRegion>& regions = kv.second;
-
-      auto iter = inputImageStats.find(label);
-      if (iter == inputImageStats.end())
-      {
-        LabelStats stats;
-        stats.label = label;
-        inputImageStats.emplace(label,stats);
-      }
-
-      for (auto& region : regions)
-      {
-        GetSamples(inputImage,region,inputImageStats[label]);
-      }
-    }
-  }
-
-  std::vector<std::string> labels;
-  std::transform(inputImageStats.begin(), inputImageStats.end(), std::back_inserter(labels),
-                 [](const auto& pair){ return pair.first; });
-
-  for (const auto& label : labels)
-  {
-    // Input Image Stats
-    {
-      LabelStats& stats = inputImageStats.at(label);
-      FillOutStats(stats);
-
-    }
-  }
-
-}
-
-void ColorClassifier::Save (const std::string& path) const
-{
-
-}
-
-void ColorClassifier::Load (const std::string& path){
-  Reset();
-
+  _stats = allStats;
+  _dimensionality = dimensionality;
 }
 
 } /* namespace Vision */
--- coretech/vision/engine/colorClassifier.h
+++ coretech/vision/engine/colorClassifier.h
@@ -1,58 +1,81 @@
 /**
- * File: colorClassifier.h
+ * File: myClassifier.h
  *
  * Author: Patrick Doran
- * Date: 11/22/2018
+ * Date: 2018-11-08
  */
 
-#ifndef __Anki_Vision_ColorClassifier_H__
-#define __Anki_Vision_ColorClassifier_H__
+#ifndef __Anki_Vision_MyClassifier_H__
+#define __Anki_Vision_MyClassifier_H__
 
-#include <string>
 #include "coretech/common/shared/types.h"
-#include "coretech/vision/engine/colorPixelTypes.h"
+
+#include <vector>
+#include <opencv2/core/mat.hpp>
 
 namespace Anki
 {
 namespace Vision
 {
 
-class ImageRGB;
-class Image;
-
-class ColorClassifier
+class MyClassifier
 {
 public:
-  struct Label
-  {
-    std::string label;
-    PixelRGB color;
-  };
+  MyClassifier ();
+  virtual ~MyClassifier ();
 
-  static const Label UNKNOWN_LABEL;
-  static const u8 UNKNOWN_INDEX = -1; // u8 max value
+  /**
+   * @brief Train a model based on samples and labels
+   * @details Train a model based on the samples and labels. Samples are expected to be NxM where N is the number of
+   * samples and M is the dimensionality of the feature. Labels is expected to be Nx1 applying the label index to the
+   * given sample.
+   * @param[in] samples - Vector filled with NxM feature vector matrix for each class K
+   * @return True if training completed.
+   */
+  bool Train(const std::vector<cv::Mat>& samples);
 
-  ColorClassifier ();
-  ~ColorClassifier ();
+  /**
+   * @brief Predict the probability that a sample belongs to a class
+   * @param[in] samples - NxM matrix of samples
+   * @param[out] labels - Output NxK matrix (float) of probabilities where K is the number of classes and
+   */
+  void Predict(const cv::Mat& samples, cv::Mat& labels) const;
 
-  void Classify(const ImageRGB& image, Image& labels) const;
-  void ColorImage(const Image& labels, ImageRGB& image) const;
-  Label GetLabel(u8 index) const;
-  inline std::vector<Label> GetLabels() const { return _labels; }
+  /**
+   * @brief Label a sample as belonging to a class
+   * @param samples - NxM matrix of samples; N is the number of samples and M is the length of the feature
+   * @param labels - Nx1 matrix (int) of labels for each sample
+   * @param unknown - Label id for samples belonging to no class
+   */
+  void Classify(const cv::Mat& samples, cv::Mat& labels, s32 unknown = -1) const;
 
-  void Train(const std::string& directory);
-  void Save(const std::string& path) const;
-  void Load(const std::string& path);
+  void Save(const std::string& filename);
+  void Load(const std::string& filename);
 
 private:
-  u8 Classify(PixelRGB color) const;
+  struct LabelStats
+  {
+    cv::Mat samples;
+    cv::Mat mean;
+    cv::Mat covariance;
+    cv::Mat covariance_inv;
+    float determinant;
+    float determinant_log;
+    cv::Mat eigenvectors;
+    cv::Mat eigenvalues;
+
+    //! Denominator for multivariate normal distribution calculation
+    float denominator;
+  };
+
   void Reset();
 
-  std::vector<Label> _labels;
-  bool _trained;
+  std::vector<LabelStats> _stats;
+  s32 _dimensionality;
+
 };
 
 } /* namespace Vision */
 } /* namespace Anki */
 
-#endif /* __Anki_Vision_ColorClassifier_H__ */
+#endif /* __Anki_Vision_MyClassifier_H__ */
