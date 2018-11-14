d4d81ae72357dcf28b669b1dc1d91939a4ada7e1
Brad Neuman <brad@anki.com>
2018-11-14 00:39:18 -0800

    Add script to start lldb to debug the anim process in webots

---
+++ tools/debug/autoDebugAnim.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+while true; do
+    lldb $DIR/../../simulator/controllers/webotsCtrlAnim/webotsCtrlAnim -s $DIR/autoDebugAnim.lldb
+    # sleep for a couple seconds so the user has time to hit ctrl+c
+    echo "restarting lldb in 2 seconds, hit ctrl+c now to abort"
+    sleep 2
+done
