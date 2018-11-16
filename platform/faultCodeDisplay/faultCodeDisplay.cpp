92fc680447e6782bced12fe9b66881bfd9a5d890
David C Mudie <dmudie@anki.com>
2018-11-15 21:06:26 -0800

    VIC-11309: Revise vic-faultCodeDisplay to display alternate text (#4315)
    1. Simplify CMakeLists.txt. Use BUILD.in and standard build macros.
    
    2. Revise vic-faultCodeDisplay to support "-r" (restart) argument.
    
    This argument will be passed by fault-code-handler to display alternate text.

--- platform/faultCodeDisplay/faultCodeDisplay.cpp
+++ platform/faultCodeDisplay/faultCodeDisplay.cpp
@@ -17,14 +17,16 @@
 
 #include "opencv2/highgui.hpp"
 
+#include <getopt.h>
 #include <inttypes.h>
 #include <unordered_map>
 
 namespace Anki {
 namespace Vector {
-   
+
 namespace {
-  static const std::string kFaultURL = "support.anki.com";
+  constexpr const char * kSupportURL = "support.anki.com";
+  constexpr const char * kVectorWillRestart = "Vector will restart";
 
   // Map of fault codes that map to images that should be drawn instead of the number
   std::unordered_map<uint16_t, std::string> kFaultImageMap = {
@@ -32,7 +34,7 @@ namespace {
   };
 }
 
-void DrawFaultCode(uint16_t fault)
+void DrawFaultCode(uint16_t fault, bool willRestart)
 {
   // Image in which the fault code is drawn
   static Vision::ImageRGB img(FACE_DISPLAY_HEIGHT, FACE_DISPLAY_WIDTH);
@@ -50,16 +52,17 @@ void DrawFaultCode(uint16_t fault)
 				   (FACE_DISPLAY_HEIGHT/2 + size.y()/4),
 				   false);
 
-  // Draw fault URL centered horizontally and slightly above
+  // Draw text centered horizontally and slightly above
   // the bottom of the screen
-  size = Vision::Image::GetTextSize(kFaultURL, 0.5, 1);
-  img.DrawTextCenteredHorizontally(kFaultURL,
-				   CV_FONT_NORMAL,
-				   0.5,
-				   1,
-				   NamedColors::WHITE,
-				   FACE_DISPLAY_HEIGHT - size.y(),
-				   false);
+  const std::string & text = (willRestart ? kVectorWillRestart : kSupportURL);
+  const int font = CV_FONT_NORMAL;
+  const f32 scale = 0.5f;
+  const int thickness = 1;
+  const auto color = NamedColors::WHITE;
+  const bool drawTwice = false;
+
+  size = Vision::Image::GetTextSize(text, scale, thickness);
+  img.DrawTextCenteredHorizontally(text, font, scale, thickness, color, FACE_DISPLAY_HEIGHT - size.y(), drawTwice);
 
   Vision::ImageRGB565 img565(img);
   lcd_draw_frame2(reinterpret_cast<u16*>(img565.GetDataPointer()), img565.GetNumRows() * img565.GetNumCols() * sizeof(u16));
@@ -89,17 +92,59 @@ extern "C" void core_common_on_exit(void)
   // Don't shutdown the lcd in order to keep the fault code displayed
   //lcd_shutdown();
 }
-  
+
+void usage(FILE * f)
+{
+  fprintf(f, "Usage: vic-faultCodeDisplay [-hr] nnn\n");
+}
+
 int main(int argc, char * argv[])
 {
   using namespace Anki::Vector;
-  
-  char* end;
-  // Convert first argument from a string to a uint16_t
-  auto res = strtoumax(argv[1], &end, 10);
-  if (res > std::numeric_limits<uint16_t>::max() || res == 0) {
+
+  bool willRestart = false;
+  char c;
+
+  // Process any options
+  while ((c = getopt (argc, argv, "hr")) != -1)
+  {
+    switch (c) {
+      case 'h':
+        usage(stdout);
+        return 0;
+      case 'r':
+        willRestart = true;
+        break;
+      case '?':
+      default:
+        usage(stderr);
+        return -1;
+    }
+  }
+
+  // Process remaining arguments
+  const char * s = nullptr;
+  for (int index = optind; index < argc; ++index) {
+    if (s != nullptr) {
+      usage(stderr);
+      return -1;
+    }
+    s = argv[index];
+  }
+
+  // Validate argument
+  if (s == nullptr) {
+    usage(stderr);
     return -1;
   }
+
+  // Convert first argument from a string to a uint16_t
+  auto res = strtoumax(s, nullptr, 10);
+  if (res > std::numeric_limits<uint16_t>::max() || res == 0) {
+    usage(stderr);
+    return -1;
+  }
+
   uint16_t code = (uint16_t)res;
 
   lcd_init();
@@ -109,11 +154,11 @@ int main(int argc, char * argv[])
   auto faultImageIt = kFaultImageMap.find(code);
   if (faultImageIt != kFaultImageMap.end()) {
     imageDrawn = DrawImage(faultImageIt->second);
-  } 
-  
+  }
+
   // Draw fault code if no image to draw or DrawImage() failed
   if (!imageDrawn) {
-    DrawFaultCode(code);
+    DrawFaultCode(code, willRestart);
   }
 
   // Don't shutdown the lcd in order to keep the fault code displayed
