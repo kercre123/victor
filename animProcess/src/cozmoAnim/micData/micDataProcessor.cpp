00607e665e85a07072226b3c1c2adc68d5eadce5
Ross Anderson <ross@anki.com>
2018-12-04 21:55:31 -0800

    Revert notch stuff in RC (#4530)
    * Revert "Fix shipping build (#4499)"
    
    This reverts commit 685ed9eddb917be0dd9450a2b0a69dc42e11dc5a.
    
    * Revert "VIC-11866 VIC-11845 Detect notch for alexa loop"
    
    This reverts commit d08a90085cf959e3cee23d1c806b11933dc7c910.

--- animProcess/src/cozmoAnim/micData/micDataProcessor.cpp
+++ animProcess/src/cozmoAnim/micData/micDataProcessor.cpp
@@ -735,8 +735,6 @@ void MicDataProcessor::ProcessRawLoop()
         job->CollectRawAudio(audioChunk, kRawAudioChunkSize);
       }
       
-      _speechRecognizerSystem->UpdateRaw(audioChunk, kRawAudioChunkSize);
-      
       // Factory test doesn't need to do any mic processing, it just uses raw data
       if(!FACTORY_TEST)
       {
--- animProcess/src/cozmoAnim/micData/micDataSystem.cpp
+++ animProcess/src/cozmoAnim/micData/micDataSystem.cpp
@@ -764,9 +764,6 @@ void MicDataSystem::SetAlexaState(AlexaSimpleState state)
     // Disable "Alexa" wake word in SpeechRecognizerSystem
     _speechRecognizerSystem->DisableAlexa();
   }
-  
-  const bool active = (_alexaState == AlexaSimpleState::Active);
-  _speechRecognizerSystem->ToggleNotchDetector( active );
 }
   
 void MicDataSystem::ToggleMicMute()
