281e6924be0325207ea1d355e3c13fea3113364d
robbula <rjc5030@gmail.com>
2018-12-11 17:02:38 -0800

    VIC-11274 initial look it face behavior (#4469)
    React To Gaze behaviors. These are behind a feature gate.

--- engine/aiComponent/behaviorComponent/behaviorFactory.cpp
+++ engine/aiComponent/behaviorComponent/behaviorFactory.cpp
@@ -74,6 +74,7 @@
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorPowerSaveStressTest.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorPowerSaveTest.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorReactToBody.h"
+#include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/behaviorReactToGazeDirection.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/playpen/behaviorPlaypenCameraCalibration.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/playpen/behaviorPlaypenDistanceSensor.h"
 #include "engine/aiComponent/behaviorComponent/behaviors/devBehaviors/playpen/behaviorPlaypenDriftCheck.h"
@@ -573,6 +574,12 @@ ICozmoBehaviorPtr BehaviorFactory::CreateBehavior(const Json::Value& config)
     case BehaviorClass::ReactToBody:
     {
       newBehavior = ICozmoBehaviorPtr(new BehaviorReactToBody(config));
+      break;
+    }
+
+    case BehaviorClass::ReactToGazeDirection:
+    {
+      newBehavior = ICozmoBehaviorPtr(new BehaviorReactToGazeDirection(config));
       break;
     }
     
