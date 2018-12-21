02d811a73a0d68ff56e94285a362d8cf4109eab8
Andrew Stein <andrew@anki.com>
2018-12-20 18:29:55 -0800

    Hack in some testing stuff for offboard vision * Adds VisionMode::OffboardVision, hooked to temporary "test_offboard_model", defined in vision_config.json * Start to flesh out either FileIO or CLAD "comms" methods for offboard models (FileIO is the original approach) * Create new Vision::OffboardImageReady CLAD message to eventually talk to new cloud-vision process and tell it a saved file's whereabouts * Adds LocalUDP client to the offboard model to use for CLAD comms (very much WIP) * Adds testSalientPoint.json file which is handy for manually creating a "result" to be picked up by the OffboardModel

--- coretech/neuralnets/neuralNetModel_offboard.h
+++ coretech/neuralnets/neuralNetModel_offboard.h
@@ -14,7 +14,11 @@
 #ifndef __Anki_NeuralNets_OffboardModel_H__
 #define __Anki_NeuralNets_OffboardModel_H__
 
+#include "clad/types/offboardVision.h"
 #include "coretech/neuralnets/neuralNetModel_interface.h"
+
+// Forward declaration
+class LocalUdpClient;
 
 namespace Anki {
 namespace NeuralNets {
@@ -29,18 +33,27 @@ public:
   
   virtual Result Detect(Vision::ImageRGB& img, std::list<Vision::SalientPoint>& salientPoints) override;
   
-  bool IsVerbose() const { return _isVerbose; }
-  
 protected:
   
   virtual Result LoadModelInternal(const std::string& modelPath, const Json::Value& config) override;
   
 private:
   
+  Result DetectWithFileIO(const Vision::ImageRGB& img, std::list<Vision::SalientPoint>& salientPoints);
+  
+  Result DetectWithCLAD(const Vision::ImageRGB& img, std::list<Vision::SalientPoint>& salientPoints);
+  
+  bool WaitForResultFile(const std::string& resultFilename);
+  bool WaitForResultCLAD();
+  
   std::string _cachePath;
   int         _pollPeriod_ms;
   bool        _isVerbose = false;
   float       _timeoutDuration_sec = 10.f;
+  Vision::OffboardCommsType _commsType = Vision::OffboardCommsType::FileIO;
+  
+  // For non-FileIO comms
+  std::unique_ptr<LocalUdpClient> _udpClient;
   
 }; // class Model
 
