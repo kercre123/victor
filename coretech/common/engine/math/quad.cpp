15af33f77e131306a841d52d0c211206222e8353
David C Mudie <dmudie@anki.com>
2019-01-09 13:26:28 -0800

    Tech Debt: Enable -Wunused-variables for all configurations (#4748)
    Some compilers (eg clang 10.0) enable -Wunused-variables for release builds but not debug builds.
    This change makes -Wunused-variables explicitly enabled for all configurations.
    
    Review with 'w=1' to hide whitespace changes.

--- coretech/common/engine/math/quad.cpp
+++ coretech/common/engine/math/quad.cpp
@@ -1,9 +1,9 @@
 #include "coretech/common/engine/math/quad_impl.h"
 
 namespace Anki {
-  
+
   namespace Quad {
-    
+
     // prefix operator (++cname)
     CornerName& operator++(CornerName& cname) {
       cname = (cname < NumCorners) ? static_cast<CornerName>( static_cast<int>(cname) + 1 ) : NumCorners;
@@ -16,35 +16,35 @@ namespace Anki {
       ++cname;
       return cnameToReturn;
     }
-    
+
     CornerName operator+(CornerName& cname, int value)
     {
       const int newValue = static_cast<int>(cname) + value;
-      
+
       const CornerName newCname = (newValue < static_cast<int>(NumCorners) ?
                                    static_cast<CornerName>(newValue) :
                                    NumCorners);
-      
+
       return newCname;
     }
-    
+
   }
-  
+
   void test(void)
   {
     Quad2f q;
-    
+
     q[Quad::TopLeft] = Point2f(.1f, .3f);
-    
+
     q.SortCornersClockwise();
-    
-    Point2f n;
+
+    //Point2f n;
     //q.SortCornersClockwise(n); // Should fail: triggers static assert because N==2
-    
+
     Quad3f q3;
     Point3f n3;
     //q3.SortCornersClockwise(); // Should fail: triggers static assert because N!=2
     q3.SortCornersClockwise(n3);
   }
-  
+
 } // namespace Anki
