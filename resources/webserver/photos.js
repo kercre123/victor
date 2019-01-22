f7043641628412c23ecb9ccaedd3a71c272086b9
Brad Neuman <brad@anki.com>
2019-01-21 18:15:10 -0800

    Support click to see full size images in photos.html

--- resources/webserver/photos.js
+++ resources/webserver/photos.js
@@ -1,25 +1,48 @@
 var dir = "/persistent/photos";
 var fileextension = ".jpg";
-var thebox = false;
+var thebox = true;
 
 $.ajax({
   url: dir,
   success: function (data) {
+    /* modal code borroed heavily from https://www.w3schools.com/howto/howto_css_modal_images.asp */
+    
+    // Get the modal
+    var modal = document.getElementById('myModal');
+    var modalImg = document.getElementById("imgModal");
+
     if( !thebox) {
-      for( var i=0; i<=6; ++i ) {
-        $('.container').append(
-          '<div class="tiles" style="background: url(/prdemo_stock_images/photo' + i + '.jpg); ' +
-            'background-repeat: no-repeat; background-size: 391px 220px;"></div>');
-      }
+      [0, 1, 2, 3, 4, 5, 6].forEach(function(i) {
+        var url = '/prdemo_stock_images/photo' + i + '.jpg';
+        var img = $('<div class="tiles" style="background: url(' + url + '); ' +
+                    'background-repeat: no-repeat; background-size: 640px 360px;"></div>').appendTo($('.container'))[0];
+        img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = url;
+        }
+      });
     }
     
     $(data).find("a:contains(" + fileextension + ")").each(function () {
       var filename = this.href.replace(window.location.host, "").replace("http://", "");
       if( filename.indexOf('.thm') < 0 ) {
-        $('.container').append(
-          '<div class="tiles" style="background: url(' + filename + '); ' +
-            'background-repeat: no-repeat; background-size: 640px 360px;"></div>');
+        var img = $('<div class="tiles" style="background: url(' + filename + '); ' +
+                    'background-repeat: no-repeat; background-size: 640px 360px;"></div>').appendTo($('.container'))[0];
+
+        img.onclick = function(){
+          modal.style.display = "block";
+          modalImg.src = filename;
+        }
       }
     });
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+      modal.style.display = "none";
+    }
+
   }
 });
