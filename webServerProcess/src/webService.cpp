fb415914007ce785c652d2c345b03e985c615412
Brad Neuman <brad@anki.com>
2019-01-23 21:21:38 -0800

    Make demo.html default url for box web

--- webServerProcess/src/webService.cpp
+++ webServerProcess/src/webService.cpp
@@ -918,6 +918,7 @@ void WebService::Start(Anki::Util::Data::DataPlatform* platform, const Json::Val
 //https://ankiinc.atlassian.net/browse/VIC-1554
 //  const std::string& passwordFile = platform->pathToResource(Util::Data::Scope::Resources, "webserver/htpasswd");
 
+  
   const char *options[] = {
     "document_root",
     webserverPath.c_str(),
@@ -929,6 +930,11 @@ void WebService::Start(Anki::Util::Data::DataPlatform* platform, const Json::Val
     rewrite.c_str(),
     "static_file_max_age",
     "0",
+
+    // THEBOX: use demo.html as root url
+    "index_files",
+    "demo.html,index.html",
+    
 //https://ankiinc.atlassian.net/browse/VIC-1554
 //    "put_delete_auth_file",
 //    passwordFile.c_str(),
