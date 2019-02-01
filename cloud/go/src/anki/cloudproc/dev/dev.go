3d137baf379639e11a67b197e0988f441e69d2d5
baustinanki <baustinanki@users.noreply.github.com>
2019-02-01 11:14:18 -0800

    [box] Move CVar web handlers to distinct package (#4925)
    Having these handlers in their own package under the cvar package,
    as opposed to the cloudproc package, would make them easier to
    integrate into web interfaces for other processes (i.e. gateway).

--- cloud/go/src/anki/cloudproc/dev/dev.go
+++ cloud/go/src/anki/cloudproc/dev/dev.go
@@ -1,6 +1,7 @@
 package dev
 
 import (
+	"anki/util/cvars/web"
 	"net/http"
 	"net/http/pprof"
 )
@@ -16,6 +17,6 @@ func AddHandlers(mux *http.ServeMux) {
 	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
 	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	mux.HandleFunc("/connect.html", connectHandler)
-	mux.HandleFunc("/cvars", listCVars)
-	mux.HandleFunc("/cvars/req", cvarHandler)
+	mux.HandleFunc("/cvars", web.ListCVars)
+	mux.HandleFunc("/cvars/req", web.CVarHandler)
 }
