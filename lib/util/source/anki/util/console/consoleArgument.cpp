a090e5b2d8dbb1e97004c2eacd0d2a7b7ec2ae98
Ross Anderson <ross@anki.com>
2019-02-05 14:25:31 -0800

    VIC-13106 Dev submit alexa directives (#4959)
    This was done to help test alexa alarms set for different times
    of the year.
    + Doubles console arg length
    + Removes ',' as a token char, since it's not used by web
    + Adds option to escape a '+' in the web console func interface
    + Logs the full unparsed directive (with header) if LogAlexaDirectives
    + Sends parsed directives to webviz if LogAlexaDirectives
    + Adds console func to send unparsed directives to directive sequencer

--- lib/util/source/anki/util/console/consoleArgument.cpp
+++ lib/util/source/anki/util/console/consoleArgument.cpp
@@ -142,7 +142,7 @@ bool ConsoleArg<const char*>::SetArgValue( const std::string& textvalue )
     return true;
   }
   
-  strncpy( value_, textvalue.c_str(), 256 );
+  strncpy( value_, textvalue.c_str(), 512 );
   isSet_ = true;
   return true;
 }
