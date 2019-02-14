b1a507bd5095345fec3d4064288c46b96416b77a
Jarrod Hatfield <jarrod@anki.com>
2019-02-13 17:14:41 -0800

    [VIC-13306] Success/Fail earcon now plays immediately after streaming stops
    [VIC-13306] Voice Commands: Move Earcon Immediately After Intent Heard
    
    Previously we waited until after the wakeword listening get-out animation finished before we played our response to the cloud intent success/fail.  This gave us extra time to “listen”, but it is causing a feeling of lagginess.  Now we move the success/fail logic immediately before the get-out is played.

--- engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToVoiceCommand.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/reactions/behaviorReactToVoiceCommand.cpp
@@ -564,13 +564,6 @@ void BehaviorReactToVoiceCommand::BehaviorUpdate()
       }
     }
   }
-  else if ( _dVars.state == EState::Thinking )
-  {
-    // we may receive an intent AFTER we're done listening for various reasons,
-    // so poll for it while we're in the thinking state
-    // note: does nothing if intent is already set
-    UpdateUserIntentStatus();
-  }
 
   if ( ( _dVars.state != EState::ListeningGetIn ) && !IsControlDelegated() )
   {
@@ -750,6 +743,7 @@ void BehaviorReactToVoiceCommand::StopListening()
   OnStreamingEnd();
 
   UpdateUserIntentStatus();
+  OnVictorListeningEnd();
   TransitionToThinking();
 }
 
@@ -877,10 +871,6 @@ void BehaviorReactToVoiceCommand::TransitionToThinking()
 
   auto callback = [this]()
   {
-    // we're keeping our "listening feedback" open until the last possible moment, since the intent can come
-    // in after we've closed our recording stream.
-    OnVictorListeningEnd();
-
     const bool streamingToCloud = _dVars.expectingStream;
     if (!streamingToCloud && _iVars.exitAfterListeningIfNotStreaming) {
       PRINT_CH_INFO("Behaviors", "BehaviorReactToVoiceCommand.TransitionToThinkingCallback.NotStreaming",
@@ -892,7 +882,6 @@ void BehaviorReactToVoiceCommand::TransitionToThinking()
 
     // Play a reaction behavior if we were told to ...
     // ** only in the case that we've heard a valid intent **
-    UpdateUserIntentStatus();
     const bool heardValidIntent = ( _dVars.intentStatus == EIntentStatus::IntentHeard );
     if ( heardValidIntent && _iVars.reactionBehavior )
     {
