322a0d03d0ae8ec926fc0f8eb44f42f7e4149cc1
Brian Chapados <chapados@anki.com>
2019-02-25 11:35:33 -0800

    Rename dir to rescue, add CommandClient interface

---
+++ platform/switchboard/switchboardd/ISwitchboardCommandClient.h
@@ -0,0 +1,43 @@
+#include <signals/simpleSignal_fwd.h>
+#include <string>
+
+namespace Anki {
+
+// Forward declarations
+namespace Vector {
+namespace ExternalInterface {
+class MessageEngineToGame;
+}
+
+namespace SwitchboardInterface {
+enum class ConnectionStatus : uint8_t;
+}
+
+} // namespace Vector
+
+namespace Switchboard {
+
+class ISwitchboardCommandClient {
+  public:
+  
+  virtual bool Init() = 0;
+  virtual bool Connect() = 0;
+  virtual bool Disconnect() = 0;
+  virtual void SendMessage(const Anki::Vector::ExternalInterface::MessageGameToEngine& message) = 0;
+  virtual void SetPairingPin(std::string pin) = 0 ;
+  virtual void SendBLEConnectionStatus(bool connected) = 0;
+  virtual void ShowPairingStatus(Anki::Vector::SwitchboardInterface::ConnectionStatus status) = 0;
+  virtual void HandleWifiScanRequest() = 0;
+  virtual void HandleWifiConnectRequest(const std::string& ssid,
+                                        const std::string& pwd,
+                                        bool disconnectAfterConnection) = 0;
+  virtual void HandleHasBleKeysRequest() = 0;
+  
+  // All subclasses must implement signal accessors
+  using EngineMessageSignal = Signal::Signal<void (Anki::Vector::ExternalInterface::MessageEngineToGame)>;
+  virtual EngineMessageSignal& OnReceivePairingStatus() = 0;
+  virtual EngineMessageSignal& OnReceiveEngineMessage() = 0;
+};
+
+} // namespace Switchboard
+} // namespace Anki
