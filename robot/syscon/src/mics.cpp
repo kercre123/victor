69f94f05fbf24f15d116a2911b3f8e1d46c5a420
Bryon Vandiver <unicdk@gmail.com>
2019-02-26 14:43:02 -0800

    Checkpoint

--- robot/syscon/src/mics.cpp
+++ robot/syscon/src/mics.cpp
@@ -15,7 +15,7 @@ extern "C" {
 }
 
 const int SAMPLES_PER_IRQ = 20;
-static const int IRQS_PER_FRAME = 80 / 20;
+static const int IRQS_PER_FRAME = 80 / SAMPLES_PER_IRQ;
 static const int PDM_BYTES_PER_IRQ = 20 * AUDIO_DECIMATION * 2 / 8;
   
 static int16_t audio_data[2][AUDIO_SAMPLES_PER_FRAME * 4];
