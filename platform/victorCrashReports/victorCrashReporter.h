befbb6b1ee57fa3a4583a16cc9b91dab02a2287d
David C Mudie <dmudie@anki.com>
2019-02-28 19:45:42 -0800

    VIC-2243: Forward kernel panic logs to backtrace (#5130)
    * VIC-2243: Forward kernel panic logs to backtrace
    
    1. Add new helper vic-log-kernel-panic to generate a placeholder crash report with panic log attached.
    
    2. Revise vic-init startup script to invoke (1) on each log in /data/panics.  On success, panic log is removed so it will not be reported again.
    
    I did some refactoring in google breakpad handler so path and naming conventions could be shared between methods.

--- platform/victorCrashReports/victorCrashReporter.h
+++ platform/victorCrashReports/victorCrashReporter.h
@@ -10,6 +10,8 @@
 #ifndef __victorCrashReporter_h
 #define __victorCrashReporter_h
 
+#include <string>
+
 namespace Anki {
 namespace Vector {
 
@@ -25,6 +27,15 @@ void InstallCrashReporter(const char * filenamePrefix);
 void UninstallCrashReporter();
 
 //
+// Write a minidump into crash directory.
+// Note that path to dump is RETURNED AS OUTPUT.
+// Incoming value is ignored.
+//
+// Returns true on success, false on error.
+//
+bool WriteMinidump(const std::string & prefix, std::string & out_dump_path);
+
+//
 // Stub class to manage lifetime of crash report handlers
 // Handlers are automatically installed when object is constructed.
 // Handlers are automatically removed when object is destroyed.
@@ -34,6 +45,7 @@ class CrashReporter
 public:
   CrashReporter(const char * filenamePrefix);
   ~CrashReporter();
+
 };
 
 } // end namespace Vector
