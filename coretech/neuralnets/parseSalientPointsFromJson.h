124802717c3010c698b8a77aeb7194d5fcb172f4
Andrew Stein <andrew@anki.com>
2019-03-07 18:28:28 -0800

    Move parsers to their own h/cpp

---
+++ coretech/neuralnets/parseSalientPointsFromJson.h
@@ -0,0 +1,39 @@
+/**
+ * File: parseSalientPointsFromJson.h
+ *
+ * Author: Andrew Stein
+ * Date:   3/7/2019
+ *
+ * Description: Helpers to parse salient points, e.g. from Json delivered from "offboard" neural nets.
+ *
+ * Copyright: Anki, Inc. 2019
+ **/
+
+#ifndef __Anki_NeuralNets_ParseSalientPointsFromJson_H__
+#define __Anki_NeuralNets_ParseSalientPointsFromJson_H__
+
+#include "clad/types/offboardVision.h"
+#include "coretech/common/shared/types.h"
+#include "json/json.h"
+
+#include <list>
+
+namespace Anki {
+  
+// Forward declaration
+namespace Vision {
+  struct SalientPoint;
+}
+  
+namespace NeuralNets {
+
+Result ParseSalientPointsFromJson(const Json::Value& jsonSalientPoints,
+                                  const Vision::OffboardProcType procType,
+                                  const int imageRows, const int imageCols, const TimeStamp_t timestamp,
+                                  std::list<Vision::SalientPoint>& salientPoints);
+
+}
+}
+}
+
+#endif /* __Anki_NeuralNets_ParseSalientPointsFromJson_H__ */
