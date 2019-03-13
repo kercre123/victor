bce7cb8ab980fcb2775afd669e1b89ed76263698
Robert Cosgriff <robert@anki.com>
2019-03-13 09:48:45 -0700

    Get offboard vision mode and neural net talking to snapper

--- engine/vision/visionModesHelpers.cpp
+++ engine/vision/visionModesHelpers.cpp
@@ -28,6 +28,7 @@ static const Util::SymmetricMap<VisionMode, std::string> sNetModeLUT{
   {VisionMode::DetectingPeople, "person_detector"},
   {VisionMode::DetectingHands,  "hand_detector"},
   {VisionMode::DetectingPets,   "mobilenet"}, // TODO: Update to real network
+  {VisionMode::OffboardVision,  "offboard_person_detection"},
 };
   
 bool GetNeuralNetsForVisionMode(const VisionMode mode, std::set<std::string>& networkNames)
