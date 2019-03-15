db6cdb6ce6ce1ead4d01127205f5a32191c7facd
rakesh <rakesh.ravishankar@anki.com>
2019-03-14 20:21:24 -0700

    Added REST endpoint test

--- tools/sdk/gateway-tests/test_json_rpcs.py
+++ tools/sdk/gateway-tests/test_json_rpcs.py
@@ -302,3 +302,6 @@ def test_alexa_opt_out(vector_connection, data):
 
 def test_set_eye_color(vector_connection):
     vector_connection.send("v1/set_eye_color", p.SetEyeColorRequest(), p.SetEyeColorResponse())
+
+def test_capture_single_image(vector_connection):
+    vector_connection.send("v1/capture_single_image", p.CaptureSingleImageRequest(), p.CaptureSingleImageResponse())
