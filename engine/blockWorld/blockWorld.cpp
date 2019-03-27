3016cb148bf7cb4938b36313c55e426ed65aa099
Arjun Menon <arjun@anki.com>
2019-03-27 14:40:43 -0700

    suggestions from PR

--- engine/blockWorld/blockWorld.cpp
+++ engine/blockWorld/blockWorld.cpp
@@ -976,8 +976,6 @@ namespace Vector {
     for (const auto& objSeen : objectsSeen) {
       DEV_ASSERT(!objSeen->GetID().IsSet(), "BlockWorld.UpdateKnownObjects.SeenObjectAlreadyHasID");
 
-      const bool skipCharger = (ignoreCharger && IsChargerType(objSeen->GetType(), false));
-
       BlockWorldFilter filter;
       filter.SetAllowedTypes({objSeen->GetType()});
       const bool isUnique = objSeen->IsUnique();
@@ -1000,18 +998,19 @@ namespace Vector {
         matchingObject = FindLocatedMatchingObject(filter);
       }
 
+      // Regardless of whether we skip updating the existing object's pose
+      //  we should update the timestamps for the observations.
+      if(matchingObject != nullptr) {
+        matchingObject->SetObservationTimes(objSeen.get());
+      }
+
       // Was the camera moving? If so, we must skip this observation _unless_ this is the dock object or carry object.
       // Might be sufficient to check for movement at historical time, but to be conservative (and account for
       // timestamping inaccuracies?) we will also check _current_ moving status.
       const bool wasCameraMoving = (_robot->GetMoveComponent().IsCameraMoving() ||
                                     _robot->GetMoveComponent().WasCameraMoving(atTimestamp));
-
-      if (skipCharger || (wasCameraMoving && !isSelectedObject)) {
-        // Regardless of whether we skip updating the existing objects pose
-        //  we should update the timestamps for the observations.
-        if(matchingObject != nullptr) {
-          matchingObject->SetObservationTimes(objSeen.get());
-        }
+      const bool ignoreChargerAndIsCharger = (ignoreCharger && IsChargerType(objSeen->GetType(), false));
+      if (ignoreChargerAndIsCharger || (wasCameraMoving && !isSelectedObject)) {
         continue;
       }
       
@@ -1024,8 +1023,6 @@ namespace Vector {
         // Update the matching object's pose
         const float distToObjSeen = objSeen->GetLastPoseUpdateDistance();
         matchingObject->SetPose(objSeen->GetPose(), distToObjSeen, PoseState::Known);
-        
-        matchingObject->SetObservationTimes(objSeen.get());
         
         // If we matched an object from a previous origin, we need to move it into the current origin
         if (matchingObjectOrigin != _robot->GetWorldOriginID()) {
