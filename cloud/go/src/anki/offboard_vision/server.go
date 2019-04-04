f20831361cdb73bf16d1f4f092baa2700fb5c772
Robert Cosgriff <robert@anki.com>
2019-04-04 15:34:06 -0700

    Follow the naming convention of *_dev.go

--- cloud/go/src/anki/offboard_vision/server.go
+++ cloud/go/src/anki/offboard_vision/server.go
@@ -1,36 +1,11 @@
-// +build !:wshipping
+// +build shipping
 
 package offboard_vision
 
 import (
-  "anki/ipc"
-  "anki/log"
   "context"
 )
 
-// Run starts the offboard vision service
+// Do nothing in  a shipping build
 func Run(ctx context.Context) {
-  runServer(ctx)
-}
-
-func runServer(ctx context.Context) {
-  serv, err := ipc.NewUnixgramServer(ipc.GetSocketPath("offboard_vision_server"))
-  if err != nil {
-    log.Println("Error creating offboard vision server:", err)
-    return
-  }
-
-  if done := ctx.Done(); done != nil {
-    go func() {
-      <-done
-      serv.Close()
-    }()
-  }
-
-  log.Println("Elemental offboard vision server is running")
-
-  for c := range serv.NewConns() {
-    cl := client{Conn: c}
-    go cl.handleConn(ctx)
-  }
 }
