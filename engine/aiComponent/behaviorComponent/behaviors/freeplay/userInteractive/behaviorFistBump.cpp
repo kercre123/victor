2d5213e383dc97a2577d1378030fa7dfc77199f5
Kevin Yoon <kevin@anki.com>
2019-04-25 11:29:10 -0700

    VIC-14602: Self-confirming fistbump (#5539)
    Wait for lift to stop moving after request animation to wait out any jitter there might be in the lift that could cause it to self-confirm

--- engine/aiComponent/behaviorComponent/behaviors/freeplay/userInteractive/behaviorFistBump.cpp
+++ engine/aiComponent/behaviorComponent/behaviors/freeplay/userInteractive/behaviorFistBump.cpp
@@ -141,7 +141,7 @@ void BehaviorFistBump::BehaviorUpdate()
     return;
   }
 
-  f32 now = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
+  const f32 now = BaseStationTimer::getInstance()->GetCurrentTimeInSeconds();
   
   // Check if should exit because of pickup
   if (GetBEI().GetOffTreadsState() != OffTreadsState::OnTreads) {
@@ -244,14 +244,13 @@ void BehaviorFistBump::BehaviorUpdate()
     }
     case State::RequestingFistBump:
     {
-      auto& robotInfo = GetBEI().GetRobotInfo();
-      _dVars.waitStartTime_s = now;
-      robotInfo.GetMoveComponent().EnableLiftPower(false);
-      robotInfo.GetMoveComponent().EnableHeadPower(false);
-      
       // Play idle anim
-      DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::FistBumpIdle));
-      
+      DelegateIfInControl(new TriggerAnimationAction(AnimationTrigger::FistBumpIdle,
+                                                     1,
+                                                     true,
+                                                     (u8)AnimTrackFlag::HEAD_TRACK | (u8)AnimTrackFlag::LIFT_TRACK));
+
+      _dVars.waitStartTime_s = now;
       _dVars.state = State::WaitingForMotorsToSettle;
       break;
     }
@@ -260,11 +259,13 @@ void BehaviorFistBump::BehaviorUpdate()
       auto& robotInfo = GetBEI().GetRobotInfo();
       if (!robotInfo.GetMoveComponent().IsLiftMoving() &&
           !robotInfo.GetMoveComponent().IsHeadMoving()) {
+        robotInfo.GetMoveComponent().EnableLiftPower(false);
+        robotInfo.GetMoveComponent().EnableHeadPower(false);
         _dVars.liftWaitingAngle_rad = robotInfo.GetLiftAngle();
         _dVars.waitingAccelX_mmps2 = robotInfo.GetHeadAccelData().x;
         _dVars.state = State::WaitingForBump;
         if (now - _dVars.waitStartTime_s > kMaxTimeForMotorSettling_s) {
-          PRINT_NAMED_WARNING("BehaviorFistBump.UpdateInternal_Legacy.MotorSettleTimeTooLong", "%f", now - _dVars.waitStartTime_s);
+          PRINT_NAMED_WARNING("BehaviorFistBump.Update.MotorSettleTimeTooLong", "%f", now - _dVars.waitStartTime_s);
         }
       }
       break;
