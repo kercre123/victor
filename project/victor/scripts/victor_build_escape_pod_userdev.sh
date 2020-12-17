02b7a9fce66148b970dabfc361305dcf3c6c385f
Brett Dietsch <brettdietsch@gmail.com>
2020-12-17 11:41:18 -0500

    script fixes

--- project/victor/scripts/victor_build_escape_pod_userdev.sh
+++ project/victor/scripts/victor_build_escape_pod_userdev.sh
@@ -3,6 +3,7 @@
 export GIT_PROJ_ROOT=`git rev-parse --show-toplevel`
 ${GIT_PROJ_ROOT}/project/victor/build-victor.sh \
 		-c Release \
+		-DANKI_DISABLE_ALEXA=0 \
 		-DANKI_PRIVACY_GUARD=1 \
         -DANKI_RESOURCE_SHIPPING=1 \
 		-DREMOTE_CONSOLE_ENABLED=1 \
