9d105c85196480da5e222b3cfe793ca66eeace68
Anant Kaushik <anantkaushik.me@gmail.com>
2021-07-12 17:42:16 -0400

    Revert "Changes necessary to get victor functioning with the new display/hardware"
    This reverts commit 1508d9381b1ab36b7807bd4f5904a28e1761f000.

--- robot/core/inc/core/lcd.h
+++ robot/core/inc/core/lcd.h
@@ -3,12 +3,8 @@
 
 #include <stdint.h>
 
-#define OLD_FRAME_WIDTH     184
-#define OLD_FRAME_HEIGHT    96
-#define LCD_FRAME_WIDTH     160
-#define LCD_FRAME_HEIGHT    80
-#define PXL_CNT  (LCD_FRAME_WIDTH * LCD_FRAME_HEIGHT)
-#define OLP_CNT  (OLD_FRAME_WIDTH * OLD_FRAME_HEIGHT)
+#define LCD_FRAME_WIDTH     184
+#define LCD_FRAME_HEIGHT    96
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,15 +15,11 @@ typedef struct LcdFrame_t {
 } LcdFrame;
 
 enum LcdColor {
-  lcd_BLACK   = 0x0000,
+  lcd_BLACK = 0x0000,
   lcd_BLUE    = 0x001F,
   lcd_GREEN   = 0x07E0,
-  lcd_CYAN    = 0x7FFF,
-  lcd_GRAY    = 0x8430,
   lcd_RED     = 0xF800,
-  lcd_MAGENTA = 0xF81F,
-  lcd_YELLOW  = 0xFFE0,
-  lcd_WHITE   = 0xFFFF,
+  lcd_WHITE = 0xFFFF,
 };
 
 int lcd_init(void);
