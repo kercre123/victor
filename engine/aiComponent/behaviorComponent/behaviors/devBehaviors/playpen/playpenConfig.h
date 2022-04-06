245dc67559d63d799e7378ee2d91839d2f90bb7b
anant-kaushik <anant@digitaldreamlabs.com>
2022-04-06 15:15:20 -0400

    Updated the cube pick up starting pose

--- engine/aiComponent/behaviorComponent/behaviors/devBehaviors/playpen/playpenConfig.h
+++ engine/aiComponent/behaviorComponent/behaviors/devBehaviors/playpen/playpenConfig.h
@@ -230,7 +230,7 @@ static f32       kToolCodeDistThreshY_pix = 40.f;
 
 // ----------Pickup Cube----------
 // Expected cube pose x relative to playpen starting pose
-static f32 kExpectedCubePoseX_mm                = 520;
+static f32 kExpectedCubePoseX_mm                = 500;
 
 // Expected cube pose y relative to playpen starting pose
 static f32 kExpectedCubePoseY_mm                = 0;
