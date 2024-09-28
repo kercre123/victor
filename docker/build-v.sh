24994a1f65d1783818abf8cd439d65d6754b370d
Kerigan <kerigancreighton@gmail.com>
2024-09-28 15:57:02 -0500

    remote -it from docker

--- docker/build-v.sh
+++ docker/build-v.sh
@@ -6,6 +6,6 @@ cd $DIR
 docker build -t victor .
 docker build -t victor-${USER} -f Dockerfile.dev --build-arg USER=${USER} --build-arg UID=$(id -u $USER) --build-arg VIC_DIR=${DIR}/../ .
 docker run \
-       -u ${USER} -it \
+       -u ${USER} \
        -v ~/:/home/${USER}/:delegated \
        --privileged victor-${USER}
